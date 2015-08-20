a90ef13645927f10dd86bf7c845f82e7ac657a46
Juha Paananen <juha.paananen@gmail.com>
2015-08-20 14:41:02 +0300

    nicer progress bar :)

--- src/test/scala/fi/oph/tor/perftest/ConsoleProgressBar.scala
+++ src/test/scala/fi/oph/tor/perftest/ConsoleProgressBar.scala
@@ -2,8 +2,14 @@ package fi.oph.tor.perftest
 
 object ConsoleProgressBar {
   def showProgress[T](things: Seq[T])(block: T => Unit) = {
+    val steps = 20
     things.zipWithIndex.map { case (thing, index) =>
-      print("\rProgress: " + index + "/" + things.length)
+      val stepsCompleted = steps * index / things.length
+      print("\rProgress: [")
+      for (a <- 0 to steps - 1) {
+        print(if (a <= stepsCompleted) { "." } else { " " })
+      }
+      print("] " + index + "/" + things.length)
       block(thing)
     }
     println
