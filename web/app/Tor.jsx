d855d9c532226a5f16ad5e7b53872ac90369fd3e
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-09-10 13:49:48 +0300

    Clean up Tor.jsx

--- web/app/Tor.jsx
+++ web/app/Tor.jsx
@@ -2,38 +2,44 @@ import React from "react"
 import Bacon from "baconjs"
 import http from "axios"
 
-const events = new Bacon.Bus();
+const oppijatS = new Bacon.Bus();
 
 const OppijaHaku = React.createClass({
     render() {
-        return <div className="oppija-haku">
-            <OppijaHakuBoksi />
-            <OppijaHakutulokset oppijat={this.props.oppijat}/>
-        </div>
+        return (
+            <div className="oppija-haku">
+                <OppijaHakuBoksi />
+                <OppijaHakutulokset oppijat={this.props.oppijat}/>
+            </div>
+        )
     }
 })
 
 const OppijaHakuBoksi = React.createClass({
     render() {
-        return <div>
-            <label>Opiskelija</label>
-            <input onInput={(e) => events.push(e.target.value)}></input>
-        </div>
+        return (
+            <div>
+                <label>Opiskelija</label>
+                <input onInput={(e) => oppijatS.push(e.target.value)}></input>
+            </div>
+        )
     }
 })
 
 const OppijaHakutulokset = React.createClass({
     render() {
-        return <ul>{this.props.oppijat.map((oppija) => <li>{oppija.etunimet} {oppija.sukunimi} {oppija.hetu}</li>)}</ul>
+        const oppijat = this.props.oppijat.map((oppija) => <li>{oppija.etunimet} {oppija.sukunimi} {oppija.hetu}</li>)
+        return (
+            <ul>
+                {oppijat}
+            </ul>
+        )
     }
 })
 
-events
-    .throttle(200)
-    .flatMapLatest(q => Bacon.fromPromise(http.get(`/oppija?nimi=${q}`)))
-    .map(".data").toProperty([]).onValue((oppijat) => React.render(
-        <OppijaHaku oppijat={oppijat} />,
-        document.getElementById('content')
-    )
-)
+const oppijatP = oppijatS.throttle(200)
+    .flatMapLatest(q => Bacon.fromPromise(http.get(`/oppija?nimi=${q}`))).map(".data")
+    .toProperty([])
+
+oppijatP.onValue((oppijat) => React.render(<OppijaHaku oppijat={oppijat} />, document.getElementById('content')))
 
