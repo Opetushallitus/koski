e19c75d33e4370ac7ca2882fa52b0c1bb9233c3a
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-09-14 16:53:35 +0300

    validate username and password length

--- web/app/Login.jsx
+++ web/app/Login.jsx
@@ -7,16 +7,33 @@ const loginE = new Bacon.Bus()
 
 export const Login = React.createClass({
   render() {
+    const usernameIsValid = this.state.username && this.state.username.length > 0
+    const passwordIsValid = this.state.password && this.state.password.length > 0
+
+    debugger
     return <form className="login">
       <h3>TOR LOGIN</h3>
-      <input ref="username" placeholder="Tunnus"></input>
-      <input ref="password" placeholder="Salasana"></input>
-      <button onClick={this.doLogin}>Kirjaudu sisään</button>
+      <input onInput={this.onInput} ref="username" placeholder="Tunnus"></input>
+      <input onInput={this.onInput} ref="password" placeholder="Salasana"></input>
+      <button onClick={this.doLogin} disabled={!usernameIsValid || !passwordIsValid}>Kirjaudu sisään</button>
     </form>
   },
+
+  formState() {
+    return { username: this.refs.username.value, password: this.refs.password.value }
+  },
+
+  getInitialState() {
+    return {username: '', password: ''}
+  },
+
   doLogin(e) {
     e.preventDefault()
-    loginE.push({ username: this.refs.username.value, password: this.refs.password.value })
+    loginE.push(this.formState())
+  },
+
+  onInput() {
+    setState(this.formState())
   }
 })
 
