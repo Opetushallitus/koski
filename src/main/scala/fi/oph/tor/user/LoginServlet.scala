90579d4bdc85352c14086f2c083798477a543e7a
Juha Paananen <juha.paananen@gmail.com>
2015-09-14 16:42:51 +0300

    add dummy auth cookie to make development easier

--- src/main/scala/fi/oph/tor/user/LoginServlet.scala
+++ src/main/scala/fi/oph/tor/user/LoginServlet.scala
@@ -2,10 +2,12 @@ package fi.oph.tor.user
 
 import fi.oph.tor.ErrorHandlingServlet
 import fi.oph.tor.json.Json
+import org.scalatra.Cookie
 
 class LoginServlet extends ErrorHandlingServlet {
   post("/") {
     val login = Json.read[Login](request.body)
+    response.addCookie(Cookie("tor-auth", "balaillaan!"))
     Json.write(User("12345", login.username))
   }
 }
--- src/main/scala/fi/oph/tor/user/UserServlet.scala
+++ src/main/scala/fi/oph/tor/user/UserServlet.scala
@@ -5,7 +5,7 @@ import fi.oph.tor.json.Json
 
 class UserServlet extends ErrorHandlingServlet {
   get("/") {
-    if (false) {
+    if (request.getCookies.filter(_.getName == "tor-auth").length > 0) {
       Json.write(User("12345", "Kalle Käyttäjä"))
     } else {
       halt(401)
