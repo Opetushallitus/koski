02cef74d1ab8fd77e6d7897b4b6e66f2a793f507
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-09-15 09:59:49 +0300

    indicate failing login in ui

--- src/main/scala/fi/oph/tor/user/LoginServlet.scala
+++ src/main/scala/fi/oph/tor/user/LoginServlet.scala
@@ -7,6 +7,9 @@ import org.scalatra.Cookie
 class LoginServlet extends ErrorHandlingServlet {
   post("/") {
     val login = Json.read[Login](request.body)
+    if(login.username == "fail") {
+      halt(401, reason = "Invalid password or username")
+    }
     response.addCookie(Cookie("tor-auth", "balaillaan!"))
     Json.write(User("12345", login.username))
   }
--- web/app/Login.jsx
+++ web/app/Login.jsx
@@ -4,12 +4,19 @@ import Bacon from "baconjs"
 import Http from "./http"
 const loginE = new Bacon.Bus()
 
+const loginResultE = loginE
+    .flatMap((credentials) => Http.post("/login", credentials))
+
+export const userP = Http.get("/user").mapError()
+    .merge(loginResultE)
+    .toProperty()
+
 export const Login = React.createClass({
   render() {
     const usernameIsValid = this.state.username && this.state.username.length > 0
     const passwordIsValid = this.state.password && this.state.password.length > 0
 
-    return <form className="login">
+    return <form className={this.state.error ? "login error": "login"}>
       <h3>TOR LOGIN</h3>
       <input onInput={this.onInput} ref="username" placeholder="Tunnus"></input>
       <input onInput={this.onInput} ref="password" placeholder="Salasana"></input>
@@ -35,14 +42,7 @@ export const Login = React.createClass({
   },
 
   componentDidMount() {
+    loginResultE.onError((e) => this.setState({error: e}))
     this.refs.username.focus()
   }
 })
-
-const loginResultE = loginE
-  .flatMap((credentials) => Http.post("/login", credentials))
-
-export const userP = Http.get("/user").mapError()
-  .merge(loginResultE)
-  .toProperty()
-
