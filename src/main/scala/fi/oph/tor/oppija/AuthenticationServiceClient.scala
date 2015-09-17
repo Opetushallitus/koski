9af1b672ea9e56cf8a2af20b6cf0cee87ae0f0d8
Juha Paananen <juha.paananen@gmail.com>
2015-09-17 14:58:58 +0300

    update scala-utils version

--- src/main/scala/fi/oph/tor/oppija/AuthenticationServiceClient.scala
+++ src/main/scala/fi/oph/tor/oppija/AuthenticationServiceClient.scala
@@ -2,7 +2,7 @@ package fi.oph.tor.oppija
 
 import fi.oph.tor.json.Json._
 import fi.oph.tor.json.Json4sHttp4s.json4sOf
-import fi.vm.sade.utils.cas.{CasAbleClient, CasClient, CasParams}
+import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasClient, CasParams}
 import org.http4s.Uri._
 import org.http4s._
 import org.http4s.client.blaze
@@ -12,7 +12,7 @@ class AuthenticationServiceClient(username: String, password: String, opintoPolk
   private val blazeHttpClient: BlazeClient = blaze.defaultClient
   private val virkailijaUrl: Path = opintoPolkuVirkailijaUrl
   private val casClient = new CasClient(virkailijaUrl, blazeHttpClient)
-  private val authenticationServiceClient = new CasAbleClient(casClient, CasParams("/authentication-service", username, password), blazeHttpClient)
+  private val authenticationServiceClient = new CasAuthenticatingClient(casClient, CasParams("/authentication-service", username, password), blazeHttpClient)
   override def findOppijat(query: String): List[Oppija] = authenticationServiceClient
     .prepAs[AuthenticationServiceUserQueryResult](Request(uri = Uri.fromString(virkailijaUrl + "/authentication-service/resources/henkilo?q=" + query).toOption.get))(json4sOf[AuthenticationServiceUserQueryResult])
     .run.results.map { result => Oppija(result.sukunimi, result.etunimet, result.hetu)}
