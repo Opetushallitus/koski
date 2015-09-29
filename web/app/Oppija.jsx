db4f52aea9e5aba408cd9031bfa2578f9aa8d143
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-09-29 16:58:11 +0300

    Add required input validation to CreateOppija

--- web/app/Oppija.jsx
+++ web/app/Oppija.jsx
@@ -5,21 +5,58 @@ export const Oppija = ({oppija, oppijat}) => oppija ?
   <div className="oppija">
     <h2>{oppija.sukunimi}, {oppija.etunimet} <span className="hetu">{oppija.hetu}</span></h2>
     <hr></hr>
-  </div> : <CreateOppija oppijat={oppijat}/>
+  </div> :
+  <CreateOppija oppijat={oppijat}/>
 
+const CreateOppija = React.createClass({
+  render() {
+    const {oppijat} = this.props
+    const {firstNames, surname, callingName, ssn} = this.state
 
-const CreateOppija = ({oppijat}) => {
-  if(oppijat.query.length > 2 && oppijat.results.length === 0) {
-    return (
-      <form className="oppija stacked">
-        <label className="first-name">Etunimet<input></input></label>
-        <label className="calling-name">Kutsumanimi<input></input></label>
-        <label>Sukunimi<input></input></label>
-        <label className="ssn">Henkilötunnus<input></input></label>
-        <button className="button blue">Lisää henkilö</button>
-      </form>
-    )
-  } else {
-    return <div></div>
+    const submitDisabled = !firstNames || !surname || !callingName || !ssn
+
+    if(oppijat.query.length > 2 && oppijat.results.length === 0) {
+      return (
+        <form className="oppija stacked">
+          <label className="first-name">
+            Etunimet
+            <input ref="firstNames" onInput={this.onInput}></input>
+          </label>
+          <label className="calling-name">
+            Kutsumanimi
+            <input ref="callingName" onInput={this.onInput}></input>
+          </label>
+          <label>
+            Sukunimi
+            <input ref="surname" onInput={this.onInput}></input>
+          </label>
+          <label className="ssn">
+            Henkilötunnus
+            <input ref="ssn" onInput={this.onInput}></input>
+          </label>
+          <button className="button blue" disabled={submitDisabled}>Lisää henkilö</button>
+        </form>
+      )
+    } else {
+      return <div></div>
+    }
+  },
+
+  getInitialState() {
+    return {firstNames: '', surname: '', callingName: '', ssn: ''}
+  },
+
+  formState() {
+    return {
+      firstNames: this.refs.firstNames.value,
+      surname: this.refs.surname.value,
+      callingName: this.refs.callingName.value,
+      ssn: this.refs.ssn.value
+    }
+  },
+
+  onInput() {
+    this.setState(this.formState())
   }
-}
+
+})
