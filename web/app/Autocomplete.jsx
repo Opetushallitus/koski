d753607035911aa314b8b799fa45246b4edbfd8b
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-10-06 17:13:08 +0300

    add key bindings to autocomplete

--- web/app/Autocomplete.jsx
+++ web/app/Autocomplete.jsx
@@ -1,23 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import ReactAutocomplete from 'react-autocomplete'
 import Bacon from "baconjs"
 
 export default React.createClass({
 
   render() {
-    const {selected} = this.props
-    const {items, query} = this.state
+    let {selected} = this.props
+    let {items, query, selectionIndex} = this.state
 
-    const itemElems = items ? items.map((item, i) => {
+    let itemElems = items ? items.map((item, i) => {
         return (
-          <li key={i} onClick={this.handleSelect.bind(this, item)}>{item.nimi}</li>
+          <li key={i} className={i === selectionIndex ? "selected" : null} onClick={this.handleSelect.bind(this, item)}>{item.nimi}</li>
         )}
     ) : []
 
     return (
       <div ref="autocomplete" className="autocomplete">
-        <input className="autocomplete-input" onInput={this.handleInput} value={query ? query : selected ? selected.nimi : ''}></input>
+        <input className="autocomplete-input" onKeyDown={this.onKeyDown} onInput={this.handleInput} value={query ? query : selected ? selected.nimi : ''}></input>
         <ul className="results">{itemElems}</ul>
       </div>
     )
@@ -34,8 +33,35 @@ export default React.createClass({
     this.props.resultBus.push(selected)
   },
 
-  getInitialState() {
-    return {query: undefined, items: []}
-  }
+  onKeyDown(e) {
+    let handler = this.keyHandlers[e.key]
+    if(handler) {
+      handler.call(this, e)
+    }
+  },
 
+  getInitialState() {
+    return {query: undefined, items: [], selectionIndex: 0}
+  },
+
+  keyHandlers: {
+    ArrowUp: function() {
+      let {selectionIndex} = this.state
+      selectionIndex = selectionIndex === 0 ? 0 : selectionIndex - 1
+      this.setState({selectionIndex: selectionIndex})
+    },
+    ArrowDown: function() {
+      let {selectionIndex, items} = this.state
+      selectionIndex = selectionIndex === items.length - 1 ? selectionIndex : selectionIndex + 1
+      this.setState({selectionIndex: selectionIndex})
+    },
+    Enter: function(e) {
+      e.preventDefault()
+      let {selectionIndex, items} = this.state
+      this.handleSelect(items[selectionIndex])
+    },
+    Escape: function() {
+      this.setState({query: undefined, items: []})
+    }
+  }
 })
