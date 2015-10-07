46ffb4d08de415348931561b92ce192fa1f6b9c8
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-10-07 11:51:28 +0300

    Fix specs

--- web/test/page/addOppijaPage.js
+++ web/test/page/addOppijaPage.js
@@ -1,6 +1,8 @@
 function AddOppijaPage() {
   function form() { return S("form.oppija") }
   function button() { return form().find("button") }
+  function oppilaitos() { return form().find('.oppilaitos .selected') }
+  function tutkinto() { return form().find('.tutkinto .selected') }
   var pageApi = Page(form);
   var api = {
     isVisible: function() {
@@ -16,12 +18,17 @@ function AddOppijaPage() {
           .then(pageApi.setInputValue(".kutsumanimi input", params.kutsumanimi))
           .then(pageApi.setInputValue(".sukunimi input", params.sukunimi))
           .then(pageApi.setInputValue(".hetu input", params.hetu))
+          .then(pageApi.setInputValue(".oppilaitos input", "Helsingin"))
+          .then(wait.until(function() { return oppilaitos().is(":visible") }))
+          .then(function() {triggerEvent(oppilaitos(), 'click')})
+          .then(pageApi.setInputValue(".tutkinto input", "auto"))
+          .then(wait.until(function() { return tutkinto().is(":visible") }))
+          .then(function() {triggerEvent(tutkinto(), 'click')})
       }
     },
     submit: function() {
       triggerEvent(button(), 'click')
     },
-
     isErrorShown: function(field) {
       return function() {
         return form().find(".error-messages ." + field).is(":visible")
