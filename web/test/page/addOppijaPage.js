366efbcd7e97b092a9824375898d98729126ba06
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-10-08 12:57:10 +0300

    use single quotes

--- web/test/page/addOppijaPage.js
+++ web/test/page/addOppijaPage.js
@@ -1,52 +1,52 @@
 function AddOppijaPage() {
-  function form() { return S("form.oppija") }
-  function button() { return form().find("button") }
+  function form() { return S('form.oppija') }
+  function button() { return form().find('button') }
   function oppilaitos() { return form().find('.oppilaitos .selected') }
   function tutkinto() { return form().find('.tutkinto .selected') }
   var pageApi = Page(form)
   var api = {
     isVisible: function() {
-      return form().is(":visible")
+      return form().is(':visible')
     },
     isEnabled: function() {
-      return !button().is(":disabled")
+      return !button().is(':disabled')
     },
     tutkintoIsEnabled: function() {
-      return !S(".tutkinto input").is(":disabled")
+      return !S('.tutkinto input').is(':disabled')
     },
     enterValidData: function(params) {
       var self = this
-      params = _.merge({ etunimet: "Ossi Olavi", kutsumanimi: "Ossi", sukunimi: "Oppija", hetu: "300994-9694", oppilaitos: "Helsingin", tutkinto: "auto"}, {}, params)
+      params = _.merge({ etunimet: 'Ossi Olavi', kutsumanimi: 'Ossi', sukunimi: 'Oppija', hetu: '300994-9694', oppilaitos: 'Helsingin', tutkinto: 'auto'}, {}, params)
       return function() {
-        return pageApi.setInputValue(".etunimet input", params.etunimet)()
-          .then(pageApi.setInputValue(".kutsumanimi input", params.kutsumanimi))
-          .then(pageApi.setInputValue(".sukunimi input", params.sukunimi))
-          .then(pageApi.setInputValue(".hetu input", params.hetu))
+        return pageApi.setInputValue('.etunimet input', params.etunimet)()
+          .then(pageApi.setInputValue('.kutsumanimi input', params.kutsumanimi))
+          .then(pageApi.setInputValue('.sukunimi input', params.sukunimi))
+          .then(pageApi.setInputValue('.hetu input', params.hetu))
           .then(self.selectOppilaitos(params.oppilaitos))
           .then(self.selectTutkinto(params.tutkinto))
       }
     },
     enterTutkinto: function(name) {
       return function() {
-        return pageApi.setInputValue(".tutkinto input", name)()
+        return pageApi.setInputValue('.tutkinto input', name)()
       }
     },
     enterOppilaitos: function(name) {
       return function() {
-        return pageApi.setInputValue(".oppilaitos input", name)()
+        return pageApi.setInputValue('.oppilaitos input', name)()
       }
     },
     selectOppilaitos: function(name) {
       return function() {
-        return pageApi.setInputValue(".oppilaitos input", name)()
-          .then(wait.until(function() { return oppilaitos().is(":visible") }))
+        return pageApi.setInputValue('.oppilaitos input', name)()
+          .then(wait.until(function() { return oppilaitos().is(':visible') }))
           .then(function() {triggerEvent(oppilaitos(), 'click')})
       }
     },
     selectTutkinto: function(name) {
       return function() {
-        return pageApi.setInputValue(".tutkinto input", name)()
-          .then(wait.until(function() { return tutkinto().is(":visible") }))
+        return pageApi.setInputValue('.tutkinto input', name)()
+          .then(wait.until(function() { return tutkinto().is(':visible') }))
           .then(function() {triggerEvent(tutkinto(), 'click')})
       }
     },
@@ -55,7 +55,7 @@ function AddOppijaPage() {
     },
     isErrorShown: function(field) {
       return function() {
-        return form().find(".error-messages ." + field).is(":visible")
+        return form().find('.error-messages .' + field).is(':visible')
       }
     }
   }
--- web/test/spec/torSpec.js
+++ web/test/spec/torSpec.js
@@ -1,4 +1,4 @@
-describe("TOR", function() {
+describe('TOR', function() {
   var page = TorPage()
   var login = LoginPage()
   var authentication = Authentication()
@@ -7,297 +7,297 @@ describe("TOR", function() {
   var eerola = 'eerola, jouni '
   var teija = 'tekijä, teija 150995-914X'
 
-  describe("Login-sivu", function() {
+  describe('Login-sivu', function() {
     before(login.openPage)
-    it("näytetään, kun käyttäjä ei ole kirjautunut sisään", function() {
+    it('näytetään, kun käyttäjä ei ole kirjautunut sisään', function() {
       expect(login.isVisible()).to.equal(true)
     })
-    describe("Väärällä käyttäjätunnuksella", function() {
-      before(login.login("fail", "fail"))
+    describe('Väärällä käyttäjätunnuksella', function() {
+      before(login.login('fail', 'fail'))
       before(wait.until(login.isLoginErrorVisible))
-      it("näytetään virheilmoitus", function() {})
+      it('näytetään virheilmoitus', function() {})
     })
-    describe("Onnistuneen loginin jälkeen", function() {
-      before(login.login("kalle", "asdf"))
+    describe('Onnistuneen loginin jälkeen', function() {
+      before(login.login('kalle', 'asdf'))
       before(wait.until(page.isVisible))
-      it("siirrytään TOR-etusivulle", function() {
+      it('siirrytään TOR-etusivulle', function() {
         expect(page.isVisible()).to.equal(true)
       })
     })
   })
 
-  describe("OppijaHaku", function() {
+  describe('OppijaHaku', function() {
     before(authentication.login, resetMocks, page.openPage)
-    it("näytetään, kun käyttäjä on kirjautunut sisään", function() {
+    it('näytetään, kun käyttäjä on kirjautunut sisään', function() {
       expect(page.isVisible()).to.equal(true)
       expect(page.oppijaHaku.isNoResultsLabelShown()).to.equal(false)
     })
-    describe("Hakutulos-lista", function() {
-      it("on aluksi tyhjä", function() {
+    describe('Hakutulos-lista', function() {
+      it('on aluksi tyhjä', function() {
         expect(page.oppijaHaku.getSearchResults().length).to.equal(0)
       })
     })
-    describe("Kun haku tuottaa yhden tuloksen", function() {
-      before(page.oppijaHaku.search("esimerkki", 1))
-      it("ensimmäinen tulos näytetään", function() {
+    describe('Kun haku tuottaa yhden tuloksen', function() {
+      before(page.oppijaHaku.search('esimerkki', 1))
+      it('ensimmäinen tulos näytetään', function() {
         expect(page.oppijaHaku.getSearchResults()).to.deep.equal([eero])
         expect(page.oppijaHaku.isNoResultsLabelShown()).to.equal(false)
       })
 
-      it("ensimmäinen tulos valitaan automaattisesti", wait.until(function() { return page.getSelectedOppija() == eero }))
+      it('ensimmäinen tulos valitaan automaattisesti', wait.until(function() { return page.getSelectedOppija() == eero }))
 
-      describe("Kun haku tuottaa uudestaan yhden tuloksen", function() {
-        before(page.oppijaHaku.search("teija", 1))
-        it("tulosta ei valita automaattisesti", function() {
+      describe('Kun haku tuottaa uudestaan yhden tuloksen', function() {
+        before(page.oppijaHaku.search('teija', 1))
+        it('tulosta ei valita automaattisesti', function() {
           expect(page.getSelectedOppija()).to.equal(eero)
         })
       })
     })
-    describe("Haun tyhjentäminen", function() {
-      before(page.oppijaHaku.search("esimerkki", 1))
-      before(page.oppijaHaku.search("", 0))
+    describe('Haun tyhjentäminen', function() {
+      before(page.oppijaHaku.search('esimerkki', 1))
+      before(page.oppijaHaku.search('', 0))
 
-      it("säilyttää oppijavalinnan", function() {
+      it('säilyttää oppijavalinnan', function() {
         expect(page.getSelectedOppija()).to.equal(eero)
       })
 
-      it("tyhjentää hakutulos-listauksen", function() {
+      it('tyhjentää hakutulos-listauksen', function() {
         expect(page.oppijaHaku.getSearchResults().length).to.equal(0)
         expect(page.oppijaHaku.isNoResultsLabelShown()).to.equal(false)
       })
     })
-    describe("Kun haku tuottaa useamman tuloksen", function() {
-      before(page.oppijaHaku.search("eero", 3))
+    describe('Kun haku tuottaa useamman tuloksen', function() {
+      before(page.oppijaHaku.search('eero', 3))
 
-      it("Hakutulokset näytetään", function() {
+      it('Hakutulokset näytetään', function() {
         expect(page.oppijaHaku.getSearchResults()).to.deep.equal([eero, eerola, markkanen])
       })
 
-      before(page.oppijaHaku.selectOppija("markkanen"))
+      before(page.oppijaHaku.selectOppija('markkanen'))
 
-      it("valitsee oppijan", function() {
+      it('valitsee oppijan', function() {
         expect(page.getSelectedOppija()).to.equal(markkanen)
       })
     })
 
-    describe("Kun haku ei tuota tuloksia", function() {
-      before(page.oppijaHaku.search("asdf", page.oppijaHaku.isNoResultsLabelShown))
+    describe('Kun haku ei tuota tuloksia', function() {
+      before(page.oppijaHaku.search('asdf', page.oppijaHaku.isNoResultsLabelShown))
 
-      it("Näytetään kuvaava teksti", function() {
+      it('Näytetään kuvaava teksti', function() {
         expect(page.oppijaHaku.isNoResultsLabelShown()).to.equal(true)
       })
     })
 
-    describe("Hakutavat", function() {
-      it ("Hetulla, case-insensitive", function() {
-        return page.oppijaHaku.search("010101-123n", [eero])()
+    describe('Hakutavat', function() {
+      it ('Hetulla, case-insensitive', function() {
+        return page.oppijaHaku.search('010101-123n', [eero])()
       })
-      it ("Nimen osalla, case-insensitive", function() {
-        return page.oppijaHaku.search("JoU", [eerola])()
+      it ('Nimen osalla, case-insensitive', function() {
+        return page.oppijaHaku.search('JoU', [eerola])()
       })
-      it ("Oidilla", function() {
-        return page.oppijaHaku.search("1.2.246.562.24.00000000003", [markkanen])()
+      it ('Oidilla', function() {
+        return page.oppijaHaku.search('1.2.246.562.24.00000000003', [markkanen])()
       })
     })
   })
-  describe("Uuden oppijan lisääminen", function() {
+  describe('Uuden oppijan lisääminen', function() {
     var addOppija = AddOppijaPage()
 
     before(
       resetMocks,
       authentication.login,
       page.openPage,
-      page.oppijaHaku.search("asdf", page.oppijaHaku.isNoResultsLabelShown),
+      page.oppijaHaku.search('asdf', page.oppijaHaku.isNoResultsLabelShown),
       page.oppijaHaku.addNewOppija
     )
 
-    describe("Aluksi", function() {
-      it("Lisää-nappi on disabloitu", function() {
+    describe('Aluksi', function() {
+      it('Lisää-nappi on disabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(false)
       })
-      it("Tutkinto-kenttä on disabloitu", function() {
+      it('Tutkinto-kenttä on disabloitu', function() {
         expect(addOppija.tutkintoIsEnabled()).to.equal(false)
       })
     })
 
-    describe("Kun syötetään validit tiedot", function() {
+    describe('Kun syötetään validit tiedot', function() {
       before(addOppija.enterValidData())
 
-      it("Lisää-nappi on enabloitu", function() {
+      it('Lisää-nappi on enabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(true)
       })
 
-      describe("Kun painetaan Lisää-nappia", function() {
+      describe('Kun painetaan Lisää-nappia', function() {
         before(addOppija.submit)
-        before(wait.until(function() { return page.getSelectedOppija() === "Oppija, Ossi Olavi 300994-9694"}))
+        before(wait.until(function() { return page.getSelectedOppija() === 'Oppija, Ossi Olavi 300994-9694'}))
 
-        it("lisätty oppija näytetään", function() {})
+        it('lisätty oppija näytetään', function() {})
       })
     })
 
-    describe("Kun syötetty hetu on jo olemassa", function() {
+    describe('Kun syötetty hetu on jo olemassa', function() {
       before(
         resetMocks,
-        openPage("/tor/uusioppija"),
-        addOppija.enterValidData({ hetu: "010101-123N"}),
+        openPage('/tor/uusioppija'),
+        addOppija.enterValidData({ hetu: '010101-123N'}),
         addOppija.submit
       )
 
-      it("Näytetään virheilmoitus", function() {
-        return wait.until(addOppija.isErrorShown("hetu"))()
+      it('Näytetään virheilmoitus', function() {
+        return wait.until(addOppija.isErrorShown('hetu'))()
       })
     })
 
-    describe("Kun sessio on vanhentunut", function() {
-      before( openPage("/tor/uusioppija"),
+    describe('Kun sessio on vanhentunut', function() {
+      before( openPage('/tor/uusioppija'),
               addOppija.enterValidData(),
               authentication.logout,
               addOppija.submit)
 
-      it("Siirrytään login-sivulle", wait.until(login.isVisible))
+      it('Siirrytään login-sivulle', wait.until(login.isVisible))
     })
 
-    describe("Kun hetu on virheellinen", function() {
+    describe('Kun hetu on virheellinen', function() {
       before(
         authentication.login,
-        openPage("/tor/uusioppija"),
-        addOppija.enterValidData({hetu: "123456-1234"})
+        openPage('/tor/uusioppija'),
+        addOppija.enterValidData({hetu: '123456-1234'})
       )
-      it("Lisää-nappi on disabloitu", function() {
+      it('Lisää-nappi on disabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(false)
       })
     })
-    describe("Kun hetu sisältää väärän tarkistusmerkin", function() {
+    describe('Kun hetu sisältää väärän tarkistusmerkin', function() {
       before(
-        addOppija.enterValidData({hetu: "011095-953Z"})
+        addOppija.enterValidData({hetu: '011095-953Z'})
       )
-      it("Lisää-nappi on disabloitu", function() {
+      it('Lisää-nappi on disabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(false)
       })
     })
-    describe("Kun hetu sisältää väärän päivämäärän, mutta on muuten validi", function() {
+    describe('Kun hetu sisältää väärän päivämäärän, mutta on muuten validi', function() {
       before(
-        addOppija.enterValidData({hetu: "300275-5557"})
+        addOppija.enterValidData({hetu: '300275-5557'})
       )
-      it("Lisää-nappi on disabloitu", function() {
+      it('Lisää-nappi on disabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(false)
       })
     })
-    describe("Kun kutsumanimi ei löydy etunimistä", function() {
+    describe('Kun kutsumanimi ei löydy etunimistä', function() {
       before(
-        addOppija.enterValidData({kutsumanimi: "eiloydy"})
+        addOppija.enterValidData({kutsumanimi: 'eiloydy'})
       )
-      it("Lisää-nappi on disabloitu", function() {
+      it('Lisää-nappi on disabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(false)
       })
-      it("Näytetään virheilmoitus", function() {
-        expect(addOppija.isErrorShown("kutsumanimi")()).to.equal(true)
+      it('Näytetään virheilmoitus', function() {
+        expect(addOppija.isErrorShown('kutsumanimi')()).to.equal(true)
       })
     })
-    describe("Kun kutsumanimi löytyy väliviivallisesta nimestä", function() {
+    describe('Kun kutsumanimi löytyy väliviivallisesta nimestä', function() {
       before(
-        addOppija.enterValidData({etunimet: "Juha-Pekka", kutsumanimi: "Pekka"})
+        addOppija.enterValidData({etunimet: 'Juha-Pekka', kutsumanimi: 'Pekka'})
       )
-      it("Lisää-nappi on enabloitu", function() {
+      it('Lisää-nappi on enabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(true)
       })
     })
-    describe("Kun oppilaitos on valittu", function() {
+    describe('Kun oppilaitos on valittu', function() {
       before(addOppija.enterValidData())
-      it("voidaan valita tutkinto", function(){
+      it('voidaan valita tutkinto', function(){
         expect(addOppija.tutkintoIsEnabled()).to.equal(true)
         expect(addOppija.isEnabled()).to.equal(true)
       })
-      describe("Kun oppilaitos-valinta muutetaan", function() {
-        before(addOppija.selectOppilaitos("Omnia"))
-        it("tutkinto pitää valita uudestaan", function() {
+      describe('Kun oppilaitos-valinta muutetaan', function() {
+        before(addOppija.selectOppilaitos('Omnia'))
+        it('tutkinto pitää valita uudestaan', function() {
           expect(addOppija.isEnabled()).to.equal(false)
         })
-        describe("Tutkinnon valinnan jälkeen", function() {
-          before(addOppija.selectTutkinto("auto"))
-          it("Lisää nappi on enabloitu", function() {
+        describe('Tutkinnon valinnan jälkeen', function() {
+          before(addOppija.selectTutkinto('auto'))
+          it('Lisää nappi on enabloitu', function() {
             expect(addOppija.isEnabled()).to.equal(true)
           })
         })
       })
     })
-    describe("Kun oppilaitos on virheellinen", function() {
-      before(addOppija.enterValidData(), addOppija.enterOppilaitos("virheellinen"))
-      it("Lisää-nappi on disabloitu", function() {
+    describe('Kun oppilaitos on virheellinen', function() {
+      before(addOppija.enterValidData(), addOppija.enterOppilaitos('virheellinen'))
+      it('Lisää-nappi on disabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(false)
       })
-      it("Tutkinnon valinta on estetty", function() {
+      it('Tutkinnon valinta on estetty', function() {
         expect(addOppija.tutkintoIsEnabled()).to.equal(false)
       })
     })
-    describe("Kun tutkinto on virheellinen", function() {
-      before(addOppija.enterValidData(), addOppija.enterTutkinto("virheellinen"))
-      it("Lisää-nappi on disabloitu", function() {
+    describe('Kun tutkinto on virheellinen', function() {
+      before(addOppija.enterValidData(), addOppija.enterTutkinto('virheellinen'))
+      it('Lisää-nappi on disabloitu', function() {
         expect(addOppija.isEnabled()).to.equal(false)
       })
     })
   })
-  describe("Navigointi suoraan oppijan sivulle", function() {
+  describe('Navigointi suoraan oppijan sivulle', function() {
     before(authentication.login)
-    before(openPage("/tor/oppija/1.2.246.562.24.00000000001", page.isOppijaSelected("eero")))
+    before(openPage('/tor/oppija/1.2.246.562.24.00000000001', page.isOppijaSelected('eero')))
 
-    it("Oppijan tiedot näytetään", function() {
+    it('Oppijan tiedot näytetään', function() {
       expect(page.getSelectedOppija()).to.equal(eero)
     })
 
-    it("Hakutulos näytetään", function() {
+    it('Hakutulos näytetään', function() {
       expect(page.oppijaHaku.getSearchResults()).to.deep.equal([eero])
     })
   })
-  describe("Tietoturva", function() {
+  describe('Tietoturva', function() {
     before(login.openPage)
 
 
-    describe("Oppijarajapinta", function() {
-      before(openPage("/tor/api/oppija?query=eero", authenticationErrorIsShown))
+    describe('Oppijarajapinta', function() {
+      before(openPage('/tor/api/oppija?query=eero', authenticationErrorIsShown))
 
-      it("vaatii autentikaation", function () {
+      it('vaatii autentikaation', function () {
         expect(authenticationErrorIsShown()).to.equal(true)
       })
     })
 
-    describe("Suoritusrajapinta", function() {
-      before(openPage("/tor/api/suoritus", authenticationErrorIsShown))
+    describe('Suoritusrajapinta', function() {
+      before(openPage('/tor/api/suoritus', authenticationErrorIsShown))
 
-      it("vaatii autentikaation", function () {
+      it('vaatii autentikaation', function () {
         expect(authenticationErrorIsShown()).to.equal(true)
       })
     })
 
-    describe("Kun klikataan logout-linkkiä", function() {
+    describe('Kun klikataan logout-linkkiä', function() {
       before(authentication.login, page.openPage, page.logout)
 
-      it("Siirrytään login-sivulle", function() {
+      it('Siirrytään login-sivulle', function() {
         expect(login.isVisible()).to.equal(true)
       })
 
-      describe("Kun ladataan sivu uudelleen", function() {
-        before(openPage("/tor", login.isVisible))
+      describe('Kun ladataan sivu uudelleen', function() {
+        before(openPage('/tor', login.isVisible))
 
-        it("Sessio on päättynyt ja login-sivu näytetään", function() {
+        it('Sessio on päättynyt ja login-sivu näytetään', function() {
           expect(login.isVisible()).to.equal(true)
         })
       })
 
-      describe("Kun kirjaudutaan uudelleen sisään", function() {
-        // TODO: implement generic "wait for ajax" mechanism instead of milliseconds?
-        before(authentication.login, page.openPage, page.oppijaHaku.search("jouni", [eerola]), page.logout, login.login("kalle", "asdf"), wait.forMilliseconds(500))
-        it ("Käyttöliittymä on palautunut alkutilaan", function() {
+      describe('Kun kirjaudutaan uudelleen sisään', function() {
+        // TODO: implement generic 'wait for ajax' mechanism instead of milliseconds?
+        before(authentication.login, page.openPage, page.oppijaHaku.search('jouni', [eerola]), page.logout, login.login('kalle', 'asdf'), wait.forMilliseconds(500))
+        it ('Käyttöliittymä on palautunut alkutilaan', function() {
           expect(page.oppijaHaku.getSearchResults()).to.deep.equal([])
-          expect(page.getSelectedOppija()).to.equal("")
+          expect(page.getSelectedOppija()).to.equal('')
         })
       })
     })
 
-    describe("Session vanhennuttua", function() {
-      before(authentication.login, page.openPage, authentication.logout, page.oppijaHaku.search("eero", login.isVisible))
+    describe('Session vanhennuttua', function() {
+      before(authentication.login, page.openPage, authentication.logout, page.oppijaHaku.search('eero', login.isVisible))
 
-      it("Siirrytään login-sivulle", function() {
+      it('Siirrytään login-sivulle', function() {
         expect(login.isVisible()).to.equal(true)
       })
     })
@@ -309,5 +309,5 @@ function authenticationErrorIsShown() {
 }
 
 function resetMocks() {
-  return Q($.ajax({ url: "/tor/fixtures/reset", method: "post"}))
+  return Q($.ajax({ url: '/tor/fixtures/reset', method: 'post'}))
 }
