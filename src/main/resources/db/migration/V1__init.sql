4054bbc3784b8c00869e6d4afce5c8cf0e8a47b0
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-10-16 13:08:27 +0300

    Add constraints and unique index to table opintooikeus

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,13 @@
 create table opintooikeus (
     id serial,
     data jsonb,
-    primary key (id)
+    primary key (id),
+    CONSTRAINT validate_oid
+        CHECK (length(data->>'oppijaOid') > 0 AND (data->>'oppijaOid') IS NOT NULL ),
+    CONSTRAINT validate_peruste
+        CHECK (length(data->>'ePerusteetDiaarinumero') > 0 AND (data->>'ePerusteetDiaarinumero') IS NOT NULL ),
+    CONSTRAINT validate_organisaation
+        CHECK (length(data->>'oppilaitosOrganisaatio') > 0  AND (data->>'oppilaitosOrganisaatio') IS NOT NULL )
 );
+
+CREATE UNIQUE INDEX opinto_oikeus_unique_idx ON opintooikeus((data->>'oppijaOid'), (data->>'ePerusteetDiaarinumero'), (data->>'oppilaitosOrganisaatio'));
