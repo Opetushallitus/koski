6e0e537b911b755261e0a1e68103a741d310cd59
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-10-20 10:33:25 +0300

    Use getAuthenticatedUser Option safely

--- src/main/scala/fi/oph/tor/security/RequiresAuthentication.scala
+++ src/main/scala/fi/oph/tor/security/RequiresAuthentication.scala
@@ -1,12 +1,15 @@
 package fi.oph.tor.security
 
+import fi.oph.tor.organisaatio.OrganisaatioPuu
 import fi.oph.tor.user.{UserRepository, UserContext}
 
 trait RequiresAuthentication extends CurrentUser {
   def userRepository: UserRepository
 
   implicit def userContext: UserContext = new UserContext {
-    def organisaatioPuu = userRepository.getUserOrganisations(getAuthenticatedUser.get.oid)
+    def organisaatioPuu = getAuthenticatedUser
+      .map(u => userRepository.getUserOrganisations(u.oid))
+      .getOrElse(OrganisaatioPuu(List.empty))
 
     override def hasReadAccess(organisaatioId: String) = {
       organisaatioPuu.findById(organisaatioId).isDefined
