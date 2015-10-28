a9ccdda78d05a90cfbd47157970532272c09f753
Juha Paananen <juha.paananen@gmail.com>
2015-10-28 11:47:20 +0200

    remove dead code

--- src/main/scala/fi/oph/tor/model/Arviointi.scala
+++
@@ -1,4 +0,0 @@
-package fi.oph.tor.model
-
-// The Identified.id alias not used below, because it causes json4s-jackson serialization to hang
-case class Arviointi(id: Option[Int], asteikko: String, numero: Int, kuvaus: Option[String]) extends Identified
--- src/main/scala/fi/oph/tor/model/Suoritus.scala
+++
@@ -1,8 +0,0 @@
-package fi.oph.tor.model
-
-import java.util.Date
-
-case class Suoritus(id: Option[Int], suoritusPäivä: Option[Date], järjestäjäOrganisaatioId: String, myöntäjäOrganisaatioId: String, oppijaId: String, status: String, kuvaus: Option[String], komoto: Komoto, arviointi: Option[Arviointi],
-                            osasuoritukset: List[Suoritus]) extends Identified
-
-case class Komoto(id: Option[Int], nimi: Option[String], kuvaus: Option[String], komoId: Option[String], komoTyyppi: Option[String], koodistoId: Option[String], koodistoKoodi: Option[String], ePeruste: Option[String])
