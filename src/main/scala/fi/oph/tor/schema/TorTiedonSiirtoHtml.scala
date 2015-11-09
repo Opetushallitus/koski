5adc2f1c1faf530512002f88c27bf5bf1a2dc36b
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-11-09 09:50:20 +0200

    Fix hardcoded example

--- src/main/scala/fi/oph/tor/schema/TorTiedonSiirtoHtml.scala
+++ src/main/scala/fi/oph/tor/schema/TorTiedonSiirtoHtml.scala
@@ -25,19 +25,19 @@ JSON Schema validaattori netissä: [jsonschemavalidator.net](http://www.jsonsche
         <meta charset="UTF-8"></meta>
         <style>
           body {{
-            font-family: Helvetica, Arial, sans-serif;
-            color: rgb(51, 51, 51);
-            font-size: 15px;
+          font-family: Helvetica, Arial, sans-serif;
+          color: rgb(51, 51, 51);
+          font-size: 15px;
           }}
           small {{
-            font-size: 10px;
+          font-size: 10px;
           }}
           .json {{
-            font-family: monospace;
-            font-size: 12px;
+          font-family: monospace;
+          font-size: 12px;
           }}
           .metadata {{
-            font-family: arial;
+          font-family: arial;
           }}
           .object,.array {{ color: rgb(51, 51, 51); }}
           .json .key {{ color: rgb(165, 116, 28); }}
@@ -46,23 +46,24 @@ JSON Schema validaattori netissä: [jsonschemavalidator.net](http://www.jsonsche
           table.json {{ border-collapse: collapse; }}
           .koodisto {{ margin-left: 10px; }}
           a, a:visited {{
-            text-decoration: none;
-            color: teal;
+          text-decoration: none;
+          color: teal;
           }}
         </style>
       </head>
       <body>
         {toXHTML( knockoff(markdown) )}
         {
-          TorOppijaExamples.examples.map { example =>
-            <div>
-              <h3>{example.description} <small><a href={"/tor/documentation/examples/" + example.name + ".json"}>lataa JSON</a></small></h3>
-              <table class="json">
-                {SchemaToJsonHtml.buildHtml(Property("", TorSchema.schemaType, Nil), TorOppijaExamples.full, TorSchema.schema, 0)}
-              </table>
-            </div>
-          }
+        TorOppijaExamples.examples.map { example =>
+          <div>
+            <h3>{example.description} <small><a href={"/tor/documentation/examples/" + example.name + ".json"}>lataa JSON</a></small></h3>
+            <table class="json">
+              {SchemaToJsonHtml.buildHtml(Property("", TorSchema.schemaType, Nil), example.oppija, TorSchema.schema, 0)}
+            </table>
+          </div>
+        }
         }
       </body>
     </html>
+  }
 }
