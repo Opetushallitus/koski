5fed1c5ae803041f5dbd85d24a1125cc4575de16
Juha Paananen <juha.paananen@gmail.com>
2015-11-24 15:13:50 +0200

    protokollan kuvausta

--- src/main/scala/fi/oph/tor/schema/TorTiedonSiirtoHtml.scala
+++ src/main/scala/fi/oph/tor/schema/TorTiedonSiirtoHtml.scala
@@ -2,18 +2,35 @@ package fi.oph.tor.schema
 import com.tristanhunt.knockoff.DefaultDiscounter._
 
 object TorTiedonSiirtoHtml {
-  def markdown =
-
-"""
+  def markdown ="""
 
 # TOR-tiedonsiirtoprotokolla
 
-Tässä kuvataan TOR-järjestelmän tiedonsiirrossa käytettävä JSON-formaatti.
+Tässä dokumentissa kuvataan TOR (Todennetun Osaamisen Rekisteri) -järjestelmän tiedonsiirrossa käytettävä protokolla.
 
-JSON Schema:
+Protokolla, kuten TOR-järjestelmäkin, on työn alla, joten kaikki voi vielä muuttua.
 
-- [tor-oppija-schema.json](/tor/documentation/tor-oppija-schema.json)
-- [visualisointi ja validointi](/tor/json-schema-viewer#tor-oppija-schema.json)
+Muutama perusasia tullee kuitenkin säilymään:
+
+- Rajapinnan avulla järjestelmään voi tallentaa tietoja oppijoiden opinto-oikeuksista, opintosuorituksista ja läsnäolosta oppilaitoksissa
+- Rajapinnan avulla tietoja voi myös hakea ja muokata
+- Rajapinnan käyttö vaatii autentikoinnin ja pääsy tietoihin rajataan käyttöoikeusryhmillä. Näin ollen esimerkiksi oikeus oppilaan tietyssä oppilaitoksessa suorittamien opintojen päivittämiseen voidaan antaa kyseisen oppilaitoksen henkilöstölle
+- Rajapinta mahdollistaa myös automaattiset tiedonsiirrot tietojärjstelmien välillä. Näin esimerkiksi tietyt viranomaiset voivat saada tietoja TORista. Samoin oppilaitoksen tietojärjestelmät voivat päivittää tietoja TORiin.
+- Järjestelmä tarjoaa REST-tyyppisen tiedonsiirtorajapinnan, jossa dataformaattina on JSON
+- Samaa tiedonsiirtoprotokollaa ja dataformaattia pyritään soveltuvilta osin käyttämään sekä käyttöliittymille, jotka näyttävät tietoa loppukäyttäjille, että järjestelmien väliseen kommunikaatioon
+
+## JSON-dataformaatti
+
+Käytettävästä JSON-formaatista on laadittu työversio, jonka toivotaan vastaavan ammatillisen koulutuksen tarpeisiin.
+Tällä formaatilla siis tulisi voida siirtää tietoja ammatillista koulutusta tarjoavien koulutustoimijoiden tietojärjestelmistä TORiin ja eteenpäin
+tietoja tarvitsevien viramomaisten järjestelmiin ja loppukäyttäjiä, kuten oppilaitosten virkailijoita palveleviin käyttöliittymiin.
+
+Käytettävä JSON-dataformaatti on kuvattu [JSON-schemalla](http://json-schema.org/), jota vasten siirretyt tiedot voidaan myös automaattisesti validoida. Voit ladata TOR:ssa käytetyn scheman täältä: [tor-oppija-schema.json](/tor/documentation/tor-oppija-schema.json).
+
+Tutustuminen käytettyyn dataformaattiin onnistunee parhaiten tutustumalla schemaan [visualisointityökalun](/tor/json-schema-viewer#tor-oppija-schema.json) avulla. Tällä työkalulla voi myös validoida JSON-viestejä schemaa vasten.
+Klikkaamalla kenttiä saat näkyviin niiden tarkemmat kuvaukset.
+
+Tietokentät, joissa validit arvot on lueteltavissa, on kooditettu käyttäen hyväksi Opintopolku-järjestelmään kuuluvaa Koodistopalvelua. Esimerkki tällaisesta kentästä on tutkintoon johtavan koulutuksen [koulutuskoodi](/tor/documentation/koodisto/koulutus/latest).
 
 ## Esimerkkidata annotoituna
 
