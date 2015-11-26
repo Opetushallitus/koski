51a43a0e23e9d1f8350b548619be1ad6b4c18edf
Juha Paananen <juha.paananen@gmail.com>
2015-11-26 14:26:53 +0200

    collapse root by default

--- web/static/js/documentation.js
+++ web/static/js/documentation.js
@@ -1,38 +1,37 @@
-var isNodeClass = function(clazz) {
+function isNodeClass (clazz) {
   return clazz.startsWith('node')
 }
 
-var forEach = function(nodelist, f) {
+function forEach(nodelist, f) {
   Array.prototype.forEach.call(nodelist, f)
 }
 
-var findParentRow = function(el) {
-  while ((el = el.parentElement) && !el.classList.contains('json-row'));
-  return el
-}
-
-var clickHandler = function(e) {
-  var row = findParentRow(e.currentTarget)
-
+function toggleCollapse(row) {
   row.classList.toggle('collapsed-parent')
-
-  forEach(row.parentNode.querySelectorAll('.json-row'), function(elem) {
+  forEach(row.parentNode.querySelectorAll('.json-row'), function (elem) {
     elem.classList.remove('collapsed')
   })
-
-  forEach(row.parentNode.querySelectorAll('.collapsed-parent'), function(parent) {
-    forEach(row.parentNode.querySelectorAll('.' + parent.className.split(' ').filter(isNodeClass).join('.')), function(elem){
-      if(!elem.classList.contains('collapsed') && elem !== parent) {
+  forEach(row.parentNode.querySelectorAll('.collapsed-parent'), function (parent) {
+    forEach(row.parentNode.querySelectorAll('.' + parent.className.split(' ').filter(isNodeClass).join('.')), function (elem) {
+      if (!elem.classList.contains('collapsed') && elem !== parent) {
         elem.classList.add('collapsed')
       }
     })
   })
 }
+var clickHandler = function(e) {
+  function findParentRow(el) {
+    while ((el = el.parentElement) && !el.classList.contains('json-row'));
+    return el
+  }
+  toggleCollapse(findParentRow(e.currentTarget));
+}
 
 forEach(document.querySelectorAll('.json-row .collapsible'), function(node) {
     node.addEventListener('click', clickHandler, false)
-  }
-)
+})
+
+forEach(document.querySelectorAll('.json-row:first-child'), toggleCollapse)
 
 forEach(document.querySelectorAll('.api-tester'), function(elem) {
   elem.querySelector(".examples select").addEventListener("change", function(a,b,c) {
