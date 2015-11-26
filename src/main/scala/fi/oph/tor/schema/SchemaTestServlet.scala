c5a12ff8f5e6c6a2a494940eccb68e2a8f2d4c03
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-11-26 08:47:18 +0200

    Clean up SchemaTestServlet

--- src/main/scala/fi/oph/tor/schema/SchemaTestServlet.scala
+++ src/main/scala/fi/oph/tor/schema/SchemaTestServlet.scala
@@ -1,29 +1,28 @@
 package fi.oph.tor.schema
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT
 import com.fasterxml.jackson.databind.node.ArrayNode
-import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper, SerializationFeature}
 import com.github.fge.jackson.JsonLoader
-import com.github.fge.jsonschema.main.{JsonSchemaFactory, JsonValidator}
+import com.github.fge.jsonschema.core.report.LogLevel.ERROR
+import com.github.fge.jsonschema.main.JsonSchemaFactory
 import fi.oph.tor.ErrorHandlingServlet
 
 import scala.collection.JavaConversions._
 
 class SchemaTestServlet extends ErrorHandlingServlet {
 
-  private val validator: JsonValidator = JsonSchemaFactory.byDefault.getValidator
-  private val schema: JsonNode =  JsonLoader.fromString(TorSchema.schemaJsonString)
-  private val mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT)
+  private val schema = JsonSchemaFactory.byDefault.getJsonSchema(JsonLoader.fromString(TorSchema.schemaJsonString))
+  private val mapper = new ObjectMapper().enable(INDENT_OUTPUT)
 
   put("/dev/null") {
-    val oppija = JsonLoader.fromString(request.body)
-    val report = validator.validate(schema, oppija)
+    val report = schema.validate(JsonLoader.fromString(request.body))
 
     if(!report.isSuccess) {
       val errorNodes: ArrayNode = mapper.createArrayNode()
-      val errorReport = mapper.createObjectNode().set("errors", errorNodes)
-      report.filter(message => message.getLogLevel.toString == "error").map(_.asJson).foreach(errorNodes.add)
+      report.filter(message => message.getLogLevel == ERROR).map(_.asJson).foreach(errorNodes.add)
 
-      halt(400, mapper.writeValueAsString(errorReport))
+      halt(400, mapper.writeValueAsString(mapper.createObjectNode().set("errors", errorNodes)))
     }
   }
 }
