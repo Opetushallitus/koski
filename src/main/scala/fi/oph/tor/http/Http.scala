414d57d3df95f392f53b69894f966c4bc502f1a7
Juha Paananen <juha.paananen@gmail.com>
2015-12-04 14:50:16 +0200

    refactor http

--- src/main/scala/fi/oph/tor/http/Http.scala
+++ src/main/scala/fi/oph/tor/http/Http.scala
@@ -1,7 +1,9 @@
 package fi.oph.tor.http
 
 import fi.oph.tor.json.Json
-import org.http4s.{Uri, Response, Request}
+import org.http4s.Method.PermitsBody
+import org.http4s.Method.Semantics.Default
+import org.http4s.{Method, Uri, Response, Request}
 import org.http4s.client.Client
 import scalaz.concurrent.Task
 import org.http4s.client.blaze
@@ -45,6 +47,25 @@ case class Http(client: Client = blaze.defaultClient) {
 
   def apply[ResultType](uri: String)(decode: (Int, String) => ResultType): ResultType = {
     apply(Task(Request(uri = Http.uriFromString(uri))))(decode)
+  }
+
+  def post[T <: AnyRef](path: Uri, entity: T)(implicit mf: Manifest[T]): Unit = {
+    apply(path, Method.POST, entity)
+  }
+
+  def put[T <: AnyRef](path: Uri, entity: T)(implicit mf: Manifest[T]): Unit = {
+    apply(path, Method.PUT, entity)
+  }
+
+  def apply[T <: AnyRef](path: Uri, method: Method, entity: T)(implicit mf: Manifest[T]): Unit = {
+    import fi.oph.tor.json.Json._
+    import fi.oph.tor.json.Json4sHttp4s._
+    val task: Task[Request] = Request(uri = path, method = method).withBody(entity)(json4sEncoderOf[T])
+
+    apply(task) {
+      case (status, text) if (status >= 300) => throw new scala.RuntimeException(status + ": " + text)
+      case _ =>
+    }
   }
 
   private def runHttp[ResultType](task: Task[Response])(block: (Int, String) => ResultType): ResultType = {
--- src/main/scala/fi/oph/tor/koodisto/RemoteKoodistoPalvelu.scala
+++ src/main/scala/fi/oph/tor/koodisto/RemoteKoodistoPalvelu.scala
@@ -2,9 +2,6 @@ package fi.oph.tor.koodisto
 
 import fi.oph.tor.http.{Http, VirkailijaHttpClient}
 import fi.vm.sade.utils.slf4j.Logging
-import org.http4s.{Method, Request}
-
-import scalaz.concurrent.Task
 
 class RemoteKoodistoPalvelu(username: String, password: String, virkailijaUrl: String) extends LowLevelKoodistoPalvelu with Logging {
   val virkalijaClient = new VirkailijaHttpClient(username, password, virkailijaUrl, "/koodisto-service")
@@ -26,24 +23,13 @@ class RemoteKoodistoPalvelu(username: String, password: String, virkailijaUrl: S
   private def noCache = "?noCache=" + System.currentTimeMillis()
 
   def createKoodisto(koodisto: Koodisto): Unit = {
-    post("/koodisto-service/rest/codes", koodisto)
+    http.post(virkalijaClient.virkailijaUriFromString("/koodisto-service/rest/codes"), koodisto)
   }
 
 
   def createKoodi(koodistoUri: String, koodi: KoodistoKoodi) = {
-    post("/koodisto-service/rest/codeelement/" + koodistoUri, koodi)
+    http.post(virkalijaClient.virkailijaUriFromString("/koodisto-service/rest/codeelement/" + koodistoUri), koodi)
   }
-
-  def post[T <: AnyRef](path: String, entity: T)(implicit mf: Manifest[T]): Unit = {
-    import fi.oph.tor.json.Json._
-    import fi.oph.tor.json.Json4sHttp4s._
-    val task: Task[Request] = Request(uri = virkalijaClient.virkailijaUriFromString(path), method = Method.POST).withBody(entity)(json4sEncoderOf[T])
-
-    http(task) { case (status, text) if (status < 300) => logger.info("Created " + entity)
-    case (status, text) => throw new scala.RuntimeException(status + ": " + text)
-    }
-  }
-
 }
 
 case class KoodistoWithLatestVersion(latestKoodistoVersio: LatestVersion)
