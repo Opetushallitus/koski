b623392725f9e4a52bb24c1a5362f635e098ee11
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-12-07 16:13:25 +0200

    fix bash options

--- scripts/mocha-phantom-test.sh
+++ scripts/mocha-phantom-test.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -e -o pipefail
-
+#!/bin/bash
+set -eou pipefail
 
 (cd $(dirname "$0")/../web && node_modules/mocha-phantomjs/bin/mocha-phantomjs $1 -R xunit | grep -E '</?test' > target/test-report.xml)
 
