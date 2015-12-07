ae02693b2bedd7259d7a7e3a274343857625fb42
Juha Paananen <juha.paananen@gmail.com>
2015-12-07 15:38:09 +0200

    fix visibility of 404 element

--- web/app/Error.jsx
+++ web/app/Error.jsx
@@ -41,4 +41,4 @@ export const Error = ({error}) => {
   return errorTexts[error.httpStatus] ? <div id="error" className="error">{errorTexts[error.httpStatus]}<a>&#10005;</a></div> : <div id="error"></div>
 }
 
-export const NotFound = () => <div className="not-found"><h1>404</h1><div className="error-message">Etsimääsi sivua ei löytynyt</div></div>
+export const NotFound = () => <div className="not-found content-area"><h1>404</h1><div className="error-message">Etsimääsi sivua ei löytynyt</div></div>
