e9bb63aee3fea42e6d766141de66a3918c2195fe
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-12-07 16:29:30 +0200

    still trying to get the path right...

--- scripts/mocha-phantom-test.sh
+++ scripts/mocha-phantom-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eou pipefail
 
-(cd $(dirname "$0")/../web && node_modules/mocha-phantomjs/bin/mocha-phantomjs $1 -R xunit | grep -E '</?test' > test-report.xml)
+(cd $(dirname "$0")/../web && node_modules/mocha-phantomjs/bin/mocha-phantomjs $1 -R xunit | grep -E '</?test' > ./target/test-report.xml)
 
