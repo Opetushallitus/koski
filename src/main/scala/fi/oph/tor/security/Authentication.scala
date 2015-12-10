e451a97ac59acb75ad22f052b9ccccbb9c566ec8
Juha Paananen <juha.paananen@gmail.com>
2015-12-10 15:55:42 +0200

    60 second cache for LDAP results

--- src/main/scala/fi/oph/tor/security/Authentication.scala
+++ src/main/scala/fi/oph/tor/security/Authentication.scala
@@ -1,18 +1,16 @@
 package fi.oph.tor.security
 
 import com.typesafe.config.Config
+import fi.oph.tor.util.CachingProxy
 import fi.vm.sade.security.ldap.{LdapConfig, LdapClient, LdapUser, DirectoryClient}
 import fi.vm.sade.security.mock.MockDirectoryClient
 
 object Authentication {
   def directoryClient(config: Config): DirectoryClient = {
-    if (config.hasPath("ldap")) {
+    CachingProxy(config, if (config.hasPath("ldap")) {
       new LdapClient(LdapConfig(config.getString("ldap.host"), config.getString("ldap.userdn"), config.getString("ldap.password")))
     } else {
-      new MockDirectoryClient(Map(
-        "kalle" -> LdapUser(List(), "käyttäjä", "kalle", "12345"),
-        "hiiri" -> LdapUser(List(), "käyttäjä", "hiiri", "11111")
-      ))
-    }
+      new MockDirectoryClient(Map("kalle" -> LdapUser(List(), "käyttäjä", "kalle", "12345"), "hiiri" -> LdapUser(List(), "käyttäjä", "hiiri", "11111")))
+    })
   }
 }
