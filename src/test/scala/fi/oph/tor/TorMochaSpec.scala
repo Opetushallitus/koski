06f3fae7ad2c6232314157790cc788d3fb46647e
Juha Paananen <juha.paananen@gmail.com>
2015-12-17 10:00:10 +0200

    log test result location when mocha fails

--- src/test/scala/fi/oph/tor/TorMochaSpec.scala
+++ src/test/scala/fi/oph/tor/TorMochaSpec.scala
@@ -7,8 +7,14 @@ import scala.sys.process._
 class TorMochaSpec extends FreeSpec with Matchers {
   "Mocha-testit" - {
     SharedJetty.start
+    log("running mocha-phantom")
     val command = Seq("scripts/mocha-phantom-test.sh", SharedJetty.baseUrl + "/test/runner.html")
     val res = command.!
+    if (res != 0) {
+      log("Mocha test result written to web/target/test-report.xml")
+    }
     res should equal (0)
   }
+
+  private def log(x: String) = println(s"********** mocha test: $x **********")
 }
