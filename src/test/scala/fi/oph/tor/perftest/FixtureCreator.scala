0dd88c8ea57b599e8e0947589eea062b11423381
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-12-17 13:55:31 +0200

    Introduce FixtureCreator

---
+++ src/test/scala/fi/oph/tor/perftest/FixtureCreator.scala
@@ -0,0 +1,65 @@
+package fi.oph.tor.perftest
+
+import java.util.concurrent.{TimeUnit, ExecutorService, Executors}
+
+import com.unboundid.util.Base64
+import fi.oph.tor.http.Http
+import fi.oph.tor.json.Json
+import fi.oph.tor.json.Json._
+import fi.oph.tor.schema.{Henkilö, OpiskeluOikeus, TorOppija}
+import org.scalatra.test.HttpComponentsClient
+
+import scala.collection.mutable
+import scala.util.Random.{nextInt => randomInt}
+
+object FixtureCreator extends App with HttpComponentsClient {
+
+  // echo "[" > dumpi.txt; grep INSERT opiskeluoikeus.pg | sed -E "s/INSERT INTO opiskeluoikeus \(id, data, oppija_oid\) VALUES \([0-9]+, '//g" | rev | cut -c 34- | rev | sed 's/$/,/' | sed -E '$s/,$//' >> dumpi.txt; echo "]" >> dumpi.txt
+
+  val oikeudet = Json.readFile("src/test/resources/opiskeluoikeudet.txt").extract[List[OpiskeluOikeus]]
+  var hetut = new mutable.Stack[String]
+  val pool: ExecutorService = Executors.newFixedThreadPool(10)
+  val amount = 100000
+
+  val t0 = System.currentTimeMillis()
+
+  1 to amount foreach { x =>
+    val oikeus: OpiskeluOikeus = oikeudet(randomInt(oikeudet.length))
+    val nimi = "Tor-Perf-" + x
+    val oppija: TorOppija = TorOppija(Henkilö(nextHetu, nimi, nimi, nimi), List(oikeus))
+    val body = Json.write(oppija).getBytes("utf-8")
+
+
+    pool.execute(new Runnable {
+      override def run(): Unit = {
+        put("api/oppija", body = body, headers = (authHeaders + ("Content-type" -> "application/json"))) {
+          if(response.status != 200) {
+            println(oikeus.id + " failed")
+            println(response.body)
+          }
+          println(nimi + " " + response.status)
+        }
+      }
+    })
+  }
+
+  pool.shutdown()
+  pool.awaitTermination(48, TimeUnit.HOURS)
+  println("Created " + amount + " opinto-oikeus in " + (System.currentTimeMillis() - t0) + "ms")
+
+  def nextHetu = {
+    hetut.synchronized {
+      if(hetut.isEmpty) {
+        hetut.pushAll(Http("http://www.telepartikkeli.net/tunnusgeneraattori/api")("/generoi/hetu/1000")(Http.parseJson[List[String]]).run)
+      }
+      hetut.pop()
+    }
+  }
+
+  def authHeaders = {
+    val auth: String = "Basic " + Base64.encode("kalle:kalle".getBytes("UTF8"))
+    Map("Authorization" -> auth)
+  }
+
+  override def baseUrl = "http://localhost:7021/tor"
+}
