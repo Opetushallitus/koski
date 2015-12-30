8c2b6d3d6d33cbd169b228a9ea4ef8f1d0bc7a4b
Kalle Kainu <kalle.kainu@reaktor.fi>
2015-12-30 13:48:37 +0200

    Retrieve one oppija in perf test

--- perftest/src/test/scala/tor/TorSimulation.scala
+++ perftest/src/test/scala/tor/TorSimulation.scala
@@ -2,20 +2,27 @@ package tor
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import tor.Scenarios.openFrontPage
+import io.gatling.http.request.builder.{Http, HttpRequestBuilder}
+import tor.Scenarios.findOppija
 import scala.concurrent.duration._
 
 
 class TorSimulation extends Simulation {
   val httpConf = http
-    .baseURL("http://tordev.tor.oph.reaktor.fi/tor/")
+    .baseURL("http://tordev.tor.oph.reaktor.fi/tor")
     .acceptEncodingHeader("gzip, deflate")
 
   val headers = Map("Content-Type" -> """application/x-www-form-urlencoded""")
 
-  setUp(openFrontPage.inject(atOnceUsers(1)).protocols(httpConf))
+
+  println(Scenarios.username, Scenarios.password)
+  setUp(findOppija.inject(atOnceUsers(1)).protocols(httpConf))
 }
 
 object Scenarios {
-  val openFrontPage = scenario("Open frontpage").exec(http("frontpage").get("/"))
+
+  val username = sys.env("TOR_USER")
+  val password = sys.env("TOR_PASS")
+
+  val findOppija = scenario("Find oppija").exec(http("/api/oppija/:oid").get("/api/oppija/1.2.246.562.24.10031632987").basicAuth(username, password))
 }
