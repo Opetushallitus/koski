6f69cc5f1748efc03724fb3174056cad0725f53f
Juha Paananen <juha.paananen@gmail.com>
2016-01-12 12:07:11 +0200

    add UpdateSimulation, increment alkupäivä field when updating sequentially

--- perftest/src/test/scala/tor/Scenarios.scala
+++ perftest/src/test/scala/tor/Scenarios.scala
@@ -1,10 +1,13 @@
 package tor
 
+import java.time.LocalDate
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ning.http.client.RequestBuilder
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.http.request.Body
 
 object Scenarios {
-
   val username = sys.env("TOR_USER")
   val password = sys.env("TOR_PASS")
 
@@ -14,17 +17,38 @@ object Scenarios {
       .basicAuth(username, password)
   )
 
-  val updateOppija = scenario("Update oppija").exec(
-    http("update")
-      .put("/api/oppija")
-      .body(RawFileBody("oppija.json")).asJSON
-      .basicAuth(username, password)
-  )
+  val updateOppija = scenario("Update oppija")
+        .feed(jsonFile("src/test/resources/bodies/oppija.json").circular)
+        .exec(
+          http("update")
+            .put("/api/oppija")
+            .body(OppijaWithOpiskeluoikeusWithIncrementingStartdate).asJSON
+            .basicAuth(username, password)
+        )
+
+
 
   val queryOppijat = scenario("Query oppijat").exec(
     http("query oppijat")
       .get("/api/oppija?opiskeluoikeusPäättynytAikaisintaan=2016-01-10&opiskeluoikeusPäättynytViimeistään=2016-01-10")
       .basicAuth(username, password)
   )
+}
 
+object OppijaWithOpiskeluoikeusWithIncrementingStartdate extends Body {
+  var dateCounter = LocalDate.parse("2012-09-01")
+  type Map = java.util.LinkedHashMap[Any, Any]
+  type Array = java.util.List[Any]
+
+  private def nextDate = this.synchronized {
+    dateCounter = dateCounter.plusDays(1)
+    dateCounter.toString
+  }
+
+  override def setBody(req: RequestBuilder, session: Session) = {
+    val contentMap = session.apply("content").as[Map]
+    contentMap.get("opiskeluoikeudet").asInstanceOf[Array].get(0).asInstanceOf[Map].put("alkamispäivä", nextDate)
+    val contentBytes = new ObjectMapper().writeValueAsBytes(contentMap)
+    req.setBody(contentBytes)
+  }
 }
---
+++ perftest/src/test/scala/tor/UpdateSimulation.scala
@@ -0,0 +1,14 @@
+package tor
+
+import io.gatling.core.Predef._
+import tor.Scenarios._
+
+import scala.concurrent.duration._
+
+class UpdateSimulation extends TorSimulation {
+  setUp(
+    updateOppija.inject(nothingFor(5 seconds), atOnceUsers(1), nothingFor(30 seconds), constantUsersPerSec(1) during(1 minute) randomized)
+  ).protocols(httpConf)
+}
+
+
