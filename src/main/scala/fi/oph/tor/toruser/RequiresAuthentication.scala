836b9ab2429f34432e4dfaca7d58cc858e91142f
Juha Paananen <juha.paananen@gmail.com>
2016-01-13 11:01:54 +0200

    fix TorUser when running in background thread

--- src/main/scala/fi/oph/tor/toruser/RequiresAuthentication.scala
+++ src/main/scala/fi/oph/tor/toruser/RequiresAuthentication.scala
@@ -6,17 +6,15 @@ import fi.oph.tor.organisaatio.InMemoryOrganisaatioRepository
 trait RequiresAuthentication extends ErrorHandlingServlet with AuthenticationSupport {
   def userRepository: UserOrganisationsRepository
 
-  implicit def userContext: TorUser = new TorUser {
-    def userOrganisations: InMemoryOrganisaatioRepository = userOption
-      .map(u => userRepository.getUserOrganisations(u.oid))
-      .getOrElse(InMemoryOrganisaatioRepository.empty)
-
-    override def user = scentry.user
-  }
+  implicit def userContext: TorUser = new UserImpl(userOption.get, userRepository)
 
   before() {
     if(!isAuthenticated) {
       scentry.authenticate()
     }
   }
+}
+
+class UserImpl(val user: AuthenticationUser, userRepository: UserOrganisationsRepository) extends TorUser {
+  lazy val userOrganisations: InMemoryOrganisaatioRepository = userRepository.getUserOrganisations(user.oid)
 }
