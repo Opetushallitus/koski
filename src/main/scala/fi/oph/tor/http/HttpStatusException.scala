e8f7fb08394f30a21e7d3bdcb92fa30ed2b408c4
Juha Paananen <juha.paananen@gmail.com>
2016-01-18 15:21:08 +0200

    make HttpStatusException Loggable

--- src/main/scala/fi/oph/tor/http/HttpStatusException.scala
+++ src/main/scala/fi/oph/tor/http/HttpStatusException.scala
@@ -1,5 +1,8 @@
 package fi.oph.tor.http
 
+import fi.oph.tor.log.Loggable
 import org.http4s.Request
 
-case class HttpStatusException(status: Int, text: String, request: Request) extends RuntimeException(status + ": " + text + " when requesting " + request.method + " " + request.uri)
+case class HttpStatusException(status: Int, text: String, request: Request) extends RuntimeException(status + ": " + text + " when requesting " + request.method + " " + request.uri) with Loggable {
+  override def toString = getMessage
+}
