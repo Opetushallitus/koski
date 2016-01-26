0d80e4fe42936c47566590031edc84671609287d
Juha Paananen <juha.paananen@gmail.com>
2016-01-26 15:34:08 +0200

    improve logging of http4s ParseException, to get CAS failure details

--- src/main/scala/fi/oph/tor/log/LoggableThrowableRenderer.scala
+++ src/main/scala/fi/oph/tor/log/LoggableThrowableRenderer.scala
@@ -3,6 +3,7 @@ package fi.oph.tor.log
 import org.apache.commons.lang.exception.ExceptionUtils
 import org.apache.log4j.DefaultThrowableRenderer
 import org.apache.log4j.spi.ThrowableRenderer
+import org.http4s.ParseException
 
 class LoggableThrowableRenderer extends ThrowableRenderer {
 
@@ -12,8 +13,11 @@ class LoggableThrowableRenderer extends ThrowableRenderer {
     case t: Loggable => omitStackTrace(t)
     case t: java.util.concurrent.TimeoutException => omitStackTrace(t)
     case t: java.sql.SQLTimeoutException => omitStackTrace(t)
+    case t: ParseException => renderer.doRender(t) ++ Array("Details :" +  t.failure.details)
     case _ => renderer.doRender(t)
   }
 
-  def omitStackTrace(t: Throwable) = Array(t.getClass.getName + ": " + t.toString)
+  def omitStackTrace(t: Throwable) = Array(signature(t))
+
+  def signature(t: Throwable): String = t.getClass.getName + ": " + t.toString
 }
