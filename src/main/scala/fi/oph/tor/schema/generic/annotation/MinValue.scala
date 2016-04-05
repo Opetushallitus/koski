5ceca52cd563e236a92918b3f83273c17c2980c2
Juha Paananen <juha.paananen@gmail.com>
2016-04-05 15:11:51 +0300

    imports

--- src/main/scala/fi/oph/tor/schema/generic/annotation/MinValue.scala
+++ src/main/scala/fi/oph/tor/schema/generic/annotation/MinValue.scala
@@ -1,6 +1,6 @@
 package fi.oph.tor.schema.generic.annotation
 
-import fi.oph.tor.schema.generic.{Metadata, MetadataSupport, ObjectWithMetadata, SchemaFactory}
+import fi.oph.tor.schema.generic.{Metadata, MetadataSupport}
 import org.json4s.JsonAST.{JDouble, JObject}
 
 object MinValue extends MetadataSupport[MinValue] {
