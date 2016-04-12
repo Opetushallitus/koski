c8f9347076106d557361f99b737c192c938afcf4
Kalle Kainu <kalle.kainu@reaktor.fi>
2016-04-12 14:41:56 +0300

    Add lukiokurssit example-data

--- src/main/scala/fi/oph/tor/documentation/ExamplesLukio.scala
+++ src/main/scala/fi/oph/tor/documentation/ExamplesLukio.scala
@@ -68,24 +68,120 @@ object ExamplesLukio {
               kurssisuoritus(valtakunnallinenKurssi("ÄI8")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
               kurssisuoritus(valtakunnallinenKurssi("ÄI9")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9))
             ))),
-            suoritus(kieli("A1", "EN")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(kieli("B1", "SV")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
-            suoritus(kieli("B3", "LA")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(matematiikka("MAA")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(oppiaine("BI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(oppiaine("GE")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
-            suoritus(oppiaine("FY")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
-            suoritus(oppiaine("KE")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
-            suoritus(uskonto("KT1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
-            suoritus(oppiaine("FI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
-            suoritus(oppiaine("PS")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(oppiaine("HI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
-            suoritus(oppiaine("YH")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
-            suoritus(oppiaine("LI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(oppiaine("MU")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
-            suoritus(oppiaine("KU")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(oppiaine("TE")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
-            suoritus(oppiaine("TE")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9))
+            suoritus(kieli("A1", "EN")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("ENA1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(10)),
+              kurssisuoritus(valtakunnallinenKurssi("ENA2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(10)),
+              kurssisuoritus(valtakunnallinenKurssi("ENA3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("ENA4")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("ENA5")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("ENA6")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("ENA7")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("ENA8")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(paikallinenKurssi("ENA 10", "Abituki")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S")) // 0.5
+            ))),
+            suoritus(kieli("B1", "SV")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("RUB11")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("RUB12")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("RUB13")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("RUB14")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("RUB15")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(6))
+            ))),
+            suoritus(kieli("B3", "LA")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("LAB31")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("LAB32")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8))
+            ))),
+            suoritus(matematiikka("MAA")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(paikallinenKurssi("MAA1", "Funktiot ja yhtälöt, pa, vuositaso 1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(10)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA4")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(10)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA5")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA6")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA7")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA8")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA9")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA10")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA11")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA12")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(10)),
+              kurssisuoritus(valtakunnallinenKurssi("MAA13")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(paikallinenKurssi("MAA14", "Kertauskurssi, ksy, vuositaso 3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(paikallinenKurssi("MAA16", "Analyyttisten menetelmien lisäkurssi, ksy, vuositaso 2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9))
+            ))),
+            suoritus(oppiaine("BI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("BI1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("BI2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("BI3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("BI4")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("BI5")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(10)),
+              kurssisuoritus(paikallinenKurssi("BI6", "Cell Biology (½ kurssia), so, vuositaso 3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S")), // 0.5
+              kurssisuoritus(paikallinenKurssi("BI7", "Biologia nova - ympäristö tutuksi (1-3 kurssia), so, vuositasot 1-2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S")),
+              kurssisuoritus(paikallinenKurssi("BI8", "Biologian kertauskurssi (½ kurssia), so, vuositaso 3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S"))
+            ))),
+            suoritus(oppiaine("GE")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("GE1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("GE2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7))
+            ))),
+            suoritus(oppiaine("FY")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("FY1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("FY2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("FY3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("FY4")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("FY5")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("FY6")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("FY7")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(paikallinenKurssi("FY8", "Aine ja säteily, sy, vuositaso 3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(paikallinenKurssi("FY9", "Kokeellinen fysiikka, so, vuositaso 2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(paikallinenKurssi("FY10", "Lukion fysiikan kokonaiskuva, so, vuositaso 3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S")),
+              kurssisuoritus(paikallinenKurssi("FY11", "Fysiikka 11")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S")),
+              kurssisuoritus(paikallinenKurssi("FY12", "Fysiikka 12")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S")),
+              kurssisuoritus(paikallinenKurssi("FY13", "Fysiikka 13")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S"))
+            ))),
+            suoritus(oppiaine("KE")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("KE1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("KE2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("KE3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(valtakunnallinenKurssi("KE4")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(5)),
+              kurssisuoritus(valtakunnallinenKurssi("KE5")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(paikallinenKurssi("KE6", "Kokeellinen kemia, so, vuositasot 2-3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(5)),
+              kurssisuoritus(paikallinenKurssi("KE7", "Lukion kemian kokonaiskuva, so, vuositaso 3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S")),
+              kurssisuoritus(paikallinenKurssi("KE8", "Kemia 8")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S"))
+            ))),
+            suoritus(uskonto("KT1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("UE1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("UE2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("UE3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8))
+            ))),
+            suoritus(oppiaine("FI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("FI1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8))
+            ))),
+            suoritus(oppiaine("PS")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("PS1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9))
+            ))),
+            suoritus(oppiaine("HI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("HI1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("HI2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("HI3")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(7)),
+              kurssisuoritus(valtakunnallinenKurssi("HI4")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(6))
+            ))),
+            suoritus(oppiaine("YH")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("YH1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("YH2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8))
+            ))),
+            suoritus(oppiaine("LI")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("LI1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("LI2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)),
+              kurssisuoritus(paikallinenKurssi("LI12", "Vanhat tanssit, kso")).copy(vahvistus = vahvistus).copy(arviointi = arviointi("S"))
+            ))),
+            suoritus(oppiaine("MU")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("MU1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8))
+            ))),
+            suoritus(oppiaine("KU")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("KU1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8)),
+              kurssisuoritus(valtakunnallinenKurssi("KU2")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9))
+            ))),
+            suoritus(oppiaine("TE")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(9)).copy(osasuoritukset = Some(List(
+              kurssisuoritus(valtakunnallinenKurssi("TE1")).copy(vahvistus = vahvistus).copy(arviointi = arviointi(8))
+            )))
             // TODO, opinto-ohjaus, kansalaisen turvakurssit
           ))
         )
