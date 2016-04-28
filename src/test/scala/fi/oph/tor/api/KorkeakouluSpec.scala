1d39348ddb30b1b9465a18d2585869cdc1727b91
Juha Paananen <juha.paananen@gmail.com>
2016-04-28 11:19:50 +0300

    first stab at testing conversion from Virta data

--- src/test/scala/fi/oph/tor/api/KorkeakouluSpec.scala
+++ src/test/scala/fi/oph/tor/api/KorkeakouluSpec.scala
@@ -1,6 +1,7 @@
 package fi.oph.tor.api
 
 import fi.oph.tor.http.TorErrorCategory
+import fi.oph.tor.oppija.MockOppijat
 import org.scalatest.{FunSpec, Matchers}
 
 class KorkeakouluSpec extends FunSpec with Matchers with OpiskeluoikeusTestMethodsKorkeakoulu {
@@ -10,6 +11,14 @@ class KorkeakouluSpec extends FunSpec with Matchers with OpiskeluoikeusTestMetho
         putOpiskeluOikeus(defaultOpiskeluoikeus) {
           verifyResponseStatus(501, TorErrorCategory.notImplemented.readOnly("Korkeakoulutuksen opiskeluoikeuksia ei voi päivittää Koski-järjestelmässä"))
         }
+      }
+    }
+
+    describe("Haettaessa") {
+      it("Konvertoidaan Virta-järjestelmän opiskeluoikeus") {
+        val opiskeluoikeus = lastOpiskeluOikeus(MockOppijat.korkeakoululainen.oid)
+
+        opiskeluoikeus.tyyppi.koodiarvo should equal("korkeakoulutus")
       }
     }
   }
