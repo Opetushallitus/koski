8ec23c2f9ab81fd9f35e89b2288d7832af60e1e0
Juha Paananen <juha.paananen@gmail.com>
2016-04-28 13:38:10 +0300

    extract class to file

---
+++ src/main/scala/fi/oph/tor/oppija/CompositeOppijaRepository.scala
@@ -0,0 +1,35 @@
+package fi.oph.tor.oppija
+
+import fi.oph.tor.henkilo.HenkiloOid
+import fi.oph.tor.http.HttpStatus
+import fi.oph.tor.schema._
+
+import scala.collection.parallel.immutable.ParSeq
+
+case class CompositeOppijaRepository(repos: List[OppijaRepository]) extends OppijaRepository {
+  override def findOppijat(query: String) = mergeDuplicates(repos.par.map(_.findOppijat(query)).toList)
+
+  override def findByOid(oid: String) = mergeDuplicates(repos.par.map(_.findByOid(oid).toList).toList).headOption
+
+  override def findOrCreate(henkilö: UusiHenkilö) = {
+    val results: ParSeq[Either[HttpStatus, Henkilö.Oid]] = repos.par.map(_.findOrCreate(henkilö))
+    results.toList.sortWith {
+      case (Right(_), Left(_)) => true // prefer success
+      case (Left(x), Left(y)) => x.statusCode < y.statusCode
+      case _ => false
+    }.head
+  }
+
+  override def findByOids(oids: List[String]) = mergeDuplicates(repos.par.map(_.findByOids(oids)).toList)
+
+  private def mergeDuplicates(oppijat: Iterable[Iterable[TaydellisetHenkilötiedot]]): List[TaydellisetHenkilötiedot] = {
+    val grouped = oppijat.flatten.toList.groupBy(_.hetu).values
+    grouped.flatMap { duplicates =>
+      // de-duplicate the ones with nonempty hetu
+      duplicates.head.hetu match {
+        case "" => duplicates
+        case _ => duplicates.sortBy(henkilo => !HenkiloOid.isValidHenkilöOid(henkilo.oid)).headOption
+      }
+    }.toList
+  }
+}
--- src/main/scala/fi/oph/tor/oppija/OppijaRepository.scala
+++ src/main/scala/fi/oph/tor/oppija/OppijaRepository.scala
@@ -3,14 +3,13 @@ package fi.oph.tor.oppija
 import com.typesafe.config.Config
 import fi.oph.tor.cache.{BaseCacheDetails, CachingProxy, CachingStrategyBase}
 import fi.oph.tor.db.TorDatabase
-import fi.oph.tor.henkilo.{HenkiloOid, AuthenticationServiceClient}
+import fi.oph.tor.henkilo.AuthenticationServiceClient
 import fi.oph.tor.http.HttpStatus
 import fi.oph.tor.koodisto.KoodistoViitePalvelu
 import fi.oph.tor.log.{Logging, TimedProxy}
 import fi.oph.tor.schema._
 import fi.oph.tor.util.Invocation
-import fi.oph.tor.virta.{VirtaClient, VirtaOppijaRepository, VirtaOpiskeluoikeusRepository}
-import scala.collection.parallel.immutable.ParSeq
+import fi.oph.tor.virta.{VirtaClient, VirtaOppijaRepository}
 
 object OppijaRepository {
   def apply(config: Config, database: TorDatabase, koodistoViitePalvelu: KoodistoViitePalvelu) = {
@@ -56,30 +55,3 @@ trait OppijaRepository extends Logging {
   def findOrCreate(henkilö: UusiHenkilö): Either[HttpStatus, Henkilö.Oid]
 }
 
-case class CompositeOppijaRepository(repos: List[OppijaRepository]) extends OppijaRepository {
-  override def findOppijat(query: String) = mergeDuplicates(repos.par.map(_.findOppijat(query)).toList)
-
-  override def findByOid(oid: String) = mergeDuplicates(repos.par.map(_.findByOid(oid).toList).toList).headOption
-
-  override def findOrCreate(henkilö: UusiHenkilö) = {
-    val results: ParSeq[Either[HttpStatus, Henkilö.Oid]] = repos.par.map(_.findOrCreate(henkilö))
-    results.toList.sortWith {
-      case (Right(_), Left(_)) => true // prefer success
-      case (Left(x), Left(y)) => x.statusCode < y.statusCode
-      case _ => false
-    }.head
-  }
-
-  override def findByOids(oids: List[String]) = mergeDuplicates(repos.par.map(_.findByOids(oids)).toList)
-
-  private def mergeDuplicates(oppijat: Iterable[Iterable[TaydellisetHenkilötiedot]]): List[TaydellisetHenkilötiedot] = {
-    val grouped = oppijat.flatten.toList.groupBy(_.hetu).values
-    grouped.flatMap { duplicates =>
-      // de-duplicate the ones with nonempty hetu
-      duplicates.head.hetu match {
-        case "" => duplicates
-        case _ => duplicates.sortBy(henkilo => !HenkiloOid.isValidHenkilöOid(henkilo.oid)).headOption
-      }
-    }.toList
-  }
-}
