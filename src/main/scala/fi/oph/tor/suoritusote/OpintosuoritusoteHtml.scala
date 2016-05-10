c4588357ada699d39b7bd6af7b81c9e2521bca6f
Kalle Kainu <kalle.kainu@reaktor.fi>
2016-05-10 09:22:24 +0300

    Add more data to opintosuoritusote

---
+++ src/main/scala/fi/oph/tor/suoritusote/OpintosuoritusoteHtml.scala
@@ -0,0 +1,65 @@
+package fi.oph.tor.suoritusote
+
+import java.text.NumberFormat
+import java.time.format.DateTimeFormatter
+
+import fi.oph.tor.localization.Locale._
+import fi.oph.tor.localization.Localizable
+import fi.oph.tor.schema.{Opiskeluoikeus, Suoritus, TaydellisetHenkilötiedot}
+import fi.oph.tor.toruser.TorUser
+
+class OpintosuoritusoteHtml(implicit val user: TorUser) {
+
+  val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+  val decimalFormat = NumberFormat.getInstance(finnish)
+  def lang = user.lang
+
+  def render(ht: TaydellisetHenkilötiedot, oo: Opiskeluoikeus) = {
+    <html>
+      <head>
+        <link rel="stylesheet" type="text/css" href="/tor/css/opintosuoritusote.css"></link>
+        <style>
+          .depth-1 {{ padding-left:0.5em; }}
+          .depth-2 {{ padding-left:1em; }}
+          .depth-3 {{ padding-left:1.5em; }}
+        </style>
+      </head>
+      <body>
+        <h1>Opintosuoritusote</h1>
+        <h3>Opintosuoritukset</h3>
+
+        <table>
+          <tr>
+            <th class="tunnus"></th>
+            <th class="nimi"></th>
+            <th class="laajuus">Op</th>
+            <th class="laajuus">Arv.</th>
+            <th class="suoritus-pvm">Suor.pvm</th>
+          </tr>
+          { suoritusLista(oo) }
+        </table>
+      </body>
+    </html>
+  }
+
+  private def suoritusWithDepth(t: (Int, Suoritus)) : List[(Int, Suoritus)] = {
+    t :: t._2.osasuoritusLista.flatMap(s => suoritusWithDepth((t._1 + 1, s)))
+  }
+
+  private def suoritusLista(opiskeluoikeus: Opiskeluoikeus) = opiskeluoikeus.suoritukset.headOption.map(tutkinto =>
+    suoritusWithDepth(0, tutkinto).flatMap {
+      case (depth, suoritus) =>
+        <tr>
+          <td class={"depth-" + depth}>{suoritus.koulutusmoduuli.tunniste.koodiarvo}</td>
+          <td class={"depth-" + depth}>{i(suoritus.koulutusmoduuli)}</td>
+          <td>{suoritus.koulutusmoduuli.laajuus.map(l => decimalFormat.format(l.arvo)).getOrElse("")}</td>
+          <td>{i(suoritus.arvosanaKirjaimin)}</td>
+          <td>{suoritus.arviointi.flatMap(_.lastOption.flatMap(_.päivä.map(dateFormatter.format(_)))).getOrElse("")}</td>
+        </tr>
+    }
+  ).toList.flatten
+
+  private def i(s: Localizable): String = s.description.get(lang)
+  private def i(s: Option[Localizable]): String = s.map(i).getOrElse("")
+
+}
--- src/main/scala/fi/oph/tor/suoritusote/SuoritusServlet.scala
+++ src/main/scala/fi/oph/tor/suoritusote/SuoritusServlet.scala
@@ -3,13 +3,10 @@ package fi.oph.tor.suoritusote
 import fi.oph.tor.http.TorErrorCategory
 import fi.oph.tor.opiskeluoikeus.OpiskeluOikeusRepository
 import fi.oph.tor.oppija.OppijaRepository
-import fi.oph.tor.schema.{Opiskeluoikeus, Suoritus, TaydellisetHenkilötiedot}
 import fi.oph.tor.servlet.ErrorHandlingServlet
 import fi.oph.tor.tor.TodennetunOsaamisenRekisteri
 import fi.oph.tor.toruser.{RequiresAuthentication, UserOrganisationsRepository}
 import fi.vm.sade.security.ldap.DirectoryClient
-
-import scala.xml.Node
 
 class SuoritusServlet(
   val userRepository: UserOrganisationsRepository,
@@ -18,63 +15,15 @@ class SuoritusServlet(
   val oppijaRepository: OppijaRepository,
   val opiskeluOikeusRepository: OpiskeluOikeusRepository) extends ErrorHandlingServlet with RequiresAuthentication {
 
-
-
   get("/:oppijaOid/:opiskeluoikeusId") {
     val oid = params("oppijaOid")
     val opiskeluoikeusId = params("opiskeluoikeusId")
+    implicit val user = torUser
 
     opiskeluOikeusRepository.findByOppijaOid(oid)(torUser).find(oo => oo.tyyppi.koodiarvo == "korkeakoulutus" && oo.lähdejärjestelmänId.exists(_.id == opiskeluoikeusId)).flatMap(oo => oppijaRepository.findByOid(oid).map((_, oo))) match {
-      case Some((ht, oo)) => renderSuoritusote(ht, oo)
+      case Some((ht, oo)) => new OpintosuoritusoteHtml().render(ht, oo)
       case _ => TorErrorCategory.notFound
     }
   }
 
-  def renderSuoritusote(ht: TaydellisetHenkilötiedot, oo: Opiskeluoikeus) = {
-
-    def suoritusWithDepth(t: (Int, Suoritus)) : List[(Int, Suoritus)] = {
-      t :: t._2.osasuoritusLista.flatMap(s => suoritusWithDepth((t._1 + 1, s)))
-    }
-
-    def suoritusLista: List[Node] = oo.suoritukset.headOption.map(ts =>
-        suoritusWithDepth(0, ts).flatMap(s =>
-          <tr>
-            <td class={"depth-" + s._1}>{s._2.koulutusmoduuli.tunniste.koodiarvo}</td>
-            <td class={"depth-" + s._1}>{s._2.koulutusmoduuli.nimi.get("FI")}</td>
-            <td>{s._2.koulutusmoduuli.laajuus.map(_.arvo).getOrElse("")}</td>
-            <td>{s._2.arviointi.flatMap(_.lastOption.flatMap(_.päivä.map(_.toString))).getOrElse("")}</td>
-          </tr>
-        )
-      ).toList.flatten
-
-    <html>
-      <head>
-        <style>
-          table {{
-            border-collapse: separate;
-            border-spacing: 30px 5px;
-          }}
-          .depth-1 {{ padding-left:0.5em; }}
-          .depth-2 {{ padding-left:1em; }}
-          .depth-3 {{ padding-left:1.5em; }}
-        </style>
-      </head>
-      <body>
-        <h1>Opintosuoritusote</h1>
-        <h3>Opintosuoritukset</h3>
-
-        <table>
-          <tr>
-            <th class="tunnus"></th>
-            <th class="nimi"></th>
-            <th class="laajuus">Op</th>
-            <th class="suoritus-pvm">Suor.pvm</th>
-          </tr>
-          {
-            suoritusLista
-          }
-        </table>
-      </body>
-    </html>
-  }
 }
