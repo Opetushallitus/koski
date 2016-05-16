9f5bdd0aca5cf32b5710c6cae13e609b419847e3
Juha Paananen <juha.paananen@gmail.com>
2016-05-16 11:34:16 +0300

    cleanup

--- src/test/scala/fi/oph/tor/api/KorkeakouluSpec.scala
+++ src/test/scala/fi/oph/tor/api/KorkeakouluSpec.scala
@@ -61,7 +61,7 @@ class KorkeakouluSpec extends FunSpec with Matchers with OpiskeluoikeusTestMetho
           verifyResponseStatus(200)
 
           val lines: Seq[String] = scala.xml.XML.loadString(response.body).flatMap(_.descendant_or_self).flatMap {
-            case tr: Node if tr.label == "tr" && tr.attribute("class").map(_.text).getOrElse("") != "header" => Some((tr \ "td").map(_.text).mkString(" ").trim)
+            case tr: Node if tr.label == "tr" && (tr \ "@class").text != "header" => Some((tr \ "td").map(_.text).mkString(" ").trim)
             case h3: Node if h3.label == "h3" => Some(h3.text.trim)
             case _ => None
           }
