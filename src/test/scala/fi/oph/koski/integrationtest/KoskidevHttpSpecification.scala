0d3a1556d54a4d9c2cc1b9d349fc7eea792052af
Kalle Kainu <kalle.kainu@reaktor.fi>
2016-05-25 16:46:14 +0300

    tor -> koski

--- src/test/scala/fi/oph/koski/integrationtest/KoskidevHttpSpecification.scala
+++ src/test/scala/fi/oph/koski/integrationtest/KoskidevHttpSpecification.scala
@@ -5,7 +5,7 @@ import fi.oph.koski.http.{BasicAuthentication, HttpSpecification}
 trait KoskidevHttpSpecification extends HttpSpecification {
   private def requiredEnv(name: String) = util.Properties.envOrNone(name).getOrElse(throw new IllegalStateException("Environment property " + name + " missing"))
 
-  override def baseUrl = "https://tordev.tor.oph.reaktor.fi/koski"
+  override def baseUrl = "https://koskidev.koski.oph.reaktor.fi/koski"
 
   lazy val username = requiredEnv("KOSKI_USER")
   lazy val password = requiredEnv("KOSKI_PASS")
