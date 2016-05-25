6adc77cfb55c42d836f4e60e4003ae54799031e5
Juha Paananen <juha.paananen@gmail.com>
2016-05-25 15:22:44 +0300

    use https in integration test

--- src/test/scala/fi/oph/koski/integrationtest/KoskidevHttpSpecification.scala
+++ src/test/scala/fi/oph/koski/integrationtest/KoskidevHttpSpecification.scala
@@ -5,7 +5,7 @@ import fi.oph.koski.http.{BasicAuthentication, HttpSpecification}
 trait KoskidevHttpSpecification extends HttpSpecification {
   private def requiredEnv(name: String) = util.Properties.envOrNone(name).getOrElse(throw new IllegalStateException("Environment property " + name + " missing"))
 
-  override def baseUrl = "http://tordev.tor.oph.reaktor.fi/koski"
+  override def baseUrl = "https://tordev.tor.oph.reaktor.fi/koski"
 
   lazy val username = requiredEnv("TOR_USER")
   lazy val password = requiredEnv("TOR_PASS")
