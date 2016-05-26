294c1b1d40e1361d1c562ee2cdcaab9722579afa
Juha Paananen <juha.paananen@gmail.com>
2016-05-26 09:28:46 +0300

    ignore certs in integration test

--- src/test/scala/fi/oph/koski/integrationtest/KoskidevHttpSpecification.scala
+++ src/test/scala/fi/oph/koski/integrationtest/KoskidevHttpSpecification.scala
@@ -1,6 +1,8 @@
 package fi.oph.koski.integrationtest
 
 import fi.oph.koski.http.{BasicAuthentication, HttpSpecification}
+import org.apache.http.conn.ssl.{SSLConnectionSocketFactory, SSLContextBuilder, TrustSelfSignedStrategy}
+import org.apache.http.impl.client.{HttpClients, HttpClientBuilder}
 
 trait KoskidevHttpSpecification extends HttpSpecification {
   private def requiredEnv(name: String) = util.Properties.envOrNone(name).getOrElse(throw new IllegalStateException("Environment property " + name + " missing"))
@@ -11,4 +13,11 @@ trait KoskidevHttpSpecification extends HttpSpecification {
   lazy val password = requiredEnv("KOSKI_PASS")
 
   def authHeaders = Map(BasicAuthentication.basicAuthHeader(username, password))
+
+  override protected def createClient = {
+    val builder = new SSLContextBuilder();
+    builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+    val sslsf = new SSLConnectionSocketFactory(builder.build(), SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
+    HttpClients.custom().setSSLSocketFactory(sslsf).build();
+  }
 }
