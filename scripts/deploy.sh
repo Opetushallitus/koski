27e6068340de28ba77ae241fc9ce125f57fd238b
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-06-01 10:26:19 +0300

    Simplify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ VALID_ENVS=(
 )
 
 function usage() {
-  echo "Usage `basename $0` <env> <package>"
+  echo "Usage: `basename $0` <env> <package>"
   echo " where <env> is one of [`echo ${VALID_ENVS[@]}|sed 's/ / | /g'`]"
   echo "   and <package> is the package to deploy"
   exit 1
@@ -22,14 +22,9 @@ if [ -z "$ENV" ] || ! [[ " ${VALID_ENVS[@]} " =~ " ${ENV} " ]] || [ ! -f "$PACKA
   usage
 fi
 
+ANSIBLE_ARGS=""
 if [ "$ENV" == "vagrant" ]; then
-  if [ ! -f "$PRIVATE_KEY" ]; then
-    echo "Please set PRIVATE_KEY env variable to point to vagrant private key found under oph-poutai-env repo, and rerun the deploy"
-    echo "eg: export PRIVATE_KEY=\"\$HOME/workspace/oph-poutai-env/.vagrant/machines/localcloudbox/virtualbox/private_key\""
-    exit 1
-  fi
-  export ANSIBLE_SSH_ARGS="-o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes -i '$PRIVATE_KEY' -o ControlMaster=auto -o ControlPersist=60s"
-  ansible-playbook -u vagrant --extra-vars=koski_package="$PACKAGE" -i scripts/inventory/$ENV scripts/site.yml
-else
-  echo not yet implemented
+  ANSIBLE_ARGS="--user=vagrant"
 fi
+
+ansible-playbook $ANSIBLE_ARGS --extra-vars=koski_package="$PACKAGE" -i scripts/inventory/$ENV scripts/site.yml
