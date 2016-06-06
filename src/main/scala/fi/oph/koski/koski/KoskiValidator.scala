5c2b37908093fd51731060e4d266e62412f5642c
Juha Paananen <juha.paananen@gmail.com>
2016-06-06 14:01:12 +0300

    fix imports

--- src/main/scala/fi/oph/koski/koski/KoskiValidator.scala
+++ src/main/scala/fi/oph/koski/koski/KoskiValidator.scala
@@ -7,7 +7,7 @@ import fi.oph.koski.koodisto.KoodistoViitePalvelu
 import fi.oph.koski.organisaatio.OrganisaatioRepository
 import fi.oph.koski.schema._
 import fi.oph.koski.koski.DateValidation._
-import fi.oph.koski.koskiuser.{KoskiUser, AccessType, KoskiUser$}
+import fi.oph.koski.koskiuser.{KoskiUser, AccessType}
 import fi.oph.koski.tutkinto.{TutkintoRakenneValidator, TutkintoRepository}
 import fi.oph.koski.util.Timing
 import org.json4s.{JArray, JValue}
