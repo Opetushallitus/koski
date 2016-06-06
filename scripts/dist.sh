e8d5c41523138ef3bf326dee0bd1302b1dd98d44
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-06-06 12:25:27 +0300

    Karsi turhia tiedostoja asennuksesta
    Korjaa myös ansible 2.1:n unarchive virheen

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -8,9 +8,10 @@ if [ -z "$DIST_DIR" ]; then
   exit 1
 fi
 
-mkdir -p $DIST_DIR/src/main
+mkdir -p $DIST_DIR/{web/node_modules,src/main}
 cd `dirname $0`/..
-cp -r web $DIST_DIR/
+cp -r web/{static,dist,test} $DIST_DIR/web/
+cp -r web/node_modules/codemirror $DIST_DIR/web/node_modules/
 cp -r src/main/{resources,webapp} $DIST_DIR/src/main/
 cd $DIST_DIR
 zip -qr ../$(basename $DIST_DIR).zip *
