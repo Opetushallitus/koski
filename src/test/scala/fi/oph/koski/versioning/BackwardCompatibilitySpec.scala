3fc8e7a1ddabe9d99643fbbbccc6996492eae946
Juha Paananen <juha.paananen@gmail.com>
2016-06-06 14:01:05 +0300

    validate backward-compatibility examples in addition to simple serialization-roundtrip check

--- src/test/scala/fi/oph/koski/versioning/BackwardCompatibilitySpec.scala
+++ src/test/scala/fi/oph/koski/versioning/BackwardCompatibilitySpec.scala
@@ -1,7 +1,9 @@
 package fi.oph.koski.versioning
 
+import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.documentation.Examples
 import fi.oph.koski.json.Json
+import fi.oph.koski.koskiuser.{AccessType, KoskiUser}
 import fi.oph.koski.schema.Oppija
 import org.scalatest.{FreeSpec, Matchers}
 
@@ -9,6 +11,10 @@ import org.scalatest.{FreeSpec, Matchers}
  * Tests that examples match saved JSON files. Run with -DupdateExamples=true to update saved JSON files from current examples.
  */
 class BackwardCompatibilitySpec extends FreeSpec with Matchers {
+  val validator = KoskiApplication().validator
+  implicit val user = KoskiUser.systemUser
+  implicit val accessType = AccessType.read
+
   "backward compatibility with stored JSON examples" in {
     val updateAll = System.getProperty("updateExamples", "false").toBoolean
     Examples.examples.foreach { example =>
@@ -16,8 +22,13 @@ class BackwardCompatibilitySpec extends FreeSpec with Matchers {
       (updateAll, Json.readFileIfExists(filename)) match {
         case (false, Some(json)) =>
           println("Checking backward compatibility: " + filename)
-          val afterRoundtrip = Json.toJValue(Json.fromJValue[Oppija](json))
+          val oppija: Oppija = Json.fromJValue[Oppija](json)
+          val afterRoundtrip = Json.toJValue(oppija)
           afterRoundtrip should equal(json)
+          validator.validateAsJson(oppija) match {
+            case Right(validated) => // Valid
+            case Left(err) => throw new IllegalStateException(err.toString)
+          }
         case _ =>
           println("Updating/creating " + filename)
           new java.io.File(filename).getParentFile().mkdirs()
