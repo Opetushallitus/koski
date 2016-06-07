16989aea07268c7c3b595663b164befec00a4137
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-06-07 15:34:54 +0300

    Validoi pilviympäristöhakemisto

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,21 +3,30 @@ set -euo pipefail
 
 ENV=${1:-}
 PACKAGE=${2:-}
+CLOUD_ENV_DIR=${CLOUD_ENV_DIR:-}
+RC_FILE=$(dirname $0)/cloudrc
 
 VALID_ENVS=(
   "vagrant"
-  "qa"
-  "production"
+  "tordev"
 )
 
 function usage() {
   echo "Usage: `basename $0` <env> <package>"
   echo " where <env> is one of [`echo ${VALID_ENVS[@]}|sed 's/ / | /g'`]"
   echo "   and <package> is the package to deploy"
+  echo
+  echo "NB: You need check out the cloud environment repository and set the CLOUD_ENV_DIR environment variable before running this script"
+  echo 'eg: export CLOUD_ENV_DIR="$HOME/workspace/oph-poutai-env"'
+  echo "Note that you can also add a file $RC_FILE and set the variable there"
   exit 1
 }
 
-if [ -z "$ENV" ] || ! [[ " ${VALID_ENVS[@]} " =~ " ${ENV} " ]] || [ ! -f "$PACKAGE" ]; then
+if [ -f "$RC_FILE" ]; then
+  source $RC_FILE
+fi
+
+if [ -z "$ENV" ] || ! [[ " ${VALID_ENVS[@]} " =~ " ${ENV} " ]] || [ ! -f "$PACKAGE" ] || [ -z "$CLOUD_ENV_DIR" ] || [ ! -d "$CLOUD_ENV_DIR" ]; then
   usage
 fi
 
