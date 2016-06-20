9951fe05c3524eff4bfc08d7e5c0adf68a798144
Juha Paananen <juha.paananen@gmail.com>
2016-06-20 12:41:35 +0300

    Use json4s.Formats.- method to exclude a given serializer
    Temporarily forked json4s to add the minus (-) method to Formats.

--- src/main/scala/fi/oph/koski/schema/Deserializers.scala
+++ src/main/scala/fi/oph/koski/schema/Deserializers.scala
@@ -29,38 +29,24 @@ object Deserializers {
 }
 
 object ArviointiSerializer extends Serializer[Arviointi] {
-  // Threadlocal flags used to prevent recursion. Yes, it's ugly.
-  val deserializing = new java.lang.ThreadLocal[Boolean]
-  val serializing = new java.lang.ThreadLocal[Boolean]
-
   override def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Arviointi] = {
-    case (TypeInfo(c, _), json: JObject) if !deserializing.get && classOf[Arviointi].isAssignableFrom(c) =>
-      deserializing.set(true)
-      try {
-        val arviointi = Extraction.extract(json, Reflector.scalaTypeOf(c)).asInstanceOf[Arviointi]
-        (json \\ "hyväksytty") match {
-          case JBool(jsonHyväksytty) if (jsonHyväksytty != arviointi.hyväksytty) =>
-            ContextualExtractor.extractionError(KoskiErrorCategory.badRequest.validation.arviointi.vääräHyväksyttyArvo())
-          case _ =>
-            arviointi
-        }
-      } finally {
-        deserializing.set(false)
+    case (TypeInfo(c, _), json: JObject) if classOf[Arviointi].isAssignableFrom(c) =>
+      val arviointi = Extraction.extract(json, Reflector.scalaTypeOf(c))(format - ArviointiSerializer).asInstanceOf[Arviointi]
+      (json \\ "hyväksytty") match {
+        case JBool(jsonHyväksytty) if (jsonHyväksytty != arviointi.hyväksytty) =>
+          ContextualExtractor.extractionError(KoskiErrorCategory.badRequest.validation.arviointi.vääräHyväksyttyArvo())
+        case _ =>
+          arviointi
       }
   }
 
   override def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
-    case (a: Arviointi) if (!serializing.get()) =>
-      serializing.set(true)
-      try {
-        val json = Extraction.decompose(a).asInstanceOf[JObject]
-        if (!json.values.contains("hyväksytty")) {
-          json.merge(JObject("hyväksytty" -> JBool(a.hyväksytty)))
-        } else {
-          json
-        }
-      } finally {
-        serializing.set(false)
+    case (a: Arviointi) =>
+      val json = Extraction.decompose(a)(format - ArviointiSerializer).asInstanceOf[JObject]
+      if (!json.values.contains("hyväksytty")) {
+        json.merge(JObject("hyväksytty" -> JBool(a.hyväksytty)))
+      } else {
+        json
       }
   }
 }
