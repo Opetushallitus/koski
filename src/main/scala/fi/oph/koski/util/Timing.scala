d886f6895cab71dfe3c0309d1ec47cff34102b98
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-06-28 11:35:39 +0300

    Performanssi loki omaan tiedostoon

--- src/main/scala/fi/oph/koski/util/Timing.scala
+++ src/main/scala/fi/oph/koski/util/Timing.scala
@@ -1,18 +1,19 @@
 package fi.oph.koski.util
 
-import fi.oph.koski.log.{LoggerWithContext, Logging}
+import fi.oph.koski.log.Logging
+import org.log4s.getLogger
 import rx.lang.scala.Observable
 
 object Timing {
-  def timed[R](blockname: String, thresholdMs: Int = 50, logger: LoggerWithContext)(block: => R): R = {
-    val timer = new Timer(logger, blockname, thresholdMs)
+  def timed[R](blockname: String, thresholdMs: Int = 50, clazz: Class[_])(block: => R): R = {
+    val timer = new Timer(blockname, thresholdMs, clazz)
     timer.complete(block)
   }
 }
 
 trait Timing extends Logging {
   def timed[R](blockname: String, thresholdMs: Int = 50)(block: => R): R = {
-    Timing.timed(blockname, thresholdMs, this.logger)(block)
+    Timing.timed(blockname, thresholdMs, getClass)(block)
   }
 
   /**
@@ -20,14 +21,14 @@ trait Timing extends Logging {
    */
   def timedObservable[R](blockname: String, thresholdMs: Int = 0)(observable: => Observable[R]): Observable[R] = {
     val t0: Long = System.nanoTime()
-    val timer = new Timer(logger, blockname, thresholdMs)
+    val timer = new Timer(blockname, thresholdMs, getClass)
     observable.doOnNext { x =>
       timer.complete(x)
     }
   }
 }
 
-class Timer(logger: LoggerWithContext, blockname: String, thresholdMs: Int) {
+class Timer(blockname: String, thresholdMs: Int, clazz: Class[_]) {
   private val t0 = System.nanoTime()
   private var completed = false
   def complete[T](result: T): T = {
@@ -35,9 +36,12 @@ class Timer(logger: LoggerWithContext, blockname: String, thresholdMs: Int) {
       completed = true
       val t1 = System.nanoTime()
       val time: Long = (t1 - t0) / 1000000
-      if (time >= thresholdMs) logger.info(blockname + " took " + time + " ms")
+      if (time >= thresholdMs) Timer.logger.info(s"${clazz.getSimpleName} - $blockname took $time ms")
     }
     result
   }
 }
 
+object Timer {
+  val logger = getLogger(classOf[Timer])
+}
