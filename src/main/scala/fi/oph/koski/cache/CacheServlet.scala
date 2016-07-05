87db8a61f6a5133a4b699be53778c1878c4dcd2a
Juha Paananen <juha.paananen@gmail.com>
2016-07-05 11:14:30 +0300

    require root access in cache/invalidate

--- src/main/scala/fi/oph/koski/cache/CacheServlet.scala
+++ src/main/scala/fi/oph/koski/cache/CacheServlet.scala
@@ -7,7 +7,10 @@ import fi.oph.koski.servlet.ApiServlet
 import fi.vm.sade.security.ldap.DirectoryClient
 
 class CacheServlet(val userRepository: UserOrganisationsRepository, val directoryClient: DirectoryClient, application: KoskiApplication) extends ApiServlet with RequiresAuthentication with Logging {
-  get("/invalidate") { // TODO: require superuser privileges
+  get("/invalidate") {
+    if (!koskiUser.isRoot) {
+      halt(403)
+    }
     logger.info("Invalidating all caches")
     application.invalidateCaches
     "Caches invalidated"
