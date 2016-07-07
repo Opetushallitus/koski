2c6aeeb81bf8faa8273f33c6fa1d50b0952152e2
Juha Paananen <juha.paananen@gmail.com>
2016-07-07 17:27:40 +0300

    add stored data validity test to OppijaIntegrationTest

--- src/test/scala/fi/oph/koski/integrationtest/OppijaIntegrationTest.scala
+++ src/test/scala/fi/oph/koski/integrationtest/OppijaIntegrationTest.scala
@@ -1,10 +1,14 @@
 package fi.oph.koski.integrationtest
 
 import fi.oph.koski.api.{OpiskeluoikeusTestMethodsAmmatillinen, SearchTestMethods}
+import fi.oph.koski.json.Json
+import fi.oph.koski.koski.ValidationResult
 import fi.oph.koski.schema.{OidHenkilö, TäydellisetHenkilötiedot, YlioppilastutkinnonOpiskeluoikeus}
 import org.scalatest.{FreeSpec, Matchers}
 
 // This test is run against the Koski application deployed in the KoskiDev test environment.
+// You need to provide environment variables KOSKI_USER, KOSKI_PASS
+// Optionally override base url using KOSKI_BASE_URL for testing against your local server.
 class OppijaIntegrationTest extends FreeSpec with Matchers with KoskidevHttpSpecification with OpiskeluoikeusTestMethodsAmmatillinen with SearchTestMethods {
   val testOid = "1.2.246.562.24.51633620848"
 
@@ -39,6 +43,23 @@ class OppijaIntegrationTest extends FreeSpec with Matchers with KoskidevHttpSpec
       case Some(oid) =>
         val o = oppija(oid)
         o.opiskeluoikeudet.filter(_.isInstanceOf[YlioppilastutkinnonOpiskeluoikeus]).length should equal(1)
+    }
+  }
+
+  "Tallennetun datan validiteetti" - {
+    "Validate all" in {
+      authGet("api/oppija/validate") {
+        verifyResponseStatus(200)
+        val results = Json.read[List[ValidationResult]](body)
+        results.length should be >= 0
+        println(s"Löytyi ${results.length} oppijaa")
+        results.foreach(printValidity)
+        results.flatMap(_.errors).length should equal(0)
+      }
+    }
+
+    def printValidity(result: ValidationResult) = {
+      println(result.oid + (if (result.isOk) {" OK"} else {" FAIL " + result.errors}))
     }
   }
 }
