ededa533bcdbee9da36ac22e1047dca3d72969b4
Juha Paananen <juha.paananen@gmail.com>
2016-07-07 11:38:01 +0300

    extract method

--- src/main/scala/fi/oph/koski/koskiuser/KäyttöoikeusRepository.scala
+++ src/main/scala/fi/oph/koski/koskiuser/KäyttöoikeusRepository.scala
@@ -9,48 +9,49 @@ import rx.lang.scala.Observable
 class KäyttöoikeusRepository(henkilöPalveluClient: AuthenticationServiceClient, organisaatioRepository: OrganisaatioRepository) extends Timing {
   private lazy val käyttöoikeusryhmätCache = henkilöPalveluClient.käyttöoikeusryhmät
   private def ryhmäById(ryhmäId: Int) = käyttöoikeusryhmätCache.find(_.id == ryhmäId).flatMap(_.toKoskiKäyttöoikeusryhmä)
-  private lazy val käyttöoikeusCache = new KeyValueCache[String, Observable[Set[Käyttöoikeus]]](
-    CachingStrategy.cacheAllNoRefresh("userOrganisations", 3600, 100),
-    henkilöOid => timedObservable("käyttäjänOrganisaatiot")(henkilöPalveluClient.käyttäjänKäyttöoikeusryhmät(henkilöOid)
-      .map { (käyttöoikeudet: List[(String, Int)]) =>
-        käyttöoikeudet.toSet.flatMap { tuple: (String, Int) =>
-          tuple match {
-            case (organisaatioOid: String, ryhmäId: Int) =>
-              organisaatioOid match {
-                case Opetushallitus.organisaatioOid =>
-                  ryhmäById(ryhmäId).flatMap{
-                    case r: GlobaaliKäyttöoikeusryhmä => Some(GlobaaliKäyttöoikeus(r))
-                    case r: OrganisaationKäyttöoikeusryhmä =>
-                      logger.warn(s"Käyttäjällä $henkilöOid on organisaatiotyyppinen käyttöoikeusryhmä $r liitettynä OPH-organisaatioon")
+  private def haeKäyttöoikeudet(henkilöOid: String): Observable[Set[Käyttöoikeus]] = timedObservable("käyttäjänOrganisaatiot")(henkilöPalveluClient.käyttäjänKäyttöoikeusryhmät(henkilöOid)
+    .map { (käyttöoikeudet: List[(String, Int)]) =>
+      käyttöoikeudet.toSet.flatMap { tuple: (String, Int) =>
+        tuple match {
+          case (organisaatioOid: String, ryhmäId: Int) =>
+            organisaatioOid match {
+              case Opetushallitus.organisaatioOid =>
+                ryhmäById(ryhmäId).flatMap{
+                  case r: GlobaaliKäyttöoikeusryhmä => Some(GlobaaliKäyttöoikeus(r))
+                  case r: OrganisaationKäyttöoikeusryhmä =>
+                    logger.warn(s"Käyttäjällä $henkilöOid on organisaatiotyyppinen käyttöoikeusryhmä $r liitettynä OPH-organisaatioon")
+                    None
+                }.toList
+              case _ =>
+                def flatten(orgs: List[OrganisaatioHierarkia]): List[OrganisaatioHierarkia] = {
+                  orgs ++ orgs.flatMap { org => org :: flatten(org.children) }
+                }
+                val organisaatioHierarkia: Option[OrganisaatioHierarkia] = organisaatioRepository.getOrganisaatioHierarkia(organisaatioOid)
+
+                val flattened: List[OrganisaatioHierarkia] = organisaatioHierarkia match {
+                  case Some(organisaatioHierarkia) => flatten(List(organisaatioHierarkia))
+                  case None =>
+                    logger.warn(s"Käyttäjän $henkilöOid käyttöoikeus $ryhmäId kohdistuu organisaatioon $organisaatioOid, jota ei löydy")
+                    Nil
+                }
+
+                flattened.flatMap { org =>
+                  val käyttöoikeus: Option[OrganisaatioKäyttöoikeus] = ryhmäById(ryhmäId).flatMap {
+                    case r: GlobaaliKäyttöoikeusryhmä =>
+                      logger.warn(s"Käyttäjällä $henkilöOid on globaali käyttöoikeusryhmä $r liitettynä organisaatioon $organisaatioOid")
                       None
-                  }.toList
-                case _ =>
-                  def flatten(orgs: List[OrganisaatioHierarkia]): List[OrganisaatioHierarkia] = {
-                    orgs ++ orgs.flatMap { org => org :: flatten(org.children) }
+                    case r: OrganisaationKäyttöoikeusryhmä =>
+                      Some(OrganisaatioKäyttöoikeus(org.toOrganisaatio, org.oppilaitostyyppi, r))
                   }
-                  val organisaatioHierarkia: Option[OrganisaatioHierarkia] = organisaatioRepository.getOrganisaatioHierarkia(organisaatioOid)
-
-                  val flattened: List[OrganisaatioHierarkia] = organisaatioHierarkia match {
-                    case Some(organisaatioHierarkia) => flatten(List(organisaatioHierarkia))
-                    case None =>
-                      logger.warn(s"Käyttäjän $henkilöOid käyttöoikeus $ryhmäId kohdistuu organisaatioon $organisaatioOid, jota ei löydy")
-                      Nil
-                  }
-
-                  flattened.flatMap { org =>
-                    val käyttöoikeus: Option[OrganisaatioKäyttöoikeus] = ryhmäById(ryhmäId).flatMap {
-                      case r: GlobaaliKäyttöoikeusryhmä =>
-                        logger.warn(s"Käyttäjällä $henkilöOid on globaali käyttöoikeusryhmä $r liitettynä organisaatioon $organisaatioOid")
-                        None
-                      case r: OrganisaationKäyttöoikeusryhmä =>
-                        Some(OrganisaatioKäyttöoikeus(org.toOrganisaatio, org.oppilaitostyyppi, r))
-                    }
-                    käyttöoikeus
-                  }
-              }
-          }
+                  käyttöoikeus
+                }
+            }
         }
-      })
+      }
+    })
+
+  private lazy val käyttöoikeusCache = new KeyValueCache[String, Observable[Set[Käyttöoikeus]]](
+    CachingStrategy.cacheAllNoRefresh("userOrganisations", 3600, 100), haeKäyttöoikeudet
   )
 
   def käyttäjänKäyttöoikeudet(oid: String): Observable[Set[Käyttöoikeus]] = käyttöoikeusCache(oid)
