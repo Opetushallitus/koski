4359db6eadebcdbc71a966aad77c8fcdaa7c7d76
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-08-08 07:54:48 +0300

    Tulosta vain error-tason virheet validoidessa

--- src/main/scala/fi/oph/koski/koski/KoskiJsonSchemaValidator.scala
+++ src/main/scala/fi/oph/koski/koski/KoskiJsonSchemaValidator.scala
@@ -2,8 +2,9 @@ package fi.oph.koski.koski
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT
-import com.github.fge.jackson.JsonLoader
-import com.github.fge.jsonschema.core.report.LogLevel.ERROR
+import com.github.fge.jackson.JsonLoader.fromString
+import com.github.fge.jsonschema.core.report.ListReportProvider
+import com.github.fge.jsonschema.core.report.LogLevel.{ERROR, FATAL}
 import com.github.fge.jsonschema.main.JsonSchemaFactory
 import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
 import fi.oph.koski.schema.KoskiSchema
@@ -13,7 +14,8 @@ import org.json4s.jackson.JsonMethods._
 import scala.collection.JavaConversions._
 
 object KoskiJsonSchemaValidator {
-  private val schema = JsonSchemaFactory.byDefault.getJsonSchema(JsonLoader.fromString(KoskiSchema.schemaJsonString))
+  private val schemaFactory= JsonSchemaFactory.newBuilder.setReportProvider(new ListReportProvider(ERROR, FATAL)).freeze()
+  private val schema = schemaFactory.getJsonSchema(fromString(KoskiSchema.schemaJsonString))
   private val mapper = new ObjectMapper().enable(INDENT_OUTPUT)
 
   def jsonSchemaValidate(node: JValue): HttpStatus = {
