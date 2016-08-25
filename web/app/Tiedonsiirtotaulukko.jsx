34cfb1680f88c2f1615f2448c67f502cb6d97ba0
Juha Paananen <juha.paananen@gmail.com>
2016-08-25 09:37:09 +0300

    näytetään sama esimerkkidata myös tiedonsiirtovirheet-sivulla

---
+++ web/app/Tiedonsiirtotaulukko.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const inputData = {'henkilö':{'oid':'1.2.246.562.24.00000000002'},'opiskeluoikeudet':[{'suoritukset':[{'koulutusmoduuli':{'tunniste':{'koodiarvo':'357305','nimi':{'fi':'Autoalan työnjohdon erikoisammattitutkinto','sv':'Specialyrkesexamen för arbetsledning inom bilbranschen','en':'Supervisor Vehicle Tech., SQ'},'lyhytNimi':{'fi':'Autoalan työnjohdon eat','sv':'Specialyrkesexamen för arbetsledning inom bilbranschen','en':'Supervisor in Vehicle Technology, Specialist Qualification'},'koodistoUri':'koulutus','koodistoVersio':6},'perusteenDiaarinumero':'39/011/2014'},'toimipiste':{'oid':'1.2.246.562.10.52251087186','oppilaitosnumero':{'koodiarvo':'10105','nimi':{'fi':'Stadin ammattiopisto'},'lyhytNimi':{'fi':'Stadin ammattiopisto'},'koodistoUri':'oppilaitosnumero'},'nimi':{'fi':'Stadin ammattiopisto'}},'tila':{'koodiarvo':'KESKEN','nimi':{'fi':'Suoritus kesken','sv':'Prestationen icke slutförd'},'koodistoUri':'suorituksentila','koodistoVersio':1},'tyyppi':{'koodiarvo':'ammatillinentutkinto','nimi':{'fi':'Ammatillinen tutkinto'},'koodistoUri':'suorituksentyyppi','koodistoVersio':1}}],'id':64825,'versionumero':1,'oppilaitos':{'oid':'1.2.246.562.10.52251087186','oppilaitosnumero':{'koodiarvo':'10105','nimi':{'fi':'Stadin ammattiopisto'},'lyhytNimi':{'fi':'Stadin ammattiopisto'},'koodistoUri':'oppilaitosnumero'},'nimi':{'fi':'Stadin ammattiopisto'}},'koulutustoimija':{'oid':'1.2.246.562.10.346830761110','nimi':{'fi':'HELSINGIN KAUPUNKI','sv':'Helsingfors stad'}},'alkamispäivä':'2000-01-01','tavoite':{'koodiarvo':'ammatillinentutkinto','nimi':{'fi':'Ammatillinen tutkinto'},'koodistoUri':'suorituksentyyppi','koodistoVersio':1},'tila':{'opiskeluoikeusjaksot':[{'alku':'2000-01-01','tila':{'koodiarvo':'lasna','nimi':{'fi':'Läsnä'},'koodistoUri':'koskiopiskeluoikeudentila','koodistoVersio':1}}]},'tyyppi':{'koodiarvo':'ammatillinenkoulutus','nimi':{'fi':'Ammatillinen koulutus'},'lyhytNimi':{'fi':'Ammatillinen koulutus'},'koodistoUri':'opiskeluoikeudentyyppi','koodistoVersio':1}}]}
+
+export const esimerkkidata = [
+  {
+    aika: '10.8.2016 11:32',
+    oppija: {hetu: '010101-123N', kutsumanimi: 'Eero', sukunimi: 'Esimerkki', oid: '1.2.246.562.24.00000000001'},
+    oppilaitos: {oid: '1.2.246.562.10.14613773812', nimi: {fi: 'Jyväskylän normaalikoulu'}},
+    virhe: 'Organisaatiota 1.2.246.562.100.24253545345 ei löydy organisaatiopalvelusta.',
+    inputData: inputData
+  }
+]
+
+
+export const Tiedonsiirtotaulukko = React.createClass({
+  render() {
+    const { rivit } = this.props
+    const showDataForRow = this.state && this.state.showDataForRow
+    return (<div>
+      <table>
+        <thead>
+        <tr>
+          <th className="aika">Aika</th>
+          <th className="hetu">Henkilötunnus</th>
+          <th className="nimi">Nimi</th>
+          <th className="oppilaitos">Oppilaitos</th>
+          <th className="virhe">Virhe</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          rivit.map((row) => <Lokirivi row={row} parent={this}/>)
+        }
+        </tbody>
+      </table>
+      {
+        showDataForRow && <LokirivinData row={showDataForRow} parent={this}/>
+      }
+    </div>)
+  }
+})
+
+const Lokirivi = React.createClass({
+  render() {
+    const {row, parent} = this.props
+    const showData = () => parent.setState({showDataForRow: row})
+    const nimi = row.oppija.kutsumanimi + ' ' + row.oppija.sukunimi
+    return (<tr>
+      <td className="aika">{row.aika}</td>
+      <td className="hetu">{row.oppija.hetu}</td>
+      <td className="nimi">{
+        row.oppija.oid
+          ? <a href={`/koski/oppija/${row.oppija.oid}`}>{nimi}</a> : nimi
+      }</td>
+      <td className="oppilaitos">{row.oppilaitos.nimi.fi}</td>
+      <td className="virhe">{row.virhe} (<a onClick={showData}>tiedot</a>)</td>
+    </tr>)
+  }
+})
+
+const LokirivinData = React.createClass({
+  render() {
+    const { row, parent } = this.props
+    console.log('showing row', row)
+    return (<div className="lokirividata-popup">
+      <a className="close" onClick={() => parent.setState({showDataForRow: null})}>Sulje</a>
+      <pre>{JSON.stringify(row.inputData, null, 2)}</pre>
+    </div>)
+  }
+})
--- web/app/Tiedonsiirtovirheet.jsx
+++ web/app/Tiedonsiirtovirheet.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Bacon from 'baconjs'
 import { tiedonsiirrotContentP } from './Tiedonsiirrot.jsx'
+import { Tiedonsiirtotaulukko, esimerkkidata } from './Tiedonsiirtotaulukko.jsx'
 
 export const tiedonsiirtovirheetContentP = tiedonsiirrotContentP('/koski/tiedonsiirrot/virheet', Bacon.constant(<div>
   <p>Alla olevien opiskelijoiden tiedot ovat virhetilassa.</p>
   <p>Opiskelija poistuu virhelistalta kun virheen aiheuttanut tieto on korjattu lähdejärjestelmässä ja opiskelijan
     tiedot on siirretty uudelleen onnistuneesti KOSKI-palveluun.</p>
+  <Tiedonsiirtotaulukko rivit={esimerkkidata}/>
 </div>))
