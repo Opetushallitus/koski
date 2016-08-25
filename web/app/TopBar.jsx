a4fc62bb443edd640816d5cc9604abb8c45905dc
Juha Paananen <juha.paananen@gmail.com>
2016-08-25 14:13:23 +0300

    The Koski heading is now a link to Koski root

--- web/app/TopBar.jsx
+++ web/app/TopBar.jsx
@@ -4,7 +4,7 @@ import {UserInfo} from './UserInfo.jsx'
 export const TopBar = ({user, saved}) => (
   <header id='topbar' className={saved ? 'saved' : ''}>
     <div id='logo'>Opintopolku.fi</div>
-    <h1>Koski</h1>
+    <h1><a href="/koski">Koski</a></h1>
     <span className="save-info">Kaikki muutokset tallennettu.</span>
     <UserInfo user={user} />
   </header>
