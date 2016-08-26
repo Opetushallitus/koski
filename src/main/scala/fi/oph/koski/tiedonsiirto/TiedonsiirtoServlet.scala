a4c305794498bef49b4ae4ae10775020dafdbbf9
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-08-26 16:46:32 +0300

    Hae tiedonsiirtoloki kannasta

--- src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoServlet.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoServlet.scala
@@ -3,46 +3,42 @@ package fi.oph.koski.tiedonsiirto
 import java.time.LocalDateTime
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.documentation.ExamplesPerusopetus
+import fi.oph.koski.db.Tables.TiedonsiirtoRow
+import fi.oph.koski.json.Json
 import fi.oph.koski.koskiuser.RequiresAuthentication
-import fi.oph.koski.localization.LocalizedString
-import fi.oph.koski.oppija.MockOppijat
-import fi.oph.koski.schema._
-import MockOppijat._
+import fi.oph.koski.schema.{Koodistokoodiviite, OrganisaatioWithOid}
 import fi.oph.koski.servlet.ApiServlet
-import fi.oph.koski.util.{DateOrdering, FinnishDateFormat}
+import fi.oph.koski.util.DateOrdering
 
 class TiedonsiirtoServlet(val application: KoskiApplication) extends ApiServlet with RequiresAuthentication {
-  import TiedonsiirtoRivi._
   get() {
-    groupByOppija(kaikki)
+    kaikkiTiedonsiirrot
   }
+
   get("/virheet") {
-    groupByOppija(virheelliset)
+    virheelliset
   }
 
-  def groupByOppija(rivit: List[TiedonsiirtoRivi]) = {
+  private def kaikkiTiedonsiirrot: List[HenkilönTiedonsiirrot] = toHenkilönTiedonsiirrot(findAllTiedonsiirrot)
+  private def virheelliset: List[HenkilönTiedonsiirrot] = toHenkilönTiedonsiirrot(findAllTiedonsiirrot.filter(_.virheet.isDefined))
+
+  implicit val formats = Json.jsonFormats
+  def toHenkilönTiedonsiirrot(tiedonsiirrot: Seq[TiedonsiirtoRow]) = {
     implicit val ordering = DateOrdering.localDateTimeReverseOrdering
-    rivit.groupBy(_.oppija).toList.map{ case (henkilö, rivit) => HenkilönTiedonsiirrot(henkilö, rivit.sortBy(_.aika))}.sortBy(_.rivit(0).aika)
+    tiedonsiirrot.groupBy(_.oppija).map {
+      case (oppijaOpt, rows) =>
+        val oppija = oppijaOpt.flatMap(_.extractOpt[Henkilö])
+        val rivit = rows.map { row =>
+          val oppilaitos = row.oppilaitos.flatMap(_.extractOpt[List[OrganisaatioWithOid]])
+          TiedonsiirtoRivi(row.aikaleima.toLocalDateTime, oppija, oppilaitos, row.virheet, row.data)
+        }
+        HenkilönTiedonsiirrot(oppija, rivit.sortBy(_.aika))
+    }.toList.sortBy(_.rivit.head.aika)
   }
+
+  private def findAllTiedonsiirrot: Seq[TiedonsiirtoRow] = application.tiedonsiirtoRepository.findByOrganisaatio(koskiUser)
 }
 
-case class HenkilönTiedonsiirrot(oppija: Option[Henkilö], rivit: List[TiedonsiirtoRivi])
-case class TiedonsiirtoRivi(aika: LocalDateTime, oppija: Option[Henkilö], oppilaitos: Option[OrganisaatioWithOid], virhe: Option[AnyRef], inputData: Option[AnyRef])
-
-object TiedonsiirtoRivi {
-  val data = ExamplesPerusopetus.ysiluokkalainen
-  private val jyväskylänNormaalikoulu = Some(OidOrganisaatio("1.2.246.562.10.14613773812", Some(LocalizedString.finnish("Jyväskylän normaalikoulu"))))
-
-
-  val kaikki = List(
-    TiedonsiirtoRivi(d("15.8.2016 11:32"), Some(eero), jyväskylänNormaalikoulu, Some("Organisaatiota 1.2.246.562.100.24253545345 ei löydy organisaatiopalvelusta."), Some(data)),
-    TiedonsiirtoRivi(d("14.8.2016 11:32"), Some(MockOppijat.markkanen), jyväskylänNormaalikoulu, None, None),
-    TiedonsiirtoRivi(d("7.8.2016 9:08"), Some(eero), jyväskylänNormaalikoulu, None, None)
-  )
-
-  val virheelliset = kaikki.filter(_.virhe.isDefined)
-
-  def d(s: String) = LocalDateTime.parse(s, FinnishDateFormat.finnishDateTimeFormat)
-
-}
+case class HenkilönTiedonsiirrot(oppija: Option[Henkilö], rivit: Seq[TiedonsiirtoRivi])
+case class TiedonsiirtoRivi(aika: LocalDateTime, oppija: Option[Henkilö], oppilaitos: Option[List[OrganisaatioWithOid]], virhe: Option[AnyRef], inputData: Option[AnyRef])
+case class Henkilö(oid: Option[String], hetu: Option[String], etunimet: Option[String], kutsumanimi: Option[String], sukunimi: Option[String], äidinkieli: Option[Koodistokoodiviite])
