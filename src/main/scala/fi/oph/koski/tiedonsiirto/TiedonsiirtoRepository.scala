7a62552415f3b3e441298217c87b04c1ce9920d2
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-01 11:24:56 +0300

    Näytä vain 1000 uusinta tiedonsiirtoa

--- src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoRepository.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoRepository.scala
@@ -8,6 +8,8 @@ import org.json4s.JsonAST.JValue
 import fi.oph.koski.db.PostgresDriverWithJsonSupport.api._
 
 class TiedonsiirtoRepository(db: DB) extends  GlobalExecutionContext with Futures {
+  val maxResults = 1000
+
   def create(kayttajaOid: String, tallentajaOrganisaatioOid: String, oppija: Option[JValue], oppilaitos: Option[JValue], error: Option[TiedonsiirtoError]) = {
 
     val (data, virheet) = error.map(e => (Some(e.data), Some(e.virheet))).getOrElse((None, None))
@@ -18,7 +20,7 @@ class TiedonsiirtoRepository(db: DB) extends  GlobalExecutionContext with Future
   }
 
   def findByOrganisaatio(koskiUser: KoskiUser): Seq[TiedonsiirtoRow] =
-    await(db.run(TiedonsiirtoWithAccessCheck(koskiUser).result))
+    await(db.run(TiedonsiirtoWithAccessCheck(koskiUser).sortBy(_.id.desc).take(maxResults).result))
 }
 
 case class TiedonsiirtoError(data: JValue, virheet: JValue)
