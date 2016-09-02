1682cd67659d368691130538ed1e4161643eaaf3
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-02 15:44:25 +0300

    Cleanup

--- src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoService.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoService.scala
@@ -1,5 +1,7 @@
 package fi.oph.koski.tiedonsiirto
 
+import java.time.LocalDateTime
+
 import fi.oph.koski.db.Tables.TiedonsiirtoRow
 import fi.oph.koski.json.Json
 import fi.oph.koski.json.Json._
@@ -9,15 +11,19 @@ import fi.oph.koski.log.KoskiOperation._
 import fi.oph.koski.log.{AuditLog, AuditLogMessage}
 import fi.oph.koski.oppija.OppijaRepository
 import fi.oph.koski.organisaatio.OrganisaatioRepository
+import fi.oph.koski.schema.{Koodistokoodiviite, OrganisaatioWithOid}
+import fi.oph.koski.util.DateOrdering
 import org.json4s.JsonAST.{JArray, JString}
 import org.json4s._
 
 class TiedonsiirtoService(tiedonsiirtoRepository: TiedonsiirtoRepository, organisaatioRepository: OrganisaatioRepository, oppijaRepository: OppijaRepository) {
+  def kaikkiTiedonsiirrot(koskiUser: KoskiUser): List[HenkilönTiedonsiirrot] = toHenkilönTiedonsiirrot(findAll(koskiUser))
 
-  def findAll(koskiUser: KoskiUser): Seq[TiedonsiirtoRow] = {
-    AuditLog.log(AuditLogMessage(TIEDONSIIRTO_KATSOMINEN, koskiUser, Map(juuriOrganisaatio -> koskiUser.juuriOrganisaatio.map(_.oid).getOrElse("ei juuriorganisaatiota"))))
-    tiedonsiirtoRepository.findByOrganisaatio(koskiUser)
-  }
+  def virheelliset(koskiUser: KoskiUser): List[HenkilönTiedonsiirrot] =
+    kaikkiTiedonsiirrot(koskiUser).filter { siirrot =>
+      siirrot.rivit.groupBy(_.oppilaitos).exists { case (_, rivit) => rivit.headOption.exists(_.virhe.isDefined) }
+    }.map(v => v.copy(rivit = v.rivit.filter(_.virhe.isDefined)))
+
 
   def storeTiedonsiirtoResult(implicit koskiUser: KoskiUser, data: Option[JValue], error: Option[TiedonsiirtoError]) {
     if (!koskiUser.isPalvelukäyttäjä) {
@@ -39,6 +45,24 @@ class TiedonsiirtoService(tiedonsiirtoRepository: TiedonsiirtoRepository, organi
     koskiUser.juuriOrganisaatio.foreach(org => tiedonsiirtoRepository.create(koskiUser.oid, org.oid, oppija, oppilaitokset, error))
   }
 
+  private def toHenkilönTiedonsiirrot(tiedonsiirrot: Seq[TiedonsiirtoRow]) = {
+    implicit val ordering = DateOrdering.localDateTimeReverseOrdering
+    tiedonsiirrot.groupBy(_.oppija).map {
+      case (oppijaOpt, rows) =>
+        val oppija = oppijaOpt.flatMap(_.extractOpt[Henkilö])
+        val rivit = rows.map { row =>
+          val oppilaitos = row.oppilaitos.flatMap(_.extractOpt[List[OrganisaatioWithOid]])
+          TiedonsiirtoRivi(row.aikaleima.toLocalDateTime, oppija, oppilaitos, row.virheet, row.data)
+        }
+        HenkilönTiedonsiirrot(oppija, rivit.sortBy(_.aika))
+    }.toList.sortBy(_.rivit.head.aika)
+  }
+
+  private def findAll(koskiUser: KoskiUser): Seq[TiedonsiirtoRow] = {
+    AuditLog.log(AuditLogMessage(TIEDONSIIRTO_KATSOMINEN, koskiUser, Map(juuriOrganisaatio -> koskiUser.juuriOrganisaatio.map(_.oid).getOrElse("ei juuriorganisaatiota"))))
+    tiedonsiirtoRepository.findByOrganisaatio(koskiUser)
+  }
+
   private def haeOidilla(oid: String)(implicit user: KoskiUser): Option[JValue] =
     oppijaRepository.findByOid(oid).map(o => toJValue(o.toHenkilötiedotJaOid))
 
@@ -46,4 +70,7 @@ class TiedonsiirtoService(tiedonsiirtoRepository: TiedonsiirtoRepository, organi
     oppijaRepository.findOppijat(hetu).headOption.map(toJValue)
 }
 
+case class HenkilönTiedonsiirrot(oppija: Option[Henkilö], rivit: Seq[TiedonsiirtoRivi])
+case class TiedonsiirtoRivi(aika: LocalDateTime, oppija: Option[Henkilö], oppilaitos: Option[List[OrganisaatioWithOid]], virhe: Option[AnyRef], inputData: Option[AnyRef])
+case class Henkilö(oid: Option[String], hetu: Option[String], etunimet: Option[String], kutsumanimi: Option[String], sukunimi: Option[String], äidinkieli: Option[Koodistokoodiviite])
 case class HetuTaiOid(oid: Option[String], hetu: Option[String])
--- src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoServlet.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoServlet.scala
@@ -1,46 +1,15 @@
 package fi.oph.koski.tiedonsiirto
 
-import java.time.LocalDateTime
-
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.db.Tables.TiedonsiirtoRow
-import fi.oph.koski.json.Json
 import fi.oph.koski.koskiuser.RequiresAuthentication
-import fi.oph.koski.schema.{Koodistokoodiviite, OrganisaatioWithOid}
 import fi.oph.koski.servlet.ApiServlet
-import fi.oph.koski.util.DateOrdering
 
 class TiedonsiirtoServlet(val application: KoskiApplication) extends ApiServlet with RequiresAuthentication {
   get() {
-    kaikkiTiedonsiirrot
+    application.tiedonsiirtoService.kaikkiTiedonsiirrot(koskiUser)
   }
 
   get("/virheet") {
-    virheelliset
+    application.tiedonsiirtoService.virheelliset(koskiUser)
   }
-
-  private def kaikkiTiedonsiirrot: List[HenkilönTiedonsiirrot] = toHenkilönTiedonsiirrot(application.tiedonsiirtoService.findAll(koskiUser))
-  private def virheelliset: List[HenkilönTiedonsiirrot] =
-    kaikkiTiedonsiirrot.filter { siirrot =>
-      siirrot.rivit.groupBy(_.oppilaitos).exists { case (_, rivit) => rivit.headOption.exists(_.virhe.isDefined) }
-    }.map(v => v.copy(rivit = v.rivit.filter(_.virhe.isDefined)))
-
-  implicit val formats = Json.jsonFormats
-  def toHenkilönTiedonsiirrot(tiedonsiirrot: Seq[TiedonsiirtoRow]) = {
-    implicit val ordering = DateOrdering.localDateTimeReverseOrdering
-    tiedonsiirrot.groupBy(_.oppija).map {
-      case (oppijaOpt, rows) =>
-        val oppija = oppijaOpt.flatMap(_.extractOpt[Henkilö])
-        val rivit = rows.map { row =>
-          val oppilaitos = row.oppilaitos.flatMap(_.extractOpt[List[OrganisaatioWithOid]])
-          TiedonsiirtoRivi(row.aikaleima.toLocalDateTime, oppija, oppilaitos, row.virheet, row.data)
-        }
-        HenkilönTiedonsiirrot(oppija, rivit.sortBy(_.aika))
-    }.toList.sortBy(_.rivit.head.aika)
-  }
-
 }
-
-case class HenkilönTiedonsiirrot(oppija: Option[Henkilö], rivit: Seq[TiedonsiirtoRivi])
-case class TiedonsiirtoRivi(aika: LocalDateTime, oppija: Option[Henkilö], oppilaitos: Option[List[OrganisaatioWithOid]], virhe: Option[AnyRef], inputData: Option[AnyRef])
-case class Henkilö(oid: Option[String], hetu: Option[String], etunimet: Option[String], kutsumanimi: Option[String], sukunimi: Option[String], äidinkieli: Option[Koodistokoodiviite])
