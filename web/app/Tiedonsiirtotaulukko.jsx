7f3013dcabac3f41a0d0b0e7daaea71fb9ab72c8
Juha Paananen <juha.paananen@gmail.com>
2016-09-05 16:03:47 +0300

    UI performance improvement

--- web/app/Tiedonsiirtotaulukko.jsx
+++ web/app/Tiedonsiirtotaulukko.jsx
@@ -4,7 +4,7 @@ import fecha from 'fecha'
 export const Tiedonsiirtotaulukko = React.createClass({
   render() {
     const { rivit, showError } = this.props
-    const showDataForRow = this.state && this.state.showDataForRow
+
     return (<div>
       <table>
         <thead>
@@ -18,29 +18,39 @@ export const Tiedonsiirtotaulukko = React.createClass({
           <th className="tiedot"></th>
         </tr>
         </thead>
-        <tbody>
         {
-          rivit.flatMap((oppijaRivi, i) => {
-            const tiedonsiirtoRivit = oppijaRivi.rivit
-            const isGroup = tiedonsiirtoRivit.length > 1
-            const isExpanded = this.state && this.state.expandedRow == tiedonsiirtoRivit[0]
-            return tiedonsiirtoRivit.flatMap((rivi, j) => {
-                const isParent = j == 0 && isGroup
-                const isChild = j > 0 && isGroup
-                const isHidden = isChild && !isExpanded
-                return isHidden
-                  ? []
-                  : [<Lokirivi key={i + '-' + j} row={rivi} isParent={isParent} isChild={isChild} isExpanded={isExpanded} isEven={i % 2 == 1} parentComponent={this} showError={showError}/>]
-              }
-            )
-          })
+          rivit.map((oppijaRivi, i) => <Lokiriviryhmä oppijaRivi={oppijaRivi} i={i} showError={showError} key={i}/>)
         }
-        </tbody>
       </table>
-      {
-        showDataForRow && <LokirivinData details={showDataForRow} parent={this}/>
-      }
     </div>)
+  }
+})
+
+const Lokiriviryhmä = React.createClass({
+  render() {
+    let { oppijaRivi, i, showError } = this.props
+    const showData = (row) => this.setState({dataToBeShown: row})
+    const setExpanded = (expanded) => this.setState({expanded})
+    const dataToBeShown = this.state && this.state.dataToBeShown
+    const isExpanded = this.state && this.state.expanded
+    const tiedonsiirtoRivit = oppijaRivi.rivit
+    const isGroup = tiedonsiirtoRivit.length > 1
+    return(<tbody>
+      {
+        tiedonsiirtoRivit.flatMap((rivi, j) => {
+            const isParent = j == 0 && isGroup
+            const isChild = j > 0 && isGroup
+            const isHidden = isChild && !isExpanded
+            return isHidden
+              ? []
+              : [<Lokirivi key={i + '-' + j} row={rivi} isParent={isParent} isChild={isChild} isExpanded={isExpanded} isEven={i % 2 == 1} showError={showError} showData={showData} setExpanded={setExpanded}/>]
+          }
+        )
+      }
+      {
+        dataToBeShown && <LokirivinData details={dataToBeShown} showData={showData}/>
+      }
+    </tbody>)
   }
 })
 
@@ -49,7 +59,7 @@ const Lokirivi = React.createClass({
     const extractName = (oppilaitokset) =>
       oppilaitokset && oppilaitokset.map((oppilaitos) => oppilaitos && oppilaitos.nimi && oppilaitos.nimi.fi).join(', ')
 
-    const {row, isParent, isChild, isExpanded, isEven, parentComponent, showError} = this.props
+    const {row, isParent, isChild, isExpanded, isEven, showError, showData, setExpanded} = this.props
 
     const errorDetails = (virheet) => { return showError ?
       <div>
@@ -59,10 +69,9 @@ const Lokirivi = React.createClass({
         <a className="virheen-tiedot" onClick={() => showErrors(virheet)}>virhe</a>
       </div> : <a className="virheen-tiedot" onClick={() => showErrors(virheet)}>virhe</a>}
 
-    const dataLink = () => <a className="viestin-tiedot" onClick={showData}>tiedot</a>
+    const dataLink = () => <a className="viestin-tiedot" onClick={() => showData(row.inputData)}>tiedot</a>
 
-    const showErrors = (virheet) => parentComponent.setState({showDataForRow: virheet})
-    const showData = () => parentComponent.setState({showDataForRow: row.inputData})
+    const showErrors = (virheet) => showData(virheet)
     const nimi = row.oppija && ((row.oppija.kutsumanimi || '') + ' ' + (row.oppija.sukunimi || ''))
     const className = ((isParent || isChild) ? 'group ' : '') + (isEven ? 'even' : 'odd')
 
@@ -71,8 +80,8 @@ const Lokirivi = React.createClass({
         {
           isParent
             ? (isExpanded
-              ? <a className="collapse" onClick={() => parentComponent.setState({expandedRow: null})}>-</a>
-              : <a className="expand" onClick={() => parentComponent.setState({expandedRow: row})}>+</a>)
+              ? <a className="collapse" onClick={() => setExpanded(false)}>-</a>
+              : <a className="expand" onClick={() => setExpanded(true)}>+</a>)
             : null
         }
         {
@@ -96,10 +105,10 @@ const Lokirivi = React.createClass({
 
 const LokirivinData = React.createClass({
   render() {
-    const { details, parent } = this.props
+    const { details, showData } = this.props
     console.log('showing details', details)
     return (<div className="lokirividata-popup">
-      <a className="close" onClick={() => parent.setState({showDataForRow: null})}>Sulje</a>
+      <a className="close" onClick={() => showData(false)}>Sulje</a>
       <pre>{JSON.stringify(details, null, 2)}</pre>
     </div>)
   }
