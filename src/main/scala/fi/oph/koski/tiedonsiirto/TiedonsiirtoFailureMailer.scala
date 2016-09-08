2b3e73b91bf12910e0c7da94ace11afae0a08310
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-08 16:37:42 +0300

    Throttle sähköpostin lähetykseen

--- src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoFailureMailer.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoFailureMailer.scala
@@ -1,17 +1,34 @@
 package fi.oph.koski.tiedonsiirto
 
+import java.time.LocalDateTime
+import java.time.LocalDateTime.now
+
 import com.typesafe.config.Config
 import fi.oph.koski.email._
 
 class TiedonsiirtoFailureMailer(config: Config) {
+  val sendTimes = scala.collection.mutable.Map[String, LocalDateTime]()
   val sender = EmailSender(config)
+
   def sendMail(organisaatioOid: String): Unit = {
     val emailAddress = "TODO"
-    sender.sendEmail(Email(EmailContent(
-      "no-reply@opintopolku.fi",
-      "Virheellinen KOSKI tiedonsiirto",
-      "Automaattisessa tiedonsiirrossa tapahtui virhe.\\nKäykää ystävällisesti tarkistamassa tapahtuneet tiedonsiirrot osoitteessa TODO.",
-      html = false
-    ), List(EmailRecipient(emailAddress))))
+    sendTimes.synchronized {
+      if (shouldSendMail(emailAddress)) {
+        sendTimes.put(emailAddress, now())
+        sender.sendEmail(Email(EmailContent(
+          "no-reply@opintopolku.fi",
+          "Virheellinen KOSKI tiedonsiirto",
+          "Automaattisessa tiedonsiirrossa tapahtui virhe.\\nKäykää ystävällisesti tarkistamassa tapahtuneet tiedonsiirrot osoitteessa TODO.",
+          html = false
+        ), List(EmailRecipient(emailAddress))))
+      }
+    }
   }
+
+  def shouldSendMail(email: String) =
+    sendTimes.get(email) match {
+      case Some(time) if time.isBefore(now().minusHours(24)) => true
+      case None => true
+      case _ => false
+    }
 }
