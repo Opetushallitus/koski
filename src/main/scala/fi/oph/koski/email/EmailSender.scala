c2a3afb9b8d00971611617346abd774b77eb1ec2
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-08 15:34:34 +0300

    Ryhmäsähköpostisender

--- src/main/scala/fi/oph/koski/email/EmailSender.scala
+++ src/main/scala/fi/oph/koski/email/EmailSender.scala
@@ -1,5 +1,10 @@
 package fi.oph.koski.email
 
+import com.typesafe.config.Config
+import fi.oph.koski.http.{Http, VirkailijaHttpClient}
+import fi.oph.koski.json.Json.jsonFormats
+import fi.oph.koski.json.Json4sHttp4s.json4sEncoderOf
+
 trait EmailSender {
   def sendEmail(envelope: Email) {}
 }
@@ -8,9 +13,29 @@ case class Email(email: EmailContent, recipient: List[EmailRecipient])
 case class EmailContent(from: String, subject: String, body: String, html: Boolean)
 case class EmailRecipient(email: String)
 
+object EmailSender {
+  def apply(config: Config): EmailSender =
+    if (config.hasPath("ryhmäsähköposti.virkailija.url")) {
+      RyhmäsähköpostiSender(config)
+    } else {
+      MockEmailSender
+    }
+}
+
 object MockEmailSender extends EmailSender {
   private var mails: List[Email] = Nil
   def sendMail(mail: Email): Unit = this.synchronized {
     mails = mail :: mails
   }
 }
+
+case class RyhmäsähköpostiSender(config: Config) extends EmailSender {
+  import Http._
+
+  val http = VirkailijaHttpClient(config.getString("ryhmäsähköposti.virkailija.username"), config.getString("ryhmäsähköposti.virkailija.password"), config.getString("ryhmäsähköposti.virkailija.url"), "/ryhmasahkoposti-service")
+
+  override def sendEmail(envelope: Email): Unit = {
+    http.post(uri"/ryhmasahkoposti-service/email", envelope)(json4sEncoderOf[Email], Http.expectSuccess)
+  }
+}
+
--- src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoFailureMailer.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoFailureMailer.scala
@@ -1,15 +1,16 @@
 package fi.oph.koski.tiedonsiirto
 
+import com.typesafe.config.Config
 import fi.oph.koski.email._
 
-class TiedonsiirtoFailureMailer {
-  val sender = MockEmailSender
+class TiedonsiirtoFailureMailer(config: Config) {
+  val sender = EmailSender(config)
   def sendMail(organisaatioOid: String): Unit = {
     val emailAddress = "TODO"
     sender.sendEmail(Email(EmailContent(
       "no-reply@opintopolku.fi",
       "Virheellinen KOSKI tiedonsiirto",
-      "Automaattisessa tiedonsiirrossa tapahtui virhe.\\nKäykää ystävällisesti tarkistamassa tapahtuneet tiedonsiirrot osoitteessa https://koskidev.koski.oph.reaktor.fi/koski/tiedonsiirrot.",
+      "Automaattisessa tiedonsiirrossa tapahtui virhe.\\nKäykää ystävällisesti tarkistamassa tapahtuneet tiedonsiirrot osoitteessa TODO.",
       html = false
     ), List(EmailRecipient(emailAddress))))
   }
