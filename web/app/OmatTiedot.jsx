ff3019cce7067e5549c88794a5ed76b73fb766fe
Kalle Kainu <kalle.kainu@reaktor.fi>
2016-09-08 09:54:33 +0300

    Handle missing omattiedot gracefully

--- web/app/OmatTiedot.jsx
+++ web/app/OmatTiedot.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Http from './http'
 import {ExistingOppija} from './Oppija.jsx'
+import Bacon from 'baconjs'
 
-const omatTiedotP = () => Http.get('/koski/api/editor/omattiedot').toProperty()
+const omatTiedotP = () => Http.get('/koski/api/editor/omattiedot').toProperty().flatMapError((e) => e.httpStatus === 404 ? null : new Bacon.Error)
 
 export const omatTiedotContentP = () => omatTiedotP().map((oppija) =>
-  <div className="content-area">
-    <nav className="sidebar"></nav>
-    <ExistingOppija className="main-content" oppija={oppija}/>
+  <div className="content-area omattiedot">
+    <nav className="sidebar omattiedot-navi"></nav>
+    { oppija ?
+      <ExistingOppija className="main-content" oppija={oppija}/> :
+      <div className="main-content ei-opiskeluoikeuksia">Tiedoillasi ei löydy opiskeluoikeuksia</div>
+    }
   </div>
 )
