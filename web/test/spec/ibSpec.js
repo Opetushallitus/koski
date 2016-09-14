37d714c687d3928f47b0d3695cc53165c8aa2711
Juha Paananen <juha.paananen@gmail.com>
2016-09-14 15:22:07 +0300

    ib exam mocha test

---
+++ web/test/spec/ibSpec.js
@@ -0,0 +1,44 @@
+describe('IB-tutkinto', function( ) {
+  var page = KoskiPage()
+  var todistus = TodistusPage()
+  var opinnot = OpinnotPage()
+  before(Authentication().login(), resetFixtures)
+
+  describe('IB-tutkintotodistus', function () {
+    before(page.openPage, page.oppijaHaku.search('130996-9225', page.isOppijaSelected('Iina')))
+    describe('Oppijan suorituksissa', function () {
+      it('näytetään', function () {
+        expect(opinnot.getTutkinto(0)).to.equal("Pre-IB luokan oppimäärä")
+        expect(opinnot.getOppilaitos()).to.equal("Ressun lukio")
+      })
+    })
+    describe('Kaikki tiedot näkyvissä', function () {
+      before(opinnot.expandAll)
+      it('toimii', function () {
+        expect(S('.preiboppiaineensuoritus:eq(0) .preibkurssinsuoritus:eq(0) .koulutusmoduuli .tunniste .value').text()).to.equal('Tekstit ja vuorovaikutus')
+      })
+    })
+    /*
+    describe('Tulostettava todistus', function () {
+      before(opinnot.avaaTodistus(3))
+      it('näytetään', function () {
+        // See more detailed content specification in LukioSpec.scala
+        expect(todistus.vahvistus()).to.equal('Jyväskylä 4.6.2016 Reijo Reksi rehtori')
+      })
+    })
+    */
+  })
+
+  describe('Opintosuoritusote', function () {
+    before(page.openPage, page.oppijaHaku.search('130996-9225', page.isOppijaSelected('Iina')))
+    /*
+    before(opinnot.avaaOpintosuoritusote(1))
+
+    describe('Kun klikataan linkkiä', function () {
+      it('näytetään', function () {
+      })
+    })
+
+    */
+  })
+})
