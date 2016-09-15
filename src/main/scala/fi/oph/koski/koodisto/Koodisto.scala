481b6d8794a2543345b13fc90f893ecea5273593
Juha Paananen <juha.paananen@gmail.com>
2016-09-15 10:39:02 +0300

    palautettu kenttä organisaatioOid

--- src/main/scala/fi/oph/koski/koodisto/Koodisto.scala
+++ src/main/scala/fi/oph/koski/koodisto/Koodisto.scala
@@ -2,7 +2,8 @@ package fi.oph.koski.koodisto
 
 import java.time.LocalDate
 
-case class Koodisto(koodistoUri: String, versio: Int, metadata: List[KoodistoMetadata], codesGroupUri: String, voimassaAlkuPvm: LocalDate) {
+// Tätä oliota käytetään myös koodistojen luonnissa. Älä poista kenttiä!
+case class Koodisto(koodistoUri: String, versio: Int, metadata: List[KoodistoMetadata], codesGroupUri: String, voimassaAlkuPvm: LocalDate, organisaatioOid: String) {
   def koodistoViite = KoodistoViite(koodistoUri, versio)
 }
 
