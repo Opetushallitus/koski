37996f82014ed03a21522fd361dd24328532c489
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-15 11:46:07 +0300

    Lisää serverin käynnistymistesti

---
+++ src/test/scala/fi/oph/koski/integrationtest/ServerStartupIntegrationTest.scala
@@ -0,0 +1,19 @@
+package fi.oph.koski.integrationtest
+
+import fi.oph.koski.jettylauncher.JettyLauncher
+import fi.oph.koski.log.LogTester
+import fi.oph.koski.util.PortChecker
+import org.apache.log4j.Level.{ERROR, FATAL}
+import org.eclipse.jetty.server.Server
+import org.scalatest.{FreeSpec, Matchers}
+
+// This test should be run against the KoskiDev test environment, you need to provide the corresponding configuration with -Dconfig.resource or
+// using System.properties
+class ServerStartupIntegrationTest extends FreeSpec with Matchers with LogTester {
+  "Server starts without errors" taggedAs(KoskiDevEnvironment) in {
+    setup
+    System.setProperty("log4j.configuration", "/dev/null")
+    new JettyLauncher(PortChecker.findFreeLocalPort).start
+    getLogMessages.find(m => m.getLevel == ERROR || m.getLevel == FATAL) should be(empty)
+  }
+}
