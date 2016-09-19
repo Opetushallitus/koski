0768b0fec3f363cb49d444baa47d2f86b914e5c1
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-19 16:36:22 +0300

    Lisää Lukion oppiaineen suoritus luvan osasuoritukiin

--- src/main/scala/fi/oph/koski/schema/Lukio.scala
+++ src/main/scala/fi/oph/koski/schema/Lukio.scala
@@ -117,7 +117,7 @@ case class LukionOppiaineenSuoritus(
   override val osasuoritukset: Option[List[LukionKurssinSuoritus]],
   @KoodistoKoodiarvo("lukionoppiaine")
   tyyppi: Koodistokoodiviite = Koodistokoodiviite(koodiarvo = "lukionoppiaine", koodistoUri = "suorituksentyyppi")
-) extends OppiaineenSuoritus with VahvistuksetonSuoritus
+) extends OppiaineenSuoritus with VahvistuksetonSuoritus with LukioonValmistavanKoulutuksenOsasuoritus
 
 case class LukionKurssinSuoritus(
   @Description("Lukion kurssin tunnistetiedot")
