f34e704af263aacc6a02af49aa249bf85a5d376f
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-29 15:53:44 +0300

    Esiopetus wip

---
+++ src/main/scala/fi/oph/koski/schema/Esiopetus.scala
@@ -0,0 +1,44 @@
+package fi.oph.koski.schema
+
+import java.time.LocalDate
+
+import fi.oph.scalaschema.annotation.Description
+
+case class EsiopetuksenOpiskeluoikeus(
+  id: Option[Int] = None,
+  versionumero: Option[Int]  = None,
+  lähdejärjestelmänId: Option[LähdejärjestelmäId] = None,
+  oppilaitos: Oppilaitos,
+  koulutustoimija: Option[Koulutustoimija] = None,
+  alkamispäivä: Option[LocalDate] = None,
+  arvioituPäättymispäivä: Option[LocalDate] = None,
+  päättymispäivä: Option[LocalDate] = None,
+  tila: OpiskeluoikeudenTila,
+  läsnäolotiedot: Option[YleisetLäsnäolotiedot] = None,
+  suoritukset: List[EsiopetuksenSuoritus],
+  @KoodistoKoodiarvo("esiopetus")
+  tyyppi: Koodistokoodiviite = Koodistokoodiviite("esiopetus", "opiskeluoikeudentyyppi")
+) extends KoskeenTallennettavaOpiskeluoikeus {
+  override def withIdAndVersion(id: Option[Int], versionumero: Option[Int]) = this.copy(id = id, versionumero = versionumero)
+  override def withKoulutustoimija(koulutustoimija: Koulutustoimija) = this.copy(koulutustoimija = Some(koulutustoimija))
+  override def withSuoritukset(suoritukset: List[PäätasonSuoritus]) = copy(suoritukset = suoritukset.asInstanceOf[List[EsiopetuksenSuoritus]])
+}
+
+case class EsiopetuksenSuoritus(
+  toimipiste: OrganisaatioWithOid,
+  tila: Koodistokoodiviite,
+  @KoodistoKoodiarvo("esiopetuksensuoritus")
+  tyyppi: Koodistokoodiviite = Koodistokoodiviite("esiopetuksensuoritus", koodistoUri = "suorituksentyyppi"),
+  vahvistus: Option[Vahvistus] = None,
+  suorituskieli: Option[Koodistokoodiviite] = None,
+  koulutusmoduuli: Esiopetus = Esiopetus()
+) extends PäätasonSuoritus with Toimipisteellinen {
+  override def arviointi: Option[List[Arviointi]] = None
+}
+
+@Description("Esiopetuksen tunnistetiedot")
+case class Esiopetus(
+  tunniste: Koodistokoodiviite = Koodistokoodiviite("110100", koodistoUri = "koulutus") // TODO: tarkista koodiarvo
+) extends KoodistostaLöytyväKoulutusmoduuli {
+  override def laajuus: Option[Laajuus] = None
+}
