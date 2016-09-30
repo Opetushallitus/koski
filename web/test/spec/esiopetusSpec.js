d36cf333c8279be4a471dac4ebc8f61545fc388a
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-09-30 13:53:39 +0300

    Lisää esiopetuksen testi

---
+++ web/test/spec/esiopetusSpec.js
@@ -0,0 +1,24 @@
+describe('Esiopetus', function() {
+  var page = KoskiPage()
+  var login = LoginPage()
+  var todistus = TodistusPage()
+  var opinnot = OpinnotPage()
+
+  before(Authentication().login(), resetFixtures)
+
+  describe('Esiopetuksen opiskeluoikeudet', function() {
+    before(page.openPage, page.oppijaHaku.search('300996-870E', page.isOppijaSelected('Essi')))
+    describe('Oppijan suorituksissa', function() {
+      it('näytetään', function() {
+        expect(opinnot.getOppilaitos()).to.equal('Jyväskylän normaalikoulu')
+        expect(opinnot.getTutkinto()).to.equal('Peruskoulun esiopetus')
+      })
+    })
+    describe('Kaikki tiedot näkyvissä', function() {
+      before(opinnot.expandAll)
+      it('toimii', function() {
+        expect(S('.esiopetuksensuoritus:eq(0) .koulutusmoduuli .inline').text()).to.equal('Peruskoulun esiopetus')
+      })
+    })
+  })
+})
