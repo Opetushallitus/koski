e565f1bdcc76be4785fd971a0565f980d8d0bab0
Juha Paananen <juha.paananen@gmail.com>
2016-10-31 12:26:08 +0200

    Luodaan vain koski-alkuiset käyttöoikeusryhmät

--- src/main/scala/fi/oph/koski/koskiuser/KäyttöoikeusRyhmätCreator.scala
+++ src/main/scala/fi/oph/koski/koskiuser/KäyttöoikeusRyhmätCreator.scala
@@ -6,6 +6,7 @@ import fi.oph.koski.henkilo.AuthenticationServiceClient.UusiKäyttöoikeusryhmä
 import fi.oph.koski.koodisto.KoodistoPalvelu
 import fi.oph.koski.log.Logging
 import fi.oph.koski.organisaatio.Opetushallitus
+import Käyttöoikeusryhmät._
 
 object KäyttöoikeusRyhmätCreator extends Logging {
   def luoKäyttöoikeusRyhmät(config: Config): Unit = {
@@ -14,7 +15,8 @@ object KäyttöoikeusRyhmätCreator extends Logging {
     val koodistopalvelu: KoodistoPalvelu = KoodistoPalvelu.withoutCache(config)
     val oppilaitostyypit: List[String] = koodistopalvelu.getLatestVersion("oppilaitostyyppi").flatMap(koodistopalvelu.getKoodistoKoodit(_)).toList.flatten.map(_.koodiArvo)
 
-    Käyttöoikeusryhmät.käyttöoikeusryhmät foreach { ryhmä =>
+
+    Käyttöoikeusryhmät.käyttöoikeusryhmät.filter(_.nimi.startsWith("koski-")) foreach { ryhmä =>
       val olemassaOlevaRyhmä = olemassaOlevatRyhmät.find(olemassaOlevaRyhmä => olemassaOlevaRyhmä.toKoskiKäyttöoikeusryhmä.map(_.nimi) == Some(ryhmä.nimi))
       val organisaatioTyypit = (ryhmä.orgAccessType, ryhmä.globalAccessType) match {
         case (Nil, Nil) => List(Opetushallitus.organisaatioOid) // ylläpitokäyttäjä => liitetään OPH-organisaatioon
