4c34819e117675bf5f71f08f146cde90e3e38738
Juha Paananen <juha.paananen@gmail.com>
2016-11-01 15:25:56 +0200

    fix healthcheck servlet design

--- src/main/scala/fi/oph/koski/healthcheck/HealthCheckApiServlet.scala
+++ src/main/scala/fi/oph/koski/healthcheck/HealthCheckApiServlet.scala
@@ -1,35 +1,13 @@
 package fi.oph.koski.healthcheck
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.documentation.AmmatillinenExampleData.perustutkintoOpiskeluoikeus
-import fi.oph.koski.http.HttpStatus
-import fi.oph.koski.koskiuser.KoskiUser.systemUser
-import fi.oph.koski.koskiuser.{AccessType, KoskiUser}
-import fi.oph.koski.schema.{OidHenkilö, Oppija}
+import fi.oph.koski.koskiuser.Unauthenticated
 import fi.oph.koski.servlet.{ApiServlet, NoCache}
 
-class HealthCheckApiServlet(val application: KoskiApplication) extends ApiServlet with NoCache {
-  private implicit val user = systemUser
-  private implicit val accessType = AccessType.write
-  private val oid = application.config.getString("healthcheck.oppija.oid")
-  private lazy val oppija: Oppija = application.validator.validateAsJson(Oppija(OidHenkilö(oid), List(perustutkintoOpiskeluoikeus()))).right.get
-
+class HealthCheckApiServlet(val application: KoskiApplication) extends ApiServlet with NoCache with Unauthenticated {
   get() {
-    HeathChecker(application).healthcheck match {
-      case Left(HttpStatus(404, _)) =>
-        logger.info(s"Healtcheck user not found creating one with oid $oid")
-        application.facade.createOrUpdate(oppija) match {
-          case Left(status) =>
-            logger.error(s"Problem creating healthchech oppija ${status.toString}")
-            renderStatus(status.statusCode)
-          case _ => renderStatus(200)
-        }
-      case Left(status) => renderStatus(status.statusCode)
-      case _ => renderStatus(200)
-    }
+    renderStatus(HeathChecker(application).healthcheck.statusCode)
   }
-
-  override def koskiUserOption: Option[KoskiUser] = Some(systemUser)
 
   private def renderStatus(sc: Int): Unit = renderObject(Map("status" -> sc))
 }
--- src/main/scala/fi/oph/koski/healthcheck/HeathChecker.scala
+++ src/main/scala/fi/oph/koski/healthcheck/HeathChecker.scala
@@ -1,8 +1,30 @@
 package fi.oph.koski.healthcheck
 
 import fi.oph.koski.config.KoskiApplication
+import fi.oph.koski.documentation.AmmatillinenExampleData._
+import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
+import fi.oph.koski.koskiuser.AccessType
 import fi.oph.koski.koskiuser.KoskiUser._
+import fi.oph.koski.log.Logging
+import fi.oph.koski.schema.{OidHenkilö, Oppija}
 
-case class HeathChecker(application: KoskiApplication) {
-  def healthcheck = application.facade.findOppija(application.config.getString("healthcheck.oppija.oid"))(systemUser)
+case class HeathChecker(application: KoskiApplication) extends Logging {
+  private implicit val user = systemUser
+  private implicit val accessType = AccessType.write
+  private val oid = application.config.getString("healthcheck.oppija.oid")
+  private lazy val oppija: Oppija = application.validator.validateAsJson(Oppija(OidHenkilö(oid), List(perustutkintoOpiskeluoikeus()))).right.get
+
+  def healthcheck: HttpStatus = application.facade.findOppija(oid) match {
+    case Left(HttpStatus(404, _)) =>
+      logger.info(s"Healtcheck user not found creating one with oid $oid")
+      application.facade.createOrUpdate(oppija) match {
+        case Left(status) =>
+          logger.error(s"Problem creating healthchech oppija ${status.toString}")
+          status
+        case _ =>
+          HttpStatus.ok
+      }
+    case Left(status) => status
+    case _ => HttpStatus.ok
+  }
 }
