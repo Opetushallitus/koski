2068bea15caedaca39c45a820e73d259b4e2fc13
Juha Paananen <juha.paananen@gmail.com>
2016-11-07 15:46:06 +0200

    add warning

--- src/test/scala/fi/oph/koski/integrationtest/ServerStartupIntegrationTest.scala
+++ src/test/scala/fi/oph/koski/integrationtest/ServerStartupIntegrationTest.scala
@@ -1,5 +1,6 @@
 package fi.oph.koski.integrationtest
 
+import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.jettylauncher.JettyLauncher
 import fi.oph.koski.log.LogTester
 import fi.oph.koski.util.PortChecker
@@ -11,6 +12,9 @@ import org.scalatest.{FreeSpec, Matchers}
 // using System.properties
 class ServerStartupIntegrationTest extends FreeSpec with Matchers with LogTester {
   "Server starts without errors" taggedAs(KoskiDevEnvironment) in {
+    if (!KoskiApplication.defaultConfig.hasPath("opintopolku.virkailija.url")) {
+      System.err.println("ServerStartupIntegrationTest run without opintopolku.virkailija.url => tests mocks only")
+    }
     setup
     new JettyLauncher(PortChecker.findFreeLocalPort) {
       override def configureLogging = {}
