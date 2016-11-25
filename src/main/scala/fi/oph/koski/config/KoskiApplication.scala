1800876f12b38de8f1f126871da13d311fdc81f1
Juha Paananen <juha.paananen@gmail.com>
2016-11-25 11:27:12 +0200

    fix import

--- src/main/scala/fi/oph/koski/config/KoskiApplication.scala
+++ src/main/scala/fi/oph/koski/config/KoskiApplication.scala
@@ -18,7 +18,7 @@ import fi.oph.koski.opiskeluoikeus.{AuxiliaryOpiskeluOikeusRepository, Composite
 import fi.oph.koski.oppija.OppijaRepository
 import fi.oph.koski.oppilaitos.OppilaitosRepository
 import fi.oph.koski.organisaatio.OrganisaatioRepository
-import fi.oph.koski.tiedonsiirto.{TiedonsiirtoFailureMailer, TiedonsiirtoRepository, TiedonsiirtoService}
+import fi.oph.koski.tiedonsiirto.{TiedonsiirtoFailureMailer, TiedonsiirtoService}
 import fi.oph.koski.tutkinto.TutkintoRepository
 import fi.oph.koski.virta.{VirtaAccessChecker, VirtaClient, VirtaOpiskeluoikeusRepository}
 import fi.oph.koski.ytr.{YlioppilasTutkintoRekisteri, YtrAccessChecker, YtrOpiskeluoikeusRepository}
