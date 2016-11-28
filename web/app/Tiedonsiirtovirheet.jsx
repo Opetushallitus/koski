e70d7d05fd7a5e6c28d79caf089384ecbc3aee2d
Juha Paananen <juha.paananen@gmail.com>
2016-11-28 11:10:14 +0200

    make front build exit with error code when it fails

--- web/app/Tiedonsiirtovirheet.jsx
+++ web/app/Tiedonsiirtovirheet.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Pager from './Pager'
+import Pager from './Pasdfager'
 import * as L from 'partial.lenses'
 import { tiedonsiirrotContentP } from './Tiedonsiirrot.jsx'
 import { Tiedonsiirtotaulukko } from './Tiedonsiirtotaulukko.jsx'
