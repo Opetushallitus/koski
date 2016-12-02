6f52dcacbae18c7558ea3d042609cf8896dbebec
Juha Paananen <juha.paananen@gmail.com>
2016-12-02 10:47:54 +0200

    fix test

--- src/test/scala/fi/oph/koski/api/StaticPagesSpec.scala
+++ src/test/scala/fi/oph/koski/api/StaticPagesSpec.scala
@@ -24,7 +24,8 @@ class StaticPagesSpec extends FreeSpec with LocalJettyHttpSpecification with Mat
 
   "Static resources" in {
     get("images/loader.gif") { verifyResponseStatus(200)}
-    get("js/bundle.js") { verifyResponseStatus(200)}
+    get("js/koski-main.js") { verifyResponseStatus(200)}
+    get("js/koski-login.js") { verifyResponseStatus(200)}
     get("test/runner.html") { verifyResponseStatus(200)}
     get("js/codemirror/codemirror.js") { verifyResponseStatus(200)}
   }
