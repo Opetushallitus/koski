d26ba1471956aa1471e1cb20d4cc23826ac0cb2f
Juha Paananen <juha.paananen@gmail.com>
2016-12-13 11:55:02 +0200

    move stuff to component state

--- web/app/OppijaHaku.jsx
+++ web/app/OppijaHaku.jsx
@@ -5,22 +5,8 @@ import {navigateToOppija, navigateToUusiOppija} from './location'
 import {oppijaStateP} from './Oppija.jsx'
 import {modelData} from './EditorModel.js'
 
-const oppijaHakuE = new Bacon.Bus()
-
-const acceptableQuery = (q) => q.length >= 3
-
-const hakuTulosE = oppijaHakuE.debounce(500)
-  .flatMapLatest(q => (acceptableQuery(q) ? Http.get(`/koski/api/henkilo/search?query=${q}`) : Bacon.once([])).map((oppijat) => ({ results: oppijat, query: q })))
-
-const oppijatP = Bacon.update(
-  { query: '', results: [] },
-  hakuTulosE, ((current, hakutulos) => hakutulos)
-)
-
-const searchInProgressP = oppijaHakuE.filter(acceptableQuery).awaiting(oppijatP.mapError().changes()).throttle(200)
-
-export const oppijaHakuElementP = Bacon.combineWith(oppijatP, searchInProgressP, oppijaStateP, (oppijat, searchInProgress, oppija) =>
-  <OppijaHaku oppijat={oppijat} valittu={modelData(oppija.valittuOppija, 'henkilö')} searching={searchInProgress}/>
+export const oppijaHakuElementP = Bacon.combineWith(oppijaStateP, (oppija) =>
+  <OppijaHaku valittu={modelData(oppija.valittuOppija, 'henkilö')}/>
 )
 
 const OppijaHakutulokset = React.createClass({
@@ -45,13 +31,14 @@ const OppijaHakutulokset = React.createClass({
 
 export const OppijaHaku = React.createClass({
   render() {
-    let {oppijat, valittu, searching} = this.props
+    let { valittu } = this.props
+    let { oppijat, searching } = this.state
     const className = searching ? 'oppija-haku searching' : 'oppija-haku'
     return (
       <div className={className}>
         <div>
           <h3>Hae tai lisää opiskelija</h3>
-          <input id='search-query' ref='query' placeholder='henkilötunnus, nimi tai oppijanumero' onInput={(e) => oppijaHakuE.push(e.target.value)}></input>
+          <input id='search-query' ref='query' placeholder='henkilötunnus, nimi tai oppijanumero' onInput={(e) => this.oppijaHakuE.push(e.target.value)}></input>
           <a href="/koski/oppija/uusioppija" className="lisaa-oppija" onClick={navigateToUusiOppija}>Lisää opiskelija</a>
         </div>
         <div className='hakutulokset'>
@@ -59,5 +46,20 @@ export const OppijaHaku = React.createClass({
         </div>
       </div>
     )
+  },
+  getInitialState() {
+    return { oppijat: { query: '', results: [] } }
+  },
+  componentWillMount() {
+    this.oppijaHakuE = new Bacon.Bus()
+
+    const acceptableQuery = (q) => q.length >= 3
+
+    const hakuTulosE = this.oppijaHakuE.debounce(500)
+      .flatMapLatest(q => (acceptableQuery(q) ? Http.get(`/koski/api/henkilo/search?query=${q}`) : Bacon.once([])).map((oppijat) => ({ results: oppijat, query: q })))
+
+    hakuTulosE.onValue((oppijat) => this.setState({oppijat, searching: false}))
+
+    this.oppijaHakuE.filter(acceptableQuery).map(true).merge(hakuTulosE.map(false)).throttle(200).onValue((searching) => this.setState({ searching }))
   }
 })
