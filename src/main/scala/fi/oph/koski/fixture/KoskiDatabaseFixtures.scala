a0223a101f3a842364aab5b79883b1db70f25459
Juha Paananen <juha.paananen@gmail.com>
2016-12-14 11:22:10 +0200

    delete henkilo table rows only for fixture persons

--- src/main/scala/fi/oph/koski/fixture/KoskiDatabaseFixtures.scala
+++ src/main/scala/fi/oph/koski/fixture/KoskiDatabaseFixtures.scala
@@ -30,7 +30,7 @@ class KoskiDatabaseFixtureCreator(database: KoskiDatabase, repository: OpiskeluO
     val deleteTiedonsiirrot = TiedonsiirtoWithAccessCheck.filter(t => t.tallentajaOrganisaatioOid === MockOrganisaatiot.stadinAmmattiopisto || t.tallentajaOrganisaatioOid === MockOrganisaatiot.helsinginKaupunki).delete
 
     runDbSync(DBIO.sequence(deleteOpiskeluOikeudet))
-    runDbSync(Tables.Henkilöt.delete)
+    runDbSync(Tables.Henkilöt.filter(_.oid inSetBind(MockOppijat.defaultOppijat.map(_.oid))).delete)
     runDbSync(deleteTiedonsiirrot)
 
     validatedOpiskeluoikeudet.foreach {
