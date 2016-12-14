d2088cc8cc0554498f90ec00c473eaaaec7c277f
Kalle Kainu <kalle.kainu@reaktor.fi>
2016-12-14 09:46:03 +0200

    Allow only one open dropdown at a time

--- web/app/Dropdown.jsx
+++ web/app/Dropdown.jsx
@@ -4,8 +4,8 @@ export default React.createClass({
   render() {
     const { options, open, selected } = this.state
     return (
-      <div className="dropdown">
-        <div className={selected ? 'select' : 'select no-selection'} onClick={this.openDropdown}>{selected ? selected.value : 'valitse'}</div>
+      <div className="dropdown" onFocus={e => this.handleFocus(e)} onBlur={e => this.handleBlur(e)} tabIndex="0" ref={el => this.dropdown = el}>
+        <div className={selected ? 'select' : 'select no-selection'} onClick={this.openDropdown} >{selected ? selected.value : 'valitse'}</div>
         { open ?
           <ul className="options">
             {
@@ -23,6 +23,7 @@ export default React.createClass({
   selectOption(e, option) {
     const selected = option.key ? option : undefined
     this.setState({selected: selected, open: false}, () => this.props.onSelectionChanged(selected))
+    this.dropdown.blur()
     e.stopPropagation()
   },
   openDropdown(e) {
@@ -30,7 +31,13 @@ export default React.createClass({
     e.stopPropagation()
   },
   closeDropdown() {
-    this.setState({open: false})
+    !this.state.active && this.setState({open: false})
+  },
+  handleFocus(e) {
+    this.setState({active:true})
+  },
+  handleBlur(e) {
+    this.setState({open: false, active: false})
   },
   componentDidMount() {
     this.props.optionsP.onValue(options => this.setState({options, selected: options.find(o => o.key == this.props.selected)}))
