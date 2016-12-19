d0c3cd1383b8f1c68971b3cb8d42b26c6674e441
Kalle Kainu <kalle.kainu@reaktor.fi>
2016-12-19 14:47:07 +0200

    start adding date range selection

--- web/app/DatePicker.jsx
+++ web/app/DatePicker.jsx
@@ -2,47 +2,83 @@ import DayPicker, {DateUtils} from 'react-day-picker'
 import {parseFinnishDate, formatFinnishDate} from './date.js'
 import React from 'react'
 
-const weekdaysLong = {
-  fi: ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'],
-  en: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-}
-const weekdaysShort = {
-  fi: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
-  en: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
-}
-const months = {
-  fi: ['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu'],
-  en: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-}
-const firstDayOfWeek = {
-  fi: 1,
-  en: 0
-}
-
-const localeUtils = {
-  formatDay: (d, locale = 'en') => `${weekdaysLong[locale][d.getDay()]}, ${d.getDate()} ${months[locale][d.getMonth()]} ${d.getFullYear()}`,
-  formatWeekdayShort: (index, locale = 'en') => weekdaysShort[locale][index],
-  formatWeekdayLong: (index, locale = 'en') => weekdaysLong[locale][index],
-  getFirstDayOfWeek: locale => firstDayOfWeek[locale],
-  getMonths: locale => months[locale],
-  formatMonthTitle: (d, locale) => `${months[locale][d.getMonth()]} ${d.getFullYear()}`
-}
-
 export default React.createClass({
   render() {
+    const {from, to, displayedStartMonth, displayedEndMonth} = this.state
     return (
-      <div onMouseDown={ this.handleContainerMouseDown }>
-        <input type="text" ref={ (el) => { this.input = el } } placeholder="kaikki" value={ this.state.value } onChange={ this.handleInputChange } onFocus={ this.handleInputFocus } onBlur={ this.handleInputBlur }/>
-        { this.state.showOverlay &&
-        <div className="DayPicker-CalendarContainer">
-          <div className="DayPicker-Overlay">
-            <DayPicker ref={ (el) => { this.daypicker = el } } onDayClick={ this.handleDayClick } selectedDays={ day => DateUtils.isSameDay(this.state.selectedDay, day) } locale="fi" localeUtils={localeUtils}/>
+      <div className="calendar">
+        <div onClick={e => this.openCalendar(e)}>{ from && to ? (formatFinnishDate(from) + '-' + formatFinnishDate(to)) : 'kaikki'}</div>
+        { this.state.open &&
+          <div className="DayPicker-CalendarContainer">
+              <DayPicker
+                onDayClick={ this.handleDayClick }
+                selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                locale="fi"
+                localeUtils={localeUtils}
+                initialMonth={displayedStartMonth}
+                fixedWeeks
+                onMonthChange={ this.handleStartMonthChange}
+                toMonth={ DateUtils.addMonths(displayedEndMonth, -1) }
+              />
+              <DayPicker
+                onDayClick={ this.handleDayClick }
+                selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                locale="fi"
+                localeUtils={localeUtils}
+                fromMonth={ DateUtils.addMonths(displayedStartMonth, 1)}
+                onMonthChange={ this.handleEndMonthChange}
+                fixedWeeks
+              />
           </div>
-        </div>
         }
       </div>
     )
   },
+  handleStartMonthChange(day) {
+    this.setState({
+      displayedStartMonth: day
+    })
+  },
+  handleEndMonthChange(day) {
+    this.setState({
+      displayedEndMonth: day
+    })
+  },
+  handleDayClick(e, day) {
+    this.setState(DateUtils.addDayToRange(day, this.state))
+  },
+  getInitialState() {
+    return {
+      displayedStartMonth: new Date(new Date().getFullYear(), 0, 1),
+      displayedEndMonth: new Date(),
+      open: false
+    }
+  },
+  openCalendar(e) {
+    this.setState({open: !this.state.open})
+  },
+  closeCalendar() {
+    console.log('close calendar')
+    this.setState({open: false})
+  },
+  componentDidMount() {
+    window.addEventListener('click', this.handleClickOutside, false)
+  },
+  componentWillUnmount() {
+    window.removeEventListener('click', this.handleClickOutside, false)
+  },
+  handleClickOutside(e) {
+    console.log('clicketi outside', !e.target.closest('.calendar'))
+    !e.target.closest('.calendar') && this.closeCalendar()
+  }
+  /*,
+  handleFocus(e) {
+    console.log("focus")
+  },
+  handleBlur(e) {
+    console.log("blur", e)
+  }*/
+  /*,
   handleContainerMouseDown() {
     this.clickedInside = true
     this.clickTimeout = setTimeout(() => {
@@ -72,25 +108,31 @@ export default React.createClass({
     if (showOverlay) {
       this.input.focus()
     }
-  },
-  handleDayClick(e, day) {
-    const value = formatFinnishDate(day)
-    this.setState({
-      value: value,
-      selectedDay: day,
-      showOverlay: false
-    }, () => this.props.onSelectionChanged(day))
-    this.input.blur()
-  },
-  getInitialState() {
-    const initialValue = this.props.selectedDay
-    return {
-      showOverlay: false,
-      value: initialValue || '',
-      selectedDay: initialValue && parseFinnishDate(initialValue) || null
-    }
-  },
-  componentWillUnmount() {
-    clearTimeout(this.clickTimeout)
-  }
+  }*/
 })
+
+const weekdaysLong = {
+  fi: ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'],
+  en: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+}
+const weekdaysShort = {
+  fi: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
+  en: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+}
+const months = {
+  fi: ['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu'],
+  en: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+}
+const firstDayOfWeek = {
+  fi: 1,
+  en: 0
+}
+
+const localeUtils = {
+  formatDay: (d, locale = 'en') => `${weekdaysLong[locale][d.getDay()]}, ${d.getDate()} ${months[locale][d.getMonth()]} ${d.getFullYear()}`,
+  formatWeekdayShort: (index, locale = 'en') => weekdaysShort[locale][index],
+  formatWeekdayLong: (index, locale = 'en') => weekdaysLong[locale][index],
+  getFirstDayOfWeek: locale => firstDayOfWeek[locale],
+  getMonths: locale => months[locale],
+  formatMonthTitle: (d, locale) => `${months[locale][d.getMonth()]} ${d.getFullYear()}`
+}
