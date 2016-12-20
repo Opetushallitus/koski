66d55530159545ac45577b46995fb03eff5129f7
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2016-12-20 14:54:03 +0200

    Välimuistin tyhjennys ilman salasanaa

--- src/main/scala/fi/oph/koski/cache/CacheServlet.scala
+++ src/main/scala/fi/oph/koski/cache/CacheServlet.scala
@@ -1,15 +1,12 @@
 package fi.oph.koski.cache
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.koskiuser.RequiresAuthentication
+import fi.oph.koski.koskiuser.Unauthenticated
 import fi.oph.koski.log.Logging
 import fi.oph.koski.servlet.{ApiServlet, NoCache}
 
-class CacheServlet(val application: KoskiApplication) extends ApiServlet with RequiresAuthentication with Logging with NoCache {
+class CacheServlet(val application: KoskiApplication) extends ApiServlet with Unauthenticated with Logging with NoCache {
   get("/invalidate") {
-    if (!koskiSession.isMaintenance) {
-      halt(403)
-    }
     application.cacheManager.invalidateAllCaches
     "Caches invalidated"
   }
