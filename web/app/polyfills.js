3c2bb9ffb71a3f082345090c9f3a731b90c0d01e
Kalle Kainu <kalle.kainu@reaktor.fi>
2016-12-20 10:33:16 +0200

    Import closest.js polyfill

--- web/app/polyfills.js
+++ web/app/polyfills.js
@@ -2,6 +2,7 @@ import './polyfill-bind.js'
 import 'whatwg-fetch'
 import 'es6-promise'
 import './window-error-handler.js'
+import './polyfills/closest.js'
 if (!String.prototype.startsWith) {
   String.prototype.startsWith = function(searchString, position) {
     position = position || 0
