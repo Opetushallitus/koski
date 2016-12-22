fdde7f2ccf302c021a73af9bbdaaeb79ab50f6a9
Juha Paananen <juha.paananen@gmail.com>
2016-12-22 15:48:40 +0200

    fix selection with c key

--- web/app/Validointi.jsx
+++ web/app/Validointi.jsx
@@ -6,34 +6,36 @@ import Bacon from 'baconjs'
 const ValidointiTaulukko = React.createClass({
   render() {
     let { validationStatus } = this.props
-    let { expandedRows } = this.state
+    let { expandedRows, message } = this.state
 
     return (
-      <table>
-        <thead>
-        <tr><th className="virhetyyppi">Virhetyyppi</th><th className="virheteksti">Virheteksti</th><th className="lukumäärä">Lukumäärä</th></tr>
-        </thead>
-        <tbody>
-        { validationStatus.map(({errors, oids, key}) => {
-          let expanded = expandedRows.indexOf(key) >= 0
-          return (<tr key={key}>
-            <td className="virhetyyppi">{
-              errors.length
-                ? errors.map((error, i) => <div key={i}>{error.key}</div>)
-                : 'Virheetön'
-            }</td>
-            <td className="virheteksti">{errors.map((error, i) => {
-              let message = typeof error.message == 'string'
-                ? <div>{error.message}</div>
-                : (expanded ? <pre className="json"><code>{JSON.stringify(error.message, null, 2)}</code></pre> : <a onClick={() => this.setState({ expandedRows: expandedRows.concat(key) })}>Näytä JSON</a>)
-              return <span key={i}>{message}</span>
-            })}</td>
-            <td className="lukumäärä">{oids.length}</td>
-          </tr>)
-        })}
-        </tbody>
-      </table>
-    )
+      <div>
+        <div id="message">{message}</div>
+        <table>
+          <thead>
+          <tr><th className="virhetyyppi">Virhetyyppi</th><th className="virheteksti">Virheteksti</th><th className="lukumäärä">Lukumäärä</th></tr>
+          </thead>
+          <tbody>
+          { validationStatus.map(({errors, oids, key}) => {
+            let expanded = expandedRows.indexOf(key) >= 0
+            return (<tr key={key}>
+              <td className="virhetyyppi">{
+                errors.length
+                  ? errors.map((error, i) => <div key={i}>{error.key}</div>)
+                  : 'Virheetön'
+              }</td>
+              <td className="virheteksti">{errors.map((error, i) => {
+                let errorMessage = typeof error.message == 'string'
+                  ? <div>{error.message}</div>
+                  : (expanded ? <pre className="json"><code>{JSON.stringify(error.message, null, 2)}</code></pre> : <a onClick={() => this.setState({ expandedRows: expandedRows.concat(key) })}>Näytä JSON</a>)
+                return <span key={i}>{errorMessage}</span>
+              })}</td>
+              <td className="lukumäärä">{oids.length}</td>
+            </tr>)
+          })}
+          </tbody>
+        </table>
+      </div>)
   },
   componentDidMount() {
     document.addEventListener('keyup', this.showSelection)
@@ -45,21 +47,26 @@ const ValidointiTaulukko = React.createClass({
     return { expandedRows: []}
   },
   showSelection(e) {
-    if (e.keyCode != 67) return
-    let { validationStatus } = this.props
-    if (!window.getSelection().focusNode || ! window.getSelection().anchorNode) return
-    let elementIndex = (el) => Array.prototype.indexOf.call(el.parentElement.children, el)
-    var startIndex = elementIndex(window.getSelection().focusNode.parentElement.closest('tr'))
-    var endIndex = elementIndex(window.getSelection().anchorNode.parentElement.closest('tr'))
-    if (startIndex < 0 || endIndex < 0) return
-    if (endIndex < startIndex) {
-      var t = endIndex
-      endIndex = startIndex
-      startIndex = t
+    if (e.keyCode == 27) { // esc
+      this.setState({ message: null })
+    } else if (e.keyCode == 67) { // C
+      let {validationStatus} = this.props
+      if (!window.getSelection().focusNode || !window.getSelection().anchorNode) return
+      let elementIndex = (el) => Array.prototype.indexOf.call(el.parentElement.children, el)
+      var startIndex = elementIndex(window.getSelection().focusNode.parentElement.closest('tr'))
+      var endIndex = elementIndex(window.getSelection().anchorNode.parentElement.closest('tr'))
+      if (startIndex < 0 || endIndex < 0) return
+      if (endIndex < startIndex) {
+        var t = endIndex
+        endIndex = startIndex
+        startIndex = t
+      }
+      let selectedRows = validationStatus.slice(startIndex, endIndex + 1)
+      let selectedIds = selectedRows.flatMap((row) => row.ids)
+      var messageElem = document.getElementById('message')
+      messageElem.textContent='(' + selectedIds.map((id) => '\'' + id + '\'').join(', ') + ')'
+      window.getSelection().selectAllChildren(messageElem)
     }
-    let selectedRows = validationStatus.slice(startIndex, endIndex+1)
-    let selectedIds = selectedRows.flatMap((row) => row.ids)
-    alert('(' + selectedIds.map((id) => '\'' + id + '\'').join(', ') + ')')
   }
 })
 
