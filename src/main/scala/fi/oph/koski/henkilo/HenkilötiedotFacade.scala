e60b61bb42fe1f4b43d817b18582f0578cb89a11
Juha Paananen <juha.paananen@gmail.com>
2016-12-29 11:01:02 +0200

    add todo

--- src/main/scala/fi/oph/koski/henkilo/HenkilötiedotFacade.scala
+++ src/main/scala/fi/oph/koski/henkilo/HenkilötiedotFacade.scala
@@ -9,6 +9,7 @@ import fi.oph.koski.schema.HenkilötiedotJaOid
 
 case class HenkilötiedotFacade(henkilöRepository: HenkilöRepository, OpiskeluoikeusRepository: OpiskeluoikeusRepository) {
   def findHenkilötiedot(queryString: String)(implicit user: KoskiSession): Seq[HenkilötiedotJaOid] = {
+    // TODO: ei löydä nimihaulla henkilöitä, joilla on opiskeluoikeuksia vain Koskessa (vs. Virta, YTR)
     val oppijat: List[HenkilötiedotJaOid] = henkilöRepository.findOppijat(queryString)
     AuditLog.log(AuditLogMessage(OPPIJA_HAKU, user, Map(hakuEhto -> queryString)))
     val filtered = OpiskeluoikeusRepository.filterOppijat(oppijat)
