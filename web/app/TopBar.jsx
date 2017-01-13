8d8ad5a1266c5e6b7e961ea6f279ba00ca83bc6f
Juha Paananen <juha.paananen@gmail.com>
2017-01-13 14:14:03 +0200

    don't reload full page

--- web/app/TopBar.jsx
+++ web/app/TopBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {UserInfo} from './UserInfo.jsx'
+import Link from './Link.jsx'
 
 export const TopBar = ({user, saved, title}) => (
   <header id='topbar' className={saved ? 'saved' : ''}>
     <div id='logo'>Opintopolku.fi</div>
-    <h1><a href="/koski">Koski</a>{title ? ' - ' + title : ''}</h1>
+    <h1><Link href="/koski/">Koski</Link>{title ? ' - ' + title : ''}</h1>
     <span className="save-info">Kaikki muutokset tallennettu.</span>
     <UserInfo user={user} />
   </header>
