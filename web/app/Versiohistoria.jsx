ec1aa40da8b44aa392e10261c95c398f81dfc91b
Juha Paananen <juha.paananen@gmail.com>
2017-01-16 09:39:55 +0200

    cleanup

--- web/app/Versiohistoria.jsx
+++ web/app/Versiohistoria.jsx
@@ -7,7 +7,7 @@ import Link from './Link.jsx'
 export default React.createClass({
   render() {
     let { opiskeluOikeusId, oppijaOid } = this.props
-    let { showHistory, history } = this.state || { showHistory: false }
+    let { showHistory, history } = this.state || { showHistory: false, history: [] }
     let toggle = () => {
       this.setState({showHistory: !showHistory})
       if (!this.historyP) {
@@ -15,13 +15,12 @@ export default React.createClass({
         this.historyP.onValue(h => this.setState({history: h}))
       }
     }
-    let versions = (this.state || {}).history || []
-    let selectedVersion = currentLocation().params.versionumero || versions.length
+    let selectedVersion = currentLocation().params.versionumero || history.length
     return (<div className="versiohistoria">
       <a className="versiohistoria" onClick={toggle}>versiohistoria</a>
       {
         showHistory && (<table> {
-          (versions).map((version) =>
+          history.map((version) =>
             <tr key={version.versionumero} className={version.versionumero == selectedVersion ? 'selected' : ''}>
               <td className="versionumero">{version.versionumero}</td>
               <td className="aikaleima"><Link href={`/koski/oppija/${oppijaOid}?opiskeluoikeus=${opiskeluOikeusId}&versionumero=${version.versionumero}`}>{ISO2FinnishDateTime(version.aikaleima)}</Link></td>
