8f5d617e8b46b60c2fca07284e15378cbb2ee083
Juha Paananen <juha.paananen@gmail.com>
2017-01-16 09:50:51 +0200

    versiohistorialistan tuunausta

--- web/app/Versiohistoria.jsx
+++ web/app/Versiohistoria.jsx
@@ -7,28 +7,39 @@ import Link from './Link.jsx'
 export default React.createClass({
   render() {
     let { opiskeluOikeusId, oppijaOid } = this.props
-    let { showHistory, history } = this.state || { showHistory: false, history: [] }
+    let { showHistory, history } = this.state
     let toggle = () => {
       this.setState({showHistory: !showHistory})
-      if (!this.historyP) {
-        this.historyP = Http.cachedGet(`/koski/api/opiskeluoikeus/historia/${opiskeluOikeusId}`).doError(showInternalError)
-        this.historyP.onValue(h => this.setState({history: h}))
-      }
+      this.fetchHistory()
     }
-    let selectedVersion = currentLocation().params.versionumero || history.length
+    let selectedVersion = this.versionumero() || history.length
     return (<div className="versiohistoria">
       <a className="versiohistoria" onClick={toggle}>versiohistoria</a>
       {
-        showHistory && (<table> {
+        showHistory && (<table><tbody>{
           history.map((version) =>
             <tr key={version.versionumero} className={version.versionumero == selectedVersion ? 'selected' : ''}>
               <td className="versionumero">{version.versionumero}</td>
               <td className="aikaleima"><Link href={`/koski/oppija/${oppijaOid}?opiskeluoikeus=${opiskeluOikeusId}&versionumero=${version.versionumero}`}>{ISO2FinnishDateTime(version.aikaleima)}</Link></td>
             </tr>
           )
-        }
-        </table>)
+        }</tbody></table>)
       }
     </div>)
+  },
+  getInitialState() {
+    return { showHistory: !!this.versionumero(), history: [] }
+  },
+  componentWillMount() {
+    this.fetchHistory()
+  },
+  fetchHistory() {
+    if (this.state.showHistory && !this.historyP) {
+      this.historyP = Http.cachedGet(`/koski/api/opiskeluoikeus/historia/${this.props.opiskeluOikeusId}`).doError(showInternalError)
+      this.historyP.onValue(h => this.setState({history: h}))
+    }
+  },
+  versionumero() {
+    return currentLocation().params.versionumero
   }
 })
