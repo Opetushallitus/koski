74223a79f45973c8ec9261d08db018c223be6725
Juha Paananen <juha.paananen@gmail.com>
2017-01-17 16:08:25 +0200

    try with more http client threads

--- src/main/scala/fi/oph/koski/util/Pools.scala
+++ src/main/scala/fi/oph/koski/util/Pools.scala
@@ -2,8 +2,7 @@ package fi.oph.koski.util
 
 import java.util.concurrent._
 
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
-import scala.math._
+import scala.concurrent.ExecutionContext
 
 /**
  *  We've tried to bundle all global threadpool/executor/executioncontext settings here.
@@ -12,8 +11,7 @@ object Pools {
   // Number of threads to use for Executors.global (scala.concurrent, our own GlobalExecution context, parallel collections etc)
   val jettyThreads = 50
   val globalExecutionContextThreads = jettyThreads
-  val forkJoinThreads = 10
-  val httpThreads = max(4, (Runtime.getRuntime.availableProcessors * 1.5).ceil.toInt)
+  val httpThreads = jettyThreads
   val httpPool = NamedThreadPool("http4s-blaze-client", httpThreads)
   val dbThreads = 20
   val globalExecutor = ExecutionContext.fromExecutor(new ThreadPoolExecutor(Pools.globalExecutionContextThreads, Pools.globalExecutionContextThreads, 60, TimeUnit.SECONDS, new ArrayBlockingQueue[Runnable](1000)))
