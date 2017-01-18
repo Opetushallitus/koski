8726068fe17a416a415228f2e8a2c7d9845fc76b
Juha Paananen <juha.paananen@gmail.com>
2017-01-18 11:38:48 +0200

    just one Http instance

--- src/test/scala/fi/oph/koski/perftest/RandomHetu.scala
+++ src/test/scala/fi/oph/koski/perftest/RandomHetu.scala
@@ -5,11 +5,11 @@ import fi.oph.koski.http.Http._
 
 object RandomHetu {
   def nextHetu = hetut.synchronized { hetut.next }
-
+  private lazy val http: Http = Http("http://www.telepartikkeli.net/tunnusgeneraattori/api")
   private lazy val hetut = {
     Iterator.continually({
       println("Haetaan hetuja...")
-      runTask(Http("http://www.telepartikkeli.net/tunnusgeneraattori/api").get(uri"/generoi/hetu/1000")(Http.parseJson[List[String]])).iterator
+      runTask(http.get(uri"/generoi/hetu/1000")(Http.parseJson[List[String]])).iterator
     }).flatten
   }
 }
