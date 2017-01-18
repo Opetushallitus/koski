1fb91ed553cdf548b0032724e3312e973cf409e8
Juha Paananen <juha.paananen@gmail.com>
2017-01-18 11:37:30 +0200

    check oid availability before spinning up threads

--- src/test/scala/fi/oph/koski/perftest/RandomOpiskeluoikeusGetter.scala
+++ src/test/scala/fi/oph/koski/perftest/RandomOpiskeluoikeusGetter.scala
@@ -4,6 +4,7 @@ import fi.oph.koski.log.Logging
 
 object RandomOpiskeluoikeusGetter extends KoskiPerfTester with Logging with App {
   val oids = RandomOppijaOid(Math.min(roundCount, 500000))
+  oids.nextOid // To check before start
   def operation(x: Int) = List(Operation(uri = s"api/oppija/${oids.nextOid}"))
 
   executeTest
