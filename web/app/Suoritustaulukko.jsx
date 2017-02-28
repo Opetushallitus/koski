cce0a1be9e090e77a8119708345edeece51d0016
Juha Paananen <juha.paananen@gmail.com>
2017-02-28 13:45:50 +0200

    Fix filtering

--- web/app/Suoritustaulukko.jsx
+++ web/app/Suoritustaulukko.jsx
@@ -29,7 +29,7 @@ const SuoritusEditor = React.createClass({
     let {model, showPakollisuus} = this.props
     let context = model.context
     let {expanded} = this.state
-    let propertyFilter = p => !['koulutusmoduuli', 'arviointi', 'tila'].includes(p.key) && GenericEditor.shouldShowProperty(context.edit)
+    let propertyFilter = p => !(['koulutusmoduuli', 'arviointi', 'tila'].includes(p.key)) && GenericEditor.shouldShowProperty(context.edit)(p)
     let properties = model.value.properties.filter(propertyFilter)
     let hasProperties = properties.length > 0
     let toggleExpand = () => { if (hasProperties) this.setState({expanded : !expanded}) }
