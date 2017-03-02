5fb3def62411d878c753de05c9f976bed7b81ad2
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-03-02 14:34:25 +0200

    Perusopetuksen laajuuden editointi

--- web/app/GenericEditor.jsx
+++ web/app/GenericEditor.jsx
@@ -284,12 +284,17 @@ StringEditor.canShowInline = () => true
 
 export const NumberEditor = React.createClass({
   render() {
-    let { model } = this.props
+    let {model} = this.props
+    let onChange = (event) => model.context.changeBus.push([model.context, {data: parseFloat(event.target.value)}])
+
     let data = modelData(model)
     let value = data
       ? Math.round(data * 100) / 100
       : data
-    return <span className="inline number">{value}</span>
+
+    return model.context.edit
+      ? <input type="text" defaultValue={modelData(model)} onChange={ onChange } className="inline number"></input>
+      : <span className="inline number">{value}</span>
   }
 })
 
