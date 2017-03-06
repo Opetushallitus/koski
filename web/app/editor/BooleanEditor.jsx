dc1fa870bec79732a121b9efc55ad0b5f7baf86b
Juha Paananen <juha.paananen@gmail.com>
2017-03-06 10:45:15 +0200

    extract BooleanEditor.jsx

---
+++ web/app/editor/BooleanEditor.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {modelData, modelTitle} from './EditorModel.js'
+
+export const BooleanEditor = React.createClass({
+  render() {
+    let {model} = this.props
+    let onChange = event => {
+      model.context.changeBus.push([model.context, {data: event.target.checked}])
+    }
+
+    return model.context.edit
+      ? <input type="checkbox" defaultChecked={modelData(model)} onChange={ onChange }></input>
+      : <span className="inline string">{modelTitle(model)}</span>
+  }
+})
+BooleanEditor.canShowInline = () => true
--- web/app/editor/CommonEditors.jsx
+++ web/app/editor/CommonEditors.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import {modelData, modelTitle} from './EditorModel.js'
 import {ObjectEditor} from './ObjectEditor.jsx'
 import {ArrayEditor} from './ArrayEditor.jsx'
 import {EnumEditor} from './EnumEditor.jsx'
@@ -12,20 +10,7 @@ import {VahvistusEditor} from './VahvistusEditor.jsx'
 import {KoulutusmoduuliEditor} from './KoulutusmoduuliEditor.jsx'
 import {PäivämääräväliEditor} from './PaivamaaravaliEditor.jsx'
 import {JaksoEditor} from './JaksoEditor.jsx'
-
-export const BooleanEditor = React.createClass({
-  render() {
-    let {model} = this.props
-    let onChange = event => {
-      model.context.changeBus.push([model.context, {data: event.target.checked}])
-    }
-
-    return model.context.edit
-      ? <input type="checkbox" defaultChecked={modelData(model)} onChange={ onChange }></input>
-      : <span className="inline string">{modelTitle(model)}</span>
-  }
-})
-BooleanEditor.canShowInline = () => true
+import {BooleanEditor} from './BooleanEditor.jsx'
 
 export const editorMapping = {
   'object': ObjectEditor,
