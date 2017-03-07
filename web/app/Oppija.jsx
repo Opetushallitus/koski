f15c79999b51bffce797c4547c0d8692d970dce7
Juha Paananen <juha.paananen@gmail.com>
2017-03-07 15:43:54 +0200

    Make the update mechanism more reliable with asyncScan

--- web/app/Oppija.jsx
+++ web/app/Oppija.jsx
@@ -21,44 +21,41 @@ export const oppijaContentP = (oppijaOid) => {
 
   const oppijaEditorUri = `/koski/api/editor/${oppijaOid}${queryString}`
 
-  const updateResultE = Bacon.Bus()
+  const loadOppijaE = Bacon.once().map(() => () => Http.cachedGet(oppijaEditorUri))
 
-  const saveE = changeBus // might consider saving only when done editing
-  const savingP = saveE.awaiting(updateResultE)
+  const refreshFromServerE = doneEditingBus.map(() => () => Http.cachedGet(oppijaEditorUri, { force: true }))
 
-  const loadOppijaE = Http.cachedGet(oppijaEditorUri).toEventStream().concat(doneEditingBus.holdWhen(savingP).flatMap(() => Http.cachedGet(oppijaEditorUri, { force: true })))
+  const savedOppijaE = changeBus.map((contextValuePairs) => ((oppijaBeforeChange) => {
+    let locallyModifiedOppija = R.splitEvery(2, contextValuePairs).reduce((acc, [context, value]) => modelSet(acc, context.path, value), oppijaBeforeChange)
 
-  const oppijaP = Bacon.update({ loading: true },
-    loadOppijaE, (previous, oppija) => oppija,
-    updateResultE.map('.opiskeluoikeudet').flatMap(Bacon.fromArray), (currentOppija, {id, versionumero}) => {
+    let firstPath = contextValuePairs[0]
+    let opiskeluoikeusPath = firstPath.split('.').slice(0, 6)
+    var oppijaData = locallyModifiedOppija.value.data
+    let opiskeluoikeus = objectLookup(oppijaData, opiskeluoikeusPath.join('.'))
+    let oppijaUpdate = {
+      henkilö: {oid: oppijaData.henkilö.oid},
+      opiskeluoikeudet: [opiskeluoikeus]
+    }
+    return Http.put('/koski/api/oppija', oppijaUpdate).map('.opiskeluoikeudet').flatMap(Bacon.fromArray).map(({id, versionumero}) => {
+      // This is done to update versionumero based on server response
       let correctId = R.whereEq({id})
       let containsOpiskeluoikeus = (oppilaitoksenOpiskeluoikeudet) => oppilaitoksenOpiskeluoikeudet.opiskeluoikeudet.find(correctId)
       let containsOppilaitos = (tyypinOpiskeluoikeudet) => tyypinOpiskeluoikeudet.opiskeluoikeudet.find(containsOpiskeluoikeus)
-
       let lens = L.compose('value', 'data', 'opiskeluoikeudet', L.find(containsOppilaitos), 'opiskeluoikeudet', L.find(containsOpiskeluoikeus), 'opiskeluoikeudet', L.find(correctId), 'versionumero')
-      var modified = L.set(lens, versionumero, currentOppija)
-      return modified
-    },
-    changeBus.log('changebus'), (currentOppija, contextValuePairs) => {
-      return R.splitEvery(2, contextValuePairs).reduce((acc, [context, value]) => modelSet(acc, context.path, value), currentOppija)
-    }
-  )
-
-  updateResultE.plug(oppijaP
-    .sampledBy(saveE, (oppija, [context]) => ({oppija, context}))
-    .flatMapLatest(({oppija, context: {path}}) => {
-      let opiskeluoikeusPath = path.split('.').slice(0, 6)
-      var oppijaData = oppija.value.data
-      let opiskeluoikeus = objectLookup(oppijaData, opiskeluoikeusPath.join('.'))
-      let oppijaUpdate = {
-        henkilö: {oid: oppijaData.henkilö.oid},
-        opiskeluoikeudet: [opiskeluoikeus]
-      }
-      return Http.put('/koski/api/oppija', oppijaUpdate)
+      return L.set(lens, versionumero, locallyModifiedOppija)
     })
-  )
+  }))
 
-  saveBus.plug(updateResultE.map(true))
+  let allUpdatesE = Bacon.mergeAll(loadOppijaE, savedOppijaE, refreshFromServerE) // :: EventStream [Model -> EventStream[Model]]
+
+  let current = { loading: true}
+  let oppijaP = allUpdatesE.flatMapConcat(updateFunc => {
+    let updateE = updateFunc(current)
+    return updateE.doAction(newValue => current = newValue)
+  }).toProperty(current)
+  oppijaP.onValue()
+
+  saveBus.plug(savedOppijaE.map(true))
 
   return Bacon.combineWith(oppijaP, oppijaHakuElementP, (oppija, haku) => {
     return {
