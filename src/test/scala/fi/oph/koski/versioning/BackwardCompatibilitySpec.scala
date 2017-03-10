cbc8def9773493ab3a953f6522fa072340222151
Juha Paananen <juha.paananen@gmail.com>
2017-03-10 16:43:16 +0200

    In BackwardCompatibilitySpec, save new version if it differs from previous

--- src/test/scala/fi/oph/koski/versioning/BackwardCompatibilitySpec.scala
+++ src/test/scala/fi/oph/koski/versioning/BackwardCompatibilitySpec.scala
@@ -6,6 +6,8 @@ import fi.oph.koski.json.Json
 import fi.oph.koski.koskiuser.{AccessType, KoskiSession}
 import fi.oph.koski.schema.{KoskeenTallennettavaOpiskeluoikeus, Oppija}
 import org.scalatest.{FreeSpec, Matchers}
+import java.io.File
+import java.time.LocalDate
 
 /**
  * Tests that examples match saved JSON files. Run with -DupdateExamples=true to update saved JSON files from current examples.
@@ -16,24 +18,35 @@ class BackwardCompatibilitySpec extends FreeSpec with Matchers {
   implicit val accessType = AccessType.read
 
   "backward compatibility with stored JSON examples" - {
-    val updateAll = System.getProperty("updateExamples", "false").toBoolean
     Examples.examples.filter(example => example.data.opiskeluoikeudet.head.isInstanceOf[KoskeenTallennettavaOpiskeluoikeus]).foreach { example =>
       example.name in {
-        val filename = "src/test/resources/backwardcompatibility/" + example.name.replaceAll(" ", "").replaceAll("ä", "a").replaceAll("ö", "o") + ".json"
-        (updateAll, Json.readFileIfExists(filename)) match {
-          case (false, Some(json)) =>
-            println("Checking backward compatibility: " + filename)
-            val oppija: Oppija = Json.fromJValue[Oppija](json)
-            val afterRoundtrip = Json.toJValue(oppija)
-            Json.write(afterRoundtrip) should equal(Json.write(json))
-            validator.validateAsJson(oppija) match {
-              case Right(validated) => // Valid
-              case Left(err) => throw new IllegalStateException(err.toString)
+        val basename = example.name.replaceAll(" ", "").replaceAll("ä", "a").replaceAll("ö", "o")
+        val currentFilename = basename + "_" + LocalDate.now.toString + ".json"
+        val dirName: String = "src/test/resources/backwardcompatibility"
+        def fullName(fileName: String) = dirName + "/" + fileName
+        val existingFiles = new File(dirName).list().filter(fn => fn == basename + ".json" || fn.startsWith(basename + "_")).toList.sorted
+        existingFiles match {
+          case Nil =>
+            println("Creating " + currentFilename)
+            new File(fullName(currentFilename)).getParentFile().mkdirs()
+            Json.writeFile(fullName(currentFilename), example.data)
+          case files =>
+            files.foreach { filename =>
+              val json = Json.readFile(fullName(filename))
+              println("Checking backward compatibility: " + filename)
+              val oppija: Oppija = Json.fromJValue[Oppija](json)
+              val afterRoundtrip = Json.toJValue(oppija)
+              Json.write(afterRoundtrip) should equal(Json.write(json))
+              validator.validateAsJson(oppija) match {
+                case Right(validated) => // Valid
+                case Left(err) => throw new IllegalStateException(err.toString)
+              }
             }
-          case _ =>
-            println("Updating/creating " + filename)
-            new java.io.File(filename).getParentFile().mkdirs()
-            Json.writeFile(filename, example.data)
+            val latest = files.last
+            if (Json.toJValue(example.data) != Json.readFile(fullName(latest))) {
+              println(s"Example data differs for ${example.name} at ${latest}. Creating new version")
+              Json.writeFile(fullName(currentFilename), example.data)
+            }
         }
       }
     }
