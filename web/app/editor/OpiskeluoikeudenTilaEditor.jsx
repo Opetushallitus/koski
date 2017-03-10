287a1fc1fa271b5c31917bab8a48e86d0534d50f
Kalle Kainu <kalle.kainu@reaktor.fi>
2017-03-10 10:48:15 +0200

    Enable save only when all required fields are set

--- web/app/editor/OpiskeluoikeudenTilaEditor.jsx
+++ web/app/editor/OpiskeluoikeudenTilaEditor.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Bacon from 'baconjs'
 import R from 'ramda'
 import * as L from 'partial.lenses'
-import {childContext, contextualizeModel, modelData, modelItems, modelLookup} from './EditorModel.js'
+import {childContext, contextualizeModel, modelData, modelItems, modelLookup, addContext} from './EditorModel.js'
 import {ArrayEditor} from './ArrayEditor.jsx'
 import {OpiskeluoikeusjaksoEditor} from './OpiskeluoikeusjaksoEditor.jsx'
 import {EnumEditor} from './EnumEditor.jsx'
@@ -11,12 +11,14 @@ import {DateEditor} from './DateEditor.jsx'
 export const OpiskeluoikeudenTilaEditor = React.createClass({
   render() {
     let {model, opiskeluoikeusModel} = this.props
-    let {saveChangesBus, cancelBus, changeBus, newTilaModel, items = modelItems(model).slice(0).reverse()} = this.state
+    let {saveChangesBus, cancelBus, alkuPäiväBus, tilaBus, newStateModels, items = modelItems(model).slice(0).reverse()} = this.state
 
     let showAddDialog = () => {
-      let tilaModel = contextualizeModel(model.prototype, R.merge(childContext(model.context, items.length), {changeBus}))
-      changeBus.push([childContext(tilaModel.context, 'alku'), {data: modelData(tilaModel, 'alku')}])
-      this.setState({newTilaModel: tilaModel})
+      let opiskeluoikeusjaksoModel = contextualizeModel(model.prototype, childContext(model.context, items.length))
+      let alkuPäiväModel = addContext(modelLookup(opiskeluoikeusjaksoModel, 'alku'), {changeBus: alkuPäiväBus})
+      let tilaModel = addContext(modelLookup(opiskeluoikeusjaksoModel, 'tila'), {changeBus: tilaBus})
+      alkuPäiväBus.push([alkuPäiväModel.context, {data: modelData(alkuPäiväModel)}])
+      this.setState({newStateModels: {alkuPäiväModel, tilaModel}})
     }
 
     let add = () => {
@@ -35,7 +37,7 @@ export const OpiskeluoikeudenTilaEditor = React.createClass({
       model.context.changeBus.push([items[0].context, {data: undefined}])
       model.context.doneEditingBus.push()
       let newItems = L.set(L.index(0), undefined, items)
-      this.setState({newTilaModel: undefined, items: newItems})
+      this.setState({newStateModels: undefined, items: newItems})
     }
 
     let lastOpiskeluoikeudenTila = modelData(modelLookup(items[0], 'tila'))
@@ -58,20 +60,20 @@ export const OpiskeluoikeudenTilaEditor = React.createClass({
             }
           </ul>
           {
-            newTilaModel && (
+            newStateModels && (
               <div className="lisaa-opiskeluoikeusjakso-modal">
                 <div className="lisaa-opiskeluoikeusjakso">
                   <a className="close-modal" onClick={cancel}>&#10005;</a>
                   <h2>Opiskeluoikeuden tilan lisäys</h2>
                   <div className="property alku">
-                    <label>{newTilaModel.value.properties.find(p => p.key == 'alku').title}:</label>
-                    <DateEditor model={modelLookup(newTilaModel, 'alku')}/>
+                    <label>Päivämäärä:</label>
+                    <DateEditor model={newStateModels.alkuPäiväModel}/>
                   </div>
                   <div className="property tila">
-                    <label>{newTilaModel.value.properties.find(p => p.key == 'tila').title}:</label>
-                    <EnumEditor asRadiogroup="true" model={modelLookup(newTilaModel, 'tila')}/>
+                    <label>Tila:</label>
+                    <EnumEditor asRadiogroup="true" model={newStateModels.tilaModel}/>
                   </div>
-                  <a className="button" onClick={add}>Lisää</a>
+                  <button disabled={!this.state.valid} className="button" onClick={add}>Lisää</button>
                   <a onClick={cancel}>Peruuta</a>
                 </div>
               </div>
@@ -82,35 +84,43 @@ export const OpiskeluoikeudenTilaEditor = React.createClass({
     )
   },
   getInitialState() {
+    return {
+      saveChangesBus: Bacon.Bus(),
+      cancelBus: Bacon.Bus(),
+      alkuPäiväBus: Bacon.Bus(),
+      tilaBus: Bacon.Bus()
+    }
+  },
+  componentDidMount() {
     let {model, opiskeluoikeusModel} = this.props
-    let changeBus = Bacon.Bus()
-    let saveChangesBus = Bacon.Bus()
-    let cancelBus = Bacon.Bus()
+    let {alkuPäiväBus, tilaBus, cancelBus, saveChangesBus} = this.state
+
+    let muutoksetE = alkuPäiväBus.merge(tilaBus)
 
     let changesP = Bacon.update([],
-      changeBus, (xs, x) => xs.concat(x),
+      muutoksetE, (xs, x) => xs.concat(x),
       cancelBus, () => []
     )
 
-    saveChangesBus.merge(cancelBus).onValue(() => this.setState({newTilaModel: undefined}))
+    Bacon.mergeAll(
+      tilaBus.map(true),
+      saveChangesBus.map(false),
+      cancelBus.map(false)
+    ).onValue(valid => this.setState({valid: valid}))
+
+    saveChangesBus.merge(cancelBus).onValue(() => this.setState({newStateModels: undefined}))
 
     changesP.sampledBy(saveChangesBus).onValue((changes) => {
       let opiskeluoikeudenPaattymispaiva = this.opiskeluoikeudenPaattymispaiva(changes)
+
       if (opiskeluoikeudenPaattymispaiva) {
         let paattymispaivaModel = modelLookup(opiskeluoikeusModel, 'päättymispäivä')
         model.context.changeBus.push([paattymispaivaModel.context, {data: opiskeluoikeudenPaattymispaiva}])
       }
-
       model.context.changeBus.push(changes)
       model.context.doneEditingBus.push()
       cancelBus.push()
     })
-
-    return {
-      saveChangesBus: saveChangesBus,
-      cancelBus: cancelBus,
-      changeBus: changeBus
-    }
   },
   opiskeluoikeudenPaattymispaiva(changes) {
     let changePairs = R.splitEvery(2, changes)
