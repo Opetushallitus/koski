eeaf419201a029732fa0ecf8eaad0a8650d65cbf
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-03-10 16:26:08 +0200

    Lisää endsWith polyfill

--- web/app/polyfills.js
+++ web/app/polyfills.js
@@ -9,6 +9,17 @@ if (!String.prototype.startsWith) {
     return this.indexOf(searchString, position) === position
   }
 }
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+    var subjectString = this.toString()
+    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+      position = subjectString.length
+    }
+    position -= searchString.length
+    var lastIndex = subjectString.lastIndexOf(searchString, position)
+    return lastIndex !== -1 && lastIndex === position
+  }
+}
 function addArrayMethod(name, fn) {
   if (!Array.prototype[name]) {
     Object.defineProperty(Array.prototype, name, {
