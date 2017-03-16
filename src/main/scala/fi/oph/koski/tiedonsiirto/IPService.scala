c7d82b6edf1d280a73e9c020fa1254c9730768a7
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-03-16 10:03:04 +0200

    Korjaa duplikaattiavainbugi kun tallennetaan ip-osoite

--- src/main/scala/fi/oph/koski/tiedonsiirto/IPService.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/IPService.scala
@@ -1,14 +1,20 @@
 package fi.oph.koski.tiedonsiirto
 
+import java.sql.SQLException
+
 import fi.oph.koski.db.KoskiDatabase.DB
-import fi.oph.koski.db.{KoskiDatabaseMethods, OppilaitosIPOsoiteRow, Tables}
-import fi.oph.koski.koskiuser.KoskiSession
 import fi.oph.koski.db.PostgresDriverWithJsonSupport.api._
 import fi.oph.koski.db.Tables.OppilaitosIPOsoite
+import fi.oph.koski.db.{KoskiDatabaseMethods, OppilaitosIPOsoiteRow}
+import fi.oph.koski.log.Logging
 
-class IPService(val db: DB) extends KoskiDatabaseMethods {
-  def setIP(username: String, ip: String): Unit =
+class IPService(val db: DB) extends KoskiDatabaseMethods with Logging {
+  def setIP(username: String, ip: String): Unit = try {
     runDbSync(OppilaitosIPOsoite.insertOrUpdate(OppilaitosIPOsoiteRow(username, ip)))
+  } catch {
+    case e:SQLException if e.getSQLState == "23505" =>
+      logger.info("Inserting new ip address caused duplicate key exception, ignoring.")
+  }
 
   def getIP(username: String): String =
     runDbSync(OppilaitosIPOsoite.filter(_.username === username).map(_.ip).result.headOption).getOrElse("")
