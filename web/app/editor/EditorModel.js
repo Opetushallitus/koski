e9f794d0dc0e2281a700807fe221ca1ebcc44e88
Juha Paananen <juha.paananen@gmail.com>
2017-03-27 17:45:02 +0300

    refactor editorModel.js: modelLens is now complete

--- web/app/editor/EditorModel.js
+++ web/app/editor/EditorModel.js
@@ -1,70 +1,35 @@
 import R from 'ramda'
 import * as L from 'partial.lenses'
 
-const lookupRecursive = (lookupStep, model, [head, ...tail]) => {
-  let found = lookupStep(model, head)
-  if (tail.length && found) {
-    return lookupRecursive(lookupStep, found, tail)
-  }
-  return found
-}
-
-let lastL = L.lens(
-  (xs) => {
-    return (xs && xs.length && xs.last()) || undefined
-  },
-  (x, xs) => xs.slice(0, -1).concat([x])
-)
-
-let indexL = (index) => index == -1 ? lastL : L.index(index)
-
 // Find submodel with given path
 export const modelLookup = (mainModel, path) => {
-  if (!path) return mainModel
-  if (!mainModel) return
-  let lens = modelLens(path)
-  var subModel = prepareModel(mainModel, L.get(lens, mainModel), path)
-  return subModel
+  return L.get(modelLens(path), mainModel)
 }
 
-const prepareModel = (mainModel, subModel, path) => {
-  if (subModel && subModel.value && !subModel.value.data && mainModel.value && mainModel.value.data) {
-    // fill in data from main model (data is not transmitted for all subModels, to save bandwidth)
-    let data = objectLookup(mainModel.value.data, path)
-    if (subModel.type == 'array' && data) {
-      data.forEach((x, i) => subModel.value[i].value.data = x)
-    } else {
-      subModel.value.data = data
-    }
-  }
-  return subModel
-}
-
-const modelLens = (path) => {
+export const modelLens = (path) => {
   let pathLenses = toPath(path).map(key => {
     let index = parseInt(key)
     return L.compose('value', Number.isNaN(index)
       ? L.compose('properties', L.find(R.whereEq({key})), 'model')
       : indexL(index))
   })
-  return L.compose(...pathLenses)
+  let theLens = L.compose(...pathLenses)
+  return L.lens(
+    (mainModel) => {
+      let subModel = L.get(theLens, mainModel)
+      return prepareModel(mainModel, subModel, path)
+    },
+    (newValue, mainModel) => {
+      let updated = L.set(theLens, newValue, mainModel)
+      let newData = modelData(newValue)
+      let withUpdatedTopLevelData = L.set(dataLens(path), newData, updated)
+      return withUpdatedTopLevelData
+    }
+  )
 }
-
-const objectLens = (path) => {
-  let pathLenses = toPath(path).map(key => {
-    let index = parseInt(key)
-    return Number.isNaN(index)
-      ? L.prop(key)
-      : indexL(index)
-  })
-  return L.compose(...pathLenses)
-}
-
-const toPath = (path) => typeof path == 'string' ? path.split('.') : path
 
 export const objectLookup = (mainObj, path) => {
-  let lens = objectLens(path)
-  return L.get(lens, mainObj)
+  return L.get(objectLens(path), mainObj)
 }
 
 export const modelData = (mainModel, path) => {
@@ -85,20 +50,8 @@ export const modelEmpty = (model) => {
   return !model.value || valueEmpty(model.value) && itemsEmpty(modelItems(model.items))
 }
 
-const valueData = (value) => {
-  if (!value) return
-  if (value.data !== undefined) return value.data
-  if (value instanceof Array) return value.map(modelData)
-}
-
 export const modelSet = (mainModel, newModel, path) => {
-  var withUpdatedModel = L.set(modelLens(path), newModel, mainModel)
-
-  let dataLens = L.compose('value', 'data', objectLens(path))
-  var data = valueData(newModel && newModel.value)
-
-  var withUpdatedTopLevelData = L.set(dataLens, data, withUpdatedModel)
-  return withUpdatedTopLevelData
+  return L.set(modelLens(path), newModel, mainModel)
 }
 
 export const modelSetData = (model, data) => {
@@ -109,18 +62,9 @@ export const modelSetValue = (model, value) => {
   return R.merge(model, { value })
 }
 
-
 export const modelItems = (mainModel, path) => {
   let model = modelLookup(mainModel, path)
   return (model && model.type == 'array' && model.value) || []
-}
-
-const valueEmpty = (value) => {
-  return !value
-}
-
-const itemsEmpty = (items) => {
-  return !items || !items.find(item => !valueEmpty(item))
 }
 
 // Add the given context to the model and all submodels. Submodels get a copy where their full path is included,
@@ -147,6 +91,23 @@ export const childContext = (context, ...pathElems) => {
   return R.merge(context, { path, root: false, arrayItems: null, parentContext: context })
 }
 
+// Add more context parameters to the current context of the model.
+export const addContext = (model, additionalContext) => contextualizeModel(model, R.merge(model.context, additionalContext))
+
+const valueData = (value) => {
+  if (!value) return
+  if (value.data !== undefined) return value.data
+  if (value instanceof Array) return value.map(modelData)
+}
+
+const valueEmpty = (value) => {
+  return !value
+}
+
+const itemsEmpty = (items) => {
+  return !items || !items.find(item => !valueEmpty(item))
+}
+
 const resolveModel = (model) => {
   if (model && model.type == 'prototype' && model.context.edit) {
     // Some models are delivered as prototype references and are replaced with the actual prototype found in the context
@@ -159,5 +120,52 @@ const resolveModel = (model) => {
   return model
 }
 
-// Add more context parameters to the current context of the model.
-export const addContext = (model, additionalContext) => contextualizeModel(model, R.merge(model.context, additionalContext))
+let lastL = L.lens(
+  (xs) => {
+    return (xs && xs.length && xs.last()) || undefined
+  },
+  (x, xs) => xs.slice(0, -1).concat([x])
+)
+
+let indexL = (index) => index == -1 ? lastL : L.index(index)
+
+const prepareModel = (mainModel, subModel, path) => {
+  if (subModel && subModel.value && !subModel.value.data && mainModel.value && mainModel.value.data) {
+    // fill in data from main model (data is not transmitted for all subModels, to save bandwidth)
+    let data = objectLookup(mainModel.value.data, path)
+    if (subModel.type == 'array' && data) {
+      data.forEach((x, i) => subModel.value[i].value.data = x)
+    } else {
+      subModel.value.data = data
+    }
+  }
+  return subModel
+}
+
+let dataLens = (path) => L.compose('value', 'data', objectLens(path))
+
+const toPath = (path) => {
+  if (path == undefined) {
+    return []
+  }
+  if (typeof path == 'number') {
+    path = '' + path
+  }
+  if (typeof path == 'string') {
+    return path.split('.')
+  }
+  if (path instanceof Array) {
+    return path
+  }
+  throw new Error('Not a path: ' + path)
+}
+
+const objectLens = (path) => {
+  let pathLenses = toPath(path).map(key => {
+    let index = parseInt(key)
+    return Number.isNaN(index)
+      ? L.prop(key)
+      : indexL(index)
+  })
+  return L.compose(...pathLenses)
+}
