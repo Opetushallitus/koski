8dd00eebe688d4f73933be25d6e0744d34369e74
Juha Paananen <juha.paananen@gmail.com>
2017-03-29 10:12:24 +0300

    Korjattu LUVA: lukion oppiaineen suoritus poistettu vaihtoehdoista

--- src/main/scala/fi/oph/koski/schema/Lukio.scala
+++ src/main/scala/fi/oph/koski/schema/Lukio.scala
@@ -139,7 +139,7 @@ case class LukionOppiaineenSuoritus(
   override val osasuoritukset: Option[List[LukionKurssinSuoritus]],
   @KoodistoKoodiarvo("lukionoppiaine")
   tyyppi: Koodistokoodiviite = Koodistokoodiviite(koodiarvo = "lukionoppiaine", koodistoUri = "suorituksentyyppi")
-) extends OppiaineenSuoritus with VahvistuksetonSuoritus with LukionOppimääränOsasuoritus with LukioonValmistavanKoulutuksenOsasuoritus
+) extends OppiaineenSuoritus with VahvistuksetonSuoritus with LukionOppimääränOsasuoritus
 
 case class LukionKurssinSuoritus(
   @Description("Lukion kurssin tunnistetiedot")
