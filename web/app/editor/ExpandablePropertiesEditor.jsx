4e5bd08d9eb94cc0cafa1eef6c7b4fea1062bdc7
Juha Paananen <juha.paananen@gmail.com>
2017-04-06 13:53:12 +0300

    use wrapOptional in ExpandablePropertiesEditor

--- web/app/editor/ExpandablePropertiesEditor.jsx
+++ web/app/editor/ExpandablePropertiesEditor.jsx
@@ -1,43 +1,28 @@
 import React from 'react'
-import {modelData, modelLookup, contextualizeModel, modelEmpty} from './EditorModel.js'
+import {modelData, modelLookup} from './EditorModel.js'
 import {PropertiesEditor} from './PropertiesEditor.jsx'
-
-// TODO: use wrapOptional for automatic creation/deletion
+import {wrapOptional} from './OptionalEditor.jsx'
 
 export const ExpandablePropertiesEditor = React.createClass({
   render() {
     let {model, propertyName} = this.props
     let {open} = this.state
+    let propertyModel = modelLookup(model, propertyName)
+    let wrappedModel = wrapOptional({model: propertyModel})
+    let edit = model.context.edit
 
-    let propertiesModel = modelLookup(model, propertyName)
-
-    return modelData(model, propertyName) || model.context.edit ?
+    return modelData(model, propertyName) || wrappedModel.context.edit ?
       <div className={'expandable-container ' + propertyName}>
         <a className={open ? 'open expandable' : 'expandable'} onClick={this.toggleOpen}>{model.value.properties.find(p => p.key === propertyName).title}</a>
-        { (open && propertiesModel.value) ?
+        { open || edit ?
           <div className="value">
-            <PropertiesEditor model={propertiesModel} />
+            <PropertiesEditor model={wrappedModel} />
           </div> : null
         }
       </div> : null
   },
   toggleOpen() {
-    let {model, propertyName} = this.props
-    let propertiesModel = modelLookup(model, propertyName)
-
-    if (model.context.edit && modelEmpty(propertiesModel)) {
-      let addingModel = contextualizeModel(propertiesModel.optionalPrototype, propertiesModel.context)
-      if (!modelData(addingModel)) {
-        throw new Error('Prototype value data missing')
-      }
-      model.context.changeBus.push([addingModel.context, addingModel])
-    }
     this.setState({open: !this.state.open})
-  },
-  componentWillReceiveProps(newProps) {
-    if (newProps.model.context.edit && !this.props.model.context.edit && !modelEmpty(modelLookup(newProps.model, newProps.propertyName))) {
-      this.setState({open: true})
-    }
   },
   getInitialState() {
     return {open: false}
