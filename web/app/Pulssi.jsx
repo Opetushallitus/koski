3b32bb8e307b9002b8201cd05203f7d3daa011d4
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-04-20 10:25:49 +0300

    Tilastot koulutusmuodoittain ja tyypeittäin

--- web/app/Pulssi.jsx
+++ web/app/Pulssi.jsx
@@ -3,14 +3,89 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Http from './http'
 import Bacon from 'baconjs'
+import './style/pulssi.less'
 
 const Pulssi = React.createClass({
   render() {
-    return (
-    <h1>Hello world</h1>
+    let {stats} = this.state
+    let valmiidenTutkintojenMäärä =  stats.määrätKoulutusmuodoittain.reduce((acc, koulutusmuoto) =>
+      acc + koulutusmuoto.määrätTiloittain.find(tila => tila.nimi === 'valmistunut').opiskeluoikeuksienMäärä, 0
     )
+
+    return (
+      <div>
+        <div className="top-row">
+          <section className="primary-metric opiskeluoikeudet-total">
+            <h3>Opiskeluoikeuksien määrä</h3>
+            <div className="metric-large">{stats.opiskeluoikeuksienMäärä}</div>
+          </section>
+          <section className="primary-metric kattavuus">
+            <h3>Kattavuus</h3>
+            <div className="metric-large">N/A</div>
+            1102 / 8582
+          </section>
+          <section className="primary-metric valmiit-tutkinnot">
+            <h3>Valmiiden tutkintojen määrä</h3>
+            <div className="metric-large">{valmiidenTutkintojenMäärä}</div>
+            {toPercent(valmiidenTutkintojenMäärä / stats.opiskeluoikeuksienMäärä)} %
+          </section>
+        </div>
+        <div className="bottom-block">
+          <section className="opiskeluoikeudet-total">
+            <ul>
+              {
+                stats.määrätKoulutusmuodoittain && stats.määrätKoulutusmuodoittain.map(stat =>
+                  <li>
+                    <span>{stat.nimi}</span><span className="metric-value">{stat.opiskeluoikeuksienMäärä}</span>
+                  </li>
+                )
+              }
+            </ul>
+          </section>
+          <section className="kattavuus">
+          </section>
+          <section className="valmiit-tutkinnot">
+            <ul>
+              {
+                stats.määrätKoulutusmuodoittain && stats.määrätKoulutusmuodoittain.map(koulutusmuoto =>
+                  <KoulutusmuotoTilasto koulutusmuoto={koulutusmuoto} />
+                )
+              }
+            </ul>
+          </section>
+        </div>
+      </div>
+    )
+  },
+  componentDidMount() {
+    Http.cachedGet('/koski/api/pulssi').onValue(stats => this.setState({stats}))
+  },
+  getInitialState() {
+    return {stats: { määrätKoulutusmuodoittain: [] } }
   }
 })
+
+const toPercent = x => Math.round(x * 100 * 10) / 10
+
+const KoulutusmuotoTilasto = ({koulutusmuoto}) => {
+  let opiskeluoikeusMääräValmiit = koulutusmuoto.määrätTiloittain.find(tila => tila.nimi === 'valmistunut').opiskeluoikeuksienMäärä
+  let opiskeluoikeusMääräKaikki = koulutusmuoto.määrätTiloittain.reduce((acc, n) => acc + n.opiskeluoikeuksienMäärä, 0)
+  let opiskeluoikeusMääräEiValmiit = opiskeluoikeusMääräKaikki - opiskeluoikeusMääräValmiit
+  let valmiitPercent = toPercent(opiskeluoikeusMääräValmiit / opiskeluoikeusMääräKaikki)
+  return (
+    <li>
+      <h3>{koulutusmuoto.nimi}</h3>
+      <div className="progress-bar">
+        <div style={{width: valmiitPercent + '%'}}></div>
+      </div>
+      <div className="metric-tiny">
+        <span>{opiskeluoikeusMääräValmiit} ({valmiitPercent} %)</span>
+        <span className="ei-valmiit">{opiskeluoikeusMääräEiValmiit} ({100 - valmiitPercent} %)</span>
+      </div>
+
+    </li>
+  )
+}
 
 ReactDOM.render(
   (<div>
