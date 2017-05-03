a9472d095978c4f615004a0039cf04d5cffece30
Juha Paananen <juha.paananen@gmail.com>
2017-05-03 16:07:02 +0300

    zero delay

--- web/app/http.js
+++ web/app/http.js
@@ -61,7 +61,7 @@ http.cachedGet = (url, options = {}) => {
         delete http.cache[url]
       })
   }
-  return Bacon.later(1).flatMap(http.cache[url]).toProperty() // Ensure async behaviour
+  return Bacon.later(0).flatMap(http.cache[url]).toProperty() // Ensure async behaviour
 }
 window.http = http
 export default http
