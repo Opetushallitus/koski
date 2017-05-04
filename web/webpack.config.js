eb88a8dec9772b3e8cbaa5262eedf1258b051200
Juha Paananen <juha.paananen@gmail.com>
2017-05-04 15:40:44 +0300

    use production node env -> react prod build

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -36,6 +36,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new CopyWebpackPlugin(
       [
         { from: 'static'},
