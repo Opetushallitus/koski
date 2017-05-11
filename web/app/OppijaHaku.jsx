6cc2455c32d9be31521a88515a630c0c8a66b89b
Juha Paananen <juha.paananen@gmail.com>
2017-05-11 15:37:32 +0300

    add keyboard navigation (up/down/enter) to OppijaHaku

--- web/app/OppijaHaku.jsx
+++ web/app/OppijaHaku.jsx
@@ -2,7 +2,7 @@ import React from 'baret'
 import Bacon from 'baconjs'
 import Atom from 'bacon.atom'
 import Http from './http'
-import {showError} from './location'
+import {showError, navigateTo} from './location'
 import delays from './delays'
 import Link from './Link.jsx'
 
@@ -23,43 +23,78 @@ const oppijatP = Bacon.update(
 
 const searchInProgressP = oppijaHakuE.awaiting(hakuTulosE.mapError()).throttle(delays().delay(200))
 
-export const OppijaHaku = () => (
-  <div className={searchInProgressP.map((searching) => searching ? 'oppija-haku searching' : 'oppija-haku')}>
-    <div>
-      <h3>Hae tai lisää opiskelija</h3>
-      <input type="text" value={searchStringAtom} id='search-query' placeholder='henkilötunnus, nimi tai oppijanumero' onChange={(e) => searchStringAtom.set(e.target.value)}></input>
+export const OppijaHaku = () => {
+  let selectedIndexAtom = Atom(-1)
 
-    </div>
-    <div className='hakutulokset'>
-      {
-        oppijatP.map(({response, query}) => {
-          if (response.henkilöt.length > 0) {
-            return (<ul>{
-              response.henkilöt.map((o, i) =>
-                  (<li key={i}>
-                    <Link href={`/koski/oppija/${o.oid}`}>{o.sukunimi}, {o.etunimet}{o.hetu && ' (' + o.hetu + ')'}</Link>
-                  </li>))
-              }
-            </ul>)
-          }
-          if (query.length > 2) {
-            let url = '/koski/uusioppija#' + query
-            return (<div className='no-results'>
-              Ei hakutuloksia
-              { response.canAddNew &&
-                (<Link href={url} className="lisaa-oppija">
-                  Lisää uusi opiskelija
-                </Link>)
-              }
-              {
-                response.error &&
-                  <div className="error">{response.error}</div>
-              }
-
-            </div>)
-          }
-        })
+  let onKeyDown = (options) => (e) => {
+    let keyHandlers = {
+      ArrowUp: () => {
+        selectedIndexAtom.modify((i) => Math.max(i - 1, -1))
+      },
+      ArrowDown: () => {
+        selectedIndexAtom.modify((i) => Math.min(i + 1, options.length -1))
+      },
+      Enter: () => {
+        let option = options[selectedIndexAtom.get()]
+        option && navigateTo(option.href)
       }
+    }
+    if (e.getModifierState("Shift") || e.getModifierState("Alt") || e.getModifierState("Meta") || e.getModifierState("Control")) return
+    let handler = keyHandlers[e.key]
+    if (handler) {
+      e.preventDefault()
+      handler()
+    }
+  }
+
+  let optionsP = Bacon.combineWith(oppijatP, selectedIndexAtom, ({response, query}, selectedIndex) => {
+    if (response.henkilöt.length > 0) {
+      return response.henkilöt.map((o, i) => {
+        let href = `/koski/oppija/${o.oid}`
+        return {href, element: (<li className={i == selectedIndex ? 'selected' : ''} key={i}>
+          <Link href={href}>{o.sukunimi}, {o.etunimet}{o.hetu && ' (' + o.hetu + ')'}</Link>
+        </li>)}
+      })
+    } else if (response.canAddNew) {
+      let href = '/koski/uusioppija#' + query
+      return [{href, element: (<Link baret-lift className={selectedIndexAtom.map(i => 'lisaa-oppija' +(i == 0 ? ' selected' : ''))} href={href}>
+        Lisää uusi opiskelija
+      </Link>)}]
+    } else {
+      return []
+    }
+  })
+
+  return (<div className={searchInProgressP.map((searching) => searching ? 'oppija-haku searching' : 'oppija-haku')}>
+      <div>
+        <h3>Hae tai lisää opiskelija</h3>
+        <input type="text" value={searchStringAtom} id='search-query' placeholder='henkilötunnus, nimi tai oppijanumero' onChange={(e) => searchStringAtom.set(e.target.value)} onKeyDown={optionsP.map(onKeyDown)}></input>
+      </div>
+      <div className='hakutulokset'>
+        {
+          Bacon.combineWith(oppijatP, optionsP, ({response, query}, options) => {
+            if (response.henkilöt.length > 0) {
+              return (<ul>{
+                options.map(option => option.element)
+              }
+              </ul>)
+            }
+            if (query.length > 2) {
+              return (<div className='no-results'>
+                Ei hakutuloksia
+                {
+                  options.map(option => option.element)
+                }
+                {
+                  response.error &&
+                  <div className="error">{response.error}</div>
+                }
+
+              </div>)
+            }
+          })
+        }
+      </div>
     </div>
-  </div>
-)
+  )
+}
