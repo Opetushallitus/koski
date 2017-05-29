6c385191ba6e0fcb8ff68f9f89e23e8b38a2b03b
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-05-29 11:05:28 +0300

    Cleanup

--- src/main/scala/fi/oph/koski/pulssi/PulssiHtmlServlet.scala
+++ src/main/scala/fi/oph/koski/pulssi/PulssiHtmlServlet.scala
@@ -1,8 +1,7 @@
 package fi.oph.koski.pulssi
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
-import fi.oph.koski.perustiedot.KoulutusmuotoTilasto
+import fi.oph.koski.http.KoskiErrorCategory
 import fi.oph.koski.servlet.HtmlServlet
 import org.scalatra.ScalatraServlet
 
@@ -16,96 +15,66 @@ class PulssiHtmlServlet(val application: KoskiApplication) extends ScalatraServl
       redirectToLogin
     }
     if (koskiSessionOption.exists(_.hasGlobalReadAccess)) {
-      <html>
-        {htmlHead()}
-        <body>
-          <h3>Oppijat ja opiskeluoikeudet</h3>
-          <ul>
-            <li>
-              Oppijoiden määrä: {pulssi.oppijoidenMäärä}
-            </li>
-            <li>
-              Opiskeluoikeuksien määrä: {pulssi.opiskeluoikeusTilasto.opiskeluoikeuksienMäärä}
-            </li>
-            <li>
-              Opiskeluoikeuksien määrät koulutusmuodoittain:
-              <ul>
-                {
-                pulssi.opiskeluoikeusTilasto.koulutusmuotoTilastot.map { tilasto =>
-                  <li>
-                    {tilasto.koulutusmuoto}: {tilasto.opiskeluoikeuksienMäärä}
-                    <ul>
-                      <li>
-                        Valmistuneita: {tilasto.valmistuneidenMäärä} ({percent(tilasto.valmistuneidenMäärä, tilasto.opiskeluoikeuksienMäärä)}%)
-                      </li>
-                    </ul>
-                  </li>
-                }}
-              </ul>
-            </li>
-          </ul>
-          <h3>Koski tiedonsiirrot</h3>
-          <ul>
-            <li>
-              Siirtäneitä oppilaitoksia: {pulssi.opiskeluoikeusTilasto.siirtäneitäOppilaitoksiaYhteensä}
-            </li>
-            <li>
-              Oppilaitoksia yhteensä : {pulssi.oppilaitosMäärät.yhteensä}
-            </li>
-            <li>
-              Siirtoprosentti:
-              {percent(pulssi.opiskeluoikeusTilasto.siirtäneitäOppilaitoksiaYhteensä, pulssi.oppilaitosMäärät.yhteensä)}
-            </li>
-            <li>
-              Koulutusmuodoittain:
-              <ul>
-                  {pulssi.opiskeluoikeusTilasto.koulutusmuotoTilastot.flatMap { tilasto =>
-                    pulssi.oppilaitosMäärät.koulutusmuodoittain.get(tilasto.koulutusmuoto).map { oppilaitoksia =>
-                      <li>
-                        {tilasto.koulutusmuoto}: {tilasto.siirtäneitäOppilaitoksia} ({percent(tilasto.siirtäneitäOppilaitoksia, oppilaitoksia)}%)
-                      </li>
-                    }
-                  }}
-              </ul>
-            </li>
-          </ul>
-          <h3>Koski käyttöoikeudet</h3>
-          <ul>
-            <li>
-              Käyttöoikeuksien määrä: {pulssi.käyttöoikeudet.kokonaismäärä}
-            </li>
-            <li>
-              Käyttöoikeuksien määrät ryhmittäin:
-              <ul>
-              {pulssi.käyttöoikeudet.ryhmienMäärät.map { case (ryhmä, määrä) =>
-                <li>
-                  {ryhmä}: {määrä}
-                </li>
-              }}
-              </ul>
-            </li>
-          </ul>
-          <h3>Metriikka viimeisen 30 päivän ajalta</h3>
-          <ul>
-            <li>
-              Tiedonsiirtovirheet: {pulssi.metrics.epäonnistuneetSiirrot}
-            </li>
-            <li>
-              Käyttökatkojen määrä: {pulssi.metrics.katkot}
-            </li>
-            <li>
-              Hälytysten määrä: {pulssi.metrics.hälytykset}
-            </li>
-            <li>
-              Lokitettujen virheiden määrä: {pulssi.metrics.virheet}
-            </li>
-          </ul>
-        </body>
-      </html>
+      raportti
     } else {
-      renderStatus(KoskiErrorCategory.notFound())
+      renderStatus(KoskiErrorCategory.forbidden())
     }
   }
+
+  private def raportti =
+    <html>
+      {htmlHead()}
+      <body>
+        <h3>Oppijat ja opiskeluoikeudet</h3>
+        <ul>
+          <li>Oppijoiden määrä: {pulssi.oppijoidenMäärä}</li>
+          <li>Opiskeluoikeuksien määrä: {pulssi.opiskeluoikeusTilasto.opiskeluoikeuksienMäärä}</li>
+          <li>
+            Opiskeluoikeuksien määrät koulutusmuodoittain:
+            <ul>
+              {pulssi.opiskeluoikeusTilasto.koulutusmuotoTilastot.map { tilasto =>
+              <li>
+                {tilasto.koulutusmuoto}: {tilasto.opiskeluoikeuksienMäärä}, joista valmistuneita: {tilasto.valmistuneidenMäärä} ({percent(tilasto.valmistuneidenMäärä, tilasto.opiskeluoikeuksienMäärä)}%)
+              </li>
+            }}
+            </ul>
+          </li>
+        </ul>
+        <h3>Koski tiedonsiirrot</h3>
+        <ul>
+          <li>Siirtäneitä oppilaitoksia: {pulssi.opiskeluoikeusTilasto.siirtäneitäOppilaitoksiaYhteensä}</li>
+          <li>Oppilaitoksia yhteensä : {pulssi.oppilaitosMäärät.yhteensä}</li>
+          <li>Siirtoprosentti: {percent(pulssi.opiskeluoikeusTilasto.siirtäneitäOppilaitoksiaYhteensä, pulssi.oppilaitosMäärät.yhteensä)}</li>
+          <li>
+            Koulutusmuodoittain:
+            <ul>
+              {pulssi.opiskeluoikeusTilasto.koulutusmuotoTilastot.flatMap { tilasto =>
+                pulssi.oppilaitosMäärät.koulutusmuodoittain.get(tilasto.koulutusmuoto).map { oppilaitoksia =>
+                  <li>{tilasto.koulutusmuoto} : {tilasto.siirtäneitäOppilaitoksia} ( {percent(tilasto.siirtäneitäOppilaitoksia, oppilaitoksia)} %)</li>
+              }
+            }}
+            </ul>
+          </li>
+        </ul>
+        <h3>Koski käyttöoikeudet</h3>
+        <ul>
+          <li>Käyttöoikeuksien määrä: {pulssi.käyttöoikeudet.kokonaismäärä}</li>
+          <li>
+            Käyttöoikeuksien määrät ryhmittäin:
+            <ul>
+              {pulssi.käyttöoikeudet.ryhmienMäärät.map { case (ryhmä, määrä) => <li>{ryhmä}: {määrä}</li> }}
+            </ul>
+          </li>
+        </ul>
+        <h3>Metriikka viimeisen 30 päivän ajalta</h3>
+        <ul>
+          <li>Tiedonsiirtovirheet: {pulssi.metrics.epäonnistuneetSiirrot}</li>
+          <li>Käyttökatkojen määrä: {pulssi.metrics.katkot}</li>
+          <li>Hälytysten määrä: {pulssi.metrics.hälytykset}</li>
+          <li>Lokitettujen virheiden määrä: {pulssi.metrics.virheet}</li>
+        </ul>
+      </body>
+    </html>
 
   private def pulssi = application.koskiPulssi
   private def percent(x: Int, y: Int) = round(1)(x.toDouble / y.toDouble * 100)
