622d50114d0bb7da918a25e5da6d2a94f41229f2
Kalle Kainu <kkainu@l1044.core.reaktor.fi>
2017-05-30 11:34:09 +0300

    Introduce RemoteLocalizationRepository

--- src/main/scala/fi/oph/koski/localization/LocalizationRepository.scala
+++ src/main/scala/fi/oph/koski/localization/LocalizationRepository.scala
@@ -3,11 +3,24 @@ package fi.oph.koski.localization
 import com.typesafe.config.Config
 import fi.oph.koski.cache.{Cache, CacheManager, CachingProxy}
 import fi.oph.koski.http.Http
+import fi.oph.koski.http.Http._
 import fi.oph.koski.json.Json._
 import fi.oph.koski.localization.LocalizedString.sanitizeRequired
+import org.json4s._
 
 trait LocalizationRepository {
-  def localizations(): Map[String, LocalizedString]
+  def localizations(): Map[String, LocalizedString]= {
+    val localized: Map[String, Map[String, String]] = fetchLocalizations().extract[List[LokalisaatioPalveluLokalisaatio]]
+      .groupBy(_.key)
+      .mapValues(_.map(v => (v.locale, v.value)).toMap)
+
+
+    readResource("/localization/default-texts.json").extract[Map[String, String]].map {
+      case (key, value) => (key, sanitizeRequired(localized.getOrElse(key, Map.empty), value))
+    }
+  }
+
+  def fetchLocalizations(): JValue
 }
 
 object LocalizationRepository {
@@ -16,27 +29,17 @@ object LocalizationRepository {
       case "mock" =>
         MockLocalizationRepository
       case url =>
-        MockLocalizationRepository
+        new RemoteLocalizationRepository(Http(url))
     })
   }
 }
 
 object MockLocalizationRepository extends LocalizationRepository {
-
-  override def localizations(): Map[String, LocalizedString] = {
-    val localized: Map[String, Map[String, String]] = readResource("/mockdata/lokalisointi/koski.json").extract[List[LokalisaatioPalveluLokalisaatio]]
-          .groupBy(_.key)
-          .mapValues(_.map(v => (v.locale, v.value)).toMap)
-
-
-    readResource("/localization/default-texts.json").extract[Map[String, String]].map {
-      case (key, value) => (key, sanitizeRequired(localized.getOrElse(key, Map.empty), value))
-    }
-  }
+  override def fetchLocalizations(): JValue = readResource("/mockdata/lokalisointi/koski.json")
 }
 
 class RemoteLocalizationRepository(http: Http) extends LocalizationRepository {
-  override def localizations(): Map[String, LocalizedString] = ???
+  override def fetchLocalizations(): JValue = runTask(http.get(uri"/lokalisointi/cxf/rest/v1/localisation?category=koski")(Http.parseJson[JValue]))
 }
 
 case class LokalisaatioPalveluLokalisaatio(
