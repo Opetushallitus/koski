725402629f8a748363d6e4f7e98a555831e5fa7f
Juha Paananen <juha.paananen@gmail.com>
2017-05-31 15:07:55 +0300

    store changes in an object to avoid sending multiple changes related to same key

--- web/app/i18n-edit.js
+++ web/app/i18n-edit.js
@@ -2,23 +2,26 @@ import Atom from 'bacon.atom'
 import { lang } from './i18n'
 import http from './http'
 import { userP } from './user'
+import * as L from 'partial.lenses'
+import R from 'ramda'
 
-let changes = Atom([])
+let changes = Atom({})
 export const hasEditAccess = userP.map('.hasLocalizationWriteAccess')
 export const edit = Atom(false)
 export const startEdit = () => {
   edit.set(true)
 }
-export const hasChanges = changes.map(c => c.length > 0)
+export const hasChanges = changes.map(c => R.keys(c).length > 0)
 export const saveChanges = () => {
-  changes.get().forEach(({key, value}) => {
-    window.koskiLocalizationMap[key][lang] = value
+  let changeList = R.toPairs(changes.get()).map(([key, value]) => ({ key, value, locale: lang}))
+  changeList.forEach(({key, value, locale}) => {
+    window.koskiLocalizationMap[key][locale] = value
   })
-  http.put('/koski/api/localization', changes.get())
+  http.put('/koski/api/localization', changeList)
   edit.set(false)
 }
 export const cancelChanges = () => {
-  changes.set([])
+  changes.set({})
   edit.set(false)
 }
-export const changeText = (key, value) => changes.modify(c => c.concat({key, value, locale: lang}))
+export const changeText = (key, value) => changes.modify(cs => L.set([key], value, cs))
