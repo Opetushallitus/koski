bdea7ec94dffdd2c9139b52754b8d9722ecab16f
Kalle Kainu <kkainu@l1044.core.reaktor.fi>
2017-05-31 09:22:49 +0300

    Insert and update localizations

--- src/main/scala/fi/oph/koski/localization/LocalizationRepository.scala
+++ src/main/scala/fi/oph/koski/localization/LocalizationRepository.scala
@@ -1,17 +1,36 @@
 package fi.oph.koski.localization
 
 import com.typesafe.config.Config
-import fi.oph.koski.cache.{Cache, CacheManager, CachingProxy}
-import fi.oph.koski.http.Http
+import fi.oph.koski.cache.{Cache, CacheManager, KeyValueCache, SingleValueCache}
 import fi.oph.koski.http.Http._
+import fi.oph.koski.http.{Http, VirkailijaHttpClient}
 import fi.oph.koski.json.Json._
+import fi.oph.koski.json.Json4sHttp4s.json4sEncoderOf
 import fi.oph.koski.localization.LocalizedString.sanitize
 import fi.oph.koski.log.Logging
 import org.json4s._
 
 trait LocalizationRepository extends Logging {
+
+  def localizations(): Map[String, LocalizedString]
+
+  def fetchLocalizations(): JValue
+
+  def createOrUpdate(localizations: List[UpdateLocalization])
+}
+
+abstract class CachedLocalizationService(implicit cacheInvalidator: CacheManager) extends LocalizationRepository {
+  private val cache = KeyValueCache[String, Map[String, LocalizedString]](
+    Cache.cacheAllRefresh("LocalizationRepository.localizations", durationSeconds = 60, maxSize = 1),
+    key => fetch()
+  )
+
   def localizations(): Map[String, LocalizedString] = {
-    val localized: Map[String, Map[String, String]] = fetchLocalizations().extract[List[LokalisaatioPalveluLokalisaatio]]
+    cache("key")
+  }
+
+  private def fetch(): Map[String, LocalizedString] = {
+    val localized: Map[String, Map[String, String]] = fetchLocalizations().extract[List[LocalizationServiceLocalization]]
       .groupBy(_.key)
       .mapValues(_.map(v => (v.locale, v.value)).toMap)
 
@@ -23,30 +42,41 @@ trait LocalizationRepository extends Logging {
         }
     }
   }
-
-  def fetchLocalizations(): JValue
 }
 
 object LocalizationRepository {
   def apply(config: Config)(implicit cacheInvalidator: CacheManager): LocalizationRepository = {
-    CachingProxy(Cache.cacheAllNoRefresh("LocalizationRepository", 60, 1), config.getString("localization.url") match {
+    config.getString("localization.url") match {
       case "mock" =>
-        MockLocalizationRepository
-      case url =>
-        new RemoteLocalizationRepository(Http(url))
-    })
+        new MockLocalizationRepository
+      case url: Any =>
+        new RemoteLocalizationRepository(VirkailijaHttpClient(config.getString("opintopolku.virkailija.username"), config.getString("opintopolku.virkailija.password"), config.getString("opintopolku.virkailija.url"), "/lokalisointi"))
+    }
   }
 }
 
-object MockLocalizationRepository extends LocalizationRepository {
+class MockLocalizationRepository(implicit cacheInvalidator: CacheManager) extends CachedLocalizationService {
   override def fetchLocalizations(): JValue = readResource("/mockdata/lokalisointi/koski.json")
+
+  override def createOrUpdate(localizations: List[UpdateLocalization]): Unit = ???
 }
 
-class RemoteLocalizationRepository(http: Http) extends LocalizationRepository {
+class RemoteLocalizationRepository(http: Http)(implicit cacheInvalidator: CacheManager) extends CachedLocalizationService {
   override def fetchLocalizations(): JValue = runTask(http.get(uri"/lokalisointi/cxf/rest/v1/localisation?category=koski")(Http.parseJson[JValue]))
+
+  override def createOrUpdate(localizations: List[UpdateLocalization]): Unit = {
+    runTask(http.post(uri"/lokalisointi/cxf/rest/v1/localisation/update", localizations)(json4sEncoderOf[List[UpdateLocalization]])(Http.unitDecoder))
+  }
 }
 
-case class LokalisaatioPalveluLokalisaatio(
+case class UpdateLocalization(
+  locale: String,
+  key: String,
+  value: String,
+  category: String = "koski"
+)
+
+case class LocalizationServiceLocalization(
   id: Integer,
   locale: String,
   category: String,
--- src/main/scala/fi/oph/koski/localization/LocalizationServlet.scala
+++ src/main/scala/fi/oph/koski/localization/LocalizationServlet.scala
@@ -1,11 +1,29 @@
 package fi.oph.koski.localization
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.koskiuser.{RequiresAuthentication, Unauthenticated}
+import fi.oph.koski.json.Json
+import fi.oph.koski.koskiuser.Unauthenticated
 import fi.oph.koski.servlet.{ApiServlet, NoCache}
 
 class LocalizationServlet (val application: KoskiApplication) extends ApiServlet with Unauthenticated with NoCache {
   get("/") {
     application.localizationRepository.localizations()
   }
+
+  put("/") {
+    withJsonBody { body =>
+      val req = Json.fromJValue[List[LocalizationRequest]](body)
+      application.localizationRepository.createOrUpdate(req.map(_.toUpdateLocalization))
+    } ()
+  }
 }
+
+case class LocalizationRequest(
+  locale: String,
+  key: String,
+  value: String
+) {
+  def toUpdateLocalization = UpdateLocalization(locale, key, value)
+}
+
+
