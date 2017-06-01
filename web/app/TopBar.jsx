4f8598c48189ac4b8cb3bad8bb4137452e6c2a0a
Kalle Kainu <kkainu@l1044.core.reaktor.fi>
2017-06-01 11:02:49 +0300

    Fix eslint

--- web/app/TopBar.jsx
+++ web/app/TopBar.jsx
@@ -13,11 +13,10 @@ export const TopBar = ({user, saved, titleKey}) => {
     }
   }
 
-  console.log("render", edit.get())
   return (<header id='topbar' className={saved ? 'saved' : ''}>
       <div id='logo'><Text name="Opintopolku.fi"/></div>
       <h1><Link href="/koski/"><Text name="Koski"/></Link>{titleKey ?
-          <span>{' - '}<Text name={titleKey}/></span> : ''}</h1>{edit.map((e) => !e && <a className="edit" onClick={onClick}>edit</a>)}
+          <span>{' - '}<Text name={titleKey}/></span> : ''}</h1>{edit.map((e) => !e && <a className="edit" onClick={onClick}>{"edit"}</a>)}
       <span className="save-info"><Text name="Kaikki muutokset tallennettu."/></span>
       <UserInfo user={user}/>
     </header>
