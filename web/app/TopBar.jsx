f87863384714e9c825901fa95de0a1420bb5b1cb
Kalle Kainu <kkainu@l1044.core.reaktor.fi>
2017-06-01 11:01:17 +0300

    Edit localizations by clicking edit

--- web/app/TopBar.jsx
+++ web/app/TopBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'baret'
 import {UserInfo} from './UserInfo.jsx'
 import Link from './Link.jsx'
 import Text from './Text.jsx'
@@ -6,17 +6,18 @@ import {edit, startEdit} from './i18n-edit'
 
 export const TopBar = ({user, saved, titleKey}) => {
   let onClick = e => {
-    // Start editing localized text by alt-clicking on the H1 Heading
-    if (e.getModifierState('Alt') && !edit.get()) {
+    if (!edit.get()) {
       startEdit()
       e.stopPropagation()
       e.preventDefault()
     }
   }
+
+  console.log("render", edit.get())
   return (<header id='topbar' className={saved ? 'saved' : ''}>
       <div id='logo'><Text name="Opintopolku.fi"/></div>
-      <h1 onClick={onClick}><Link href="/koski/"><Text name="Koski"/></Link>{titleKey ?
-        <span>{' - '}<Text name={titleKey}/></span> : ''}</h1>
+      <h1><Link href="/koski/"><Text name="Koski"/></Link>{titleKey ?
+          <span>{' - '}<Text name={titleKey}/></span> : ''}</h1>{edit.map((e) => !e && <a className="edit" onClick={onClick}>edit</a>)}
       <span className="save-info"><Text name="Kaikki muutokset tallennettu."/></span>
       <UserInfo user={user}/>
     </header>
