01083bc89bf348a516672ff531b1ad7d1cf22605
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-06-09 11:03:19 +0300

    Siistitään ticket kokonaan pois osoitteesta

--- web/app/Koski.jsx
+++ web/app/Koski.jsx
@@ -11,6 +11,9 @@ import {locationP} from './location.js'
 import {savedBus} from './Oppija.jsx'
 import LocalizationEditBar from './LocalizationEditBar.jsx'
 import { t } from './i18n'
+import {currentLocation} from './location'
+
+history.replaceState(null, null, currentLocation().filterQueryParams((k, v) => k !== 'ticket').toString())
 
 // Stays at `true` for five seconds after latest saved change. Reset to `false` when another Oppija is selected.
 const savedP = savedBus.flatMapLatest(() => Bacon.once(true).concat((locationP.changes().merge(Bacon.later(5000))).map(false))).toProperty(false).skipDuplicates()
