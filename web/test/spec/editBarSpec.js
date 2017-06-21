02b5e992f557d51222db9ab94209f2d8d7af9e06
Hannes Karppila <hannes.karppila@gmail.com>
2017-06-21 10:28:59 +0300

    Clean up editBarSpec

--- web/test/spec/editBarSpec.js
+++ web/test/spec/editBarSpec.js
@@ -1,40 +1,56 @@
 describe('EditBar', function( ){
+    function currentURL() {
+        return testFrame().location.href
+    }
+    
+    function hasClass(selector, className) {
+        return S(selector).attr("class").split(" ").includes(className)
+    }
+
+    function isVisible(selector) {
+        return hasClass(selector, "visible")
+    }
+
+    function click(selector) {
+        triggerEvent(S(selector)[0], "click")
+    }
+    
     var page = KoskiPage()
     before(Authentication().login(), resetFixtures)
 
     describe('Visibility', function() {
         before(page.openPage, page.oppijaHaku.searchAndSelect('020655-2479'))
         it('hidden by default', function() {
-            expect(S("#edit-bar").attr("class").split(" ").includes("visible")).to.equal(false)
+            expect(isVisible("#edit-bar")).to.equal(false)
         })
 
         it('visible on edit mode', function() {
-            triggerEvent(S("button.toggle-edit")[0], "click")
-            expect(S("#edit-bar").attr("class").split(" ").includes("visible")).to.equal(true)
+            click("button.toggle-edit")
+            expect(isVisible("#edit-bar")).to.equal(true)
         })
 
         it('hidden when exiting edit mode', function() {
-            triggerEvent(S("a.cancel")[0], "click")
-            expect(S("#edit-bar").attr("class").split(" ").includes("visible")).to.equal(false)
+            click("a.cancel")
+            expect(isVisible("#edit-bar")).to.equal(false)
         })
 
         it('visible when entering edit mode again', function() {
-            triggerEvent(S("button.toggle-edit")[0], "click")
-            expect(S("#edit-bar").attr("class").split(" ").includes("visible")).to.equal(true)
+            click("button.toggle-edit")
+            expect(isVisible("#edit-bar")).to.equal(true)
         })
 
         it('returns to oppijataulukko with back link', function() {
-            triggerEvent(S("a.back-link")[0], "click")
-            expect(testFrame().location.href.endsWith("/koski/")).to.equal(true)
-            expect(S("#topbar + div").first().attr("class").split(" ").includes("oppijataulukko")).to.equal(true)
+            click("a.back-link")
+            expect(currentURL().endsWith("/koski/")).to.equal(true)
+            expect(hasClass("#topbar + div", "oppijataulukko")).to.equal(true)
         })
 
         it('shows correctly when returning to edit mode with back button', function(done) {
             goBack()
             setTimeout(() => {
                 try {
-                    expect(testFrame().location.href.endsWith("/koski/")).to.equal(false)
-                    expect(S("#edit-bar").attr("class").split(" ").includes("visible")).to.equal(true)
+                    expect(currentURL().endsWith("/koski/")).to.equal(false)
+                    expect(isVisible("#edit-bar")).to.equal(true)
                 }
                 catch (err) {
                     done(err)
