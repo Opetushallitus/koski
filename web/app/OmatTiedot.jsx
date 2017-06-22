1d7879e070994d158b7c077220633f5cf5cc31cd
Juha Paananen <juha.paananen@gmail.com>
2017-06-22 14:06:51 +0300

    Fix Omat Tiedot

--- web/app/OmatTiedot.jsx
+++ web/app/OmatTiedot.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Http from './http'
 import {ExistingOppija} from './Oppija.jsx'
+import {Editor} from './editor/Editor.jsx'
 import Bacon from 'baconjs'
 import Text from './Text.jsx'
+import {editorMapping} from './editor/Editors.jsx'
 
 export const omatTiedotContentP = () => innerContentP().map(inner =>
   ({
@@ -18,5 +20,7 @@ export const omatTiedotContentP = () => innerContentP().map(inner =>
 const omatTiedotP = () => Http.cachedGet('/koski/api/editor/omattiedot', { errorMapper: (e) => e.httpStatus === 404 ? null : new Bacon.Error}).toProperty()
 
 const innerContentP = () => omatTiedotP().map(oppija =>
-  oppija ? <div className="main-content oppija"><ExistingOppija oppija={oppija} stateP={Bacon.constant('viewing')}/></div> : <div className="main-content ei-opiskeluoikeuksia"><Text name="Tiedoillasi ei löydy opiskeluoikeuksia"/></div>
+  oppija
+    ? <div className="main-content oppija"><ExistingOppija oppija={Editor.setupContext(oppija, {editorMapping})} stateP={Bacon.constant('viewing')}/></div>
+    : <div className="main-content ei-opiskeluoikeuksia"><Text name="Tiedoillasi ei löydy opiskeluoikeuksia"/></div>
 ).startWith(<div className="main-content ajax-indicator-bg"><Text name="Ladataan..."/></div>)
