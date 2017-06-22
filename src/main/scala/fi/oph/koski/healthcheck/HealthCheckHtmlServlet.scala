73bfc7589f646d6910b34e7032114c7d943c91ac
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-06-22 11:25:16 +0300

    Healthcheck tarkistaa integraatiot ja ohittaa kakut

--- src/main/scala/fi/oph/koski/healthcheck/HealthCheckHtmlServlet.scala
+++ src/main/scala/fi/oph/koski/healthcheck/HealthCheckHtmlServlet.scala
@@ -5,12 +5,19 @@ import fi.oph.koski.servlet.HtmlServlet
 
 class HealthCheckHtmlServlet(val application: KoskiApplication) extends HtmlServlet{
   get("/") {
-    val status = application.healthCheck.healthcheck.isOk match {
+    val healthcheck = application.healthCheck.healthcheck
+    val status = healthcheck.isOk match {
       case true => <h1 style="font-size: 20vw; background: #528a1e; background: linear-gradient(to bottom, darkgreen 0%,green 100%); color: white; font-family: sans-serif; border-radius: 3px;">OK</h1>
-      case false => <h1 style="font-size: 20vw; background: red;">FAIL</h1>
+      case false =>
+        <div style="background: red;">
+          <h1 style="font-size: 20vw; margin-bottom: 0;">FAIL</h1>
+          <ul style="list-style: none; line-height: 25px; padding-bottom: 50px">
+            {healthcheck.errors.map(_.key).distinct.map(e => <li>{e}</li>)}
+          </ul>
+        </div>
     }
     <html style="text-align: center;">
-      <h1>{status}</h1>
+      {status}
     </html>
   }
 }
--- src/main/scala/fi/oph/koski/healthcheck/HealthChecker.scala
+++ src/main/scala/fi/oph/koski/healthcheck/HealthChecker.scala
@@ -3,49 +3,98 @@ package fi.oph.koski.healthcheck
 import fi.oph.koski.cache.{Cache, CacheManager, Cached, CachingProxy}
 import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.documentation.AmmatillinenExampleData._
-import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
+import fi.oph.koski.http.{ErrorDetail, HttpStatus, HttpStatusException, KoskiErrorCategory}
+import fi.oph.koski.koodisto.{KoodistoPalvelu, KoodistoViite}
 import fi.oph.koski.koskiuser.AccessType
 import fi.oph.koski.koskiuser.KoskiSession._
 import fi.oph.koski.log.Logging
-import fi.oph.koski.schema.{NimellinenHenkilö, OidHenkilö, Oppija}
+import fi.oph.koski.organisaatio.{MockOrganisaatiot, RemoteOrganisaatioRepository}
+import fi.oph.koski.schema._
 
 trait HealthCheck extends Logging {
   private implicit val user = systemUser
   private implicit val accessType = AccessType.write
   private val oid = application.config.getString("healthcheck.oppija.oid")
-  private def oppija: Either[HttpStatus, Oppija] = application.validator.validateAsJson(Oppija(OidHenkilö(oid), List(perustutkintoOpiskeluoikeusValmis())))
+  private val koodistoPalvelu = KoodistoPalvelu.withoutCache(application.config)
+  private def healthcheckOppija: Either[HttpStatus, Oppija] = application.validator.validateAsJson(Oppija(OidHenkilö(oid), List(perustutkintoOpiskeluoikeusValmis())))
 
-  def healthcheck: HttpStatus = try {
-    application.oppijaFacade.findOppija(oid) match {
-      case Left(HttpStatus(404, _)) => createHealthCheckUser
-      case Left(status) => status
-      case Right(Oppija(henkilö: NimellinenHenkilö, _)) =>
-        if (application.perustiedotRepository.findOids(henkilö.kokonimi).contains(oid)) {
-         HttpStatus.ok
-        } else {
-          logger.error(s"Healthcheck user $oid, not found from elasticsearch")
-          KoskiErrorCategory.notFound.oppijaaEiLöydy(s"Healthcheck user $oid, not found from elasticsearch")
-        }
-      case Right(o) => KoskiErrorCategory.internalError(s"Healthcheck user didn't have a name ${o.henkilö}")
+  def healthcheck: HttpStatus = {
+    val oppija = findOrCreateOppija
+    val checks: List[() => HttpStatus] = List(
+      () => oppijaCheck(oppija),
+      () => elasticCheck(oppija),
+      () => koodistopalveluCheck,
+      () => organisaatioPalveluCheck
+    )
+
+    HttpStatus.fold(checks.par.map(_.apply).toList)
+  }
+
+  private def oppijaCheck(oppija: Either[HttpStatus, NimellinenHenkilö]): HttpStatus = oppija.left.getOrElse(HttpStatus.ok)
+
+  private def elasticCheck(oppija: Either[HttpStatus, NimellinenHenkilö]): HttpStatus = oppija.flatMap { henkilö =>
+    get("elasticsearch", application.perustiedotRepository.findOids(henkilö.kokonimi))
+      .filterOrElse(_.contains(oid), KoskiErrorCategory.notFound.oppijaaEiLöydy(s"Healthcheck user $oid, not found from elasticsearch"))
+  }.left.getOrElse(HttpStatus.ok)
+
+  private def koodistopalveluCheck: HttpStatus =
+    get("koodistopalvelu", koodistoPalvelu.getKoodistoKoodit(KoodistoViite("suorituksentyyppi", 1)).toList.flatten)
+      .filterOrElse(_.nonEmpty, KoskiErrorCategory.notFound.koodistoaEiLöydy())
+      .left.getOrElse(HttpStatus.ok)
+
+  private def organisaatioPalveluCheck: HttpStatus =
+    application.organisaatioRepository match {
+      case remote: RemoteOrganisaatioRepository =>
+        get("organisaatiopalvelu", remote.fetch(MockOrganisaatiot.helsinginYliopisto)).filterOrElse(_.organisaatiot.nonEmpty, KoskiErrorCategory.notFound.oppilaitostaEiLöydy())
+          .left.getOrElse(HttpStatus.ok)
+      case _ => HttpStatus.ok
     }
-  } catch {
-    case e: Exception =>
-      logger.error(e)("healthcheck failed")
-      KoskiErrorCategory.internalError("healthcheck failed")
+
+  private def findOrCreateOppija: Either[HttpStatus, NimellinenHenkilö] = {
+    def findOrCreate(canCreate: Boolean): Either[HttpStatus, NimellinenHenkilö] = getOppija(oid) match {
+      case Left(HttpStatus(404, _)) if canCreate =>
+        createHealthCheckUser
+        findOrCreate(canCreate = false)
+      case Right(Oppija(henkilö: NimellinenHenkilö, _)) => Right(henkilö)
+      case Right(o) => Left(KoskiErrorCategory.internalError(s"Healthcheck käyttäjällä ei ollut nimeä ${o.henkilö}"))
+      case Left(status) => Left(status)
+    }
+    findOrCreate(canCreate = true)
+  }
+
+  private def getOppija(oid: String): Either[HttpStatus, Oppija] = {
+    get("oppijanumerorekisteri", application.henkilöRepository.opintopolku.findByOid(oid))
+      .flatMap(_.toRight(KoskiErrorCategory.notFound.oppijaaEiLöydy(s"Healtcheck käyttäjää $oid ei löydy oppijanumerorekisteristä")))
+      .flatMap { henkilö =>
+        get("postgres", application.opiskeluoikeusRepository.findByOppijaOid(oid)).flatMap { oos =>
+          if (oos.isEmpty) Left(KoskiErrorCategory.notFound.opiskeluoikeuttaEiLöydyTaiEiOikeuksia(s"Healthcheck käyttäjän $oid opiskeluoikeuksia ei löydy tietokannasta"))
+          else Right(Oppija(henkilö, oos))
+        }
+      }
   }
 
   private def createHealthCheckUser: HttpStatus = {
     logger.info(s"Healtcheck user not found creating one with oid $oid")
-    oppija match {
+    healthcheckOppija match {
       case Left(status) => status
       case Right(oppija) =>
-        application.oppijaFacade.createOrUpdate(oppija, true) match {
+        application.oppijaFacade.createOrUpdate(oppija, allowUpdate = true) match {
           case Left(status) =>
             logger.error(s"Problem creating healthchech oppija ${status.toString}")
             status
           case _ => HttpStatus.ok
         }
     }
+  }
+
+  private def get[T](key: String, f: => T): Either[HttpStatus, T] = try {
+    Right(f)
+  } catch {
+    case e: HttpStatusException =>
+      Left(HttpStatus(e.status, List(ErrorDetail(key, e.text))))
+    case e =>
+      logger.error(e)("healthcheck failed")
+      Left(KoskiErrorCategory.internalError.subcategory(key, "healthcheck failed")())
   }
 
   def application: KoskiApplication
