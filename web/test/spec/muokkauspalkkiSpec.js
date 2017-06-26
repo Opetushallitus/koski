3703c193dcd31d6519a022d6d09cfacac439c615
Hannes Karppila <hannes.karppila@gmail.com>
2017-06-26 16:29:51 +0300

    Rewrite muokkauspalkkiSpec

--- web/test/spec/muokkauspalkkiSpec.js
+++ web/test/spec/muokkauspalkkiSpec.js
@@ -1,79 +1,82 @@
-describe('Muokkauspalkki', function( ){
-    function currentURL() {
-        return testFrame().location.href
-    }
-    
-    function hasClass(selector, className) {
-        return S(selector).attr("class").split(" ").includes(className)
-    }
+describe('Muokkauspalkki', function () {
+  function currentURL() {
+    return testFrame().location.href
+  }
 
-    function isVisible(selector) {
-        return hasClass(selector, "visible")
-    }
+  function editBarVisible() {
+    return S('#edit-bar').hasClass('visible')
+  }
 
-    function click(selector) {
-        triggerEvent(S(selector)[0], "click")
-    }
-    
-    var page = KoskiPage()
-    before(Authentication().login(), resetFixtures)
+  function click(selector) {
+    triggerEvent(S(selector)[0], 'click')
+  }
 
-    describe('Näkyvyys', function() {
-        before(page.openPage, page.oppijaHaku.searchAndSelect('020655-2479'))
-        it('piilossa näyttötilassa', function() {
-            expect(isVisible("#edit-bar")).to.equal(false)
-        })
+  var page = KoskiPage()
+  describe('Näkyvyys', function () {
+    beforeEach(Authentication().login())
 
-        it('näkyvillä muokkaustilassa', function() {
-            click("button.toggle-edit")
-            expect(isVisible("#edit-bar")).to.equal(true)
-        })
+    before(page.openPage, page.oppijaHaku.searchAndSelect('020655-2479'))
 
-        it('piilossa muokkaustilasta poistuttaessa', function() {
-            click("a.cancel")
-            expect(isVisible("#edit-bar")).to.equal(false)
-        })
-
-
-        it('piilossa oppijataulukosta näyttötilaan edellinen-painikkeella palattaessa', function(done) {
-            click("a.back-link")
-            expect(currentURL().endsWith("/koski/")).to.equal(true)
-            expect(hasClass("#topbar + div", "oppijataulukko")).to.equal(true)
-            goBack()
-            setTimeout(() => {
-                try {
-                    expect(currentURL().endsWith("/koski/")).to.equal(false)
-                    expect(isVisible("#edit-bar")).to.equal(false)
-                }
-                catch (err) {
-                    done(err)
-                    return
-                }
-                done()
-            }, 0)
-        })
-
-        it('näkyvillä muokkaustilaan palattaessa', function() {
-            click("button.toggle-edit")
-            expect(isVisible("#edit-bar")).to.equal(true)
-        })
-
-        it('näkyvillä oppijataulukosta muokkaustilaan edellinen-painikkeella palattaessa', function(done) {
-            click("a.back-link")
-            expect(currentURL().endsWith("/koski/")).to.equal(true)
-            expect(hasClass("#topbar + div", "oppijataulukko")).to.equal(true)
-            goBack()
-            setTimeout(() => {
-                try {
-                    expect(currentURL().endsWith("/koski/")).to.equal(false)
-                    expect(isVisible("#edit-bar")).to.equal(true)
-                }
-                catch (err) {
-                    done(err)
-                    return
-                }
-                done()
-            }, 0)
-        })
+    describe('Näyttötilassa', function () {
+      it('piilossa', function () {
+        expect(editBarVisible()).to.equal(false)
+      })
     })
+
+    describe('Muokkaustilassa', function () {
+      before(function () {
+        click('button.toggle-edit')
+      })
+      it('Näkyvillä', function () {
+        expect(editBarVisible()).to.equal(true)
+      })
+    })
+
+    describe('Muokkaustilasta poistuttaessa', function () {
+      before(function () {
+        click('a.cancel')
+      })
+      it('Piilossa', function () {
+        expect(editBarVisible()).to.equal(false)
+      })
+    })
+
+
+    describe('Oppijataulukosta näyttötilaan edellinen-painikkeella palattaessa', function () {
+      before(function () {
+        this.timeout(2000)
+        click('a.back-link')
+        wait.until(function () {
+          return currentURL().endsWith('/koski/') && S('#topbar + div').hasClass(':oppijataulukko')
+        })
+        goBack()
+        wait.until(function () {
+          return !currentURL().endsWith('/koski/')
+        })
+      })
+      it('Pysyy piilossa', function () {
+        expect(editBarVisible()).to.equal(false)
+      })
+    })
+
+    describe('Oppijataulukosta näyttötilaan edellinen-painikkeella palattaessa', function () {
+      before(function () {
+        this.timeout(2000)
+        click('button.toggle-edit')
+        click('a.back-link')
+        wait.until(function () {
+          return currentURL().endsWith('/koski/') && S('#topbar + div').hasClass(':oppijataulukko')
+        })
+        goBack()
+        wait.until(function () {
+          return !currentURL().endsWith('/koski/')
+        })
+      })
+
+      it('Pysyy näkyvillä', function () {
+        expect(editBarVisible()).to.equal(true)
+      })
+    })
+
+  })
 })
