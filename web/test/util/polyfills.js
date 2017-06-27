4f5c2d87be913eef2666db23229690547926068b
Hannes Karppila <hannes.karppila@gmail.com>
2017-06-27 12:16:32 +0300

    Add startsWith and endsWith polyfills for tests

--- web/test/util/polyfills.js
+++ web/test/util/polyfills.js
@@ -9,3 +9,21 @@ function addArrayMethod(name, fn) {
 addArrayMethod('includes', function (item) {
   return this.indexOf(item) >= 0
 })
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0
+    return this.indexOf(searchString, position) === position
+  }
+}
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+    var subjectString = this.toString()
+    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+      position = subjectString.length
+    }
+    position -= searchString.length
+    var lastIndex = subjectString.lastIndexOf(searchString, position)
+    return lastIndex !== -1 && lastIndex === position
+  }
+}
