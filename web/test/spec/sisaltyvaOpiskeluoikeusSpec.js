52a5dbbb1eb5c2fd34027f93f5705a2665182d32
Juha Paananen <juha.paananen@gmail.com>
2017-06-28 14:47:26 +0300

    mocha-testi sisältävän opiskeluoikeuden id:n validoinnille

--- web/test/spec/sisaltyvaOpiskeluoikeusSpec.js
+++ web/test/spec/sisaltyvaOpiskeluoikeusSpec.js
@@ -22,34 +22,50 @@ describe('Opiskeluoikeuden sisältyvyys', function() {
       addOppija.submitAndExpectSuccess('280618-402H', 'Autoalan perustutkinto'),
       editor.edit,
       editor.property('sisältyyOpiskeluoikeuteen').addValue,
-      editor.property('oppilaitos').organisaatioValitsin().select('Stadin ammattiopisto'),
-      function() {
-        return editor.property('sisältyyOpiskeluoikeuteen').property('id').setValue(sisältävänOpiskeluoikeudenId)()
-      },
-      editor.saveChanges
+      editor.property('oppilaitos').organisaatioValitsin().select('Stadin ammattiopisto')
     )
 
-    it('toimii', function() {
-
+    describe('Kun sisältävää opiskeluoikeutta ei löydy id:llä', function() {
+      before(
+        editor.property('sisältyyOpiskeluoikeuteen').property('id').setValue(929292929),
+        editor.saveChangesAndExpectError,
+        wait.until(page.isErrorShown)
+      )
+      it('Tallennus epäonnistuu ja näytetään virheilmoitus', function() {
+        expect(page.getErrorMessage()).to.equal('Sisältävää opiskeluoikeutta ei löydy id-arvolla 929292929')
+      })
     })
 
-    describe('Sisältyvän opiskeluoikeuden näkyvyys sisältävän opiskeluoikeuden organisaatiolle', function() {
+    describe('Kun sisältävä opiskeluoikeus löytyy id:llä', function() {
       before(
-        Authentication().login('tallentaja'),
-        page.openPage,
-        page.oppijaHaku.searchAndSelect('280618-402H')
+        function() {
+          return editor.property('sisältyyOpiskeluoikeuteen').property('id').setValue(sisältävänOpiskeluoikeudenId)()
+        },
+        editor.saveChanges
       )
 
-      it('Lukuoikeudet on', function() {
-        expect(opinnot.opiskeluoikeudet.opiskeluoikeuksienOtsikot()).to.deep.equal([
-          'Stadin ammattiopisto,Luonto- ja ympäristöalan perustutkinto',
-          'Omnian ammattiopisto,Autoalan perustutkinto'
-        ])
+      it('tallennus onnistuu', function() {
+
       })
 
-      it('Kirjoitusoikeuksia ei ole', function() {
-        expect(opinnot.opiskeluoikeusEditor(0).isEditable()).to.equal(true)
-        expect(opinnot.opiskeluoikeusEditor(1).isEditable()).to.equal(false)
+      describe('Sisältyvän opiskeluoikeuden näkyvyys sisältävän opiskeluoikeuden organisaatiolle', function() {
+        before(
+          Authentication().login('tallentaja'),
+          page.openPage,
+          page.oppijaHaku.searchAndSelect('280618-402H')
+        )
+
+        it('Lukuoikeudet on', function() {
+          expect(opinnot.opiskeluoikeudet.opiskeluoikeuksienOtsikot()).to.deep.equal([
+            'Stadin ammattiopisto,Luonto- ja ympäristöalan perustutkinto',
+            'Omnian ammattiopisto,Autoalan perustutkinto'
+          ])
+        })
+
+        it('Kirjoitusoikeuksia ei ole', function() {
+          expect(opinnot.opiskeluoikeusEditor(0).isEditable()).to.equal(true)
+          expect(opinnot.opiskeluoikeusEditor(1).isEditable()).to.equal(false)
+        })
       })
     })
   })
