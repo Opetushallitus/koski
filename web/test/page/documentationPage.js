16cd6fde7ba4fe3e979be12bed25fdddfb7401d2
Hannes Karppila <hannes.karppila@gmail.com>
2017-07-24 17:00:00 +0300

    Fix documentation page test

--- web/test/page/documentationPage.js
+++ web/test/page/documentationPage.js
@@ -2,7 +2,7 @@ function DocumentationPage() {
 
   var api = {
     openPage: function() {
-      return openPage('/koski/documentation', api.isVisible)()
+      return openPage('/koski/dokumentaatio', api.isVisible)()
     },
     isVisible: function() {
       return isElementVisible(S('.content h1'))
