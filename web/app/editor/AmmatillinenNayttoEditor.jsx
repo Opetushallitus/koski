f15be7fcb73e8bef31c94ba74f3a3f91bcc4db53
Hannes Karppila <hannes.karppila@gmail.com>
2017-07-26 17:12:39 +0300

    Use proper fa icon and align it properly

--- web/app/editor/AmmatillinenNayttoEditor.jsx
+++ web/app/editor/AmmatillinenNayttoEditor.jsx
@@ -1,17 +1,37 @@
 import React from 'baret'
+import Bacon from 'baconjs'
+import Atom from 'bacon.atom'
 import R from 'ramda'
 import Text from '../Text.jsx'
+import ModalDialog from './ModalDialog.jsx'
 import {wrapOptional} from './OptionalEditor.jsx'
 import {modelData, modelTitle} from './EditorModel.js'
 import {ISO2FinnishDate} from '../date'
 
-const YksittäinenNäyttöEditor = ({wm}) => {
+const UusiNäyttöPopup = ({model, doneCallback}) => {
+  let validP = Bacon.constant(true)
+  let submitBus = Bacon.Bus()
+
+  submitBus.onValue(() => {
+    console.log('sb')
+    doneCallback()
+  })
+
+  return (
+    <ModalDialog className="lisaa-näyttö-modal" onDismiss={doneCallback} onSubmit={() => submitBus.push()} okTextKey="Lisää" validP={validP}>
+      <h2><Text name="Ammattiosaamisen näyttö"/></h2>
+      <p>{'..FIELDS..'}</p>
+    </ModalDialog>
+  )
+}
+
+const YksittäinenNäyttöEditor = ({edit, wm}) => {
   let alku  = ISO2FinnishDate(modelTitle(wm, 'suoritusaika.alku'))
   let loppu = ISO2FinnishDate(modelTitle(wm, 'suoritusaika.loppu'))
 
   return (<li>
     <div>
-      <a className="remove-value">{''}</a>
+      {edit && <a className="remove-value fa fa-times-circle-o"></a>}
       {alku === loppu
         ? <span className="pvm">{alku}</span>
         : <span><span className="alku pvm">{alku}</span>{' - '}<span className="loppu pvm">{loppu}</span></span>
@@ -26,6 +46,9 @@ const YksittäinenNäyttöEditor = ({wm}) => {
 }
 
 export const AmmatillinenNäyttöEditor = ({model}) => {
+  const popupVisibleA = Atom(false)
+
+  const edit = model.context.edit
   let wrappedModel = wrapOptional({model})
 
   console.log(wrappedModel)
@@ -33,13 +56,19 @@ export const AmmatillinenNäyttöEditor = ({model}) => {
 
   let sections = []
   if (data !== undefined) {
-    sections.push(<YksittäinenNäyttöEditor wm={wrappedModel}/>)
+    sections.push(<YksittäinenNäyttöEditor edit={edit} wm={wrappedModel}/>)
   }
 
   return (
     <div>
+      {popupVisibleA.map(visible => visible
+        ? <UusiNäyttöPopup edit={edit} model={model} doneCallback={() => popupVisibleA.set(false)}/>
+        : '')
+      }
       <ul>{sections}</ul>
-      <a><Text name="Lisää ammattiosaamisen näyttö"/></a>
+      {edit &&
+        <a onClick={() => popupVisibleA.set(true)}><Text name="Lisää ammattiosaamisen näyttö"/></a>
+      }
     </div>
   )
 }
