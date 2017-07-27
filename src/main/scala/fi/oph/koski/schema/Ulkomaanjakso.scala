07956b56974cf64087cb2e94ad5e0165b6965209
juhomikkonen <juho.mikkonen@oph.fi>
2017-07-27 09:47:39 +0300

    Update Ulkomaanjakso.scala

--- src/main/scala/fi/oph/koski/schema/Ulkomaanjakso.scala
+++ src/main/scala/fi/oph/koski/schema/Ulkomaanjakso.scala
@@ -7,9 +7,7 @@ import fi.oph.scalaschema.annotation.Description
 
 @Description("Opintoihin liittyvien ulkomaanjaksojen tiedot. Ulkomaanjakson tiedot sisältävät alku- ja loppupäivämäärät, sekä tiedon siitä, missä maassa jakso on suoritettu.")
 case class Ulkomaanjakso(
-  @Description("Jakson alkamispäivämäärä. Muoto YYYY-MM-DD")
   alku: LocalDate,
-  @Description("Jakson loppumispäivämäärä. Muoto YYYY-MM-DD")
   loppu: Option[LocalDate],
   @Description("Missä maassa jakso on suoritettu")
   @KoodistoUri("maatjavaltiot2")
