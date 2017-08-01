b16b449fc23ab8878e868b179835c88ae9899b61
Juha Paananen <juha.paananen@gmail.com>
2017-08-01 16:39:33 +0300

    remove dead code

--- web/app/editor/Suoritus.js
+++ web/app/editor/Suoritus.js
@@ -7,7 +7,6 @@ export const suoritusValmis = (suoritus) => suorituksenTila(suoritus) === 'VALMI
 export const suoritusKesken = (suoritus) => suorituksenTila(suoritus) === 'KESKEN'
 export const suorituksenTila = (suoritus) => modelData(suoritus, 'tila').koodiarvo
 export const hasArvosana = (suoritus) => !!modelData(suoritus, 'arviointi.-1.arvosana')
-export const arvosanaLens = modelLens('arviointi.-1.arvosana')
 export const arviointiPuuttuu = (m) => !m.value.classes.includes('arvioinniton') && !hasArvosana(m)
 export const lastArviointiLens = modelLens('arviointi.-1')
 export const tilaLens = modelLens('tila')
