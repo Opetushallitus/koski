dcfe04bf3b149f34b76d8d7880ced01a12cc18c5
Kalle Kainu <kalle.kainu@reaktor.fi>
2017-08-08 15:35:27 +0300

    Don't use production NODE_ENV when running webpack --watch

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -43,11 +43,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
     new CopyWebpackPlugin(
       [
         { from: 'static'},
