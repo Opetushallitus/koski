81610b60351d54f259b930e5dff9e6ca2685043c
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-08-10 11:40:34 +0300

    Opiskeluoikeus oid constraints

--- src/main/resources/db/migration/V38__add_opiskeluoikeus_oid.sql
+++ src/main/resources/db/migration/V38__add_opiskeluoikeus_oid.sql
@@ -1,3 +1,5 @@
-ALTER TABLE opiskeluoikeus ADD oid TEXT UNIQUE;
+ALTER TABLE opiskeluoikeus ADD oid TEXT NOT NULL UNIQUE;
 ALTER TABLE opiskeluoikeus DROP COLUMN sisaltava_opiskeluoikeus_id CASCADE;
 ALTER TABLE opiskeluoikeus ADD COLUMN sisaltava_opiskeluoikeus_oid TEXT;
+ALTER TABLE opiskeluoikeus DROP CONSTRAINT no_id_in_json;
+ALTER TABLE opiskeluoikeus ADD CONSTRAINT oid_matches_oid_in_json CHECK (oid = (data->>'oid'));
