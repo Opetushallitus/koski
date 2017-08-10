bb607a44d77ae1fb460c41853c33f89646ead2b7
Juha Paananen <juha.paananen@gmail.com>
2017-08-10 11:27:11 +0300

    Revert "Don't use production NODE_ENV when running webpack --watch"
    This reverts commit dcfe04bf3b149f34b76d8d7880ced01a12cc18c5.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -43,6 +43,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new CopyWebpackPlugin(
       [
         { from: 'static'},
