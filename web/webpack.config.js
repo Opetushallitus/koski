3a7772c2f0777c202460478132019bdc749eaf11
Kalle Kainu <kalle.kainu@reaktor.fi>
2017-08-11 13:18:47 +0300

    Don't use production build when running make watch

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -63,11 +63,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
     new CopyWebpackPlugin(
       [
         {from: 'static'},
