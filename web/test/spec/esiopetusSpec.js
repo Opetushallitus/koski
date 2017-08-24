6cf61b1b3c3202846116333e9a770a340ccc417e
Juha Paananen <juha.paananen@gmail.com>
2017-08-24 17:31:07 +0300

    simplify structure

--- web/test/spec/esiopetusSpec.js
+++ web/test/spec/esiopetusSpec.js
@@ -23,37 +23,33 @@ describe('Esiopetus', function() {
   })
 
   describe('Opiskeluoikeuden lisääminen', function() {
-    describe('Esiopetus', function() {
-      before(prepareForNewOppija('kalle', '230872-7258'))
+    before(prepareForNewOppija('kalle', '230872-7258'))
 
-      describe('Aluksi', function() {
-        it('Lisää-nappi on disabloitu', function() {
-          expect(addOppija.isEnabled()).to.equal(false)
+    describe('Aluksi', function() {
+      it('Lisää-nappi on disabloitu', function() {
+        expect(addOppija.isEnabled()).to.equal(false)
+      })
+    })
+
+    describe('Kun syötetään validit tiedot', function() {
+      before(addOppija.enterValidDataEsiopetus({suorituskieli: 'ruotsi'}))
+
+      describe('Käyttöliittymän tila', function() {
+        it('Lisää-nappi on enabloitu', function() {
+          expect(addOppija.isEnabled()).to.equal(true)
         })
       })
 
-      describe('Kun syötetään validit tiedot', function() {
-        before(addOppija.enterValidDataEsiopetus({suorituskieli: 'ruotsi'}))
+      describe('Kun painetaan Lisää-nappia', function() {
+        before(
+          addOppija.submitAndExpectSuccess('Tyhjä, Tero (230872-7258)', 'Peruskoulun esiopetus')
+        )
 
-        describe('Käyttöliittymän tila', function() {
-          it('Lisää-nappi on enabloitu', function() {
-            expect(addOppija.isEnabled()).to.equal(true)
-          })
-        })
-
-        describe('Kun painetaan Lisää-nappia', function() {
-          before(addOppija.submitAndExpectSuccess('Tyhjä, Tero (230872-7258)', 'Peruskoulun esiopetus'))
-
-          it('lisätty oppija näytetään', function() {})
-
-          describe('Käyttöliittymän tila', function() {
-            it('Lisätty opiskeluoikeus näytetään', function() {
-              expect(opinnot.getTutkinto()).to.equal('Peruskoulun esiopetus')
-              expect(opinnot.getOppilaitos()).to.equal('Jyväskylän normaalikoulu')
-              expect(opinnot.getSuorituskieli()).to.equal('ruotsi')
-              expect(editor.propertyBySelector('.diaarinumero').getValue()).to.equal('102/011/2014')
-            })
-          })
+        it('lisätty oppija näytetään', function() {
+          expect(opinnot.getTutkinto()).to.equal('Peruskoulun esiopetus')
+          expect(opinnot.getOppilaitos()).to.equal('Jyväskylän normaalikoulu')
+          expect(opinnot.getSuorituskieli()).to.equal('ruotsi')
+          expect(editor.propertyBySelector('.diaarinumero').getValue()).to.equal('102/011/2014')
         })
       })
     })
