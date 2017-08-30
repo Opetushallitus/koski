e7e40ad6875db4d69536137ef764953e4087cf80
Juha Paananen <juha.paananen@gmail.com>
2017-08-30 09:09:33 +0300

    more efficient serializer for EditorModel (2x faster than before)

--- src/main/scala/fi/oph/koski/editor/EditorModelSerializer.scala
+++ src/main/scala/fi/oph/koski/editor/EditorModelSerializer.scala
@@ -21,25 +21,60 @@ object EditorPropertySerializer extends Serializer[EditorProperty] {
 object EditorModelSerializer extends Serializer[EditorModel] with Logging {
   override def deserialize(implicit format: Formats) = PartialFunction.empty
 
+  private def propsToFields(props: Map[String, Any])(implicit format: Formats) = props.toList.map{ case (key, value) => JField(key, Extraction.decompose(value)) }
+
   override def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
     case (model: EditorModel) => {
       model match {
         case (ObjectModel(c, properties, title, editable, prototypes, props)) =>
-          json("object", "value" -> Map("classes" -> c, "title" -> title, "properties" -> properties), "editable" -> editable, "prototypes" -> (if (prototypes.nonEmpty) { prototypes} else {JNothing})).merge(json(props))
-        case (PrototypeModel(key, props)) => json("prototype", "key" -> key).merge(json(props))
+          val protos = if (prototypes.nonEmpty) { JObject(prototypes.toList.map { case (key, model) => JField(key, serialize(format)(model)) }) } else { JNothing }
+          JObject(List(
+            JField("type", JString("object")),
+            JField("value", JObject(
+              JField("classes", JArray(c.map(JString(_)))),
+              JField("title", title.map(JString(_)).getOrElse(JNothing)),
+              JField("properties", JArray(properties.map{ case EditorProperty(key, title, model, props) =>
+                JObject(List(
+                  JField("key", JString(key)),
+                  JField("title", JString(title)),
+                  JField("model", serialize(format)(model))
+                ) ++ propsToFields(props))
+              }))
+            )),
+            JField("editable", JBool(editable)),
+            JField("prototypes", protos)
+          ) ++ propsToFields(props))
+        case (PrototypeModel(key, props)) =>
+          JObject(
+            List(
+              JField("type", JString("prototype")),
+              JField("key", JString(key))
+            ) ++ propsToFields(props)
+          )
         case (OptionalModel(model, prototype, props)) =>
-          val optionalInfo: JValue = json("optional" -> true, "optionalPrototype" -> prototype)
+          val optionalInfo: JValue = JObject(
+            JField("optional", JBool(true)),
+            JField("optionalPrototype", prototype.map(p => serialize(format)(p)).getOrElse(JNothing))
+          )
+
           val typeAndValue = modelOrEmptyObject(model)
-          typeAndValue.merge(optionalInfo).merge(json(props))
+          typeAndValue.merge(optionalInfo).merge(JObject(propsToFields(props)))
 
         case (ListModel(items, prototype, props)) =>
-          json("array", "value" -> items, "arrayPrototype" -> prototype).merge(json(props))
+          JObject(List(
+            JField("type", JString("array")),
+            JField("value", JArray(items.map(item => serialize(format)(item)))),
+            JField("arrayPrototype", prototype.map(p => serialize(format)(p)).getOrElse(JNothing))
+          ) ++ propsToFields(props))
+
         case (EnumeratedModel(value, alternatives, path, props)) =>
           json("enum", "alternatives" -> alternatives, "alternativesPath" -> path, "value" -> value).merge(json(props))
         case (OneOfModel(c, model, prototypes, props)) =>
-          val oneOfInfo: JValue = json("oneOfClass" -> c, "oneOfPrototypes" -> prototypes)
-          Extraction.decompose(model).merge(oneOfInfo).merge(json(props))
-
+          val oneOfInfo = JObject(
+            JField("oneOfClass", JString(c)),
+            JField("oneOfPrototypes", JArray(prototypes.map(p => serialize(format)(p))))
+          )
+          serialize(format)(model).merge(oneOfInfo).merge(JObject(propsToFields(props)))
         case (NumberModel(value, props)) => json("number", "value" -> value).merge(json(props))
         case (BooleanModel(value, props)) => json("boolean", "value" -> value).merge(json(props))
         case (DateModel(value, props)) => json("date", "value" -> value).merge(json(props))
@@ -49,7 +84,7 @@ object EditorModelSerializer extends Serializer[EditorModel] with Logging {
     }
   }
 
-  private def modelOrEmptyObject(model: Option[EditorModel])(implicit format: Formats) = model.map(Extraction.decompose).getOrElse(emptyObject)
+  private def modelOrEmptyObject(model: Option[EditorModel])(implicit format: Formats) = model.map(serialize(format)(_)).getOrElse(emptyObject)
 
   private def json(tyep: String, props: (String, Any)*)(implicit format: Formats): JValue = {
     val elems: List[(String, Any)] = ("type" -> tyep) :: props.toList
--- src/test/scala/fi/oph/koski/editor/EditorPerfTester.scala
+++ src/test/scala/fi/oph/koski/editor/EditorPerfTester.scala
@@ -3,6 +3,7 @@ package fi.oph.koski.editor
 import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.documentation.AmmatillinenPerustutkintoExample
 import fi.oph.koski.henkilo.MockOppijat
+import fi.oph.koski.json.Json
 import fi.oph.koski.koskiuser.KoskiSession
 import fi.oph.koski.schema.Oppija
 import fi.oph.koski.util.Timing
@@ -10,12 +11,25 @@ import fi.oph.koski.util.Timing
 object EditorPerfTester extends App with Timing {
   implicit val application = KoskiApplication.apply
   implicit val user = KoskiSession.systemUser
-  round(0)
 
-  val rounds = 1000
-  timed(s"$rounds rounds") {
-    (1 to rounds).foreach(round)
+  lazy val prebuiltModel = buildModel
+  runTest(TestCase("build model", 10, (n) => buildModel))
+  runTest(TestCase("serialize model", 10, (n) => Json.write(prebuiltModel)))
+
+
+  def runTest(testCase: TestCase) = {
+    testCase.round(0)
+    timed(s"${testCase.rounds} rounds ${testCase.name}") {
+      (1 to testCase.rounds).foreach(testCase.round)
+    }
   }
 
-  def round(n: Int) = OppijaEditorModel.toEditorModel(Oppija(MockOppijat.eero, AmmatillinenPerustutkintoExample.perustutkinto.opiskeluoikeudet), true)
+  private def buildModel = {
+    OppijaEditorModel.toEditorModel(Oppija(MockOppijat.eero, AmmatillinenPerustutkintoExample.perustutkinto.opiskeluoikeudet), true)
+  }
+
 }
+
+
+
+case class TestCase(name: String, rounds: Int, round: (Int) => Unit)
