cb7b6526dbe955755c7b4b12ab720f5f153a3cd2
Juha Paananen <juha.paananen@gmail.com>
2017-09-20 14:50:07 +0300

    extract SuoritusTabs.jsx

--- web/app/editor/OpiskeluoikeusEditor.jsx
+++ web/app/editor/OpiskeluoikeusEditor.jsx
@@ -1,22 +1,17 @@
 import React from 'baret'
-import Atom from 'bacon.atom'
 import R from 'ramda'
 import {addContext, modelData, modelItems, modelLookup, modelTitle} from './EditorModel.js'
 import {TogglableEditor} from './TogglableEditor.jsx'
 import {PropertiesEditor} from './PropertiesEditor.jsx'
-import {onLopputilassa, OpiskeluoikeudenTilaEditor} from './OpiskeluoikeudenTilaEditor.jsx'
+import {OpiskeluoikeudenTilaEditor} from './OpiskeluoikeudenTilaEditor.jsx'
 import Versiohistoria from '../Versiohistoria.jsx'
-import Link from '../Link.jsx'
-import {currentLocation} from '../location.js'
 import {yearFromIsoDateString} from '../date'
 import {ExpandablePropertiesEditor} from './ExpandablePropertiesEditor.jsx'
-import UusiPerusopetuksenSuoritusPopup from './UusiPerusopetuksenSuoritusPopup.jsx'
 import {Editor} from './Editor.jsx'
 import {navigateTo} from '../location'
-import {pushModel} from './EditorModel'
-import {suorituksenTyyppi, suoritusValmis} from './Suoritus'
+import {suorituksenTyyppi, suoritusTitle} from './Suoritus'
 import Text from '../Text.jsx'
-import {isPerusopetuksenOppimäärä, luokkaAste} from './Perusopetus'
+import {assignTabNames, suoritusIndex, SuoritusTabs, urlForTab} from './SuoritusTabs.jsx'
 
 export const OpiskeluoikeusEditor = ({model}) => {
   let oid = modelData(model, 'oid')
@@ -26,12 +21,12 @@ export const OpiskeluoikeusEditor = ({model}) => {
     let suoritukset = modelItems(mdl, 'suoritukset')
     assignTabNames(suoritukset)
     let excludedProperties = ['suoritukset', 'alkamispäivä', 'arvioituPäättymispäivä', 'päättymispäivä', 'oppilaitos', 'lisätiedot']
-    var suoritusIndex = SuoritusTabs.suoritusIndex(mdl, suoritukset)
-    if (suoritusIndex < 0 || suoritusIndex >= suoritukset.length) {
-      navigateTo(SuoritusTabs.urlForTab(mdl, suoritukset[0].tabName))
+    var index = suoritusIndex(mdl, suoritukset)
+    if (index < 0 || index >= suoritukset.length) {
+      navigateTo(urlForTab(mdl, suoritukset[0].tabName))
       return null
     }
-    let valittuSuoritus = suoritukset[suoritusIndex]
+    let valittuSuoritus = suoritukset[index]
 
     return (
       <div className="opiskeluoikeus">
@@ -102,78 +97,6 @@ const OpiskeluoikeudenVoimassaoloaika = ({opiskeluoikeus}) => {
   </div>)
 }
 
-const assignTabNames = (suoritukset) => {
-  suoritukset = R.reverse(suoritukset) // they are in reverse chronological-ish order
-  let tabNamesInUse = {}
-  for (var i in suoritukset) {
-    let suoritus = suoritukset[i]
-    if (suoritus.tabName) {
-      tabNamesInUse[suoritus.tabName] = true
-    }
-  }
-  for (var i in suoritukset) {
-    let suoritus = suoritukset[i]
-    if (!suoritus.tabName) {
-      let tabName = modelTitle(suoritus, 'koulutusmoduuli.tunniste')
-      while (tabNamesInUse[tabName]) {
-        tabName += '-2'
-      }
-      tabNamesInUse[tabName] = true
-      suoritus.tabName = tabName
-    }
-  }
-}
-
-const SuoritusTabs = ({ model, suoritukset }) => {
-  let addingAtom = Atom(false)
-  let uusiSuoritusCallback = (suoritus) => {
-    if (suoritus) {
-      pushModel(suoritus, model.context.changeBus)
-      let suoritukset2 = [suoritus].concat(suoritukset)
-      assignTabNames(suoritukset2) // to get the correct tab name for the new suoritus
-      navigateTo(SuoritusTabs.urlForTab(model, suoritus.tabName))
-    } else {
-      addingAtom.set(false)
-    }
-  }
-  let tabTitle = (suoritusModel) => suorituksenTyyppi(suoritusModel) == 'perusopetuksenoppimaara' ? <Text name="Päättötodistus"/> : suoritusTitle(suoritusModel)
-
-  return (<ul className="suoritus-tabs">
-    {
-      suoritukset.map((suoritusModel, i) => {
-        let selected = i === SuoritusTabs.suoritusIndex(model, suoritukset)
-        let titleEditor = tabTitle(suoritusModel)
-        return (<li className={selected ? 'selected': null} key={i}>
-          { selected ? titleEditor : <Link href={ SuoritusTabs.urlForTab(model, suoritusModel.tabName) } exitHook={false}> {titleEditor} </Link>}
-        </li>)
-      })
-    }
-    {
-      model.context.edit && !onLopputilassa(model) && UusiPerusopetuksenSuoritusPopup.canAddSuoritus(model) && (
-        <li className="add-suoritus"><a onClick={() => { addingAtom.modify(x => !x) }}><span className="plus">{''}</span>{UusiPerusopetuksenSuoritusPopup.addSuoritusTitle(model)}</a></li>
-      )
-    }
-    {
-      addingAtom.map(adding => adding && <UusiPerusopetuksenSuoritusPopup opiskeluoikeus={model} resultCallback={uusiSuoritusCallback}/>)
-    }
-  </ul>
-)}
-
-SuoritusTabs.urlForTab = (model, i) => currentLocation().addQueryParams({[SuoritusTabs.suoritusQueryParam(model.context)]: i}).toString()
-SuoritusTabs.suoritusQueryParam = context => (modelData(context.opiskeluoikeus, 'oid') || context.opiskeluoikeusIndex) + '.suoritus'
-SuoritusTabs.suoritusIndex = (model, suoritukset) => {
-  let paramName = SuoritusTabs.suoritusQueryParam(model.context)
-  let selectedTabName = currentLocation().params[paramName]
-  let index = suoritukset.map(s => s.tabName).indexOf(selectedTabName)
-  if (index < 0) {
-    index = suoritukset.findIndex(s => luokkaAste(s) || (isPerusopetuksenOppimäärä(s) && suoritusValmis(s) ))
-    if (index < 0) index = 0
-    selectedTabName = suoritukset[index].tabName
-    let newLocation = currentLocation().addQueryParams({ [paramName]: selectedTabName }).toString()
-    history.replaceState(null, null, newLocation)
-  }
-  return index
-}
 class OpiskeluoikeudenOpintosuoritusoteLink extends React.Component {
   render() {
     let {opiskeluoikeus} = this.props
@@ -210,13 +133,4 @@ export const näytettävätPäätasonSuoritukset = (opiskeluoikeus) => {
     return { title, suoritukset }
   })
   return grouped
-}
-
-const suoritusTitle = (suoritus) => {
-  let title = modelTitle(suoritus, 'koulutusmoduuli.tunniste')
-  switch(suorituksenTyyppi(suoritus)) {
-    case 'ammatillinentutkintoosittainen': return `${title}, osittainen` // TODO: i18n
-    case 'aikuistenperusopetuksenoppimaara': return modelTitle(suoritus, 'tyyppi')
-    default: return title
-  }
 }
