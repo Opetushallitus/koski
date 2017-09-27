9df337b7da9e17710670a2361bd9cb942aac8dcc
Juha Paananen <juha.paananen@gmail.com>
2017-09-27 15:35:17 +0300

    order by key, locale

--- src/test/scala/fi/oph/koski/localization/LocalizationMockDataUpdater.scala
+++ src/test/scala/fi/oph/koski/localization/LocalizationMockDataUpdater.scala
@@ -2,10 +2,14 @@ package fi.oph.koski.localization
 
 import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.json.JsonFiles
-import org.json4s.JValue
+import fi.oph.koski.json.JsonSerializer.extract
+import org.json4s.{JArray, JObject, JValue}
 
 object LocalizationMockDataUpdater extends App {
-  val localizations: JValue = KoskiApplication.apply(KoskiApplication.defaultConfig).localizationRepository.fetchLocalizations
+  val JArray(localizations) = KoskiApplication.apply(KoskiApplication.defaultConfig).localizationRepository.fetchLocalizations.asInstanceOf[JArray]
+  val sorted: JValue = JArray(localizations.sortBy {
+    case j: JObject => (extract[String](j \ "key"), extract[String](j \ "locale"))
+  })
   val filename = "src/main/resources" + MockLocalizationRepository.resourceName
-  JsonFiles.writeFile(filename, localizations)
+  JsonFiles.writeFile(filename, sorted)
 }
