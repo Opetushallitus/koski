8f10969cb7b3a634e5bc7c84bdb701e9dd1acccb
Juha Paananen <juha.paananen@gmail.com>
2017-09-27 09:54:19 +0300

    oppiaine- ja kurssitaulukon tyylitystä

--- web/app/editor/PerusopetuksenOppiaineRowEditor.jsx
+++ web/app/editor/PerusopetuksenOppiaineRowEditor.jsx
@@ -71,7 +71,7 @@ export class PerusopetuksenOppiaineRowEditor extends React.Component {
     {
       expanded && <tr key='details'><td colSpan="4" className="details"><PropertiesEditor context={model.context} properties={extraProperties} /></td></tr>
     }
-    <tr className="kurssit"><td colSpan="4"><KurssitEditor model={model}/></td></tr>
+    <tr className="kurssit"><td><KurssitEditor model={model}/></td></tr>
     {
       modelErrorMessages(model).map((error, i) => <tr key={'error-' + i} className="error"><td colSpan="42" className="error">{error}</td></tr>)
     }
