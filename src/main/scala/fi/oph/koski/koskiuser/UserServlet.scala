3e05cf7642f24915ebcb44aa44bd8d68945efc50
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-10-12 11:10:13 +0300

    Piilota validointilinkki peruskäyttäjiltä

--- src/main/scala/fi/oph/koski/koskiuser/UserServlet.scala
+++ src/main/scala/fi/oph/koski/koskiuser/UserServlet.scala
@@ -4,8 +4,12 @@ import fi.oph.koski.servlet.{ApiServlet, NoCache}
 
 class UserServlet(implicit val application: UserAuthenticationContext) extends ApiServlet with AuthenticationSupport with NoCache {
   get("/") {
-    renderEither(getUser.right.map(user => UserWithAccessRights(user.name, koskiSessionOption.map(_.hasAnyWriteAccess).getOrElse(false), koskiSessionOption.map(_.hasLocalizationWriteAccess).getOrElse(false))))
+    renderEither(getUser.right.map { user =>
+      koskiSessionOption.map { session =>
+        UserWithAccessRights(user.name, session.hasAnyWriteAccess, session.hasLocalizationWriteAccess, session.hasGlobalReadAccess)
+      }.getOrElse(UserWithAccessRights(user.name))
+    })
   }
 }
 
-case class UserWithAccessRights(name: String, hasWriteAccess: Boolean, hasLocalizationWriteAccess: Boolean)
+case class UserWithAccessRights(name: String, hasWriteAccess: Boolean = false, hasLocalizationWriteAccess: Boolean = false, hasGlobalReadAccess: Boolean = false)
--- web/app/TopBar.jsx
+++ web/app/TopBar.jsx
@@ -7,23 +7,23 @@ import {OpiskeluoikeusInvalidatedMessage} from './OpiskeluoikeusInvalidation.jsx
 
 export const TopBar = ({user, titleKey, inRaamit, location}) => {
   return (inRaamit
-    ? <RaamitTopBar location={location}/>
+    ? <RaamitTopBar location={location} user={user}/>
     : <LocalTopBar location={location} user={user} titleKey={titleKey}/>
   )
 }
 
-const NavList = ({location}) => {
+const NavList = ({location, user}) => {
   return (<ul>
     <li>{naviLink('/koski/', 'Opiskelijat', location.path, '', (path, loc) => loc == path || loc.startsWith('/koski/oppija'))}</li>
     <li>{naviLink('/koski/tiedonsiirrot', 'Tiedonsiirrot', location.path, '', (path, loc) => loc.startsWith(path))}</li>
-    <li>{naviLink('/koski/validointi', 'Validointi', location.path, '')}</li>
+    {user.hasGlobalReadAccess && <li>{naviLink('/koski/validointi', 'Validointi', location.path, '')}</li>}
     <li>{naviLink('/koski/documentation', 'Dokumentaatio', location.path, '')}</li>
   </ul>)
 }
 
-const RaamitTopBar = ({location}) => {
+const RaamitTopBar = ({location, user}) => {
   return (<header id="topbar" className="inraamit topbarnav">
-    <NavList location={location}/>
+    <NavList location={location} user={user}/>
     {location.path === '/koski/' && <OpiskeluoikeusInvalidatedMessage/>}
   </header>)
 }
@@ -40,7 +40,7 @@ const LocalTopBar = ({location, user, titleKey}) => {
       <UserInfo user={user}/>
       {(user !== null) &&
         <div className='topbarnav'>
-          <NavList location={location}/>
+          <NavList location={location} user={user}/>
           {location.path === '/koski/' && <OpiskeluoikeusInvalidatedMessage/>}
         </div>
       }
