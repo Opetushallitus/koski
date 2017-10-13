5fb7e006cb3a0692e95d320151917cd0496c6d02
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-10-13 15:15:55 +0300

    Cleanup

--- web/app/editor/PerusopetuksenOppiaineenOppimaaranSuoritusEditor.jsx
+++ web/app/editor/PerusopetuksenOppiaineenOppimaaranSuoritusEditor.jsx
@@ -5,7 +5,7 @@ import {modelItems} from './EditorModel'
 import {arvioituTaiVahvistettu} from './Suoritus'
 
 export default ({model}) => {
-  let suorituksiaTehty = modelItems(model, 'osasuoritukset').filter(arvioituTaiVahvistettu).length > 0;
+  let suorituksiaTehty = modelItems(model, 'osasuoritukset').filter(arvioituTaiVahvistettu).length > 0
   return (<div className="kurssit">
     {(model.context.edit || suorituksiaTehty) && <h5><Text name="Kurssit"/></h5>}
     <KurssitEditor model={model}/>
