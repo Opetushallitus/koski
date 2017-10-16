d06b6568c9dbd1a02791777bedee6ab62683a7b0
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-10-16 14:40:19 +0300

    Lisää OpiskeluoikeusValidationSpec

---
+++ src/test/scala/fi/oph/koski/api/OpiskeluoikeusValidationSpec.scala
@@ -0,0 +1,35 @@
+package fi.oph.koski.api
+
+import java.time.LocalDate
+
+import fi.oph.koski.KoskiApplicationForTests
+import fi.oph.koski.henkilo.{MockOppijat, VerifiedHenkilöOid}
+import fi.oph.koski.json.JsonSerializer.parse
+import fi.oph.koski.koskiuser.KoskiSession
+import fi.oph.koski.opiskeluoikeus.ValidationResult
+import org.scalatest.{FreeSpec, Matchers}
+
+class OpiskeluoikeusValidationSpec extends FreeSpec with Matchers with OpiskeluoikeusTestMethods with LocalJettyHttpSpecification {
+  implicit val session: KoskiSession = KoskiSession.systemUser
+
+  "Validoi" - {
+    "validi opiskeluoikeus" in {
+      val opiskeluoikeusOid = oppija(MockOppijat.eero.oid).tallennettavatOpiskeluoikeudet.flatMap(_.oid).head
+      authGet(s"api/opiskeluoikeus/validate/$opiskeluoikeusOid") {
+        verifyResponseStatus(200)
+        validationResult.errors should be(empty)
+      }
+    }
+
+    "epävalidi opiskeluoikeus" in {
+      val opiskeluoikeus = oppija(MockOppijat.eero.oid).tallennettavatOpiskeluoikeudet.head.withPäättymispäivä(LocalDate.now)
+      KoskiApplicationForTests.opiskeluoikeusRepository.createOrUpdate(VerifiedHenkilöOid(MockOppijat.eero.henkilö), opiskeluoikeus, allowUpdate = true)
+      authGet(s"api/opiskeluoikeus/validate/${opiskeluoikeus.oid.get}") {
+        verifyResponseStatus(200)
+        validationResult.errors.map(_.key) should equal(List("badRequest.validation.date.päättymispäivämäärä"))
+      }
+    }
+  }
+
+  private def validationResult = parse[ValidationResult](body)
+}
