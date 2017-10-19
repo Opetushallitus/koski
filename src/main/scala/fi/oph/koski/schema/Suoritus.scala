309a64c9ba3e430cde90eab054f7c2c2d9e7751e
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-10-19 14:42:48 +0300

    Cleanup

--- src/main/scala/fi/oph/koski/schema/Suoritus.scala
+++ src/main/scala/fi/oph/koski/schema/Suoritus.scala
@@ -2,7 +2,6 @@ package fi.oph.koski.schema
 
 import java.time.LocalDate
 
-import fi.oph.koski.koodisto.MockKoodistoViitePalvelu
 import fi.oph.koski.localization.LocalizedString
 import fi.oph.koski.localization.LocalizedString.unlocalized
 import fi.oph.scalaschema.annotation._
