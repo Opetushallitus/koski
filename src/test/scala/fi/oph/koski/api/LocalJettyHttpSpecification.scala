6740fff04008f2e7dc626537b690743a693f7b70
Juha Paananen <juha.paananen@gmail.com>
2017-10-25 14:26:07 +0300

    add test.externalJettyPort system property for testing against local jetty

--- src/test/scala/fi/oph/koski/api/LocalJettyHttpSpecification.scala
+++ src/test/scala/fi/oph/koski/api/LocalJettyHttpSpecification.scala
@@ -2,9 +2,10 @@ package fi.oph.koski.api
 
 import fi.oph.koski.KoskiApplicationForTests
 import fi.oph.koski.http.HttpSpecification
-import fi.oph.koski.jettylauncher.SharedJetty
+import fi.oph.koski.jettylauncher.{JettyLauncher, SharedJetty}
 import fi.oph.koski.koskiuser.MockUsers
-import fi.oph.koski.log.AuditLogTester
+import fi.oph.koski.log.{AuditLogTester, Logging}
+import fi.oph.koski.util.PortChecker
 
 trait LocalJettyHttpSpecification extends HttpSpecification {
   def refreshElasticSearchIndex = {
@@ -13,20 +14,32 @@ trait LocalJettyHttpSpecification extends HttpSpecification {
 
   override def baseUrl = {
     LocalJettyHttpSpecification.setup(this)
-    SharedJetty.baseUrl
+    LocalJettyHttpSpecification.baseUrl
   }
 
   def defaultUser = MockUsers.kalle
 }
 
-object LocalJettyHttpSpecification {
+object LocalJettyHttpSpecification extends Logging {
+  private lazy val myJetty = externalJettyPort match {
+    case None => SharedJetty
+    case Some(port) =>
+      logger.info(s"Using external jetty on port $port")
+      new JettyLauncher(port)
+  }
+
   var running = false
   def setup(spec: HttpSpecification) = synchronized {
     if (!running) {
       running = true
-      SharedJetty.start
+      if (externalJettyPort.isEmpty) SharedJetty.start
       AuditLogTester.setup
       spec.resetFixtures
     }
   }
+
+  def baseUrl = myJetty.baseUrl
+
+  def externalJettyPort = Option(System.getProperty("test.externalJettyPort")).map(_.toInt)
+  def portForSharedJetty = externalJettyPort.getOrElse(PortChecker.findFreeLocalPort)
 }
