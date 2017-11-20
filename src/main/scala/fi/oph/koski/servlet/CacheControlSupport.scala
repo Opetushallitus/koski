2ddf7effad964884276bed5ccdd5bf7455e14fde
Juha Paananen <juha.paananen@gmail.com>
2017-11-20 10:08:02 +0200

    Refactor CacheControlSupport trait

--- src/main/scala/fi/oph/koski/servlet/CacheControlSupport.scala
+++ src/main/scala/fi/oph/koski/servlet/CacheControlSupport.scala
@@ -5,34 +5,26 @@ import org.scalatra.ScalatraServlet
 import scala.concurrent.duration._
 
 trait CacheControlSupport extends ScalatraServlet {
-  def noCache = {
-    addHeaders(Map( "Cache-Control" -> "no-store, no-cache, must-revalidate", "Pragma" -> "no-cache" ))
-  }
+  def getCacheHeaders: Map[String, String]
 
-  def cacheForUserSession = {
-    addHeaders(Map("Vary" -> "Cookie", "Cache-Control" -> "private"))
+  before() {
+    for ( ( key, value ) <- getCacheHeaders ) response.addHeader( key, value )
   }
-
-  def cacheMaxAge(duration: Duration) = {
-    addHeaders(Map( "Cache-Control" -> ( "max-age=" + duration.toSeconds ) ))
-  }
-
-  private def addHeaders(headers: Map[String, String]) = for ( ( key, value ) <- headers ) response.addHeader( key, value )
 }
 
 trait NoCache extends CacheControlSupport {
-  before() {
-    noCache
-  }
+  def getCacheHeaders = Map( "Cache-Control" -> "no-store, no-cache, must-revalidate", "Pragma" -> "no-cache" )
 }
 
 trait Cached extends CacheControlSupport {
   def cacheDuration: Duration
-  before() {
-    cacheMaxAge(cacheDuration)
-  }
+  def getCacheHeaders = Map( "Cache-Control" -> ( "max-age=" + cacheDuration.toSeconds ) )
 }
 
 trait Cached24Hours extends Cached {
   def cacheDuration = 24 hours
+}
+
+trait CacheForUserSession extends CacheControlSupport {
+  def getCacheControlHeaders = Map("Vary" -> "Cookie", "Cache-Control" -> "private")
 }
