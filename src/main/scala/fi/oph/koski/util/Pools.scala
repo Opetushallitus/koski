4bb2aa40213a9625e9dd50c738e3a066490bf0cf
Juha Paananen <juha.paananen@gmail.com>
2017-11-28 09:21:40 +0200

    export threadpool status to JMX

--- src/main/scala/fi/oph/koski/util/Pools.scala
+++ src/main/scala/fi/oph/koski/util/Pools.scala
@@ -1,6 +1,8 @@
 package fi.oph.koski.util
 
+import java.lang.management.ManagementFactory
 import java.util.concurrent._
+import javax.management.ObjectName
 
 import scala.concurrent.ExecutionContext
 
@@ -15,12 +17,12 @@ object Pools {
   val httpThreads = jettyThreads
   val httpPool = NamedThreadPool("http4s-blaze-client", httpThreads)
   val dbThreads = 20
-  val globalExecutor = ExecutionContext.fromExecutor(new ThreadPoolExecutor(Pools.globalExecutionContextThreads, Pools.globalExecutionContextThreads, 60, TimeUnit.SECONDS, new ArrayBlockingQueue[Runnable](1000)))
-  val backgroundExecutor = ExecutionContext.fromExecutor(new ThreadPoolExecutor(0, Pools.backgroundExecutionContextThreads, 60, TimeUnit.SECONDS, new ArrayBlockingQueue[Runnable](1000)))
+  val globalExecutor = ManagedExecutionContext.fromExecutor("globalPool", new ThreadPoolExecutor(Pools.globalExecutionContextThreads, Pools.globalExecutionContextThreads, 60, TimeUnit.SECONDS, new ArrayBlockingQueue[Runnable](1000)))
+  val backgroundExecutor = ManagedExecutionContext.fromExecutor("backgroundPool", new ThreadPoolExecutor(0, Pools.backgroundExecutionContextThreads, 60, TimeUnit.SECONDS, new ArrayBlockingQueue[Runnable](1000)))
 }
 
 object NamedThreadPool {
-  def apply(name: String, size: Int) = Executors.newFixedThreadPool(size, threadFactory(name))
+  def apply(name: String, size: Int) = ThreadPoolExecutorMXBean.register(name, Executors.newFixedThreadPool(size, threadFactory(name)).asInstanceOf[ThreadPoolExecutor])
 
   private def threadFactory(namePrefix: String) = new ThreadFactory {
     val defaultThreadFactory = Executors.defaultThreadFactory()
@@ -35,5 +37,43 @@ object NamedThreadPool {
       t.setDaemon(true)
       t
     }
+  }
+}
+
+trait ThreadPoolMXBean {
+  def getActiveCount: Int
+  def getCompletedTaskCount: Long
+  def getCorePoolSize: Int
+  def getLargestPoolSize: Int
+  def getMaximumPoolSize: Int
+  def getPoolSize: Int
+  def getTaskCount: Long
+  def getQueuedTaskCount: Long
+  def getIdleCount: Int
+}
+
+class ThreadPoolExecutorMXBean(executor: ThreadPoolExecutor) extends ThreadPoolMXBean {
+  def getActiveCount = executor.getActiveCount
+  def getCompletedTaskCount = executor.getCompletedTaskCount
+  def getCorePoolSize = executor.getCorePoolSize
+  def getLargestPoolSize = executor.getLargestPoolSize
+  def getMaximumPoolSize = executor.getMaximumPoolSize
+  def getPoolSize = executor.getPoolSize
+  def getTaskCount = executor.getTaskCount
+  def getQueuedTaskCount = executor.getTaskCount - getCompletedTaskCount - getActiveCount
+  def getIdleCount = getPoolSize - getActiveCount
+}
+
+object ThreadPoolExecutorMXBean {
+  private val mbeanServer = ManagementFactory.getPlatformMBeanServer()
+  def register(name: String, executor: ThreadPoolExecutor) = {
+    mbeanServer.registerMBean(new ThreadPoolExecutorMXBean(executor), new ObjectName(s"fi.oph.koski:type=ThreadPool,name=" + name))
+    executor
+  }
+}
+
+object ManagedExecutionContext {
+  def fromExecutor(name: String, executor: ThreadPoolExecutor) = {
+    ExecutionContext.fromExecutor(ThreadPoolExecutorMXBean.register(name, executor))
   }
 }
