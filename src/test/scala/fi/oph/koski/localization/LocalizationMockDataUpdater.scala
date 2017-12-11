a6234e584fa14fde1ae60362257c2aa5345f40a6
Juha Paananen <juha.paananen@gmail.com>
2017-12-11 14:13:45 +0200

    stabilize mock localizations by sorting fields

--- src/test/scala/fi/oph/koski/localization/LocalizationMockDataUpdater.scala
+++ src/test/scala/fi/oph/koski/localization/LocalizationMockDataUpdater.scala
@@ -6,10 +6,20 @@ import fi.oph.koski.json.JsonSerializer.extract
 import org.json4s.{JArray, JObject, JValue}
 
 object LocalizationMockDataUpdater extends App {
-  val JArray(localizations) = KoskiApplication.apply(KoskiApplication.defaultConfig).localizationRepository.fetchLocalizations.asInstanceOf[JArray]
-  val sorted: JValue = JArray(localizations.sortBy {
-    case j: JObject => (extract[String](j \ "key"), extract[String](j \ "locale"))
-  })
   val filename = "src/main/resources" + MockLocalizationRepository.resourceName
+  val JArray(localizations) = KoskiApplication.apply(KoskiApplication.defaultConfig).localizationRepository.fetchLocalizations.asInstanceOf[JArray]
+  //val JArray(existing) = JsonFiles.readFile(filename)
+  val sorted = stabilize(localizations)
   JsonFiles.writeFile(filename, sorted)
+
+  private def stabilize(entries: Seq[JValue]) = {
+    entries
+      .sortBy {
+        case j: JObject => (extract[String](j \ "key"), extract[String](j \ "locale"))
+      }
+      .map {
+        case JObject(fields) => JObject(fields.sortBy(_._1))
+      }
+  }
 }
+
