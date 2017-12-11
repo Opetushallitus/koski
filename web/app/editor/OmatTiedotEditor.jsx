04e9f661477ecd7128e1d522be534fe40e425720
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-12-11 16:48:02 +0200

    Navbar oppilaitoksittain

--- web/app/editor/OmatTiedotEditor.jsx
+++ web/app/editor/OmatTiedotEditor.jsx
@@ -3,19 +3,21 @@ import {addContext, modelData} from './EditorModel.js'
 import {currentLocation} from '../location.js'
 import {OpiskeluoikeusEditor} from './OpiskeluoikeusEditor.jsx'
 import {modelItems, modelLookup} from './EditorModel'
+import OpiskeluoikeudetOppilaitoksittainNavBar from './OpiskeluoikeudetOppilaitoksittainNavBar.jsx'
 
 export const OmatTiedotEditor = ({model}) => {
   let oppijaOid = modelData(model, 'henkilö.oid')
   let selectedOppilaitos = currentLocation().params.oppilaitos
-  let opiskeluoikeudet = modelItems(model, 'opiskeluoikeudet')
+  let oppilaitokset = modelItems(model, 'opiskeluoikeudet')
 
   let selectedIndex = selectedOppilaitos
-    ? opiskeluoikeudet.findIndex(oppilaitos => selectedOppilaitos === modelData(oppilaitos).oid)
+    ? oppilaitokset.findIndex(oppilaitos => selectedOppilaitos === modelData(oppilaitos, 'oppilaitos').oid)
     : 0
 
   let oppilaitos = modelLookup(model, 'opiskeluoikeudet.' + selectedIndex)
   return (
     <div>
+      <OpiskeluoikeudetOppilaitoksittainNavBar {...{ opiskeluoikeudet: oppilaitokset, selectedIndex }}/>
       <ul className="opiskeluoikeuksientiedot">
         {
           modelItems(oppilaitos, 'opiskeluoikeudet').map((opiskeluoikeus, opiskeluoikeusIndex) => {
