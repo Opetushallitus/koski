dcd203d47556652f3d52818d7e9703969d1b1ee8
Juha Paananen <juha.paananen@gmail.com>
2017-12-13 16:29:58 +0200

    fix build: don't evaluate root url when not actually running LocalizationLanguagesTest

--- src/test/scala/fi/oph/koski/localization/LocalizationLanguagesTest.scala
+++ src/test/scala/fi/oph/koski/localization/LocalizationLanguagesTest.scala
@@ -7,20 +7,20 @@ import org.scalatest.{FreeSpec, Matchers, Tag}
   * Tests that Swedish translations exist in the environment defined by the VIRKAILIJA_ROOT environment variable
   */
 class LocalizationLanguagesTest extends FreeSpec with Matchers {
-  val root = sys.env.getOrElse("VIRKAILIJA_ROOT", throw new RuntimeException("Environment variable VIRKAILIJA_ROOT missing"))
-  s"Kielistetyt tekstit ${root}-ympäristössä" - {
-    implicit val cacheManager = GlobalCacheManager
+  "Kielistetyt tekstit" - {
+    implicit lazy val cacheManager = GlobalCacheManager
+    
+    lazy val root = sys.env.getOrElse("VIRKAILIJA_ROOT", throw new RuntimeException("Environment variable VIRKAILIJA_ROOT missing"))
     lazy val remoteLocalizations = new ReadOnlyRemoteLocalizationRepository(root).localizations
-
     lazy val localLocalizations = new MockLocalizationRepository().localizations
 
-    "Suomenkieliset tekstit" taggedAs(LocalizationTestTag) in {
+    s"Suomenkieliset tekstit" taggedAs(LocalizationTestTag) in {
       val missingKeys = localLocalizations.keySet -- remoteLocalizations.keySet
 
       missingKeys shouldBe(empty)
     }
 
-    "Ruotsinkieliset tekstit" taggedAs(LocalizationTestTag) in {
+    s"Ruotsinkieliset tekstit" taggedAs(LocalizationTestTag) in {
       val ignoredKey = (key: String) => key.startsWith("description:")
 
       val missingKeys = localLocalizations.keySet.filterNot(ignoredKey) -- remoteLocalizations.filter(_._2.hasLanguage("sv")).keySet
