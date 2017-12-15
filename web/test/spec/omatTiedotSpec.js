771b36fdeb0b445eb79baa7d49b1bad6448dea64
Juha Paananen <juha.paananen@gmail.com>
2017-12-15 09:43:32 +0200

    add structure to spec

--- web/test/spec/omatTiedotSpec.js
+++ web/test/spec/omatTiedotSpec.js
@@ -3,26 +3,47 @@ describe('Omat tiedot', function() {
   var opinnot = OpinnotPage()
   var authentication = Authentication()
 
-  describe("Kun virkailijalla on opiskeluoikeuksia", function() {
-    before(authentication.login('Oili'), omattiedot.openPage)
-    it('ne näytetään', function() {
-      expect(omattiedot.oppija()).to.equal("Opintosuorituksesi")
-      expect(opinnot.opiskeluoikeudet.oppilaitokset()).to.deep.equal([
-        'Stadin ammattiopisto', 'Jyväskylän normaalikoulu' ])
+  describe("Virkailijana", function() {
+    describe("Kun virkailijalla on opiskeluoikeuksia", function() {
+      before(authentication.login('Oili'), omattiedot.openPage)
+      it('ne näytetään', function() {
+        expect(omattiedot.oppija()).to.equal("Opintosuorituksesi")
+        expect(opinnot.opiskeluoikeudet.oppilaitokset()).to.deep.equal([
+          'Stadin ammattiopisto', 'Jyväskylän normaalikoulu' ])
+      })
+    })
+
+    describe("Kun virkailijalla ei ole opiskeluoikeuksia", function() {
+      before(authentication.login(), omattiedot.openPage)
+      it('näytetään viesti', function() {
+        expect(omattiedot.virhe()).to.equal("Tiedoillasi ei löydy opiskeluoikeuksia")
+      })
     })
   })
 
-  describe("Kun oppijalla ei ole opiskeluoikeuksia", function() {
-    before(authentication.login(), omattiedot.openPage)
-    it('näytetään viesti', function() {
-      expect(omattiedot.virhe()).to.equal("Tiedoillasi ei löydy opiskeluoikeuksia")
-    })
-  })
+  describe("Kansalaisena", function() {
+    var etusivu = LandingPage()
+    var korhopankki = KorhoPankki()
+    before(authentication.logout, etusivu.openPage)
 
-  var etusivu = LandingPage()
-  var korhopankki = KorhoPankki()
-  describe("Kun kansalainen kirjautuu ulos", function() {
-    before(authentication.logout, etusivu.openPage, etusivu.login(), wait.until(korhopankki.isReady), korhopankki.login('290492-9455'), wait.until(omattiedot.isVisible) , click(findSingle('#logout')), wait.until(etusivu.isVisible))
-    it("Näytetään etusivu", function() { })
+    describe("Kun ei olla kirjauduttu sisään", function() {
+      it("Näytetään länderi", function() {
+
+      })
+    })
+
+    describe("Kun kirjaudutaan sisään", function() {
+      before(etusivu.login(), wait.until(korhopankki.isReady), korhopankki.login('290492-9455'), wait.until(omattiedot.isVisible))
+      it("Näytetään opiskeluoikeudet", function() {
+
+      })
+
+      describe("Kun kirjaudutaan ulos", function () {
+        before(click(findSingle('#logout')), wait.until(etusivu.isVisible))
+        it("Näytetään länderi", function() {
+
+        })
+      })
+    })
   })
 })
