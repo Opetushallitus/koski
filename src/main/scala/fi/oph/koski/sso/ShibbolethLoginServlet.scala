8a1b4fd21227bf5a3bbe4ba9fe39d5f62aa4257c
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-12-17 19:00:18 +0200

    Korjaa Shibbolethlogin uudelleenohjaus SharedJetty:ssä

--- src/main/scala/fi/oph/koski/sso/ShibbolethLoginServlet.scala
+++ src/main/scala/fi/oph/koski/sso/ShibbolethLoginServlet.scala
@@ -1,6 +1,6 @@
 package fi.oph.koski.sso
 
-import fi.oph.koski.config.KoskiApplication
+import fi.oph.koski.config.{Environment, KoskiApplication}
 import fi.oph.koski.http.KoskiErrorCategory
 import fi.oph.koski.koskiuser.{AuthenticationSupport, AuthenticationUser, KoskiSession}
 import fi.oph.koski.servlet.{ApiServlet, NoCache}
@@ -12,11 +12,15 @@ case class ShibbolethLoginServlet(application: KoskiApplication) extends ApiServ
         application.henkilöRepository.findOppijat(hetu)(KoskiSession.untrustedUser).headOption match {
           case Some(oppija) =>
             setUser(Right(localLogin(AuthenticationUser(oppija.oid, oppija.oid, s"${oppija.etunimet} ${oppija.sukunimi}", None, kansalainen = true))))
-            redirect(s"${application.config.getString("koski.root.url")}/omattiedot")
+            redirect(s"$rootUrl/omattiedot")
           case _ =>
             haltWithStatus(KoskiErrorCategory.notFound("oppija not found"))
         }
       case _ => haltWithStatus(KoskiErrorCategory.badRequest("nationalidentificationnumber header missing"))
     }
   }
+
+  private def rootUrl =
+    if (Environment.isLocalDevelopmentEnvironment) ""
+    else application.config.getString("koski.root.url")
 }
