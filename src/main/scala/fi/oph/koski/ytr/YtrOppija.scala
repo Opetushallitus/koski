f744f1fd47de404264245d7acd20ce644d2a9fe7
Juha Paananen <juha.paananen@gmail.com>
2017-12-18 14:41:57 +0200

    Format and add some notes and TODOs to YtrOppija

--- src/main/scala/fi/oph/koski/ytr/YtrOppija.scala
+++ src/main/scala/fi/oph/koski/ytr/YtrOppija.scala
@@ -2,6 +2,30 @@ package fi.oph.koski.ytr
 
 import java.time.LocalDate
 
-case class YtrOppija(ssn: String, lastname: String, firstnames: String, graduationDate: Option[LocalDate], graduationPeriod: Option[String], exams: List[YtrExam], graduationSchoolOphOid: Option[String], graduationSchoolYtlNumber: Option[Int])
-case class YtrExam(period: String, examId: String, examNameFi: Option[String], examNameSv: Option[String], examNameEn: Option[String], examRole: Option[String], grade: String, points: Int, sections: List[YtrSection])
-case class YtrSection(sectionId: String, sectionPoints: Int)
+case class YtrOppija(
+  ssn: String,
+  lastname: String,
+  firstnames: String,
+  graduationDate: Option[LocalDate], // Toteutunut valmistumispäivä
+  graduationPeriod: Option[String], // Toteutunut tutkintokerta
+  exams: List[YtrExam],
+  graduationSchoolOphOid: Option[String],
+  graduationSchoolYtlNumber: Option[Int]
+  // TODO: language=fi/sv tutkinnon kieli
+  // TODO: hasCompletedMandatoryExams: Boolean
+)
+case class YtrExam(
+  period: String, // Esim 2013S, TODO: tää pitäis palastella ja kielistää
+  examId: String, // TODO: näistä pitäis päätellä nimet. Ehkä koodistoon?
+  examNameFi: Option[String], // TODO: nimet poistettu rajapinnasta
+  examNameSv: Option[String],
+  examNameEn: Option[String],
+  examRole: Option[String], // TODO: nykyisin examRoleShort, examRoleLegacy
+  grade: String,
+  points: Int,
+  sections: List[YtrSection]
+)
+case class YtrSection(
+ sectionId: String,
+ sectionPoints: Int
+)
