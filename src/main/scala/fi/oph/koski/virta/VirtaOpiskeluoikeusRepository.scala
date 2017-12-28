2f842398b19dd3c5ac118e861cb8c56f1c9119eb
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2017-12-28 13:25:51 +0200

    Korjaa testi

--- src/main/scala/fi/oph/koski/virta/VirtaOpiskeluoikeusRepository.scala
+++ src/main/scala/fi/oph/koski/virta/VirtaOpiskeluoikeusRepository.scala
@@ -1,7 +1,8 @@
 package fi.oph.koski.virta
 
 import fi.oph.koski.cache.{CacheManager, GlobalCacheManager}
-import fi.oph.koski.henkilo.{FindByOid, HenkilöRepository, Hetu, MockOpintopolkuHenkilöRepository}
+import fi.oph.koski.config.Environment.isLocalDevelopmentEnvironment
+import fi.oph.koski.henkilo.{FindByOid, Hetu, MockOpintopolkuHenkilöRepository}
 import fi.oph.koski.koodisto.{KoodistoViitePalvelu, MockKoodistoViitePalvelu}
 import fi.oph.koski.koskiuser.{AccessChecker, SkipAccessCheck}
 import fi.oph.koski.log.Logging
@@ -14,7 +15,7 @@ case class VirtaOpiskeluoikeusRepository(virta: VirtaClient, henkilöRepository:
 
   private val converter = VirtaXMLConverter(oppilaitosRepository, koodistoViitePalvelu)
 
-  override def opiskeluoikeudetByHetu(hetu: String): List[KorkeakoulunOpiskeluoikeus] = Hetu.validate(hetu) match {
+  override def opiskeluoikeudetByHetu(hetu: String): List[KorkeakoulunOpiskeluoikeus] = Hetu.validate(hetu, isLocalDevelopmentEnvironment) match {
     case Right(h) => virta.opintotiedot(VirtaHakuehtoHetu(h)).toList.flatMap(converter.convertToOpiskeluoikeudet)
     case Left(status) =>
       logger.warn(s"Virta haku prevented $status")
