2b5374be18e0ad979a0f2f25091e3a1ff1b70780
juhomikkonen <juho.mikkonen@oph.fi>
2017-12-28 10:30:24 +0200

    Update Ulkomaanjakso.scala

--- src/main/scala/fi/oph/koski/schema/Ulkomaanjakso.scala
+++ src/main/scala/fi/oph/koski/schema/Ulkomaanjakso.scala
@@ -6,14 +6,17 @@ import fi.oph.koski.localization.LocalizedString
 import fi.oph.koski.schema.annotation.KoodistoUri
 import fi.oph.scalaschema.annotation.Description
 
-@Description("Opintoihin liittyvien ulkomaanjaksojen tiedot. Ulkomaanjakson tiedot sisältävät alku- ja loppupäivämäärät, sekä tiedon siitä, missä maassa jakso on suoritettu")
+@Description("Opintoihin liittyvien ulkomaanjaksojen tiedot. Ulkomaanjakson tiedot sisältävät alku- ja loppupäivämäärät, tiedon siitä, missä maassa jakso on suoritettu, sekä kuvauksen jakson sisällöstä.")
+@Tooltip("Opintoihin liittyvien ulkomaanjaksojen tiedot. Ulkomaanjakson tiedot sisältävät alku- ja loppupäivämäärät, tiedon siitä, missä maassa jakso on suoritettu, sekä kuvauksen jakson sisällöstä. Voit lisätä useita ulkomaanjaksoja 'lisää uusi'-painikkeesta.")
 case class Ulkomaanjakso(
   alku: LocalDate,
   loppu: Option[LocalDate],
   @Description("Missä maassa jakso on suoritettu")
+  @Tooltip("Tieto siitä, missä maassa jakso on suoritettu. Valitse maa pudotusvalikosta.")
   @KoodistoUri("maatjavaltiot2")
   maa: Koodistokoodiviite,
-  @Description("Kuvaus ulkomaanjakson sisällöstä. Esimerkiksi tieto siitä, että opiskeleeko oppija ulkomaisessa oppilaitoksessa vai onko työharjoittelussa tai työssäoppimisessa")
+  @Description("Kuvaus ulkomaanjakson sisällöstä. Esimerkiksi tieto siitä, opiskeleeko oppija ulkomaisessa oppilaitoksessa vai onko työharjoittelussa tai työssäoppimisessa.")
+  @Tooltip("Kuvaus ulkomaanjakson sisällöstä. Esimerkiksi tieto siitä, opiskeleeko oppija ulkomaisessa oppilaitoksessa vai onko työharjoittelussa tai työssäoppimisessa.")
   kuvaus: LocalizedString
 ) extends Jakso
 
