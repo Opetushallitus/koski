880fc6855989fab95b56f488a11029ff75566df5
Kalle Kainu <kalle.kainu@reaktor.fi>
2018-01-08 10:52:52 +0200

    Fix pagination link
    It is enough that the link element is even partly vertically visible to trigger paging

--- web/app/components/PaginationLink.jsx
+++ web/app/components/PaginationLink.jsx
@@ -28,8 +28,6 @@ function isElementInViewport (el) {
   let rect = el.getBoundingClientRect()
   return (
     rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
   )
 }
