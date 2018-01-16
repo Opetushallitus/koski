94ad31dcb6101f68108f57fd64748d7f970f7aa8
Petri Myllys <petri.myllys@reaktor.com>
2018-01-16 18:04:31 +0200

    Simplify

--- web/app/ib/IB.jsx
+++ web/app/ib/IB.jsx
@@ -4,29 +4,27 @@ import {LukionOppiaineEditor} from '../lukio/Lukio'
 import {LukionOppiaineetTableHead} from '../lukio/fragments/LukionOppiaineetTable'
 import {modelData, modelLookup} from '../editor/EditorModel'
 
-export class IBTutkinnonOppiaineetEditor extends React.Component {
-  render() {
-    const {oppiaineet} = this.props
-    const aineryhmittäin = R.groupBy(
-      oppiaine => modelData(oppiaine, 'koulutusmoduuli.ryhmä').koodiarvo,
-      oppiaineet
-    )
-    return (
-      <table className='suoritukset oppiaineet'>
-        <LukionOppiaineetTableHead />
-        <tbody>
-        {
-          Object.values(aineryhmittäin).map(aineet => [
-            <tr className='aineryhmä'>
-              <th colSpan='4'>{modelLookup(aineet[0], 'koulutusmoduuli.ryhmä').value.title}</th>
-            </tr>,
-            aineet.map((oppiaine, oppiaineIndex) =>
-              <LukionOppiaineEditor key={oppiaineIndex} oppiaine={oppiaine} />
-            )
-          ])
-        }
-        </tbody>
-      </table>
-    )
-  }
+export const IBTutkinnonOppiaineetEditor = ({oppiaineet}) => {
+  const aineryhmittäin = R.groupBy(
+    oppiaine => modelData(oppiaine, 'koulutusmoduuli.ryhmä').koodiarvo,
+    oppiaineet
+  )
+
+  return (
+    <table className='suoritukset oppiaineet'>
+      <LukionOppiaineetTableHead />
+      <tbody>
+      {
+        Object.values(aineryhmittäin).map(aineet => [
+          <tr className='aineryhmä'>
+            <th colSpan='4'>{modelLookup(aineet[0], 'koulutusmoduuli.ryhmä').value.title}</th>
+          </tr>,
+          aineet.map((oppiaine, oppiaineIndex) =>
+            <LukionOppiaineEditor key={oppiaineIndex} oppiaine={oppiaine} />
+          )
+        ])
+      }
+      </tbody>
+    </table>
+  )
 }
