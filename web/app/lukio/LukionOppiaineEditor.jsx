dd7e0ec55e56138c69c01e36c2a3e289c96025f7
Petri Myllys <petri.myllys@reaktor.com>
2018-01-24 13:19:48 +0200

    Lisää mahdollisuus poistaa oppiaine

--- web/app/lukio/LukionOppiaineEditor.jsx
+++ web/app/lukio/LukionOppiaineEditor.jsx
@@ -6,6 +6,7 @@ import {suorituksenTilaSymbol} from '../suoritus/Suoritustaulukko'
 import {KurssitEditor} from '../kurssi/KurssitEditor'
 import {tilaText} from '../suoritus/Suoritus'
 import {FootnoteHint} from '../components/footnote'
+import {pushRemoval} from '../editor/EditorModel'
 
 export const LukionOppiaineEditor = ({oppiaine, footnote}) => {
   const arviointi = modelData(oppiaine, 'arviointi')
@@ -15,6 +16,7 @@ export const LukionOppiaineEditor = ({oppiaine, footnote}) => {
   const kurssitNumeerisellaArvosanalla = suoritetutKurssit.filter(kurssi => !isNaN(numeerinenArvosana(kurssi)))
   const keskiarvo = kurssitNumeerisellaArvosanalla.length > 0 && Math.round((kurssitNumeerisellaArvosanalla.map(numeerinenArvosana).reduce((a, b) => a + b) / kurssitNumeerisellaArvosanalla.length) * 10) / 10
   const oppiaineTitle = t(modelData(oppiaine, 'koulutusmoduuli.tunniste.nimi'))
+  const {edit} = oppiaine.context
 
   return (
     <tr className={'oppiaine oppiaine-rivi ' + modelData(oppiaine, 'koulutusmoduuli.tunniste.koodiarvo')}>
@@ -35,6 +37,13 @@ export const LukionOppiaineEditor = ({oppiaine, footnote}) => {
         </div>
         <div className="keskiarvo">{keskiarvo ? '(' + keskiarvo.toFixed(1).replace('.', ',') + ')' : ''}</div>
       </td>
+      {
+        edit && (
+          <td>
+            <a className="remove-value" onClick={() => pushRemoval(oppiaine)}/>
+          </td>
+        )
+      }
     </tr>
   )
 }
