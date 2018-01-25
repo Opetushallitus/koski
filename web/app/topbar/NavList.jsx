b3caaf10298cae800044fa6ddb5af3a50738fe5c
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2018-01-25 16:19:30 +0200

    Poista validointi kälistä

--- web/app/topbar/NavList.jsx
+++ web/app/topbar/NavList.jsx
@@ -8,7 +8,6 @@ export default ({location, user}) => {
   return (<ul>
     <li>{naviLink('/koski/virkailija', 'Opiskelijat', location.path, '', (path, loc) => loc == path || loc.startsWith('/koski/oppija'))}</li>
     <li>{naviLink('/koski/tiedonsiirrot', 'Tiedonsiirrot', location.path, '', (path, loc) => loc.startsWith(path))}</li>
-    {user && user.hasGlobalReadAccess && <li>{naviLink('/koski/validointi', 'Validointi', location.path, '')}</li>}
     <li>{naviLink('/koski/dokumentaatio', 'Dokumentaatio', location.path, '')}</li>
   </ul>)
 }
