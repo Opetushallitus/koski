53c1be4bd7a90104912920507c82a497bd2c5bdd
Petri Myllys <petri.myllys@reaktor.com>
2018-02-01 17:30:24 +0200

    Use stateless component

--- web/app/lukio/LuvaEditor.jsx
+++ web/app/lukio/LuvaEditor.jsx
@@ -2,29 +2,27 @@ import React from 'react'
 import {LukionOppiaineetEditor} from './LukionOppiaineetEditor'
 import Text from '../i18n/Text'
 
-export class LuvaEditor extends React.Component {
-  render() {
-    const suoritukset = this.props.oppiaineet || []
-    const lukionkurssinsuoritukset = suoritukset.filter(s => s.value.classes.includes('lukionoppiaineenopintojensuorituslukioonvalmistavassakoulutuksessa'))
-    const lukioonvalmistavankurssinsuoritukset = suoritukset.filter(s => s.value.classes.includes('lukioonvalmistavankoulutuksenoppiaineensuoritus'))
+export const LuvaEditor = ({oppiaineet}) => {
+  const suoritukset = oppiaineet || []
+  const lukionkurssinsuoritukset = suoritukset.filter(s => s.value.classes.includes('lukionoppiaineenopintojensuorituslukioonvalmistavassakoulutuksessa'))
+  const lukioonvalmistavankurssinsuoritukset = suoritukset.filter(s => s.value.classes.includes('lukioonvalmistavankoulutuksenoppiaineensuoritus'))
 
-    return (
-      <div>
-        {
-          lukioonvalmistavankurssinsuoritukset.length > 0 &&
-          <div>
-            <h5><Text name="Lukioon valmistavat opinnot"/></h5>
-            <LukionOppiaineetEditor oppiaineet={lukioonvalmistavankurssinsuoritukset} />
-          </div>
-        }
-        {
-          lukionkurssinsuoritukset.length > 0 &&
-          <div>
-            <h5><Text name="Valinnaisena suoritetut lukiokurssit"/></h5>
-            <LukionOppiaineetEditor oppiaineet={lukionkurssinsuoritukset} />
-          </div>
-        }
-      </div>
-    )
-  }
+  return (
+    <div>
+      {
+        lukioonvalmistavankurssinsuoritukset.length > 0 &&
+        <div>
+          <h5><Text name="Lukioon valmistavat opinnot"/></h5>
+          <LukionOppiaineetEditor oppiaineet={lukioonvalmistavankurssinsuoritukset}/>
+        </div>
+      }
+      {
+        lukionkurssinsuoritukset.length > 0 &&
+        <div>
+          <h5><Text name="Valinnaisena suoritetut lukiokurssit"/></h5>
+          <LukionOppiaineetEditor oppiaineet={lukionkurssinsuoritukset}/>
+        </div>
+      }
+    </div>
+  )
 }
