971be976baec48d5a53c5dc01533b280791f6783
Pasi Eronen <pasi.eronen@reaktor.com>
2018-02-07 14:16:46 +0200

    Salli api/opiskeluoikeus/validate rajapinta vain root-käyttäjälle (koska se käyttää sisäänkirjautuneen käyttäjän sessiota validoinnissa)

--- src/test/scala/fi/oph/koski/api/OppijaValidationApiSpec.scala
+++ src/test/scala/fi/oph/koski/api/OppijaValidationApiSpec.scala
@@ -2,10 +2,13 @@ package fi.oph.koski.api
 
 import fi.oph.koski.henkilo.MockOppijat
 import fi.oph.koski.json.JsonSerializer
+import fi.oph.koski.koskiuser.MockUsers
 import fi.oph.koski.opiskeluoikeus.ValidationResult
 import org.scalatest.{FreeSpec, Matchers}
 
 class OppijaValidationApiSpec extends FreeSpec with LocalJettyHttpSpecification with OpiskeluoikeusTestMethods with Matchers {
+  override def defaultUser = MockUsers.paakayttaja
+
   "Validation of stored data using the validation API" - {
     "Validate all - fast" in {
       resetFixtures
@@ -38,6 +41,13 @@ class OppijaValidationApiSpec extends FreeSpec with LocalJettyHttpSpecification 
       println(result.henkilöOid)
       result.errors should equal(Nil)
       println(result.henkilöOid + " ok")
+    }
+
+    "Forbiddon for non-root users" in {
+      val oo = lastOpiskeluoikeus(MockOppijat.eero.oid)
+      authGet("api/opiskeluoikeus/validate/" + oo.oid.get, user = MockUsers.kalle) {
+        verifyResponseStatus(403, Nil)
+      }
     }
   }
 }
