9c034edc97c03a7b96b8392aaf2d3d4c017388f4
Petri Myllys <petri.myllys@reaktor.com>
2018-02-09 12:55:17 +0200

    Fix typo

--- web/app/EiSuorituksia.jsx
+++ web/app/EiSuorituksia.jsx
@@ -10,7 +10,7 @@ export const EiSuorituksia = () => (
       <p>{'Koski-palvelussa pystytään näyttämään seuraavat tiedot:'}</p>
       <ul>
         <li>{'Vuoden 2018 tammikuun jälkeen suoritetut peruskoulun, lukion ja ammattikoulun opinnot ja voimassa olevat opiskeluoikeudet.'}</li>
-        <li>{'vuoden 1990 jälkeen suoritetut ylioppilastutkinnot.'}</li>
+        <li>{'Vuoden 1990 jälkeen suoritetut ylioppilastutkinnot.'}</li>
         <li>{'Korkeakoulutusuoritukset ja opiskeluoikeudet ovat näkyvissä pääsääntöisesti vuodesta 1995 eteenpäin, mutta tässä voi olla korkeakoulukohtaisia poikkeuksia.'}</li>
       </ul>
       <p>{'Mikäli tiedoistasi puuttuu opintosuorituksia tai opiskeluoikeuksia, joiden kuuluisi ylläolevien tietojen perusteella näkyä Koski-palvelussa, voit ilmoittaa asiasta oppilaitoksellesi.'}</p>
