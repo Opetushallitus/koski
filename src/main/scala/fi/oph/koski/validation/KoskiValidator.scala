ebd21ba03d90ca23ad80080c90bb93ba3d1e1b76
Pasi Eronen <pasi.eronen@reaktor.com>
2018-02-21 13:54:14 +0200

    TOR-384 Jos tutkintosuoritukselta puuttuu alkamispäivä, päättele siirtymäaika ensimmäisestä läsnä-opiskeluoikeusjaksosta

--- src/main/scala/fi/oph/koski/validation/KoskiValidator.scala
+++ src/main/scala/fi/oph/koski/validation/KoskiValidator.scala
@@ -69,7 +69,8 @@ class KoskiValidator(tutkintoRepository: TutkintoRepository, val koodistoPalvelu
         (validateAccess(opiskeluoikeus.getOppilaitos)
           .onSuccess { validateLähdejärjestelmä(opiskeluoikeus) }
           .onSuccess {
-            HttpStatus.fold(opiskeluoikeus.suoritukset.map(TutkintoRakenneValidator(tutkintoRepository, koodistoPalvelu).validateTutkintoRakenne(_)))
+            HttpStatus.fold(opiskeluoikeus.suoritukset.map(TutkintoRakenneValidator(tutkintoRepository, koodistoPalvelu).validateTutkintoRakenne(_,
+              opiskeluoikeus.tila.opiskeluoikeusjaksot.find(_.tila.koodiarvo == "lasna").map(_.alku))))
           })
           .onSuccess { HttpStatus.fold(
             validateSisältyvyys(henkilö, opiskeluoikeus),
