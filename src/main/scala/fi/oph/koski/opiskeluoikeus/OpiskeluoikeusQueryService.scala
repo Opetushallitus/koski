efe88ab58ef2c89c942485f942668f07c0e2e11a
Mikko Siukola <mikko.siukola@cybercom.com>
2018-02-23 13:45:55 +0200

    Lisätty muuttunutEnnen-parametri hakurajapintaan

--- src/main/scala/fi/oph/koski/opiskeluoikeus/OpiskeluoikeusQueryService.scala
+++ src/main/scala/fi/oph/koski/opiskeluoikeus/OpiskeluoikeusQueryService.scala
@@ -51,6 +51,7 @@ class OpiskeluoikeusQueryService(val db: DB) extends DatabaseExecutionContext wi
         }
       case (query, IdHaku(ids)) => query.filter(_._1.id inSetBind ids)
       case (query, SuoritusJsonHaku(json)) => query.filter(_._1.data.+>("suoritukset").@>(json))
+      case (query, MuuttunutEnnen(aikaleima)) => query.filter(_._1.aikaleima < Timestamp.valueOf(aikaleima))
       case (query, MuuttunutJälkeen(aikaleima)) => query.filter(_._1.aikaleima > Timestamp.valueOf(aikaleima))
       case (query, filter) => throw new InvalidRequestException(KoskiErrorCategory.internalError("Hakua ei ole toteutettu: " + filter))
     }
