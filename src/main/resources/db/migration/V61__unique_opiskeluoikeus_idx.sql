d5e84c05afcd51e20042b8aa0ca322b67558e677
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2018-02-26 11:38:31 +0200

    Luo indeksi ilman lukitusta

--- src/main/resources/db/migration/V61__unique_opiskeluoikeus_idx.sql
+++ src/main/resources/db/migration/V61__unique_opiskeluoikeus_idx.sql
@@ -1,1 +1,1 @@
-create unique index unique_opiskeluoikeus_idx on opiskeluoikeus(oppija_oid, (data->'oppilaitos'->>'oid'), (data->'tyyppi'->>'koodiarvo'), (data->>'lähdejärjestelmänId')) where mitatoity = false;
+create unique index concurrently unique_opiskeluoikeus_idx on opiskeluoikeus(oppija_oid, (data->'oppilaitos'->>'oid'), (data->'tyyppi'->>'koodiarvo'), (data->>'lähdejärjestelmänId')) where mitatoity = false;
