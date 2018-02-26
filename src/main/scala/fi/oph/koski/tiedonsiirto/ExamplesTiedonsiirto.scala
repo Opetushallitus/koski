eb8de894a7c63b6d9dfa2323bbfcdec439c6bdea
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2018-02-26 12:50:23 +0200

    Korjaa tiedonsiirtoesimerkit

--- src/main/scala/fi/oph/koski/tiedonsiirto/ExamplesTiedonsiirto.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/ExamplesTiedonsiirto.scala
@@ -3,6 +3,7 @@ package fi.oph.koski.tiedonsiirto
 import fi.oph.koski.documentation.AmmatillinenExampleData._
 import fi.oph.koski.documentation.{AmmatillinenExampleData, Example}
 import fi.oph.koski.henkilo.MockOppijat
+import fi.oph.koski.henkilo.MockOppijat.asUusiOppija
 import fi.oph.koski.organisaatio.MockOrganisaatiot
 import fi.oph.koski.schema.{AmmatillinenOpiskeluoikeus, Oppija, Oppilaitos}
 
@@ -11,9 +12,9 @@ object ExamplesTiedonsiirto {
   val failingOpiskeluoikeus: AmmatillinenOpiskeluoikeus = opiskeluoikeus.copy(oppilaitos = Some(Oppilaitos(MockOrganisaatiot.aaltoYliopisto)))
 
   val examples: List[Example] = List(
-    Example("tiedonsiirto - onnistunut", "Onnistunut tiedonsiirto", Oppija(MockOppijat.tiedonsiirto.henkilö, List(opiskeluoikeus)), 403),
+    Example("tiedonsiirto - onnistunut", "Onnistunut tiedonsiirto", Oppija(asUusiOppija(MockOppijat.tiedonsiirto.henkilö), List(opiskeluoikeus)), 403),
     Example("tiedonsiirto - vain syntymäaika", "Onnistunut tiedonsiirto", Oppija(MockOppijat.hetuton.henkilö, List(opiskeluoikeus)), 403),
-    Example("tiedonsiirto - epäonnistunut", "Epäonnistunut tiedonsiirto", Oppija(MockOppijat.tiedonsiirto.henkilö, List(failingOpiskeluoikeus)), 403),
-    Example("tiedonsiirto - epäonnistunut 2", "Onnistunut tiedonsiirto", Oppija(MockOppijat.ammattilainen.henkilö, List(failingOpiskeluoikeus)), 403)
+    Example("tiedonsiirto - epäonnistunut", "Epäonnistunut tiedonsiirto", Oppija(asUusiOppija(MockOppijat.tiedonsiirto.henkilö), List(failingOpiskeluoikeus)), 403),
+    Example("tiedonsiirto - epäonnistunut 2", "Onnistunut tiedonsiirto", Oppija(asUusiOppija(MockOppijat.ammattilainen.henkilö), List(failingOpiskeluoikeus)), 403)
   )
 }
