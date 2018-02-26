fe05326430e4a287106d852c788dd22984961e7b
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2018-02-26 12:28:58 +0200

    Skippaa indeksin luonti jos se on jo olemassa

--- src/main/resources/db/migration/V61__unique_opiskeluoikeus_idx.sql
+++ src/main/resources/db/migration/V61__unique_opiskeluoikeus_idx.sql
@@ -1,1 +1,1 @@
-create unique index concurrently unique_opiskeluoikeus_idx on opiskeluoikeus(oppija_oid, (data->'oppilaitos'->>'oid'), (data->'tyyppi'->>'koodiarvo'), (data->>'lähdejärjestelmänId')) where mitatoity = false;
+create unique index if not exists unique_opiskeluoikeus_idx on opiskeluoikeus(oppija_oid, (data->'oppilaitos'->>'oid'), (data->'tyyppi'->>'koodiarvo'), (data->>'lähdejärjestelmänId')) where mitatoity = false;
