32f1fa0094e2c537092a6654d96a4102a557b7f3
Heikki Hulkko <heikki.hulkko@reaktor.fi>
2018-02-26 12:31:04 +0200

    Poista vanha indeksi

--- src/main/resources/db/migration/V61__unique_opiskeluoikeus_idx.sql
+++ src/main/resources/db/migration/V61__unique_opiskeluoikeus_idx.sql
@@ -1,1 +1,2 @@
 create unique index if not exists unique_opiskeluoikeus_idx on opiskeluoikeus(oppija_oid, (data->'oppilaitos'->>'oid'), (data->'tyyppi'->>'koodiarvo'), (data->>'lähdejärjestelmänId')) where mitatoity = false;
+drop index opiskeluoikeus_oppija_oppilaitos_tyyppi_lähdejärjestelmänid_idx;
