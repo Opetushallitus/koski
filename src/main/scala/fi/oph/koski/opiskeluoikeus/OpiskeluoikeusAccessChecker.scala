78d13f938b7c1047520d9b721eb39d1c1225d9b7
Pasi Eronen <pasi.eronen@reaktor.com>
2018-03-05 11:49:42 +0200

    Älä näytä mitätöinti-nappia Virta tai YTR opiskeluoikeuksille

--- src/main/scala/fi/oph/koski/opiskeluoikeus/OpiskeluoikeusAccessChecker.scala
+++ src/main/scala/fi/oph/koski/opiskeluoikeus/OpiskeluoikeusAccessChecker.scala
@@ -1,14 +1,15 @@
 package fi.oph.koski.opiskeluoikeus
 
 import fi.oph.koski.koskiuser.KoskiSession
-import fi.oph.koski.schema.{Opiskeluoikeus}
+import fi.oph.koski.schema.{KoskeenTallennettavaOpiskeluoikeus, Opiskeluoikeus}
 
 object OpiskeluoikeusAccessChecker {
   def isInvalidatable(opiskeluoikeus: Opiskeluoikeus, session: KoskiSession): Boolean = {
     val orgWriteAccess = opiskeluoikeus.omistajaOrganisaatio.exists(o => session.hasWriteAccess(o.oid))
     val orgTiedonsiirronMitätöintiAccess = opiskeluoikeus.omistajaOrganisaatio.exists(o => session.hasTiedonsiirronMitätöintiAccess(o.oid))
     val lähdejärjestelmällinen = opiskeluoikeus.lähdejärjestelmänId.nonEmpty
+    val koskeenTallennettava = opiskeluoikeus.isInstanceOf[KoskeenTallennettavaOpiskeluoikeus]
     val sisältääValmiitaSuorituksia = opiskeluoikeus.suoritukset.exists(_.valmis) || opiskeluoikeus.suoritukset.exists(_.osasuoritukset.exists(_.exists(_.valmis)))
-    ((!lähdejärjestelmällinen && orgWriteAccess) || (lähdejärjestelmällinen && orgTiedonsiirronMitätöintiAccess)) && !sisältääValmiitaSuorituksia
+    koskeenTallennettava && ((!lähdejärjestelmällinen && orgWriteAccess) || (lähdejärjestelmällinen && orgTiedonsiirronMitätöintiAccess)) && !sisältääValmiitaSuorituksia
   }
 }
