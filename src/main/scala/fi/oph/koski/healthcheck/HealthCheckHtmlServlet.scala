64336245784ba9e171a3130c7aeea21f06807c30
Pasi Eronen <pasi.eronen@reaktor.com>
2018-03-13 10:42:41 +0200

    Minor tweak for healthcheck page CSS

--- src/main/scala/fi/oph/koski/healthcheck/HealthCheckHtmlServlet.scala
+++ src/main/scala/fi/oph/koski/healthcheck/HealthCheckHtmlServlet.scala
@@ -13,15 +13,15 @@ class HealthCheckHtmlServlet(implicit val application: KoskiApplication) extends
       case true =>
         <div style="height: 100%; background: #528a1e; background: linear-gradient(to bottom, darkgreen 0%,green 100%); color: white; font-family: sans-serif;">
           <h1 style="font-size: 20vw; margin-bottom: 4vw;">OK</h1>
-          <div style="font-size: 4vw; padding-bottom: 4vw;">{version} {buildDate}</div>
+          <div style="font-size: 4vw; padding-bottom: 4vw;">{version} — {buildDate}</div>
         </div>
       case false =>
         <div style="height: 100%; background: red;">
           <h1 style="font-size: 20vw; margin-bottom: 0;">FAIL</h1>
-          <ul style="font-size: 4vw; list-style: none; line-height: 5vw;">
+          <ul style="font-size: 4vw; list-style: none; line-height: 5vw; padding-left: 0;">
             {healthcheck.errors.map(_.key).distinct.map(e => <li>{e}</li>)}
           </ul>
-          <div style="font-size: 4vw; padding-bottom: 4vw;">{version} {buildDate}</div>
+          <div style="font-size: 4vw; padding-bottom: 4vw;">{version} — {buildDate}</div>
         </div>
     }
     <html style="text-align: center;">
