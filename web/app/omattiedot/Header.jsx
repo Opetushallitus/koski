134b9343146ae1aa05a42c1543b7a2a8d83fea84
Petri Myllys <petri.myllys@reaktor.com>
2018-03-14 13:18:44 +0200

    Refactor

--- web/app/omattiedot/Header.jsx
+++ web/app/omattiedot/Header.jsx
@@ -8,55 +8,64 @@ import {Popup} from '../components/Popup'
 import {TiedotPalvelussa} from './TiedotPalvelussa'
 import {Virheraportointi} from './Virheraportointi'
 
+const togglePopup = stateA => () => stateA.modify(v => !v)
+
+const HeaderInfo = ({henkilö, showPalvelussaNäkyvätTiedotA, showVirheraportointiA}) => {
+  const nimi = <p>{`${modelData(henkilö, 'etunimet')} ${modelData(henkilö, 'sukunimi')}`}</p>
+  const syntymäaika = modelTitle(henkilö, 'syntymäaika') &&
+    <p className='syntynyt'>
+      <Text name='syntynyt'/>
+      <span> {ISO2FinnishDate(modelTitle(henkilö, 'syntymäaika'))}</span>
+    </p>
+
+  return (
+    <header>
+      <h2 className='header__heading'>
+        <Text name='Opintoni'/>
+      </h2>
+      <div className='header__caption'>
+        <p>
+          <Text name='Opintoni ingressi'/>
+        </p>
+        <a onClick={togglePopup(showPalvelussaNäkyvätTiedotA)}>
+          <Text name='Mitkä tiedot palvelussa näkyvät?'/>
+        </a>
+      </div>
+      {ift(showPalvelussaNäkyvätTiedotA,
+        <Popup showStateAtom={showPalvelussaNäkyvätTiedotA} inline={true}>
+          <TiedotPalvelussa/>
+        </Popup>
+      )}
+      <div className='header__bottom-row'>
+        <div className='header__name'>
+          {nimi}
+          {syntymäaika}
+        </div>
+
+        <button onClick={togglePopup(showVirheraportointiA)}>
+          <Text name='Suorituksissani on virhe'/>
+        </button>
+      </div>
+    </header>
+  )
+}
+
 export class Header extends React.Component {
   constructor(props) {
     super(props)
+
     this.showPalvelussaNäkyvätTiedot = Atom(false)
     this.showVirheraportointi = Atom(false)
   }
 
   render() {
-    const {henkilö} = this.props
-
-    const nimi = <p>{`${modelData(henkilö, 'etunimet')} ${modelData(henkilö, 'sukunimi')}`}</p>
-    const syntymäaika = modelTitle(henkilö, 'syntymäaika') &&
-      <p className='syntynyt'>
-        <Text name='syntynyt'/>
-        <span> {ISO2FinnishDate(modelTitle(henkilö, 'syntymäaika'))}</span>
-      </p>
-
-    const togglePopup = stateA => () => stateA.modify(v => !v)
-
     return (
       <div>
-        <header>
-          <h2 className='header__heading'>
-            <Text name='Opintoni'/>
-          </h2>
-          <div className='header__caption'>
-            <p>
-              <Text name='Opintoni ingressi'/>
-            </p>
-            <a onClick={togglePopup(this.showPalvelussaNäkyvätTiedot)}>
-              <Text name='Mitkä tiedot palvelussa näkyvät?'/>
-            </a>
-          </div>
-          {ift(this.showPalvelussaNäkyvätTiedot,
-            <Popup showStateAtom={this.showPalvelussaNäkyvätTiedot} inline={true}>
-              <TiedotPalvelussa/>
-            </Popup>
-          )}
-          <div className='header__bottom-row'>
-            <div className='header__name'>
-              {nimi}
-              {syntymäaika}
-            </div>
-
-            <button onClick={togglePopup(this.showVirheraportointi)}>
-              <Text name='Suorituksissani on virhe'/>
-            </button>
-          </div>
-        </header>
+        <HeaderInfo
+          henkilö={this.props.henkilö}
+          showPalvelussaNäkyvätTiedotA={this.showPalvelussaNäkyvätTiedot}
+          showVirheraportointiA={this.showVirheraportointi}
+        />
 
         {ift(this.showVirheraportointi,
           <div className='virheraportointi'>
