7c53f58daccb4f07bb06697342c76ba1f6ccbc0d
Petri Myllys <petri.myllys@reaktor.com>
2018-03-14 13:14:15 +0200

    Lisää virheraportointi-kälikomponentti omat tiedot -sivulle

--- web/app/omattiedot/Header.jsx
+++ web/app/omattiedot/Header.jsx
@@ -6,11 +6,13 @@ import {ISO2FinnishDate} from '../date/date'
 import {modelData, modelTitle} from '../editor/EditorModel'
 import {Popup} from '../components/Popup'
 import {TiedotPalvelussa} from './TiedotPalvelussa'
+import {Virheraportointi} from './Virheraportointi'
 
 export class Header extends React.Component {
   constructor(props) {
     super(props)
-    this.showPopup = Atom(false)
+    this.showPalvelussaNäkyvätTiedot = Atom(false)
+    this.showVirheraportointi = Atom(false)
   }
 
   render() {
@@ -23,33 +25,47 @@ export class Header extends React.Component {
         <span> {ISO2FinnishDate(modelTitle(henkilö, 'syntymäaika'))}</span>
       </p>
 
-    const togglePopup = () => this.showPopup.modify(v => !v)
+    const togglePopup = stateA => () => stateA.modify(v => !v)
 
     return (
-      <header>
-        <h2 className='header__heading'>
-          <Text name='Opintoni'/>
-        </h2>
-        <div className='header__caption'>
-          <p>
-            <Text name='Opintoni ingressi'/>
-          </p>
-          <div>
-            <a onClick={togglePopup}>
+      <div>
+        <header>
+          <h2 className='header__heading'>
+            <Text name='Opintoni'/>
+          </h2>
+          <div className='header__caption'>
+            <p>
+              <Text name='Opintoni ingressi'/>
+            </p>
+            <a onClick={togglePopup(this.showPalvelussaNäkyvätTiedot)}>
               <Text name='Mitkä tiedot palvelussa näkyvät?'/>
             </a>
           </div>
-        </div>
-        {ift(this.showPopup,
-          <Popup showStateAtom={this.showPopup} inline={true}>
-            <TiedotPalvelussa/>
-          </Popup>
+          {ift(this.showPalvelussaNäkyvätTiedot,
+            <Popup showStateAtom={this.showPalvelussaNäkyvätTiedot} inline={true}>
+              <TiedotPalvelussa/>
+            </Popup>
+          )}
+          <div className='header__bottom-row'>
+            <div className='header__name'>
+              {nimi}
+              {syntymäaika}
+            </div>
+
+            <button onClick={togglePopup(this.showVirheraportointi)}>
+              <Text name='Suorituksissani on virhe'/>
+            </button>
+          </div>
+        </header>
+
+        {ift(this.showVirheraportointi,
+          <div className='virheraportointi'>
+            <Popup showStateAtom={this.showVirheraportointi} inline={true}>
+              <Virheraportointi/>
+            </Popup>
+          </div>
         )}
-        <div className='header__name'>
-          {nimi}
-          {syntymäaika}
-        </div>
-      </header>
+      </div>
     )
   }
 }
