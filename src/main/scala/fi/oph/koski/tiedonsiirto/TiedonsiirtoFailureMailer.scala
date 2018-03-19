3f322f93bbc52c1c6743f28a9eeeb46e9b827534
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-03-19 16:08:34 +0200

    Korjaa peräkkäisen mailinlähetysten esto

--- src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoFailureMailer.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/TiedonsiirtoFailureMailer.scala
@@ -7,7 +7,7 @@ import com.typesafe.config.Config
 import fi.oph.koski.email._
 import fi.oph.koski.henkilo.OpintopolkuHenkilöFacade
 import fi.oph.koski.log.Logging
-import fi.oph.koski.schema.{Koulutustoimija, Oppilaitos, OrganisaatioWithOid}
+import fi.oph.koski.schema.OrganisaatioWithOid
 
 class TiedonsiirtoFailureMailer(config: Config, authenticationServiceClient: OpintopolkuHenkilöFacade) extends Logging {
   private val sendTimes = scala.collection.mutable.Map[String, LocalDateTime]()
@@ -15,14 +15,17 @@ class TiedonsiirtoFailureMailer(config: Config, authenticationServiceClient: Opi
   private val koskiPääkäyttäjät = "KOSKI-pääkäyttäjä"
   private val vastuukayttajat = "Vastuukayttajat"
 
-  def sendMail(rootOrg: OrganisaatioWithOid, oppilaitokset: List[OrganisaatioWithOid]): Unit = {
-    val emails = oppilaitokset.flatMap(oppilaitosSähköpostit) match {
+  def sendMail(rootOrg: OrganisaatioWithOid, oppilaitos: Option[OrganisaatioWithOid]): Unit = {
+    if (!shouldSendMail(rootOrg, oppilaitos)) {
+      return
+    }
+    val emails = oppilaitosSähköpostit(oppilaitos) match {
       case Nil => rootOrgSähköpostit(rootOrg)
       case emailAddresses => emailAddresses
     }
 
     emails.distinct match {
-      case Nil => logger.info(s"Organisaatioille ${(rootOrg :: oppilaitokset).map(_.oid).mkString(", ")} ei löydy sähköpostiosoitetta")
+      case Nil => logger.info(s"Organisaatioille ${(rootOrg.oid +: oppilaitos.toList.map(_.oid)).mkString(" ja ")} ei löydy sähköpostiosoitetta")
       case emailAddresses =>
         val mail: Email = Email(EmailContent(
           "no-reply@opintopolku.fi",
@@ -35,32 +38,24 @@ class TiedonsiirtoFailureMailer(config: Config, authenticationServiceClient: Opi
     }
   }
 
-  private def shouldSendMail(organisaatio: String) = sendTimes.synchronized {
+  private def shouldSendMail(rootOrg: OrganisaatioWithOid, organisaatio: Option[OrganisaatioWithOid]) = sendTimes.synchronized {
     val limit = now().minusHours(24)
-    if (!sendTimes.getOrElse(organisaatio, limit).isAfter(limit)) {
-      sendTimes.put(organisaatio, now())
+    val key = rootOrg.oid + organisaatio.map(_.oid).getOrElse("")
+    if (!sendTimes.getOrElse(key, limit).isAfter(limit)) {
+      sendTimes.put(key, now())
       true
     } else {
       false
     }
   }
 
-  private def rootOrgSähköpostit(k: OrganisaatioWithOid): List[String] = {
-    if (!shouldSendMail(k.oid)) {
-      return Nil
-    }
-    organisaatioSähköpostit(k.oid, koskiPääkäyttäjät) match {
-      case Nil => organisaatioSähköpostit(k.oid, vastuukayttajat)
-      case emails => emails
-    }
+  private def rootOrgSähköpostit(k: OrganisaatioWithOid) = organisaatioSähköpostit(k.oid, koskiPääkäyttäjät) match {
+    case Nil => organisaatioSähköpostit(k.oid, vastuukayttajat)
+    case emails => emails
   }
 
-  private def oppilaitosSähköpostit(org: OrganisaatioWithOid): List[String] = {
-    if (!shouldSendMail(org.oid)) {
-      return Nil
-    }
-    organisaatioSähköpostit(org.oid, koskiPääkäyttäjät)
-  }
+  private def oppilaitosSähköpostit(org: Option[OrganisaatioWithOid]) =
+    org.toList.flatMap(o => organisaatioSähköpostit(o.oid, koskiPääkäyttäjät))
 
   private def organisaatioSähköpostit(oid: String, ryhmä: String): List[String] = {
     authenticationServiceClient.organisaationSähköpostit(oid, ryhmä)
