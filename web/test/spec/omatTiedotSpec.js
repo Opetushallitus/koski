7ba5b5437c3cf1de4b1a4147fced50fdc720f59a
Petri Myllys <petri.myllys@reaktor.com>
2018-03-20 12:37:13 +0200

    Fix quote style

--- web/test/spec/omatTiedotSpec.js
+++ web/test/spec/omatTiedotSpec.js
@@ -4,59 +4,59 @@ describe('Omat tiedot', function() {
   var authentication = Authentication()
   before(authentication.login(), resetFixtures)
 
-  describe("Virkailijana", function() {
-    describe("Kun virkailijalla on opiskeluoikeuksia", function() {
+  describe('Virkailijana', function() {
+    describe('Kun virkailijalla on opiskeluoikeuksia', function() {
       before(authentication.login('Oili'), omattiedot.openPage)
       it('ne näytetään', function() {
-        expect(omattiedot.oppija()).to.equal("Opintoni")
+        expect(omattiedot.oppija()).to.equal('Opintoni')
         expect(opinnot.opiskeluoikeudet.oppilaitokset()).to.have.members([
           'Stadin ammattiopisto', 'Jyväskylän normaalikoulu' ])
       })
     })
 
-    describe("Kun virkailijalla ei ole opiskeluoikeuksia", function() {
+    describe('Kun virkailijalla ei ole opiskeluoikeuksia', function() {
       before(authentication.login(), omattiedot.openPage)
       it('näytetään viesti', function() {
         expect(omattiedot.virhe()).to.equal(
-          "Tiedoillasi ei löydy opintosuorituksia eikä opiskeluoikeuksia." +
-          "Koski-palvelussa pystytään näyttämään seuraavat tiedot:" +
-          "Vuoden 2018 tammikuun jälkeen suoritetut peruskoulun, lukion ja ammattikoulun opinnot ja voimassa olevat opiskeluoikeudet." +
-          "Vuoden 1990 jälkeen suoritetut ylioppilastutkinnot." +
-          "Korkeakoulutusuoritukset ja opiskeluoikeudet ovat näkyvissä pääsääntöisesti vuodesta 1995 eteenpäin, mutta tässä voi olla korkeakoulukohtaisia poikkeuksia." +
-          "Mikäli tiedoistasi puuttuu opintosuorituksia tai opiskeluoikeuksia, joiden kuuluisi ylläolevien tietojen perusteella näkyä Koski-palvelussa, voit ilmoittaa asiasta oppilaitoksellesi.")
+          'Tiedoillasi ei löydy opintosuorituksia eikä opiskeluoikeuksia.' +
+          'Koski-palvelussa pystytään näyttämään seuraavat tiedot:' +
+          'Vuoden 2018 tammikuun jälkeen suoritetut peruskoulun, lukion ja ammattikoulun opinnot ja voimassa olevat opiskeluoikeudet.' +
+          'Vuoden 1990 jälkeen suoritetut ylioppilastutkinnot.' +
+          'Korkeakoulutusuoritukset ja opiskeluoikeudet ovat näkyvissä pääsääntöisesti vuodesta 1995 eteenpäin, mutta tässä voi olla korkeakoulukohtaisia poikkeuksia.' +
+          'Mikäli tiedoistasi puuttuu opintosuorituksia tai opiskeluoikeuksia, joiden kuuluisi ylläolevien tietojen perusteella näkyä Koski-palvelussa, voit ilmoittaa asiasta oppilaitoksellesi.')
       })
     })
   })
 
-  describe("Kansalaisena", function() {
+  describe('Kansalaisena', function() {
     var etusivu = LandingPage()
     var korhopankki = KorhoPankki()
     before(authentication.logout, etusivu.openPage)
 
-    describe("Kun ei olla kirjauduttu sisään", function() {
-      it("Näytetään länderi", function() {
+    describe('Kun ei olla kirjauduttu sisään', function() {
+      it('Näytetään länderi', function() {
 
       })
     })
 
-    describe("Kun kirjaudutaan sisään", function() {
+    describe('Kun kirjaudutaan sisään', function() {
       before(etusivu.login(), wait.until(korhopankki.isReady), korhopankki.login('030658-998X', 'Kansalainen', 'VÃ¤inÃ¶ TÃµnis', 'VÃ¤inÃ¶'), wait.until(omattiedot.isVisible))
-      describe("Sivun sisältö", function() {
-        it("Näytetään opiskeluoikeudet", function() {
-          expect(omattiedot.nimi()).to.equal("Väinö Tõnis Kansalainen")
-          expect(omattiedot.oppija()).to.equal("Opintoni")
+      describe('Sivun sisältö', function() {
+        it('Näytetään opiskeluoikeudet', function() {
+          expect(omattiedot.nimi()).to.equal('Väinö Tõnis Kansalainen')
+          expect(omattiedot.oppija()).to.equal('Opintoni')
           expect(opinnot.opiskeluoikeudet.oppilaitokset()).to.deep.equal([
             'Itä-Suomen yliopisto' ])
         })
 
-        it("Näytetään opintoni-ingressi", function() {
+        it('Näytetään opintoni-ingressi', function() {
           expect(omattiedot.ingressi()).to.equal(
-            "Tällä sivulla näkyvät kaikki sähköisesti tallennetut opintosuoritukset yksittäisistä kursseista kokonaisiin tutkintoihin."
+            'Tällä sivulla näkyvät kaikki sähköisesti tallennetut opintosuoritukset yksittäisistä kursseista kokonaisiin tutkintoihin.'
           )
         })
 
-        it("Näytetään nimi", function() {
-          expect(omattiedot.headerNimi()).to.equal("Väinö Tõnis Kansalainen")
+        it('Näytetään nimi', function() {
+          expect(omattiedot.headerNimi()).to.equal('Väinö Tõnis Kansalainen')
         })
 
         it("Näytetään 'Mitkä tiedot palvelussa näkyvät?' -painike", function() {
@@ -64,90 +64,90 @@ describe('Omat tiedot', function() {
         })
 
         describe("'Mitkä tiedot palvelussa näkyvät' -teksti", function () {
-          it("Aluksi ei näytetä tekstiä", function () {
-            expect(omattiedot.palvelussaNäkyvätTiedotText()).to.equal("")
+          it('Aluksi ei näytetä tekstiä', function () {
+            expect(omattiedot.palvelussaNäkyvätTiedotText()).to.equal('')
           })
 
-          describe("Kun painetaan painiketta", function () {
+          describe('Kun painetaan painiketta', function () {
             before(click(omattiedot.palvelussaNäkyvätTiedotButton))
 
-            it("näytetään teksti", function () {
+            it('näytetään teksti', function () {
               expect(omattiedot.palvelussaNäkyvätTiedotText()).to.equal(
-                "Koski-palvelussa pystytään näyttämään seuraavat tiedot:\n" +
-                "Vuoden 2018 tammikuun jälkeen suoritetut peruskoulun, lukion ja ammattikoulun opinnot ja voimassa olevat opiskeluoikeudet.\n" +
-                "Vuoden 1990 jälkeen suoritetut ylioppilastutkinnot.\n" +
-                "Korkeakoulutusuoritukset ja opiskeluoikeudet ovat näkyvissä pääsääntöisesti vuodesta 1995 eteenpäin, mutta tässä voi olla korkeakoulukohtaisia poikkeuksia."
+                'Koski-palvelussa pystytään näyttämään seuraavat tiedot:\n' +
+                'Vuoden 2018 tammikuun jälkeen suoritetut peruskoulun, lukion ja ammattikoulun opinnot ja voimassa olevat opiskeluoikeudet.\n' +
+                'Vuoden 1990 jälkeen suoritetut ylioppilastutkinnot.\n' +
+                'Korkeakoulutusuoritukset ja opiskeluoikeudet ovat näkyvissä pääsääntöisesti vuodesta 1995 eteenpäin, mutta tässä voi olla korkeakoulukohtaisia poikkeuksia.'
               )
             })
           })
 
-          describe("Kun painetaan painiketta uudestaan", function () {
+          describe('Kun painetaan painiketta uudestaan', function () {
             before(click(omattiedot.palvelussaNäkyvätTiedotButton))
 
-            it("teksti piilotetaan", function () {
-              expect(omattiedot.palvelussaNäkyvätTiedotText()).to.equal("")
+            it('teksti piilotetaan', function () {
+              expect(omattiedot.palvelussaNäkyvätTiedotText()).to.equal('')
             })
           })
 
-          describe("Kun teksti on näkyvissä", function () {
+          describe('Kun teksti on näkyvissä', function () {
             before(click(omattiedot.palvelussaNäkyvätTiedotButton))
 
-            it("alkutila (teksti näkyvissä)", function () {
-              expect(omattiedot.palvelussaNäkyvätTiedotText()).to.not.equal("")
+            it('alkutila (teksti näkyvissä)', function () {
+              expect(omattiedot.palvelussaNäkyvätTiedotText()).to.not.equal('')
             })
 
-            describe("Pop-upin painikkeella", function () {
+            describe('Pop-upin painikkeella', function () {
               before(click(omattiedot.palvelussaNäkyvätTiedotCloseButton))
 
-              it("voidaan piilottaa teksti", function () {
-                expect(omattiedot.palvelussaNäkyvätTiedotText()).to.equal("")
+              it('voidaan piilottaa teksti', function () {
+                expect(omattiedot.palvelussaNäkyvätTiedotText()).to.equal('')
               })
             })
           })
         })
       })
 
-      describe("Kun kirjaudutaan ulos", function () {
+      describe('Kun kirjaudutaan ulos', function () {
         before(click(findSingle('#logout')), wait.until(etusivu.isVisible))
-        it("Näytetään länderi", function() {
+        it('Näytetään länderi', function() {
 
         })
       })
 
-      describe("Kun henkilöllä on syntymäaika", function () {
+      describe('Kun henkilöllä on syntymäaika', function () {
         before(authentication.logout, etusivu.openPage)
         before(etusivu.login(), wait.until(korhopankki.isReady), korhopankki.login('010170-9173'), wait.until(omattiedot.isVisible))
 
-        it("Näytetään nimi ja syntymäaika", function() {
+        it('Näytetään nimi ja syntymäaika', function() {
           expect(omattiedot.headerNimi()).to.equal(
-            "Sylvi Syntynyt\n" +
-            "s. 1.1.1970"
+            'Sylvi Syntynyt\n' +
+            's. 1.1.1970'
           )
         })
       })
 
-      describe("Kun tiedot löytyvät vain YTR:stä", function() {
+      describe('Kun tiedot löytyvät vain YTR:stä', function() {
         before(authentication.logout, etusivu.openPage)
 
         before(etusivu.login(), wait.until(korhopankki.isReady), korhopankki.login('010342-8411'), wait.until(omattiedot.isVisible))
 
-        describe("Sivun sisältö", function() {
-          it("Näytetään opiskeluoikeudet", function() {
-            expect(omattiedot.nimi()).to.equal("Mia Orvokki Numminen")
-            expect(omattiedot.oppija()).to.equal("Opintoni")
+        describe('Sivun sisältö', function() {
+          it('Näytetään opiskeluoikeudet', function() {
+            expect(omattiedot.nimi()).to.equal('Mia Orvokki Numminen')
+            expect(omattiedot.oppija()).to.equal('Opintoni')
             expect(opinnot.opiskeluoikeudet.oppilaitokset()).to.deep.equal([
               'Ylioppilastutkintolautakunta' ])
           })
         })
       })
 
-      describe("Virhetilanne", function() {
+      describe('Virhetilanne', function() {
         before(authentication.logout, etusivu.openPage)
 
         before(etusivu.login(), wait.until(korhopankki.isReady), korhopankki.login('010342-8413'), wait.until(VirhePage().isVisible))
 
-        describe("Sivun sisältö", function() {
-          it("Näytetään virhesivu", function() {
+        describe('Sivun sisältö', function() {
+          it('Näytetään virhesivu', function() {
             expect(VirhePage().teksti().trim()).to.equalIgnoreNewlines('Koski-järjestelmässä tapahtui virhe, yritä myöhemmin uudelleen\n          Palaa etusivulle')
           })
         })
