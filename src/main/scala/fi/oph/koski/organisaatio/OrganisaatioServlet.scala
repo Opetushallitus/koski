1864b2cdf1c84520f1015016b93ff9fc3fc2c39c
Pasi Eronen <pasi.eronen@reaktor.com>
2018-04-04 18:00:56 +0300

    Salli /api/organisaatio ilman autentikointia

--- src/main/scala/fi/oph/koski/organisaatio/OrganisaatioServlet.scala
+++ src/main/scala/fi/oph/koski/organisaatio/OrganisaatioServlet.scala
@@ -2,31 +2,32 @@ package fi.oph.koski.organisaatio
 
 import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.http.KoskiErrorCategory
-import fi.oph.koski.koskiuser.{KäyttöoikeusOrg, RequiresAuthentication}
+import fi.oph.koski.koskiuser.{AuthenticationSupport, KäyttöoikeusOrg, RequiresAuthentication}
 import fi.oph.koski.schema.OrganisaatioOid
 import fi.oph.koski.servlet.{ApiServlet, NoCache}
 
-class OrganisaatioServlet(implicit val application: KoskiApplication) extends ApiServlet with RequiresAuthentication with NoCache {
+class OrganisaatioServlet(implicit val application: KoskiApplication) extends ApiServlet with AuthenticationSupport with NoCache {
   get("/hierarkia") {
     val query = params.get("query")
     val all = getBooleanParam("all")
-    val filtered = if (all || koskiSession.hasGlobalReadAccess) {
+    val lang = koskiSessionOption.map(_.lang).getOrElse("fi")
+
+    val filtered = if (all || koskiSessionOption.isEmpty || koskiSessionOption.get.hasGlobalReadAccess) {
       query match {
         case Some(query) if (query.length >= 3) =>
           application.organisaatioRepository.findHierarkia(query)
         case _ =>
           Nil
       }
-
     } else {
-      val all: Set[OrganisaatioHierarkia] = koskiSession.orgKäyttöoikeudet.filter(_.juuri).flatMap{ko: KäyttöoikeusOrg => application.organisaatioRepository.getOrganisaatioHierarkia(ko.organisaatio.oid)}
+      val all: Set[OrganisaatioHierarkia] = koskiSessionOption.get.orgKäyttöoikeudet.filter(_.juuri).flatMap{ko: KäyttöoikeusOrg => application.organisaatioRepository.getOrganisaatioHierarkia(ko.organisaatio.oid)}
       query match {
         case Some(query) =>
-          OrganisaatioHierarkiaFilter(query, koskiSession.lang).filter(all)
+          OrganisaatioHierarkiaFilter(query, lang).filter(all)
         case None => all
       }
     }
-    filtered.toList.sortBy(_.nimi.get(koskiSession.lang))
+    filtered.toList.sortBy(_.nimi.get(lang))
   }
 
   get("/sahkoposti-virheiden-raportointiin") {
