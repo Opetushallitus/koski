3081b7fd90f27fdcfda0947dc05c74e4066f8466
Petri Myllys <petri.myllys@reaktor.com>
2018-04-04 17:15:23 +0300

    Lisää virheenkäsittely JSON-validointivirheille

--- src/main/scala/fi/oph/koski/suoritusjako/SuoritusjakoServlet.scala
+++ src/main/scala/fi/oph/koski/suoritusjako/SuoritusjakoServlet.scala
@@ -1,10 +1,13 @@
 package fi.oph.koski.suoritusjako
 
 import fi.oph.koski.config.KoskiApplication
+import fi.oph.koski.http.KoskiErrorCategory
 import fi.oph.koski.json.JsonSerializer
 import fi.oph.koski.koskiuser.RequiresKansalainen
 import fi.oph.koski.log.Logging
 import fi.oph.koski.servlet.{ApiServlet, NoCache}
+
+import scala.util.Try
 
 class SuoritusjakoServlet(implicit val application: KoskiApplication) extends ApiServlet with RequiresKansalainen with Logging with NoCache {
   get("/:uuid") {
@@ -13,8 +16,12 @@ class SuoritusjakoServlet(implicit val application: KoskiApplication) extends Ap
 
   put("/") {
     withJsonBody({ body =>
-      val suoritusIds = JsonSerializer.extract[List[SuoritusIdentifier]](body)
-      renderEither(application.suoritusjakoService.put(koskiSession.oid, suoritusIds).right.map(SuoritusjakoResponse))
+      Try(JsonSerializer.extract[List[SuoritusIdentifier]](body)).toOption match {
+        case Some(suoritusIds) =>
+          renderEither(application.suoritusjakoService.put(koskiSession.oid, suoritusIds).right.map(SuoritusjakoResponse))
+        case None =>
+          haltWithStatus(KoskiErrorCategory.badRequest.format())
+      }
     })()
   }
 }
