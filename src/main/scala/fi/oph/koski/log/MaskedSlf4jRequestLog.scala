e1c23aaefdc1c4c62b2dfab900c6aef44991f22c
Pasi Eronen <pasi.eronen@reaktor.com>
2018-04-06 10:57:09 +0300

    Piilota jaettujen linkkien salaisuus access logista

---
+++ src/main/scala/fi/oph/koski/log/MaskedSlf4jRequestLog.scala
@@ -0,0 +1,15 @@
+package fi.oph.koski.log
+
+import java.io.IOException
+import org.eclipse.jetty.server.Slf4jRequestLog
+
+class MaskedSlf4jRequestLog extends Slf4jRequestLog {
+  @throws[IOException]
+  override def write(requestEntry: String): Unit = {
+    super.write(maskSensitiveInformation(requestEntry))
+  }
+
+  private def maskSensitiveInformation(s: String): String = {
+    s.replaceAll("(/koski/opinnot/[0-9a-f]{8})([0-9a-f]+)", "$1************************")
+  }
+}
