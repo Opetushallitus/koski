f7f237d36931a5144bd6f98815544999fd7ed994
Petri Myllys <petri.myllys@reaktor.com>
2018-04-12 12:55:58 +0300

    Fit url

--- web/app/omattiedot/suoritusjako/SuoritusjakoLink.jsx
+++ web/app/omattiedot/suoritusjako/SuoritusjakoLink.jsx
@@ -6,12 +6,12 @@ import {parseISODate} from '../../date/date'
 
 export const SuoritusjakoLink = ({suoritusjako}) => {
   const {secret, expirationDate} = suoritusjako
+  const url = `${window.location.origin}/koski/opinnot/${secret}`
 
   return (
     <div className='suoritusjako-link'>
       <div className='suoritusjako-link__top-container'>
-        <CopyableText className='suoritusjako-link__url' message={secret} multiline={false}/>
-        <a className='suoritusjako-link__preview' href=''><Text name='Esikatsele'/></a>
+        <CopyableText className='suoritusjako-link__url' message={url} multiline={false}/>
         <div className='suoritusjako-link__expiration'>
           <label>
             <Text name='Linkin voimassaoloaika'/>
@@ -21,6 +21,9 @@ export const SuoritusjakoLink = ({suoritusjako}) => {
         </div>
       </div>
       <div className='suoritusjako-link__bottom-container'>
+        <div className='suoritusjako-link__preview'>
+          <a target='_blank' href={url}><Text name='Esikatsele'/></a>
+        </div>
         <div className='suoritusjako-link__remove'>
           <a href=''><Text name='Poista linkki käytöstä'/></a>
         </div>
