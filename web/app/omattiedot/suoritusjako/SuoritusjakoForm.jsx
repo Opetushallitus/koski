8c63b2db3a02227f5c0b0f8e97af8978c962e145
Petri Myllys <petri.myllys@reaktor.com>
2018-04-12 17:25:00 +0300

    Piilota uuden jaon lisäys kun jo olemassaolevia jakoja

--- web/app/omattiedot/suoritusjako/SuoritusjakoForm.jsx
+++ web/app/omattiedot/suoritusjako/SuoritusjakoForm.jsx
@@ -5,6 +5,7 @@ import Http from '../../util/http'
 import Text from '../../i18n/Text'
 import {SuoritusjakoLink} from './SuoritusjakoLink'
 import {SelectableSuoritusList} from './SelectableSuoritusList'
+import {ToggleButton} from '../../components/ToggleButton'
 
 const Url = '/koski/api/suoritusjako'
 const doShare = suoritusIds => Http.put(Url, [...suoritusIds])
@@ -19,16 +20,20 @@ const Ingressi = () => (
 )
 
 const SuoritusjakoList = ({opiskeluoikeudet, suoritusjaot}) => (
-  <ul className='suoritusjako-form__link-list'>
-    {suoritusjaot.map(suoritusjako => (
-      <li key={suoritusjako.secret}>
-        <SuoritusjakoLink suoritusjako={suoritusjako} opiskeluoikeudet={opiskeluoikeudet}/>
-      </li>
-    ))}
-  </ul>
+  <div>
+    {!R.isEmpty(suoritusjaot) && <h2><Text name='Voimassaolevat linkit'/></h2>}
+
+    <ul className='suoritusjako-form__link-list'>
+      {suoritusjaot.map(suoritusjako => (
+        <li key={suoritusjako.secret}>
+          <SuoritusjakoLink suoritusjako={suoritusjako} opiskeluoikeudet={opiskeluoikeudet}/>
+        </li>
+      ))}
+    </ul>
+  </div>
 )
 
-const NewSuoritusjakoButton = ({selectedSuoritusIds, onSuccess}) => (
+const CreateNewSuoritusjakoButton = ({selectedSuoritusIds, onSuccess}) => (
   <div className='create-suoritusjako__button'>
     <button onClick={() => doShare(selectedSuoritusIds).onValue(onSuccess)}>
       <Text name='Jaa valitsemasi opinnot'/>
@@ -36,13 +41,23 @@ const NewSuoritusjakoButton = ({selectedSuoritusIds, onSuccess}) => (
   </div>
 )
 
-const NewSuoritusjako = ({opiskeluoikeudet, selectedSuoritusIds, onSuccess}) => (
+const NewSuoritusjako = ({opiskeluoikeudet, selectedSuoritusIds, onSuccess, showForm, canCancelForm}) => (
   <div className='suoritusjako-form__create-suoritusjako'>
-    <h2><Text name='Valitse jaettavat suoritustiedot'/></h2>
-    <div className='create-suoritusjako'>
-      <SelectableSuoritusList opiskeluoikeudet={opiskeluoikeudet} selectedSuoritusIds={selectedSuoritusIds}/>
-      <NewSuoritusjakoButton baret-lift selectedSuoritusIds={selectedSuoritusIds} onSuccess={onSuccess}/>
-    </div>
+    {showForm.map(show => show
+      ? (
+        <div>
+          <div className='create-suoritusjako-header-row'>
+            <h2><Text name='Valitse jaettavat suoritustiedot'/></h2>
+            {canCancelForm.map(canCancel => canCancel && <ToggleButton toggleA={showForm} text='Peruuta' style='text'/>)}
+          </div>
+          <div className='create-suoritusjako'>
+            <SelectableSuoritusList opiskeluoikeudet={opiskeluoikeudet} selectedSuoritusIds={selectedSuoritusIds}/>
+            <CreateNewSuoritusjakoButton baret-lift selectedSuoritusIds={selectedSuoritusIds} onSuccess={onSuccess}/>
+          </div>
+        </div>
+      )
+      : <ToggleButton toggleA={showForm} text='Luo uusi' style='text'/>
+    )}
   </div>
 )
 
@@ -52,9 +67,13 @@ export class SuoritusjakoForm extends React.Component {
 
     this.selectedSuoritusIds = Atom([])
     this.suoritusjaot = Atom([])
+
+    this.showNewSuoritusjakoForm = Atom(false)
+    this.canCancelForm = this.suoritusjaot.map(R.complement(R.isEmpty))
   }
 
   componentDidMount() {
+    this.suoritusjaot.onValue(suoritusjaot => this.showNewSuoritusjakoForm.set(R.isEmpty(suoritusjaot)))
     Http.get(Url).onValue(jaot => this.suoritusjaot.set(jaot))
   }
 
@@ -68,12 +87,14 @@ export class SuoritusjakoForm extends React.Component {
 
     return (
       <section className='suoritusjako-form'>
-        <Ingressi/>
+        {this.suoritusjaot.map(suoritusjaot => R.isEmpty(suoritusjaot) && <Ingressi/>)}
         <SuoritusjakoList baret-lift opiskeluoikeudet={opiskeluoikeudet} suoritusjaot={this.suoritusjaot}/>
         <NewSuoritusjako
           opiskeluoikeudet={opiskeluoikeudet}
           selectedSuoritusIds={this.selectedSuoritusIds}
           onSuccess={this.addLink.bind(this)}
+          showForm={this.showNewSuoritusjakoForm}
+          canCancelForm={this.canCancelForm}
         />
       </section>
     )
