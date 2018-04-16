733bab0660c507580b48b1fdb44162cf929a30e9
Petri Myllys <petri.myllys@reaktor.com>
2018-04-16 15:49:48 +0300

    Käsittele päivämäärämuutoksen odotus
    - Estä myös menneen päivämäärän valinta

--- web/app/omattiedot/suoritusjako/SuoritusjakoLink.jsx
+++ web/app/omattiedot/suoritusjako/SuoritusjakoLink.jsx
@@ -18,26 +18,59 @@ export class SuoritusjakoLink extends React.Component {
     super(props)
 
     this.state = {
-      isDeletePending: false
+      isDeletePending: false,
+      isDateUpdatePending: false
     }
 
-    this.dateValue = new Bacon.Bus()
+    this.dateChangeBus = new Bacon.Bus()
   }
 
   componentDidMount() {
-    this.dateValue
+    const debounced = this.dateChangeBus
       .toProperty()
       .changes()
-      .debounce(1000)
-      .onValue(date => doUpdate(this.props.suoritusjako.secret, formatISODate(date)))
+      .debounce(500)
+
+    debounced.onValue(() => this.setState({isDateUpdatePending: true}))
+
+    const update = debounced
+      .flatMapLatest(date => date && doUpdate(this.props.suoritusjako.secret, formatISODate(date)))
+
+    update.onValue(() => this.setState({isDateUpdatePending: false}))
+  }
+
+  static isDateInFuture(date) {
+    const today = new Date()
+    const tomorrow = new Date()
+    tomorrow.setDate(today.getDate() + 1)
+    tomorrow.setHours(0,0,0,0)
+
+    return date.getTime() >= tomorrow.getTime()
+  }
+
+  removeHandler() {
+    const {isDateUpdatePending} = this.state
+    const {suoritusjako, onRemove} = this.props
+    const {secret} = suoritusjako
+
+    if (isDateUpdatePending) return
+
+    this.setState({isDeletePending: true},
+      () => {
+        const res = doDelete(secret)
+        res.onValue(() => onRemove(suoritusjako))
+        res.onError(() => this.setState({isDeletePending: false}))
+      }
+    )
   }
 
   render() {
-    const {suoritusjako, onRemove} = this.props
+    const {isDeletePending, isDateUpdatePending} = this.state
+    const {suoritusjako} = this.props
     const {secret, expirationDate} = suoritusjako
     const url = `${window.location.origin}/koski/opinnot/${secret}`
 
-    return this.state.isDeletePending ? <SuoritusjakoLinkPlaceholder/>
+    return isDeletePending ? <SuoritusjakoLinkPlaceholder/>
       : (
         <div className='suoritusjako-link'>
           <div className='suoritusjako-link__top-container'>
@@ -46,10 +79,15 @@ export class SuoritusjakoLink extends React.Component {
               <label>
                 <Text name='Linkin voimassaoloaika'/>
                 <Text name='Päättyy'/>
-                <DateInput
-                  value={parseISODate(expirationDate)}
-                  valueCallback={date => this.dateValue.push(date)}
-                />
+                <div style={{display: 'inline'}} className={isDateUpdatePending ? 'ajax-indicator-right' : ''}>
+                  <DateInput
+                    value={parseISODate(expirationDate)}
+                    valueCallback={date => this.dateChangeBus.push(date)}
+                    validityCallback={isValid => !isValid && this.dateChangeBus.push(null)}
+                    isAllowedDate={SuoritusjakoLink.isDateInFuture}
+                    isPending={isDateUpdatePending}
+                  />
+                </div>
               </label>
             </div>
           </div>
@@ -58,12 +96,7 @@ export class SuoritusjakoLink extends React.Component {
               <a target='_blank' href={url}><Text name='Esikatsele'/></a>
             </div>
             <div className='suoritusjako-link__remove'>
-              <a onClick={() => this.setState({isDeletePending: true}, () => {
-                  const res = doDelete(secret)
-                  res.onValue(() => onRemove(suoritusjako))
-                  res.onError(() => this.setState({isDeletePending: false}))
-                }
-              )}>
+              <a className={`text-button${(isDateUpdatePending ? '--disabled' : '')}`} onClick={this.removeHandler.bind(this)}>
                 <Text name='Poista linkki käytöstä'/>
               </a>
             </div>
