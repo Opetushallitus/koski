8b90036c0f4b7afca755f80ad780cb59b62dad62
Pasi Eronen <pasi.eronen@reaktor.com>
2018-04-16 10:39:48 +0300

    Yksinkertaista henkilötietojen haku -testejä

--- src/test/scala/fi/oph/koski/api/OppijaSearchSpec.scala
+++ src/test/scala/fi/oph/koski/api/OppijaSearchSpec.scala
@@ -1,32 +1,20 @@
 package fi.oph.koski.api
 
-import java.time.LocalDate
-
-import fi.oph.koski.KoskiApplicationForTests
-import fi.oph.koski.documentation.AmmatillinenExampleData.stadinAmmattiopisto
-import fi.oph.koski.documentation.ExampleData
 import fi.oph.koski.henkilo.MockOppijat
 import fi.oph.koski.henkilo.MockOppijat._
 import fi.oph.koski.http.KoskiErrorCategory
 import fi.oph.koski.koskiuser.MockUsers.omniaKatselija
 import fi.oph.koski.log.AuditLogTester
-import fi.oph.koski.perustiedot.{NimitiedotJaOid, OpiskeluoikeudenPerustiedot, OpiskeluoikeusJaksonPerustiedot}
-import fi.oph.koski.schema.Henkilö.Oid
-import fi.oph.koski.schema.{Koodistokoodiviite, Oppilaitos}
 import org.scalatest.{FreeSpec, Matchers}
 
 class OppijaSearchSpec extends FreeSpec with Matchers with SearchTestMethods with LocalJettyHttpSpecification with OpiskeluoikeusTestMethodsAmmatillinen {
   "/api/henkilo/search" - {
     "Finds by name" in {
+      resetFixtures
       searchForNames("eero") should equal(List("Jouni Eerola", "Eero Esimerkki", "Eéro Jorma-Petteri Markkanen-Fagerström"))
     }
     "Find only those from your organization" in {
-      val oids = generatePerustiedotIntoElastic(10, "Jorma-Petteri", stadinAmmattiopisto)
-      try {
-        searchForNames("Jorma-Petteri", omniaKatselija) should equal(List("Eéro Jorma-Petteri Markkanen-Fagerström"))
-      } finally {
-        KoskiApplicationForTests.perustiedotIndexer.deleteByOppijaOids(oids)
-      }
+      searchForNames("eero", omniaKatselija) should equal(List("Eéro Jorma-Petteri Markkanen-Fagerström"))
     }
     "Finds by hetu" in {
       searchForNames("010101-123N") should equal(List("Eero Esimerkki"))
@@ -50,16 +38,5 @@ class OppijaSearchSpec extends FreeSpec with Matchers with SearchTestMethods wit
         }
       }
     }
-  }
-
-  private def generatePerustiedotIntoElastic(count: Int, name: String, oppilaitos: Oppilaitos): List[Oid] = {
-    val tyyppi = Koodistokoodiviite("ammatillinenkoulutus", "opiskeluoikeudentyyppi")
-    val tiedot = 0 to count map { i =>
-      val henkilöOid = s"1.2.246.562.24.000000000000$i"
-      OpiskeluoikeudenPerustiedot(i, Some(NimitiedotJaOid(henkilöOid, name, name, name)), Some(henkilöOid), stadinAmmattiopisto, None, None, None, tyyppi, Nil, Some(List(OpiskeluoikeusJaksonPerustiedot(LocalDate.now, None, ExampleData.opiskeluoikeusLäsnä))), None)
-    }
-    KoskiApplicationForTests.perustiedotIndexer.updateBulk(tiedot, true)
-    refreshElasticSearchIndex
-    tiedot.map(t => t.henkilö.get.oid).toList
   }
 }
