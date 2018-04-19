1a42b3f4aabedcc7697bfb67152f3f9b327d711a
Kalle Kainu <kalle.kainu@reaktor.fi>
2018-04-19 09:26:58 +0300

    Show ajax-indicator while deleting

--- web/app/tiedonsiirrot/Tiedonsiirtovirheet.jsx
+++ web/app/tiedonsiirrot/Tiedonsiirtovirheet.jsx
@@ -12,29 +12,33 @@ export const tiedonsiirtovirheetContentP = (queryString) => {
   const pager = Pager('/koski/api/tiedonsiirrot/virheet' + queryString, L.prop('henkilöt'))
 
   const selected = Atom([])
+  const deleting = Atom(false)
 
   const removeSelected = () => {
-    Http.post('/koski/api/tiedonsiirrot/delete', {ids: selected.get()}).onValue(() => {
-        selected.set([])
-        window.location.reload(true)
-      }
-    )
+    deleting.set(true)
+    Http.post('/koski/api/tiedonsiirrot/delete', {ids: selected.get()}).onValue(() => window.location.reload(true))
   }
 
-  let contentP = pager.rowsP.map(({henkilöt, oppilaitos}) =>
-    ({
-      content: (
-        <div className="tiedonsiirto-virheet">
-          <ReloadButton/>
-          <button className="remove-selected" disabled={selected.map(s => !s.length)} onClick={removeSelected}>Poista valitut</button>
-          <span><Text name="Alla olevien opiskelijoiden tiedot ovat virhetilassa"/><OppilaitosTitle oppilaitos={oppilaitos}/>{'.'}</span>
-          <p><Text name="Opiskelija poistuu virhelistalta"/></p>
-          <Tiedonsiirtotaulukko rivit={henkilöt} showError={true} pager={pager} selected={selected}/>
-        </div>
-      ),
-      title: 'Tiedonsiirtovirheet'
-    })
-  )
+  let contentP = deleting.not().flatMap(notDeleting => notDeleting
+    ? pager.rowsP.map(({henkilöt, oppilaitos}) =>
+      ({
+        content: (
+          <div className="tiedonsiirto-virheet">
+            <ReloadButton/>
+            <button className="remove-selected" disabled={selected.map(s => !s.length)} onClick={removeSelected}>Poista
+              valitut
+            </button>
+            <span><Text name="Alla olevien opiskelijoiden tiedot ovat virhetilassa"/><OppilaitosTitle
+              oppilaitos={oppilaitos}/>{'.'}</span>
+            <p><Text name="Opiskelija poistuu virhelistalta"/></p>
+            <Tiedonsiirtotaulukko rivit={henkilöt} showError={true} pager={pager} selected={selected}/>
+          </div>
+        ),
+        title: 'Tiedonsiirtovirheet'
+      })
+    )
+    : ({content: <div className="ajax-loading-placeholder"><Text name='Poistetaan...'/></div>, title: 'Tiedonsiirtovirheet'})
+  ).toProperty()
 
   return tiedonsiirrotContentP('/koski/tiedonsiirrot/virheet', contentP)
 }
