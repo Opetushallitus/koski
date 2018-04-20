0c8ac3aa9cf0dc1181908b4c40c9a3a93716b8b9
Kalle Kainu <kalle.kainu@reaktor.fi>
2018-04-20 13:19:40 +0300

    Reformat

--- web/test/page/tiedonsiirrotPage.js
+++ web/test/page/tiedonsiirrotPage.js
@@ -4,13 +4,13 @@ function TiedonsiirrotPage() {
   }
 
   var api = {
-    openPage: function () {
+    openPage: function() {
       return openPage('/koski/tiedonsiirrot', api.isVisible)()
     },
-    isVisible: function () {
+    isVisible: function() {
       return isElementVisible(S('#content .tiedonsiirrot-content .tiedonsiirto-taulukko'))
     },
-    tiedot: function () {
+    tiedot: function() {
       return S('.tiedonsiirrot-content table tbody tr').toArray().map(function (row) {
         return $(row).find('td:not(.tila):not(.aika):not(.valitse)').toArray().map(function (td) {
           return $(td).text().trim()
