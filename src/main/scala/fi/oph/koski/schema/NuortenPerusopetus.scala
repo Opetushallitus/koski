f84a53b71843c3c7373a22742c5563d81aa6728b
Petri Myllys <petri.myllys@reaktor.com>
2018-05-03 10:34:43 +0300

    Lisää kenttä perusopetuksen erityisopetuksen toteutuspaikalle

--- src/main/scala/fi/oph/koski/schema/NuortenPerusopetus.scala
+++ src/main/scala/fi/oph/koski/schema/NuortenPerusopetus.scala
@@ -159,7 +159,9 @@ Huom: toiminta-alue arviointeineen on kuvattu oppiaineen suorituksessa.""")
   @Tooltip("Suorittaako erityisoppilas koulutusta omassa erityisryhmässään vai inklusiivisesti opetuksen mukana.")
   @OksaUri("tmpOKSAID444", "opetusryhmä")
   @Title("Opiskelee erityisryhmässä")
-  erityisryhmässä: Boolean
+  erityisryhmässä: Boolean,
+  @KoodistoUri("erityisopetuksentoteutuspaikka")
+  toteutuspaikka: Option[Koodistokoodiviite] = None
 )
 
 trait PerusopetuksenPäätasonSuoritus extends KoskeenTallennettavaPäätasonSuoritus with Toimipisteellinen with MonikielinenSuoritus with Suorituskielellinen
