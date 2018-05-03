773f9f9ee415d26c07dbb81f2202456fa178116b
Petri Myllys <petri.myllys@reaktor.com>
2018-05-03 10:16:38 +0300

    Käytä lokalisointeja tiedot palvelussa -teksteissä

--- web/app/omattiedot/EiSuorituksiaInfo.jsx
+++ web/app/omattiedot/EiSuorituksiaInfo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {TiedotPalvelussa} from './TiedotPalvelussa'
+import {t} from '../i18n/i18n'
 
 export const EiSuorituksiaInfo = () => (
   <div className='ei-suorituksia'>
-    <h2>{'Tiedoillasi ei löydy opintosuorituksia eikä opiskeluoikeuksia.'}</h2>
+    <h2>{t('Tiedoillasi ei löydy opintosuorituksia eikä opiskeluoikeuksia.')}</h2>
     <TiedotPalvelussa/>
-    <p>{'Mikäli tiedoistasi puuttuu opintosuorituksia tai opiskeluoikeuksia, joiden kuuluisi ylläolevien tietojen perusteella näkyä Koski-palvelussa, voit ilmoittaa asiasta oppilaitoksellesi.'}</p>
+    <p>{t('Mikäli tiedoistasi puuttuu opintosuorituksia tai opiskeluoikeuksia, joiden kuuluisi ylläolevien tietojen perusteella näkyä Koski-palvelussa, voit ilmoittaa asiasta oppilaitoksellesi.')}</p>
   </div>
 )
--- web/app/omattiedot/TiedotPalvelussa.jsx
+++ web/app/omattiedot/TiedotPalvelussa.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import {t} from '../i18n/i18n'
 
 export const TiedotPalvelussa = () => (
   <div className='tiedot-palvelussa'>
-    <p>{'Koski-palvelussa pystytään näyttämään seuraavat tiedot:'}</p>
+    <p>{t('Koski-palvelussa pystytään näyttämään seuraavat tiedot:')}</p>
     <ul>
-      <li>{'Vuoden 2018 tammikuun jälkeen suoritetut peruskoulun, lukion ja ammattikoulun opinnot ja voimassa olevat opiskeluoikeudet.'}</li>
-      <li>{'Vuoden 1990 jälkeen suoritetut ylioppilastutkinnot.'}</li>
-      <li>{'Korkeakoulutusuoritukset ja opiskeluoikeudet ovat näkyvissä pääsääntöisesti vuodesta 1995 eteenpäin, mutta tässä voi olla korkeakoulukohtaisia poikkeuksia.'}</li>
+      <li>{t('Vuoden 2018 tammikuun jälkeen suoritetut peruskoulun, lukion ja ammattikoulun opinnot ja voimassa olevat opiskeluoikeudet.')}</li>
+      <li>{t('Vuoden 1990 jälkeen suoritetut ylioppilastutkinnot.')}</li>
+      <li>{t('Korkeakoulutusuoritukset ja opiskeluoikeudet ovat näkyvissä pääsääntöisesti vuodesta 1995 eteenpäin, mutta tässä voi olla korkeakoulukohtaisia poikkeuksia.')}</li>
     </ul>
   </div>
 )
