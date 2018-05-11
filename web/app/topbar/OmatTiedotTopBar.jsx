a542abc321dc4b2bceb0c0a3cddfc2d2aab64b0b
Petri Myllys <petri.myllys@reaktor.com>
2018-05-11 15:28:32 +0300

    Fix content overflowing on IE
    - IE overflows the content when the side paddings are negative (on
    narrow windows)

--- web/app/topbar/OmatTiedotTopBar.jsx
+++ web/app/topbar/OmatTiedotTopBar.jsx
@@ -5,13 +5,15 @@ import Text from '../i18n/Text'
 
 export default ({user}) => {
   return (
-    <header id='topbar' className="local">
-      <div id='logo'><Text name="Opintopolku.fi"/></div>
-      <h1>
-        <Link href="/koski/"><Text name="Koski"/></Link>
-        <span>{' - '}<Text name="Omat tiedot"/></span>
-      </h1>
-      <UserInfo user={user}/>
+    <header id='topbar' className="local topbar-omattiedot">
+      <div className='topbar-content-wrapper'>
+        <div id='logo'><Text name="Opintopolku.fi"/></div>
+        <h1>
+          <Link href="/koski/"><Text name="Koski"/></Link>
+          <span>{' - '}<Text name="Omat tiedot"/></span>
+        </h1>
+        <UserInfo user={user}/>
+      </div>
     </header>
   )
 }
