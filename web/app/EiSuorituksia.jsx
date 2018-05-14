dc909e2a798dba15f9316c7491dd7132af652ee7
Petri Myllys <petri.myllys@reaktor.com>
2018-05-14 15:27:08 +0300

    Muuta ei suorituksia -sivun otsikointi landeria vastaavaksi

--- web/app/EiSuorituksia.jsx
+++ web/app/EiSuorituksia.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style/main.less'
 import {EiSuorituksiaInfo} from './omattiedot/EiSuorituksiaInfo'
+import {t} from './i18n/i18n'
 
 ReactDOM.render((
   <div>
-    <div className='koski-heading'><h1>{'KOSKI'}</h1></div>
+    <h1>{t('Opintoni')}</h1>
     <EiSuorituksiaInfo/>
   </div>
 ), document.getElementById('content'))
