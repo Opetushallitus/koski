18585a406612da7ad2c0056337c557dc5e4306df
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-05-15 11:46:11 +0300

    Lisää raamit virhesivulle ja eisuorituksiasivulle

--- src/main/scala/fi/oph/koski/servlet/EiSuorituksiaServlet.scala
+++ src/main/scala/fi/oph/koski/servlet/EiSuorituksiaServlet.scala
@@ -3,8 +3,8 @@ package fi.oph.koski.servlet
 import fi.oph.koski.config.KoskiApplication
 import org.scalatra.ScalatraServlet
 
-class EiSuorituksiaServlet(implicit val application: KoskiApplication) extends ScalatraServlet with HtmlServlet {
+class EiSuorituksiaServlet(implicit val application: KoskiApplication) extends ScalatraServlet with HtmlServlet with OppijaRaamitSupport {
   get("/") {
-    htmlIndex("koski-eisuorituksia.js", responsive = true)
+    htmlIndex("koski-eisuorituksia.js", raamit = oppijaRaamit, responsive = true)
   }
 }
