2fd9b499dc85fc0e9e85c25e57407193edfeae36
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-05-28 20:00:26 +0300

    Added texts to mydata authorization page

--- web/app/omadata/Hyvaksynta.jsx
+++ web/app/omadata/Hyvaksynta.jsx
@@ -7,11 +7,27 @@ import {lang} from '../i18n/i18n'
 
 const Hyvaksynta = () => (
   <div>
-    <div className="koski-heading"><h1>{'Oma Opintopolku'}</h1></div>
-    <div className="koski-mydata-body">
-
+    <div className="header">
+      <div className="title"><h1><Text name="Oma Opintopolku"/></h1></div>
+      <div className="user">
+        <div className="username">Clara Nieminen</div>
+        <div className="logout"><Text name="Kirjaudu ulos"/></div>
+      </div>
     </div>
-    <div className="lander-logo">
+    <div className="heading"><h1><Text name="Henkilökohtaisten tietojen käyttö"/></h1></div>
+    <div className="user"></div>
+    <div className="acceptance-box">
+      <div className="acceptance-title"><Text name="Omadata hyväksyntä otsikko"/></div>
+      <div className="acceptance-member-name"> </div>
+      <div className="acceptance-share-info">
+        <Text name="Palveluntarjoaja näkee"/>
+        <Text name="Näkee läsnäolotiedot"/>
+        <Text name="Näkee oppilaitoksen tiedot"/>
+      </div>
+      <div className="acceptance-button-container"> </div>
+    </div>
+
+    <div className="footer">
       <img src="/koski/images/oph_fin_vaaka.png" />
       <img src="/koski/images/logo_okm.png" />
     </div>
