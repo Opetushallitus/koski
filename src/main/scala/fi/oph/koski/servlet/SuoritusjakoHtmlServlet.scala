1a0674b0d18391518f63edd812df5c93eb0cc4d8
Petri Myllys <petri.myllys@reaktor.com>
2018-05-28 14:59:34 +0300

    Näytä raamit suoritusjaon katselussa kielenvaihtoa varten

--- src/main/scala/fi/oph/koski/servlet/SuoritusjakoHtmlServlet.scala
+++ src/main/scala/fi/oph/koski/servlet/SuoritusjakoHtmlServlet.scala
@@ -3,8 +3,8 @@ package fi.oph.koski.servlet
 import fi.oph.koski.config.KoskiApplication
 import org.scalatra.ScalatraServlet
 
-class SuoritusjakoHtmlServlet(implicit val application: KoskiApplication) extends ScalatraServlet with HtmlServlet {
+class SuoritusjakoHtmlServlet(implicit val application: KoskiApplication) extends ScalatraServlet with HtmlServlet with OmaOpintopolkuSupport {
   get("/:secret") {
-    htmlIndex("koski-suoritusjako.js", responsive = true)
+    htmlIndex("koski-suoritusjako.js", raamit = oppijaRaamit, responsive = true)
   }
 }
