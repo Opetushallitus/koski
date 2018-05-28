9c7ac0f596b3f6fb87679df641b6539987bd9cbb
Petri Myllys <petri.myllys@reaktor.com>
2018-05-28 15:33:30 +0300

    Aseta kieli domainin perusteella suoritusjaoissa

--- src/main/scala/fi/oph/koski/servlet/SuoritusjakoHtmlServlet.scala
+++ src/main/scala/fi/oph/koski/servlet/SuoritusjakoHtmlServlet.scala
@@ -5,6 +5,7 @@ import org.scalatra.ScalatraServlet
 
 class SuoritusjakoHtmlServlet(implicit val application: KoskiApplication) extends ScalatraServlet with HtmlServlet with OmaOpintopolkuSupport {
   get("/:secret") {
+    setLangCookieFromDomainIfNecessary
     htmlIndex("koski-suoritusjako.js", raamit = oppijaRaamit, responsive = true)
   }
 }
