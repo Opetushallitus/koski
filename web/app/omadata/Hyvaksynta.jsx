f2d065052ba60672b07024f7d294b83a70c2ad3c
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-05-29 10:29:11 +0300

    acceptance styling

--- web/app/omadata/Hyvaksynta.jsx
+++ web/app/omadata/Hyvaksynta.jsx
@@ -14,19 +14,23 @@ const Hyvaksynta = () => (
         <div className="logout"><Text name="Kirjaudu ulos"/></div>
       </div>
     </div>
-    <div className="heading"><h1><Text name="Henkilökohtaisten tietojen käyttö"/></h1></div>
-    <div className="user"></div>
-    <div className="acceptance-box">
-      <div className="acceptance-title"><Text name="Omadata hyväksyntä otsikko"/></div>
-      <div className="acceptance-member-name"> </div>
-      <div className="acceptance-share-info">
-        <Text name="Palveluntarjoaja näkee"/>
-        <Text name="Näkee läsnäolotiedot"/>
-        <Text name="Näkee oppilaitoksen tiedot"/>
-      </div>
-      <div className="acceptance-button-container"> </div>
-    </div>
 
+    <div className="acceptance-container">
+      <div className="heading"><h1><Text name="Henkilökohtaisten tietojen käyttö"/></h1></div>
+      <div className="user">Clara Nieminen <span className="dateofbirth">s. 25.07.1988</span></div>
+      <div className="acceptance-box">
+        <div className="acceptance-title"><Text name="Omadata hyväksyntä otsikko"/></div>
+        <div className="acceptance-member-name">HSL Helsingin Seudun Liikenne</div>
+        <div className="acceptance-share-info">
+          <Text name="Palveluntarjoaja näkee"/>
+          <ul>
+            <li><Text name="Näkee läsnäolotiedot"/></li>
+            <li><Text name="Näkee oppilaitoksen tiedot"/></li>
+          </ul>
+        </div>
+        <div className="acceptance-button-container"> </div>
+      </div>
+    </div>
     <div className="footer">
       <img src="/koski/images/oph_fin_vaaka.png" />
       <img src="/koski/images/logo_okm.png" />
