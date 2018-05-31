f989f61515aca4e57700013995f86ec4a3467640
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-05-31 15:38:54 +0300

    get username and date of birth from our APIs

--- web/app/omadata/Header.jsx
+++ web/app/omadata/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Text from '../i18n/Text'
 
-export default ({ firstName, lastName }) => (
+export default ({ name }) => (
   <div className="header">
     <div className="title"><h1><Text name="Oma Opintopolku"/></h1></div>
     <div className="user">
-      <div className="username">{firstName} {lastName}</div>
+      <div className="username">{name}</div>
       <div className="logout"><Text name="Kirjaudu ulos"/></div>
     </div>
   </div>
--- web/app/omadata/HyvaksyntaLanding.jsx
+++ web/app/omadata/HyvaksyntaLanding.jsx
@@ -4,11 +4,13 @@ import AnnaHyvaksynta from './AnnaHyvaksynta'
 import HyvaksyntaAnnettu from './HyvaksyntaAnnettu'
 import Footer from './Footer'
 import Header from './Header'
-import {formatFinnishDate} from '../date/date.js'
+import {formatFinnishDate, parseISODate} from '../date/date.js'
 import Text from '../i18n/Text'
 import '../polyfills/polyfills.js'
 import Http from '../util/http'
 import { currentLocation, parseQuery } from '../util/location'
+import {userP} from '../util/user'
+
 
 const memberCodeRegex = /\/koski\/mydata\/(.*)/
 
@@ -17,11 +19,8 @@ class HyvaksyntaLanding extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      firstName: 'Clara',
-      lastName: 'Nieminen',
-      dateOfBirth: new Date('December 17, 1995 03:24:00'),
       memberName: 'HSL Helsingin Seudun Liikenne',
-      authorizationGiven: true,
+      authorizationGiven: false,
       memberCode: this.getMemberCodeFromRequest(),
       callback: parseQuery(currentLocation().queryString).callback
     }
@@ -29,6 +28,14 @@ class HyvaksyntaLanding extends React.Component {
     this.postAuthorization = this.postAuthorization.bind(this)
 
     console.log(`Membercode: ${this.state.memberCode}, callback: ${this.state.callback}`)
+
+    userP.onValue((value) => {
+      this.setState({
+        name: value.name
+      })
+    })
+
+    this.initializeBirthDate()
   }
 
   getMemberCodeFromRequest() {
@@ -37,6 +44,26 @@ class HyvaksyntaLanding extends React.Component {
     return (captureGroups && captureGroups.length > 1 && captureGroups[1]) ?
       captureGroups[1] :
       null
+  }
+
+  initializeBirthDate() {
+    try {
+      Http.cachedGet('/koski/api/omattiedot/editor', {})
+        .doError(() => {} )
+        .onValue((response) => {
+          const dateOfBirth = response.value.properties.find(p => p.key === 'henkilö')
+            .model.value.properties.find(p => p.key === 'syntymäaika')
+            .model.value.data
+
+          this.setState({
+            dateOfBirth: parseISODate(dateOfBirth)
+          })
+        })
+    } catch (e) {
+      console.log('Failed to get user birth date')
+      console.log(e)
+    }
+
   }
 
   postAuthorization() {
@@ -60,13 +87,17 @@ class HyvaksyntaLanding extends React.Component {
       <HyvaksyntaAnnettu callback={this.state.callback}/> :
       <AnnaHyvaksynta memberName={this.state.memberName} onAcceptClick={this.postAuthorization} />
 
+    const birthDate = this.state.dateOfBirth ?
+      ` s. ${formatFinnishDate(this.state.dateOfBirth)}` :
+      ''
+
     return (
       <div>
-        <Header firstName={this.state.firstName} lastName={this.state.lastName}/>
+        <Header name={this.state.name}/>
 
         <div className="acceptance-container">
           <div className="heading"><h1><Text name="Henkilökohtaisten tietojen käyttö"/></h1></div>
-          <div className="user">{this.state.firstName} {this.state.lastName}<span className="dateofbirth"> s. {formatFinnishDate(this.state.dateOfBirth)}</span></div>
+          <div className="user">{this.state.name}<span className="dateofbirth">{birthDate}</span></div>
 
           {acceptanceBox}
         </div>
