d5cf3ba68269565a59841f30e6ce06b210bfb471
Petri Myllys <petri.myllys@reaktor.com>
2018-05-31 10:06:03 +0300

    Poista tietosuojaselostelinkki vanhalta länderiltä

--- web/app/Lander.jsx
+++ web/app/Lander.jsx
@@ -16,18 +16,10 @@ const LanderInfo = () => (
         <p><Text name='Lander ingressi 3'/></p>
         <p><Text name='Lander ingressi 4'/></p>
       </div>
-      <p className="tietosuojaseloste"><TietosuojaselosteLinkki><Text name="KOSKI-palvelun tietosuojaseloste (sisältää rekisteriselosteen)"/></TietosuojaselosteLinkki></p>
       <button className='login-button' onClick={() => window.location=window.kansalaisenAuthUrl}><Text name="Kirjaudu sisään" /></button>
     </div>
   </div>
 )
-
-const TietosuojaselosteLinkki = ({children}) => {
-  const linkki = (lang === 'sv')
-    ? 'https://confluence.csc.fi/download/attachments/58828884/Dataskyddsbeskrivning%20KOSKI%2012.2.2018.pdf?api=v2'
-    : 'https://confluence.csc.fi/download/attachments/64962405/Tietosuojaseloste%20-%20KOSKI-palvelu%2023.5.2018.pdf?api=v2'
-  return <a href={linkki}>{children}</a>
-}
 
 ReactDOM.render((
   <div>
