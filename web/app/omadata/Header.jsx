db00d09b6eb3cb15bf8ca856be4b9a8e2f22d39c
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-01 10:25:15 +0300

    use Bacon for state

--- web/app/omadata/Header.jsx
+++ web/app/omadata/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'baret'
 import Text from '../i18n/Text'
 
-export default ({ name }) => (
+export default ({ userP }) => (
   <div className="header">
     <div className="title"><h1><Text name="Oma Opintopolku"/></h1></div>
     <div className="user">
-      <div className="username">{name}</div>
+      <div className="username">{ userP.map(user => user.name) }</div>
       <div className="logout"><Text name="Kirjaudu ulos"/></div>
     </div>
   </div>
