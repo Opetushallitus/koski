0876a6e04f0eef072326c75aeb502c55917de3ba
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-06 10:37:25 +0300

    added menu button for mobile layout

--- web/app/omadata/Header.jsx
+++ web/app/omadata/Header.jsx
@@ -3,10 +3,17 @@ import Text from '../i18n/Text'
 
 export default ({ userP }) => (
   <div className="header">
+    <button id="header-mobile-menu-button"><img src="/koski/images/baseline-menu-24px.svg" /></button>
     <div className="title"><h1><Text name="Oma Opintopolku"/></h1></div>
     <div className="user">
       <div className="username">{ userP.map(user => user && user.name ) }</div>
       <div className="logout"><Text name="Kirjaudu ulos"/></div>
+    </div>
+    <div id="header-mobile-menu">
+      <div className="top">
+        <div className="username">{ userP.map(user => user && user.name ) }</div>
+        <div className="logout"><Text name="Kirjaudu ulos"/></div>
+      </div>
     </div>
   </div>
 )
