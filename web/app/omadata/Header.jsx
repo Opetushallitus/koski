a6a5ce2d9fedf4f3ced6b67126e82929701bc85e
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-08 12:52:15 +0300

    added user icon to header

--- web/app/omadata/Header.jsx
+++ web/app/omadata/Header.jsx
@@ -9,7 +9,10 @@ export default ({ userP }) => (
     <button id="header-mobile-menu-button" onClick={() => menuOpened.set(!menuOpened.get())}><img src="/koski/images/baseline-menu-24px.svg" /></button>
     <div className="title"><h1><Text name="Oma Opintopolku"/></h1></div>
     <div className="user">
-      <div className="username">{ userP.map(user => user && user.name ) }</div>
+      <div className="username">
+        <img src="/koski/images/profiili.svg" alt="user-icon" />
+        { userP.map(user => user && user.name ) }
+      </div>
       <div className="logout"><Text name="Kirjaudu ulos"/></div>
     </div>
 
