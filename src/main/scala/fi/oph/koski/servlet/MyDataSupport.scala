62c41a08152683ad664b09538052cf3db12aff11
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-12 19:31:30 +0300

    throw sane exception if mydata member not found

--- src/main/scala/fi/oph/koski/servlet/MyDataSupport.scala
+++ src/main/scala/fi/oph/koski/servlet/MyDataSupport.scala
@@ -1,6 +1,8 @@
 package fi.oph.koski.servlet
 
 import fi.oph.koski.config.KoskiApplication
+import fi.oph.koski.http.KoskiErrorCategory
+
 import scala.collection.JavaConverters._
 
 
@@ -9,7 +11,7 @@ trait MyDataSupport extends LanguageSupport {
 
   def getConfigForMember(id: String): com.typesafe.config.Config = {
     application.config.getConfigList("mydata.members").asScala.find(member =>
-      member.getString("id") == id).get
+      member.getString("id") == id).getOrElse(throw InvalidRequestException(KoskiErrorCategory.notFound.myDataMemberEiLöydy))
   }
 
   def getLoginUrlForMember(id: String): String = getConfigForMember(id).getString("login." + langFromCookie.getOrElse(langFromDomain))
