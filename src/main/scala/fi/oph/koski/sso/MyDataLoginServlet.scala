cb0ee614613f803fd806e9b57945279d5ae36a1f
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-12 15:09:03 +0300

    commented reason for extending the existing login servlet

--- src/main/scala/fi/oph/koski/sso/MyDataLoginServlet.scala
+++ src/main/scala/fi/oph/koski/sso/MyDataLoginServlet.scala
@@ -4,7 +4,10 @@ import fi.oph.koski.config.KoskiApplication
 
 case class MyDataLoginServlet(override val application: KoskiApplication) extends AbstractLoginServlet(application) {
 
-  // ScalatraBootstrap cannot mount one class two times without issues, so lets extend and reconfigure based on our needs
+  /* Trying to mount one class two times (two instances) in ScalatraBootstrap causes issues: the one mounted first will
+     get all traffic, regardless of mount path.
+     So we'll create a new class to bypass this issue and just override the required functions.
+   */
   override protected def onSuccess: String = "/omadata/hsl"
   override protected def onFailure: String = "/omadata/failed"
   override protected def onUserNotFound: String = "/omadata/usernotfound"
