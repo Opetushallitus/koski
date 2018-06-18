056e6f25f86743d8cadf22d9d315ef121451fa75
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-18 15:19:29 +0300

    remove copy-pasta

--- src/test/scala/fi/oph/koski/mydata/MyDataSupportTest.scala
+++ src/test/scala/fi/oph/koski/mydata/MyDataSupportTest.scala
@@ -11,33 +11,30 @@ class MyDataSupportTest extends FreeSpec with Matchers with MockFactory {
   val memberId = "hsl"
   val lang = "fi"
 
+  def support(mockRequest: HttpServletRequest): MyDataSupport = {
+    new MyDataSupport {
+      override def application = KoskiApplicationForTests
+      override def getCurrentURL(implicit httpServletRequest: HttpServletRequest): String = super.getCurrentURL(mockRequest)
+    }
+  }
+
   "MyDataSupport" - {
     "Palauttaa oikean Korhopankki-URL:n sisään loggautumattomille" in {
       val mockRequest = mock[HttpServletRequest]
       (mockRequest.getQueryString _).expects().returning("callback=http://www.hsl.fi").repeat(2)
       (mockRequest.getRequestURI _).expects().returning("/koski/omadata/hsl")
 
-      def support: MyDataSupport = new MyDataSupport {
-        override def application = KoskiApplicationForTests
-        override def getCurrentURL(implicit httpServletRequest: HttpServletRequest): String = super.getCurrentURL(mockRequest)
-      }
-
-      support.getLoginUrlForMember(memberId, lang) should
+      support(mockRequest).getLoginUrlForMember(memberId, lang) should
         equal("/koski/login/shibboleth?login=/koski/user/omadatalogin/hsl%3FonLoginSuccess%3D%2Fkoski%2Fomadata%2Fhsl%3Fcallback%3Dhttp%3A%2F%2Fwww.hsl.fi")
 
     }
     "Palauttaa oikean URL:n sisään loganneille" in {
       val mockRequest = mock[HttpServletRequest]
-      (mockRequest.getQueryString _).expects().returning("callback=http://www.hsl.fi").repeat(2)
+      (mockRequest.getQueryString _).expects().returning("callback=http://www.hsl.fi/alennus").repeat(2)
       (mockRequest.getRequestURI _).expects().returning("/koski/omadata/hsl")
 
-      def support: MyDataSupport = new MyDataSupport {
-        override def application = KoskiApplicationForTests
-        override def getCurrentURL(implicit httpServletRequest: HttpServletRequest): String = super.getCurrentURL(mockRequest)
-      }
-
-      support.getLoginSuccessTarget(memberId, lang) should
-        equal("/koski/user/omadatalogin/hsl?onLoginSuccess=/koski/omadata/hsl?callback=http://www.hsl.fi")
+      support(mockRequest).getLoginSuccessTarget(memberId, lang) should
+        equal("/koski/user/omadatalogin/hsl?onLoginSuccess=/koski/omadata/hsl?callback=http://www.hsl.fi/alennus")
     }
   }
 }
