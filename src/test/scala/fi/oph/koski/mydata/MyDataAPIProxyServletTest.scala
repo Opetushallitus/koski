02e1f4a10953db99d02610205129a06b7029edc5
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-25 15:58:43 +0300

    Test that data can be pulled when student has permitted it

--- src/test/scala/fi/oph/koski/mydata/MyDataAPIProxyServletTest.scala
+++ src/test/scala/fi/oph/koski/mydata/MyDataAPIProxyServletTest.scala
@@ -7,11 +7,13 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class MyDataAPIProxyServletTest extends FreeSpec with LocalJettyHttpSpecification with Matchers with HttpTester {
 
-  def oid = "1.2.3.4.5"
-  implicit def application = KoskiApplicationForTests
+  val oid = "1.2.246.562.24.00000000023"
+  val memberId = "hsl"
+  val memberCode = "2769790-1" // HSL
+
+  def application = KoskiApplicationForTests
 
   "ApiProxyServlet" - {
-
     "Ei palauta mitään mikäli X-ROAD-MEMBER headeria ei ole asetettu" in {
       authGet(s"api/omadata/oppija/${oid}") {
         status should equal(400)
@@ -20,9 +22,22 @@ class MyDataAPIProxyServletTest extends FreeSpec with LocalJettyHttpSpecificatio
     }
 
     "Ei palauta mitään mikäli käyttäjä ei ole antanut lupaa" in {
-      authGet(s"api/omadata/oppija/${oid}", headers = Map("X-ROAD-MEMBER" -> "2769790-1")) {
+      KoskiApplicationForTests.mydataRepository.delete(oid, memberId)
+
+      authGet(s"api/omadata/oppija/${oid}", headers = Map("X-ROAD-MEMBER" -> memberCode)) {
         status should equal(400)
         body should include("X-ROAD-MEMBER:llä ei ole lupaa hakea opiskelijan tietoja")
+      }
+    }
+
+    "Palauttaa opiskelutiedot mikäli käyttäjä on antanut siihen luvan" in {
+      KoskiApplicationForTests.mydataRepository.create(oid, memberId)
+
+      authGet(s"api/omadata/oppija/${oid}", headers = Map("X-ROAD-MEMBER" -> memberCode)) {
+        status should equal(200)
+        /* Cannot verify response body, it will throw "java.io.IOException: write beyond end of stream"
+           when servletContext.getRequestDispatcher().forward() is called within tests
+         */
       }
     }
 
