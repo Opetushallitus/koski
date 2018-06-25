760001d9b93c25b938c7501cc4ac2b20e8719a15
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-25 13:14:00 +0300

    Initial tests for mydata servlet

---
+++ src/test/scala/fi/oph/koski/mydata/MyDataAPIProxyServletTest.scala
@@ -0,0 +1,26 @@
+package fi.oph.koski.mydata
+
+import fi.oph.koski.KoskiApplicationForTests
+import fi.oph.koski.api.LocalJettyHttpSpecification
+import fi.oph.koski.http.HttpTester
+import org.scalatest.{FreeSpec, Matchers}
+
+class MyDataAPIProxyServletTest extends FreeSpec with LocalJettyHttpSpecification with Matchers with HttpTester {
+
+  def oid = "1.2.3.4.5"
+  implicit def application = KoskiApplicationForTests
+
+  "ApiProxyServlet" - {
+
+    "Ei palauta mitään mikäli X-ROAD-MEMBER headeria ei ole asetettu" in {
+      authGet(s"api/omadata/oppija/${oid}") {
+        status should equal(400)
+        body should include("Vaadittu X-ROAD-MEMBER http-otsikkokenttä puuttuu")
+      }
+    }
+
+    "Ei palauta mitään mikäli käyttäjä ei ole antanut lupaa" in {
+    }
+
+  }
+}
