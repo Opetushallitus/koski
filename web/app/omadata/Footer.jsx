9d10874a9a7f005b817ff290826636c0efed6500
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-26 12:55:47 +0300

    include okm logo in footer

--- web/app/omadata/Footer.jsx
+++ web/app/omadata/Footer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default () => (
   <div className="footer">
     <img src="/koski/images/oph_fin_vaaka.png" />
-    <img src="/koski/images/logo_okm.png" />
+    <img src="/koski/images/logo_okm.svg" />
   </div>
 )
