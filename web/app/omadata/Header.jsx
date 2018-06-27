ca1937f5734f7876e00a900d34cc73b5a6a97c91
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-06-27 12:41:46 +0300

    simplify function which opens the mydata menu

--- web/app/omadata/Header.jsx
+++ web/app/omadata/Header.jsx
@@ -6,7 +6,9 @@ const menuOpened = Atom(false)
 
 export default ({ userP, onLogoutClicked }) => (
   <div className="header">
-    <button id="header-mobile-menu-button" onClick={() => menuOpened.set(!menuOpened.get())}><img src="/koski/images/baseline-menu-24px.svg" /></button>
+    <button id="header-mobile-menu-button" onClick={() => menuOpened.set(!menuOpened.get())}>
+      <img src="/koski/images/baseline-menu-24px.svg" />
+    </button>
     <div className="title">
       <img src="/koski/images/opintopolku_logo.svg" alt="" />
       <h1><Text name="Oma Opintopolku"/></h1>
@@ -19,15 +21,11 @@ export default ({ userP, onLogoutClicked }) => (
       <div className="logout" onClick={() => { onLogoutClicked() }}><Text name="Kirjaudu ulos"/></div>
     </div>
 
-    {menuOpened.map((opened) => {
-      return (
-        <div id="header-mobile-menu" className={opened ? 'menu-open' : 'menu-closed'}>
-          <div className="top">
-            <div className="username">{ userP.map(user => user && user.name ) }</div>
-            <div className="logout" onClick={() => { onLogoutClicked() }}><Text name="Kirjaudu ulos"/></div>
-          </div>
-        </div>
-      )
-    })}
+    <div id="header-mobile-menu" className={menuOpened.map(opened => opened ? 'menu-open' : 'menu-closed')}>
+      <div className="top">
+        <div className="username">{ userP.map(user => user && user.name ) }</div>
+        <div className="logout" onClick={() => { onLogoutClicked() }}><Text name="Kirjaudu ulos"/></div>
+      </div>
+    </div>
   </div>
 )
