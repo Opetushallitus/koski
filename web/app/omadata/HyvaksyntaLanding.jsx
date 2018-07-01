e9f56fcd1b5ab009a473da67f2b0a48f934b7e59
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-07-01 19:21:12 +0300

    refactored logic to new component

--- web/app/omadata/HyvaksyntaLanding.jsx
+++ web/app/omadata/HyvaksyntaLanding.jsx
@@ -1,21 +1,14 @@
 import React from 'baret'
 import ReactDOM from 'react-dom'
-import AnnaHyvaksynta from './AnnaHyvaksynta'
-import HyvaksyntaAnnettu from './HyvaksyntaAnnettu'
+import UusiHyvaksynta from './UusiHyvaksynta'
 import Footer from './Footer'
 import Header from './Header'
-import { formatFinnishDate, parseISODate } from '../date/date.js'
-import Text from '../i18n/Text'
 import '../polyfills/polyfills.js'
 import Http from '../util/http'
 import { currentLocation, parseQuery } from '../util/location'
 import { userP } from '../util/user'
 import { Error } from '../util/Error'
 import {t} from '../i18n/i18n'
-
-
-const memberP = memberId => Http.cachedGet(`/koski/api/omadata/kumppani/${memberId}`, { errorMapper: () => undefined }).toProperty()
-const editorP = Http.cachedGet('/koski/api/omattiedot/editor', { errorMapper: () => undefined }).toProperty()
 
 const memberCodeRegex = /\/koski\/omadata\/(.*)/
 
@@ -31,36 +24,24 @@ class HyvaksyntaLanding extends React.Component {
 
     this.authorizeMember = this.authorizeMember.bind(this)
     this.onLogoutClicked = this.onLogoutClicked.bind(this)
-    this.getLogoutURL = this.getLogoutURL.bind(this)
+
+    console.log(`Member code: ${this.state.memberCode}`)
   }
 
-
-  getBirthDate(editorResponse) {
-    if (!editorResponse) return
-
-    return formatFinnishDate(
-      parseISODate(
-        editorResponse.value.properties.find(p => p.key === 'henkilö')
-          .model.value.properties.find(p => p.key === 'syntymäaika')
-          .model.value.data
-      )
-    )
-  }
-
-  authorizeMember(memberCode) {
-    Http.post(`/koski/api/omadata/valtuutus/${memberCode}`, {})
+  authorizeMember() {
+    Http.post(`/koski/api/omadata/valtuutus/${this.state.memberCode}`, {})
       .doError((e) => {
         if (e && e.httpStatus === 401) {
-          console.log(`Must be logged in before we can authorize ${memberCode}`)
+          console.log(`Must be logged in before we can authorize ${this.state.memberCode}`)
           this.setState({error: t('Sinun tulee olla kirjautunut sisään')})
         } else {
-          console.log(`Failed to add permissions for ${memberCode}`, e)
+          console.log(`Failed to add permissions for ${this.state.memberCode}`, e)
           this.setState({error: t('Tallennus epäonnistui')})
         }
       })
       .onValue((response) => {
         if (response.success === true) {
-          console.log(`Permissions added for ${memberCode}`)
+          console.log(`Permissions added for ${this.state.memberCode}`)
           this.setState({
             authorizationGiven: true
           })
@@ -79,16 +60,6 @@ class HyvaksyntaLanding extends React.Component {
   }
 
   render() {
-
-    const acceptanceBox = this.state.authorizationGiven ?
-      <HyvaksyntaAnnettu logoutURL={this.getLogoutURL()}/> :
-      (
-        <AnnaHyvaksynta memberP={memberP(this.state.memberCode)}
-        onAcceptClick={() => this.authorizeMember(this.state.memberCode)}
-        onCancelClick={() => this.onLogoutClicked()}
-        />
-      )
-
     const error = this.state.error ?  <Error error={{text: this.state.error}} /> : null
 
     return (
@@ -96,14 +67,12 @@ class HyvaksyntaLanding extends React.Component {
         <Header userP={userP} onLogoutClicked={this.onLogoutClicked}/>
         {error}
 
-        <div className="acceptance-container">
-          <div className="heading"><h1><Text name="Henkilökohtaisten tietojen käyttö"/></h1></div>
-          <div className="user">
-            <div className="username">{userP.map(user => user && user.name)}</div>
-            <div className="dateofbirth"> {editorP.map(s => this.getBirthDate(s))}</div>
-          </div>
-          {acceptanceBox}
-        </div>
+        <UusiHyvaksynta
+          memberCode={this.state.memberCode}
+          logoutURL={this.getLogoutURL()}
+          onAuthorization={this.authorizeMember}
+          authorizationGiven={this.state.authorizationGiven}
+        />
 
         <Footer/>
       </div>
---
+++ web/app/omadata/UusiHyvaksynta.jsx
@@ -0,0 +1,41 @@
+import React from 'baret'
+import {userP} from '../util/user'
+import Http from '../util/http'
+import Text from '../i18n/Text'
+import AnnaHyvaksynta from './AnnaHyvaksynta'
+import HyvaksyntaAnnettu from './HyvaksyntaAnnettu'
+import {formatFinnishDate, parseISODate} from '../date/date'
+
+const editorP = Http.cachedGet('/koski/api/omattiedot/editor', { errorMapper: () => undefined }).toProperty()
+const memberP = memberId => Http.cachedGet(`/koski/api/omadata/kumppani/${memberId}`, { errorMapper: () => undefined }).toProperty()
+
+const getBirthDate = (editorResponse) => {
+  if (!editorResponse) return
+
+  return formatFinnishDate(
+    parseISODate(
+      editorResponse.value.properties.find(p => p.key === 'henkilö')
+        .model.value.properties.find(p => p.key === 'syntymäaika')
+        .model.value.data
+    )
+  )
+}
+
+
+export default ({memberCode, logoutURL, onAuthorization, authorizationGiven}) => (
+  <div className="acceptance-container">
+    <div className="heading"><h1><Text name="Henkilökohtaisten tietojen käyttö"/></h1></div>
+    <div className="user">
+      <div className="username">{userP.map(user => user && user.name)}</div>
+      <div className="dateofbirth"> {editorP.map(s => getBirthDate(s))}</div>
+    </div>
+
+    { authorizationGiven ?
+      <HyvaksyntaAnnettu logoutURL={logoutURL}/> :
+      <AnnaHyvaksynta memberP={memberP(memberCode)}
+                      onAcceptClick={() => onAuthorization()}
+                      onCancelClick={() => this.onLogoutClicked()} />
+    }
+
+  </div>
+)
