3dd2aa141f1caeb66df1a837a884d2bc13835392
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-07-05 16:46:02 +0300

    use vertical svg logo

--- web/app/omadata/Footer.jsx
+++ web/app/omadata/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
   <div className='footer'>
-    <img src='/koski/images/oph_fin_vaaka.png' />
+    <img src='/koski/images/oph_logo_fin_pysty.svg' />
     <img src='/koski/images/logo_okm.svg' />
   </div>
 )
