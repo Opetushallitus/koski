17531620e4b6d3e1abd7fedd8c5c25bd4fad4c99
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-07-05 21:16:52 +0300

    Log error to backend

--- web/app/omadata/HyvaksyntaLanding.jsx
+++ web/app/omadata/HyvaksyntaLanding.jsx
@@ -30,7 +30,10 @@ class HyvaksyntaLanding extends React.Component {
 
   componentDidMount() {
     const pathParam = memberCodeRegex.exec(currentLocation().path)[1]
-    Http.cachedGet(`/koski/api/omadata/kumppani/${pathParam}`, { errorHandler: () => this.setState({ loading: false }) })
+    Http.cachedGet(`/koski/api/omadata/kumppani/${pathParam}`, { errorHandler: (e) => {
+        logError(e)
+        this.setState({loading: false})
+    }})
       .onValue(member => this.setState({
         memberName: member.name,
         memberCode: member.id,
