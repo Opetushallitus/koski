f288782c61f8b393a7b74ac560dd01bc90ba0600
Valtteri Lakaniemi <valtteri.lakaniemi@reaktor.com>
2018-07-09 13:32:27 +0300

    make all buttons in confirmation modal accessible with keyboard

--- web/app/editor/ModalDialog.jsx
+++ web/app/editor/ModalDialog.jsx
@@ -3,6 +3,7 @@ import Bacon from 'baconjs'
 import Atom from 'bacon.atom'
 import {doActionWhileMounted, parseBool} from '../util/util'
 import Text from '../i18n/Text'
+import {t} from '../i18n/i18n'
 
 export default ({className, onDismiss, onSubmit, children, submitOnEnterKey, okTextKey = 'Ok', cancelTextKey = 'Peruuta', validP = Bacon.constant(true), fullscreen = false}) => {
   submitOnEnterKey = parseBool(submitOnEnterKey, true)
@@ -16,10 +17,10 @@ export default ({className, onDismiss, onSubmit, children, submitOnEnterKey, okT
   return (<div className={classNameP}>
     <div className='modal-shield' onClick={() => onDismiss()}/>
     <div className='modal-content'>
-      <a className='close-modal' onClick={() => onDismiss()}/>
+      <button className='close-modal' onClick={() => onDismiss()} aria-label={t('Sulje')}/>
       { children }
       <div className='actions'>
-        <a className='peruuta' onClick={() => onDismiss()}><Text name={cancelTextKey}/></a>
+        <button className='peruuta text-button-small' onClick={() => onDismiss()}><Text name={cancelTextKey}/></button>
         <button className='koski-button' disabled={validP.not().or(submittedAtom)} onClick={(e) => {e.preventDefault(); submittedAtom.set(true); onSubmit()}}><Text name={okTextKey}/></button>
       </div>
     </div>
