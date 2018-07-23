4590537cdadf32f80e2492f17a7e523b5d3a87d8
suarela <aleksi.saarela@reaktor.fi>
2018-07-23 16:12:03 +0300

    suoritusjako responseen lisätty aikaleima

--- src/main/scala/fi/oph/koski/suoritusjako/Suoritusjako.scala
+++ src/main/scala/fi/oph/koski/suoritusjako/Suoritusjako.scala
@@ -1,5 +1,6 @@
 package fi.oph.koski.suoritusjako
 
 import java.time.LocalDate
+import java.sql.Timestamp
 
-case class Suoritusjako(secret: String, expirationDate: LocalDate)
+case class Suoritusjako(secret: String, expirationDate: LocalDate, timestamp: Timestamp)
