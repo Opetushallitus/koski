75d269d03ad46b2ea2569f3f715c0713a7d4ccdd
Petri Myllys <petri.myllys@reaktor.com>
2018-08-01 15:17:07 +0300

    Lisää FAB suoritustietojen jaolle

--- web/app/omattiedot/header/HeaderButtons.jsx
+++ web/app/omattiedot/header/HeaderButtons.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import {MultistateToggleButton} from '../../components/ToggleButton'
 import {withFeatureFlag} from '../../components/withFeatureFlag'
 import {FormState} from './Header'
+import FloatingActionButton from '../../components/FloatingActionButton'
 
 const VirheraportointiButton = withFeatureFlag(FEATURE.OMAT_TIEDOT.VIRHERAPORTOINTI, MultistateToggleButton)
 const SuoritusjakoButton = withFeatureFlag(FEATURE.OMAT_TIEDOT.SUORITUSJAKO, MultistateToggleButton)
+const FloatingSuoritusjakoButton = withFeatureFlag(FEATURE.OMAT_TIEDOT.SUORITUSJAKO, FloatingActionButton)
+
+const ACTION_BUTTON_OFFSET = 270
 
 export const HeaderButtons = ({uiModeA}) => (
   <div className='header__buttons'>
@@ -20,6 +24,12 @@ export const HeaderButtons = ({uiModeA}) => (
       value={FormState.SUORITUSJAKO}
       clearedStateValue={FormState.NONE}
       text='Jaa suoritustietoja'
+    />
+
+    <FloatingSuoritusjakoButton
+      text={'Suoritustietojen jakaminen'}
+      onClick={() => uiModeA.set(FormState.SUORITUSJAKO)}
+      visibilityOffset={ACTION_BUTTON_OFFSET}
     />
   </div>
 )
