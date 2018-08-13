b62517e80d8dda38c11553d713f1f48f71c0913d
Petri Myllys <petri.myllys@reaktor.com>
2018-08-13 13:10:17 +0300

    Käytä secondary button -tyyliä huonosti näkyvissä painikkeissa

--- web/app/components/ToggleButton.jsx
+++ web/app/components/ToggleButton.jsx
@@ -5,9 +5,9 @@ const toggleState = stateA => () => stateA.modify(v => !v)
 const setOrClearState = (stateA, value, clearedStateValue) => () =>
   stateA.modify(current => value === current ? clearedStateValue : value)
 
-export const ToggleButton = ({toggleA, text, style}) => style === 'text'
-  ? <button className='toggle-button text-button' onClick={toggleState(toggleA)}><Text name={text}/></button>
-  : <button className='toggle-button' onClick={toggleState(toggleA)}><Text name={text}/></button>
+export const ToggleButton = ({toggleA, text, style}) => style === 'secondary'
+  ? <button className='koski-button toggle-button koski-button-secondary' onClick={toggleState(toggleA)}><Text name={text}/></button>
+  : <button className='koski-button toggle-button' onClick={toggleState(toggleA)}><Text name={text}/></button>
 
 export const MultistateToggleButton = ({stateA, value, clearedStateValue = null, text, style}) => style === 'secondary'
   ? (
