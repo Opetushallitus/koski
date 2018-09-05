161e475e9b86f4072eaf7251dca921ccb806de03
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-09-05 17:42:57 +0300

    Näytä lisätiedot jos ne sisältävät virheitä

--- web/app/editor/ExpandablePropertiesEditor.jsx
+++ web/app/editor/ExpandablePropertiesEditor.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import {modelLookup} from './EditorModel.js'
 import {PropertiesEditor, shouldShowProperty} from './PropertiesEditor'
-import {modelProperties, wrapOptional} from './EditorModel'
-import {modelProperty} from './EditorModel'
-import {navigateWithQueryParams, currentLocation} from '../util/location'
+import {modelErrorMessages, modelProperties, modelProperty, wrapOptional} from './EditorModel'
+import {currentLocation, navigateWithQueryParams} from '../util/location'
 import {parseBool} from '../util/util'
 import Text from '../i18n/Text'
+import {isEmpty} from 'ramda'
 
 export const ExpandablePropertiesEditor = ({model, propertyName, propertyFilter = () => true}) => {
   let propertyModel = modelLookup(model, propertyName)
   let edit = model.context.edit
   let paramName = propertyName + '-expanded'
-  let expanded = parseBool(currentLocation().params[paramName])
+  let hasErrors = !isEmpty(modelErrorMessages(propertyModel, true))
+  let expanded = parseBool(currentLocation().params[paramName]) || hasErrors
   let wrappedModel = edit ? wrapOptional(propertyModel) : propertyModel
   let toggleOpen = () => {
     navigateWithQueryParams({[paramName]: !expanded ? 'true' : undefined})
