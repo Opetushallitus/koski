600a204e9b5469e5788f59c6ca1e8213652e9a78
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-09-06 10:10:40 +0300

    Convert to react component

--- web/app/editor/ExpandablePropertiesEditor.jsx
+++ web/app/editor/ExpandablePropertiesEditor.jsx
@@ -7,25 +7,28 @@ import {parseBool} from '../util/util'
 import Text from '../i18n/Text'
 import {isEmpty} from 'ramda'
 
-export const ExpandablePropertiesEditor = ({model, propertyName, propertyFilter = () => true}) => {
-  let propertyModel = modelLookup(model, propertyName)
-  let edit = model.context.edit
-  let paramName = propertyName + '-expanded'
-  let hasErrors = !isEmpty(modelErrorMessages(propertyModel, true))
-  let expanded = parseBool(currentLocation().params[paramName]) || hasErrors
-  let wrappedModel = edit ? wrapOptional(propertyModel) : propertyModel
-  let toggleOpen = () => {
-    navigateWithQueryParams({[paramName]: !expanded ? 'true' : undefined})
-  }
-  let zeroVisibleProperties = modelProperties(wrappedModel, shouldShowProperty(model.context)).filter(propertyFilter).length === 0
+export class ExpandablePropertiesEditor extends React.Component {
+  render() {
+    const {model, propertyName, propertyFilter = () => true} = this.props
+    let propertyModel = modelLookup(model, propertyName)
+    let edit = model.context.edit
+    let paramName = propertyName + '-expanded'
+    let hasErrors = !isEmpty(modelErrorMessages(propertyModel, true))
+    let expanded = parseBool(currentLocation().params[paramName]) || hasErrors
+    let wrappedModel = edit ? wrapOptional(propertyModel) : propertyModel
+    let toggleOpen = () => {
+      navigateWithQueryParams({[paramName]: !expanded ? 'true' : undefined})
+    }
+    let zeroVisibleProperties = modelProperties(wrappedModel, shouldShowProperty(model.context)).filter(propertyFilter).length === 0
 
-  return !zeroVisibleProperties || wrappedModel.context.edit ?
-    <div className={'expandable-container ' + propertyName}>
-      <a className={expanded ? 'open expandable' : 'expandable'} onClick={toggleOpen}><Text name={modelProperty(model, propertyName).title}/></a>
-      { expanded ?
-        <div className="value">
-          <PropertiesEditor model={wrappedModel} propertyFilter={propertyFilter}/>
-        </div> : null
-      }
-    </div> : null
+    return !zeroVisibleProperties || wrappedModel.context.edit ?
+      <div className={'expandable-container ' + propertyName}>
+        <a className={expanded ? 'open expandable' : 'expandable'} onClick={toggleOpen}><Text name={modelProperty(model, propertyName).title}/></a>
+        { expanded ?
+          <div className="value">
+            <PropertiesEditor model={wrappedModel} propertyFilter={propertyFilter}/>
+          </div> : null
+        }
+      </div> : null
+  }
 }
