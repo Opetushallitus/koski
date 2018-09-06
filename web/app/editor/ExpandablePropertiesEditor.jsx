ef5593fb0d30a9b2f709635a95ff9e0a134d928e
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-09-06 13:32:58 +0300

    Jätä lisätiedot auki kun virheet on korjattu

--- web/app/editor/ExpandablePropertiesEditor.jsx
+++ web/app/editor/ExpandablePropertiesEditor.jsx
@@ -6,24 +6,26 @@ import {currentLocation, navigateWithQueryParams} from '../util/location'
 import {parseBool} from '../util/util'
 import Text from '../i18n/Text'
 import {isEmpty} from 'ramda'
+import * as Bacon from 'baconjs'
 
 export class ExpandablePropertiesEditor extends React.Component {
+  constructor() {
+    super()
+    this.expandBus = new Bacon.Bus()
+  }
+
   render() {
     const {model, propertyName, propertyFilter = () => true} = this.props
     let propertyModel = modelLookup(model, propertyName)
     let edit = model.context.edit
     let paramName = propertyName + '-expanded'
-    let hasErrors = !isEmpty(modelErrorMessages(propertyModel, true))
-    let expanded = parseBool(currentLocation().params[paramName]) || hasErrors
+    let expanded = parseBool(currentLocation().params[paramName])
     let wrappedModel = edit ? wrapOptional(propertyModel) : propertyModel
-    let toggleOpen = () => {
-      navigateWithQueryParams({[paramName]: !expanded ? 'true' : undefined})
-    }
     let zeroVisibleProperties = modelProperties(wrappedModel, shouldShowProperty(model.context)).filter(propertyFilter).length === 0
 
     return !zeroVisibleProperties || wrappedModel.context.edit ?
       <div className={'expandable-container ' + propertyName}>
-        <a className={expanded ? 'open expandable' : 'expandable'} onClick={toggleOpen}><Text name={modelProperty(model, propertyName).title}/></a>
+        <a className={expanded ? 'open expandable' : 'expandable'} onClick={() => setExpanded(paramName, !expanded)}><Text name={modelProperty(model, propertyName).title}/></a>
         { expanded ?
           <div className="value">
             <PropertiesEditor model={wrappedModel} propertyFilter={propertyFilter}/>
@@ -31,4 +33,19 @@ export class ExpandablePropertiesEditor extends React.Component {
         }
       </div> : null
   }
+
+  componentDidMount() {
+    this.expandBus.onValue(() => setExpanded(this.props.propertyName + '-expanded', true))
+  }
+
+  componentDidUpdate() {
+    const {model, propertyName} = this.props
+    const hasErrors = !isEmpty(modelErrorMessages(modelLookup(model, propertyName), true))
+    if (hasErrors) {
+      this.expandBus.push()
+    }
+  }
 }
+
+const setExpanded = (paramName, expand) =>
+  navigateWithQueryParams({[paramName]: expand ? 'true' : undefined})
