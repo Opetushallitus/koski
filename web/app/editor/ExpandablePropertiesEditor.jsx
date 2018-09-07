a183061d1a96e85bde3a49f16b11d4ff3219488a
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-09-07 17:10:40 +0300

    Cleanup

--- web/app/editor/ExpandablePropertiesEditor.jsx
+++ web/app/editor/ExpandablePropertiesEditor.jsx
@@ -8,44 +8,47 @@ import Text from '../i18n/Text'
 import {isEmpty} from 'ramda'
 import * as Bacon from 'baconjs'
 
-export class ExpandablePropertiesEditor extends React.Component {
+export const ExpandablePropertiesEditor = ({model, propertyName, propertyFilter = () => true}) => {
+  const propertyModel = modelLookup(model, propertyName)
+  const paramName = propertyName + '-expanded'
+  const wrappedModel = model.context.edit ? wrapOptional(propertyModel) : propertyModel
+  const zeroVisibleProperties = modelProperties(wrappedModel, shouldShowProperty(model.context)).filter(propertyFilter).length === 0
+
+  return !zeroVisibleProperties || wrappedModel.context.edit
+    ? <Expandable editor={<div className='value'><PropertiesEditor model={wrappedModel} propertyFilter={propertyFilter}/></div>}
+                  expanded={parseBool(currentLocation().params[paramName])}
+                  setExpanded={expand => setExpandedQueryParam(paramName, expand)}
+                  propertyName={propertyName}
+                  title={modelProperty(model, propertyName).title}
+                  hasErrors={!isEmpty(modelErrorMessages(propertyModel, true))} />
+    : null
+}
+
+export class Expandable extends React.Component {
   constructor() {
     super()
     this.expandBus = new Bacon.Bus()
   }
 
   render() {
-    const {model, propertyName, propertyFilter = () => true} = this.props
-    let propertyModel = modelLookup(model, propertyName)
-    let edit = model.context.edit
-    let paramName = propertyName + '-expanded'
-    let expanded = parseBool(currentLocation().params[paramName])
-    let wrappedModel = edit ? wrapOptional(propertyModel) : propertyModel
-    let zeroVisibleProperties = modelProperties(wrappedModel, shouldShowProperty(model.context)).filter(propertyFilter).length === 0
-
-    return !zeroVisibleProperties || wrappedModel.context.edit ?
-      <div className={'expandable-container ' + propertyName}>
-        <a className={expanded ? 'open expandable' : 'expandable'} onClick={() => setExpanded(paramName, !expanded)}><Text name={modelProperty(model, propertyName).title}/></a>
-        { expanded ?
-          <div className="value">
-            <PropertiesEditor model={wrappedModel} propertyFilter={propertyFilter}/>
-          </div> : null
-        }
-      </div> : null
+    const {editor, expanded, setExpanded, propertyName, title} = this.props
+    const toggleExpand = () => setExpanded(!expanded)
+    return (<div className={'expandable-container ' + propertyName}>
+      <a className={expanded ? 'open expandable' : 'expandable'} onClick={toggleExpand}><Text name={title}/></a>
+      {expanded ? editor : null}
+    </div>)
   }
 
   componentDidMount() {
-    this.expandBus.onValue(() => setExpanded(this.props.propertyName + '-expanded', true))
+    this.expandBus.onValue(() => this.props.setExpanded(true))
   }
 
   componentDidUpdate() {
-    const {model, propertyName} = this.props
-    const hasErrors = !isEmpty(modelErrorMessages(modelLookup(model, propertyName), true))
-    if (hasErrors) {
+    if (this.props.hasErrors) {
       this.expandBus.push()
     }
   }
 }
 
-const setExpanded = (paramName, expand) =>
+const setExpandedQueryParam = (paramName, expand) =>
   navigateWithQueryParams({[paramName]: expand ? 'true' : undefined})
