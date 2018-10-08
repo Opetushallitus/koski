b42971cf5e6fe2ee4635c5c2900969bf3083cd01
Pasi Eronen <pasi.eronen@reaktor.com>
2018-10-08 16:30:07 +0300

    Erota paremmin Virta/YTR henkilöhaku ja henkilön lisäys oppijanumerorekisteriin

--- src/main/scala/fi/oph/koski/virta/VirtaHenkiloRepository.scala
+++ src/main/scala/fi/oph/koski/virta/VirtaHenkiloRepository.scala
@@ -1,41 +1,53 @@
 package fi.oph.koski.virta
 
 import fi.oph.koski.henkilo.{FindByHetu, OpintopolkuHenkilöRepository}
+import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
 import fi.oph.koski.koskiuser.KoskiSession
 import fi.oph.koski.log.Logging
 import fi.oph.koski.schema.{HenkilötiedotJaOid, UusiHenkilö}
+
+import scala.util.control.NonFatal
 
 case class VirtaHenkilöRepository(v: VirtaClient, henkilöpalvelu: OpintopolkuHenkilöRepository, accessChecker: VirtaAccessChecker) extends FindByHetu with Logging {
   override def findByHetu(hetu: String)(implicit user: KoskiSession): Option[HenkilötiedotJaOid] = {
     if (!accessChecker.hasAccess(user)) {
       None
     } else {
-      try {
-        // Tänne tullaan vain, jos oppijaa ei löytynyt henkilöpalvelusta (ks CompositeHenkilöRepository)
-        val hakuehto: VirtaHakuehtoHetu = VirtaHakuehtoHetu(hetu)
-        // Oppijan organisaatiot haetaan ensin tällä raskaammalla kyselyllä
-        val organisaatiot = v.opintotiedot(hakuehto).toSeq.flatMap(_ \\ "Opiskeluoikeus" \ "Myontaja").map(_.text)
-        // Organisaatioden avulla haetaan henkilötietoja ja valitaan niistä ensimmäinen validi
-        val opiskelijaNodes = organisaatiot.flatMap(v.henkilötiedot(hakuehto, _)).flatMap(_ \\ "Opiskelija")
-        opiskelijaNodes
-          .map { opiskelijaNode => ((opiskelijaNode \ "Sukunimi").text, (opiskelijaNode \ "Etunimet").text) }
-          .find { case (sukunimi, etunimet) => !sukunimi.isEmpty && !etunimet.isEmpty }
-          .flatMap { case (sukunimi, etunimet) =>
-            val kutsumanimi = etunimet.split(" ").toList.head
-            // Validi oppija lisätään henkilöpalveluun, jolloin samaa oppijaa ei haeta enää uudestaan Virrasta
-            henkilöpalvelu.findOrCreate(UusiHenkilö(hetu, etunimet, Some(kutsumanimi), sukunimi)) match {
-              case Right(henkilö) => Some(henkilö)
-              case Left(error) =>
-                logger.error("Virta-oppijan lisäys henkilöpalveluun epäonnistui: " + error)
-                None
-            }
+      // Tänne tullaan vain, jos oppijaa ei löytynyt henkilöpalvelusta (ks CompositeHenkilöRepository)
+      findByHetuDontCreate(hetu) match {
+        case Right(Some(uusiOppija)) =>
+          // Validi oppija lisätään henkilöpalveluun, jolloin samaa oppijaa ei haeta enää uudestaan Virrasta
+          henkilöpalvelu.findOrCreate(uusiOppija) match {
+            case Right(henkilö) => Some(henkilö.toHenkilötiedotJaOid)
+            case Left(error) =>
+              logger.error("Virta-oppijan lisäys henkilöpalveluun epäonnistui: " + error)
+              None
           }
-          .map(_.toHenkilötiedotJaOid)
-      } catch {
-        case e: Exception =>
-          logger.error(e)("Failed to fetch data from Virta")
-          None
+        case Right(None) => None
+        case Left(_) => None
       }
+    }
+  }
+
+  def findByHetuDontCreate(hetu: String): Either[HttpStatus, Option[UusiHenkilö]] = {
+    try {
+      // Tänne tullaan vain, jos oppijaa ei löytynyt henkilöpalvelusta (ks CompositeHenkilöRepository)
+      val hakuehto: VirtaHakuehtoHetu = VirtaHakuehtoHetu(hetu)
+      // Oppijan organisaatiot haetaan ensin tällä raskaammalla kyselyllä
+      val organisaatiot = v.opintotiedot(hakuehto).toSeq.flatMap(_ \\ "Opiskeluoikeus" \ "Myontaja").map(_.text)
+      // Organisaatioden avulla haetaan henkilötietoja ja valitaan niistä ensimmäinen validi
+      val opiskelijaNodes = organisaatiot.flatMap(v.henkilötiedot(hakuehto, _)).flatMap(_ \\ "Opiskelija")
+      Right(opiskelijaNodes
+        .map { opiskelijaNode => ((opiskelijaNode \ "Sukunimi").text, (opiskelijaNode \ "Etunimet").text) }
+        .find { case (sukunimi, etunimet) => !sukunimi.isEmpty && !etunimet.isEmpty }
+        .map { case (sukunimi, etunimet) =>
+          val kutsumanimi = etunimet.split(" ").toList.headOption
+          UusiHenkilö(hetu = hetu, etunimet = etunimet, kutsumanimi = kutsumanimi, sukunimi = sukunimi)
+        })
+    } catch {
+      case NonFatal(e) =>
+        logger.error(e)("Failed to fetch data from Virta")
+        Left(KoskiErrorCategory.unavailable.virta())
     }
   }
 
--- src/main/scala/fi/oph/koski/ytr/YtrHenkiloRepository.scala
+++ src/main/scala/fi/oph/koski/ytr/YtrHenkiloRepository.scala
@@ -1,29 +1,41 @@
 package fi.oph.koski.ytr
 
 import fi.oph.koski.henkilo.{FindByHetu, OpintopolkuHenkilöRepository}
+import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
 import fi.oph.koski.koskiuser.{AccessChecker, KoskiSession}
 import fi.oph.koski.log.Logging
 import fi.oph.koski.schema.UusiHenkilö
+
+import scala.util.control.NonFatal
 
 case class YtrHenkilöRepository(ytr: YtrClient, henkilöpalvelu: OpintopolkuHenkilöRepository, accessChecker: AccessChecker) extends FindByHetu with Logging {
   override def findByHetu(hetu: String)(implicit user: KoskiSession) = if (!accessChecker.hasAccess(user)) {
     None
   } else {
-    try {
-      ytr.oppijaByHetu(hetu).flatMap { ytrOppija =>
-        val kutsumanimi = ytrOppija.firstnames.split(" ").toList.head
-        henkilöpalvelu.findOrCreate(UusiHenkilö(hetu, ytrOppija.firstnames, Some(kutsumanimi), ytrOppija.lastname)) match {
-          case Right(henkilö) =>
-            Some(henkilö)
+    findByHetuDontCreate(hetu) match {
+      case Right(Some(uusiOppija)) =>
+        // Validi oppija lisätään henkilöpalveluun, jolloin samaa oppijaa ei haeta enää uudestaan YTR:stä
+        henkilöpalvelu.findOrCreate(uusiOppija) match {
+          case Right(henkilö) => Some(henkilö.toHenkilötiedotJaOid)
           case Left(error) =>
             logger.error("YTR-oppijan lisäys henkilöpalveluun epäonnistui: " + error)
             None
         }
-      }.map(_.toHenkilötiedotJaOid)
+      case Right(None) => None
+      case Left(_) => None
+    }
+  }
+
+  def findByHetuDontCreate(hetu: String): Either[HttpStatus, Option[UusiHenkilö]] = {
+    try {
+      Right(ytr.oppijaByHetu(hetu).map { ytrOppija =>
+        val kutsumanimi = ytrOppija.firstnames.split(" ").toList.head
+        UusiHenkilö(hetu, ytrOppija.firstnames, Some(kutsumanimi), ytrOppija.lastname)
+      })
     } catch {
-      case e: Exception =>
+      case NonFatal(e) =>
         logger.error(e)("Failed to fetch data from YTR")
-        None
+        Left(KoskiErrorCategory.unavailable.ytr())
     }
   }
 
