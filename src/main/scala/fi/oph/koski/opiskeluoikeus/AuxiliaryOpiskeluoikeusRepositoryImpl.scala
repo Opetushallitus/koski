386753d6c6004ca027fd3a8daa92af91b01b476d
Pasi Eronen <pasi.eronen@reaktor.com>
2018-10-11 15:01:54 +0300

    Refaktoroi HetuBasedOpiskeluoikeusRepository tukemaan myös muita tunnisteita (Virta/Ytr repository ei vielä tue näitä)

---
+++ src/main/scala/fi/oph/koski/opiskeluoikeus/AuxiliaryOpiskeluoikeusRepositoryImpl.scala
@@ -0,0 +1,65 @@
+package fi.oph.koski.opiskeluoikeus
+
+import fi.oph.koski.cache.{CacheManager, ExpiringCache, KeyValueCache}
+import fi.oph.koski.koskiuser.{AccessChecker, KoskiSession}
+import fi.oph.koski.log.Logging
+import fi.oph.koski.schema.{HenkilönTunnisteet, Opiskeluoikeus, Organisaatio}
+
+import scala.concurrent.duration._
+import scala.util.control.NonFatal
+
+abstract class AuxiliaryOpiskeluoikeusRepositoryImpl[OO <: Opiskeluoikeus, CK <: AnyRef](accessChecker: AccessChecker)(implicit cacheInvalidator: CacheManager) extends AuxiliaryOpiskeluoikeusRepository with Logging {
+
+  override def filterOppijat[A <: HenkilönTunnisteet](oppijat: List[A])(implicit user: KoskiSession): List[A] = {
+    val globalAccess = accessChecker.hasGlobalAccess(user)
+    try {
+      if (globalAccess) {
+        oppijat.filter(oppija => cachedOpiskeluoikeudet(oppija).nonEmpty)
+      } else {
+        quickAccessCheck(oppijat.par.filter(oppija => cachedOrganizations(oppija).exists(user.hasReadAccess)).toList)
+      }
+    } catch {
+      case NonFatal(e) =>
+        logger.error(e)(s"Failed to fetch data for filterOppijat, ${if (globalAccess) "returning everything" else "not returning anything"}")
+        if (globalAccess) oppijat else Nil
+    }
+  }
+
+  override def findByOppija(tunnisteet: HenkilönTunnisteet)(implicit user: KoskiSession): List[OO] = {
+    quickAccessCheck(filterByOrganisaatio(cachedOpiskeluoikeudet(tunnisteet)))
+  }
+
+  override def findByCurrentUser(tunnisteet: HenkilönTunnisteet)(implicit user: KoskiSession): List[OO] = {
+    assert(tunnisteet.oid == user.oid, "Käyttäjän oid: " + user.oid + " poikkeaa etsittävän oppijan oidista: " + tunnisteet.oid)
+    cachedOpiskeluoikeudet(tunnisteet)
+  }
+
+  private val cache = KeyValueCache[CK, List[OO]](ExpiringCache(getClass.getSimpleName + ".opiskeluoikeudet", 1.hour, 100), uncachedOpiskeluoikeudet)
+
+  protected def uncachedOpiskeluoikeudet(cacheKey: CK): List[OO]
+
+  private def cachedOpiskeluoikeudet(tunnisteet: HenkilönTunnisteet): List[OO] = {
+    cache(buildCacheKey(tunnisteet))
+  }
+
+  protected def buildCacheKey(tunnisteet: HenkilönTunnisteet): CK
+
+  // tunniste -> org.oids cache for filtering only (much larger than opiskeluoikeus cache)
+  private val organizationsCache = KeyValueCache[CK, List[Organisaatio.Oid]](ExpiringCache(getClass.getSimpleName + ".organisations", 1.hour, 100000), uncachedOrganizations)
+
+  private def uncachedOrganizations(cacheKey: CK): List[Organisaatio.Oid] = {
+    cache(cacheKey).flatMap(_.oppilaitos).map(_.oid)
+  }
+  private def cachedOrganizations(tunnisteet: HenkilönTunnisteet): List[Organisaatio.Oid] = {
+    organizationsCache(buildCacheKey(tunnisteet))
+  }
+
+  private def quickAccessCheck[T](list: => List[T])(implicit user: KoskiSession): List[T] = if (accessChecker.hasAccess(user)) { list } else { Nil }
+
+  private def filterByOrganisaatio(opiskeluoikeudet: List[OO])(implicit user: KoskiSession): List[OO] = {
+    opiskeluoikeudet.filter { oo =>
+      accessChecker.hasGlobalAccess(user) ||
+        oo.oppilaitos.exists(oppilaitos => user.hasReadAccess(oppilaitos.oid))
+    }
+  }
+}
--- src/main/scala/fi/oph/koski/virta/VirtaOpiskeluoikeusRepository.scala
+++ src/main/scala/fi/oph/koski/virta/VirtaOpiskeluoikeusRepository.scala
@@ -1,12 +1,13 @@
 package fi.oph.koski.virta
 
 import fi.oph.koski.cache.{CacheManager, GlobalCacheManager}
-import fi.oph.koski.henkilo.Hetu
+import fi.oph.koski.http.HttpStatus
 import fi.oph.koski.koodisto.{KoodistoViitePalvelu, MockKoodistoViitePalvelu}
-import fi.oph.koski.koskiuser.{AccessChecker, SkipAccessCheck}
-import fi.oph.koski.log.Logging
+import fi.oph.koski.koskiuser.{AccessChecker, AccessType, KoskiSession, SkipAccessCheck}
+import fi.oph.koski.log.NotLoggable
+import fi.oph.koski.opiskeluoikeus.AuxiliaryOpiskeluoikeusRepositoryImpl
 import fi.oph.koski.oppilaitos.{MockOppilaitosRepository, OppilaitosRepository}
-import fi.oph.koski.schema._
+import fi.oph.koski.schema.{HenkilönTunnisteet, KorkeakoulunOpiskeluoikeus, Oppija, UusiHenkilö}
 import fi.oph.koski.validation.KoskiValidator
 
 case class VirtaOpiskeluoikeusRepository(
@@ -14,18 +15,30 @@ case class VirtaOpiskeluoikeusRepository(
   oppilaitosRepository: OppilaitosRepository,
   koodistoViitePalvelu: KoodistoViitePalvelu,
   accessChecker: AccessChecker,
-  hetuValidator: Hetu,
   validator: Option[KoskiValidator] = None
-)(implicit cacheInvalidator: CacheManager) extends HetuBasedOpiskeluoikeusRepository[KorkeakoulunOpiskeluoikeus](oppilaitosRepository, koodistoViitePalvelu, accessChecker, validator) with Logging {
-
+)(implicit cacheInvalidator: CacheManager) extends AuxiliaryOpiskeluoikeusRepositoryImpl[KorkeakoulunOpiskeluoikeus, VirtaCacheKey](accessChecker) {
   private val converter = VirtaXMLConverter(oppilaitosRepository, koodistoViitePalvelu)
 
-  override protected def opiskeluoikeudetByHetu(hetu: String): List[KorkeakoulunOpiskeluoikeus] = hetuValidator.validate(hetu) match {
-    case Right(h) => virta.opintotiedot(VirtaHakuehtoHetu(h)).toList.flatMap(converter.convertToOpiskeluoikeudet)
-    case Left(status) =>
-      logger.warn(s"Virta haku prevented $status")
-      Nil
+  override protected def uncachedOpiskeluoikeudet(cacheKey: VirtaCacheKey): List[KorkeakoulunOpiskeluoikeus] = {
+    val opiskeluoikeudet = cacheKey.hetut.headOption match {
+      case Some(hetu) => virta.opintotiedot(VirtaHakuehtoHetu(hetu)).toList.flatMap(converter.convertToOpiskeluoikeudet)
+      case None => Nil
+    }
+    opiskeluoikeudet.foreach(validate)
+    opiskeluoikeudet
+  }
+
+  override protected def buildCacheKey(tunnisteet: HenkilönTunnisteet): VirtaCacheKey =
+    VirtaCacheKey((tunnisteet.hetu.toList ++ tunnisteet.vanhatHetut).sorted, (tunnisteet.oid +: tunnisteet.linkitetytOidit).sorted)
+
+  private def validate(opiskeluoikeus: KorkeakoulunOpiskeluoikeus): Unit = {
+    val oppija = Oppija(UusiHenkilö("010101-123N", "tuntematon", Some("tuntematon"), "tuntematon"), List(opiskeluoikeus))
+    validator.foreach(_.validateAsJson(oppija)(KoskiSession.systemUser, AccessType.read).left.foreach { status: HttpStatus =>
+      logger.warn("Ulkoisesta järjestelmästä saatu opiskeluoikeus sisältää validointivirheitä " + status)
+    })
   }
 }
 
-object MockVirtaOpiskeluoikeusRepository extends VirtaOpiskeluoikeusRepository(MockVirtaClient, MockOppilaitosRepository, MockKoodistoViitePalvelu, SkipAccessCheck, new Hetu(true))(GlobalCacheManager)
+private[virta] case class VirtaCacheKey(hetut: List[String], oidit: List[String]) extends NotLoggable
+
+object MockVirtaOpiskeluoikeusRepository extends VirtaOpiskeluoikeusRepository(MockVirtaClient, MockOppilaitosRepository, MockKoodistoViitePalvelu, SkipAccessCheck)(GlobalCacheManager)
