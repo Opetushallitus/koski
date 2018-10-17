25227da71c345e490751b90517f30659ae8a10dd
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-10-17 17:27:16 +0300

    extracted common styles, increased margin, changed "active" tab

--- web/app/omadata/luvanhallinta/NavBar.jsx
+++ web/app/omadata/luvanhallinta/NavBar.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-//import constants from 'ui/constants'
-//import media from 'ui/media'
 import Text from '../../i18n/Text'
-import {t} from '../../i18n/i18n'
 
 const NavBar = () => (
   <div className='navigation'>
-    <a className='link' href='/koski/omadata/kayttooikeudet'>
-      <span className='inactiveTab'><Text name={'Annetut käyttöluvat'} /></span>
+    <span className='activeTab'><Text name={'Annetut käyttöluvat'} /></span>
+    <a className='link' href='/oma-opintopolku-loki/'>
+      <span className='inactiveTab'><Text name={'Tietojani käyttäneet toimijat'} /></span>
     </a>
-    <span className='activeTab'><Text name={'Tietojani käyttäneet toimijat'} /></span>
     <span className='filler'/>
   </div>
 )
