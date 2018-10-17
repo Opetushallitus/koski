707e15b5bd2576b186fb7435f19b7c25d8b92cdc
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-10-17 18:03:50 +0300

    Same styles to description as in oma-opintopolku-loki

--- web/app/omadata/luvanhallinta/LuvanHallintaHeadline.jsx
+++ web/app/omadata/luvanhallinta/LuvanHallintaHeadline.jsx
@@ -6,8 +6,9 @@ export const LuvanHallintaHeadline = ({birthday}) => (
   <div className='kayttoluvat-headline' tabIndex={0}>
     <div className='kayttoluvat-info'>
       <h1><Text name='Tietojeni käyttö'/></h1>
-      <Text className='info' name={'Tällä sivulla voit tarkastella ja hallinnoida antamiasi käyttölupia tietoihisi. Lisäksi näet mitkä tahot, esim. viranomaiset, ovat katsoneet opintotietojasi.'}
-      />
+      <p className='info'>
+        <Text name={'Tällä sivulla voit tarkastella ja hallinnoida antamiasi käyttölupia tietoihisi. Lisäksi näet mitkä tahot, esim. viranomaiset, ovat katsoneet opintotietojasi.'} />
+      </p>
     </div>
     <h3 className='oppija-nimi'><span className='nimi'>{userP.map(user => user && user.name)}</span><span className='pvm'>{` s. ${birthday}`}</span></h3>
   </div>
