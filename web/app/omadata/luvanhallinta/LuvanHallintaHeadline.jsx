7f30afc35389f60085df8af4b0cf64589bd28692
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-10-19 15:23:45 +0300

    Added missing localizations

--- web/app/omadata/luvanhallinta/LuvanHallintaHeadline.jsx
+++ web/app/omadata/luvanhallinta/LuvanHallintaHeadline.jsx
@@ -3,13 +3,13 @@ import {userP} from '../../util/user'
 import Text from '../../i18n/Text'
 
 export const LuvanHallintaHeadline = ({birthday}) => (
-  <div className="kayttoluvat-headline" tabIndex={0}>
-    <div className="kayttoluvat-info">
-      <h1><Text name="Tietojeni käyttö"/></h1>
-      <p className="info">
-        <Text name="Tällä sivulla voit tarkastella ja hallinnoida antamiasi käyttölupia tietoihisi. Lisäksi näet mitkä tahot, esim. viranomaiset, ovat katsoneet opintotietojasi." />
+  <div className='kayttoluvat-headline' tabIndex={0}>
+    <div className='kayttoluvat-info'>
+      <h1><Text name='Tietojeni käyttö'/></h1>
+      <p className='info'>
+        <Text name='Tällä sivulla voit tarkastella ja hallinnoida antamiasi käyttölupia tietoihisi. Lisäksi näet...' />
       </p>
     </div>
-    <h3 className="oppija-nimi"><span className="nimi">{userP.map(user => user && user.name)}</span><span className="pvm">{` s. ${birthday}`}</span></h3>
+    <h3 className='oppija-nimi'><span className='nimi'>{userP.map(user => user && user.name)}</span><span className='pvm'>{` s. ${birthday}`}</span></h3>
   </div>
 )
