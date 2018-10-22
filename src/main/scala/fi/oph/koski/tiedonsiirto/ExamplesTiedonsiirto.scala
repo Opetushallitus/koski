845252cc69112ec6e945a0f1dc6adec860a890ae
Pasi Eronen <pasi.eronen@reaktor.com>
2018-10-22 15:28:02 +0300

    Korjaa tiedonsiirto-esimerkki-testi

--- src/main/scala/fi/oph/koski/tiedonsiirto/ExamplesTiedonsiirto.scala
+++ src/main/scala/fi/oph/koski/tiedonsiirto/ExamplesTiedonsiirto.scala
@@ -5,7 +5,7 @@ import fi.oph.koski.documentation.{AmmatillinenExampleData, AmmatillinenPerustut
 import fi.oph.koski.henkilo.{MockOppijat, OppijaHenkilö}
 import fi.oph.koski.henkilo.MockOppijat.asUusiOppija
 import fi.oph.koski.organisaatio.MockOrganisaatiot
-import fi.oph.koski.schema.{AmmatillinenOpiskeluoikeus, Oppija, Oppilaitos}
+import fi.oph.koski.schema.{AmmatillinenOpiskeluoikeus, Oppija, Oppilaitos, TäydellisetHenkilötiedot}
 
 object ExamplesTiedonsiirto {
   val opiskeluoikeus: AmmatillinenOpiskeluoikeus = AmmatillinenExampleData.opiskeluoikeus().copy(lähdejärjestelmänId = Some(winnovaLähdejärjestelmäId))
@@ -17,9 +17,12 @@ object ExamplesTiedonsiirto {
 
   val examples: List[Example] = List(
     Example("tiedonsiirto - onnistunut", "Onnistunut tiedonsiirto", Oppija(asUusiOppija(MockOppijat.tiedonsiirto), List(opiskeluoikeus)), 403),
-    Example("tiedonsiirto - vain syntymäaika", "Onnistunut tiedonsiirto", Oppija(MockOppijat.hetuton.toHenkilötiedotJaOid, List(opiskeluoikeus)), 403),
+    Example("tiedonsiirto - vain syntymäaika", "Onnistunut tiedonsiirto", Oppija(asTäydellisetHenkilötiedotExample(MockOppijat.hetuton), List(opiskeluoikeus)), 403),
     Example("tiedonsiirto - epäonnistunut", "Epäonnistunut tiedonsiirto", Oppija(asUusiOppija(MockOppijat.tiedonsiirto), List(failingOpiskeluoikeus)), 403),
     Example("tiedonsiirto - epäonnistunut 2", "Onnistunut tiedonsiirto", Oppija(asUusiOppija(MockOppijat.ammattilainen), List(failingOpiskeluoikeus)), 403),
     Example("tiedonsiirto - epäonnistunut 3", "Epäonnistunut tiedonsiirto", Oppija(asUusiOppija(epävalidiHenkilö), List(failingTutkinnonosaOpiskeluoikeus)), 403)
   )
+
+  private def asTäydellisetHenkilötiedotExample(henkilö: OppijaHenkilö): TäydellisetHenkilötiedot =
+    TäydellisetHenkilötiedot(oid = henkilö.oid, etunimet = henkilö.etunimet, kutsumanimi = henkilö.kutsumanimi, sukunimi = henkilö.sukunimi)
 }
