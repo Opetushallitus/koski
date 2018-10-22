f154d1960eb6bc6c6318cef3f24066bf0daf03c7
Petri Myllys <petri.myllys@reaktor.com>
2018-10-22 10:20:06 +0300

    Generalisoi ryhmitelty oppiainetaulukko

--- web/app/ib/IB.jsx
+++ web/app/ib/IB.jsx
@@ -1,22 +1,11 @@
-import React from 'baret'
 import * as R from 'ramda'
 import Bacon from 'baconjs'
-import {LukionOppiaineEditor} from '../lukio/LukionOppiaineEditor'
-import {
-  LukionOppiaineetTableHead,
-  OmatTiedotLukionOppiaineetTableHead
-} from '../lukio/fragments/LukionOppiaineetTableHead'
-import {modelData, modelItems, modelLookup} from '../editor/EditorModel'
-import {FootnoteDescriptions} from '../components/footnote'
-import {UusiIBOppiaineDropdown} from './UusiIBOppiaineDropdown'
+import {modelData, modelLookup} from '../editor/EditorModel'
 import {koodistoValues} from '../uusioppija/koodisto'
-import {t} from '../i18n/i18n'
-import {OmatTiedotLukionOppiaine} from '../lukio/OmatTiedotLukionOppiaineet'
-import {isMobileAtom} from '../util/isMobileAtom'
 
-const ArvosanaFootnote = {title: 'Ennustettu arvosana', hint: '*'}
+export const arvosanaFootnote = {title: 'Ennustettu arvosana', hint: '*'}
 
-const IBRyhmät = (oppiaineet, päätasonSuoritusModel, edit) => {
+export const ibRyhmät = (oppiaineet, päätasonSuoritusModel, edit) => {
   const ryhmätOppiaineista = Bacon.constant(oppiaineet.map(oppiaine => modelData(oppiaine, 'koulutusmoduuli.ryhmä')))
   const ryhmätKoodistosta = edit ? koodistoValues('aineryhmaib') : Bacon.constant([])
   const ryhmätKaikki = Bacon.combineWith(ryhmätOppiaineista, ryhmätKoodistosta,
@@ -27,90 +16,8 @@ const IBRyhmät = (oppiaineet, päätasonSuoritusModel, edit) => {
 
   const yhteisetIbSuoritukset = ['theoryOfKnowledge', 'creativityActionService', 'extendedEssay'].map(k => modelLookup(päätasonSuoritusModel, k))
   const footnotes = R.any(s => modelData(s, 'arviointi.-1.predicted'), R.concat(oppiaineet, yhteisetIbSuoritukset))
-    ? [ArvosanaFootnote]
+    ? [arvosanaFootnote]
     : []
 
   return {aineryhmät, footnotes}
-}
-
-export const IBTutkinnonOppiaineetEditor = ({suorituksetModel, additionalEditableKoulutusmoduuliProperties}) => {
-  const {edit, suoritus: päätasonSuoritusModel} = suorituksetModel.context
-  const oppiaineet = modelItems(suorituksetModel)
-
-  const {aineryhmät, footnotes} = IBRyhmät(oppiaineet, päätasonSuoritusModel, edit)
-
-  return (
-    <div>
-      <table className='suoritukset oppiaineet'>
-        <LukionOppiaineetTableHead />
-        <tbody>
-        {
-          aineryhmät.map(ryhmät => ryhmät.map(r => [
-            <tr className='aineryhmä' key={r.ryhmä.koodiarvo}>
-              <th colSpan='4'>{t(r.ryhmä.nimi)}</th>
-            </tr>,
-            r.aineet && r.aineet.map((oppiaine, oppiaineIndex) => {
-              const footnote = modelData(oppiaine, 'arviointi.-1.predicted') && ArvosanaFootnote
-              return (
-                <LukionOppiaineEditor
-                  key={oppiaineIndex}
-                  oppiaine={oppiaine}
-                  footnote={footnote}
-                  additionalEditableKoulutusmoduuliProperties={additionalEditableKoulutusmoduuliProperties}
-                />
-              )
-            }),
-            <tr className='uusi-oppiaine' key={`uusi-oppiaine-${r.ryhmä.koodiarvo}`}>
-              <td colSpan='4'>
-                <UusiIBOppiaineDropdown
-                  model={päätasonSuoritusModel}
-                  aineryhmä={r.ryhmä}
-                />
-              </td>
-            </tr>
-          ]))
-        }
-        </tbody>
-      </table>
-      {!R.isEmpty(footnotes) && <FootnoteDescriptions data={footnotes}/>}
-    </div>
-  )
-}
-
-export const OmatTiedotIBTutkinnonOppiaineet = ({suorituksetModel}) => {
-  const {suoritus: päätasonSuoritusModel} = suorituksetModel.context
-  const oppiaineet = modelItems(suorituksetModel)
-
-  const {aineryhmät, footnotes} = IBRyhmät(oppiaineet, päätasonSuoritusModel)
-
-  return (
-    <div className='ib-aineryhmat'>
-      {
-        aineryhmät.map(ryhmät => ryhmät.map(r => [
-          <h4 key={r.ryhmä.koodiarvo} className='aineryhma-title'>
-            {t(r.ryhmä.nimi)}
-          </h4>,
-          <table key={`suoritustable-${r.ryhmä.koodiarvo}`} className='omattiedot-suoritukset'>
-            <OmatTiedotLukionOppiaineetTableHead />
-            <tbody>
-            {r.aineet && r.aineet.map((oppiaine, oppiaineIndex) => {
-              const footnote = modelData(oppiaine, 'arviointi.-1.predicted') && ArvosanaFootnote
-              return (
-                <OmatTiedotLukionOppiaine
-                  baret-lift
-                  key={oppiaineIndex}
-                  oppiaine={oppiaine}
-                  isMobile={isMobileAtom}
-                  footnote={footnote}
-                />
-              )
-            })}
-            </tbody>
-          </table>
-        ]))
-      }
-      {!R.isEmpty(footnotes) && <FootnoteDescriptions data={footnotes}/>}
-    </div>
-  )
-
 }
---
+++ web/app/suoritus/RyhmiteltyOppiaineetEditor.jsx
@@ -0,0 +1,112 @@
+import React from 'baret'
+import * as R from 'ramda'
+import {modelData, modelItems} from '../editor/EditorModel'
+import {t} from '../i18n/i18n'
+import {isMobileAtom} from '../util/isMobileAtom'
+import {
+  LukionOppiaineetTableHead,
+  OmatTiedotLukionOppiaineetTableHead
+} from '../lukio/fragments/LukionOppiaineetTableHead'
+import {LukionOppiaineEditor} from '../lukio/LukionOppiaineEditor'
+import {arvosanaFootnote, ibRyhmät} from '../ib/IB'
+import {UusiIBOppiaineDropdown} from '../ib/UusiIBOppiaineDropdown'
+import {FootnoteDescriptions} from '../components/footnote'
+import {OmatTiedotLukionOppiaine} from '../lukio/OmatTiedotLukionOppiaineet'
+
+const resolveGroupingFn = päätasonSuoritusClass => {
+  switch (päätasonSuoritusClass) {
+    case 'ibtutkinnonsuoritus': return ibRyhmät
+    default: {
+      console.error(`Oppiaineiden ryhmittely ei onnistu päätason suoritukselle ${päätasonSuoritusClass}.`)
+      return () => ({})
+    }
+  }
+}
+
+const resolveFootnotes = päätasonSuoritusClass => {
+  switch (päätasonSuoritusClass) {
+    case 'ibtutkinnonsuoritus': return arvosanaFootnote
+    default: return null
+  }
+}
+
+export const RyhmiteltyOppiaineetEditor = ({suorituksetModel, päätasonSuoritusClass, additionalEditableKoulutusmoduuliProperties}) => {
+  const {edit, suoritus: päätasonSuoritusModel} = suorituksetModel.context
+  const oppiaineet = modelItems(suorituksetModel)
+
+  const {aineryhmät, footnotes} = resolveGroupingFn(päätasonSuoritusClass)(oppiaineet, päätasonSuoritusModel, edit)
+
+  return aineryhmät ? (
+    <div>
+      <table className='suoritukset oppiaineet'>
+        <LukionOppiaineetTableHead />
+        <tbody>
+        {
+          aineryhmät.map(ryhmät => ryhmät.map(r => [
+            <tr className='aineryhmä' key={r.ryhmä.koodiarvo}>
+              <th colSpan='4'>{t(r.ryhmä.nimi)}</th>
+            </tr>,
+            r.aineet && r.aineet.map((oppiaine, oppiaineIndex) => {
+              const footnote = resolveFootnotes(päätasonSuoritusClass)
+              return (
+                <LukionOppiaineEditor
+                  key={oppiaineIndex}
+                  oppiaine={oppiaine}
+                  footnote={footnote}
+                  additionalEditableKoulutusmoduuliProperties={additionalEditableKoulutusmoduuliProperties}
+                />
+              )
+            }),
+            <tr className='uusi-oppiaine' key={`uusi-oppiaine-${r.ryhmä.koodiarvo}`}>
+              <td colSpan='4'>
+                <UusiIBOppiaineDropdown
+                  model={päätasonSuoritusModel}
+                  aineryhmä={r.ryhmä}
+                />
+              </td>
+            </tr>
+          ]))
+        }
+        </tbody>
+      </table>
+      {!R.isEmpty(footnotes) && <FootnoteDescriptions data={footnotes}/>}
+    </div>
+  ) : null
+}
+
+export const OmatTiedotRyhmiteltyOppiaineet = ({suorituksetModel, päätasonSuoritusClass}) => {
+  const {suoritus: päätasonSuoritusModel} = suorituksetModel.context
+  const oppiaineet = modelItems(suorituksetModel)
+
+  const {aineryhmät, footnotes} = resolveGroupingFn(päätasonSuoritusClass)(oppiaineet, päätasonSuoritusModel)
+
+  return aineryhmät ? (
+    <div className='aineryhmat'>
+      {
+        aineryhmät.map(ryhmät => ryhmät.map(r => [
+          <h4 key={r.ryhmä.koodiarvo} className='aineryhma-title'>
+            {t(r.ryhmä.nimi)}
+          </h4>,
+          <table key={`suoritustable-${r.ryhmä.koodiarvo}`} className='omattiedot-suoritukset'>
+            <OmatTiedotLukionOppiaineetTableHead />
+            <tbody>
+            {r.aineet && r.aineet.map((oppiaine, oppiaineIndex) => {
+              const footnote = modelData(oppiaine, 'arviointi.-1.predicted') && arvosanaFootnote
+              return (
+                <OmatTiedotLukionOppiaine
+                  baret-lift
+                  key={oppiaineIndex}
+                  oppiaine={oppiaine}
+                  isMobile={isMobileAtom}
+                  footnote={footnote}
+                />
+              )
+            })}
+            </tbody>
+          </table>
+        ]))
+      }
+      {!R.isEmpty(footnotes) && <FootnoteDescriptions data={footnotes}/>}
+    </div>
+  ) : null
+}
