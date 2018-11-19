fb37ccea620578d656130c3b9f81c2732db5d38f
suarela <aleksi.saarela@reaktor.fi>
2018-11-19 13:57:59 +0200

    dokumentoi Caller-Id

--- src/main/scala/fi/oph/koski/http/OpintopolkuCallerId.scala
+++ src/main/scala/fi/oph/koski/http/OpintopolkuCallerId.scala
@@ -1,6 +1,6 @@
 package fi.oph.koski.http
 
 object OpintopolkuCallerId {
-  // clientSubSystemCode used by Koski when calling Opintopolku services via HTTP
+  // Caller-Id used by Koski when calling Opintopolku services via HTTP
   val koski = "1.2.246.562.10.00000000001.koski"
 }
