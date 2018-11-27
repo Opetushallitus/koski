60a870b09ef527ed3979290fb5a9cb680bd75ecd
suarela <aleksi.saarela@reaktor.fi>
2018-11-27 17:14:33 +0200

    api palauttaa asiakkaan käyttötarkoituksen

--- src/main/scala/fi/oph/koski/mydata/MyDataJakoItem.scala
+++ src/main/scala/fi/oph/koski/mydata/MyDataJakoItem.scala
@@ -3,4 +3,4 @@ package fi.oph.koski.mydata
 import java.sql.Timestamp
 import java.time.LocalDate
 
-case class MyDataJakoItem(asiakasId: String, asiakasName: String, expirationDate: LocalDate, timestamp: Timestamp)
+case class MyDataJakoItem(asiakasId: String, asiakasName: String, expirationDate: LocalDate, timestamp: Timestamp, purpose: String)
