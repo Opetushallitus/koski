8feafb8c3bac9efa1d302cd96134a97ac85782c8
suarela <aleksi.saarela@reaktor.fi>
2018-11-28 14:38:38 +0200

    poistaa yläpalkista uloskirjautumisen ja mobiilimenun

--- web/app/omadata/Header.jsx
+++ web/app/omadata/Header.jsx
@@ -1,10 +1,7 @@
 import React from 'baret'
-import Atom from 'bacon.atom'
 import Text from '../i18n/Text'
-import Logout from './fragments/Logout'
 import {lang, setLang} from '../i18n/i18n'
 
-const menuOpened = Atom(false)
 
 const ChangeLang = () =>
   (<span className='change-lang' onClick={() => lang === 'sv' ? setLang('fi') : setLang('sv')}>
@@ -12,11 +9,8 @@ const ChangeLang = () =>
   </span>)
 
 
-export default ({ logoutURL }) => (
+export default () => (
   <div className='header'>
-    <button id='header-mobile-menu-button' onClick={() => menuOpened.modify(x => !x)}>
-      <img src='/koski/images/baseline-menu-24px.svg' />
-    </button>
     <div className='title'>
       <img src='/koski/images/opintopolku_logo.svg' alt='' />
       <h1><Text name='Oma Opintopolku'/></h1>
@@ -26,16 +20,5 @@ export default ({ logoutURL }) => (
       <ChangeLang />
     </div>
 
-    <div>
-      <div className='user'>
-        <Logout logoutURL={logoutURL} />
-      </div>
-
-      <div id='header-mobile-menu' className={menuOpened.map(opened => opened ? 'menu-open' : 'menu-closed')}>
-        <div className='top'>
-          <Logout logoutURL={logoutURL} />
-        </div>
-      </div>
-    </div>
   </div>
 )
