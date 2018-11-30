8f5afb72c65f736279078be3482762c45fbdd56e
Petri Myllys <petri.myllys@reaktor.com>
2018-11-30 09:25:27 +0200

    Lisää lisäaineet

--- web/app/dia/DIA.jsx
+++ web/app/dia/DIA.jsx
@@ -1,18 +1,20 @@
 import * as R from 'ramda'
 import Bacon from 'baconjs'
-import {modelData} from '../editor/EditorModel'
+import {modelData, modelLookup} from '../editor/EditorModel'
 import {koodistoValues} from '../uusioppija/koodisto'
 
 export const diaRyhmät = (oppiaineet, päätasonSuoritusModel, edit) => {
-  const osaAlueetOppiaineista = Bacon.constant(oppiaineet.map(oppiaine => modelData(oppiaine, 'koulutusmoduuli.osaAlue')))
+  const [aineryhmäAineet, muutAineet] = R.partition(a => modelLookup(a, 'koulutusmoduuli').value.classes.includes('diaosaalueoppiaine'), oppiaineet)
+
+  const osaAlueetOppiaineista = Bacon.constant(aineryhmäAineet.map(oppiaine => modelData(oppiaine, 'koulutusmoduuli.osaAlue')))
   const osaAlueetKoodistosta = edit ? koodistoValues('diaosaalue') : Bacon.constant([])
   const osaAlueetKaikki = Bacon.combineWith(osaAlueetOppiaineista, osaAlueetKoodistosta,
     (oppiaineista, koodistosta) => R.pipe(R.uniqBy(R.prop('koodiarvo')), R.sortBy(R.prop('koodiarvo')))(koodistosta.concat(oppiaineista)))
 
-  const oppiaineetAineryhmittäin = R.groupBy(oppiaine => modelData(oppiaine, 'koulutusmoduuli.osaAlue').koodiarvo, oppiaineet)
+  const oppiaineetAineryhmittäin = R.groupBy(oppiaine => modelData(oppiaine, 'koulutusmoduuli.osaAlue').koodiarvo, aineryhmäAineet)
   const aineryhmät = osaAlueetKaikki.map(ryhmät => ryhmät.map(ryhmä => ({ryhmä, aineet: oppiaineetAineryhmittäin[ryhmä.koodiarvo]})))
 
   const footnotes = []
 
-  return {aineryhmät, footnotes}
+  return {aineryhmät, muutAineet, footnotes}
 }
