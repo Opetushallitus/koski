9fff1e03dfc5a471f348dbfa7e3996cacf4fcfcb
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-12-03 17:20:43 +0200

    Virheenhanskausta

--- src/main/scala/fi/oph/koski/luovutuspalvelu/LuovutuspalveluServlet.scala
+++ src/main/scala/fi/oph/koski/luovutuspalvelu/LuovutuspalveluServlet.scala
@@ -7,7 +7,6 @@ import fi.oph.koski.henkilo.{HenkilöOid, Hetu}
 import fi.oph.koski.http.{HttpStatus, JsonErrorMessage, KoskiErrorCategory}
 import fi.oph.koski.json.JsonSerializer
 import fi.oph.koski.koskiuser.RequiresLuovutuspalvelu
-import fi.oph.koski.luovutuspalvelu.SoapTester.soapString
 import fi.oph.koski.schema.{Henkilö, Opiskeluoikeus}
 import fi.oph.koski.servlet.{ApiServlet, NoCache, ObservableSupport}
 import fi.oph.koski.util.{Timing, XML}
@@ -15,12 +14,13 @@ import org.json4s.JValue
 import org.json4s.JsonAST.{JBool, JObject}
 import org.scalatra.ContentEncodingSupport
 
-import scala.xml.NodeSeq
+import scala.util.control.NonFatal
+import scala.xml.transform.{RewriteRule, RuleTransformer}
+import scala.xml.{Elem, Node, NodeSeq, PCData}
 
 class LuovutuspalveluServlet(implicit val application: KoskiApplication) extends ApiServlet with ObservableSupport with RequiresLuovutuspalvelu with ContentEncodingSupport with NoCache with Timing {
   before() {
-    // Tämä koodi ei ole vielä tuotantokelpoista.
-    if (application.features.luovutuspalvelu) {
+    if (!application.features.luovutuspalvelu) {
       haltWithStatus(KoskiErrorCategory.badRequest("Luovutuspalvelu-rajapinta ei käytössä tässä ympäristössä."))
     }
   }
@@ -53,33 +53,49 @@ class LuovutuspalveluServlet(implicit val application: KoskiApplication) extends
   }
 
   post("/suomi-fi-rekisteritiedot") {
-    val soapString = request.body
-    val soap = SoapUtils.loadXmlString(soapString) match {
-      case Right(x) => x
-      case Left(err) =>
-        println(soapString)
-        throw new RuntimeException(err.toString)
-    }
-    val req = soap \\ "Envelope" \\ "Body" \\ "suomiFiRekisteritiedot"
-    val ssn = (req\\ "hetu").headOption.map(_.text.trim)
-    println("ssn: " + ssn)
-
-    val result = application.suomiFiService.suomiFiOpiskeluoikeudet(ssn.get)
-
-    println(result)
-
-    result match {
-      case Right(o)=> {
-        val foo = JsonSerializer.writeWithRoot(o).toString
-        val responseBody = <ns1:suomiFiRekisteritiedotResponse xmlns:ns1="http://docs.koski-xroad.fi/producer">
-          <ns1:opiskeluoikeudet>{foo}</ns1:opiskeluoikeudet>
-          </ns1:suomiFiRekisteritiedotResponse>
-        val soapResponse: NodeSeq = SoapUtils.replaceSoapBody(soap, responseBody)
+    (for {
+      xml <- readXml
+      hetu <- extractHetu(xml)
+      opiskeluoikeudet <- application.suomiFiService.suomiFiOpiskeluoikeudet(hetu)
+    } yield soapBody(xml, opiskeluoikeudet)) match {
+      case Right(soap)=>
         contentType = "text/xml"
-        response.writer.print(XML.prettyPrintNodes(soapResponse))
-      }
+        response.writer.print(XML.prettyPrintNodes(soap))
       case _ => KoskiErrorCategory.internalError
     }
+  }
+
+  private def extractHetu(soap: Elem) =
+    (soap \\ "Envelope" \\ "Body" \\ "suomiFiRekisteritiedot" \\ "hetu")
+      .headOption.map(_.text.trim)
+      .toRight(KoskiErrorCategory.badRequest.validation.henkilötiedot.hetu("Hetu puuttuu"))
+
+  private def soapBody(soap: Elem, o: SuomiFiResponse) = {
+    replaceSoapBody(soap, <ns1:suomiFiRekisteritiedotResponse xmlns:ns1="http://docs.koski-xroad.fi/producer">
+      <ns1:opiskeluoikeudet>
+        {PCData(JsonSerializer.writeWithRoot(o))}
+      </ns1:opiskeluoikeudet>
+    </ns1:suomiFiRekisteritiedotResponse>)
+  }
+
+  private def readXml: Either[HttpStatus, Elem] = try {
+    Right(scala.xml.XML.loadString(request.body))
+  } catch {
+    case NonFatal(e) =>
+      Left(KoskiErrorCategory.badRequest.format.xml(e.getMessage))
+  }
+
+  private def replaceSoapBody(envelope: NodeSeq, newBody: Node): NodeSeq = {
+    val SoapEnvelopeNamespace = "http://schemas.xmlsoap.org/soap/envelope/"
+    val requestToResponse = new RewriteRule {
+      override def transform(n: Node): Seq[Node] = n match {
+        case e@Elem(prefix, "Body", attribs, scope, _, _*) if e.namespace == SoapEnvelopeNamespace =>
+          Elem(prefix, "Body", attribs, scope, false, newBody)
+        case other =>
+          other
+      }
+    }
+    new RuleTransformer(requestToResponse).transform(envelope)
   }
 
   private def parseOidRequestV1(parsedJson: JValue): Either[HttpStatus, OidRequestV1] = {
--- src/main/scala/fi/oph/koski/luovutuspalvelu/SuomiFiService.scala
+++ src/main/scala/fi/oph/koski/luovutuspalvelu/SuomiFiService.scala
@@ -3,35 +3,30 @@ package fi.oph.koski.luovutuspalvelu
 import java.time.LocalDate
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.editor.{OppijaEditorModel, OppilaitoksenOpiskeluoikeudet}
+import fi.oph.koski.editor.OppilaitoksenOpiskeluoikeudet
 import fi.oph.koski.http.HttpStatus
 import fi.oph.koski.koskiuser.KoskiSession
 import fi.oph.koski.omattiedot.OmatTiedotEditorModel
-import fi.oph.koski.schema.{AmmatillinenTutkintoKoulutus, LocalizedString, Opiskeluoikeus, Oppija}
-
+import fi.oph.koski.schema.{AmmatillinenTutkintoKoulutus, LocalizedString, Opiskeluoikeus}
 
 class SuomiFiService(koskiApplication: KoskiApplication) {
+  def suomiFiOpiskeluoikeudet(hetu: String)(implicit user: KoskiSession): Either[HttpStatus, SuomiFiResponse] =
+    koskiApplication.oppijaFacade.findOppijaByHetuOrCreateIfInYtrOrVirta(hetu).flatMap(_.warningsToLeft)
+      .map(OmatTiedotEditorModel.opiskeluoikeudetOppilaitoksittain)
+      .map(convertToSuomiFi)
+      .map(SuomiFiResponse)
 
- def suomiFiOpiskeluoikeudet(hetu: String)(implicit user: KoskiSession): Either[HttpStatus, SuomiFiResponse] = {
-  val oppija = koskiApplication.oppijaFacade.findOppijaByHetuOrCreateIfInYtrOrVirta(hetu, useVirta = true, useYtr = true)
-   oppija.flatMap(_.warningsToLeft)
-     .map(OmatTiedotEditorModel.opiskeluoikeudetOppilaitoksittain)
-     .map(_.map(convertToSuomiFi))
-     .map(SuomiFiResponse)
- }
-
-  private def convertToSuomiFi(oos: OppilaitoksenOpiskeluoikeudet) = {
-    val nimi = oos.oppilaitos.nimi.get
-    val opiskeluoikeudet: List[SuomiFiOpiskeluoikeus] = oos.opiskeluoikeudet.map(o => SuomiFiOpiskeluoikeus(o.tila.opiskeluoikeusjaksot.last.tila.nimi.get, o.alkamispäivä.get, o.päättymispäivä, getSuorituksenNimi(o)))
-    SuomiFiOppilaitos(nimi, opiskeluoikeudet)
+  private def convertToSuomiFi(oppilaitosOpiskeluoikeudet: List[OppilaitoksenOpiskeluoikeudet]) = oppilaitosOpiskeluoikeudet.map { oos =>
+    SuomiFiOppilaitos(oos.oppilaitos.nimi.get, oos.opiskeluoikeudet.map { oo =>
+      SuomiFiOpiskeluoikeus(oo.tila.opiskeluoikeusjaksot.last.tila.nimi.get, oo.alkamispäivä.get, oo.päättymispäivä, suorituksenNimi(oo).getOrElse(oo.tyyppi.nimi.get))
+    })
   }
 
-  private def getSuorituksenNimi(oo: Opiskeluoikeus) = {
+  private def suorituksenNimi(oo: Opiskeluoikeus) =
     oo.suoritukset.head.koulutusmoduuli match {
-      case a: AmmatillinenTutkintoKoulutus => a.perusteenNimi.get
-      case x => x.tunniste.getNimi.get
+      case a: AmmatillinenTutkintoKoulutus => a.perusteenNimi
+      case x => x.tunniste.getNimi
     }
-  }
 }
 
 case class SuomiFiOpiskeluoikeus(tila: LocalizedString, alku: LocalDate, loppu: Option[LocalDate], nimi: LocalizedString)
