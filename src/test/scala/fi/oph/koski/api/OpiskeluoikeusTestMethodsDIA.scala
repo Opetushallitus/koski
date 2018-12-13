dc4a128070e6fc249acc2f6af49fe03c794a5059
Petri Myllys <petri.myllys@reaktor.com>
2018-12-13 19:31:39 +0200

    Testaa DIA-suorituksen validointia

---
+++ src/test/scala/fi/oph/koski/api/OpiskeluoikeusTestMethodsDIA.scala
@@ -0,0 +1,44 @@
+package fi.oph.koski.api
+
+import java.time.LocalDate.{of => date}
+
+import fi.oph.koski.documentation.ExampleData._
+import fi.oph.koski.documentation.LukioExampleData
+import fi.oph.koski.documentation.DIAExampleData.saksalainenKoulu
+import fi.oph.koski.schema._
+
+trait OpiskeluoikeusTestMethodsDIA extends PutOpiskeluoikeusTestMethods[DIAOpiskeluoikeus]{
+  def tag = implicitly[reflect.runtime.universe.TypeTag[DIAOpiskeluoikeus]]
+
+  override def defaultOpiskeluoikeus = OpiskeluoikeusTestMethodsDIA.opiskeluoikeus
+  def valmisOpiskeluoikeus = OpiskeluoikeusTestMethodsDIA.opiskeluoikeusValmis
+}
+
+object OpiskeluoikeusTestMethodsDIA {
+  def tutkintoSuoritus = DIATutkinnonSuoritus(
+    toimipiste = saksalainenKoulu,
+    suorituskieli = englanti,
+    kokonaispistemäärä = Some(800),
+    osasuoritukset = None
+  )
+
+  def opiskeluoikeus = DIAOpiskeluoikeus(
+    oppilaitos = Some(saksalainenKoulu),
+    tila = LukionOpiskeluoikeudenTila(
+      List(
+        LukionOpiskeluoikeusjakso(date(2012, 9, 1), LukioExampleData.opiskeluoikeusAktiivinen)
+      )
+    ),
+    suoritukset = List(tutkintoSuoritus)
+  )
+
+  def opiskeluoikeusValmis = opiskeluoikeus.copy(
+    päättymispäivä = Some(date(2016, 6, 4)),
+    tila = LukionOpiskeluoikeudenTila(
+      List(
+        LukionOpiskeluoikeusjakso(date(2012, 9, 1), LukioExampleData.opiskeluoikeusAktiivinen),
+        LukionOpiskeluoikeusjakso(date(2016, 6, 4), LukioExampleData.opiskeluoikeusPäättynyt)
+      )
+    )
+  )
+}
