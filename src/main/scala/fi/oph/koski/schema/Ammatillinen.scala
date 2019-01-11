422d877491dfc54cfa152240c9e9a26ac54f072a
Petri Myllys <petri.myllys@reaktor.com>
2019-01-11 09:56:57 +0200

    Lisää muun ammatillisen koulutuksen osasuoritus-rakenne

--- src/main/scala/fi/oph/koski/schema/Ammatillinen.scala
+++ src/main/scala/fi/oph/koski/schema/Ammatillinen.scala
@@ -1114,6 +1114,8 @@ case class MuunAmmatillisenKoulutuksenSuoritus(
   @Tooltip("Osaamisen hankkimistavat (oppisopimus, koulutussopimus, oppilaitosmuotoinen koulutus) eri ajanjaksoina.")
   osaamisenHankkimistavat: Option[List[OsaamisenHankkimistapajakso]] = None,
   koulutussopimukset: Option[List[Koulutussopimusjakso]] = None,
+  @Description("Suoritukseen kuuluvien osasuoritusten suoritukset")
+  override val osasuoritukset: Option[List[MuunAmmatillisenKoulutuksenOsasuorituksenSuoritus]],
   todistuksellaNäkyvätLisätiedot: Option[LocalizedString] = None,
   @KoodistoKoodiarvo("muuammatillinenkoulutus")
   tyyppi: Koodistokoodiviite = Koodistokoodiviite("muuammatillinenkoulutus", "suorituksentyyppi"),
@@ -1144,6 +1146,44 @@ case class PaikallinenMuuAmmatillinenKoulutus(
   @Tooltip("Kuvaus koulutuksen sisällöstä osaamisena.")
   kuvaus: LocalizedString
 ) extends PaikallinenKoulutusmoduuli with MuuAmmatillinenKoulutus
+
+case class MuunAmmatillisenKoulutuksenOsasuorituksenSuoritus(
+  koulutusmoduuli: MuunAmmatillisenKoulutuksenOsasuoritus,
+  override val alkamispäivä: Option[LocalDate],
+  arviointi: Option[List[AmmatillinenArviointi]],
+  @Tooltip("Tiedot aiemmin hankitun osaamisen tunnustamisesta.")
+  @ComplexObject
+  tunnustettu: Option[OsaamisenTunnustaminen] = None,
+  @Tooltip("Suoritukseen liittyvät lisätiedot, kuten esimerkiksi mukautettu arviointi tai poikkeus arvioinnissa. Sisältää lisätiedon tyypin sekä vapaamuotoisen kuvauksen.")
+  @ComplexObject
+  lisätiedot: Option[List[MuunAmmatillisenKoulutuksenOsasuorituksenLisätieto]],
+  suorituskieli: Option[Koodistokoodiviite],
+  @Description("Osasuoritukseen liittyvän näytön tiedot")
+  @Tooltip("Osasuoritukseen kuuluvan ammattiosaamisen näytön tiedot.")
+  @ComplexObject
+  näyttö: Option[Näyttö] = None,
+  @Description("Osasuoritukseen sisältyvien osasuoritusten suoritukset")
+  @Title("Sisältyvät osasuoritukset")
+  override val osasuoritukset: Option[List[MuunAmmatillisenKoulutuksenOsasuorituksenSuoritus]] = None,
+  @KoodistoKoodiarvo("muunammatillisenkoulutuksenosasuoritus")
+  tyyppi: Koodistokoodiviite = Koodistokoodiviite("muunammatillisenkoulutuksenosasuoritus", koodistoUri = "suorituksentyyppi")
+) extends Suoritus with Vahvistukseton
+
+case class MuunAmmatillisenKoulutuksenOsasuoritus(
+  tunniste: PaikallinenKoodi,
+  pakollinen: Boolean,
+  laajuus: Option[Laajuus],
+  kuvaus: LocalizedString
+) extends PaikallinenKoulutusmoduuli
+
+@Description("Suoritukseen liittyvät lisätiedot, kuten esimerkiksi mukautettu arviointi tai poikkeus arvioinnissa.")
+case class MuunAmmatillisenKoulutuksenOsasuorituksenLisätieto(
+  @Description("Lisätiedon tyyppi kooditettuna")
+  @KoodistoUri("ammatillisentutkinnonosanlisatieto")
+  tunniste: Koodistokoodiviite,
+  @Description("Lisätiedon kuvaus siinä muodossa, kuin se näytetään todistuksella")
+  kuvaus: LocalizedString
+)
 
 trait AmmatillinenKoodistostaLöytyväArviointi extends KoodistostaLöytyväArviointi with ArviointiPäivämäärällä {
   @KoodistoUri("arviointiasteikkoammatillinenhyvaksyttyhylatty")
