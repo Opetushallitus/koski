d72e835a209f52c52ea8aca25df9d1fdbb26cb6c
Heikki Hulkko <heikki.hulkko@reaktor.com>
2019-03-27 15:17:55 +0200

    Cleanup

--- src/main/scala/fi/oph/koski/opiskeluoikeus/OpiskeluoikeusChangeMigrator.scala
+++ src/main/scala/fi/oph/koski/opiskeluoikeus/OpiskeluoikeusChangeMigrator.scala
@@ -4,12 +4,16 @@ import fi.oph.koski.schema._
 
 object OpiskeluoikeusChangeMigrator {
   def kopioiValmiitSuorituksetUuteen(vanhaOpiskeluoikeus: KoskeenTallennettavaOpiskeluoikeus, uusiOpiskeluoikeus: KoskeenTallennettavaOpiskeluoikeus): KoskeenTallennettavaOpiskeluoikeus = {
-    val puuttuvatSuorituksetUudessa = vanhaOpiskeluoikeus.suoritukset
-      .filter(kopioitavaPäätasonSuoritus)
-      .filter { vanhaSuoritus =>
-        vanhaSuoritus.valmis && !uusiOpiskeluoikeus.suoritukset.exists(_.koulutusmoduuli.tunniste == vanhaSuoritus.koulutusmoduuli.tunniste)
-      }
-    uusiOpiskeluoikeus.withSuoritukset(puuttuvatSuorituksetUudessa ++ uusiOpiskeluoikeus.suoritukset)
+    if (OpiskeluoikeudenTyyppi.ammatillinenkoulutus == uusiOpiskeluoikeus.tyyppi) {
+      uusiOpiskeluoikeus
+    } else {
+      val puuttuvatSuorituksetUudessa = vanhaOpiskeluoikeus.suoritukset
+        .filter(kopioitavaPäätasonSuoritus)
+        .filter { vanhaSuoritus =>
+          vanhaSuoritus.valmis && !uusiOpiskeluoikeus.suoritukset.exists(_.koulutusmoduuli.tunniste == vanhaSuoritus.koulutusmoduuli.tunniste)
+        }
+      uusiOpiskeluoikeus.withSuoritukset(puuttuvatSuorituksetUudessa ++ uusiOpiskeluoikeus.suoritukset)
+    }
   }
 
   private def kopioitavaPäätasonSuoritus(suoritus: KoskeenTallennettavaPäätasonSuoritus) = suoritus match {
