d53e50b3bb6b8d83a58babdb3024a7edc298c994
Henry Heikkinen <henry.heikkinen@reaktor.com>
2019-04-01 10:10:57 +0300

    TOR-649: Vastaa HTTP 401 jos luovutuspalvelukäyttäjän tunnukset ovat väärät
    HTTP 403 vastataan edelleen, jos opinto-oikeuksien luovutukseen ei ole
    myönnetty lupaa.

--- src/main/scala/fi/oph/koski/koskiuser/RequiresLuovutuspalvelu.scala
+++ src/main/scala/fi/oph/koski/koskiuser/RequiresLuovutuspalvelu.scala
@@ -1,13 +1,18 @@
 package fi.oph.koski.koskiuser
 
-import fi.oph.koski.http.KoskiErrorCategory
+import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
 
 trait RequiresLuovutuspalvelu extends AuthenticationSupport {
   implicit def koskiSession: KoskiSession = koskiSessionOption.get
 
   before() {
-    if (!koskiSessionOption.exists(_.hasLuovutuspalveluAccess)) {
-      haltWithStatus(KoskiErrorCategory.forbidden.vainViranomainen())
+    getUser match {
+      case Left(status) if status.statusCode == 401 =>
+        haltWithStatus(status)
+      case _ =>
+        if (!koskiSessionOption.exists(_.hasLuovutuspalveluAccess)) {
+          haltWithStatus(KoskiErrorCategory.forbidden.vainViranomainen())
+        }
     }
   }
 }
