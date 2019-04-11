2a78af73ace0529fd6068bc37adb8b7a6e7d83e1
suarela <aleksi.saarela@reaktor.fi>
2019-04-11 14:09:48 +0300

    rm trailing comma

--- src/main/scala/fi/oph/koski/raportointikanta/HenkilöLoader.scala
+++ src/main/scala/fi/oph/koski/raportointikanta/HenkilöLoader.scala
@@ -51,6 +51,6 @@ object HenkilöLoader extends Logging {
       etunimet = oppija.etunimet,
       aidinkieli = oppija.äidinkieli,
       kansalaisuus = oppija.kansalaisuus.filter(_.nonEmpty).map(_.sorted.mkString(",")),
-      turvakielto = oppija.turvakielto,
+      turvakielto = oppija.turvakielto
     )
 }
