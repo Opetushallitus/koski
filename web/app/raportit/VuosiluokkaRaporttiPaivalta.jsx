0b5f4ab74ef95c837345168ccde9f9d876ad49e0
suarela <aleksi.saarela@reaktor.fi>
2019-05-08 14:46:49 +0300

    Näytä info-teksti ja ajax-spinneri kun raportin latausta odotetaan

--- web/app/raportit/VuosiluokkaRaporttiPaivalta.jsx
+++ web/app/raportit/VuosiluokkaRaporttiPaivalta.jsx
@@ -8,6 +8,7 @@ import {formatISODate} from '../date/date'
 import {generateRandomPassword} from '../util/password'
 import {downloadExcel} from './downloadExcel'
 import Dropdown from '../components/Dropdown'
+import RaporttiDownloadButton from './RaporttiDownloadButton'
 
 export const VuosiluokkaRaporttiPaivalta = ({oppilaitosAtom, apiEndpoint, title, description, example}) => {
   const paivaAtom = Atom()
@@ -27,25 +28,22 @@ export const VuosiluokkaRaporttiPaivalta = ({oppilaitosAtom, apiEndpoint, title,
 
   const inProgressP = submitBus.awaiting(downloadExcelE.mapError())
   const submitEnabledP = downloadExcelP.map(x => !!x).and(inProgressP.not())
-  const buttonTextP = inProgressP.map((inProgress) => <Text name={!inProgress ? 'Lataa Excel-tiedosto' : 'Ladataan...'}/>)
   const vuosiluokat = [1, 2, 3, 4, 5, 6, 7, 8, 10]
 
   return (<section>
     <h2>{title}</h2>
     <p>{description}</p>
     <p>{example}</p>
-    <div>
       <div className='parametri'>
         <label><Text name='Päivä'/></label>
         <DateInput value={paivaAtom.get()} valueCallback={(value) => paivaAtom.set(value)} validityCallback={(valid) => !valid && paivaAtom.set(undefined)} />
       </div>
-    </div>
     <div className='dropdown-selection parametri'>
       <label><Text name='Vuosiluokka'/></label>
       <VuosiluokkaDropdown value={vuosiluokkaAtom} vuosiluokat={vuosiluokat}/>
     </div>
     <div className='password'><Text name='Excel-tiedosto on suojattu salasanalla'/> {password}</div>
-    <button className='koski-button' disabled={submitEnabledP.not()} onClick={e => { e.preventDefault(); submitBus.push(); return false }}>{buttonTextP}</button>
+    <RaporttiDownloadButton inProgressP={inProgressP} disabled={submitEnabledP.not()} onSubmit={e => { e.preventDefault(); submitBus.push(); return false }} />
   </section>)
 }
 
