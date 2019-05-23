a263a2f903436a4d1b259191488359344dfb4fb8
Heikki Hulkko <heikki.hulkko@reaktor.com>
2019-05-23 12:56:52 +0300

    after-kutsu oikeaan paikkaan

--- web/test/spec/lukioSpec.js
+++ web/test/spec/lukioSpec.js
@@ -531,37 +531,31 @@ describe('Lukiokoulutus', function( ){
   })
 
   describe('Lukion useamman oppiaineen aineopiskelija', function() {
-    before(
-      page.openPage,
-      page.oppijaHaku.searchAndSelect('110122-5090')
-    )
-
     describe('Opiskeluoikeuden tilaa', function () {
       before(
+        page.openPage,
+        page.oppijaHaku.searchAndSelect('110122-5090'),
         editor.edit,
         opinnot.valitseSuoritus(undefined, 'Kemia'),
-        opinnot.opiskeluoikeusEditor().edit,
         opinnot.avaaLisaysDialogi
       )
+
       it('ei voida merkitä valmiiksi', function () {
         expect(OpiskeluoikeusDialog().radioEnabled('valmistunut')).to.equal(false)
-        after(editor.cancelChanges)
       })
-    })
 
-    describe('Kun yksikin suoritus merkitään valmiiksi', function () {
-      before(
-        editor.edit,
-        opinnot.tilaJaVahvistus.merkitseValmiiksi,
-        opinnot.tilaJaVahvistus.lisääVahvistus('01.01.2000'),
-        opinnot.opiskeluoikeusEditor().edit,
-        opinnot.avaaLisaysDialogi
-      )
+      describe('Kun yksikin suoritus merkitään valmiiksi', function () {
+        before(
+          opinnot.tilaJaVahvistus.merkitseValmiiksi,
+          opinnot.tilaJaVahvistus.lisääVahvistus('01.01.2000'),
+          opinnot.avaaLisaysDialogi
+        )
 
-      it('myös opiskeluoikeuden tila voidaan merkitä valmiiksi', function () {
-        expect(OpiskeluoikeusDialog().radioEnabled('valmistunut')).to.equal(true)
-        after(editor.cancelChanges)
+        it('myös opiskeluoikeuden tila voidaan merkitä valmiiksi', function () {
+          expect(OpiskeluoikeusDialog().radioEnabled('valmistunut')).to.equal(true)
+        })
       })
+      after(editor.cancelChanges)
     })
 
     describe('Jos opiskelijalla on "ei tiedossa"-oppiaineita', function () {
@@ -575,8 +569,8 @@ describe('Lukiokoulutus', function( ){
 
       it('Opiskeluoikeuden tilaa ei voi merkitä valmiiksi', function () {
         expect(OpiskeluoikeusDialog().radioEnabled('valmistunut')).to.equal(false)
-        after(editor.cancelChanges)
       })
+      after(editor.cancelChanges)
     })
   })
 
