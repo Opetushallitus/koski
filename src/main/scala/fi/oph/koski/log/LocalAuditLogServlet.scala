f9d0fcd8b1dd82b7c702006493ac3c1f644e6b2f
suarela <aleksi.saarela@reaktor.fi>
2019-07-09 14:54:02 +0300

    Lokaalisti kutsuttava api, jolla voi luoda auditlogeja listalle oideja. Liittyy tor-618

---
+++ src/main/scala/fi/oph/koski/log/LocalAuditLogServlet.scala
@@ -0,0 +1,69 @@
+package fi.oph.koski.log
+
+import fi.oph.koski.config.KoskiApplication
+import fi.oph.koski.henkilo.HenkilöOid
+import fi.oph.koski.http.{HttpStatus, JsonErrorMessage, KoskiErrorCategory}
+import fi.oph.koski.json.JsonSerializer
+import fi.oph.koski.koskiuser.RequiresVirkailijaOrPalvelukäyttäjä
+import fi.oph.koski.log.KoskiOperation.KoskiOperation
+import fi.oph.koski.log.KoskiMessageField.oppijaHenkiloOid
+import fi.oph.koski.schema.Henkilö.Oid
+import fi.oph.koski.servlet.{ApiServlet, NoCache}
+import org.json4s.JsonAST.JValue
+
+import scala.util._
+
+class LocalAuditLogServlet(implicit val application: KoskiApplication) extends ApiServlet with RequiresVirkailijaOrPalvelukäyttäjä with Logging with NoCache {
+
+  before() {
+    if (request.getRemoteHost != "127.0.0.1") {
+      haltWithStatus(KoskiErrorCategory.forbidden(""))
+    }
+  }
+
+  post("/") {
+    withJsonBody { json =>
+      extractAndValidate(json) match {
+        case Left(status) => haltWithStatus(status)
+        case Right(req) => makeAuditLogs(req)
+      }
+    }()
+  }
+
+  private def extractAndValidate(json: JValue) = {
+    JsonSerializer.validateAndExtract[LocalAuditLogRequest](json)
+      .left.map(errors => KoskiErrorCategory.badRequest.validation.jsonSchema(JsonErrorMessage(errors)))
+      .flatMap(validateRequest)
+  }
+
+  private def validateRequest(logRequest: LocalAuditLogRequest) = {
+    for {
+      oids <- eitherOfList(logRequest.oids.map(HenkilöOid.validateHenkilöOid).toList, Nil)
+      operation <- validateOperation(logRequest.operation)
+    } yield (OiditJaOperaatio(oids, operation))
+  }
+
+  private def eitherOfList(list: List[Either[HttpStatus, Oid]], oids: List[Oid]): Either[HttpStatus, List[Oid]] = {
+    list match {
+      case Nil => Right(oids)
+      case x :: xs => x match {
+        case Left(status) => Left(status)
+        case Right(oid) => eitherOfList(xs, oids :+ oid)
+      }
+    }
+  }
+
+  private def validateOperation(operation: String): Either[HttpStatus, KoskiOperation] = Try(KoskiOperation.withName(operation)) match {
+    case Success(op) => Right(op)
+    case _ => Left(KoskiErrorCategory.badRequest.queryParam(s"Operaatiota $operation ei löydy"))
+  }
+
+  private def makeAuditLogs(o: OiditJaOperaatio) = {
+    o.oids.foreach(oid => AuditLog.log(AuditLogMessage(o.operation, koskiSession, Map(oppijaHenkiloOid -> oid))))
+    HttpStatus.ok
+  }
+}
+
+case class LocalAuditLogRequest(oids: Seq[String], operation: String)
+
+case class OiditJaOperaatio(oids: Seq[Oid], operation: KoskiOperation)
