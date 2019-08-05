6b7850e83679c8b39ef93ac32204ba8582e3f4c8
Heikki Hulkko <heikki.hulkko@reaktor.com>
2019-08-05 17:15:08 +0300

    Korjaa perftest validoimalla telepartikkelin hetut
    Telepartikkeli palauttaa hetuja tulevaisuudesta

--- src/test/scala/fi/oph/koski/perftest/RandomHetu.scala
+++ src/test/scala/fi/oph/koski/perftest/RandomHetu.scala
@@ -1,14 +1,15 @@
 package fi.oph.koski.perftest
 
+import fi.oph.koski.henkilo.Hetu
 import fi.oph.koski.integrationtest.KoskidevHttpSpecification
 
 class RandomHetu extends KoskidevHttpSpecification {
   def nextHetu = hetut.synchronized { hetut.next }
-  private lazy val hetut = {
+  private lazy val hetut: Iterator[String] = {
     Iterator.continually({
       println("Haetaan hetuja...")
-      EasyHttp.getJson[List[String]]("http://www.telepartikkeli.net/tunnusgeneraattori/api/generoi/hetu/1000").iterator
-    }).flatten
+      EasyHttp.getJson[List[String]]("https://www.telepartikkeli.net/tunnusgeneraattori/api/generoi/hetu/1000").iterator
+    }).flatten.filter(Hetu.validate(_, acceptSynthetic = false).isRight)
   }
 }
 
