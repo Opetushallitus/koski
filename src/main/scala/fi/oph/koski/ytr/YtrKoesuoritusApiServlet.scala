652738862b79b3c1776ae0be06be5b3fcaa5db2f
Heikki Hulkko <heikki.hulkko@reaktor.com>
2019-09-30 14:56:47 +0300

    Varoita jos ytr-oppilaalla ei ole koesuorituksia

--- src/main/scala/fi/oph/koski/ytr/YtrKoesuoritusApiServlet.scala
+++ src/main/scala/fi/oph/koski/ytr/YtrKoesuoritusApiServlet.scala
@@ -1,6 +1,9 @@
 package fi.oph.koski.ytr
 
+import java.time.LocalDate
+
 import fi.oph.koski.config.KoskiApplication
+import fi.oph.koski.henkilo.LaajatOppijaHenkilöTiedot
 import fi.oph.koski.koskiuser.RequiresKansalainen
 import fi.oph.koski.servlet.{ApiServlet, NoCache}
 
@@ -9,11 +12,20 @@ class YtrKoesuoritusApiServlet(implicit val application: KoskiApplication) exten
     exams
   }
 
-  private def exams: List[ExamResponse] =
-    application.henkilöRepository.findByOid(koskiSession.oid)
-      .flatMap(application.ytrRepository.findByTunnisteet).toList
-      .flatMap(_.exams)
-      .map(toExamResponse)
+  private def exams: List[ExamResponse] = {
+    val henkilö: Option[LaajatOppijaHenkilöTiedot] = application.henkilöRepository.findByOid(koskiSession.oid)
+    val ytrHenkilö: List[YtrOppija] = henkilö.flatMap(application.ytrRepository.findByTunnisteet).toList
+    val examResponse: List[ExamResponse] = ytrHenkilö.flatMap(_.exams).map(toExamResponse)
+    if (shouldHaveExams(ytrHenkilö) && examResponse.forall(_.copyOfExamPaper.isEmpty)) {
+      logger.warn(s"Oppija ${koskiSession.oid} with graduadion date ${ytrHenkilö.flatMap(_.graduationDate).mkString} has an empty exam list")
+    }
+    examResponse
+  }
+
+  private def shouldHaveExams(ytrHenkilö: List[YtrOppija]): Boolean =
+    ytrHenkilö.exists(_.graduationDate.exists(_.isAfter(shouldHaveExamsDate)))
+
+  private lazy val shouldHaveExamsDate = LocalDate.of(2019, 9, 30)
 
   private def toExamResponse(exam: YtrExam) = ExamResponse(period = exam.period, examId = exam.examId, copyOfExamPaper = exam.copyOfExamPaper)
 }
