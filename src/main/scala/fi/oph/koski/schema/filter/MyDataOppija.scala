fe1b1dc2f5a132b8b14174925aa20dc6257a43bd
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2019-10-24 15:44:07 +0300

    TOR-798: Palautetaan TäydellisetHenkilötiedot MyData-vastauksessa

--- src/main/scala/fi/oph/koski/schema/filter/MyDataOppija.scala
+++ src/main/scala/fi/oph/koski/schema/filter/MyDataOppija.scala
@@ -2,12 +2,12 @@ package fi.oph.koski.schema.filter
 
 import java.time.LocalDate
 
-import fi.oph.koski.schema.{Henkilö, Opiskeluoikeus}
+import fi.oph.koski.schema.{Opiskeluoikeus, TäydellisetHenkilötiedot}
 import fi.oph.scalaschema.annotation._
 
 @Description("Oppija Koski-järjestelmässä. Sisältää henkilötiedot ja listan opiskeluoikeuksista, jotka puolestaan sisältävät suorituksia, läsnäolotietoja jne. Henkilötietoja ei tallenneta Koskeen, vaan haetaan/tallennetaan Opintopolun henkilöpalveluun")
 case class MyDataOppija(
-  henkilö: Henkilö,
+  henkilö: TäydellisetHenkilötiedot,
   @Description("Lista henkilön opiskeluoikeuksista. Sisältää vain ne opiskeluoikeudet, joihin käyttäjällä on oikeudet. Esimerkiksi ammatilliselle toimijalle ei välttämättä näy henkilön lukio-opintojen tietoja")
   opiskeluoikeudet: Seq[Opiskeluoikeus],
   suostumuksenPaattymispaiva: Option[LocalDate]
