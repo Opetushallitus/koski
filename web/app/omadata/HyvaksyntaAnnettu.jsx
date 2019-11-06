aaedd187567223c9d9490173d0694aca2178aaa1
suarela <aleksi.saarela@reaktor.fi>
2019-11-06 10:14:55 +0200

    omadata luvan annettua palaa nopeammin palveluntarjoajan sivulle

--- web/app/omadata/HyvaksyntaAnnettu.jsx
+++ web/app/omadata/HyvaksyntaAnnettu.jsx
@@ -7,7 +7,7 @@ export default class HyvaksyntaAnnettu extends React.Component {
     super(props)
     this.state = {
       logoutURL: this.props.logoutURL,
-      timeout: this.props.timeout || 6000
+      timeout: 4000
     }
 
     this.moveToCallbackURL = this.moveToCallbackURL.bind(this)
