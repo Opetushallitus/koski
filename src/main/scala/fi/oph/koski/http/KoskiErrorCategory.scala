e810af8aebaa4e39e38e3713f508d27564d64d76
Heikki Hulkko <heikki.hulkko@reaktor.com>
2020-02-07 14:47:21 +0200

    Korjaa validaatioviesti: omaan organisaatioon -> oman organisaation toimipaikkaan

--- src/main/scala/fi/oph/koski/http/KoskiErrorCategory.scala
+++ src/main/scala/fi/oph/koski/http/KoskiErrorCategory.scala
@@ -50,7 +50,7 @@ object KoskiErrorCategory {
         val tuntematon = subcategory("tuntematon", "Tuntematon organisaatio: oid-tunnisteella ei löytynyt organisaatiota.")
         val eiOppilaitos = subcategory("eiOppilaitos", "Toimipisteenä käytetylle organisaatiolle ei löydy oppilaitos-tyyppistä yliorganisaatiota.")
         val vääräKoulutustoimija = subcategory("vääräKoulutustoimija", "Koulutustoimija ei vastaa organisaatiopalvelun mukaista tietoa")
-        val järjestämismuoto = subcategory("järjestämismuoto", "Opiskeluoikeutta ei voi tallentaa omaan organisaatioon jos järjestämismuoto on syötetty")
+        val järjestämismuoto = subcategory("järjestämismuoto", "Opiskeluoikeutta ei voi tallentaa oman organisaation toimipaikkaan jos järjestämismuotona ostopalvelu tai palveluseteli")
         val koulutustoimijaPakollinen = subcategory("koulutustoimijaPakollinen", "Koulutustoimijaa ei voi yksiselitteisesti päätellä.")
         val oppilaitosPuuttuu = subcategory("oppilaitosPuuttuu", "Oppilaitos puuttuu")
       }
