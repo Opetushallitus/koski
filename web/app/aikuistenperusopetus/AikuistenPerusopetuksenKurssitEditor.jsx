a88063e6e26d110816d2f1da151a27e531dd5674
Jalmari Ikävalko <tzaeru@gmail.com>
2020-02-28 15:21:43 +0200

    Korjattu aikuisten kursseihin myös aikuisten perusopetuksen alkuvaiheen kurssit

--- web/app/aikuistenperusopetus/AikuistenPerusopetuksenKurssitEditor.jsx
+++ web/app/aikuistenperusopetus/AikuistenPerusopetuksenKurssitEditor.jsx
@@ -111,4 +111,4 @@ const customAlternativesFn = (oppiaine, kurssiPrototypes, oppiaineKoodisto) => {
   return Bacon.combineAsArray(kurssiPrototypes.map(fetchAlternatives)).last().map(R.unnest)
 }
 
-export const isAikuistenPerusopetuksenKurssi = kurssi => kurssi.value.classes.includes('aikuistenperusopetuksenkurssinsuoritus')
+export const isAikuistenPerusopetuksenKurssi = kurssi => kurssi.value.classes.includes('aikuistenperusopetuksenkurssinsuoritus') || kurssi.value.classes.includes('aikuistenperusopetuksenalkuvaiheenkurssinsuoritus')
