3456d0f4dbe5843c8cb712959c3d92cb3aee7a18
suarela <aleksi.saarela@reaktor.fi>
2020-03-02 16:55:06 +0200

    formatoi case class

--- src/main/scala/fi/oph/koski/koskiuser/AuthenticationUser.scala
+++ src/main/scala/fi/oph/koski/koskiuser/AuthenticationUser.scala
@@ -2,7 +2,14 @@ package fi.oph.koski.koskiuser
 
 import fi.oph.koski.userdirectory.DirectoryUser
 
-case class AuthenticationUser(oid: String, username: String, name: String, serviceTicket: Option[String], kansalainen: Boolean = false, huollettava: Boolean = false) extends UserWithUsername with UserWithOid {
+case class AuthenticationUser(
+  oid: String,
+  username: String,
+  name: String,
+  serviceTicket: Option[String],
+  kansalainen: Boolean = false,
+  huollettava: Boolean = false
+) extends UserWithUsername with UserWithOid {
   def isSuoritusjakoKatsominen: Boolean = oid == KoskiSession.SUORITUSJAKO_KATSOMINEN_USER
 }
 
