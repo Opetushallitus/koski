2e51052289d7364d92762988d01d5bae7171f70f
suarela <aleksi.saarela@reaktor.fi>
2020-04-02 09:33:04 +0300

    Tuki opiskeluoikeuden tyyppikohtaisille käyttöoikeuksille. Sallitaan vielä käyttöoikeudet joilla ei ole tyyppejä määritelty

---
+++ src/test/scala/fi/oph/koski/koskiuser/KaikilleOpiskeluoikeudenTyypeilleOnKayttooikeusSpec.scala
@@ -0,0 +1,24 @@
+package fi.oph.koski.koskiuser
+
+import fi.oph.koski.schema.OpiskeluoikeudenTyyppi
+import org.scalatest.{FreeSpec, Matchers}
+
+class KaikilleOpiskeluoikeudenTyypeilleOnKayttooikeusSpec extends FreeSpec with Matchers {
+  "Kaikille opiskeluoikeuden tyypeille on määritelty Koskessa rooli" in {
+    OpiskeluoikeudenTyyppi.kaikkiTyypit.map(_.koodiarvo) should equal(Set(
+      Rooli.AIKUISTENPERUSOPETUS,
+      Rooli.AMMATILLINENKOULUTUS,
+      Rooli.DIATUTKINTO,
+      Rooli.ESIOPETUS,
+      Rooli.IBTUTKINTO,
+      Rooli.INTERNATIONALSCHOOL,
+      Rooli.KORKEAKOULUTUS,
+      Rooli.LUKIOKOULUTUS,
+      Rooli.LUVA,
+      Rooli.PERUSOPETUKSEENVALMISTAVAOPETUS,
+      Rooli.PERUSOPETUKSENLISAOPETUS,
+      Rooli.PERUSOPETUS,
+      Rooli.YLIOPPILASTUTKINTO
+    ).map(_.toLowerCase))
+  }
+}
