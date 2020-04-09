49779e1f024d0bf0fa218386722adeddc1a9b26d
Matias Katajamäki <mkataja@reaktor.fi>
2020-04-09 22:03:39 +0300

    Poista kuollutta koodia

--- src/main/scala/fi/oph/koski/validation/KoskiValidator.scala
+++ src/main/scala/fi/oph/koski/validation/KoskiValidator.scala
@@ -305,8 +305,6 @@ class KoskiValidator(tutkintoRepository: TutkintoRepository, val koodistoPalvelu
 
   private def validatePäivämäärät(opiskeluoikeus: KoskeenTallennettavaOpiskeluoikeus): HttpStatus = {
     val ensimmäisenJaksonPäivä: Option[LocalDate] = opiskeluoikeus.tila.opiskeluoikeusjaksot.headOption.map(_.alku)
-    val päättäväJakso: Option[Opiskeluoikeusjakso] = opiskeluoikeus.tila.opiskeluoikeusjaksot.filter(_.opiskeluoikeusPäättynyt).lastOption
-    val päättävänJaksonPäivä: Option[LocalDate] = päättäväJakso.map(_.alku)
     def formatOptionalDate(date: Option[LocalDate]) = date match {
       case Some(d) => d.toString
       case None => "null"
