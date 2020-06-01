9ee513008304d30f38361e203170dcb75f449374
Matias Katajamäki <mkataja@reaktor.fi>
2020-06-01 14:11:01 +0300

    Estä formin lähetys nappia painettaessa
    Formin lähetys aiheuttaa ainakin Firefoxissa fetch-pyynnön keskeytymisen, ja lähetys hoidetaan oikeasti Baconilla.

--- web/app/uusioppija/UusiOppija.jsx
+++ web/app/uusioppija/UusiOppija.jsx
@@ -37,7 +37,7 @@ export const UusiOppija = ({hetu, oid}) => {
         <UusiHenkilö {...{ hetu, oid, henkilöAtom, henkilöValidAtom }}/>
         <hr/>
         <UusiOpiskeluoikeus opiskeluoikeusAtom={opiskeluoikeusAtom}/>
-        <button className='koski-button' disabled={submitEnabledP.not()} onClick={() => submitBus.push()}>{buttonTextP}</button>
+        <button type='button' className='koski-button' disabled={submitEnabledP.not()} onClick={() => submitBus.push()}>{buttonTextP}</button>
       </form>
     </div>
   )
