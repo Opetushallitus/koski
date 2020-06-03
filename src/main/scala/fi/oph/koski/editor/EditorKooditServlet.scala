08e94517ab5b3688014a8a013e9484b3d7b0986f
Matias Katajamäki <mkataja@reaktor.fi>
2020-06-03 21:48:32 +0300

    Poista käyttämättömät importit

--- src/main/scala/fi/oph/koski/editor/EditorKooditServlet.scala
+++ src/main/scala/fi/oph/koski/editor/EditorKooditServlet.scala
@@ -3,12 +3,10 @@ package fi.oph.koski.editor
 import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.http.KoskiErrorCategory
 import fi.oph.koski.http.KoskiErrorCategory.badRequest.validation.koodisto.tuntematonKoodi
-import fi.oph.koski.json.LegacyJsonSerialization
 import fi.oph.koski.koodisto.KoodistoViite
 import fi.oph.koski.koskiuser.RequiresVirkailijaOrPalvelukäyttäjä
 import fi.oph.koski.schema._
-import fi.oph.koski.servlet.{KoskiSpecificApiServlet, NoCache}
-import org.json4s.jackson.Serialization
+import fi.oph.koski.servlet.NoCache
 
 /**
   *  Endpoints for the Koski UI, related to koodistot/koodit, returns editor models
