90bc86c7db903d2f4b00d1cd5c2b31bf416d4ca7
Heikki Hulkko <heikki.hulkko@reaktor.com>
2020-07-02 20:37:14 +0300

    Käytä oikeaa laajuuden yksikköä otsikossa

--- web/app/lukio/LukionOppiaineetEditor.jsx
+++ web/app/lukio/LukionOppiaineetEditor.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as R from 'ramda'
 import {LukionOppiaineEditor} from './LukionOppiaineEditor'
 import {UusiLukionOppiaineDropdown} from './UusiLukionOppiaineDropdown'
-import {modelErrorMessages, modelItems, modelLookup} from '../editor/EditorModel'
+import {modelData, modelErrorMessages, modelItems, modelLookup} from '../editor/EditorModel'
 import {LukionOppiaineetTableHead} from './fragments/LukionOppiaineetTableHead'
 import {t} from '../i18n/i18n'
 import {flatMapArray} from '../util/util'
@@ -10,11 +10,11 @@ import {hyväksytystiSuoritetutKurssit, laajuudet} from './lukio'
 import {numberToString} from '../util/format.js'
 import {isPaikallinen} from '../suoritus/Koulutusmoduuli'
 import {FootnoteDescriptions} from '../components/footnote'
+import {koodistoValues} from '../uusioppija/koodisto'
 
 export const LukionOppiaineetEditor = ({suorituksetModel, classesForUusiOppiaineenSuoritus, suoritusFilter, additionalEditableKoulutusmoduuliProperties}) => {
   const {edit, suoritus: päätasonSuoritusModel} = suorituksetModel.context
   const oppiaineet = modelItems(suorituksetModel).filter(suoritusFilter || R.identity)
-
   if (!edit && R.isEmpty(oppiaineet)) return null
 
   const oppiaineRows = oppiaineet.map((oppiaine, oppiaineIndex) => (
@@ -34,7 +34,7 @@ export const LukionOppiaineetEditor = ({suorituksetModel, classesForUusiOppiaine
   return (
     <section>
       <table className="suoritukset oppiaineet">
-        {!R.isEmpty(oppiaineet) && <LukionOppiaineetTableHead />}
+        {!R.isEmpty(oppiaineet) && <LukionOppiaineetTableHead laajuusyksikkö={laajuusYksikköP(oppiaineet[0])} />}
         <tbody>
         {oppiaineetWithErrorRows}
         </tbody>
@@ -53,3 +53,8 @@ export const paikallisiaLukionOppiaineitaTaiKursseja = oppiaineet => oppiaineet.
 export const paikallisiaKursseja = oppiaine => modelItems(oppiaine, 'osasuoritukset').some(kurssi => isPaikallinen(modelLookup(kurssi, 'koulutusmoduuli')))
 
 export const arvioidutKurssit = oppiaineet => flatMapArray(oppiaineet, oppiaine => hyväksytystiSuoritetutKurssit(modelItems(oppiaine, 'osasuoritukset')))
+
+export const laajuusYksikköP = suoritus => {
+  const laajuusYksikkö = modelData(suoritus, 'koulutusmoduuli.laajuus.yksikkö.koodiarvo')
+  return koodistoValues('opintojenlaajuusyksikko').map(yksiköt => yksiköt.find(y => y.koodiarvo === laajuusYksikkö)).map(y => y && y.nimi && y.nimi.fi)
+}
