cfa1ceb6a467f978dd9b075ffe892d33185f4ed3
Matias Katajamäki <mkataja@reaktor.fi>
2020-08-12 12:09:15 +0300

    Hae suoritukset dynaamisesti komponentissa

--- web/app/omattiedot/suoritusjako/SuoritusjakoList.jsx
+++ web/app/omattiedot/suoritusjako/SuoritusjakoList.jsx
@@ -1,31 +1,43 @@
 import React, {fromBacon} from 'baret'
 import Bacon from 'baconjs'
+import Atom from 'bacon.atom'
 import Text from '../../i18n/Text'
 import Http from '../../util/http'
 import {SuoritusjakoLink} from './SuoritusjakoLink'
 
-const suoritusjaotP = () => {
-  const url = '/koski/api/suoritusjakoV2/available'
-  return Http.cachedGet(
-    url,
-    {
-      errorMapper: (e) => e.httpStatus === 404 ? null : new Bacon.Error(e)
-    }
-  ).toProperty()
-}
+const url = '/koski/api/suoritusjakoV2/available'
 
-const SuoritusjakoList = () => {
-  return fromBacon(suoritusjaotP().map(suoritusjaot => (
-    <>
-      <p className='textstyle-like-h2'>{suoritusjaot.length} <Text name='voimassaolevaa linkkiä'/></p>
-      <p><Text name=''/></p>
-      <div>
-        {suoritusjaot.length > 0
-          ? suoritusjaot.map((suoritusjako, i) => <SuoritusjakoLink key={i} suoritusjako={suoritusjako}/>)
-          : <Text name='Ei jakolinkkejä'/>}
-      </div>
-    </>
-  )))
+class SuoritusjakoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.suoritusjaot = Atom([])
+  }
+
+  componentDidMount() {
+    Http.get(
+      url,
+      {
+        errorMapper: (e) => e.httpStatus === 404 ? null : new Bacon.Error(e)
+      }
+    ).onValue(suoritusjaot => this.suoritusjaot.set(suoritusjaot))
+  }
+
+  render() {
+    return fromBacon(this.suoritusjaot.map(suoritusjaot => (
+      <>
+        <p className='textstyle-like-h2'>{suoritusjaot.length} <Text
+          name='voimassaolevaa linkkiä'/></p>
+        <p><Text name=''/></p>
+        <div>
+          {suoritusjaot.length > 0
+            ? suoritusjaot.map((suoritusjako, i) =>
+              <SuoritusjakoLink baret-lift key={i} suoritusjako={suoritusjako}/>
+            )
+            : <Text name='Ei jakolinkkejä'/>}
+        </div>
+      </>
+    )))
+  }
 }
 
 export default SuoritusjakoList
