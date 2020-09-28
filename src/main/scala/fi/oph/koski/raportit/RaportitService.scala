35cb04b6a0516d039fbb827a203845179ca99251
Matias Katajamäki <mkataja@reaktor.fi>
2020-09-28 12:48:34 +0300

    Korjaa viittaus väärään raporttiin

--- src/main/scala/fi/oph/koski/raportit/RaportitService.scala
+++ src/main/scala/fi/oph/koski/raportit/RaportitService.scala
@@ -3,8 +3,7 @@ package fi.oph.koski.raportit
 import java.sql.Date
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.koskiuser.{AccessType, KoskiSession}
-import fi.oph.koski.schema.Organisaatio.Oid
+import fi.oph.koski.koskiuser.KoskiSession
 
 class RaportitService(application: KoskiApplication) {
   private val raportointiDatabase = application.raportointiDatabase
@@ -17,6 +16,7 @@ class RaportitService(application: KoskiApplication) {
   private val muuammatillinenRaportti = MuuAmmatillinenRaporttiBuilder(raportointiDatabase.db)
   private val topksAmmatillinenRaportti = TOPKSAmmatillinenRaporttiBuilder(raportointiDatabase.db)
   private val esiopetuksenOppijamäärätRaportti = EsiopetuksenOppijamäärätRaportti(raportointiDatabase.db, application.organisaatioService)
+  private val aikuistenPerusopetuksenOppijamäärätRaportti = AikuistenPerusopetuksenOppijamäärätRaportti(raportointiDatabase.db, application.organisaatioService)
 
   def opiskelijaVuositiedot(request: AikajaksoRaporttiRequest): OppilaitosRaporttiResponse = {
     aikajaksoRaportti(request, AmmatillinenOpiskalijavuositiedotRaportti)
@@ -121,7 +121,7 @@ class RaportitService(application: KoskiApplication) {
         application.organisaatioService.organisaationAlaisetOrganisaatiot(oid)
     }
     OppilaitosRaporttiResponse(
-      sheets = Seq(esiopetuksenOppijamäärätRaportti.build(oppilaitosOids, Date.valueOf(request.paiva))),
+      sheets = Seq(aikuistenPerusopetuksenOppijamäärätRaportti.build(oppilaitosOids, Date.valueOf(request.paiva))),
       workbookSettings = WorkbookSettings("Aikuisten perusopetuksen oppijamäärien raportti", Some(request.password)),
       filename = s"aikuisten_perusopetuksen_oppijamäärät_raportti-${request.paiva}.xlsx",
       downloadToken = request.downloadToken
