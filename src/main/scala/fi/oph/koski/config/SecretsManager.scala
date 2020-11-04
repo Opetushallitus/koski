40ff1be78e5c44fd3d126873dcc5e0f04f6431f5
Juho Majasaari <juho.majasaari@reaktor.com>
2020-11-04 15:08:20 +0200

    Lisää tuki db-asetuksten hakemiseen secretmanagerista

--- src/main/scala/fi/oph/koski/config/SecretsManager.scala
+++ src/main/scala/fi/oph/koski/config/SecretsManager.scala
@@ -4,10 +4,13 @@ import fi.oph.koski.json.JsonSerializer
 import org.json4s.jackson.JsonMethods.parse
 import scala.reflect.runtime.universe._
 import com.amazonaws.secretsmanager.caching.SecretCache
+import fi.oph.koski.log.Logging
 
 
-object SecretsManager extends SecretCache {
+
+class SecretsManager extends SecretCache with Logging {
   def getStructuredSecret[T: TypeTag](secretId: String): T = {
+    logger.info(s"Searching for secret $secretId")
     JsonSerializer.extract[T](parse(getSecretString(secretId)), ignoreExtras = true)
   }
 }
--- src/main/scala/fi/oph/koski/db/KoskiDatabase.scala
+++ src/main/scala/fi/oph/koski/db/KoskiDatabase.scala
@@ -2,7 +2,7 @@ package fi.oph.koski.db
 
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.config.ConfigValueFactory._
-import fi.oph.koski.config.Environment
+import fi.oph.koski.config.{Environment, SecretsManager}
 import fi.oph.koski.db.KoskiDatabase._
 import fi.oph.koski.executors.Pools
 import fi.oph.koski.log.Logging
@@ -15,6 +15,8 @@ import slick.jdbc.PostgresProfile.api._
 
 import scala.sys.process._
 
+case class DatabaseConfig(password: String, dbname: String, port: Int, host: String, username: String)
+
 object KoskiDatabase {
   type DB = PostgresProfile.backend.DatabaseDef
 
@@ -26,6 +28,7 @@ object KoskiDatabase {
 }
 
 case class KoskiDatabaseConfig(c: Config, readOnly: Boolean = false, raportointiSchema: Option[Schema] = None) {
+  private val useSecretsManager = Environment.usesAwsSecretsManager
   private val baseConfig = c.getConfig("db").withoutPath("replica").withoutPath("raportointi")
   private val configFromFile = (raportointiSchema, readOnly) match {
     case (Some(schema), _) =>
@@ -35,28 +38,54 @@ case class KoskiDatabaseConfig(c: Config, readOnly: Boolean = false, raportointi
     case (_, false) => baseConfig
     case (_, true) => c.getConfig("db.replica").withFallback(baseConfig)
   }
+
+  private lazy val envVarForSecretId = raportointiSchema match {
+    case Some(_) => "DB_RAPORTOINTI_SECRET_ID"
+    case _ => "DB_KOSKI_SECRET_ID"
+  }
+  private lazy val cachedSecretsClient = new SecretsManager
+  private lazy val secretId = cachedSecretsClient.getSecretId("Koski DB secrets", envVarForSecretId)
+
+  val DatabaseConfig(password, dbname, port, host: String, username) = {
+    if (useSecretsManager) {
+      val dbSecretFromSecretsManager = cachedSecretsClient.getStructuredSecret[DatabaseConfig](secretId)
+      val replicaHost = sys.env.get("DB_KOSKI_REPLICA_HOST").getOrElse(throw new RuntimeException("Secrets manager enabled for DB secrets but environment variable DB_KOSKI_REPLICA_HOST not set"))
+
+      if (readOnly) {
+        dbSecretFromSecretsManager.copy(host = replicaHost)
+      } else {
+        dbSecretFromSecretsManager
+      }
+    } else {
+      DatabaseConfig(
+        configFromFile.getString("password"),
+        configFromFile.getString("name"),
+        configFromFile.getInt("port"),
+        configFromFile.getString("host"),
+        configFromFile.getString("user")
+      )
+    }
+  }
+
   private val otherConfig = ConfigFactory.empty
-    .withValue("url", fromAnyRef("jdbc:postgresql://" + configFromFile.getString("host") + ":" + configFromFile.getInt("port") + "/" + configFromFile.getString("name")))
+    .withValue("url", fromAnyRef(s"jdbc:postgresql://$host:$port/$dbname"))
     .withValue("readOnly", fromAnyRef(readOnly))
     .withValue("numThreads", fromAnyRef(Pools.dbThreads))
 
   private val config = configFromFile.withFallback(otherConfig)
 
-  def dbName = config.getString("name")
-  def user = config.getString("user")
-  def password = config.getString("password")
-  def host = config.getString("host")
-  def port = config.getInt("port")
-  def url = config.getString("url")
-
-  def isLocal = host == "localhost"
-  def isRemote = !isLocal
-  def toSlickDatabase = Database.forConfig("", config)
+  def isLocal: Boolean = config.getString("host") == "localhost" && !useSecretsManager
+  def isRemote: Boolean = !isLocal
+  def toSlickDatabase = if (useSecretsManager) {
+    Database.forDriver(driver = new com.amazonaws.secretsmanager.sql.AWSSecretsManagerPostgreSQLDriver, url = s"jdbc-secretsmanager:postgresql://$host:$port/$dbname", user = secretId)
+  } else {
+    Database.forConfig("", config)
+  }
 }
 
 
 class KoskiDatabase(val config: KoskiDatabaseConfig) extends Logging {
-  val serverProcess = startLocalDatabaseServerIfNotRunning
+  val serverProcess: Option[PostgresRunner] = startLocalDatabaseServerIfNotRunning
 
   if (!config.isRemote && !config.readOnly) {
     createDatabase
@@ -78,13 +107,13 @@ class KoskiDatabase(val config: KoskiDatabaseConfig) extends Logging {
   }
 
   private def createDatabase = {
-    val dbName = config.dbName
+    val dbName = config.dbname
     val port = config.port
     s"createdb -p $port -T template0 -E UTF-8 $dbName".!
   }
 
   private def createUser = {
-    val user = config.user
+    val user = config.username
     val port = config.port
     s"createuser -p $port -s $user -w".!
   }
@@ -92,11 +121,11 @@ class KoskiDatabase(val config: KoskiDatabaseConfig) extends Logging {
   private def migrateSchema = {
     try {
       val flyway = new Flyway
-      flyway.setDataSource(config.url, config.user, config.password)
-      flyway.setSchemas(config.user)
+      flyway.setDataSource(s"jdbc:postgresql://${config.host}:${config.port}/${config.dbname}", config.username, config.password)
+      flyway.setSchemas(config.username)
       flyway.setValidateOnMigrate(false)
       if (System.getProperty("koski.db.clean", "false").equals("true")) {
-        flyway.clean
+        flyway.clean()
       }
       if (Environment.isLocalDevelopmentEnvironment && databaseIsLarge) {
         logger.warn("Skipping database migration for database larger than 100 rows, when running in local development environment")
