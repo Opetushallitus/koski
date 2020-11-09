51c306973245b6019f0dafdbcdf38972ab755b90
Jalmari Ikävalko <tzaeru@gmail.com>
2020-11-09 12:58:55 +0200

    uusia kenttiä (yhteismäärät)

--- src/main/scala/fi/oph/koski/raportit/AikuistenPerusopetuksenKurssikertymäRaportti.scala
+++ src/main/scala/fi/oph/koski/raportit/AikuistenPerusopetuksenKurssikertymäRaportti.scala
@@ -15,14 +15,18 @@ import scala.concurrent.duration._
 case class AikuistenPerusopetuksenKurssikertymäRaportti(db: DB, organisaatioService: OrganisaatioService) extends KoskiDatabaseMethods {
   implicit private val getResult: GetResult[AikuistenPerusopetuksenKurssikertymäRaporttiRow] = GetResult(r =>
     AikuistenPerusopetuksenKurssikertymäRaporttiRow(
+      oppilaitosOid = r.rs.getString("oppilaitos_oid"),
       oppilaitos =  r.rs.getString("oppilaitos_nimi"),
       yhteensäSuorituksia = r.rs.getInt("yhteensäSuorituksia"),
+      yhteensäSuoritettujaSuorituksia = r.rs.getInt("yhteensäSuoritettujaSuorituksia"),
       yhteensäTunnistettujaSuorituksia = r.rs.getInt("yhteensäTunnistettujaSuorituksia"),
       yhteensäTunnistettujaSuorituksiaRahoituksenPiirissä = r.rs.getInt("yhteensäTunnistettujaSuorituksiaRahoituksenPiirissä"),
       päättövaiheenSuorituksia = r.rs.getInt("päättövaiheenSuorituksia"),
+      päättövaiheenSuoritettujaSuorituksia = r.rs.getInt("päättövaiheenSuoritettujaSuorituksia"),
       päättövaiheenTunnistettujaSuorituksia = r.rs.getInt("päättövaiheenTunnistettujaSuorituksia"),
       päättövaiheenTunnistettujaSuorituksiaRahoituksenPiirissä = r.rs.getInt("päättövaiheenTunnistettujaSuorituksiaRahoituksenPiirissä"),
       alkuvaiheenSuorituksia = r.rs.getInt("alkuvaiheenSuorituksia"),
+      alkuvaiheenSuoritettujaSuorituksia = r.rs.getInt("alkuvaiheenSuoritettujaSuorituksia"),
       alkuvaiheenTunnistettujaSuorituksia = r.rs.getInt("alkuvaiheenTunnistettujaSuorituksia"),
       alkuvaiheenTunnistettujaSuorituksiaRahoituksenPiirissä = r.rs.getInt("alkuvaiheenTunnistettujaSuorituksiaRahoituksenPiirissä")
     )
@@ -42,6 +46,7 @@ case class AikuistenPerusopetuksenKurssikertymäRaportti(db: DB, organisaatioSer
     sql"""
       with paatason_suoritus as (
             select
+              r_opiskeluoikeus.oppilaitos_oid,
               r_opiskeluoikeus.oppilaitos_nimi,
               r_paatason_suoritus.paatason_suoritus_id,
               r_opiskeluoikeus.sisaltyy_opiskeluoikeuteen_oid
@@ -58,14 +63,18 @@ case class AikuistenPerusopetuksenKurssikertymäRaportti(db: DB, organisaatioSer
                   and r_opiskeluoikeus_aikajakso.loppu >= $aikaisintaan
               )
       ) select
+          oppilaitos_oid oppilaitos_oid,
           oppilaitos_nimi oppilaitos_nimi,
-          count(*) filter (where tunnustettu = false) yhteensäSuorituksia,
+          count(*) yhteensäSuorituksia,
+          count(*) filter (where tunnustettu = false) yhteensäSuoritettujaSuorituksia,
           count(*) filter (where tunnustettu) yhteensäTunnistettujaSuorituksia,
           count(*) filter (where tunnustettu_rahoituksen_piirissa) yhteensäTunnistettujaSuorituksiaRahoituksenPiirissä,
-          count(*) filter (where tunnustettu = false and suorituksen_tyyppi = 'aikuistenperusopetuksenkurssi') päättövaiheenSuorituksia,
+          count(*) filter (where suorituksen_tyyppi = 'aikuistenperusopetuksenkurssi') päättövaiheenSuorituksia,
+          count(*) filter (where tunnustettu = false and suorituksen_tyyppi = 'aikuistenperusopetuksenkurssi') päättövaiheenSuoritettujaSuorituksia,
           count(*) filter (where tunnustettu and suorituksen_tyyppi = 'aikuistenperusopetuksenkurssi') päättövaiheenTunnistettujaSuorituksia,
           count(*) filter (where tunnustettu_rahoituksen_piirissa and suorituksen_tyyppi = 'aikuistenperusopetuksenkurssi') päättövaiheenTunnistettujaSuorituksiaRahoituksenPiirissä,
-          count(*) filter (where tunnustettu = false and suorituksen_tyyppi = 'aikuistenperusopetuksenalkuvaiheenkurssi') alkuvaiheenSuorituksia,
+          count(*) filter (where suorituksen_tyyppi = 'aikuistenperusopetuksenalkuvaiheenkurssi') alkuvaiheenSuorituksia,
+          count(*) filter (where tunnustettu = false and suorituksen_tyyppi = 'aikuistenperusopetuksenalkuvaiheenkurssi') alkuvaiheenSuoritettujaSuorituksia,
           count(*) filter (where tunnustettu and suorituksen_tyyppi = 'aikuistenperusopetuksenalkuvaiheenkurssi') alkuvaiheenTunnistettujaSuorituksia,
           count(*) filter (where tunnustettu_rahoituksen_piirissa and suorituksen_tyyppi = 'aikuistenperusopetuksenalkuvaiheenkurssi') alkuvaiheenTunnistettujaSuorituksiaRahoituksenPiirissä
         from paatason_suoritus
@@ -73,7 +82,7 @@ case class AikuistenPerusopetuksenKurssikertymäRaportti(db: DB, organisaatioSer
         where (r_osasuoritus.suorituksen_tyyppi = 'aikuistenperusopetuksenkurssi' or r_osasuoritus.suorituksen_tyyppi = 'aikuistenperusopetuksenalkuvaiheenkurssi')
           and r_osasuoritus.arviointi_paiva >= $aikaisintaan
           and r_osasuoritus.arviointi_paiva <= $viimeistaan
-        group by paatason_suoritus.oppilaitos_nimi;
+        group by paatason_suoritus.oppilaitos_nimi, paatason_suoritus.oppilaitos_oid;
   """
   }
 
@@ -86,28 +95,36 @@ case class AikuistenPerusopetuksenKurssikertymäRaportti(db: DB, organisaatioSer
   }
 
   val columnSettings: Seq[(String, Column)] = Seq(
+    "oppilaitosOid" -> Column("Oppilaitoksen oid-tunniste"),
     "oppilaitos" -> Column("Oppilaitos"),
-    "yhteensäSuorituksia" -> Column("yhteensäSuorituksia"),
-    "yhteensäTunnistettujaSuorituksia" -> Column("yhteensäTunnistettujaSuorituksia"),
-    "yhteensäTunnistettujaSuorituksiaRahoituksenPiirissä" -> Column("yhteensäTunnistettujaSuorituksiaRahoituksenPiirissä", comment = Some("\"Läsnä\"-tilaiset aikuisten perusopetuksen opiskeluoikeudet raportin tulostusparametreissa määriteltynä päivänä.")),
-    "päättövaiheenSuorituksia" -> Column("päättövaiheenSuorituksia", comment = Some("\"Läsnä\"-tilaiset aikuisten perusopetuksen opiskeluoikeudet raportin tulostusparametreissa määriteltynä päivänä.")),
-    "päättövaiheenTunnistettujaSuorituksia" -> Column("päättövaiheenTunnistettujaSuorituksia", comment = Some("\"Läsnä\"-tilaiset aikuisten perusopetuksen opiskeluoikeudet raportin tulostusparametreissa määriteltynä päivänä.")),
-    "päättövaiheenTunnistettujaSuorituksiaRahoituksenPiirissä" -> Column("päättövaiheenTunnistettujaSuorituksiaRahoituksenPiirissä", comment = Some("\"Läsnä\"-tilaiset aikuisten perusopetuksen opiskeluoikeudet raportin tulostusparametreissa määriteltynä päivänä.")),
-    "alkuvaiheenSuorituksia" -> Column("alkuvaiheenSuorituksia", comment = Some("\"Läsnä\"-tilaiset aikuisten perusopetuksen opiskeluoikeudet raportin tulostusparametreissa määriteltynä päivänä.")),
-    "alkuvaiheenTunnistettujaSuorituksia" -> Column("alkuvaiheenTunnistettujaSuorituksia", comment = Some("\"Läsnä\"-tilaiset aikuisten perusopetuksen opiskeluoikeudet raportin tulostusparametreissa määriteltynä päivänä.")),
-    "alkuvaiheenTunnistettujaSuorituksiaRahoituksenPiirissä" -> Column("alkuvaiheenTunnistettujaSuorituksiaRahoituksenPiirissä"),
+    "yhteensäSuorituksia" -> Column("Kurssikertymä yhteensä", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän tai alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään.")),
+    "yhteensäSuoritettujaSuorituksia" -> Column("Suoritetut kurssit”", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän tai alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään ja joita ei ole merkitty tunnustetuiksi.")),
+    "yhteensäTunnistettujaSuorituksia" -> Column("Tunnustetut kurssit", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän tai alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään ja jotka on merkitty tunnustetuiksi.")),
+    "yhteensäTunnistettujaSuorituksiaRahoituksenPiirissä" -> Column("Tunnustetut kurssit - rahoituksen piirissä", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän tai alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään, jotka on merkitty tunnustetuiksi ja jotka on merkitty rahoituksen piirissä oleviksi.")),
+    "päättövaiheenSuorituksia" -> Column("Kurssikertymä yhteensä - päättövaihe", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään.")),
+    "päättövaiheenSuoritettujaSuorituksia" -> Column("Suoritetut kurssit - päättövaihe”", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään ja joita ei ole merkitty tunnustetuiksi.")),
+    "päättövaiheenTunnistettujaSuorituksia" -> Column("Tunnustetut kurssit - päättövaihe", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään ja jotka on merkitty tunnustetuiksi.")),
+    "päättövaiheenTunnistettujaSuorituksiaRahoituksenPiirissä" -> Column("Tunnustetut kurssit - rahoituksen piirissä - päättövaihe", comment = Some("Kaikki sellaiset aikuisten perusopetuksen oppimäärän suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään, jotka on merkitty tunnustetuiksi ja jotka on merkitty rahoituksen piirissä oleviksi.")),
+    "alkuvaiheenSuorituksia" -> Column("Kurssikertymä yhteensä - alkuvaihe", comment = Some("Kaikki sellaiset aikuisten perusopetuksen alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään.")),
+    "alkuvaiheenSuoritettujaSuorituksia" -> Column("Suoritetut kurssit - alkuvaihe", comment = Some("Kaikki sellaiset aikuisten perusopetuksen alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään ja joita ei ole merkitty tunnustetuiksi.")),
+    "alkuvaiheenTunnistettujaSuorituksia" -> Column("Tunnustetut kurssit - alkuvaihe", comment = Some("Kaikki sellaiset aikuisten perusopetuksen alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään ja jotka on merkitty tunnustetuiksi.")),
+    "alkuvaiheenTunnistettujaSuorituksiaRahoituksenPiirissä" -> Column("Tunnustetut kurssit - rahoituksen piirissä - alkuvaihe", comment = Some("Kaikki sellaiset aikuisten perusopetuksen alkuvaiheen suorituksen sisältä löytyvät kurssit, joiden arviointipäivämäärä osuu tulostusparametreissa määritellyn aikajakson sisään, jotka on merkitty tunnustetuiksi ja jotka on merkitty rahoituksen piirissä oleviksi.")),
   )
 }
 
 case class AikuistenPerusopetuksenKurssikertymäRaporttiRow(
+   oppilaitosOid: String,
    oppilaitos: String,
    yhteensäSuorituksia: Int,
+   yhteensäSuoritettujaSuorituksia: Int,
    yhteensäTunnistettujaSuorituksia: Int,
    yhteensäTunnistettujaSuorituksiaRahoituksenPiirissä: Int,
    päättövaiheenSuorituksia: Int,
+   päättövaiheenSuoritettujaSuorituksia: Int,
    päättövaiheenTunnistettujaSuorituksia: Int,
    päättövaiheenTunnistettujaSuorituksiaRahoituksenPiirissä: Int,
    alkuvaiheenSuorituksia: Int,
+   alkuvaiheenSuoritettujaSuorituksia: Int,
    alkuvaiheenTunnistettujaSuorituksia: Int,
    alkuvaiheenTunnistettujaSuorituksiaRahoituksenPiirissä: Int
 )
