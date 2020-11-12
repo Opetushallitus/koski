fcd2ca907a3a681c2e62b55321a6e335bd34ad09
Matias Katajamäki <mkataja@reaktor.fi>
2020-11-13 01:24:16 +0200

    Järjestä henkilöhaun tulokset oikein

--- src/main/scala/fi/oph/koski/localization/Locale.scala
+++ src/main/scala/fi/oph/koski/localization/Locale.scala
@@ -1,5 +1,13 @@
 package fi.oph.koski.localization
 
+import java.text.Collator
+
 object Locale {
   val finnish = new java.util.Locale("fi", "FI")
+
+  val finnishCollator: Collator = Collator.getInstance(finnish)
+
+  def finnishComparator(a: String, b: String): Int = finnishCollator.compare(a, b)
+
+  val finnishAlphabeticalOrdering: Ordering[String] = Ordering.comparatorToOrdering[String](finnishComparator)
 }
