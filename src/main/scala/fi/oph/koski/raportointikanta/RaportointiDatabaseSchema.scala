a7fa9eac20df3b6b301b2c4348ed3b220a6cb36a
suarela <aleksi.saarela@reaktor.fi>
2020-11-24 16:06:25 +0200

    Muuta r_osasuoritus-taulun indeksiä; Postgres käyttää indeksistä ensimmäisenä vasemman puoleisinta columnia, arvionti_paiva ei rajaa merkittävästi osasuoritusten joukkuoa, koska sama indeksi sisältää kaikkien Koskesta löytyvien osasuoritusten arviointi_päivät, esim. tilanne jossa arviointipäivän tulisi osua vuoden mittaiselle aikajaksolle. Käyttämällä päätason_suoritus_id:tä ensimmäisenä columnina indeksissä, tulosjoukko pienenee huomattavasti enemmän.

--- src/main/scala/fi/oph/koski/raportointikanta/RaportointiDatabaseSchema.scala
+++ src/main/scala/fi/oph/koski/raportointikanta/RaportointiDatabaseSchema.scala
@@ -51,7 +51,7 @@ object RaportointiDatabaseSchema {
     sqlu"CREATE INDEX ON #${s.name}.r_osasuoritus(paatason_suoritus_id)",
     sqlu"CREATE INDEX ON #${s.name}.r_osasuoritus(opiskeluoikeus_oid)",
     sqlu"CREATE INDEX ON #${s.name}.r_osasuoritus(ylempi_osasuoritus_id)",
-    sqlu"CREATE INDEX ON #${s.name}.r_osasuoritus(arviointi_paiva, paatason_suoritus_id, suorituksen_tyyppi)",
+    sqlu"CREATE INDEX ON #${s.name}.r_osasuoritus(paatason_suoritus_id, suorituksen_tyyppi, arviointi_paiva)",
 
     sqlu"CREATE INDEX ON #${s.name}.esiopetus_opiskeluoik_aikajakso(opiskeluoikeus_oid)",
   )
