1889478107da84056c2919b966180a9834934652
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2020-12-22 12:49:40 +0200

    Lisää Jestille palikat React-komponenttien testaamiseen

--- valpas-web/src/index.tsx
+++ valpas-web/src/index.tsx
@@ -1,10 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-type GreetingProps = {
-  children: string
-}
-
-const Greeting = (props: GreetingProps) => <h1>Hello, {props.children}!</h1>
+import { Greeting } from "./components/Greeting"
 
 ReactDOM.render(<Greeting>world</Greeting>, document.getElementById("app"))
--- valpas-web/test/example.test.ts
+++
@@ -1,3 +0,0 @@
-test("Jest test example", () => {
-  expect(1 + 2).toBe(3)
-})
