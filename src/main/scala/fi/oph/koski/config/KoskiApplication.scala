f4494bcde2a219e7fb75174e75ee24cfca88e223
Matias Katajamäki <mkataja@reaktor.fi>
2020-12-23 12:46:49 +0200

    Rivit

--- src/main/scala/fi/oph/koski/config/KoskiApplication.scala
+++ src/main/scala/fi/oph/koski/config/KoskiApplication.scala
@@ -110,7 +110,13 @@ class KoskiApplication(val config: Config, implicit val cacheManager: CacheManag
   def init(): Future[Unit] = {
     Future(perustiedotIndexer.init()) // This one will not be awaited for; it's ok that indexing continues while application is running
     AuditLog.startHeartbeat() // No need to await this one either
-    tryCatch("Koodistojen luonti") { if (config.getString("opintopolku.virkailija.url") != "mock") KoodistoCreator(this).createAndUpdateCodesBasedOnMockData }
+
+    tryCatch("Koodistojen luonti") {
+      if (config.getString("opintopolku.virkailija.url") != "mock") {
+        KoodistoCreator(this).createAndUpdateCodesBasedOnMockData
+      }
+    }
+
     val parallels: immutable.Seq[Future[Any]] = List(
       Future { tiedonsiirtoService.init() },
       Future { scheduledTasks.init },
