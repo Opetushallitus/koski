bdb19ccf3371e419dab4871d60bf23d675de8ebf
suarela <aleksi.saarela@reaktor.fi>
2020-12-28 15:28:37 +0200

    Enabloi pois kommentoitu testi

--- src/test/scala/fi/oph/koski/mocha/KoskiJsUnitSpec.scala
+++ src/test/scala/fi/oph/koski/mocha/KoskiJsUnitSpec.scala
@@ -2,8 +2,6 @@ package fi.oph.koski.mocha
 
 class KoskiJsUnitSpec extends KoskiCommandLineSpec {
   "JS unit tests" in {
-    /*
     runTestCommand("js-unit-tests", Seq("make", "js-unit-test"))
-    */
   }
 }
