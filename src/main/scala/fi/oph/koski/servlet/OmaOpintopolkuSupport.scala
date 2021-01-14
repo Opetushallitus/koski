d90407f3ad8692dd958252fc628b81afd64b485b
Jalmari Ikävalko <tzaeru@gmail.com>
2021-01-14 16:53:10 +0200

    tor-750 - shibbolethurl -> loginurl

--- src/main/scala/fi/oph/koski/servlet/OmaOpintopolkuSupport.scala
+++ src/main/scala/fi/oph/koski/servlet/OmaOpintopolkuSupport.scala
@@ -6,8 +6,8 @@ import fi.oph.koski.koskiuser.AuthenticationSupport
 import org.scalatra.servlet.RichRequest
 
 trait OmaOpintopolkuSupport extends AuthenticationSupport with LanguageSupport {
-  def oppijaRaamit: Raamit = if (oppijaRaamitSet || useOppijaRaamitProxy) Oppija(koskiSessionOption, request, shibbolethUrl) else EiRaameja
-  def shibbolethUrl: String = application.config.getString("identification.url." + langFromCookie.getOrElse(langFromDomain))
+  def oppijaRaamit: Raamit = if (oppijaRaamitSet || useOppijaRaamitProxy) Oppija(koskiSessionOption, request, loginUrl) else EiRaameja
+  def loginUrl: String = application.config.getString("identification.url." + langFromCookie.getOrElse(langFromDomain))
   def oppijaRaamitSet: Boolean = isCloudEnvironment
   private val useOppijaRaamitProxy = application.config.hasPath("oppijaRaamitProxy")
   private lazy val isCloudEnvironment = !Environment.isLocalDevelopmentEnvironment
