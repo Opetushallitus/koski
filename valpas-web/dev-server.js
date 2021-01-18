3ae47988e70379b90341e71908020533b6ce90c4
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2021-01-18 08:27:53 +0200

    Lisää virkailija-raamit Valpas-palveluun;
    Parcelissa ei ole sisäänrakennettua proxyä, minkä vuoksi kehityspalvelin on vaihdettu käyttämään
    expressiä ja parcel:ia middleware:na.
    
    Parcel ei tue skriptien skippaamista lähdetiedostoissaan, minkä vuoksi on tehty paikallinen
    parcel plugin, jonka kanssa parcel skippaa virkailija-raamit -skriptin.

---
+++ valpas-web/dev-server.js
@@ -0,0 +1,21 @@
+const Bundler = require('parcel-bundler')
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const PORT = process.env.PORT || 1234
+const VIRKAILIJA_RAAMIT_PROXY = process.env.VIRKAILIJA_RAAMIT_PROXY || undefined
+
+const app = express()
+
+// https://stackoverflow.com/questions/53902896/is-there-a-way-to-proxy-requests-in-parcel-as-in-webpack
+if (VIRKAILIJA_RAAMIT_PROXY) {
+  app.use(createProxyMiddleware('/virkailija-raamit', {
+    target: VIRKAILIJA_RAAMIT_PROXY,
+    changeOrigin: true
+  }))
+}
+
+const bundler = new Bundler('src/index.html', { cache: false })
+app.use(bundler.middleware())
+
+app.listen(Number(PORT))
