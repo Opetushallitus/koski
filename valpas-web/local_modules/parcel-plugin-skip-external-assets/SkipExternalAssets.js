c93f8206338536d557c8e5b77132f50f797a8857
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2021-01-25 10:21:05 +0200

    Linttaa myös Valppaan local_modules ja devserver lähdekoodi

--- valpas-web/local_modules/parcel-plugin-skip-external-assets/SkipExternalAssets.js
+++ valpas-web/local_modules/parcel-plugin-skip-external-assets/SkipExternalAssets.js
@@ -1,20 +1,22 @@
 // Customized HTML asset for parcel that supports ignoring custom external frontend dependencies. Workaround for
 // parcel configurability limitation based on https://github.com/parcel-bundler/parcel/issues/1186#issuecomment-453468414
-const HTMLAsset = require('parcel-bundler/lib/assets/HTMLAsset')
+const HTMLAsset = require("parcel-bundler/lib/assets/HTMLAsset")
 
-function shouldIgnore (file) {
-  return /\/virkailija-raamit\/apply-raamit.js/.test(file) ||
+function shouldIgnore(file) {
+  return (
+    /\/virkailija-raamit\/apply-raamit.js/.test(file) ||
     /\/valpas\/localization\/set-localization-map.js/.test(file)
+  )
 }
 
 class SkipExternalAssets extends HTMLAsset {
-  addDependency (name, opts) {
+  addDependency(name, opts) {
     if (!shouldIgnore(opts.resolved)) {
       return super.addDependency(name, opts)
     }
   }
 
-  processSingleDependency (p, opts) {
+  processSingleDependency(p, opts) {
     if (shouldIgnore(p)) return p
     else return super.processSingleDependency(p, opts)
   }
