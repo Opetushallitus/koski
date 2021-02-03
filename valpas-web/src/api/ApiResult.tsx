89ad556021ed2d68f76cfc3b8cec4e3adc3b0604
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-02-03 10:24:47 +0200

    Apukomponentti API-vaiheiden renderöintiin

---
+++ valpas-web/src/api/ApiResult.tsx
@@ -0,0 +1,64 @@
+import { pipe } from "fp-ts/lib/pipeable"
+import * as O from "fp-ts/Option"
+import React from "react"
+import { ApiFailure } from "./apiFetch"
+import {
+  ApiMethodHook,
+  isError,
+  isInitial,
+  isLoading,
+  isSuccessful,
+} from "./apiHooks"
+
+type ApiMappings<T, S> = {
+  initial?: () => S
+  loading?: () => S
+  successful?: (data: T) => S
+  error?: (error: ApiFailure) => S
+  else?: () => S
+}
+
+export type ApiResultProps<T> = ApiMappings<T, React.ReactNode> & {
+  state: ApiMethodHook<T, any>
+}
+
+export const ApiResult = <T,>({
+  state,
+  ...mappings
+}: ApiResultProps<T>): React.ReactElement => (
+  <>{mapApiResult(state, mappings)}</>
+)
+
+export const mapApiResult = <T, S>(
+  state: ApiMethodHook<T, any>,
+  mappings: ApiMappings<T, S>
+): O.Option<S> => {
+  if (isInitial(state)) {
+    return pipe(
+      O.fromNullable(mappings.initial),
+      O.map((f) => f())
+    )
+  }
+  if (isLoading(state)) {
+    return pipe(
+      O.fromNullable(mappings.loading),
+      O.map((f) => f())
+    )
+  }
+  if (isSuccessful(state)) {
+    return pipe(
+      O.fromNullable(mappings.successful),
+      O.map((f) => f(state.data))
+    )
+  }
+  if (isError(state)) {
+    return pipe(
+      O.fromNullable(mappings.error),
+      O.map((f) => f(state))
+    )
+  }
+  return pipe(
+    O.fromNullable(mappings.else),
+    O.map((f) => f())
+  )
+}
