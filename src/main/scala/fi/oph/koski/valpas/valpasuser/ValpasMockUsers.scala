ce230bd13a54a0c0137258caf45d973a8c0d9112
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2021-02-16 08:23:06 +0200

    Lisää testikäyttäjä, jolla Valpas- ja Koskioikeuksia

--- src/main/scala/fi/oph/koski/valpas/valpasuser/ValpasMockUsers.scala
+++ src/main/scala/fi/oph/koski/valpas/valpasuser/ValpasMockUsers.scala
@@ -1,5 +1,6 @@
 package fi.oph.koski.valpas.valpasuser
 
+import fi.oph.koski.koskiuser.MockKäyttöoikeusryhmät
 import fi.oph.koski.koskiuser.MockUser
 import fi.oph.koski.organisaatio.MockOrganisaatiot._
 import fi.oph.koski.valpas.valpasuser.ValpasMockKäyttöoikeusryhmät._
@@ -10,18 +11,25 @@ object ValpasMockUsers {
   val valpasHelsinki = MockUser(
     "käyttäjä",
     "valpas-helsinki",
-    "1.2.246.562.24.99999999587",
+    "1.2.246.562.24.12312312301",
     Set(kuntakäyttäjä(helsinginKaupunki)))
 
   val valpasJklNormaalikoulu = MockUser(
     "käyttäjä",
     "valpas-jkl-normaali",
-    "1.2.246.562.24.99999999487",
+    "1.2.246.562.24.12312312302",
     Set(oppilaitoskäyttäjä(jyväskylänNormaalikoulu)))
+
+  val valpasJklNormaalikouluJaKoskiHelsinkiTallentaja = MockUser(
+    "käyttäjä",
+    "valpas-jkl-normaali-koski-hki",
+    "1.2.246.562.24.12312312303",
+    Set(oppilaitoskäyttäjä(jyväskylänNormaalikoulu), MockKäyttöoikeusryhmät.oppilaitosTallentaja(helsinginKaupunki))
+  )
 
   def users = {
     mockUsersEnabled match {
-      case true => List(valpasHelsinki, valpasJklNormaalikoulu)
+      case true => List(valpasHelsinki, valpasJklNormaalikoulu, valpasJklNormaalikouluJaKoskiHelsinkiTallentaja)
       case false => List()
     }
   }
