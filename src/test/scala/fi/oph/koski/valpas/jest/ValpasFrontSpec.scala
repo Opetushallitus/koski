5a48d494b421720472c5f71f9f2fcacc9c4ac5d4
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-02-17 13:56:22 +0200

    Alusta raportointikanta ennen testejä

--- src/test/scala/fi/oph/koski/valpas/jest/ValpasFrontSpec.scala
+++ src/test/scala/fi/oph/koski/valpas/jest/ValpasFrontSpec.scala
@@ -1,12 +1,23 @@
 package fi.oph.koski.valpas.jest
 
+import fi.oph.koski.KoskiApplicationForTests
 import fi.oph.koski.api.SharedJetty
 import fi.oph.koski.mocha.KoskiCommandLineSpec
+import fi.oph.koski.valpas.valpasuser.ValpasMockUsers
 import org.scalatest.Tag
 
 class ValpasFrontSpec extends KoskiCommandLineSpec {
+  val fixtureCreator = KoskiApplicationForTests.fixtureCreator
+  val raportointikantaService = KoskiApplicationForTests.raportointikantaService
+
   "Valpas front specs" taggedAs(ValpasFrontTag) in {
     SharedJetty.start
+
+    ValpasMockUsers.mockUsersEnabled = true
+    fixtureCreator.resetFixtures(fixtureCreator.valpasFixtureState)
+    raportointikantaService.dropAndCreateSchema()
+    raportointikantaService.loadRaportointikanta(true)
+    
     runTestCommand("valpas-front", Seq("scripts/valpas-front-test.sh", SharedJetty.hostUrl))
   }
 }
