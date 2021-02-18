3493c3cc5a0c38aa111abb8e47a88962528c0e69
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-02-18 15:59:45 +0200

    Palauta vain oppivelvollisuusikäiset oppijat

--- src/main/scala/fi/oph/koski/valpas/henkilo/ValpasMockOppijat.scala
+++ src/main/scala/fi/oph/koski/valpas/henkilo/ValpasMockOppijat.scala
@@ -7,6 +7,7 @@ object ValpasMockOppijat {
   private val valpasOppijat = new MockOppijat
 
   val oppivelvollinenYsiluokkaKeskenKeväällä2021 = valpasOppijat.oppija("Oppivelvollinen-ysiluokka-kesken-keväällä-2021", "Valpas", "221105A3023")
+  val eiOppivelvollinenSyntynytEnnen2004 = valpasOppijat.oppija("Ei-oppivelvollinen-syntynyt-ennen-2004", "Valpas", "210303A707J")
 
   def defaultOppijat = valpasOppijat.getOppijat
 }
--- src/test/scala/fi/oph/koski/valpas/ValpasRootApiServletSpec.scala
+++ src/test/scala/fi/oph/koski/valpas/ValpasRootApiServletSpec.scala
@@ -1,6 +1,7 @@
 package fi.oph.koski.valpas
 
 import fi.oph.koski.log.AuditLogTester
+import fi.oph.koski.valpas.henkilo.ValpasMockOppijat
 import fi.oph.koski.valpas.valpasuser.ValpasMockUsers
 import org.scalatest.Tag
 
@@ -8,13 +9,20 @@ class ValpasRootApiServletSpec extends ValpasTestMethods {
   override def defaultUser = ValpasMockUsers.valpasJklNormaalikoulu
 
   "Oppijan lataaminen tuottaa auditlogin" taggedAs(ValpasBackendTag) in {
-    val oppijaOid = "1.2.246.562.24.00000000001"
+    val oppijaOid = ValpasMockOppijat.oppivelvollinenYsiluokkaKeskenKeväällä2021.oid
     AuditLogTester.clearMessages
     authGet(s"/valpas/api/oppija/$oppijaOid") {
       verifyResponseStatusOk()
       AuditLogTester.verifyAuditLogMessage(Map(
         "operation" -> "VALPAS_OPPIJA_KATSOMINEN",
         "target" -> Map("OPPIJA_OID" -> oppijaOid)))
+    }
+  }
+
+  "Ei-oppivelvollisen oppijan tietojen lataaminen ei onnistu" taggedAs(ValpasBackendTag) in {
+    val oppijaOid = ValpasMockOppijat.eiOppivelvollinenSyntynytEnnen2004
+    authGet(s"/valpas/api/oppija/$oppijaOid") {
+      verifyResponseStatus(403, ValpasErrorCategory.forbidden.oppija())
     }
   }
 }
