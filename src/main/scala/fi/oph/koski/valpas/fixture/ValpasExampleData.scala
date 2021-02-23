e11ca00ad0ca6ac68cf1c5385dee855ea91b8993
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-02-23 14:40:54 +0200

    Testit näytettävien oppijoiden tietokantahauille

--- src/main/scala/fi/oph/koski/valpas/fixture/ValpasExampleData.scala
+++ src/main/scala/fi/oph/koski/valpas/fixture/ValpasExampleData.scala
@@ -1,10 +1,13 @@
 package fi.oph.koski.valpas.fixture
 
-import java.time.LocalDate.{of => date}
-import fi.oph.koski.documentation.ExampleData.{opiskeluoikeusLäsnä, suomenKieli, vahvistusPaikkakunnalla}
-import fi.oph.koski.documentation.PerusopetusExampleData.{kahdeksannenLuokanSuoritus, omanÄidinkielenOpinnotSaame, perusopetuksenOppimääränSuoritusKesken, perusopetus, suoritustapaKoulutus, yhdeksännenLuokanSuoritus}
+import fi.oph.koski.documentation.ExampleData.{opiskeluoikeusEronnut, opiskeluoikeusLäsnä, vahvistusPaikkakunnalla}
+import fi.oph.koski.documentation.ExamplesLukio
+import fi.oph.koski.documentation.PerusopetusExampleData.{kahdeksannenLuokanSuoritus, perusopetuksenOppimääränSuoritusKesken, yhdeksännenLuokanSuoritus}
 import fi.oph.koski.documentation.YleissivistavakoulutusExampleData.{jyväskylänNormaalikoulu, kulosaarenAlaAste}
-import fi.oph.koski.schema.{NuortenPerusopetuksenOpiskeluoikeudenTila, NuortenPerusopetuksenOpiskeluoikeusjakso, NuortenPerusopetuksenOppimääränSuoritus, PerusopetuksenOpiskeluoikeus}
+import fi.oph.koski.schema.{Aikajakso, NuortenPerusopetuksenOpiskeluoikeudenTila, NuortenPerusopetuksenOpiskeluoikeusjakso, PerusopetuksenOpiskeluoikeudenLisätiedot, PerusopetuksenOpiskeluoikeus}
+
+import java.time.{LocalDate, Month}
+import java.time.LocalDate.{of => date}
 
 object ValpasExampleData {
   def oppivelvollinenYsiluokkaKeskenKeväällä2021Opiskeluoikeus = PerusopetuksenOpiskeluoikeus(
@@ -69,4 +72,48 @@ object ValpasExampleData {
     )
   )
 
+  def lukionOpiskeluoikeus = ExamplesLukio.lukioKesken
+
+  def kasiluokkaKeskenKeväällä2021Opiskeluoikeus = PerusopetuksenOpiskeluoikeus(
+    oppilaitos = Some(jyväskylänNormaalikoulu),
+    koulutustoimija = None,
+    suoritukset = List(
+      perusopetuksenOppimääränSuoritusKesken,
+      kahdeksannenLuokanSuoritus.copy(
+        alkamispäivä = Some(date(2020, 8, 15)),
+        vahvistus = None,
+      )
+    ),
+    tila = NuortenPerusopetuksenOpiskeluoikeudenTila(
+      List(
+        NuortenPerusopetuksenOpiskeluoikeusjakso(date(2012, 8, 15), opiskeluoikeusLäsnä)
+      )
+    )
+  )
+
+  def kotiopetusMeneilläänOpiskeluoikeus = oppivelvollinenYsiluokkaKeskenKeväällä2021Opiskeluoikeus.copy(
+    lisätiedot = Some(PerusopetuksenOpiskeluoikeudenLisätiedot(
+      kotiopetusjaksot = Some(List(
+        Aikajakso(alku = date(2020, 1, 1), loppu = None)
+      ))
+    ))
+  )
+
+  def kotiopetusMenneisyydessäOpiskeluoikeus = oppivelvollinenYsiluokkaKeskenKeväällä2021Opiskeluoikeus.copy(
+    lisätiedot = Some(PerusopetuksenOpiskeluoikeudenLisätiedot(
+      kotiopetusjaksot = Some(List(
+        Aikajakso(alku = date(2020, 1, 1), loppu = Some(date(2020, 2, 1)))
+      ))
+    ))
+  )
+
+  def eronnutOpiskeluoikeus = oppivelvollinenYsiluokkaKeskenKeväällä2021Opiskeluoikeus.copy(
+
+    tila = NuortenPerusopetuksenOpiskeluoikeudenTila(
+      List(
+        NuortenPerusopetuksenOpiskeluoikeusjakso(date(2012, 8, 15), opiskeluoikeusLäsnä),
+        NuortenPerusopetuksenOpiskeluoikeusjakso(date(2021, 1, 1), opiskeluoikeusEronnut)
+      )
+    )
+  )
 }
--- src/test/scala/fi/oph/koski/valpas/ValpasDatabaseServiceSpec.scala
+++ src/test/scala/fi/oph/koski/valpas/ValpasDatabaseServiceSpec.scala
@@ -1,46 +1,62 @@
 package fi.oph.koski.valpas
 
 import fi.oph.koski.KoskiApplicationForTests
-import fi.oph.koski.db.PostgresDriverWithJsonSupport.api.actionBasedSQLInterpolation
 import fi.oph.koski.henkilo.LaajatOppijaHenkilöTiedot
 import fi.oph.koski.organisaatio.MockOrganisaatiot
-import fi.oph.koski.schema.PerusopetuksenOpiskeluoikeus
+import fi.oph.koski.schema.{PerusopetuksenOpiskeluoikeus, PerusopetuksenVuosiluokanSuoritus}
 import fi.oph.koski.valpas.fixture.ValpasExampleData
 import fi.oph.koski.valpas.henkilo.ValpasMockOppijat
-import fi.oph.koski.valpas.repository.{ValpasDatabaseService, ValpasHenkilö, ValpasOpiskeluoikeus, ValpasOppija}
-import org.scalatest.FreeSpec
+import fi.oph.koski.valpas.repository.{ValpasDatabaseService, ValpasOppija}
 import org.scalatest.Matchers._
 
-class ValpasDatabaseServiceSpec extends FreeSpec {
+class ValpasDatabaseServiceSpec extends ValpasTestBase {
   val db = new ValpasDatabaseService(KoskiApplicationForTests)
   val oppilaitokset = List(MockOrganisaatiot.jyväskylänNormaalikoulu)
 
+  // Jyväskylän normaalikoulusta löytyvät näytettävät oppivelvolliset aakkosjärjestyksessä
+  val oppivelvolliset = List(
+    (
+      ValpasMockOppijat.kotiopetusMenneisyydessäOppija,
+      ValpasExampleData.kotiopetusMenneisyydessäOpiskeluoikeus
+    ),
+    (
+      ValpasMockOppijat.oppivelvollinenYsiluokkaKeskenKeväällä2021,
+      ValpasExampleData.oppivelvollinenYsiluokkaKeskenKeväällä2021Opiskeluoikeus
+    ),
+    (
+      ValpasMockOppijat.päällekkäisiäOppivelvollisuuksia,
+      ValpasExampleData.oppivelvollinenVaihtanutKouluaMuttaOpiskeluoikeusMerkkaamattaOikein2
+    )
+  )
+
   "getOppivelvollinenHenkilö palauttaa vain annetun oppijanumeron mukaisen oppijan" in {
-    val oppija = db.getOppivelvollinenHenkilö(ValpasMockOppijat.päällekkäisiäOppivelvollisuuksia.oid, oppilaitokset)
+    val (expectedOppija, expectedOpiskeluoikeus) = oppivelvolliset(1)
+    val oppija = db.getOppivelvollinenHenkilö(expectedOppija.oid, oppilaitokset)
 
     validateOppija(
       oppija.get,
-      ValpasMockOppijat.päällekkäisiäOppivelvollisuuksia,
-      ValpasExampleData.oppivelvollinenYsiluokkaKeskenKeväällä2021Opiskeluoikeus,
-    "9B")
+      expectedOppija,
+      expectedOpiskeluoikeus)
   }
 
   "getOppivelvollinsetHenkilötJaOpiskeluoikeudet palauttaa oikeat tulokset" in {
-    val oppijat = db.getOppivelvollinsetHenkilötJaOpiskeluoikeudet(oppilaitokset)
+    val oppijat = db.getOppivelvollinsetHenkilötJaOpiskeluoikeudet(oppilaitokset).toList
 
-    oppijat.length shouldBe 2
-    validateOppija(
-      oppijat.head,
-      ValpasMockOppijat.oppivelvollinenYsiluokkaKeskenKeväällä2021,
-      ValpasExampleData.oppivelvollinenYsiluokkaKeskenKeväällä2021Opiskeluoikeus,
-    "9C")
+    oppijat.map(_.henkilö.oid) shouldBe oppivelvolliset.map(_._1.oid)
+
+    (oppijat zip oppivelvolliset).foreach { actualAndExpected =>
+      val (oppija, (expectedOppija, expectedOppivelvollisuus)) = actualAndExpected
+      validateOppija(
+        oppija,
+        expectedOppija,
+        expectedOppivelvollisuus)
+    }
   }
 
   def validateOppija(
     oppija: ValpasOppija,
     expectedOppija: LaajatOppijaHenkilöTiedot,
-    expectedOpiskeluoikeus: PerusopetuksenOpiskeluoikeus,
-    expectedRyhmä: String
+    expectedOpiskeluoikeus: PerusopetuksenOpiskeluoikeus
   ) = {
     oppija.henkilö.oid shouldBe expectedOppija.oid
     oppija.henkilö.hetu shouldBe expectedOppija.hetu
@@ -52,6 +68,8 @@ class ValpasDatabaseServiceSpec extends FreeSpec {
     opiskeluoikeus.oppilaitos.oid shouldBe expectedOpiskeluoikeus.oppilaitos.get.oid
     opiskeluoikeus.alkamispäivä shouldBe expectedOpiskeluoikeus.alkamispäivä.map(_.toString)
     opiskeluoikeus.päättymispäivä shouldBe expectedOpiskeluoikeus.päättymispäivä.map(_.toString)
-    opiskeluoikeus.ryhmä shouldBe Some(expectedRyhmä)
+    expectedOpiskeluoikeus.suoritukset.last match {
+      case p: PerusopetuksenVuosiluokanSuoritus => opiskeluoikeus.ryhmä shouldBe Some(p.luokka)
+    }
   }
 }
