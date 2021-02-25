07fa3f899bda6cc8daf1bca1e65b8c277ea9e045
Matias Katajamäki <mkataja@reaktor.fi>
2021-02-25 11:50:41 +0200

    Korjaa laiska lataus oikeaan paikkaan

--- valpas-web/src/views/ValpasApp.tsx
+++ valpas-web/src/views/ValpasApp.tsx
@@ -1,15 +1,28 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route,
+  RouteComponentProps,
+  Switch,
+} from "react-router-dom"
 import { NotFoundView } from "../views/ErrorView"
 import HealthView from "../views/HealthView"
-import VirkailijaApp from "../views/VirkailijaApp"
 
-export default () => {
+export const ValpasApp = () => {
+  const VirkailijaApp = React.lazy(() => import("./VirkailijaApp"))
+
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/health" component={HealthView} />
-        <Route path="/virkailija" component={VirkailijaApp} />
+        <Route
+          path="/virkailija"
+          render={({ match: { path } }: RouteComponentProps) => (
+            <React.Suspense fallback={<></>}>
+              <VirkailijaApp path={path} />
+            </React.Suspense>
+          )}
+        />
         <Route component={NotFoundView} />
       </Switch>
     </Router>
