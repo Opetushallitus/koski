ac3ef1bdc83e76a8f1733d22eda3c82d057e2cdd
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-03-09 12:57:01 +0200

    Piilota oppijanäkymästä hakutaulu, ettei näkymä hajoa

--- valpas-web/src/views/oppija/OppijanHaut.tsx
+++ valpas-web/src/views/oppija/OppijanHaut.tsx
@@ -1,13 +1,7 @@
 import React from "react"
-import { IconSection } from "../../components/containers/IconSection"
-import { HakuIcon } from "../../components/icons/Icon"
-import { Datum } from "../../components/tables/DataTable"
-import { LeanTable } from "../../components/tables/LeanTable"
-import { TertiaryHeading } from "../../components/typography/headings"
 import { NoDataMessage } from "../../components/typography/NoDataMessage"
-import { formatFixedNumber, getLocalized, T, t } from "../../i18n/i18n"
-import { ValintatietotilaKoodistoviite } from "../../state/koodistot"
-import { Haku, Oppija, Valintatieto } from "../../state/oppijat"
+import { T } from "../../i18n/i18n"
+import { Haku, Oppija } from "../../state/oppijat"
 
 export type OppijanHautProps = {
   oppija: Oppija
@@ -16,9 +10,11 @@ export type OppijanHautProps = {
 export const OppijanHaut = (props: OppijanHautProps) =>
   props.oppija.haut && props.oppija.haut.length > 0 ? (
     <div>
-      {props.oppija.haut.map((haku) => (
-        <HakuTable key={haku.luotu} haku={haku} />
-      ))}
+      {props.oppija.haut.map(
+        (_haku) =>
+          // <HakuTable key={haku.luotu} haku={haku} />
+          "TODO"
+      )}
     </div>
   ) : (
     <NoDataMessage>
@@ -30,54 +26,54 @@ type HakuTableProps = {
   haku: Haku
 }
 
-const HakuTable = (props: HakuTableProps) => (
-  <IconSection icon={<HakuIcon color="gray" />}>
-    <TertiaryHeading>{getLocalized(props.haku.nimi)}</TertiaryHeading>
-    <LeanTable
-      columns={[
-        {
-          label: "Oppilaitos",
-        },
-        {
-          label: "Valinta",
-        },
-        {
-          label: "Pisteet",
-        },
-        {
-          label: "Alin pistemäärä",
-        },
-      ]}
-      data={props.haku.valintatiedot.map(valintatietoToTableValue)}
-    />
-  </IconSection>
-)
+// const HakuTable = (props: HakuTableProps) => (
+//   <IconSection icon={<HakuIcon color="gray" />}>
+//     <TertiaryHeading>{getLocalized(props.haku.nimi)}</TertiaryHeading>
+//     <LeanTable
+//       columns={[
+//         {
+//           label: "Oppilaitos",
+//         },
+//         {
+//           label: "Valinta",
+//         },
+//         {
+//           label: "Pisteet",
+//         },
+//         {
+//           label: "Alin pistemäärä",
+//         },
+//       ]}
+//       data={props.haku.valintatiedot.map(valintatietoToTableValue)}
+//     />
+//   </IconSection>
+// )
 
-const valintatietoToTableValue = (
-  valinta: Valintatieto,
-  index: number
-): Datum => ({
-  key: index.toString(),
-  values: [
-    {
-      value: `${valinta.hakukohdenumero}. ${getLocalized(
-        valinta.hakukohde.nimi
-      )}`,
-    },
-    {
-      value:
-        valinta.tila &&
-        (ValintatietotilaKoodistoviite.isHyväksytty(valinta.tila)
-          ? t("oppija__haut_hyvaksytty")
-          : valinta.tila.koodiarvo === "hylätty"
-          ? t("oppija__haut_hylatty")
-          : null),
-    },
-    { value: valinta.pisteet ? formatFixedNumber(valinta.pisteet, 2) : "" },
-    {
-      value: valinta.alinPistemäärä
-        ? formatFixedNumber(valinta.alinPistemäärä, 2)
-        : "",
-    },
-  ],
-})
+// const valintatietoToTableValue = (
+//   valinta: Valintatieto,
+//   index: number
+// ): Datum => ({
+//   key: index.toString(),
+//   values: [
+//     {
+//       value: `${valinta.hakukohdenumero}. ${getLocalized(
+//         valinta.hakukohde.nimi
+//       )}`,
+//     },
+//     {
+//       value:
+//         valinta.tila &&
+//         (ValintatietotilaKoodistoviite.isHyväksytty(valinta.tila)
+//           ? t("oppija__haut_hyvaksytty")
+//           : valinta.tila.koodiarvo === "hylätty"
+//           ? t("oppija__haut_hylatty")
+//           : null),
+//     },
+//     { value: valinta.pisteet ? formatFixedNumber(valinta.pisteet, 2) : "" },
+//     {
+//       value: valinta.alinPistemäärä
+//         ? formatFixedNumber(valinta.alinPistemäärä, 2)
+//         : "",
+//     },
+//   ],
+// })
