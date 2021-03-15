0831a10014d7f1bfdac4d80d3d439b45a5de14fd
Matias Katajamäki <mkataja@reaktor.fi>
2021-03-15 14:36:40 +0200

    Lisää EnumValues-annotaatio

--- src/main/scala/fi/oph/koski/schema/annotation/Annotations.scala
+++ src/main/scala/fi/oph/koski/schema/annotation/Annotations.scala
@@ -37,3 +37,19 @@ case class Tooltip(text: String) extends RepresentationalMetadata
 
 /* Numeric field should be rendered using this scale */
 case class Scale(numberOfDigits: Int) extends RepresentationalMetadata
+
+// TODO: Toteuta siistimmin suoraan scala-schemaan
+case class EnumValues(values: Set[String]) extends Metadata {
+  override def applyMetadata(x: ObjectWithMetadata[_], schemaFactory: SchemaFactory): ObjectWithMetadata[_] = {
+    val metadata = x match {
+      case p: Property => p.schema match {
+        case s: StringSchema => p.copy(schema = s.copy(enumValues = Option(s.enumValues.toList.flatten ++ values)))
+        case other: Any => throw new UnsupportedOperationException("EnumValues not supported for " + other)
+      }
+      case other: Any => throw new UnsupportedOperationException("EnumValues not supported for " + other)
+    }
+    super.applyMetadata(metadata, schemaFactory)
+  }
+
+  override def appendMetadataToJsonSchema(obj: JsonAST.JObject): JsonAST.JObject = obj
+}
