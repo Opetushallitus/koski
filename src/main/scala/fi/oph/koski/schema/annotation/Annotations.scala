be24ab4d3ba8fc41ef34a18751ef57b7cc862057
Matias Katajamäki <mkataja@reaktor.fi>
2021-03-15 14:36:28 +0200

    Poista scala-schemasta kopioitua koodia

--- src/main/scala/fi/oph/koski/schema/annotation/Annotations.scala
+++ src/main/scala/fi/oph/koski/schema/annotation/Annotations.scala
@@ -1,13 +1,8 @@
 package fi.oph.koski.schema.annotation
 
 import fi.oph.koski.koskiuser.Rooli.Role
-import fi.oph.scalaschema.Metadata
+import fi.oph.scalaschema._
 import org.json4s.JsonAST
-import org.json4s.JsonAST.JObject
-
-trait RepresentationalMetadata extends Metadata {
-  override def appendMetadataToJsonSchema(obj: JObject) = obj // Does not affect JSON schema
-}
 
 /* This property can be used to represent the whole entity */
 case class Representative() extends RepresentationalMetadata
