3046411ae131f185aeb5980c82c696e567202662
Matias Katajamäki <mkataja@reaktor.fi>
2021-03-18 20:52:32 +0200

    Testaa että aidon API-vastauksen serialisointi onnistuu

--- src/test/scala/fi/oph/koski/valpas/SureHakukoosteServiceSpec.scala
+++ src/test/scala/fi/oph/koski/valpas/SureHakukoosteServiceSpec.scala
@@ -7,7 +7,7 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration._
 import com.typesafe.config.ConfigFactory
 import fi.oph.koski.valpas.hakukooste.{Hakukooste, HakukoosteExampleData, ValpasHakukoosteService}
 import fi.oph.koski.valpas.henkilo.ValpasMockOppijat
-import org.json4s.jackson.Serialization.write
+import org.json4s.jackson.Serialization
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatest._
 
@@ -72,6 +72,19 @@ class SureHakukoosteServiceSpec extends ValpasTestBase with Matchers with Either
       val result = mockClient.getHakukoosteet(queryOids).right.value
       result.map(_.oppijaOid) should equal(queryOids.toList)
     }
+
+    "osaa serialisoida aidon API-vastauksen" in {
+      val queryOids = Set("1.2.246.562.24.85292063498")
+      wireMockServer.stubFor(
+        WireMock.post(urlPathEqualTo(sureHakukoosteUrl))
+          .willReturn(ok().withBody(SureHakukoosteServiceSpec.realResponse))
+      )
+      val result = mockClient.getHakukoosteet(queryOids).right.value.head
+      result.oppijaOid should equal(queryOids.head)
+      result.hakutapa.nimi.get.get("sv") should equal("Gemensam ansökan")
+      result.hakuNimi.get("en") should startWith("Joint Application")
+      result.hakutoiveet.map(_.hakukohdeOid) should equal(List("1.2.246.562.20.80878445842", "1.2.246.562.20.24492106752"))
+    }
   }
 
   private def mockResponseForOids(
@@ -86,6 +99,80 @@ class SureHakukoosteServiceSpec extends ValpasTestBase with Matchers with Either
     wireMockServer.stubFor(
       WireMock.post(urlPathEqualTo(sureHakukoosteUrl))
         .withRequestBody(equalToJson(expectedRequest))
-        .willReturn(ok().withBody(write(response))))
+        .willReturn(ok().withBody(Serialization.write(response))))
   }
+}
+
+object SureHakukoosteServiceSpec {
+  private val realResponse =
+    """[
+      |  {
+      |    "hakutapa": {
+      |      "koodiarvo": "01",
+      |      "nimi": {
+      |        "fi": "Yhteishaku",
+      |        "sv": "Gemensam ansökan"
+      |      },
+      |      "lyhytNimi": {
+      |        "fi": "YH",
+      |        "sv": "GA"
+      |      },
+      |      "koodistoUri": "hakutapa_01",
+      |      "koodistoVersio": 1
+      |    },
+      |    "hakutyyppi": {
+      |      "koodiarvo": "01",
+      |      "nimi": {
+      |        "fi": "Varsinainen haku",
+      |        "sv": "Egentlig ansökan"
+      |      },
+      |      "lyhytNimi": {
+      |        "fi": "VH",
+      |        "sv": "EA"
+      |      },
+      |      "koodistoUri": "hakutyyppi_01",
+      |      "koodistoVersio": 1
+      |    },
+      |    "muokattu": "",
+      |    "oppijaOid": "1.2.246.562.24.85292063498",
+      |    "hakemusOid": "1.2.246.562.11.00000000000000675952",
+      |    "hakuOid": "1.2.246.562.29.72389663526",
+      |    "hakuNimi": {
+      |      "fi": "Korkeakoulujen kevään 2021 ensimmäinen yhteishaku",
+      |      "sv": "Högskolornas första gemensamma ansökan, våren 2021",
+      |      "en": "Joint Application to Higher Education, Spring 2021"
+      |    },
+      |    "email": "hakija-33809716-muokkaus@oph.fi",
+      |    "matkapuhelin": "050 86732551",
+      |    "postinumero": "00100",
+      |    "lahiosoite": "Kinkkuaukio 275",
+      |    "postitoimipaikka": "Olsztyn Poland",
+      |    "hakutoiveet": [
+      |      {
+      |        "hakukohdeNimi": {},
+      |        "koulutusNimi": {},
+      |        "vastaanottotieto": "KESKEN",
+      |        "valintatila": "KESKEN",
+      |        "ilmoittautumistila": "EI_TEHTY",
+      |        "hakutoivenumero": 0,
+      |        "hakukohdeOid": "1.2.246.562.20.80878445842",
+      |        "hakukohdeKoulutuskoodi": "751201",
+      |        "hakukohdeOrganisaatio": "1.2.246.562.10.38864316104",
+      |        "koulutusOid": "1.2.246.562.17.83264128444"
+      |      },
+      |      {
+      |        "hakukohdeNimi": {},
+      |        "koulutusNimi": {},
+      |        "vastaanottotieto": "KESKEN",
+      |        "valintatila": "KESKEN",
+      |        "ilmoittautumistila": "EI_TEHTY",
+      |        "hakutoivenumero": 1,
+      |        "hakukohdeOid": "1.2.246.562.20.24492106752",
+      |        "hakukohdeKoulutuskoodi": "751301",
+      |        "hakukohdeOrganisaatio": "1.2.246.562.10.39920288212",
+      |        "koulutusOid": "1.2.246.562.17.58545683169"
+      |      }
+      |    ]
+      |  }
+      |]""".stripMargin
 }
