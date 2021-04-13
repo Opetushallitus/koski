b64dd7761a733ee6db85fe52a624b3d00ea0cb49
Matias Katajamäki <mkataja@reaktor.fi>
2021-04-13 18:08:19 +0300

    Päättele raamien käyttö konfiguraation perusteella

--- src/main/scala/fi/oph/koski/servlet/OmaOpintopolkuSupport.scala
+++ src/main/scala/fi/oph/koski/servlet/OmaOpintopolkuSupport.scala
@@ -3,12 +3,16 @@ package fi.oph.koski.servlet
 import fi.oph.koski.config.Environment
 import fi.oph.koski.html.{EiRaameja, Oppija, Raamit}
 import fi.oph.koski.koskiuser.KoskiSpecificAuthenticationSupport
-import org.scalatra.servlet.RichRequest
 
 trait OmaOpintopolkuSupport extends KoskiSpecificAuthenticationSupport with LanguageSupport {
-  def oppijaRaamit: Raamit = if (oppijaRaamitSet || useOppijaRaamitProxy) Oppija(koskiSessionOption, request, loginUrl) else EiRaameja
-  def loginUrl: String = application.config.getString("identification.url." + langFromCookie.getOrElse(langFromDomain))
-  def oppijaRaamitSet: Boolean = isCloudEnvironment
+  private val oppijaRaamitSet: Boolean = Environment.isServerEnvironment(application.config)
+  private def loginUrl: String = application.config.getString("identification.url." + langFromCookie.getOrElse(langFromDomain))
   private val useOppijaRaamitProxy = application.config.hasPath("oppijaRaamitProxy")
-  private lazy val isCloudEnvironment = !Environment.isLocalDevelopmentEnvironment
+
+  protected def oppijaRaamit: Raamit =
+    if (oppijaRaamitSet || useOppijaRaamitProxy) {
+      Oppija(koskiSessionOption, request, loginUrl)
+    } else {
+      EiRaameja
+    }
 }
