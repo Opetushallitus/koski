8e9e2cb17fe23f17e1d7d29eaba64f9cc1b73d15
Matias Katajamäki <mkataja@reaktor.fi>
2021-06-14 19:37:24 +0300

    Nosta valpas-kannan smallDatabaseMaxRows

--- src/main/scala/fi/oph/koski/valpas/db/ValpasDatabase.scala
+++ src/main/scala/fi/oph/koski/valpas/db/ValpasDatabase.scala
@@ -4,7 +4,7 @@ import fi.oph.koski.db.{Database, DatabaseConfig}
 import slick.jdbc.PostgresProfile.api._
 
 class ValpasDatabase(protected val config: DatabaseConfig) extends Database {
-  override final val smallDatabaseMaxRows = 5
+  override final val smallDatabaseMaxRows = 50
 
   override protected lazy val dbSizeQuery = ValpasSchema.Ilmoitukset.length.result
 }
