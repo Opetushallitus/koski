320c1c4b2102c1f67b6d2b4027f4a57a3b49eed8
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-06-30 13:51:52 +0300

    Modaalin aukaisu- ja sulkemisanimaatio

--- valpas-web/src/utils/environment.ts
+++ valpas-web/src/utils/environment.ts
@@ -1,4 +1,6 @@
 const runningOnJest = process.env.JEST_WORKER_ID !== undefined
 
+export const animationsDisabled = () => process.env.NODE_ENV === "test"
+
 export const runningLocally: boolean =
   runningOnJest || ["local", "unittest"].includes(window.environment || "")
