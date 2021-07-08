790e9f7f0034a47a7eca0d609f8411bf89991287
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-07-08 06:56:33 +0300

    Ohjaa devaaja juuresta /valpas/virkailija -polkuun

--- valpas-web/.proxyrc.js
+++ valpas-web/.proxyrc.js
@@ -2,15 +2,28 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 const express = require("express")
 
 const STATIC_RESPONSE_APP_PORT = 10101
-
-const staticApp = express()
-staticApp.get("/**", (_req, res) => res.send(""))
-staticApp.listen(STATIC_RESPONSE_APP_PORT)
+const REDIRECTS = {
+  "/": "/valpas/virkailija",
+  "/valpas": "/valpas/virkailija",
+}
 
 module.exports = function (app) {
   setupBackendProxy(app)
   setupRaamitProxy(app)
+  setupRedirects(app)
 }
+
+// Staattinen apupalvelin, joka tarvitaan koska API-proxy ei tue get- eikä redirect-metodeita
+
+const staticApp = express()
+Object.entries(REDIRECTS).map(([path, target]) => {
+  staticApp.get(path, (_req, res) => res.redirect(target))
+  console.log(`Redirecting: ${path} -> ${target}`)
+})
+staticApp.get("/raamit", (_req, res) => res.send(""))
+staticApp.listen(STATIC_RESPONSE_APP_PORT)
+
+// Proxy backend-palvelimeen
 
 const setupBackendProxy = (app) => {
   const backendHost = process.env.BACKEND_HOST || "http://localhost:7021"
@@ -22,14 +35,27 @@ const setupBackendProxy = (app) => {
   )
 }
 
+// Proxy raameihin tai jos ei asetettu, apupalvelimelle
+
 const setupRaamitProxy = (app) => {
   const target =
     process.env.VIRKAILIJA_RAAMIT_HOST ||
-    `http://localhost:${STATIC_RESPONSE_APP_PORT}`
+    `http://localhost:${STATIC_RESPONSE_APP_PORT}/raamit`
   app.use(
     createProxyMiddleware("/virkailija-raamit", {
       target,
       changeOrigin: true,
     })
   )
+}
+
+const setupRedirects = (app) => {
+  Object.keys(REDIRECTS).map((path) => {
+    app.use(
+      createProxyMiddleware((pathname) => pathname === path, {
+        target: `http://localhost:${STATIC_RESPONSE_APP_PORT}${path}`,
+        changeOrigin: true,
+      })
+    )
+  })
 }
