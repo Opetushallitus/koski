5fcce0c4a9a74a7db3bea5cfb6d5737fc9fe9898
Matias Katajamäki <mkataja@reaktor.fi>
2021-09-14 21:12:23 +0300

    Refaktoroi

--- src/main/scala/fi/oph/koski/raportointikanta/OrganisaatioHistoriaRowBuilder.scala
+++ src/main/scala/fi/oph/koski/raportointikanta/OrganisaatioHistoriaRowBuilder.scala
@@ -2,19 +2,16 @@ package fi.oph.koski.raportointikanta
 
 import java.time.LocalDate
 
-import scalaz._
-import syntax.std.list._
+import scalaz.syntax.std.list._
 
 import fi.oph.koski.schema.{KoskeenTallennettavaOpiskeluoikeus, OpiskeluoikeudenOrganisaatiohistoria}
 import fi.oph.koski.util.DateOrdering.localDateOrdering
 
 object OrganisaatioHistoriaRowBuilder {
+  private val EarlyDate = LocalDate.of(1900, 1, 1)
+  private val LateDate = LocalDate.of(9999, 12, 31)
+
   def buildOrganisaatioHistoriaRows(opiskeluoikeus: KoskeenTallennettavaOpiskeluoikeus): Seq[ROrganisaatioHistoriaRow] = {
-    def latterOf(a: LocalDate, b: LocalDate): LocalDate = if (b.isAfter(a)) b else a
-
-    val EarlyDate = LocalDate.of(1900, 1, 1)
-    val LateDate = LocalDate.of(9999, 12, 31)
-
     val nykyinenOrganisaatio = OpiskeluoikeudenOrganisaatiohistoria(
       muutospäivä = LateDate,
       oppilaitos = opiskeluoikeus.oppilaitos,
@@ -23,17 +20,33 @@ object OrganisaatioHistoriaRowBuilder {
     val kaikkiOrganisaatiot = opiskeluoikeus.organisaatiohistoria.toList.flatten ::: List(nykyinenOrganisaatio)
     val firstStartDate = opiskeluoikeus.alkamispäivä.getOrElse(EarlyDate)
 
-    val organisaatiohistoriat = kaikkiOrganisaatiot.mapAccumLeft(firstStartDate, (previousMuutospäivä: LocalDate, organisaatioHistoria) => {
-      val loppu = latterOf(organisaatioHistoria.muutospäivä.minusDays(1), previousMuutospäivä)
-      val row = ROrganisaatioHistoriaRow(
-        opiskeluoikeusOid = opiskeluoikeus.oid.get,
-        alku = previousMuutospäivä,
-        loppu = loppu,
-        oppilaitosOid = organisaatioHistoria.oppilaitos.map(_.oid),
-        koulutustoimijaOid = organisaatioHistoria.koulutustoimija.map(_.oid)
-      )
-      (organisaatioHistoria.muutospäivä, row)
-    })._2
-    organisaatiohistoriat.groupBy(_.alku).values.map(_.last).toList.sortBy(_.alku)(localDateOrdering)
+    val organisaatiohistoriat = kaikkiOrganisaatiot.mapAccumLeft(
+      firstStartDate,
+      buildOrganisaatioHistoriaRow(opiskeluoikeus)
+    )._2
+
+    organisaatiohistoriat
+      .groupBy(_.alku)
+      .values
+      .map(_.last)
+      .toList
+      .sortBy(_.alku)(localDateOrdering)
+  }
+
+  private def latterOf(a: LocalDate, b: LocalDate): LocalDate = if (b.isAfter(a)) b else a
+
+  private def buildOrganisaatioHistoriaRow
+    (opiskeluoikeus: KoskeenTallennettavaOpiskeluoikeus)
+    (previousMuutospäivä: LocalDate, organisaatioHistoria: OpiskeluoikeudenOrganisaatiohistoria)
+  : (LocalDate, ROrganisaatioHistoriaRow) = {
+    val loppu = latterOf(organisaatioHistoria.muutospäivä.minusDays(1), previousMuutospäivä)
+    val row = ROrganisaatioHistoriaRow(
+      opiskeluoikeusOid = opiskeluoikeus.oid.get,
+      alku = previousMuutospäivä,
+      loppu = loppu,
+      oppilaitosOid = organisaatioHistoria.oppilaitos.map(_.oid),
+      koulutustoimijaOid = organisaatioHistoria.koulutustoimija.map(_.oid)
+    )
+    (organisaatioHistoria.muutospäivä, row)
   }
 }
