ad447b6486d67cb8458abbf531e01ec8f7689363
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-09-30 14:33:07 +0300

    Siirrä hakutilannetaulun dataformatointia omiin moduuleihin

---
+++ valpas-web/src/utils/tableDataFormatters/opiskelupaikanVastaanotto.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { SuccessIcon } from "../../components/icons/Icon"
+import { Value } from "../../components/tables/DataTable"
+import { getLocalizedMaybe, t } from "../../i18n/i18n"
+import { HakuSuppeatTiedot, selectByHakutoive } from "../../state/apitypes/haku"
+import { isVastaanotettu } from "../../state/apitypes/hakutoive"
+import { nullableValue } from "./commonFormatters"
+
+export const opiskelupaikanVastaanottotietoValue = (
+  hakutilanteet: HakuSuppeatTiedot[]
+): Value => {
+  const vastaanotetut = selectByHakutoive(hakutilanteet, isVastaanotettu)
+  switch (vastaanotetut.length) {
+    case 0:
+      return nullableValue(null)
+    case 1:
+      return {
+        value: getLocalizedMaybe(vastaanotetut[0]?.organisaatioNimi),
+        icon: <SuccessIcon />,
+      }
+    default:
+      return {
+        value: t("vastaanotettu__n_paikkaa", {
+          lukumäärä: vastaanotetut.length,
+        }),
+        tooltip: vastaanotetut
+          .map((vo) => getLocalizedMaybe(vo.organisaatioNimi))
+          .join("\n"),
+        icon: <SuccessIcon />,
+      }
+  }
+}
---
+++ valpas-web/src/utils/tableDataFormatters/valintatila.tsx
@@ -0,0 +1,83 @@
+import * as A from "fp-ts/Array"
+import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray"
+import React from "react"
+import { WarningIcon } from "../../components/icons/Icon"
+import { Value } from "../../components/tables/DataTable"
+import { getLocalizedMaybe, t } from "../../i18n/i18n"
+import { HakuSuppeatTiedot, selectByHakutoive } from "../../state/apitypes/haku"
+import {
+  isEiPaikkaa,
+  isHyväksytty,
+  isVarasijalla,
+  SuppeaHakutoive,
+} from "../../state/apitypes/hakutoive"
+import { nonEmptyEvery } from "../arrays"
+import { nullableValue } from "./commonFormatters"
+
+export const valintatilaValue = (haut: HakuSuppeatTiedot[]): Value => {
+  const hyväksytytHakutoiveet = selectByHakutoive(haut, isHyväksytty)
+  if (A.isNonEmpty(hyväksytytHakutoiveet)) {
+    return hyväksyttyValintatila(hyväksytytHakutoiveet)
+  }
+
+  const [varasija] = selectByHakutoive(haut, isVarasijalla)
+  if (varasija) {
+    return {
+      value: t("valintatieto__varasija"),
+      display: t("valintatieto__varasija_hakukohde", {
+        hakukohde: getLocalizedMaybe(varasija.organisaatioNimi) || "?",
+      }),
+    }
+  }
+
+  if (
+    nonEmptyEvery(haut, (haku) => nonEmptyEvery(haku.hakutoiveet, isEiPaikkaa))
+  ) {
+    return {
+      value: t("valintatieto__ei_opiskelupaikkaa"),
+      icon: <WarningIcon />,
+    }
+  }
+
+  return nullableValue(null)
+}
+
+const hyväksyttyValintatila = (
+  hyväksytytHakutoiveet: NonEmptyArray<SuppeaHakutoive>
+): Value => {
+  const buildHyväksyttyValue = (hakutoive: SuppeaHakutoive) => {
+    return {
+      value: t("valintatieto__hyväksytty", {
+        hakukohde: orderedHakukohde(
+          hakutoive.hakutoivenumero,
+          t("valintatieto__hakukohde_lc")
+        ),
+      }),
+      display: orderedHakukohde(
+        hakutoive.hakutoivenumero,
+        getLocalizedMaybe(hakutoive.organisaatioNimi) || "?"
+      ),
+    }
+  }
+
+  if (hyväksytytHakutoiveet.length === 1) {
+    return buildHyväksyttyValue(hyväksytytHakutoiveet[0])
+  }
+
+  return {
+    value: t("valintatieto__hyväksytty_n_hakutoivetta", {
+      lukumäärä: hyväksytytHakutoiveet.length,
+    }),
+    filterValues: hyväksytytHakutoiveet.map(
+      (hakutoive) => buildHyväksyttyValue(hakutoive).value
+    ),
+    tooltip: hyväksytytHakutoiveet
+      .map((ht) => buildHyväksyttyValue(ht).display)
+      .join("\n"),
+  }
+}
+
+const orderedHakukohde = (
+  hakutoivenumero: number | undefined,
+  hakukohde: string
+) => (hakutoivenumero ? `${hakutoivenumero}. ${hakukohde}` : hakukohde)
