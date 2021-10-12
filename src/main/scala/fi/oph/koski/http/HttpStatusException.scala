e8da9c227041679c6607eee4a1bbb3c7bd67ad61
Matias Katajamäki <mkataja@reaktor.fi>
2021-10-12 17:20:42 +0300

    Refaktoroi ja muotoile
    Muutetaan LoggableException luokan nimeksi kuvaavampi HttpException.

--- src/main/scala/fi/oph/koski/http/HttpStatusException.scala
+++ src/main/scala/fi/oph/koski/http/HttpStatusException.scala
@@ -1,26 +1,35 @@
 package fi.oph.koski.http
 
 import cats.effect.IO
-import fi.oph.koski.log.LogUtils.maskSensitiveInformation
+import fi.oph.koski.log.LogUtils
 import fi.oph.koski.log.Loggable
 import org.http4s.Request
 
 /**
  *  Thrown when an external service returns an unexpected HTTP status code.
  */
-case class HttpStatusException(status: Int, text: String, method: String, uri: String) extends LoggableException(status + ": " + text + " when requesting " + method + " " + uri)
+case class HttpStatusException(status: Int, msg: String, method: String, uri: String)
+  extends HttpException(status + ": " + msg + " when requesting " + method + " " + uri)
 
 object HttpStatusException {
-  def apply(status: Int, text: String, request: Request[IO]): HttpStatusException = HttpStatusException(status, text, request.method.toString, request.uri.toString)
+  def apply(status: Int, text: String, request: Request[IO]): HttpStatusException =
+    HttpStatusException(status, text, request.method.toString, request.uri.toString)
 }
 
-case class HttpConnectionException(text: String, method: String, uri: String) extends LoggableException(text + " when requesting " + method + " " + uri)
+case class HttpConnectionException(msg: String, method: String, uri: String)
+  extends HttpException(msg, method, uri)
 
 object HttpConnectionException {
-  def apply(text: String, request: Request[IO]): HttpConnectionException = HttpConnectionException(text, request.method.toString, request.uri.toString)
+  def apply(text: String, request: Request[IO]): HttpConnectionException =
+    HttpConnectionException(text, request.method.toString, request.uri.toString)
 }
 
-abstract class LoggableException(msg: String) extends RuntimeException(msg) with Loggable {
-  def logString = getMessage
-  override def getMessage: String = maskSensitiveInformation(super.getMessage)
+abstract class HttpException(msg: String) extends RuntimeException(msg) with Loggable {
+  def this(msg: String, method: String, uri: String) = this(s"${msg} when requesting ${method} ${uri}")
+
+  def this(msg: String, request: Request[IO]) = this(msg, request.method.toString(), request.uri.toString())
+
+  override def logString: String = getMessage
+
+  override def getMessage: String = LogUtils.maskSensitiveInformation(super.getMessage)
 }
