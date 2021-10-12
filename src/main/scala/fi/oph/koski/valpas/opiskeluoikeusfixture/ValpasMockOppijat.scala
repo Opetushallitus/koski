8cfd5e4bcb83bcd872c2a85127ac8f04ba091b00
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2021-10-12 08:59:55 +0300

    Tutki myös deprekoidun kotiopetusrakenteen data Valppaassa;
    kotiopetus-kenttä on deprekoitu jo keväällä 2019, mutta lähdejärjestelmät eivät ole vieläkään
    ottaneet käyttöön uutta kotiopetusjaksoja-kenttää. Valpas tutki aiemmin vain tätä uutta kenttää,
    minkä seurauksena suurin osa kotiopetusoppilaista näkyi edelleen Valppaassa.

--- src/main/scala/fi/oph/koski/valpas/opiskeluoikeusfixture/ValpasMockOppijat.scala
+++ src/main/scala/fi/oph/koski/valpas/opiskeluoikeusfixture/ValpasMockOppijat.scala
@@ -132,6 +132,7 @@ object ValpasMockOppijat {
   val telma = valpasOppijat.oppijaSyntymäaikaHetusta("Telma", "Valpas", "160304A7532", kotikunta = Some("091"))
   val telmaJaAmis = valpasOppijat.oppijaSyntymäaikaHetusta("Telma-ja-amis", "Valpas", "030204A7935", kotikunta = Some("091"))
   val kaksiToisenAsteenOpiskelua = valpasOppijat.oppijaSyntymäaikaHetusta("Kaksi-toisen-asteen-opiskelua", "Valpas", "120504A399N", kotikunta = Some("091"))
+  val kotiopetusMeneilläänVanhallaRakenteellaOppija = valpasOppijat.oppijaSyntymäaikaHetusta("Kotiopetus-meneillä-vanha-rakenne", "Valpas", "170205A776W", kotikunta = Some("091"))
 
   // Kutsumanimi ja yhteystiedot haetaan oppijanumerorekisteristä Valpas-käyttäjälle, tallennetaan siksi käyttäjä myös "oppijana" mockeihin
   val käyttäjäValpasJklNormaalikoulu = valpasOppijat.oppija(
