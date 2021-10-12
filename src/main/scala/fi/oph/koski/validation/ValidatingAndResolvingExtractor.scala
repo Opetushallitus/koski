21f46c0995d605a3c6cacf2068e567d083ae7d5c
Matias Katajamäki <mkataja@reaktor.fi>
2021-10-12 17:53:45 +0300

    Käsittele ScalaScheman heittämät poikkeukset

--- src/main/scala/fi/oph/koski/validation/ValidatingAndResolvingExtractor.scala
+++ src/main/scala/fi/oph/koski/validation/ValidatingAndResolvingExtractor.scala
@@ -2,6 +2,7 @@ package fi.oph.koski.validation
 
 import fi.oph.koski.http.{HttpStatus, JsonErrorMessage, KoskiErrorCategory}
 import fi.oph.koski.koodisto.{KoodistoResolvingCustomDeserializer, KoodistoViitePalvelu}
+import fi.oph.koski.log.Logging
 import fi.oph.koski.organisaatio.{OrganisaatioRepository, OrganisaatioResolvingCustomDeserializer}
 import fi.oph.koski.schema.JaksoCustomDeserializer
 import fi.oph.scalaschema.{ExtractionContext, SchemaValidatingExtractor}
@@ -9,11 +10,12 @@ import fi.oph.scalaschema.extraction.ValidationError
 import org.json4s._
 
 import scala.reflect.runtime.universe.TypeTag
+import scala.util.{Failure, Success, Try}
 
 class ValidatingAndResolvingExtractor(
   koodistoPalvelu: KoodistoViitePalvelu,
   organisaatioRepository: OrganisaatioRepository
-) {
+) extends Logging {
   /**
    * Extracts object from json value, and validates/resolves all KoodistoKoodiViite objects on the way.
    */
@@ -29,12 +31,17 @@ class ValidatingAndResolvingExtractor(
   }
 
   def extract[T](json: JValue, deserializationContext: ExtractionContext)(implicit tag: TypeTag[T]): Either[HttpStatus, T] = {
-    SchemaValidatingExtractor.extract(json)(deserializationContext, tag) match {
-      case Right(t) => Right(t)
-      // TODO: Validaatiovirhe ei aina tarkoita bad request -virhettä koska virhe
-      //  voi olla sisäisessä datan käsittelyssä. Refaktoroi tämä palauttamaan
-      //  kaikki validaatiovirheet ja muuta se HttpStatukseksi muualla.
-      case Left(errors: List[ValidationError]) => Left(KoskiErrorCategory.badRequest.validation.jsonSchema.apply(JsonErrorMessage(errors)))
+    Try(SchemaValidatingExtractor.extract(json)(deserializationContext, tag)) match {
+      case Success(value) => value match {
+        case Right(t) => Right(t)
+        // TODO: Validaatiovirhe ei aina tarkoita bad request -virhettä koska virhe
+        //  voi olla sisäisessä datan käsittelyssä. Refaktoroi tämä palauttamaan
+        //  kaikki validaatiovirheet ja muuta se HttpStatukseksi muualla.
+        case Left(errors: List[ValidationError]) => Left(KoskiErrorCategory.badRequest.validation.jsonSchema.apply(JsonErrorMessage(errors)))
+      }
+      case Failure(exception) =>
+        logger.error(exception)("Unexpected exception during deserialization")
+        Left(KoskiErrorCategory.internalError())
     }
   }
 }
