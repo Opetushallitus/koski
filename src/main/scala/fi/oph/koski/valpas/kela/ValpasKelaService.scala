ab8f3677bec6924e7ff91cde4bf47698424b89e2
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2021-10-22 09:17:14 +0300

    Lisää ajastustilastointi Valpas Kela-API:n kutsuihin

--- src/main/scala/fi/oph/koski/valpas/kela/ValpasKelaService.scala
+++ src/main/scala/fi/oph/koski/valpas/kela/ValpasKelaService.scala
@@ -3,15 +3,25 @@ package fi.oph.koski.valpas.kela
 import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.http.HttpStatus
 import fi.oph.koski.log._
+import fi.oph.koski.util.Timing
 import fi.oph.koski.valpas.ValpasErrorCategory
 import fi.oph.koski.valpas.db.ValpasSchema.OppivelvollisuudenKeskeytysRow
 import fi.oph.koski.valpas.opiskeluoikeusrepository.ValpasOppivelvollisuustiedotRow
 
-class ValpasKelaService(application: KoskiApplication) extends Logging {
+class ValpasKelaService(application: KoskiApplication) extends Logging with Timing {
   def findValpasKelaOppijatByHetut(hetut: Seq[String]): Either[HttpStatus, Seq[ValpasKelaOppija]] = {
-    val oppivelvollisuusTiedot = application.valpasOpiskeluoikeusDatabaseService.getOppivelvollisuusTiedot(hetut)
 
-    Right(asValpasKelaOppijatWithOppivelvollisuudenKeskeytykset(oppivelvollisuusTiedot))
+    val timedBlockName = hetut.length match {
+      case 1 => "findValpasKelaOppijatByHetut1"
+      case n if n <= 100 => "findValpasKelaOppijatByHetut2To100"
+      case n if n <= 500 => "findValpasKelaOppijatByHetut101To500"
+      case _ => "findValpasKelaOppijatByHetut501To1000"
+    }
+
+    timed(timedBlockName, 10) {
+      val oppivelvollisuusTiedot = application.valpasOpiskeluoikeusDatabaseService.getOppivelvollisuusTiedot(hetut)
+      Right(asValpasKelaOppijatWithOppivelvollisuudenKeskeytykset(oppivelvollisuusTiedot))
+    }
   }
 
   private def asValpasKelaOppijatWithOppivelvollisuudenKeskeytykset(oppijat: Seq[ValpasOppivelvollisuustiedotRow]): Seq[ValpasKelaOppija] = {
