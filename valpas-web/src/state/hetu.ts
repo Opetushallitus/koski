7afd824818e609c5027e43b2f6bd8d0bd84c904d
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-10-29 11:41:07 +0300

    Korjaa hidas hetujen syöttö

--- valpas-web/src/state/hetu.ts
+++ valpas-web/src/state/hetu.ts
@@ -1,8 +1,8 @@
-import * as A from "fp-ts/lib/Array"
 import { pipe } from "fp-ts/lib/function"
-import * as string from "fp-ts/string"
 
 export const hetulikeRegex = /[0123]\d[01]\d\d\d[-+A]\d\d\d[0123456789ABCDEFHJKLMNPRSTUVWXY]/g
 
 export const parseHetulikes = (input: string): string[] =>
-  pipe(input.toUpperCase().match(hetulikeRegex) || [], A.uniq(string.Eq))
+  pipe(input.toUpperCase().match(hetulikeRegex) || [], uniq)
+
+const uniq = <T>(ts: T[]): T[] => Array.from(new Set(ts))
