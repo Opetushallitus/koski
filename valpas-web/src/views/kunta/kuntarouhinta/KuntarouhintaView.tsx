8b1016764a259031d04fe161cb6eefabb0f6f052
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-11-02 14:20:22 +0200

    Kuntarouhinnan organisaatiovalitsin

--- valpas-web/src/views/kunta/kuntarouhinta/KuntarouhintaView.tsx
+++ valpas-web/src/views/kunta/kuntarouhinta/KuntarouhintaView.tsx
@@ -1,7 +1,16 @@
-import React from "react"
+import React, { useCallback, useMemo } from "react"
+import { useHistory } from "react-router-dom"
 import { Page } from "../../../components/containers/Page"
+import {
+  getOrganisaatiot,
+  OrganisaatioValitsin,
+} from "../../../components/shared/OrganisaatioValitsin"
 import { t } from "../../../i18n/i18n"
-import { withRequiresKuntavalvonta } from "../../../state/accessRights"
+import {
+  useOrganisaatiotJaKäyttöoikeusroolit,
+  withRequiresKuntavalvonta,
+} from "../../../state/accessRights"
+import { Oid } from "../../../state/common"
 import {
   kuntarouhintaPathWithOid,
   OrganisaatioOidRouteProps,
@@ -31,9 +40,36 @@ export type KuntarouhintaViewProps = OrganisaatioOidRouteProps
 export const KuntarouhintaView = withRequiresKuntavalvonta(
   (props: KuntarouhintaViewProps) => {
     const organisaatioOid = props.match.params.organisaatioOid!
+    const organisaatiotJaKäyttöoikeusroolit = useOrganisaatiotJaKäyttöoikeusroolit()
+    const organisaatiot = useMemo(
+      () =>
+        getOrganisaatiot(
+          organisaatiotJaKäyttöoikeusroolit,
+          organisaatioHakuRooli,
+          organisaatioTyyppi
+        ),
+      [organisaatiotJaKäyttöoikeusroolit]
+    )
+
+    const history = useHistory()
+    const changeOrganisaatio = useCallback(
+      (oid?: Oid) => {
+        if (oid) {
+          history.push(oid)
+        }
+      },
+      [history]
+    )
 
     return (
       <Page>
+        <OrganisaatioValitsin
+          organisaatioTyyppi={organisaatioTyyppi}
+          organisaatioHierarkia={organisaatiot}
+          valittuOrganisaatioOid={organisaatioOid}
+          label={t("Kunta")}
+          onChange={changeOrganisaatio}
+        />
         <KuntaNavigation selectedOrganisaatio={organisaatioOid} />
         TODO: Rouhinta {organisaatioOid}
       </Page>
