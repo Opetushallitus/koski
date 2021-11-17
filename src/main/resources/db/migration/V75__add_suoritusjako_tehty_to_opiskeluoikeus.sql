a95b52167f9d486ae6fe9af05e85d5936d9f72cf
Jalmari Ikävalko <tzaeru@gmail.com>
2021-11-17 17:23:16 +0200

    TOR-1370 - db-migraation queryä yksinkertaistettu

--- src/main/resources/db/migration/V75__add_suoritusjako_tehty_to_opiskeluoikeus.sql
+++ src/main/resources/db/migration/V75__add_suoritusjako_tehty_to_opiskeluoikeus.sql
@@ -1,9 +1,2 @@
 ALTER TABLE opiskeluoikeus
-  ADD IF NOT EXISTS suoritusjako_tehty_rajapaivan_jalkeen BOOLEAN;
-
-UPDATE opiskeluoikeus
-SET suoritusjako_tehty_rajapaivan_jalkeen = false
-WHERE suoritusjako_tehty_rajapaivan_jalkeen IS NULL;
-
-CREATE INDEX IF NOT EXISTS suoritusjako_tehty_rajapaivan_jalkeen
-  ON opiskeluoikeus (suoritusjako_tehty_rajapaivan_jalkeen);
+  ADD COLUMN IF NOT EXISTS suoritusjako_tehty_rajapaivan_jalkeen BOOLEAN DEFAULT FALSE;
