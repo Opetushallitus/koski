bd3b754452ed82e80ee7ff84c6be5fc7c31d4b6e
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2021-12-22 11:41:24 +0200

    Korjaa Korhopankki toimimaan CI-putkessa

--- scripts/install-and-verify-valpas-jest-deps.sh
+++ scripts/install-and-verify-valpas-jest-deps.sh
@@ -4,3 +4,7 @@ echo "Verifying test environment..."
 cd $(dirname "$0")/../valpas-web
 npm ci
 RUN_WITHOUT_SERVER=1 npm run test:integration -- -t "Testiympäristön oikeellisuus"
+
+cd ../web
+npm ci
+npm run build:prod
