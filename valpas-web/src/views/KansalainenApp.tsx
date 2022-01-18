90be1a6b2d9fe7dd1b937e6bd9d18adb970c0a79
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2022-01-18 13:26:49 +0200

    Kansalaisen sivusto kevyen feature flagin taakse;
    Enabloitavissa polusta /valpas/enable/beta

--- valpas-web/src/views/KansalainenApp.tsx
+++ valpas-web/src/views/KansalainenApp.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import { Redirect, Route, Switch } from "react-router"
+import { Page } from "../components/containers/Page"
 import { LoadingModal } from "../components/icons/Spinner"
 import { getCurrentKansalainenUser, hasValpasAccess } from "../state/auth"
 import { BasePathProvider, useBasePath } from "../state/basePath"
 import { User } from "../state/common"
+import { FeatureFlagEnabler, isFeatureFlagEnabled } from "../state/featureFlags"
 import { KansalainenContextProvider } from "../state/kansalainenContext"
 import {
   kansalainenEiOpintopolussaPath,
@@ -48,11 +50,21 @@ const ProtectedKansalainenRoutes = (props: ProtectedOppijaRoutesProps) => {
   const basePath = useBasePath()
   return (
     <Switch>
+      <Route exact path="/enable/beta">
+        <FeatureFlagEnabler
+          features={["kansalainenBeta"]}
+          redirectTo={kansalainenOmatTiedotPath.href(basePath)}
+        />
+      </Route>
       <Route exact path={basePath}>
         <Redirect to={kansalainenOmatTiedotPath.href(basePath)} />
       </Route>
       <Route exact path={kansalainenOmatTiedotPath.route(basePath)}>
-        <KansalainenOmatJaHuollettavienTiedotView user={props.user} />
+        {isFeatureFlagEnabled("kansalainenBeta") ? (
+          <KansalainenOmatJaHuollettavienTiedotView user={props.user} />
+        ) : (
+          <Page>Kansalaisen näkymä ei ole vielä käytössä</Page>
+        )}
       </Route>
       <Route>
         <NotFoundView />
