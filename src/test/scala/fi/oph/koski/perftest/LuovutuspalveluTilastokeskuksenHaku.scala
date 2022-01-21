f6dac330d32dca2779f26ee4a8657801303fa528
Jalmari Ikävalko <tzaeru@gmail.com>
2022-01-21 17:16:44 +0200

    tilastokeskus-käyttäjä tilastokeskuksen perffitestille

--- src/test/scala/fi/oph/koski/perftest/LuovutuspalveluTilastokeskuksenHaku.scala
+++ src/test/scala/fi/oph/koski/perftest/LuovutuspalveluTilastokeskuksenHaku.scala
@@ -1,12 +1,15 @@
 package fi.oph.koski.perftest
 
-import java.time.{LocalDate}
+import fi.oph.koski.koskiuser.MockUsers
+
+import java.time.LocalDate
 
 object LuovutuspalveluTilastokeskuksenHaku extends App {
   PerfTestRunner.executeTest(LuovutuspalveluTilastokeskuksenHakuScenario)
 }
 
 object LuovutuspalveluTilastokeskuksenHakuScenario extends PerfTestScenario {
+  override def defaultUser = MockUsers.tilastokeskusKäyttäjä
   val alkupäivä = LocalDate.now.minusMonths(1).toString
   def operation(x: Int) = List(Operation(uri = s"api/luovutuspalvelu/haku&opiskeluoikeusAlkanutViimeistään=${alkupäivä}"))
 }
