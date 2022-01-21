d7b22a6a8ad48519299772a7a7eff9408cfc4c78
Jalmari Ikävalko <tzaeru@gmail.com>
2022-01-21 17:52:23 +0200

    korjaus tilastokeskuksen käyttäjien käyttöön perffitestissä

--- src/test/scala/fi/oph/koski/perftest/LuovutuspalveluTilastokeskuksenHaku.scala
+++ src/test/scala/fi/oph/koski/perftest/LuovutuspalveluTilastokeskuksenHaku.scala
@@ -1,6 +1,7 @@
 package fi.oph.koski.perftest
 
-import fi.oph.koski.koskiuser.{MockUsers, UserWithPassword}
+import fi.oph.koski.koskiuser.UserWithPassword
+import fi.oph.koski.perftest.LocalYtrFetchKoesuoritusScenario.{Headers, defaultUser, kansalainenAuthHeaders}
 
 import java.time.LocalDate
 
@@ -9,10 +10,16 @@ object LuovutuspalveluTilastokeskuksenHaku extends App {
 }
 
 object LuovutuspalveluTilastokeskuksenHakuScenario extends PerfTestScenario {
-  override def defaultUser = new UserWithPassword {
+  def tilastokeskusUser = new UserWithPassword {
     override def username = requiredEnv("TILASTOKESKUS_USER")
     override def password = requiredEnv("TILASTOKESKUS_PASS")
   }
+  override def authHeaders(user: UserWithPassword = tilastokeskusUser): Headers = {
+    Map(basicAuthHeader(user.username, user.password))
+  }
   val alkupäivä = LocalDate.now.minusMonths(1).toString
-  def operation(x: Int) = List(Operation(uri = s"api/luovutuspalvelu/haku&opiskeluoikeusAlkanutViimeistään=${alkupäivä}"))
+  def operation(x: Int) = {
+    println(tilastokeskusUser.username)
+    List(Operation(uri = s"api/luovutuspalvelu/haku&opiskeluoikeusAlkanutViimeistään=${alkupäivä}", headers = authHeaders()))
+  }
 }
