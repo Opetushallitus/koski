cded17c4c53bcd798d154eca14cb9a470763598b
Jalmari Ikävalko <tzaeru@gmail.com>
2022-01-21 18:26:40 +0200

    tilastokeskus-häsläys

--- src/test/scala/fi/oph/koski/perftest/LuovutuspalveluTilastokeskuksenHaku.scala
+++ src/test/scala/fi/oph/koski/perftest/LuovutuspalveluTilastokeskuksenHaku.scala
@@ -11,11 +11,12 @@ object LuovutuspalveluTilastokeskuksenHaku extends App {
 
 object LuovutuspalveluTilastokeskuksenHakuScenario extends PerfTestScenario {
   def tilastokeskusUser = new UserWithPassword {
-    override def username = requiredEnv("TILASTOKESKUS_USER")
+    override def username = "passketti"
     override def password = requiredEnv("TILASTOKESKUS_PASS")
   }
   val alkupäivä = LocalDate.now.minusMonths(1).toString
+  val loppupäivä = LocalDate.now.toString
   def operation(x: Int) = {
-    List(Operation(uri = s"api/luovutuspalvelu/haku?v=1&opiskeluoikeusAlkanutAikaisintaan=${alkupäivä}", headers = authHeaders(tilastokeskusUser)))
+    List(Operation(uri = s"api/luovutuspalvelu/haku?v=1&opiskeluoikeusAlkanutAikaisintaan=$alkupäivä&opiskeluoikeusAlkanutViimeistään=$loppupäivä", headers = authHeaders(tilastokeskusUser)))
   }
 }
