ffca0839670bfe8928928e038860443c28f422f9
Jalmari Ikävalko <tzaeru@gmail.com>
2022-01-25 15:46:19 +0200

    linkataan tietokantamigraatioiden dokumentaatio migraatiotestistä

--- src/test/scala/fi/oph/koski/migration/MigrationSpec.scala
+++ src/test/scala/fi/oph/koski/migration/MigrationSpec.scala
@@ -7,8 +7,8 @@ import java.io.File
 
 class MigrationSpec extends AnyFreeSpec with Matchers {
   "Migraatiot" - {
-    "Havaittiin uusi tietokannan migraatiotiedosto. Migraatiot, varsinkin jos koskevat Kosken suurimpia tauluja, on hyvä testata tietokantareplikaa vasten. " +
-      "Korjaa tämän testin odottama tiedostomäärä, kun olet varma että migraatiot voi viedä eteenpäin." in {
+    "Havaittiin uusi tietokannan migraatiotiedosto. Migraatiot, varsinkin jos koskevat Kosken suurimpia tauluja, on hyvä testata tietokantareplikaa vasten.\n" +
+      "Korjaa tämän testin odottama tiedostomäärä, kun olet varma että migraatiot voi viedä eteenpäin.\nDokumentaatio: documentation/tietokantamigraatiot.md" in {
       new File("./src/main/resources/db/migration").listFiles.length should equal (78)
     }
   }
