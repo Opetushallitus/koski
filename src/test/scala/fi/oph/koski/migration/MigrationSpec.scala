15bb6ac5f4ff7626ccbf178b98f13756957aab6a
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2022-05-02 13:46:05 +0300

    Päivitä MigrationSpeciin uusi hash

--- src/test/scala/fi/oph/koski/migration/MigrationSpec.scala
+++ src/test/scala/fi/oph/koski/migration/MigrationSpec.scala
@@ -30,7 +30,7 @@ class MigrationSpec extends AnyFreeSpec with Matchers {
         "OpiskeluoikeusLoader.scala"                                -> "a3703edd2f80da2bf107cd2c74e29fd",
         "OrganisaatioHistoriaRowBuilder.scala"                      -> "7e586d9e273a5a4ee7beae257f22c7f4",
         "OrganisaatioLoader.scala"                                  -> "9e2e45da33ed335af4a7b0a31b139a7",
-        "RaportointiDatabase.scala"                                 -> "947b8a7ee6c36799f62aecc74346f7a1",
+        "RaportointiDatabase.scala"                                 -> "e07363d1c9ec42d0fbac33492af50695",
         "RaportointiDatabaseCustomFunctions.scala"                  -> "956f101d1219c49ac9134b72a30caf3a",
         "RaportointiDatabaseSchema.scala"                           -> "c4a3d5d595d9d48e0c42b8d0a9caa5f3",
         "RaportointikantaService.scala"                             -> "f8971bd0805acec3059a805a0de4c912",
