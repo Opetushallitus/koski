febda1f33607d1a0cc3245a3a416e1e4e6a514bb
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2022-05-03 08:48:06 +0300

    Refaktoroi: Hetu- yms. maskaus uudelleenkäytettäväksi;
    Tarvitaan content-security-policyn logituksessa.

--- src/main/scala/fi/oph/koski/log/MaskedSlf4jRequestLogWriter.scala
+++ src/main/scala/fi/oph/koski/log/MaskedSlf4jRequestLogWriter.scala
@@ -3,22 +3,34 @@ package fi.oph.koski.log
 import java.io.IOException
 import org.eclipse.jetty.server.Slf4jRequestLogWriter
 
-class MaskedSlf4jRequestLogWriter extends Slf4jRequestLogWriter {
-  @throws[IOException]
-  override def write(requestEntry: String): Unit = {
-    super.write(maskSensitiveInformation(requestEntry))
+object MaskedSlf4jRequestLogWriter {
+  def maskSensitiveInformation(s: String): String = {
+    maskSensitiveInformationAPIUris(
+      maskSensitiveInformationFrontendUris(s)
+    )
   }
 
-  private def maskSensitiveInformation(s: String): String = {
+  def maskSensitiveInformationFrontendUris(s: String): String = {
     s
       .replaceAll("(/koski/opinnot/[0-9a-f]{8})([0-9a-f]+)", "$1************************")
+      .replaceAll("(/koski/kela/)(\\d\\S+)", "$1******-****")
+  }
+
+  def maskSensitiveInformationAPIUris(s: String): String = {
+    s
       .replaceAll("(/koski/api/henkilo/hetu/)(\\S+)", "$1*")
       .replaceAll("(/koski/api/henkilo/search\\?query=)(\\S+)", "$1*")
       .replaceAll("(/koski/api/henkilo/search\\?query=)(\\S+)", "$1*")
       .replaceAll("(/koski/api/luovutuspalvelu/valvira/)(\\S+)", "$1******-****")
-      .replaceAll("(/koski/kela/)(\\d\\S+)", "$1******-****")
       .replaceAll("(/koski/valpas/api/henkilohaku/suorittaminen/)(\\d\\S+)", "$1******-****")
       .replaceAll("(/koski/valpas/api/henkilohaku/maksuttomuus/)(\\d\\S+)", "$1******-****")
       .replaceAll("(/koski/valpas/api/henkilohaku/kunta/)(\\d\\S+)", "$1******-****")
+  }
+}
+
+class MaskedSlf4jRequestLogWriter extends Slf4jRequestLogWriter {
+  @throws[IOException]
+  override def write(requestEntry: String): Unit = {
+    super.write(MaskedSlf4jRequestLogWriter.maskSensitiveInformation(requestEntry))
   }
 }
