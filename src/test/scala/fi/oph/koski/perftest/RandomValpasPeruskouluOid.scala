a8c2b0d42b7ed2a0fc38c47ad4fb417007f9fd28
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2022-05-04 15:52:57 +0300

    Testaa perftestissä uusi hakutieto-api

--- src/test/scala/fi/oph/koski/perftest/RandomValpasPeruskouluOid.scala
+++ src/test/scala/fi/oph/koski/perftest/RandomValpasPeruskouluOid.scala
@@ -2,6 +2,7 @@ package fi.oph.koski.perftest
 
 import fi.oph.koski.util.EnvVariables
 import fi.oph.koski.schema.Organisaatio
+import fi.oph.koski.valpas.opiskeluoikeusrepository.ValpasHenkilö
 
 import scala.io.Source
 import scala.util.Random
@@ -11,11 +12,22 @@ class RandomValpasPeruskouluOid extends EnvVariables {
   private def randomLines(filename: String) = Random.shuffle(Source.fromFile(filename).getLines().toList)
   private val oids = randomLines(organisaatioListaTiedosto)
   private var index = 0;
-  private def nextFrom(list: List[Organisaatio.Oid]) = this.synchronized {
+  private def nextFrom(list: List[String]) = this.synchronized {
     index = index + 1
-    list.apply(index % list.length)
+    PeruskouluJaOppijat(list.apply(index % list.length))
   }
 
   def next = nextFrom(oids)
 }
 
+case class PeruskouluJaOppijat(
+  oppilaitos: Organisaatio.Oid,
+  oppijat: List[ValpasHenkilö.Oid],
+)
+
+object PeruskouluJaOppijat {
+  def apply(source: String): PeruskouluJaOppijat = {
+    val oppilaitos :: oppijat = source.split(" ").toList
+    PeruskouluJaOppijat(oppilaitos, oppijat)
+  }
+}
