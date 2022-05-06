cddd209b8da6257b5ef5b68483aeae52bce64ac7
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2022-05-06 10:21:37 +0300

    Lisää varoituskommentti luokkien nimien muuttamisesta

--- src/main/scala/fi/oph/koski/frontendvalvonta/ReportToService.scala
+++ src/main/scala/fi/oph/koski/frontendvalvonta/ReportToService.scala
@@ -4,6 +4,8 @@ import fi.oph.koski.log.Logging
 import org.json4s.{JArray, JBool, JObject, JValue}
 import org.json4s.jackson.JsonMethods
 
+// Huom: Älä muuta luokan nimeä: Muuten logien keruu myöhemmin putkessa hajoaa, koska logimerkinnät tunnistetaan
+// log4j:ssä tällä hetkellä luokan nimen perusteella
 class ReportToService extends Logging {
 
   def report(request: JValue): Unit = {
--- src/main/scala/fi/oph/koski/frontendvalvonta/ReportUriService.scala
+++ src/main/scala/fi/oph/koski/frontendvalvonta/ReportUriService.scala
@@ -4,6 +4,8 @@ import fi.oph.koski.log.Logging
 import org.json4s.{JBool, JObject, JValue}
 import org.json4s.jackson.JsonMethods
 
+// Huom: Älä muuta luokan nimeä: Muuten logien keruu myöhemmin putkessa hajoaa, koska logimerkinnät tunnistetaan
+// log4j:ssä tällä hetkellä luokan nimen perusteella
 class ReportUriService extends Logging {
   def report(request: JValue): Unit = {
     val valueWithTagField = request.merge(JObject("cspreporturi" -> JBool(true)))
