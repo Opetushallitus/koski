8be9ab1a4276b8d1616fea505f4aa4b72c071d72
Juha Rantanen <juha.rantanen@reaktor.fi>
2022-05-25 11:18:24 +0300

    Lisätään debuggausta

--- src/main/scala/fi/oph/koski/luovutuspalvelu/PalveluvaylaServlet.scala
+++ src/main/scala/fi/oph/koski/luovutuspalvelu/PalveluvaylaServlet.scala
@@ -12,24 +12,37 @@ class PalveluvaylaServlet(implicit val application: KoskiApplication) extends So
   private val suomiFiService = new SuomiFiService(application)
 
   post("/suomi-fi-rekisteritiedot") {
+    logger.info("POST /suomi-fi-rekisteritiedot")
     requireSuomiFiUser
+    logger.info("Oikeudet kunnossa")
     val soapResp = (for {
       xml <- xmlBody
       hetu <- extractHetu(xml)
       opiskeluoikeudet <- suomiFiService.suomiFiOpiskeluoikeudet(hetu)
     } yield suomiFiBody(xml,opiskeluoikeudet)) match {
-      case Right(soap) => soap
-      case Left(status) => haltWithStatus(status)
+      case Right(soap) => {
+        logger.info("Parsittiin hetu onnistuneesti")
+        soap
+      }
+      case Left(status) => {
+        logger.info("Hetun parsinta epäonnistui")
+        haltWithStatus(status)
+      }
     }
-
+    logger.info("Lähetetään vastaus: " + soapResp.toString)
     writeXml(soapResp)
   }
 
   // This check is in addition to RequiresLuovutuspalvelu
-  private def requireSuomiFiUser =
+  private def requireSuomiFiUser = {
+    logger.info("Käyttöoikeuksien tarkistus")
+    logger.info("Sesssion oid: " + koskiSession.oid)
+    logger.info("suomi-fi-user-oid: " + application.config.getString("suomi-fi-user-oid"))
+
     if (koskiSession.oid != application.config.getString("suomi-fi-user-oid")) {
       haltWithStatus(KoskiErrorCategory.forbidden.kiellettyKäyttöoikeus())
     }
+  }
 
   private def extractHetu(soap: Elem) =
     (soap \\ "Envelope" \\ "Body" \\ "suomiFiRekisteritiedot" \\ "hetu")
