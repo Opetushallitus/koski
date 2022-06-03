4e029632bf3be02f87e8eaac7c410431fad39249
Juha Rantanen <juha.rantanen@reaktor.fi>
2022-06-03 16:33:38 +0300

    Lokitetaan suresta tulevat virheet suoraan performanssitestin lokiin

--- src/test/scala/fi/oph/koski/perftest/ValpasRandomPerusopetuksenOppijaGetter.scala
+++ src/test/scala/fi/oph/koski/perftest/ValpasRandomPerusopetuksenOppijaGetter.scala
@@ -8,7 +8,13 @@ object ValpasRandomPerusopetuksenOppijaGetterScenario extends PerfTestScenario {
   val oids = new RandomValpasOppijaOid()
   oids.next // To check before start
   def operation(x: Int) = List(Operation(uri = s"valpas/api/oppija/${oids.next}", uriPattern=Some("valpas/api/oppija/_")))
-  override def bodyValidator: Boolean = !this.body.contains("hakutilanneError")
+  override def bodyValidator: Boolean = {
+    val bodyContainsError = this.body.contains("hakutilanneError")
+    if (bodyContainsError) {
+      logger.error(this.body)
+    }
+    !bodyContainsError
+  }
 }
 
 
