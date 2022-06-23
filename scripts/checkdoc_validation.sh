f3d77334e320136c5900791ae804f621f4988fbd
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-06-23 13:20:34 +0300

    Korjaa typo validaation muutoshistorian tarkastuksessa

--- scripts/checkdoc_validation.sh
+++ scripts/checkdoc_validation.sh
@@ -5,13 +5,13 @@ function has_validation_changes() {
   git log --name-only --pretty=format: HEAD...origin/master | grep -E "koski/validation|api/OppijaValidation" > /dev/null
 }
 
-function has_validaation_muustohistoria_changes() {
+function has_validaation_muutoshistoria_changes() {
   git log --name-only --pretty=format: HEAD...origin/master | grep -E "validaation_muutoshistoria" > /dev/null
 }
 
 function check_changes() {
   if has_validation_changes; then
-    if ! has_validaation_muustohistoria_changes; then
+    if ! has_validaation_muutoshistoria_changes; then
       echo "ERROR! Branch has validation changes but no changes to validaation_muutoshistoria.md:"
       echo
       git log --name-only HEAD...origin/master
