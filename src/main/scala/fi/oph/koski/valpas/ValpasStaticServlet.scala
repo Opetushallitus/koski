09f588e748bf57f2273e7c14446e52824cf6fe3e
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-07-20 10:24:01 +0300

    Lokita Valppaan front-endin hostauksen virheet, palauta HTTP 500 virheen sattuessa

--- src/main/scala/fi/oph/koski/valpas/ValpasStaticServlet.scala
+++ src/main/scala/fi/oph/koski/valpas/ValpasStaticServlet.scala
@@ -3,17 +3,26 @@ package fi.oph.koski.valpas
 import fi.oph.koski.config.{Environment, KoskiApplication}
 import fi.oph.koski.frontendvalvonta.FrontendValvontaMode.FrontendValvontaMode
 import fi.oph.koski.frontendvalvonta.{FrontendValvontaMode, FrontendValvottuServlet}
+import fi.oph.koski.http.KoskiErrorCategory
+import fi.oph.koski.log.Logging
+import fi.oph.koski.servlet.HtmlServlet
 import org.scalatra.ScalatraServlet
 
 import scala.io.{Codec, Source}
 
-class ValpasStaticServlet(implicit val application: KoskiApplication) extends ScalatraServlet with FrontendValvottuServlet {
+class ValpasStaticServlet(implicit val application: KoskiApplication) extends ScalatraServlet with Logging with FrontendValvottuServlet {
   def allowFrameAncestors = false
   override def frontendValvontaMode: FrontendValvontaMode = FrontendValvontaMode(application.config.getString("frontend-valvonta.mode"))
 
   get("/*")(nonce => {
     contentType = "text/html"
-    Source.fromInputStream(getServletContext.getResource("/valpas/index.html.template").openStream())(Codec.UTF8).mkString.replaceAll("<%==VALPAS_CSP_NONCE==%>", nonce)
+    try {
+      Source.fromInputStream(getServletContext.getResource("/valpas/index.html.template").openStream())(Codec.UTF8).mkString.replaceAll("<%==VALPAS_CSP_NONCE==%>", nonce)
+    } catch {
+      case e: Exception =>
+        logger.error(e)("ValpasStaticServlet failure: " + e.getMessage)
+        halt(500, "Internal server error")
+    }
   })
 
 }
