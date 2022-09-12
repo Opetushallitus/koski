135c202e10254f9bf70c2c934abb9731f9a66df2
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-09-12 15:10:12 +0300

    Lisää perffitesti, jolla tarkastetaan löytyykö oppijan tiedoista 'valintatila'-arvoa

--- src/test/scala/fi/oph/koski/perftest/ValpasRandomPerusopetuksenOppijaGetter.scala
+++ src/test/scala/fi/oph/koski/perftest/ValpasRandomPerusopetuksenOppijaGetter.scala
@@ -1,5 +1,8 @@
 package fi.oph.koski.perftest
 
+import org.json4s.JField
+import org.json4s.jackson.JsonMethods
+
 object ValpasRandomPerusopetuksenOppijaGetter extends App {
   PerfTestRunner.executeTest(ValpasRandomPerusopetuksenOppijaGetterScenario)
 }
@@ -10,10 +13,20 @@ object ValpasRandomPerusopetuksenOppijaGetterScenario extends PerfTestScenario {
   def operation(x: Int) = List(Operation(uri = s"valpas/api/oppija/${oids.next}", uriPattern=Some("valpas/api/oppija/_")))
   override def bodyValidator: Boolean = {
     val bodyContainsError = this.body.contains("hakutilanneError")
-    if (bodyContainsError) {
+    val bodyContainsValintaTila = JsonMethods.parse(this.body) findField {
+      case JField("valintatila", _) => true
+      case _ => false
+    } match {
+      case Some(_) => true
+      case None => false
+    }
+    val hasError = bodyContainsError || !bodyContainsValintaTila
+
+    if (hasError) {
       logger.error(this.body)
     }
-    !bodyContainsError
+
+    !hasError
   }
 }
 
