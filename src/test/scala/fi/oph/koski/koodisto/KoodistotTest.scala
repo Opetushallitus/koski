3247e235e2eb5470425b850e6b1fd28bdaf9e339
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2022-10-13 15:30:10 +0300

    Korjaa Koodistotestiin uudet poikkeavat koodi-URIt;
    Näitä on tullut lisää luultavasti sen seurauksena, että koodiarvoja on editoitu käsin
    koodistopalvelun käyttöliittymässä sen jälkeen, kun ne on ensimmäisen kerran luotu
    Kosken lokaalien koodistotietojen perusteella.

--- src/test/scala/fi/oph/koski/koodisto/KoodistotTest.scala
+++ src/test/scala/fi/oph/koski/koodisto/KoodistotTest.scala
@@ -56,7 +56,12 @@ class KoodistotTest extends AnyFreeSpec with TestEnvironment with Matchers {
       "koskiyoarvosanat_i-2",
       "koskiyoarvosanat_i-3",
       "erityinenkoulutustehtava_ib-1",
-      "lahdejarjestelma_espoovarda"
+      "lahdejarjestelma_espoovarda",
+      "lahdejarjestelma_espoovarda-1",
+      "arviointiasteikkoib_fail",
+      "suorituksentyyppi_lukionoppiaineidenoppimaarat2019",
+      "suorituksentyyppi_lukionpaikallinenopintojakso2019",
+      "suorituksentyyppi_lukionvaltakunnallinenmoduuli2019",
     )
     // Tässä koodistossa on niin monta poikkeusta ettei erikseen luetella niitä tässä.
     val PoikkeavatKoodistot = Seq(
