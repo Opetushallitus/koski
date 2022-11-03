c71e76a4b97c1c7e32c6f72f02abdf78806d9514
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-11-03 09:01:31 +0200

    Lisää arvioitsijat ja kuvaus ESH:n alaosasuoritukseen

--- web/app/esh/EuropeanSchoolOfHelsinkiOsasuoritusEditor.jsx
+++ web/app/esh/EuropeanSchoolOfHelsinkiOsasuoritusEditor.jsx
@@ -24,6 +24,7 @@ export class EuropeanSchoolOfHelsinkiOsasuoritusEditor extends React.Component {
     const displayProperties = properties.filter(
       (p) => p.key !== 'osasuoritukset'
     )
+    console.log(displayProperties)
     const osasuoritukset = modelLookup(model, 'osasuoritukset')
     const showOsasuoritukset = osasuoritukset
       ? osasuoritukset.value !== undefined
