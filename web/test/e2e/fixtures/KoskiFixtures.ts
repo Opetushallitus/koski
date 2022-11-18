09d639d19ca2ece2e83c2d0eb9b2cf1bc432ce00
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-11-18 13:55:27 +0200

    Salli raportointikannan generoinnin ohitus Playwright-testeissä

--- web/test/e2e/fixtures/KoskiFixtures.ts
+++ web/test/e2e/fixtures/KoskiFixtures.ts
@@ -3,8 +3,13 @@ import { Page, expect } from '@playwright/test'
 export class KoskiFixtures {
   constructor(private readonly page: Page) {}
 
-  async reset() {
-    const request = await this.page.request.post('/koski/fixtures/reset')
+  async reset(reloadRaportointikanta = false) {
+    const params = new URLSearchParams({
+      reloadRaportointikanta: reloadRaportointikanta ? 'true' : 'false'
+    })
+    const request = await this.page.request.post(
+      `/koski/fixtures/reset?${params.toString()}`
+    )
     expect(request.ok()).toBeTruthy()
   }
 }
