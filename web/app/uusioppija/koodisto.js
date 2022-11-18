1de643a542897f87dd66decce1fa83ebf43e6b33
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2022-11-18 08:54:53 +0200

    Poista käyttämätön koodinpätkä

--- web/app/uusioppija/koodisto.js
+++ web/app/uusioppija/koodisto.js
@@ -1,6 +1,4 @@
-import Bacon from 'baconjs'
 import Http from '../util/http'
-import { useState, useEffect } from 'react'
 
 export const koodiarvoMatch =
   (...koodiarvot) =>
@@ -11,22 +9,3 @@ export const koodistoValues = (koodistoUri) =>
   Http.cachedGet(`/koski/api/editor/koodit/${koodistoUri}`).map((values) =>
     values.map((t) => t.data)
   )
-
-export const useKoodistovalues = (...koodistoUris) => {
-  const [values, setValues] = useState([])
-  useEffect(() => {
-    const property = Bacon.combineWith(
-      (...koodistoProperties) =>
-        koodistoProperties.reduce((prev, curr) => [...prev, ...curr], []),
-      koodistoUris.map((koodistoUri) => koodistoValues(koodistoUri))
-    )
-    const dispose = property.onValue((val) => setValues(val))
-    // eslint-disable-next-line no-console
-    const errorDispose = property.onError((err) => console.error(err))
-    return () => {
-      dispose()
-      errorDispose()
-    }
-  }, [])
-  return values
-}
