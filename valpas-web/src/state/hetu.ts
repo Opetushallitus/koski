79f2d10c860fe6bf57f174b023d7f2adf6fe6f89
Henri Ruoho <henri.ruoho@reaktor.com>
2022-12-08 13:55:49 +0200

    Lisää uudet välimerkit valppaan hetuformaattiin

--- valpas-web/src/state/hetu.ts
+++ valpas-web/src/state/hetu.ts
@@ -5,7 +5,7 @@ import { DATE_FORMAT } from "../utils/date"
 import { pluck } from "../utils/objects"
 
 export const hetulikeRegex =
-  /[0123]\d[01]\d\d\d[-+A]\d\d\d[0123456789ABCDEFHJKLMNPRSTUVWXY]/g
+  /[0123]\d[01]\d\d\d[-+ABCDEFXYWVU]\d\d\d[0123456789ABCDEFHJKLMNPRSTUVWXY]/g
 
 export const parseHetulikes = (input: string): string[] =>
   pipe(input.toUpperCase().match(hetulikeRegex) || [], uniq)
@@ -14,7 +14,7 @@ const uniq = <T>(ts: T[]): T[] => Array.from(new Set(ts))
 
 export const hetuToBirthday = (input: string = "") =>
   pipe(
-    input.toUpperCase().match(/(\d\d)(\d\d)(\d\d)([+-A]).+/),
+    input.toUpperCase().match(/(\d\d)(\d\d)(\d\d)([-+ABCDEFXYWVU]).+/),
     O.fromNullable,
     O.map((tokens) => {
       const [d, m, y] = tokens.slice(1, 4).map((n) => parseInt(n, 10))
@@ -28,7 +28,17 @@ const delimiterToCentury = (d: string): number | undefined =>
     {
       "+": 1800,
       "-": 1900,
+      X: 1900,
+      Y: 1900,
+      W: 1900,
+      V: 1900,
+      U: 1900,
       A: 2000,
+      B: 2000,
+      C: 2000,
+      D: 2000,
+      E: 2000,
+      F: 2000,
     } as Record<string, number>,
     pluck(d)
   )
