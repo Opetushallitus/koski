c55e497957ad9221e2c1940c28591bbf75481666
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-12-19 13:57:07 +0200

    Lisää testejä $class-metadatan hakemiselle oppija-rajapinnasta

---
+++ src/test/scala/fi/oph/koski/api/ClassReferenceSpec.scala
@@ -0,0 +1,30 @@
+package fi.oph.koski.api
+
+import fi.oph.koski.KoskiHttpSpec
+import fi.oph.koski.henkilo.KoskiSpecificMockOppijat
+import fi.oph.koski.http.KoskiErrorCategory
+import fi.oph.koski.koskiuser.MockUsers
+import fi.oph.koski.log.AuditLogTester
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class ClassReferenceSpec extends AnyFreeSpec with Matchers with KoskiHttpSpec with OpiskeluoikeusTestMethods {
+
+  "GET /api/oppija/:oid" - {
+    "Should show $class metadata with class_refs=true" in {
+      get("api/oppija/" + KoskiSpecificMockOppijat.eero.oid, params = List(("class_refs", "true")), headers = authHeaders()) {
+        verifyResponseStatusOk()
+        body should include("\"$class\":\"fi.oph.koski.schema.Koodistokoodiviite\"")
+        body should include("\"$class\":\"fi.oph.koski.schema.AmmatillinenOpiskeluoikeus\"")
+      }
+    }
+    "Should not show $class metadata with class_refs=false" in {
+      get("api/oppija/" + KoskiSpecificMockOppijat.eero.oid, params = List(("class_refs", "false")), headers = authHeaders()) {
+        verifyResponseStatusOk()
+        body should not include("\"$class\":\"fi.oph.koski.schema.Koodistokoodiviite\"")
+        body should not include("\"$class\":\"fi.oph.koski.schema.AmmatillinenOpiskeluoikeus\"")
+      }
+    }
+  }
+}
+
