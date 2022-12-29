896492be680ba792455755d6d86158b55d0b9f9b
Henri Ruoho <henri.ruoho@reaktor.com>
2022-12-29 12:35:35 +0200

    Lisää smoketestit migrin apille

--- src/test/scala/fi/oph/koski/migri/MigriServletSpec.scala
+++ src/test/scala/fi/oph/koski/migri/MigriServletSpec.scala
@@ -6,27 +6,29 @@ import fi.oph.koski.koskiuser.MockUsers
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.util.matching.Regex
+
 class MigriServletSpec extends AnyFreeSpec with KoskiHttpSpec with HttpSpecification with Matchers {
   "MigriServlet" - {
     "Palauttaa json-objektin hetulla" in {
       post(
         uri = "api/luovutuspalvelu/migri/hetu",
-        body = "{\"hetu\": \"170249-378D\"}",
+        body = "{\"hetu\": \"211097-402L\"}",
         headers = authHeaders(MockUsers.luovutuspalveluKäyttäjä) ++ jsonContent
       ) {
         verifyResponseStatusOk()
-        response.body should equal("{\"oids\":[\"1.2.246.562.24.51986460849\"],\"username\":\"Lasse\",\"password\":\"Lasse\"}")
+        response.body should include ("1.2.246.562.24.00000000026")
       }
     }
 
     "Palauttaa json-objektin henkilo-oidilla" in {
       post(
         uri = "api/luovutuspalvelu/migri/oid",
-        body = "{\"oids\": [\"1.2.246.562.24.51986460849\"]}",
+        body = "{\"oid\": \"1.2.246.562.24.00000000026\"}",
         headers = authHeaders(MockUsers.luovutuspalveluKäyttäjä) ++ jsonContent
       ) {
         verifyResponseStatusOk()
-        response.body should equal("{\"oids\":[\"1.2.246.562.24.51986460849\"],\"username\":\"Lasse\",\"password\":\"Lasse\"}")
+        response.body should include ("211097-402L")
       }
     }
 
