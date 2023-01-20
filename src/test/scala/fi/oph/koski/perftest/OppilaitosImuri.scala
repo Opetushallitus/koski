37c196b494e0d08db32510b5a59912cc7a01dbf7
Juho Tammela <juho.tammela@itrex.fi>
2023-01-20 17:03:22 +0200

    Suodata pois epävalidit organisaatio-oidit perffitesteissä

--- src/test/scala/fi/oph/koski/perftest/OppilaitosImuri.scala
+++ src/test/scala/fi/oph/koski/perftest/OppilaitosImuri.scala
@@ -1,10 +1,10 @@
 package fi.oph.koski.perftest
 
-import java.net.URLEncoder
-
-import fi.oph.koski.organisaatio.{OrganisaatioPalveluOrganisaatioTyyppi, OrganisaatioTyyppiHakuTulos}
-import fi.oph.koski.schema.OidOrganisaatio
+import fi.oph.koski.organisaatio.OrganisaatioTyyppiHakuTulos
+import fi.oph.koski.schema.{OidOrganisaatio, Organisaatio}
 import fi.oph.koski.util.EnvVariables
+
+import java.net.URLEncoder
 
 /**
   * Hakee eri tyyppiset oppilaitokset Opintopolun organisaatiopalvelusta.
@@ -12,14 +12,16 @@ import fi.oph.koski.util.EnvVariables
 object OppilaitosImuri extends App with EnvVariables {
   lazy val virkailijaRoot = env("VIRKAILIJA", "https://virkailija.untuvaopintopolku.fi")
 
-  lazy val lukiot: List[OidOrganisaatio] = haeOppilaitostyypillä("oppilaitostyyppi_15#1", "Peruskoulu")
-  lazy val ammatillisetOppilaitokset: List[OidOrganisaatio] = haeOppilaitostyypillä("oppilaitostyyppi_21#1", "Lukio")
-  lazy val peruskoulut: List[OidOrganisaatio] = haeOppilaitostyypillä("oppilaitostyyppi_11#1", "Ammatillinen")
+  lazy val lukiot = haeOppilaitostyypillä("oppilaitostyyppi_15#1", "Lukio")
+  lazy val ammatillisetOppilaitokset = haeOppilaitostyypillä("oppilaitostyyppi_21#1", "Ammatillinen")
+  lazy val peruskoulut = haeOppilaitostyypillä("oppilaitostyyppi_11#1", "Peruskoulu")
 
 
-  def haeOppilaitostyypillä(tyyppi: String, tyypinNimi: String) = {
+  def haeOppilaitostyypillä(tyyppi: String, tyypinNimi: String): List[OidOrganisaatio] = {
     val url: String = s"$virkailijaRoot/organisaatio-service/rest/organisaatio/v2/hae/tyyppi?aktiiviset=true&suunnitellut=true&lakkautetut=false&oppilaitostyyppi=${URLEncoder.encode(tyyppi, "UTF-8")}"
-    val organisaatiOidit = EasyHttp.getJson[OrganisaatioTyyppiHakuTulos](url).organisaatiot.map { org: OrganisaatioPalveluOrganisaatioTyyppi => OidOrganisaatio(org.oid) }
+    val organisaatiOidit = EasyHttp.getJson[OrganisaatioTyyppiHakuTulos](url).organisaatiot
+      .map(org => OidOrganisaatio(org.oid))
+      .filter(org => Organisaatio.isValidOrganisaatioOid(org.oid))
     println(tyypinNimi + " määrä: " + organisaatiOidit.length)
     organisaatiOidit
   }
