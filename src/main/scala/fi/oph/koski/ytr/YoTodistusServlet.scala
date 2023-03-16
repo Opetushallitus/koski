6602d41e745231b3fd0278122c8616cd8bfee9ed
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2023-03-16 15:24:02 +0200

    Yo-todistusten audit-logitus

--- src/main/scala/fi/oph/koski/ytr/YoTodistusServlet.scala
+++ src/main/scala/fi/oph/koski/ytr/YoTodistusServlet.scala
@@ -2,8 +2,11 @@ package fi.oph.koski.ytr
 
 import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
-import fi.oph.koski.koskiuser.RequiresKansalainen
+import fi.oph.koski.koskiuser.{KoskiSpecificSession, RequiresKansalainen}
+import fi.oph.koski.log.{AuditLog, KoskiAuditLogMessage, KoskiAuditLogMessageField}
+import fi.oph.koski.log.KoskiOperation.{KoskiOperation, YTR_YOTODISTUKSEN_LUONTI, YTR_YOTODISTUKSEN_LATAAMINEN}
 import fi.oph.koski.servlet.{KoskiSpecificApiServlet, NoCache}
+import fi.oph.koski.util.ChainingSyntax._
 
 class YoTodistusServlet(implicit val application: KoskiApplication)
   extends KoskiSpecificApiServlet
@@ -17,12 +20,17 @@ class YoTodistusServlet(implicit val application: KoskiApplication)
   }
 
   get("/generate/:lang/:oppijaOid") {
-    renderEither(getRequest.flatMap(service.initiateGenerating))
+    renderEither(
+      getRequest
+        .flatMap(service.initiateGenerating)
+        .tap(_ => mkAuditLog(session, YTR_YOTODISTUKSEN_LUONTI))
+    )
   }
 
   get("/download/:lang/:oppijaOid/:filename") {
     getRequest.flatMap(service.currentStatus) match {
       case Right(state: YtrCertificateCompleted) =>
+        mkAuditLog(session, YTR_YOTODISTUKSEN_LATAAMINEN)
         contentType = "application/pdf"
         service.download(state, response.getOutputStream)
       case _ =>
@@ -42,4 +50,7 @@ class YoTodistusServlet(implicit val application: KoskiApplication)
     } else {
       Left(KoskiErrorCategory.unauthorized())
     }
+
+  private def mkAuditLog(session: KoskiSpecificSession, operation: KoskiOperation): Unit = mkAuditLog(session.oid, operation)
+  private def mkAuditLog(oid: String, operation: KoskiOperation): Unit = AuditLog.log(KoskiAuditLogMessage(operation, session, Map(KoskiAuditLogMessageField.oppijaHenkiloOid -> oid)))
 }
