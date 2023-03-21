7605fc5a8617950fc050332fb3f86612d476421c
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2023-03-21 12:21:13 +0200

    Uudelleenyritetään auditlogien lukemista viipeen takia

--- src/test/scala/fi/oph/koski/log/AuditLogTester.scala
+++ src/test/scala/fi/oph/koski/log/AuditLogTester.scala
@@ -1,25 +1,33 @@
 package fi.oph.koski.log
 
 import fi.oph.koski.json.GenericJsonFormats
+import fi.oph.koski.log.AuditLogTester.retryingTest
+import org.json4s.Formats
 import org.json4s.JsonAST.JObject
 import org.json4s.jackson.JsonMethods.parse
 import org.scalatest.matchers.should.Matchers
+
+import scala.annotation.tailrec
 
 object AuditLogTester extends Matchers with LogTester {
   override def appenderName: String = "Audit"
 
   def verifyAuditLogMessage(params: Map[String, Any]): Unit = {
-    val message = getLogMessages.lastOption.map(m => parse(m))
-    message match {
-      case Some(msg: JObject) => verifyAuditLogMessage(msg, params)
-      case _ => throw new IllegalStateException("No audit log message found")
+    retryingTest(times = 3) { () =>
+      val message = getLogMessages.lastOption.map(m => parse(m))
+      message match {
+        case Some(msg: JObject) => verifyAuditLogMessage(msg, params)
+        case _ => throw new IllegalStateException("No audit log message found")
+      }
     }
   }
 
   def verifyAuditLogMessage(loggingEvent: String, params: Map[String, Any]): Unit = {
-    parse(loggingEvent) match {
-      case msg: JObject => verifyAuditLogMessage(msg, params)
-      case _ => throw new IllegalStateException("No audit log message found")
+    retryingTest(times = 3) { () =>
+      parse(loggingEvent) match {
+        case msg: JObject => verifyAuditLogMessage(msg, params)
+        case _ => throw new IllegalStateException("No audit log message found")
+      }
     }
   }
 
@@ -30,13 +38,27 @@ object AuditLogTester extends Matchers with LogTester {
   }
 
   private def verifyAuditLogMessage(msg: JObject, params: Map[String, Any]): Unit = {
-    implicit val formats = GenericJsonFormats.genericFormats
-    params.foreach {
-      case (key, expectedValue: String) =>
-        msg.values.get(key) should equal(Some(expectedValue))
-      case (key, newParams: Map[String, Any] @unchecked) =>
-        verifyAuditLogMessage((msg \ key).extract[JObject], newParams)
-      case _ => ???
+    implicit val formats: Formats = GenericJsonFormats.genericFormats
+    retryingTest(times = 3) { () =>
+      params.foreach {
+        case (key, expectedValue: String) =>
+          msg.values.get(key) should equal(Some(expectedValue))
+        case (key, newParams: Map[String, Any]@unchecked) =>
+          verifyAuditLogMessage((msg \ key).extract[JObject], newParams)
+        case _ => ???
+      }
+    }
+  }
+
+  private def retryingTest(times: Int)(fn: () => Unit): Unit = {
+    try {
+      fn()
+    } catch {
+      case _: Throwable if times > 1 => {
+        Thread.sleep(1000)
+        retryingTest(times - 1)(fn)
+      }
+      case t: Throwable => throw t
     }
   }
 }
