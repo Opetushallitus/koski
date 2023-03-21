f3eed477de8e1b7044e9a778fd2025d63f97b21a
Henri Ruoho <henri.ruoho@reaktor.com>
2023-03-21 20:47:48 +0200

    Korjaa aiemmin syntynyt minMonth/maxMonth bugi ja lisää testi

--- src/main/scala/fi/oph/koski/ytr/download/YtrSsnData.scala
+++ src/main/scala/fi/oph/koski/ytr/download/YtrSsnData.scala
@@ -3,15 +3,18 @@ package fi.oph.koski.ytr.download
 import fi.oph.koski.henkilo.Hetu
 import fi.oph.koski.util.DateOrdering.{localDateOptionOrdering, localDateOrdering}
 
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 
 case class YtrSsnData(
   ssns: Option[List[String]]
 ) {
-  private def formatOptionalBirthday(o: Option[String]): String = o.map(_.format(DateTimeFormatter.ofPattern("yyyy-MM"))).getOrElse("-")
-  private def minAndMaxMonth: (String, String) = ssnsSortedByBirthdays.map(xs => {
-    (formatOptionalBirthday(xs.headOption), formatOptionalBirthday(xs.lastOption))
+  private def formatBirthMonth(o: Option[LocalDate]): String = o.map(_.format(DateTimeFormatter.ofPattern("yyyy-MM"))).getOrElse("-")
+  private def minAndMaxMonth: (String, String) = ssnsSortedByBirthdays.map(ssns => {
+    val first = ssns.headOption.flatMap(Hetu.toBirthday)
+    val last = ssns.lastOption.flatMap(Hetu.toBirthday)
+    (formatBirthMonth(first), formatBirthMonth(last))
   }).getOrElse(("-", "-"))
   def minMonth: String = minAndMaxMonth._1
   def maxMonth: String = minAndMaxMonth._2
