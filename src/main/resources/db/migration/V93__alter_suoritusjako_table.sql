731e4cb5ed72cb67c3c12749bb3610a8e20c7f76
Henri Ruoho <henri.ruoho@reaktor.com>
2023-05-26 08:50:11 +0300

    Lisää default-arvot suoritusjako-tauluun

--- src/main/resources/db/migration/V93__alter_suoritusjako_table.sql
+++ src/main/resources/db/migration/V93__alter_suoritusjako_table.sql
@@ -1,3 +1,3 @@
-ALTER TABLE suoritusjako
-ADD COLUMN kokonaisuudet JSONB;
-
+ALTER TABLE suoritusjako DROP COLUMN IF EXISTS kokonaisuudet;
+ALTER TABLE suoritusjako ADD COLUMN kokonaisuudet JSONB NOT NULL DEFAULT '[]'::jsonb;
+ALTER TABLE suoritusjako ALTER COLUMN suoritus_ids SET DEFAULT '[]'::jsonb;
