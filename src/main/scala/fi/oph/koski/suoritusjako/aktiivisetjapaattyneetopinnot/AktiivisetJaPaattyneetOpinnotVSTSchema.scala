4e215618d893b0729d6957ebd4f8327848b6793f
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2023-06-09 11:57:34 +0300

    Splittaa akt.ja päätt. skeeman VST-suoritukset;
    Näin dokumentaatiosta selviää paremmin, mitä kaikkia tapauksia on olemassa ja mitä eroja niiden
    tietomalleissa on.

--- src/main/scala/fi/oph/koski/suoritusjako/aktiivisetjapaattyneetopinnot/AktiivisetJaPaattyneetOpinnotVSTSchema.scala
+++ src/main/scala/fi/oph/koski/suoritusjako/aktiivisetjapaattyneetopinnot/AktiivisetJaPaattyneetOpinnotVSTSchema.scala
@@ -2,7 +2,7 @@ package fi.oph.koski.suoritusjako.aktiivisetjapaattyneetopinnot
 
 import fi.oph.koski.schema
 import fi.oph.koski.schema.annotation.KoodistoKoodiarvo
-import fi.oph.scalaschema.annotation.Title
+import fi.oph.scalaschema.annotation.{OnlyWhen, Title}
 
 @Title("Vapaan sivistystyön opiskeluoikeus")
 case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönOpiskeluoikeus(
@@ -27,22 +27,68 @@ case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönOpiskeluoikeus(
   override def withoutSisältyyOpiskeluoikeuteen: AktiivisetJaPäättyneetOpinnotOpiskeluoikeus = this
 }
 
-@Title("Vapaan sivistystyön päätason suoritus")
-case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönPäätasonSuoritus(
-  koulutusmoduuli: AktiivisetJaPäättyneetOpinnotVapaanSivistyönKoulutus,
+trait AktiivisetJaPäättyneetOpinnotVapaanSivistystyönPäätasonSuoritus extends Suoritus
+
+@Title("Oppivelvollisille suunnattu maahanmuuttajien kotoutusmiskoulutuksen suoritus")
+case class AktiivisetJaPäättyneetOpinnotOppivelvollisilleSuunnattuMaahanmuuttajienKotoutumiskoulutuksenSuoritus(
+  koulutusmoduuli: AktiivisetJaPäättyneetOpinnotVapaanSivistystyönMaahanmuuttajienKotoutumiskoulutus,
   vahvistus: Option[Vahvistus],
   toimipiste: Option[Toimipiste],
   @KoodistoKoodiarvo("vstmaahanmuuttajienkotoutumiskoulutus")
+  tyyppi: schema.Koodistokoodiviite
+) extends AktiivisetJaPäättyneetOpinnotVapaanSivistystyönPäätasonSuoritus
+
+@Title("Oppivelvollisille suunnattu vapaan sivistyön koulutuksen suoritus")
+case class AktiivisetJaPäättyneetOpinnotOppivelvollisilleSuunnattuVapaanSivistystyönKoulutuksenSuoritus(
+  koulutusmoduuli: AktiivisetJaPäättyneetOpinnotOppivelvollisilleSuunnattuVapaanSivistystyönKoulutus,
+  vahvistus: Option[Vahvistus],
+  toimipiste: Option[Toimipiste],
   @KoodistoKoodiarvo("vstoppivelvollisillesuunnattukoulutus")
+  tyyppi: schema.Koodistokoodiviite
+) extends AktiivisetJaPäättyneetOpinnotVapaanSivistystyönPäätasonSuoritus
+
+@Title("Vapaan sivistystyön JOTPA-koulutuksen suoritus")
+case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönJotpaKoulutuksenSuoritus(
+  koulutusmoduuli: AktiivisetJaPäättyneetOpinnotVapaanSivistystyönJotpaKoulutus,
+  vahvistus: Option[Vahvistus],
+  toimipiste: Option[Toimipiste],
   @KoodistoKoodiarvo("vstjotpakoulutus")
+  tyyppi: schema.Koodistokoodiviite
+) extends AktiivisetJaPäättyneetOpinnotVapaanSivistystyönPäätasonSuoritus
+
+@Title("Vapaan sivistystyön lukutaitokoulutuksen suoritus")
+case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönLukutaitokoulutuksenSuoritus(
+  koulutusmoduuli: AktiivisetJaPäättyneetOpinnotVapaanSivistystyönLukutaitokoulutus,
+  vahvistus: Option[Vahvistus],
+  toimipiste: Option[Toimipiste],
   @KoodistoKoodiarvo("vstlukutaitokoulutus")
   tyyppi: schema.Koodistokoodiviite
-) extends Suoritus
+) extends AktiivisetJaPäättyneetOpinnotVapaanSivistystyönPäätasonSuoritus
 
-@Title("Vapaan sivistyön koulutus")
-case class AktiivisetJaPäättyneetOpinnotVapaanSivistyönKoulutus(
+@Title("Vapaan sivistystyön maahanmuuttajien kotoutusmiskoulutus")
+case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönMaahanmuuttajienKotoutumiskoulutus(
   tunniste: AktiivisetJaPäättyneetOpinnotKoodistokoodiviite,
   perusteenDiaarinumero: Option[String],
   koulutustyyppi: Option[AktiivisetJaPäättyneetOpinnotKoodistokoodiviite],
-  opintokokonaisuus: Option[AktiivisetJaPäättyneetOpinnotKoodistokoodiviite],
+) extends SuorituksenKooditettuKoulutusmoduuli
+
+@Title("Oppivelvollisille suunnattu vapaan sivistystyön koulutus")
+case class AktiivisetJaPäättyneetOpinnotOppivelvollisilleSuunnattuVapaanSivistystyönKoulutus(
+  tunniste: AktiivisetJaPäättyneetOpinnotKoodistokoodiviite,
+  perusteenDiaarinumero: Option[String],
+  koulutustyyppi: Option[AktiivisetJaPäättyneetOpinnotKoodistokoodiviite],
+) extends SuorituksenKooditettuKoulutusmoduuli
+
+@Title("Vapaan sivistystyön JOTPA-koulutus")
+case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönJotpaKoulutus(
+  tunniste: AktiivisetJaPäättyneetOpinnotKoodistokoodiviite,
+  koulutustyyppi: Option[AktiivisetJaPäättyneetOpinnotKoodistokoodiviite],
+  opintokokonaisuus: AktiivisetJaPäättyneetOpinnotKoodistokoodiviite,
+) extends SuorituksenKooditettuKoulutusmoduuli
+
+@Title("Vapaan sivistystyön lukutaitokoulutus")
+case class AktiivisetJaPäättyneetOpinnotVapaanSivistystyönLukutaitokoulutus(
+  tunniste: AktiivisetJaPäättyneetOpinnotKoodistokoodiviite,
+  perusteenDiaarinumero: Option[String],
+  koulutustyyppi: Option[AktiivisetJaPäättyneetOpinnotKoodistokoodiviite],
 ) extends SuorituksenKooditettuKoulutusmoduuli
