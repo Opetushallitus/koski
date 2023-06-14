78d1e40ed5df4be4a86d3fdb521f7d08f6852f9d
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2023-06-14 12:47:45 +0300

    Siisti linkkijakojen sisältökäyttöliittymää;
    Näytä Haetaan-teksti, kun tietoja vielä haetaan, ja virheilmoitus vain virheen tapahduttua.
    
    Näytä aina jokin otsikko, vaikka oppilaitos ja koulutustoimija puuttuisivat.
    
    Palauta useamman suorituksen tiedot samassa opiskeluoikeudessa pilkutetulla listalla.

--- web/app/SuoritetutTutkinnot.tsx
+++ web/app/SuoritetutTutkinnot.tsx
@@ -1,18 +1,23 @@
-import { isSuccess, useApiWithParams } from './api-fetch'
+import {
+  mapInitial,
+  mapLoading,
+  mapSuccess,
+  mapError,
+  useApiWithParams
+} from './api-fetch'
 import { fetchSuoritetutTutkinnot } from './util/koskiApi'
 import * as R from 'ramda'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { isSuoritetutTutkinnotOppija } from './types/fi/oph/koski/suoritusjako/suoritetuttutkinnot/SuoritetutTutkinnotOppija'
 import { SuoritusjakoTopBar } from './components-v2/layout/SuoritusjakoTopBar'
-import { t } from './i18n/i18n'
 import { ContentContainer } from './components-v2/containers/ContentContainer'
 import { PlainList } from './components-v2/containers/PlainList'
-import { isSuoritetutTutkinnotAmmatillinenOpiskeluoikeus } from './types/fi/oph/koski/suoritusjako/suoritetuttutkinnot/SuoritetutTutkinnotAmmatillinenOpiskeluoikeus'
 import { Trans } from './components-v2/texts/Trans'
 import { Flex } from './components-v2/containers/Flex'
 import { isSuoritetutTutkinnotOpiskeluoikeus } from './types/fi/oph/koski/suoritusjako/suoritetuttutkinnot/SuoritetutTutkinnotOpiskeluoikeus'
 import { isSuoritetutTutkinnotKoskeenTallennettavaOpiskeluoikeus } from './types/fi/oph/koski/suoritusjako/suoritetuttutkinnot/SuoritetutTutkinnotKoskeenTallennettavaOpiskeluoikeus'
+import Text from './i18n/Text'
+import { SuoritetutTutkinnotOppija } from './types/fi/oph/koski/suoritusjako/suoritetuttutkinnot/SuoritetutTutkinnotOppija'
 // @ts-ignore
 __webpack_nonce__ = window.nonce
 // @ts-ignore
@@ -24,88 +29,115 @@ interface SuoritetutTutkinnotProps {
 }
 const SuoritetutTutkinnot = ({ testId }: SuoritetutTutkinnotProps) => {
   const response = useApiWithParams(fetchSuoritetutTutkinnot, [secret])
-  return isSuccess(response) && isSuoritetutTutkinnotOppija(response.data) ? (
+  return (
     <>
-      <SuoritusjakoTopBar />
-      <ContentContainer
-        className="content-area suoritusjako suoritusjako-page"
-        testId={testId}
-      >
-        <div className="main-content">
-          <h1>
-            <Trans>{'Suoritetut tutkinnot'}</Trans>
-          </h1>
-          <a
-            className="text-button-small"
-            target="_blank"
-            href={`/koski/api/opinnot/suoritetut-tutkinnot/${secret}`}
-            rel="noopener noreferrer"
+      {mapInitial(response, () => (
+        <Trans>{'Haetaan'}</Trans>
+      ))}
+      {mapLoading(response, () => (
+        <Trans>{'Haetaan'}</Trans>
+      ))}
+      {mapError(response, () => (
+        <Trans>{'Tietojen hakeminen epäonnistui'}</Trans>
+      ))}
+      {mapSuccess(response, (responseData: SuoritetutTutkinnotOppija) => (
+        <>
+          <SuoritusjakoTopBar />
+          <ContentContainer
+            className="content-area suoritusjako suoritusjako-page"
+            testId={testId}
           >
-            <Trans>{'Tiedot koneluettavassa muodossa'}</Trans>
-          </a>
-          <div>
-            <div className="header__name">
-              <p className="textstyle-like-h2">
-                {response.data.henkilö.etunimet}{' '}
-                {response.data.henkilö.sukunimi}
-              </p>
-            </div>
-          </div>
-
-          <div></div>
-
-          <div>
-            <p>
-              <Trans>
-                {
-                  'Tässä listataan suoritetut tutkinnot otsikkotasolla. Tarkemmat tiedot löytyvät koneluettavassa muodossa yllä olevasta linkistä'
-                }
-              </Trans>
-            </p>
-            <PlainList>
-              {response.data.opiskeluoikeudet.map((oo, index) => (
-                <div
-                  key={oo.$class + '-' + index}
-                  className={'suoritettu-tutkinto'}
-                >
-                  <h3>
-                    <Trans>{oo.oppilaitos?.nimi}</Trans>
-                  </h3>
-                  <Flex>
-                    <span>
-                      <Trans>{oo.tyyppi.nimi}</Trans>
-                      {': '}
-                      {oo.suoritukset.map((s) => (
-                        <Trans>{s.koulutusmoduuli.tunniste.nimi}</Trans>
-                      ))}
-                    </span>
-                    <span>
-                      {isSuoritetutTutkinnotKoskeenTallennettavaOpiskeluoikeus(
-                        oo
-                      )
-                        ? `Opiskeluoikeuden oid: ${oo.oid}`
-                        : ''}
-                    </span>
-                    <span>
-                      {isSuoritetutTutkinnotOpiskeluoikeus(oo)
-                        ? `Vahvistettu: ${oo.suoritukset.map(
-                            (foo) => foo.vahvistus?.päivä
-                          )}`
-                        : ''}
-                    </span>
-                  </Flex>
+            <div className="main-content">
+              <h1>
+                <Trans>{'Suoritetut tutkinnot'}</Trans>
+              </h1>
+              <a
+                className="text-button-small"
+                target="_blank"
+                href={`/koski/api/opinnot/suoritetut-tutkinnot/${secret}`}
+                rel="noopener noreferrer"
+              >
+                <Trans>{'Tiedot koneluettavassa muodossa'}</Trans>
+              </a>
+              <div>
+                <div className="header__name">
+                  <p className="textstyle-like-h2">
+                    {responseData.henkilö.etunimet}{' '}
+                    {responseData.henkilö.sukunimi}
+                  </p>
                 </div>
-              ))}
-            </PlainList>
-            {response.data.opiskeluoikeudet.length <= 0 && (
-              <Trans>{'Suoritettuja tutkintoja ei löytynyt'}</Trans>
-            )}
-          </div>
-        </div>
-      </ContentContainer>
+              </div>
+
+              <div></div>
+
+              <div>
+                <p>
+                  <Trans>
+                    {
+                      'Tässä listataan suoritetut tutkinnot otsikkotasolla. Tarkemmat tiedot löytyvät koneluettavassa muodossa yllä olevasta linkistä'
+                    }
+                  </Trans>
+                </p>
+                <PlainList>
+                  {responseData.opiskeluoikeudet.map((oo, index) => (
+                    <div
+                      key={oo.$class + '-' + index}
+                      className={'suoritettu-tutkinto'}
+                    >
+                      <h3>
+                        {oo.oppilaitos ? (
+                          <Trans>{oo.oppilaitos.nimi}</Trans>
+                        ) : oo.koulutustoimija ? (
+                          <Trans>{oo.koulutustoimija.nimi}</Trans>
+                        ) : (
+                          <>
+                            {/* @ts-expect-error Text */}
+                            <Text
+                              name={
+                                'Oppilaitos tai koulutustoimija ei tiedossa'
+                              }
+                            />
+                          </>
+                        )}
+                      </h3>{' '}
+                      <Flex>
+                        <span>
+                          <Trans>{oo.tyyppi.nimi}</Trans>
+                          {': '}
+                          {oo.suoritukset.map((s, i) => (
+                            <>
+                              {i > 0 && ', '}
+                              <Trans>{s.koulutusmoduuli.tunniste.nimi}</Trans>
+                            </>
+                          ))}
+                        </span>
+                        <span>
+                          {isSuoritetutTutkinnotKoskeenTallennettavaOpiskeluoikeus(
+                            oo
+                          )
+                            ? `Opiskeluoikeuden oid: ${oo.oid}`
+                            : ''}
+                        </span>
+                        <span>
+                          {isSuoritetutTutkinnotOpiskeluoikeus(oo)
+                            ? `Vahvistettu: ${oo.suoritukset.map(
+                                (foo) => foo.vahvistus?.päivä
+                              )}`
+                            : ''}
+                        </span>
+                      </Flex>
+                    </div>
+                  ))}
+                </PlainList>
+                {responseData.opiskeluoikeudet.length <= 0 && (
+                  <Trans>{'Suoritettuja tutkintoja ei löytynyt'}</Trans>
+                )}
+              </div>
+            </div>
+          </ContentContainer>
+        </>
+      ))}
     </>
-  ) : (
-    <Trans>{'Tietojen hakeminen epäonnistui'}</Trans>
   )
 }
 
