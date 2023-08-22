91f81cd29c4ded951ef481bfc2729feabbe892a9
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2023-08-22 17:39:15 +0300

    WIP: lisää filehogs yritys

---
+++ scripts/filehogs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Filehogs:"
+for x in `ps -eF| awk '{ print $2 }'`;do echo `ls /proc/$x/fd 2> /dev/null | wc -l` $x `cat /proc/$x/cmdline 2> /dev/null`;done 2> /dev/null | sort -n -r | head -n 10
