4340cf62da9bba931d8b51e9da99cca0e58b7128
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2023-09-13 11:44:38 +0300

    Palauta feature flägin käyttö VST:n uudessa kälissä

--- web/app/components-v2/interoperability/uiAdapters.ts
+++ web/app/components-v2/interoperability/uiAdapters.ts
@@ -3,8 +3,18 @@ import { VSTEditor } from '../../vst/VSTEditor'
 import { AdaptedOpiskeluoikeusEditorCollection } from './useUiAdapter'
 
 const editors: AdaptedOpiskeluoikeusEditorCollection = {
-  taiteenperusopetus: TaiteenPerusopetusEditor,
-  vapaansivistystyonkoulutus: VSTEditor
+  taiteenperusopetus: TaiteenPerusopetusEditor
+}
+
+// VST:n uusi käyttöliittymä - otetaan käyttöön feature flägin avulla
+const url = new URL(window.location.href)
+const useNewVST = url.searchParams.has('newVSTUI')
+if (useNewVST) {
+  console.warn(`WARNING: Using new user interface for VST.
+  Stuff might be broken.
+  To unset, remove the "newVSTUI" query parameter from your URL.
+  `)
+  editors['vapaansivistystyonkoulutus'] = VSTEditor
 }
 
 export const opiskeluoikeusEditors: AdaptedOpiskeluoikeusEditorCollection =
