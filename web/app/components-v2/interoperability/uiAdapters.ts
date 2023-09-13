5166c2d2fe198c797d561fdaecd9353310011fac
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2023-09-13 10:13:45 +0300

    Käytä oletuksena uutta VST:n käyttöliittymää

--- web/app/components-v2/interoperability/uiAdapters.ts
+++ web/app/components-v2/interoperability/uiAdapters.ts
@@ -3,21 +3,8 @@ import { VSTEditor } from '../../vst/VSTEditor'
 import { AdaptedOpiskeluoikeusEditorCollection } from './useUiAdapter'
 
 const editors: AdaptedOpiskeluoikeusEditorCollection = {
-  taiteenperusopetus: TaiteenPerusopetusEditor
-}
-
-// VST:n uusi käyttöliittymä - otetaan käyttöön feature flägin avulla
-const url = new URL(window.location.href)
-const useNewVST = url.searchParams.has('newVSTUI')
-if (useNewVST) {
-  console.warn(`WARNING: Using new user interface for VST.
-
-  Stuff might be broken.
-
-
-  To unset, remove the "newVSTUI" query parameter from your URL.
-  `)
-  editors['vapaansivistystyonkoulutus'] = VSTEditor
+  taiteenperusopetus: TaiteenPerusopetusEditor,
+  vapaansivistystyonkoulutus: VSTEditor
 }
 
 export const opiskeluoikeusEditors: AdaptedOpiskeluoikeusEditorCollection =
