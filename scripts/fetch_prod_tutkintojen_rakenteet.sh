ecae613742f0e17b5b767876230825578bfabf32
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2023-09-27 09:31:47 +0300

    Lisää varoitukset skriptiin

--- scripts/fetch_prod_tutkintojen_rakenteet.sh
+++ scripts/fetch_prod_tutkintojen_rakenteet.sh
@@ -9,16 +9,25 @@ RESOURCES_DIR="$ROOT_DIR/src/main/resources"
 NAME_PATTERN=${1:-rakenne-*.json}
 RAKENTEET_PATH="$RESOURCES_DIR/mockdata/eperusteet/$NAME_PATTERN"
 
+function validaatio() {
+    jq -r '{ "voimassaoloAlkaa": .voimassaoloAlkaa, "voimassaoloLoppuu": .voimassaoloLoppuu, "siirtymaPaattyy": .siirtymaPaattyy, "diaarinumero": .diaarinumero }' < "$1"
+}
+
 for RAKENNE in $RAKENTEET_PATH; do
     BASENAME=$(basename "$RAKENNE")
+    git restore "$RAKENNE"
+    
     DIAARINUMERO=$(jq -r .diaarinumero < "$RAKENNE")
+
     if [ "$DIAARINUMERO" != "null" ]; then
-      ID=$(jq -r .id < "$RAKENNE")
-      DATA=$(curl -s "https://eperusteet.opintopolku.fi/eperusteet-service/api/external/peruste/$ID" | jq .)
-      KOODI=$(echo "$DATA" | jq -r .koodi)
-      if [ "$KOODI" != "null" ]; then
+        ID=$(jq -r .id < "$RAKENNE")
+        ALKUPERAINEN=$(validaatio "$RAKENNE")
+
+        DATA=$(curl -s "https://eperusteet.opintopolku.fi/eperusteet-service/api/external/peruste/$ID" | jq .)
+        KOODI=$(echo "$DATA" | jq -r .koodi)
+        if [ "$KOODI" != "null" ]; then
         echo "Skip $BASENAME: Tunnisteella $ID ei löytynyt rakennetta"
-      else
+        else
         echo "OK   $BASENAME: $DIAARINUMERO (id: $ID)"
         PATCH="$RAKENNE.patch"
         if [ -f "$PATCH" ]; then
@@ -27,7 +36,19 @@ for RAKENNE in $RAKENTEET_PATH; do
         else
             echo "$DATA" > "$RAKENNE"
         fi
-      fi
+
+        UUSI=$(validaatio "$RAKENNE")
+        
+        if [ "$ALKUPERAINEN" != "$UUSI" ]; then
+            echo "     VAROITUS: Testien kannalta mahdollisesti oleellisia kenttiä muuttui:"
+            echo ""
+            echo "     Alkuperäinen: $ALKUPERAINEN"
+            echo "     Uusi: $UUSI"
+            echo ""
+        fi
+
+    fi
+
     else
       echo "Skip $BASENAME: Ei diaarinumeroa"
     fi
