2eb7b53f93777d35c87bfe7f188c04ec6e46942c
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2023-11-06 09:16:47 +0200

    Lisää puuttuva indeksiavain

--- web/app/components-v2/forms/FormListField.tsx
+++ web/app/components-v2/forms/FormListField.tsx
@@ -60,6 +60,7 @@ export const FormListField = <
             errorsFromPath={errorsFromPath}
             testId={testId}
             index={index}
+            key={index}
           />
         )
       })}
