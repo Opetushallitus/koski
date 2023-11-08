e4974e1ba9cc31f726d3da725e731cdf8ac2224f
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2023-11-08 10:49:00 +0200

    Siivoa peruste-editoria

--- web/app/components-v2/opiskeluoikeus/PerusteField.tsx
+++ web/app/components-v2/opiskeluoikeus/PerusteField.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { Peruste, usePeruste } from '../../appstate/peruste'
+import React from 'react'
+import { usePeruste } from '../../appstate/peruste'
 import { t } from '../../i18n/i18n'
-import { CommonProps, testId } from '../CommonProps'
+import { CommonProps } from '../CommonProps'
 import { OptionList, Select } from '../controls/Select'
-import { FieldViewerProps, FieldEditorProps } from '../forms/FormField'
+import { FieldEditorProps, FieldViewerProps } from '../forms/FormField'
 
 type PerusteViewProps = CommonProps<FieldViewerProps<string | undefined, {}>>
 
@@ -20,34 +20,6 @@ type PerusteEditProps = CommonProps<
     }
   >
 >
-
-function useTutkinnonPerusteet(diaariNumero: string) {
-  const [perusteet, setPerusteet] = useState<Peruste[]>([])
-  useEffect(() => {
-    const ab = new AbortController()
-    // TODO: Selvitä, onko fetchin käyttö ok kun datan hakemiselle löytyy myös hookkeja
-    fetch(
-      `/koski/api/tutkinnonperusteet/diaarinumerot/suorituksentyyppi/${diaariNumero}`,
-      {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json'
-        },
-        signal: ab.signal
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setPerusteet(data)
-      })
-      .catch(console.error)
-    return () => {
-      ab.abort('Component unmounted')
-    }
-  }, [diaariNumero])
-
-  return perusteet
-}
 
 export const PerusteEdit: React.FC<PerusteEditProps> = (props) => {
   const perusteet = usePeruste(props.diaariNumero)
