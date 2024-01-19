350929615a8034d22fdd764d18f2a2d5e8ee4f7d
Henri Ruoho <henri.ruoho@reaktor.com>
2024-01-19 11:17:04 +0200

    Siirrä ErityisenTuenPäätös omaan tiedostoonsa
    Selvästi käytetään laajemmin kuin vain nuorten perusopetuksessa

--- src/main/scala/fi/oph/koski/schema/NuortenPerusopetus.scala
+++ src/main/scala/fi/oph/koski/schema/NuortenPerusopetus.scala
@@ -207,37 +207,6 @@ case class TuvaErityisenTuenPäätös(
   loppu: Option[LocalDate],
 ) extends MahdollisestiAlkupäivällinenJakso
 
-@Description("Oppivelvollisen erityisen tuen päätöstiedot")
-case class ErityisenTuenPäätös(
-  @Description("Jakson alkamispäivämäärä. Muoto YYYY-MM-DD")
-  alku: Option[LocalDate],
-  @Description("Jakson loppumispäivämäärä. Muoto YYYY-MM-DD")
-  loppu: Option[LocalDate],
-  @Description("""Oppilas opiskelee toiminta-alueittain (true/false).
-Toiminta-alueittain opiskelussa oppilaalla on yksilöllistetty oppimäärä ja opetus järjestetty toiminta-alueittain.
-Tuolloin oppilaalla on aina erityisen tuen päätös.
-Oppilaan opetussuunnitelmaan kuuluvat toiminta-alueet ovat motoriset taidot, kieli ja kommunikaatio, sosiaaliset taidot, päivittäisten toimintojen taidot ja kognitiiviset taidot.
-Huom: toiminta-alue arviointeineen on kuvattu oppiaineen suorituksessa.""")
-  @Tooltip("Opiskeleeko oppilas toiminta-alueittain? Toiminta-alueittain opiskelussa oppilaalla on yksilöllistetty oppimäärä ja opetus järjestetty toiminta-alueittain. Tuolloin oppilaalla on aina erityisen tuen päätös. Oppilaan opetussuunnitelmaan kuuluvat toiminta-alueet ovat motoriset taidot, kieli ja kommunikaatio, sosiaaliset taidot, päivittäisten toimintojen taidot ja kognitiiviset taidot.")
-  @Title("Opiskelee toiminta-alueittain")
-  opiskeleeToimintaAlueittain: Boolean = false,
-  @Deprecated("Ei käytössä 19.1.2024 eteenpäin")
-  @Description("Tätä tietoa ei tarvitse jatkossa välittää. Tieto erityisryhmän toteutuspaikasta välitetään toteutuspaikka-rakenteessa.")
-  @Tooltip("Suorittaako erityisoppilas koulutusta omassa erityisryhmässään vai inklusiivisesti opetuksen mukana.")
-  @OksaUri("tmpOKSAID444", "opetusryhmä")
-  @Title("Opiskelee erityisryhmässä")
-  erityisryhmässä: Option[Boolean],
-  @Description("Tieto erityisopetuksen toteutuspaikasta välitetään tämän rakenteen koodiston mukaisesti.")
-  @Tooltip("Tieto erityisopetuksen toteutuspaikasta.")
-  @KoodistoUri("erityisopetuksentoteutuspaikka")
-  @Title("Erityisopetuksen toteutuspaikka")
-  @SensitiveData(Set(Rooli.LUOTTAMUKSELLINEN_KAIKKI_TIEDOT))
-  @RedundantData
-  toteutuspaikka: Option[Koodistokoodiviite] = None,
-  @RedundantData
-  tukimuodot: Option[List[Koodistokoodiviite]] = None
-) extends MahdollisestiAlkupäivällinenJakso with Tukimuodollinen
-
 case class TehostetunTuenPäätös(
   alku: LocalDate,
   loppu: Option[LocalDate],
