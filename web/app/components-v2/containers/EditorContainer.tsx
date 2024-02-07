b22ca8b3b922f76b191123e99bab4d1adb7829cc
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2024-02-07 09:34:10 +0200

    Lisätiedot vst-näkymiin

--- web/app/components-v2/containers/EditorContainer.tsx
+++ web/app/components-v2/containers/EditorContainer.tsx
@@ -170,7 +170,7 @@ export const EditorContainer = <T extends Opiskeluoikeus>(
           view={OrganisaatiohistoriaView}
         />
         <Spacer />
-        {LisätiedotContainer !== undefined && lisatiedotOpen && (
+        {LisätiedotContainer !== undefined && (
           <>
             <FlatButton
               onClick={(e) => {
@@ -182,8 +182,7 @@ export const EditorContainer = <T extends Opiskeluoikeus>(
                 ? 'lisatiedot:sulje_lisatiedot'
                 : 'lisatiedot:nayta_lisatiedot'}
             </FlatButton>
-            <Spacer />
-            <LisätiedotContainer form={props.form} />
+            {lisatiedotOpen && <LisätiedotContainer form={props.form} />}
             <Spacer />
           </>
         )}
