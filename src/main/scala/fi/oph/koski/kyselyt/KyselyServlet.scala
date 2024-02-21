c6688572450ea2c06e4a28e6fbd9f0a61d356713
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2024-02-21 11:50:46 +0200

    Palauta rajapinnasta vain halutut tiedot

--- src/main/scala/fi/oph/koski/kyselyt/KyselyServlet.scala
+++ src/main/scala/fi/oph/koski/kyselyt/KyselyServlet.scala
@@ -4,14 +4,18 @@ import fi.oph.koski.config.KoskiApplication
 import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
 import fi.oph.koski.koskiuser.RequiresVirkailijaOrPalvelukäyttäjä
 import fi.oph.koski.schema.KoskiSchema.strictDeserialization
+import fi.oph.koski.schema.annotation.EnumValues
 import fi.oph.koski.servlet.{KoskiSpecificApiServlet, NoCache}
 import fi.oph.koski.util.UuidUtils
 import org.json4s.jackson.JsonMethods
+
+import java.time.LocalDateTime
 
 class KyselyServlet(implicit val application: KoskiApplication)
   extends KoskiSpecificApiServlet with RequiresVirkailijaOrPalvelukäyttäjä with JsonMethods with NoCache
 {
   val kyselyt: KyselyService = application.kyselyService
+  val rootUrl: String = application.config.getString("koski.root.url")
 
   post("/") {
     withJsonBody { body =>
@@ -20,6 +24,7 @@ class KyselyServlet(implicit val application: KoskiApplication)
           .validatingAndResolvingExtractor
           .extract[QueryParameters](strictDeserialization)(body)
           .flatMap(kyselyt.add)
+          .map(q => QueryResponse(rootUrl, q).withResponseUrl(rootUrl))
       }
     } (parseErrorHandler = jsonErrorHandler)
   }
@@ -29,6 +34,7 @@ class KyselyServlet(implicit val application: KoskiApplication)
       UuidUtils.optionFromString(getStringParam("id"))
         .toRight(KoskiErrorCategory.badRequest.queryParam("Epävalidi tunniste"))
         .flatMap(kyselyt.get)
+        .map(q => QueryResponse(rootUrl, q))
     }
   }
 
@@ -48,5 +54,34 @@ class KyselyServlet(implicit val application: KoskiApplication)
 
   private def jsonErrorHandler(status: HttpStatus) = {
     haltWithStatus(status)
+  }
+}
+
+case class QueryResponse(
+  queryId: String,
+  @EnumValues(QueryState.*)
+  status: String,
+  requestedBy: String,
+  query: QueryParameters,
+  creationTime: LocalDateTime,
+  workStartTime: Option[LocalDateTime] = None,
+  endTime: Option[LocalDateTime] = None,
+  resultsApi: Option[String] = None,
+  files: Option[List[String]] = None,
+) {
+  def withResponseUrl(rootUrl: String): QueryResponse = copy(
+    resultsApi = Some(s"$rootUrl/api/kyselyt/${queryId}")
+  )
+  def withFiles(rootUrl: String, query: CompleteQuery): QueryResponse = copy(
+    files = Some(query.resultFiles.map(name => s"$rootUrl/api/kyselyt/${query.queryId}/$name"))
+  )
+}
+
+object QueryResponse {
+  def apply(rootUrl: String, query: Query): QueryResponse = query match {
+    case q: PendingQuery => QueryResponse(q.queryId, "pending", q.requestedBy, q.query, q.creationTime)
+    case q: RunningQuery => QueryResponse(q.queryId, "running", q.requestedBy, q.query, q.creationTime, Some(q.workStartTime))
+    case q: FailedQuery => QueryResponse(q.queryId, "failed", q.requestedBy, q.query, q.creationTime, Some(q.workStartTime), Some(q.endTime))
+    case q: CompleteQuery => QueryResponse(q.queryId, "complete", q.requestedBy, q.query, q.creationTime, Some(q.workStartTime), Some(q.endTime)).withFiles(rootUrl, q)
   }
 }
