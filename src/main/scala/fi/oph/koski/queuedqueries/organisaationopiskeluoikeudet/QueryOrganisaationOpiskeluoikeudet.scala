40c5f748b2e68be1e3a6d4c2c67ec904175c2ec3
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2024-02-29 12:43:22 +0200

    Refaktoroi csv- ja json-kyselyiden yhteinen koodi kantaluokkaan

---
+++ src/main/scala/fi/oph/koski/queuedqueries/organisaationopiskeluoikeudet/QueryOrganisaationOpiskeluoikeudet.scala
@@ -0,0 +1,133 @@
+package fi.oph.koski.queuedqueries.organisaationopiskeluoikeudet
+
+import fi.oph.koski.config.KoskiApplication
+import fi.oph.koski.db.PostgresDriverWithJsonSupport.api.actionBasedSQLInterpolation
+import fi.oph.koski.db.PostgresDriverWithJsonSupport.plainAPI._
+import fi.oph.koski.db._
+import fi.oph.koski.henkilo.LaajatOppijaHenkilöTiedot
+import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
+import fi.oph.koski.koskiuser.{AccessType, KoskiSpecificSession}
+import fi.oph.koski.log.KoskiAuditLogMessageField.hakuEhto
+import fi.oph.koski.log.KoskiOperation.OPISKELUOIKEUS_HAKU
+import fi.oph.koski.log.{AuditLog, KoskiAuditLogMessage, Logging}
+import fi.oph.koski.opiskeluoikeus.OpiskeluoikeusQueryContext
+import fi.oph.koski.queuedqueries.{QueryParameters, QueryResultWriter}
+import fi.oph.koski.schema.{KoskeenTallennettavaOpiskeluoikeus, KoskiSchema, Opiskeluoikeus, Organisaatio}
+import fi.oph.koski.util.ChainingSyntax.chainingOps
+import org.json4s.JValue
+import slick.jdbc.SQLActionBuilder
+
+import java.sql.Timestamp
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import scala.util.Try
+
+trait QueryOrganisaationOpiskeluoikeudet extends QueryParameters with DatabaseConverters with Logging {
+  def organisaatioOid: Option[String]
+  def alkamispaiva: LocalDate
+  def tila: Option[String] // TODO: Lisää sallitut arvot
+  def koulutusmuoto: Option[String] // TODO: Lisää sallitut arvot
+  def suoritustyyppi: Option[String] // TODO: Lisää sallitut arvot
+
+  def fetchData(application: KoskiApplication, writer: QueryResultWriter, oppilaitosOids: List[Organisaatio.Oid]): Try[Unit]
+
+  def run(application: KoskiApplication, writer: QueryResultWriter)(implicit user: KoskiSpecificSession): Either[String, Unit] = {
+    val oppilaitosOids = application.organisaatioService.organisaationAlaisetOrganisaatiot(organisaatioOid.get)
+    fetchData(
+      application = application,
+      writer = writer,
+      oppilaitosOids = oppilaitosOids,
+    ).toEither
+      .tap(_ => auditLog())
+      .left.map { error =>
+        logger.error(error)("Query failed")
+        error.toString
+      }
+  }
+
+  def queryAllowed(application: KoskiApplication)(implicit user: KoskiSpecificSession): Boolean =
+    user.hasGlobalReadAccess || (
+      organisaatioOid.exists(user.organisationOids(AccessType.read).contains)
+        && koulutusmuoto.forall(user.allowedOpiskeluoikeusTyypit.contains)
+      )
+
+  override def withDefaults(implicit user: KoskiSpecificSession): Either[HttpStatus, QueryOrganisaationOpiskeluoikeudet] = {
+    import mojave._
+    if (organisaatioOid.isEmpty) {
+      defaultOrganisaatio.map { oid =>
+        shapeless
+          .lens[QueryOrganisaationOpiskeluoikeudet]
+          .field[Option[String]]("organisaatioOid")
+          .set(this)(Some(oid))
+      }
+    } else {
+      Right(this)
+    }
+  }
+
+  protected def defaultOrganisaatio(implicit user: KoskiSpecificSession) = {
+    val organisaatiot = user.juuriOrganisaatiot
+    if (organisaatiot.isEmpty) {
+      Left(KoskiErrorCategory.unauthorized("Käyttäjäoikeuksissa ei ole määritelty eksplisiittisesti lukuoikeutta yhdenkään tietyn organisaation tietoihin.")) // Mahdollista esim. pääkäyttäjän tunnuksilla
+    } else if (organisaatiot.size > 1) {
+      Left(KoskiErrorCategory.unauthorized("Kenttää `organisaatioOid` ei ole annettu, eikä organisaatiota voi yksiselitteisesti päätellä käyttöoikeuksista."))
+    } else {
+      Right(user.juuriOrganisaatiot.head.oid)
+    }
+  }
+
+  protected def auditLog()(implicit user: KoskiSpecificSession): Unit = {
+    AuditLog.log(KoskiAuditLogMessage(
+      OPISKELUOIKEUS_HAKU,
+      user,
+      Map(hakuEhto -> OpiskeluoikeusQueryContext.queryForAuditLog(Map(
+        "organisaatio" -> List(organisaatioOid.get),
+        "opiskeluoikeusAlkanutAikaisintaan" -> List(alkamispaiva.format(DateTimeFormatter.ISO_DATE)),
+        "opiskeluoikeudenTila" -> tila.toList,
+        "suorituksenTyyppi" -> suoritustyyppi.toList,
+        "koulutusmuoto" -> koulutusmuoto.toList,
+      ).filter(_._2.nonEmpty))),
+    ))
+  }
+
+  protected def getDb(application: KoskiApplication): DB = application.replicaDatabase.db
+
+  protected def defaultBaseFilter(oppilaitosOids: List[Organisaatio.Oid]) = SQLHelpers.concatMany(
+    Some(sql"WHERE NOT poistettu AND NOT mitatoity AND oppilaitos_oid = ANY($oppilaitosOids) AND alkamispaiva >= $alkamispaiva "),
+    tila.map(t => sql" AND tila = $t "),
+    koulutusmuoto.map(t => sql"AND koulutusmuoto = $t "),
+    suoritustyyppi.map(t => sql"AND $t IN suoritustyypit "),
+  )
+
+  protected def getOppijaOids(db: DB, filters: SQLActionBuilder): Seq[String] = QueryMethods.runDbSync(
+    db,
+    SQLHelpers.concat(sql"SELECT DISTINCT oppija_oid FROM opiskeluoikeus ", filters).as[(String)]
+  )
+
+  protected def forEachOpiskeluoikeus(
+    application: KoskiApplication,
+    filters: SQLActionBuilder,
+    oppijaOids: Seq[String])(f: (LaajatOppijaHenkilöTiedot, Seq[KoskeenTallennettavaOpiskeluoikeus]) => Unit,
+  ): Unit =
+    oppijaOids.foreach {
+      application.henkilöRepository.findByOid(_, findMasterIfSlaveOid = true).map { henkilö =>
+        val henkilöFilter = SQLHelpers.concat(filters, sql"AND oppija_oid = ANY(${henkilö.kaikkiOidit})")
+        val opiskeluoikeudet = QueryMethods.runDbSync(
+          getDb(application),
+          SQLHelpers.concat(sql"SELECT data, oid, versionumero, aikaleima FROM opiskeluoikeus ", henkilöFilter).as[(JValue, String, Int, Timestamp)]
+        ).flatMap { case (data, oid, versionumero, aikaleima) =>
+          val json = KoskiTables.KoskiOpiskeluoikeusTable.readAsJValue(data, oid, versionumero, aikaleima)
+          application.validatingAndResolvingExtractor.extract[Opiskeluoikeus](KoskiSchema.strictDeserialization)(json) match {
+            case Right(oo: KoskeenTallennettavaOpiskeluoikeus) => Some(oo)
+            case Right(oo: Opiskeluoikeus) =>
+              logger.warn(s"${oo.oid} does not deserialize to KoskeenTallennettavaOpiskeluoikeus")
+              None
+            case Left(errors) =>
+              logger.warn(s"Error deserializing opiskeluoikeus: ${errors}")
+              None
+          }
+        }
+        f(henkilö, opiskeluoikeudet)
+      }
+    }
+}
--- src/main/scala/fi/oph/koski/queuedqueries/organisaationopiskeluoikeudet/QueryOrganisaationOpiskeluoikeudetJson.scala
+++ src/main/scala/fi/oph/koski/queuedqueries/organisaationopiskeluoikeudet/QueryOrganisaationOpiskeluoikeudetJson.scala
@@ -1,24 +1,11 @@
 package fi.oph.koski.queuedqueries.organisaationopiskeluoikeudet
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.db.PostgresDriverWithJsonSupport.api.actionBasedSQLInterpolation
-import fi.oph.koski.db.PostgresDriverWithJsonSupport.plainAPI._
-import fi.oph.koski.db.{DatabaseConverters, KoskiTables, QueryMethods, SQLHelpers}
-import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
-import fi.oph.koski.koskiuser.{AccessType, KoskiSpecificSession}
-import fi.oph.koski.log.KoskiAuditLogMessageField.hakuEhto
-import fi.oph.koski.log.KoskiOperation.OPISKELUOIKEUS_HAKU
-import fi.oph.koski.log.{AuditLog, KoskiAuditLogMessage, Logging}
-import fi.oph.koski.opiskeluoikeus.OpiskeluoikeusQueryContext
-import fi.oph.koski.queuedqueries.{QueryParameters, QueryResultWriter}
-import fi.oph.koski.schema.{KoskiSchema, Opiskeluoikeus, Oppija, Organisaatio}
-import fi.oph.koski.util.ChainingSyntax.chainingOps
+import fi.oph.koski.queuedqueries.QueryResultWriter
+import fi.oph.koski.schema.{Oppija, Organisaatio}
 import fi.oph.scalaschema.annotation.EnumValue
-import org.json4s.JValue
 
-import java.sql.Timestamp
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import scala.util.Try
 
 case class QueryOrganisaationOpiskeluoikeudetJson(
@@ -28,100 +15,25 @@ case class QueryOrganisaationOpiskeluoikeudetJson(
   format: String = "application/json",
   organisaatioOid: Option[Organisaatio.Oid],
   alkamispaiva: LocalDate,
-  tila: Option[String], // TODO: Lisää sallitut arvot
-  koulutusmuoto: Option[String], // TODO: Lisää sallitut arvot
-  suoritustyyppi: Option[String], // TODO: Lisää sallitut arvot
-) extends QueryParameters with DatabaseConverters with Logging {
-
-  def run(application: KoskiApplication, writer: QueryResultWriter)(implicit user: KoskiSpecificSession): Either[String, Unit] = {
-    val oppilaitosOids = application.organisaatioService.organisaationAlaisetOrganisaatiot(organisaatioOid.get)
-    fetchData(
-      application = application,
-      writer = writer,
-      oppilaitosOids = oppilaitosOids,
-    ).toEither
-      .tap(_ => auditLog())
-      .left.map(_.getMessage)
-  }
-
-  private def fetchData(
+  tila: Option[String],
+  koulutusmuoto: Option[String],
+  suoritustyyppi: Option[String],
+) extends QueryOrganisaationOpiskeluoikeudet {
+  def fetchData(
     application: KoskiApplication,
     writer: QueryResultWriter,
     oppilaitosOids: List[Organisaatio.Oid],
   ): Try[Unit] = Try {
-    val db = application.replicaDatabase.db
+    val db = getDb(application)
+    val filters = defaultBaseFilter(oppilaitosOids)
+    val oppijaOids = getOppijaOids(db, filters)
 
-    val filters = SQLHelpers.concatMany(
-      Some(sql"WHERE NOT poistettu AND NOT mitatoity AND oppilaitos_oid = ANY($oppilaitosOids) AND alkamispaiva >= $alkamispaiva "),
-      tila.map(t => sql" AND tila = $t "),
-      koulutusmuoto.map(t => sql"AND koulutusmuoto = $t "),
-      suoritustyyppi.map(t => sql"AND $t IN suoritustyypit "),
-    )
-
-    val oppijaOids = QueryMethods.runDbSync(
-      db,
-      SQLHelpers.concat(sql"SELECT DISTINCT oppija_oid FROM opiskeluoikeus ", filters).as[(String)]
-    )
-
-    oppijaOids.foreach { oppijaOid =>
-      application.henkilöRepository.findByOid(oppijaOid, findMasterIfSlaveOid = true).map { henkilö =>
-        val henkilöFilter = SQLHelpers.concat(filters, sql"AND oppija_oid = ANY(${henkilö.kaikkiOidit})")
-        val opiskeluoikeudet = QueryMethods.runDbSync(
-          db,
-          SQLHelpers.concat(sql"SELECT data, oid, versionumero, aikaleima FROM opiskeluoikeus ", henkilöFilter).as[(JValue, String, Int, Timestamp)]
-        )
-        val data = Oppija(
-          henkilö = application.henkilöRepository.oppijaHenkilöToTäydellisetHenkilötiedot(henkilö),
-          opiskeluoikeudet = opiskeluoikeudet.map { row =>
-            val (data, oid, versionumero, aikaleima) = row
-            val json = KoskiTables.KoskiOpiskeluoikeusTable.readAsJValue(data, oid, versionumero, aikaleima)
-            application.validatingAndResolvingExtractor.extract[Opiskeluoikeus](KoskiSchema.strictDeserialization)(json) match {
-              case Right(oo) => oo
-              case Left(errors) => throw new Exception(s"Error deserializing opiskeluoikeus ${oid}: ${errors}")
-            }
-          },
-        )
-        writer.putJson(henkilö.oid, data)
-      }
+    forEachOpiskeluoikeus(application, filters, oppijaOids) { (henkilö, opiskeluoikeudet) =>
+      writer.putJson(henkilö.oid, Oppija(
+        henkilö = application.henkilöRepository.oppijaHenkilöToTäydellisetHenkilötiedot(henkilö),
+        opiskeluoikeudet = opiskeluoikeudet,
+      ))
     }
-  }
-
-  def queryAllowed(application: KoskiApplication)(implicit user: KoskiSpecificSession): Boolean =
-    user.hasGlobalReadAccess || (
-      organisaatioOid.exists(user.organisationOids(AccessType.read).contains)
-        && koulutusmuoto.forall(user.allowedOpiskeluoikeusTyypit.contains)
-      )
-
-  override def withDefaults(implicit user: KoskiSpecificSession): Either[HttpStatus, QueryOrganisaationOpiskeluoikeudetJson] =
-    if (organisaatioOid.isEmpty) {
-      defaultOrganisaatio.map(oid => copy(organisaatioOid = Some(oid)))
-    } else {
-      Right(this)
-    }
-
-  private def defaultOrganisaatio(implicit user: KoskiSpecificSession) = {
-    val organisaatiot = user.juuriOrganisaatiot
-    if (organisaatiot.isEmpty) {
-      Left(KoskiErrorCategory.unauthorized("Käyttäjäoikeuksissa ei ole määritelty eksplisiittisesti lukuoikeutta yhdenkään tietyn organisaation tietoihin.")) // Mahdollista esim. pääkäyttäjän tunnuksilla
-    } else if (organisaatiot.size > 1) {
-      Left(KoskiErrorCategory.unauthorized("Kenttää `organisaatioOid` ei ole annettu, eikä organisaatiota voi yksiselitteisesti päätellä käyttöoikeuksista."))
-    } else {
-      Right(user.juuriOrganisaatiot.head.oid)
-    }
-  }
-
-  private def auditLog()(implicit user: KoskiSpecificSession): Unit = {
-    AuditLog.log(KoskiAuditLogMessage(
-      OPISKELUOIKEUS_HAKU,
-      user,
-      Map(hakuEhto -> OpiskeluoikeusQueryContext.queryForAuditLog(Map(
-        "organisaatio" -> List(organisaatioOid.get),
-        "opiskeluoikeusAlkanutAikaisintaan" -> List(alkamispaiva.format(DateTimeFormatter.ISO_DATE)),
-        "opiskeluoikeudenTila" -> tila.toList,
-        "suorituksenTyyppi" -> suoritustyyppi.toList,
-        "koulutusmuoto" -> koulutusmuoto.toList,
-      ).filter(_._2.nonEmpty))),
-    ))
   }
 }
 
