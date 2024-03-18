22e168ec6df9b461d15d2134a455994732060999
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2024-03-18 14:15:45 +0200

    Uudelleenlaske päällekkäisten opiskeluoikeuksien raportti, jos raportointikanta vaihtui alta kesken

--- scripts/local-reset-raportointikanta.sh
+++ scripts/local-reset-raportointikanta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-time curl -N -u 'pää:pää' 127.0.0.1:7021/koski/api/raportointikanta/load?force=true
+time curl -N -u 'pää:pää' 127.0.0.1:7021/koski/api/test/raportointikanta/load?force=true
