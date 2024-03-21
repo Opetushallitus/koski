61951b7cde0ca60debc6ba867032b1405bd8f52a
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2024-03-21 12:11:09 +0200

    Korjaa opensearch indeksointiasetukset

--- src/main/scala/fi/oph/koski/perustiedot/OpiskeluoikeudenPerustiedotIndexer.scala
+++ src/main/scala/fi/oph/koski/perustiedot/OpiskeluoikeudenPerustiedotIndexer.scala
@@ -22,7 +22,7 @@ object OpiskeluoikeudenPerustiedotIndexer {
       "filter" -> Map(
         "finnish_folding" -> Map(
           "type" -> "icu_folding",
-          "unicodeSetFilter" -> "[^åäöÅÄÖ]"
+          "unicode_set_filter" -> "[^åäöÅÄÖ]"
         )
       ),
       "analyzer" -> Map(
@@ -88,7 +88,7 @@ class OpiskeluoikeudenPerustiedotIndexer(
   val index = new OpenSearchIndex(
     openSearch = openSearch,
     name = "perustiedot",
-    mappingVersion = 3,
+    mappingVersion = 4,
     mapping = OpiskeluoikeudenPerustiedotIndexer.mapping,
     settings = OpiskeluoikeudenPerustiedotIndexer.settings,
     initialLoader = this.indexAllDocuments
