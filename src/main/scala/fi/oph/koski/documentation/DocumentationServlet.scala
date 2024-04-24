23fb7f48d226ee32f4974d68d2d5cb53e3b4720b
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2024-04-24 15:15:59 +0300

    Korjaa dokumentaation route

--- src/main/scala/fi/oph/koski/documentation/DocumentationServlet.scala
+++ src/main/scala/fi/oph/koski/documentation/DocumentationServlet.scala
@@ -25,7 +25,7 @@ class DocumentationServlet(implicit val application: KoskiApplication)
   val frontendValvontaMode: FrontendValvontaMode.FrontendValvontaMode =
     FrontendValvontaMode(application.config.getString("frontend-valvonta.mode"))
 
-  get("^/(|tietomalli|koodistot|rajapinnat/oppilashallintojarjestelmat|rajapinnat/luovutuspalvelu|rajapinnat/palveluvayla-omadata|rajapinnat/massaluovutus/koulutuksenjarjestajat)$".r)(nonce => {
+  get("^/(|tietomalli|koodistot|rajapinnat/opintohallintojarjestelmat|rajapinnat/luovutuspalvelu|rajapinnat/palveluvayla-omadata|rajapinnat/massaluovutus/koulutuksenjarjestajat)$".r)(nonce => {
     htmlIndex("koski-main.js", raamit = virkailijaRaamit, allowIndexing = true, nonce = nonce)
   })
 
