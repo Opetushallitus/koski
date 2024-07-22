0a353610fa121874f2c6c3d1a4c90d82ee5e6fad
Henri Ruoho <henri.ruoho@reaktor.com>
2024-07-22 14:05:10 +0300

    Siivoa servlet

--- src/main/scala/fi/oph/koski/hakemuspalvelu/HakemuspalveluServlet.scala
+++ src/main/scala/fi/oph/koski/hakemuspalvelu/HakemuspalveluServlet.scala
@@ -1,15 +1,14 @@
 package fi.oph.koski.hakemuspalvelu
 
 import fi.oph.koski.config.KoskiApplication
-import fi.oph.koski.henkilo.{HenkilöOid, Hetu}
+import fi.oph.koski.henkilo.{HenkilöOid}
 import fi.oph.koski.http.{HttpStatus, JsonErrorMessage, KoskiErrorCategory}
 import fi.oph.koski.json.JsonSerializer
-import fi.oph.koski.koskiuser.{KoskiSpecificSession, RequiresHakemuspalvelu, RequiresVkt}
+import fi.oph.koski.koskiuser.{KoskiSpecificSession, RequiresHakemuspalvelu}
 import fi.oph.koski.servlet.{KoskiSpecificApiServlet, NoCache}
 import org.json4s.JValue
 
 case class OidRequest(oid: String)
-case class HetuRequest(hetu: String)
 
 class HakemuspalveluServlet(implicit val application: KoskiApplication) extends KoskiSpecificApiServlet with RequiresHakemuspalvelu with NoCache {
   post("/oid") {
@@ -19,19 +18,8 @@ class HakemuspalveluServlet(implicit val application: KoskiApplication) extends 
     }()
   }
 
-  post("/hetu") {
-    withJsonBody { json =>
-      renderEither(extractAndValidateHetu(json).flatMap(application.hakemuspalveluService.findOppijaByHetu))
-    }()
-  }
-
   private def extractAndValidateOid(json: JValue): Either[HttpStatus, String] =
     JsonSerializer.validateAndExtract[OidRequest](json)
       .left.map(errors => KoskiErrorCategory.badRequest.validation.jsonSchema(JsonErrorMessage(errors)))
       .flatMap(req => HenkilöOid.validateHenkilöOid(req.oid))
-
-  private def extractAndValidateHetu(json: JValue): Either[HttpStatus, String] =
-    JsonSerializer.validateAndExtract[HetuRequest](json)
-      .left.map(errors => KoskiErrorCategory.badRequest.validation.jsonSchema(JsonErrorMessage(errors)))
-      .flatMap(req => Hetu.validFormat(req.hetu))
 }
