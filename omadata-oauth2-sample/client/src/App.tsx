3a01e5062fc8575dda2bb21544d384b284d98a0f
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2024-09-24 12:49:49 +0300

    Esimerkki OmaDataOAuth2 , fronttirunko

---
+++ omadata-oauth2-sample/client/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState<object | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+
+    fetch("/api")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`API result: ${res.status}:${res.statusText}`)
+        }
+        setError('SUCCESS')
+        return res.json()
+      })
+      .then((data) => {
+        setData(data)
+      })
+      .catch(error => {
+        console.error(error)
+        setData({result: "ERROR"})
+        setError(error.message)
+      })
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : JSON.stringify(data)}</p>
+        <p>{!error ? "Checking errors..." : error}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
