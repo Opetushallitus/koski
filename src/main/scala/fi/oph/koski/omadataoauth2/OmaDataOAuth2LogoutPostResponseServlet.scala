6f82c0312698c4282de1b3dc846d7c8a87b61773
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2024-10-15 13:11:43 +0300

    Mahdollista form-action -CSP:n skippaus client-kohtaisesti;
    Käytännössä form on niin simppeli, että ei siinä pitäisi nykymuodossa olla HTML-injektion mahdollisuutta.
    
    Skippaustuki tarvitaan, koska jotkut selaimet estävät clientin tekemät redirectit, esim. clientin backendiltä
    clientin mobiiliapplikaatio-frontille.

--- src/main/scala/fi/oph/koski/omadataoauth2/OmaDataOAuth2LogoutPostResponseServlet.scala
+++ src/main/scala/fi/oph/koski/omadataoauth2/OmaDataOAuth2LogoutPostResponseServlet.scala
@@ -15,16 +15,19 @@ class OmaDataOAuth2LogoutPostResponseServlet(implicit val application: KoskiAppl
   val frontendValvontaMode: FrontendValvontaMode.FrontendValvontaMode =
     FrontendValvontaMode(application.config.getString("frontend-valvonta.mode"))
 
-  // Aseta form-action:ille sallituksi parametreissa tullut redirect_uri
-  override def formActionSources: String = {
-    val redirectUriFromParams = multiParams("redirect_uri").headOption.getOrElse("")
+  override def formActionSources: Option[String] = {
+    if (useFormActionCspHeader(multiParams("client_id").headOption.getOrElse(""))) {
+      val redirectUriFromParams = multiParams("redirect_uri").headOption.getOrElse("")
 
-    Uri.fromString(redirectUriFromParams).map(fullUri => Uri(fullUri.scheme, fullUri.authority, fullUri.path)) match {
-      case Right(result) if result.host.isDefined =>
-        val formActionUri = result.toString
-        formActionUri
-      case Left(failure) =>
-        "'self'"
+      Uri.fromString(redirectUriFromParams).map(fullUri => Uri(fullUri.scheme, fullUri.authority, fullUri.path)) match {
+        case Right(result) if result.host.isDefined =>
+          val formActionUri = result.toString
+          Some(formActionUri)
+        case _ =>
+          super.formActionSources
+      }
+    } else {
+      None
     }
   }
 
