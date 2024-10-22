25e9b5185aac8569265a56514244a01fd37c7e1d
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2024-10-22 10:31:34 +0300

    Aja testit vain kerran, mvn-parametrit aiheutti tuplaamisen yms.;
    GA:n retryä ei tarvita, playwright osaa itse retryttää CI:llä.

--- src/test/scala/fi/oph/koski/e2e/KoskiFrontSpec.scala
+++ src/test/scala/fi/oph/koski/e2e/KoskiFrontSpec.scala
@@ -2,6 +2,7 @@ package fi.oph.koski.e2e
 
 import fi.oph.koski.{KoskiApplicationForTests, SharedJetty}
 import fi.oph.koski.mocha.KoskiCommandLineSpec
+import org.scalatest.Tag
 import org.scalatest.freespec.AnyFreeSpec
 
 
@@ -9,7 +10,7 @@ class KoskiFrontSpec extends AnyFreeSpec with KoskiCommandLineSpec {
   // Oletuksena vain yksi shardi, jotta kaikki testit menevät kerralla ajoon
   def shardIndex: String = scala.util.Properties.envOrElse("PLAYWRIGHT_SHARD_INDEX", "1")
   def shardTotal: String = scala.util.Properties.envOrElse("PLAYWRIGHT_SHARD_TOTAL", "1")
-  "Koski integration tests" in {
+  "Koski integration tests" taggedAs(KoskiFrontTag) in {
     val sharedJetty = new SharedJetty(KoskiApplicationForTests)
     sharedJetty.start()
     runTestCommand("koski-integration-tests", Seq(
@@ -20,3 +21,5 @@ class KoskiFrontSpec extends AnyFreeSpec with KoskiCommandLineSpec {
     ))
   }
 }
+
+object KoskiFrontTag extends Tag("integrationtest")
