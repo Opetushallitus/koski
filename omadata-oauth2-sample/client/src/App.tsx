d00d6846fb826e45b64679e4153764e876e1acab
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2024-11-18 07:32:22 +0200

    Poista sample-frontista dummy-testeri;
    dummy access tokenin käyttö korvautuu koko OAuth2-flown toteutuksella, joten näin suoraviivainen yhden
    routen kutsuminen dummy-sisällöllä ei enää jatkossa toimi, vaan putki pitää smoke-testata täydellisemmin.

--- omadata-oauth2-sample/client/src/App.tsx
+++ omadata-oauth2-sample/client/src/App.tsx
@@ -2,28 +2,6 @@ import React from "react"
 import "./App.css"
 
 function App() {
-  const [data, setData] = React.useState<object | null>(null)
-  const [error, setError] = React.useState<string | null>(null)
-
-  React.useEffect(() => {
-    fetch("/api/front-page-dummy")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`API result: ${res.status}:${res.statusText}`)
-        }
-        setError("SUCCESS")
-        return res.json()
-      })
-      .then((data) => {
-        setData(data)
-      })
-      .catch((error) => {
-        console.error(error)
-        setData({ result: "ERROR" })
-        setError(error.message)
-      })
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
@@ -37,9 +15,6 @@ function App() {
           Test authorization code flow with invalid redirect_uri
         </a>
       </p>
-      <h2>Fetch resource using access token result:</h2>
-      <p>{!data ? "Loading..." : JSON.stringify(data)}</p>
-      <p>{!error ? "Checking errors..." : error}</p>
     </div>
   )
 }
