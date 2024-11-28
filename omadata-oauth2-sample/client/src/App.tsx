730ea25b5b31a18acc9858014f5e43c9bead937f
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2024-11-28 14:20:20 +0200

    Lisää testiapplikaatioon testerit kaikille scopeille

--- omadata-oauth2-sample/client/src/App.tsx
+++ omadata-oauth2-sample/client/src/App.tsx
@@ -8,7 +8,31 @@ function App() {
         <p>OmaDataOAuth2 Sample app</p>
       </header>
       <p>
-        <a href={"/api/openid-api-test"}>Test whole authorization code flow</a>
+        <a
+          href={
+            "/api/openid-api-test?scope=HENKILOTIEDOT_KAIKKI_TIEDOT+OPISKELUOIKEUDET_SUORITETUT_TUTKINNOT"
+          }
+        >
+          Test whole authorization code flow - suoritetut tutkinnot
+        </a>
+      </p>
+      <p>
+        <a
+          href={
+            "/api/openid-api-test?scope=HENKILOTIEDOT_KAIKKI_TIEDOT+OPISKELUOIKEUDET_AKTIIVISET_JA_PAATTYNEET_OPINNOT"
+          }
+        >
+          Test whole authorization code flow - aktiiviset ja päättyneet opinnot
+        </a>
+      </p>
+      <p>
+        <a
+          href={
+            "/api/openid-api-test?scope=HENKILOTIEDOT_KAIKKI_TIEDOT+OPISKELUOIKEUDET_KAIKKI_TIEDOT"
+          }
+        >
+          Test whole authorization code flow - kaikki tiedot
+        </a>
       </p>
       <p>
         <a href={"/api/openid-api-test/invalid-redirect-uri"}>
