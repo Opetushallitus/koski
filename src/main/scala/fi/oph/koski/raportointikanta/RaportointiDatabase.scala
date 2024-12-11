d603cc1c2581519e49bb541c2b5d2ae6e1e37bf9
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2024-12-11 19:59:31 +0200

    Korjaa MigrationSpec ja RaportointikantaSpec

--- src/main/scala/fi/oph/koski/raportointikanta/RaportointiDatabase.scala
+++ src/main/scala/fi/oph/koski/raportointikanta/RaportointiDatabase.scala
@@ -28,7 +28,7 @@ object RaportointiDatabase {
   // Jälkimmäinen arvo on skeemasta laskettu tunniste (kts. QueryMethods::getSchemaHash).
   // Testi nimeltä "Schema version has been updated" tarkastaa että versionumeroa päivitetään skeemamuutosten
   // myötä.
-  def schemaVersion: (Int, String) = (16, "0d358ffbd7efa9e7aab3a5c8b1e9d0cc")
+  def schemaVersion: (Int, String) = (17, "8324874241be3f98b4fbccf4d1864619")
 }
 
 class RaportointiDatabase(config: RaportointiDatabaseConfigBase) extends Logging with QueryMethods {
