68e94348ac2e99be6878501fbd997a2ef54d95d5
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2024-12-12 15:25:37 +0200

    Älä palauta valtavaa möllykkää constraint-apista haettaessa tiettyjä luokkia

--- src/main/scala/fi/oph/koski/typemodel/SchemaExport.scala
+++ src/main/scala/fi/oph/koski/typemodel/SchemaExport.scala
@@ -1,19 +1,29 @@
 package fi.oph.koski.typemodel
 
 import fi.oph.scalaschema._
-import fi.oph.scalaschema.annotation.DefaultValue
 
 import scala.language.implicitConversions
 
 object SchemaExport {
-  def toTypeDef(schema: Schema, followClassRefs: Boolean = false): Seq[TypeModelWithClassName] =
-    parse(schema, followClassRefs).collect { case s: TypeModelWithClassName => s }
+  case class Context(
+    followClassRefs: Boolean,
+    depth: Int,
+    stopParsingAt: List[String]
+  ) {
+    def next(): Context = copy(depth = depth - 1)
+    def isEnd: Boolean = depth <= 0
+    def isEnd(className: String): Boolean = depth <= 0 || stopParsingAt.contains(className)
+  }
 
-  private def parse(schema: Schema, followClassRefs: Boolean = false): Seq[TypeModel] =
-    Seq(parseSchema(schema)) ++ parseAssociatedSchemas(schema, followClassRefs)
+  def toTypeDef(schema: Schema, followClassRefs: Boolean = false, maxDepth: Int = Int.MaxValue, stopParsingAt: List[String] = Nil): Seq[TypeModelWithClassName] =
+    parse(schema, Context(followClassRefs, maxDepth, stopParsingAt)).collect { case s: TypeModelWithClassName => s }
 
-  private def parseSchema(schema: Schema): TypeModel = {
+  private def parse(schema: Schema, ctx: Context): Seq[TypeModel] =
+    Seq(parseSchema(schema, ctx)) ++ parseAssociatedSchemas(schema, ctx)
+
+  private def parseSchema(schema: Schema, ctx: Context): TypeModel = {
     (schema match {
+      case s: SchemaWithClassName if ctx.isEnd(s.fullClassName) => BoundaryType(s.fullClassName)
       case StringSchema(enumValues) =>
         enumValues match {
           case Some(enumValues) => EnumType(DataTypes.String, enumValues)
@@ -32,24 +42,24 @@ object SchemaExport {
       case DateSchema(_) =>
         DateType() // TODO: Pitää varmaan tukea myös kellonaikoja
       case OptionalSchema(itemSchema) =>
-        OptionalType(parseSchema(itemSchema))
+        OptionalType(parseSchema(itemSchema, ctx.next()))
       case ListSchema(itemSchema) =>
-        ArrayType(parseSchema(itemSchema))
+        ArrayType(parseSchema(itemSchema, ctx.next()))
       case MapSchema(itemSchema) =>
-        RecordType(parseSchema(itemSchema))
+        RecordType(parseSchema(itemSchema, ctx.next()))
       case schema: ClassSchema if schema.readFlattened.isDefined =>
-        parseSchema(schema.asAnyOfSchema)
+        parseSchema(schema.asAnyOfSchema, ctx.next())
       case classSchema: ClassSchema =>
-        parseClassSchema(classSchema)
+        parseClassSchema(classSchema, ctx.next())
       case classRef: ClassRefSchema =>
         ClassRef(classRef.fullClassName)
       case anyOf: AnyOfSchema =>
         UnionType(
           fullClassName = anyOf.fullClassName,
-          anyOf = anyOf.alternatives.map(parseSchema)
+          anyOf = anyOf.alternatives.map(parseSchema(_, ctx.next()))
         )
       case flattenedSchema: FlattenedSchema =>
-        parseSchema(flattenedSchema.property.schema)
+        parseSchema(flattenedSchema.property.schema, ctx.next())
       case _: AnyObjectSchema =>
         AnyObjectType()
       case _: AnyListSchema =>
@@ -59,19 +69,23 @@ object SchemaExport {
     }).withMetadata(schema.metadata)
   }
 
-  private def parseAssociatedSchemas(schema: Schema, followClassRefs: Boolean = false): Seq[TypeModel] = schema match {
-    case classSchema: ClassSchema => classSchema.definitions.flatMap(clss => parse(clss))
-    case classRefSchema: ClassRefSchema if followClassRefs =>
-      println(s"Parse associated schemas: ref ${classRefSchema.fullClassName}")
-      TypeExport.getObjectModels(Class.forName(classRefSchema.fullClassName))
-    case anyOfSchema: AnyOfSchema =>
-      println(s"Parse associated schemas: anyOf ${anyOfSchema.fullClassName}")
-      anyOfSchema.alternatives.flatMap(alt => parseAssociatedSchemas(alt, followClassRefs))
-    case _ => Seq.empty
+  private def parseAssociatedSchemas(schema: Schema, ctx: Context): Seq[TypeModel] = {
+    schema match {
+      case _ if ctx.isEnd => Seq.empty
+      case classSchema: ClassSchema => classSchema.definitions.flatMap(clss => parse(clss, ctx.next()))
+      case classRefSchema: ClassRefSchema if ctx.followClassRefs =>
+        // println(s"Parse associated schemas: ref ${classRefSchema.fullClassName}")
+        TypeExport.getObjectModels(Class.forName(classRefSchema.fullClassName))
+      case anyOfSchema: AnyOfSchema =>
+        // println(s"Parse associated schemas: anyOf ${anyOfSchema.fullClassName}")
+        anyOfSchema.alternatives.flatMap(alt => parseAssociatedSchemas(alt, ctx.next()))
+      case _ => Seq.empty
+    }
   }
 
-  private def parseClassSchema(classSchema: ClassSchema): TypeModel = {
-    val propTypes = classSchema.properties.map(p => parseSchema(p.schema).withMetadata(p.metadata))
+  private def parseClassSchema(classSchema: ClassSchema, ctx: Context): TypeModel = {
+    val nextCtx = ctx.next()
+    val propTypes = classSchema.properties.map(p => parseSchema(p.schema, nextCtx).withMetadata(p.metadata))
     val propKeys = classSchema.properties.map(_.key)
     ObjectType(
       fullClassName = classSchema.fullClassName,
