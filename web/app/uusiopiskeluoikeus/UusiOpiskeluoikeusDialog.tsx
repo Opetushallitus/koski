af4a43200bf226e503c2e4d92e6146de29a859c6
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-01-28 12:04:54 +0200

    Estä 'lisää opiskeluoikeus' -napin painaminen useamman kerran

--- web/app/uusiopiskeluoikeus/UusiOpiskeluoikeusDialog.tsx
+++ web/app/uusiopiskeluoikeus/UusiOpiskeluoikeusDialog.tsx
@@ -11,9 +11,10 @@ import { t } from '../i18n/i18n'
 import { Opiskeluoikeus } from '../types/fi/oph/koski/schema/Opiskeluoikeus'
 import { UusiOpiskeluoikeusForm } from './UusiOpiskeluoikeusForm'
 import { TestIdRoot } from '../appstate/useTestId'
+import { useSafeState } from '../api-fetch'
 
 export type UusiOpiskeluoikeusDialogProps = {
-  onSubmit: (opiskeluoikeus: Opiskeluoikeus) => void
+  onSubmit: (opiskeluoikeus: Opiskeluoikeus) => Promise<void>
   onClose: () => void
 }
 
@@ -21,9 +22,18 @@ export const UusiOpiskeluoikeusDialog = (
   props: UusiOpiskeluoikeusDialogProps
 ) => {
   const [result, setResult] = useState<Opiskeluoikeus>()
+  const [isCreating, setCreating] = useSafeState(false)
 
-  const onSubmit = useCallback(() => {
-    result && props.onSubmit(result)
+  const onSubmit = useCallback(async () => {
+    if (result) {
+      setCreating(true)
+      try {
+        await props.onSubmit(result)
+      } catch (err) {
+        console.error(err)
+      }
+      setCreating(false)
+    }
   }, [props, result])
 
   return (
@@ -37,7 +47,11 @@ export const UusiOpiskeluoikeusDialog = (
           <FlatButton onClick={props.onClose} testId="cancel">
             {t('Peruuta')}
           </FlatButton>
-          <RaisedButton onClick={onSubmit} disabled={!result} testId="submit">
+          <RaisedButton
+            onClick={onSubmit}
+            disabled={!result || isCreating}
+            testId="submit"
+          >
             {t('Lisää opiskeluoikeus')}
           </RaisedButton>
         </ModalFooter>
