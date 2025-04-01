0ea0e928f223082d79ba002d5407257a0c2ad417
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-04-01 10:26:40 +0300

    Yleisen kielitutkinnon validaation testit

--- src/test/scala/fi/oph/koski/kielitutkinto/KielitutkintorekisteriSpec.scala
+++ src/test/scala/fi/oph/koski/kielitutkinto/KielitutkintorekisteriSpec.scala
@@ -1,18 +1,20 @@
 package fi.oph.koski.kielitutkinto
 
-import fi.oph.koski.api.misc.{OpiskeluoikeusTestMethods, PutOpiskeluoikeusTestMethods}
+import fi.oph.koski.api.misc.PutOpiskeluoikeusTestMethods
+import fi.oph.koski.documentation.ExamplesKielitutkinto.Opiskeluoikeusjakso
+import fi.oph.koski.documentation.ExamplesKielitutkinto.YleisetKielitutkinnot.tutkinnonOsa
 import fi.oph.koski.documentation.{ExamplesKielitutkinto, ExamplesLukio}
 import fi.oph.koski.henkilo.{KoskiSpecificMockOppijat, LaajatOppijaHenkilöTiedot}
 import fi.oph.koski.http.{HttpStatus, KoskiErrorCategory}
 import fi.oph.koski.json.JsonSerializer
 import fi.oph.koski.koskiuser.{KoskiSpecificSession, MockUsers}
+import fi.oph.koski.oppija.{HenkilönOpiskeluoikeusVersiot, OppijaServletOppijaAdder}
+import fi.oph.koski.schema._
+import fi.oph.koski.util.WithWarnings
 import fi.oph.koski.{KoskiApplicationForTests, KoskiHttpSpec}
+import org.json4s.JValue
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
-import fi.oph.koski.oppija.{HenkilönOpiskeluoikeusVersiot, OppijaServletOppijaAdder}
-import fi.oph.koski.schema.{KielitutkinnonOpiskeluoikeus, Koodistokoodiviite, LukionOpiskeluoikeus, LähdejärjestelmäId, Oppija, ValtionhallinnonKielitutkinnonArviointi, ValtionhallinnonKielitutkinnonKirjallisenKielitaidonSuoritus, ValtionhallinnonKielitutkinnonSuoritus, YlioppilastutkinnonOpiskeluoikeus}
-import fi.oph.koski.util.WithWarnings
-import org.json4s.JValue
 
 import java.time.LocalDate
 import scala.reflect.runtime.universe
@@ -22,8 +24,6 @@ class KielitutkintorekisteriSpec
     with KoskiHttpSpec
     with Matchers
     with PutOpiskeluoikeusTestMethods[KielitutkinnonOpiskeluoikeus] {
-
-
 
   val oppijaAdder = new OppijaServletOppijaAdder(KoskiApplicationForTests)
   val kielitutkinnonSuorittaja = ExamplesKielitutkinto.exampleMockOppija
@@ -69,6 +69,150 @@ class KielitutkintorekisteriSpec
   }
 
   "Validaatiot" - {
+    "Yleinen kielitutkinto" - {
+      val tutkintopäivä = LocalDate.of(2025, 1, 15)
+      val opiskeluoikeus = ExamplesKielitutkinto.YleisetKielitutkinnot.opiskeluoikeus(
+        tutkintopäivä = tutkintopäivä,
+        kieli = "FI",
+        tutkintotaso = "pt",
+      )
+      val arviointipäivä = opiskeluoikeus.tila.opiskeluoikeusjaksot.find(_.tila.koodiarvo != "lasna").get.alku
+
+      "Opiskeluoikeuden tila" - {
+        "Ei voi siirtää ilman läsnä-tilaa" in {
+          val invalidOo = opiskeluoikeus.copy(
+            tila = KielitutkinnonOpiskeluoikeudenTila(
+              opiskeluoikeusjaksot = List(
+                Opiskeluoikeusjakso.valmis(arviointipäivä),
+              )
+            )
+          )
+
+          postOpiskeluoikeus(invalidOo) {
+            verifyResponseStatus(400, KoskiErrorCategory.badRequest.validation.tila.tilaPuuttuu(
+              "Kielitutkinnolta puuttuu 'lasna'-tilainen opiskeluoikeuden jakso"
+            ))
+          }
+        }
+
+        "Siirrossa on aina oltava mukana hyvaksytystisuoritettu- tai paattynyt-tila" in {
+          val invalidOo = opiskeluoikeus.copy(
+            tila = KielitutkinnonOpiskeluoikeudenTila(
+              opiskeluoikeusjaksot = List(
+                Opiskeluoikeusjakso.tutkintopäivä(tutkintopäivä),
+              )
+            )
+          )
+
+          postOpiskeluoikeus(invalidOo) {
+            verifyResponseStatus(400,
+              KoskiErrorCategory.badRequest.validation.tila.tilaPuuttuu(
+                "Kielitutkinnolta puuttuu 'hyvaksytystisuoritettu' tai 'paattynyt' -tilainen opiskeluoikeuden jakso"
+              ),
+              KoskiErrorCategory.badRequest.validation.date.päättymispäivämäärä (
+                "Arviointipäivä null (hyvaksytystisuoritettu tai paattynyt -tilainen opiskeluoikeusjakso) on eri kuin vahvistuksen päivämäärä 2025-03-16"
+              ),
+            )
+          }
+        }
+
+        "hyvaksytystisuoritettu -päivän on vastattava vahvistuksen päivämäärää" in {
+          val invalidDate = arviointipäivä.plusDays(1)
+          val invalidOo = opiskeluoikeus.copy(
+            tila = KielitutkinnonOpiskeluoikeudenTila(
+              opiskeluoikeusjaksot = List(
+                Opiskeluoikeusjakso.tutkintopäivä(tutkintopäivä),
+                Opiskeluoikeusjakso.valmis(invalidDate),
+              )
+            )
+          )
+
+          postOpiskeluoikeus(invalidOo) {
+            verifyResponseStatus(400, KoskiErrorCategory.badRequest.validation.date.päättymispäivämäärä(
+              s"Arviointipäivä $invalidDate (hyvaksytystisuoritettu -tilainen opiskeluoikeusjakso) on eri kuin vahvistuksen päivämäärä $arviointipäivä")
+            )
+          }
+        }
+      }
+
+      "Arvioinnit" - {
+        "Älä salli perustasolle muita arvosanoja kuin alle 1, 1 ja 2" in {
+          val invalidOo = opiskeluoikeus.copy(
+            suoritukset = List(
+              ExamplesKielitutkinto.YleisetKielitutkinnot.päätasonSuoritus(
+                tutkintotaso = "pt",
+                kieli = "FI",
+                arviointipäivä = arviointipäivä
+              ).copy(
+                osasuoritukset = Some(List(
+                  tutkinnonOsa("tekstinymmartaminen", "alle3", arviointipäivä),
+                  tutkinnonOsa("kirjoittaminen", "alle1", arviointipäivä),
+                  tutkinnonOsa("puheenymmartaminen", "1", arviointipäivä),
+                  tutkinnonOsa("puhuminen", "9", arviointipäivä),
+                ))
+              )
+            )
+          )
+
+          postOpiskeluoikeus(invalidOo) {
+            verifyResponseStatus(400, KoskiErrorCategory.badRequest.validation.arviointi.epäsopivaArvosana (
+              s"Suoritus ykisuorituksenosa/tekstinymmartaminen sisältää virheellisen arvosanan alle3 (sallitut koodiarvot ovat: alle1, 1, 2, 9, 10, 11)")
+            )
+          }
+        }
+
+        "Älä salli keskitasolle muita arvosanoja kuin alle 3, 3 ja 4" in {
+          val invalidOo = opiskeluoikeus.copy(
+            suoritukset = List(
+              ExamplesKielitutkinto.YleisetKielitutkinnot.päätasonSuoritus(
+                tutkintotaso = "kt",
+                kieli = "FI",
+                arviointipäivä = arviointipäivä
+              ).copy(
+                osasuoritukset = Some(List(
+                  tutkinnonOsa("tekstinymmartaminen", "alle1", arviointipäivä),
+                  tutkinnonOsa("kirjoittaminen", "alle3", arviointipäivä),
+                  tutkinnonOsa("puheenymmartaminen", "3", arviointipäivä),
+                  tutkinnonOsa("puhuminen", "10", arviointipäivä),
+                ))
+              )
+            )
+          )
+
+          postOpiskeluoikeus(invalidOo) {
+            verifyResponseStatus(400, KoskiErrorCategory.badRequest.validation.arviointi.epäsopivaArvosana (
+              s"Suoritus ykisuorituksenosa/tekstinymmartaminen sisältää virheellisen arvosanan alle1 (sallitut koodiarvot ovat: alle3, 3, 4, 9, 10, 11)")
+            )
+          }
+        }
+
+        "Älä salli ylemmälle tasolle muita arvosanoja kuin alle 5, 5 ja 6" in {
+          val invalidOo = opiskeluoikeus.copy(
+            suoritukset = List(
+              ExamplesKielitutkinto.YleisetKielitutkinnot.päätasonSuoritus(
+                tutkintotaso = "yt",
+                kieli = "FI",
+                arviointipäivä = arviointipäivä
+              ).copy(
+                osasuoritukset = Some(List(
+                  tutkinnonOsa("tekstinymmartaminen", "alle3", arviointipäivä),
+                  tutkinnonOsa("kirjoittaminen", "alle5", arviointipäivä),
+                  tutkinnonOsa("puheenymmartaminen", "5", arviointipäivä),
+                  tutkinnonOsa("puhuminen", "11", arviointipäivä),
+                ))
+              )
+            )
+          )
+
+          postOpiskeluoikeus(invalidOo) {
+            verifyResponseStatus(400, KoskiErrorCategory.badRequest.validation.arviointi.epäsopivaArvosana (
+              s"Suoritus ykisuorituksenosa/tekstinymmartaminen sisältää virheellisen arvosanan alle3 (sallitut koodiarvot ovat: alle5, 5, 6, 9, 10, 11)")
+            )
+          }
+        }
+      }
+    }
+
     "Valtionhallinnon kielitutkinto" - {
       val arviointipäivä = LocalDate.of(2025, 1, 15)
       val opiskeluoikeus = ExamplesKielitutkinto.ValtionhallinnonKielitutkinnot.Opiskeluoikeus.valmis(
