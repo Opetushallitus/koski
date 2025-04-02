a376c9316ec0127a9b26181b4978246f1f0bd8b9
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2025-04-02 12:12:47 +0300

    Refaktoroi: Splittaa metodi uudelleenkäytettäväksi

--- src/main/scala/fi/oph/koski/valpas/oppija/ValpasOppijanumerorekisteriService.scala
+++ src/main/scala/fi/oph/koski/valpas/oppija/ValpasOppijanumerorekisteriService.scala
@@ -80,24 +80,35 @@ class ValpasOppijanumerorekisteriService(application: KoskiApplication) {
     onMaksuttomuuskäyttäjälleNäkyväVainOnrssäOlevaOppija(henkilö)
 
   def onMaksuttomuuskäyttäjälleNäkyväVainOnrssäOlevaOppija(henkilö: OppijaHenkilö): Boolean = {
+    lazy val näytäKotikunnanPerusteella = onKotikunnanPerusteellaLaajennetunOppivelvollisuudenPiirissä(henkilö)
+
+    onMaksuttomuuskäyttäjänHenkilöhaussaNäkyväVainOnrssäOlevaOppija(henkilö) &&
+      näytäKotikunnanPerusteella
+  }
+
+  def onMaksuttomuuskäyttäjänHenkilöhaussaNäkyväVainOnrssäOlevaOppija(henkilö: OppijaHenkilö): Boolean = {
     val onMahdollisestiLainPiirissä =
       MaksuttomuusValidation.eiOppivelvollisuudenLaajentamislainPiirissäSyyt(
         henkilö.syntymäaika,
         opiskeluoikeusRepository.getPerusopetuksenAikavälitIlmanKäyttöoikeustarkistusta(None, henkilö.oid),
         rajapäivätService
       ).isEmpty
-    lazy val onTarpeeksiVanhaKeskeytysmerkintöjäVarten = rajapäivätService.oppijaOnTarpeeksiVanhaKeskeytysmerkintöjäVarten(henkilö.syntymäaika)
-    lazy val näytäKotikunnanPerusteella = onKotikunnanPerusteellaLaajennetunOppivelvollisuudenPiirissä(henkilö)
-    // Toistaiseksi vain hetullisilla voi olla kotikunta, mutta tämä saattaa tulevaisuudessa muuttua, joten varmistetaan,
-    // että henkilöllä on myös hetu
+    lazy val onTarpeeksiVanhaKeskeytysmerkintöjäVarten = onTarpeeksiVanhaOllakseenLainPiirissä(henkilö)
     lazy val näytäHetunOlemassaolonPerusteella = henkilö.hetu.isDefined
-    lazy val maksuttomuudenPäättymispäiväTulevaisuudessa = !rajapäivätService.tarkastelupäivä.isAfter(rajapäivätService.maksuttomuusVoimassaAstiIänPerusteella(henkilö.syntymäaika.get))
+    lazy val maksuttomuudenPäättymispäiväTulevaisuudessa = maksuttomuudenPäättymispäiväOnTulevaisuudessa(henkilö)
 
     onMahdollisestiLainPiirissä &&
       onTarpeeksiVanhaKeskeytysmerkintöjäVarten &&
-      näytäKotikunnanPerusteella &&
       näytäHetunOlemassaolonPerusteella &&
       maksuttomuudenPäättymispäiväTulevaisuudessa
+  }
+
+  private def onTarpeeksiVanhaOllakseenLainPiirissä(henkilö: OppijaHenkilö): Boolean = {
+    rajapäivätService.oppijaOnTarpeeksiVanhaKeskeytysmerkintöjäVarten(henkilö.syntymäaika)
+  }
+
+  private def maksuttomuudenPäättymispäiväOnTulevaisuudessa(henkilö: OppijaHenkilö) = {
+    !rajapäivätService.tarkastelupäivä.isAfter(rajapäivätService.maksuttomuusVoimassaAstiIänPerusteella(henkilö.syntymäaika.get))
   }
 
   private def onKansalaiselleNäkyväVainOnrssäOlevaOppija(henkilö: OppijaHenkilö): Boolean = {
