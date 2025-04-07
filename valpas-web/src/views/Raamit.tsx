a3b6f59e19ff7583b468224be6356b401ab2bd4c
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2025-04-07 15:08:07 +0300

    Prettier test

--- valpas-web/src/views/Raamit.tsx
+++ valpas-web/src/views/Raamit.tsx
@@ -62,3 +62,5 @@ const loadExternalVirkailijaRaamitScript = loadExternalRaamitScript(
 const loadExternalOppijaRaamitScript = loadExternalRaamitScript(
   "/oppija-raamit/js/apply-raamit.js",
 )
+
+
