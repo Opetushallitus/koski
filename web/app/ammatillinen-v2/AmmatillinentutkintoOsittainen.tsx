5b866b0face723170e787ee4c1a88ce1f5fbbc92
Axel Wikström <axel.wikstrom@reaktor.com>
2025-04-23 15:57:32 +0300

    WIP lisätietokomponentin alkua jne

---
+++ web/app/ammatillinen-v2/AmmatillinentutkintoOsittainen.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { AdaptedOpiskeluoikeusEditorProps } from '../components-v2/interoperability/useUiAdapter'
+import { AmmatillinenOpiskeluoikeus } from '../types/fi/oph/koski/schema/AmmatillinenOpiskeluoikeus'
+import {
+  EditorContainer,
+  usePäätasonSuoritus
+} from '../components-v2/containers/EditorContainer'
+import { getValue, useForm } from '../components-v2/forms/FormModel'
+import { useSchema } from '../appstate/constraints'
+import { UusiOpiskeluoikeusjakso } from '../components-v2/opiskeluoikeus/UusiOpiskeluoikeudenTilaModal'
+import { AmmatillinenOpiskeluoikeusjakso } from '../types/fi/oph/koski/schema/AmmatillinenOpiskeluoikeusjakso'
+import { useTree } from '../appstate/tree'
+import { AmmatillinenLisatiedot } from './AmmatillinenLisatiedot'
+
+const AmmatillinenTutkintoOsittainenEditor: React.FC<
+  AdaptedOpiskeluoikeusEditorProps<AmmatillinenOpiskeluoikeus>
+> = (props) => {
+  const opiskeluoikeusSchema = useSchema(AmmatillinenOpiskeluoikeus.className)
+  const form = useForm(props.opiskeluoikeus, false, opiskeluoikeusSchema)
+  const [päätasonSuoritus, setPäätasonSuoritus] = usePäätasonSuoritus(form)
+
+  const createAmmatillinenOpiskeluoikeusJakso = (
+    seed: UusiOpiskeluoikeusjakso<AmmatillinenOpiskeluoikeusjakso>
+  ) => AmmatillinenOpiskeluoikeusjakso(seed)
+
+  const { TreeNode, ...tree } = useTree()
+
+
+
+  return (
+    <TreeNode>
+      <EditorContainer
+        form={form}
+        oppijaOid={props.oppijaOid}
+        invalidatable={props.invalidatable}
+        onChangeSuoritus={setPäätasonSuoritus}
+        testId={päätasonSuoritus.testId}
+        createOpiskeluoikeusjakso={createAmmatillinenOpiskeluoikeusJakso}
+        lisätiedotContainer={AmmatillinenLisatiedot}
+      />
+    </TreeNode>
+  )
+}
+
+export default AmmatillinenTutkintoOsittainenEditor
