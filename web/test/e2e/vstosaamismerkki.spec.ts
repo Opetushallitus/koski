5c7f9c132f354a8448a24a50880c3848375240ce
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-04-30 09:53:19 +0300

    Korjaus vst-osaamismerkkitestiin

--- web/test/e2e/vstosaamismerkki.spec.ts
+++ web/test/e2e/vstosaamismerkki.spec.ts
@@ -72,7 +72,26 @@ test.describe('Vapaan sivistyön VST osaamismerkki', () => {
     await search.clickOnFirst()
 
     // Varmista versiohistoriaa käyttämällä, että versio 2 tuli luotua muokatessa
-    await page.getByTestId('oo.0.opiskeluoikeus.versiohistoria.button').click()
-    await page.getByTestId('oo.0.opiskeluoikeus.versiohistoria.list.2').click()
+    await eventually(
+      () =>
+        page.getByTestId('oo.0.opiskeluoikeus.versiohistoria.button').click(),
+      () =>
+        page
+          .getByTestId('oo.0.opiskeluoikeus.versiohistoria.list.2')
+          .click({ trial: true })
+    )
   })
 })
+
+async function eventually(initialize: () => void, trial: () => void) {
+  let retriesLeft = 3
+  while (retriesLeft > 0) {
+    await initialize()
+    try {
+      await trial()
+      return
+    } catch {
+      retriesLeft--
+    }
+  }
+}
