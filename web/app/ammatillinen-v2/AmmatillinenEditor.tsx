5ec68bd5049f9a056241b4c6a96cbc46b843b905
Axel Wikström <axel.wikstrom@reaktor.com>
2025-05-05 17:02:28 +0300

    WIP Lisää päätason suorituksen kenttiä

--- web/app/ammatillinen-v2/AmmatillinenEditor.tsx
+++ web/app/ammatillinen-v2/AmmatillinenEditor.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AdaptedOpiskeluoikeusEditorProps } from '../components-v2/interoperability/useUiAdapter'
 import { AmmatillinenOpiskeluoikeus } from '../types/fi/oph/koski/schema/AmmatillinenOpiskeluoikeus'
 import {
+  ActivePäätasonSuoritus,
   EditorContainer,
   usePäätasonSuoritus
 } from '../components-v2/containers/EditorContainer'
@@ -12,6 +13,22 @@ import { AmmatillinenOpiskeluoikeusjakso } from '../types/fi/oph/koski/schema/Am
 import { AmmatillinenLisatiedot } from './AmmatillinenLisatiedot'
 import { VirkailijaKansalainenContainer } from '../components-v2/containers/VirkailijaKansalainenContainer'
 import { t } from '../i18n/i18n'
+import {
+  KeyValueRow,
+  KeyValueTable
+} from '../components-v2/containers/KeyValueTable'
+import { TestIdText } from '../appstate/useTestId'
+import { FormField } from '../components-v2/forms/FormField'
+import {
+  OrganisaatioEdit,
+  OrganisaatioView
+} from '../components-v2/opiskeluoikeus/OrganisaatioField'
+import {
+  KoodistoEdit,
+  KoodistoView
+} from '../components-v2/opiskeluoikeus/KoodistoField'
+import { Spacer } from '../components-v2/layout/Spacer'
+import { SuorituksenVahvistusField } from '../components-v2/opiskeluoikeus/SuorituksenVahvistus'
 
 export type AmmatillinenEditorProps =
   AdaptedOpiskeluoikeusEditorProps<AmmatillinenOpiskeluoikeus>
@@ -46,6 +63,43 @@ const AmmatillinenPäätasonSuoritusEditor: React.FC<
   return <AmmatillinenTutkintoOsittainenEditor {...props} />
 }
 
+const AmmatillisPääsuorituksenTiedot: React.FC<{
+  form: FormModel<AmmatillinenOpiskeluoikeus>
+  päätasonSuoritus: ActivePäätasonSuoritus<AmmatillinenOpiskeluoikeus>
+}> = ({ form, päätasonSuoritus }) => {
+  const path = päätasonSuoritus.path
+
+  return (
+    <KeyValueTable>
+      <KeyValueRow localizableLabel="Koulutus">
+        <TestIdText id="koulutus">
+          {/*TODO Koulutusmoduuli editor perustelinkillä*/}
+          {t(päätasonSuoritus.suoritus.koulutusmoduuli.tunniste.nimi)}
+        </TestIdText>
+      </KeyValueRow>
+      <KeyValueRow localizableLabel="Oppilaitos / toimipiste">
+        <FormField
+          form={form}
+          path={path.prop('toimipiste')}
+          view={OrganisaatioView}
+          edit={OrganisaatioEdit}
+        />
+      </KeyValueRow>
+      <KeyValueRow localizableLabel="Suorituskieli">
+        <FormField
+          form={form}
+          path={path.prop('suorituskieli')}
+          view={KoodistoView}
+          edit={KoodistoEdit}
+          editProps={{ koodistoUri: 'kieli' }}
+          testId="suorituskieli"
+        />
+      </KeyValueRow>
+      {/*TODO lisää rivejä tietomallissa?*/}
+    </KeyValueTable>
+  )
+}
+
 const AmmatillinenTutkintoOsittainenEditor: React.FC<
   AmmatillinenEditorProps & {
     form: FormModel<AmmatillinenOpiskeluoikeus>
@@ -54,6 +108,10 @@ const AmmatillinenTutkintoOsittainenEditor: React.FC<
   const [päätasonSuoritus, setPäätasonSuoritus] = usePäätasonSuoritus(
     props.form
   )
+
+  const organisaatio =
+    props.opiskeluoikeus.oppilaitos || props.opiskeluoikeus.koulutustoimija
+
   const createAmmatillinenOpiskeluoikeusJakso = (
     seed: UusiOpiskeluoikeusjakso<AmmatillinenOpiskeluoikeusjakso>
   ) => AmmatillinenOpiskeluoikeusjakso(seed)
@@ -67,7 +125,21 @@ const AmmatillinenTutkintoOsittainenEditor: React.FC<
       testId={päätasonSuoritus.testId}
       createOpiskeluoikeusjakso={createAmmatillinenOpiskeluoikeusJakso}
       lisätiedotContainer={AmmatillinenLisatiedot}
-    />
+    >
+      <AmmatillisPääsuorituksenTiedot
+        form={props.form}
+        päätasonSuoritus={päätasonSuoritus}
+      />
+
+      <Spacer />
+
+      <SuorituksenVahvistusField
+        form={props.form}
+        suoritusPath={päätasonSuoritus.path}
+        organisaatio={organisaatio}
+        disableAdd={false /*TODO?*/}
+      />
+    </EditorContainer>
   )
 }
 
