6843e54796ae77c31bebb592810cdbff5f4a79b7
Axel Wikström <axel.wikstrom@reaktor.com>
2025-05-22 15:58:09 +0300

    Enemmän tilaa taulukon otsikoille

--- web/app/components-v2/containers/KeyValueTable.tsx
+++ web/app/components-v2/containers/KeyValueTable.tsx
@@ -35,7 +35,7 @@ export const KeyValueRow = (props: KeyValueRowProps) => {
     ? { default: 8, small: 12, phone: 16 }
     : props.largeLabel
       ? { default: 8, small: 12, phone: 12 }
-      : { default: 4, small: 8, phone: 12 }
+      : { default: 6, small: 8, phone: 12 }
   const valueSpans = {
     default: 24 - nameSpans.default - indent,
     small: 24 - nameSpans.small - indent,
