76e2484f46472d6b8bc160e99ee347f51af86c4d
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2025-06-04 10:21:19 +0300

    Korjaa threadcount

--- src/main/scala/fi/oph/koski/executors/NamedForkJoinPoolExecutor.scala
+++ src/main/scala/fi/oph/koski/executors/NamedForkJoinPoolExecutor.scala
@@ -1,11 +1,17 @@
 package fi.oph.koski.executors
 
+import fi.oph.koski.log.Logging
+
 import java.util.concurrent.ForkJoinPool
 
-object NamedForkJoinPoolExecutor {
+object NamedForkJoinPoolExecutor extends Logging {
   def apply(name: String): ForkJoinPool = {
+    val jdkProcessorCount = Runtime.getRuntime.availableProcessors
+    val threadCount = Integer.max(jdkProcessorCount, 4)
+    logger.info(s"JDK Processor count: ${jdkProcessorCount}, creating fork join pool of ${threadCount} threads")
+
     val executor = new ForkJoinPool(
-      Runtime.getRuntime.availableProcessors,
+      threadCount,
       ForkJoinPool.defaultForkJoinWorkerThreadFactory, null, true
     )
     ManagedForkJoinPoolExecutor.register(name, executor)
