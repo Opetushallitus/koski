a33ed391357ad291fa82a5725fb870c909e38753
Juho Tammela <juho.tammela@itrex.fi>
2025-10-09 17:58:42 +0300

    Tyylit: paranna preib2019 suoritustietojen luettavuutta kälissä

--- web/app/components-v2/layout/Spacer.tsx
+++ web/app/components-v2/layout/Spacer.tsx
@@ -1,3 +1,13 @@
 import React from 'react'
 
 export const Spacer = () => <div style={{ height: 24 }} />
+
+export const SpacerLine = () => (
+  <div
+    style={{
+      height: '7px',
+      marginBottom: '6px',
+      borderBottom: 'solid lightgray 1px'
+    }}
+  />
+)
