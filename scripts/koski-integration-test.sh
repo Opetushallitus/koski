2c08a6829544314dd61a58fac7b1586f41b5747c
Henri Ruoho <henri.ruoho@reaktor.com>
2025-10-30 21:49:36 +0200

    ci: update GitHub Actions and test scripts for pnpm

--- scripts/koski-integration-test.sh
+++ scripts/koski-integration-test.sh
@@ -3,4 +3,4 @@ set -eou pipefail
 echo "Running Koski Playwright tests, shard $2 of $3, using backend on $1"
 cd $(dirname "$0")/../web
 
-BACKEND_HOST="$1" npm run playwright:test -- --shard=$2/$3
+BACKEND_HOST="$1" pnpm run playwright:test --shard=$2/$3
