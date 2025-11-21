93676a05bd12c51d91ce1d08a85d18ce51e19d18
Axel Wikström <axel.wikstrom@reaktor.com>
2025-11-21 15:09:54 +0200

    fix XMLSpec

--- src/test/scala/fi/oph/koski/util/XMLSpec.scala
+++ src/test/scala/fi/oph/koski/util/XMLSpec.scala
@@ -31,7 +31,7 @@ class XMLSpec extends AnyFreeSpec with TestEnvironment with Matchers {
 
     val transformed = XML.transform(node) {
       case e: Elem if e.label == "leaf1" => NodeSeq.Empty
-      case e: Elem if e.label == "leaf2" => e.copy(child = e.child.flatMap(n => Text(n.text.toUpperCase)))
+      case e: Elem if e.label == "leaf2" => XML.copyElem(e, e.child.flatMap(n => Text(n.text.toUpperCase)))
     }
 
     transformed should equal(<top><subtree><leaf2>B</leaf2><leaf3>c</leaf3></subtree></top>)
