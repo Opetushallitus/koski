accaee17c995abc99b56cc39b515aa32eb5f13bf
Henri Ruoho <henri.ruoho@reaktor.com>
2026-01-12 16:28:13 +0200

    Lisää Java sample E2E-testeihin
    Päivitetään E2E-testiskripti käynnistämään Java-esimerkkisovellus
    testien ajaksi.

--- scripts/omadata-oauth2-e2e-test.sh
+++ scripts/omadata-oauth2-e2e-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eou pipefail
 echo "Running OmaDataOAuth2 Sample Playwright tests, shard $2 of $3, using backend on port $1"
-cd $(dirname "$0")/../omadata-oauth2-sample/client
+cd $(dirname "$0")/../omadata-oauth2-sample/server
 
 KOSKI_BACKEND_PORT="$1" pnpm run playwright:test --shard=$2/$3
