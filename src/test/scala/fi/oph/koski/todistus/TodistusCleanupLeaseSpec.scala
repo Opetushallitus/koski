c28abcf1ae8a79957568379ee452673bd57bd41e
Henri Ruoho <henri.ruoho@reaktor.com>
2026-02-20 12:03:09 +0200

    Pause todistusCleanupScheduler during test

--- src/test/scala/fi/oph/koski/todistus/TodistusCleanupLeaseSpec.scala
+++ src/test/scala/fi/oph/koski/todistus/TodistusCleanupLeaseSpec.scala
@@ -4,16 +4,26 @@ import fi.oph.koski.KoskiApplicationForTests
 import fi.oph.koski.db.PostgresDriverWithJsonSupport.plainAPI._
 import fi.oph.koski.db.QueryMethods
 import fi.oph.koski.TestEnvironment
-import org.scalatest.BeforeAndAfterEach
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.time.{Duration, LocalDateTime}
 import java.util.UUID
 
-class TodistusCleanupLeaseSpec extends AnyFreeSpec with TestEnvironment with Matchers with BeforeAndAfterEach {
+class TodistusCleanupLeaseSpec extends AnyFreeSpec with TestEnvironment with Matchers with BeforeAndAfterEach with BeforeAndAfterAll {
   private val app = KoskiApplicationForTests
   private val db = app.masterDatabase.db
+
+  override protected def beforeAll(): Unit = {
+    super.beforeAll()
+    app.todistusCleanupScheduler.pause(Duration.ofMinutes(5))
+  }
+
+  override protected def afterAll(): Unit = {
+    app.todistusCleanupScheduler.resume()
+    super.afterAll()
+  }
 
   override protected def beforeEach(): Unit = {
     QueryMethods.runDbSync(db, sql"TRUNCATE TABLE todistus_job".asUpdate)
