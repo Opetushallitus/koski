8d8634352695daf25226dc1bb162d2f77a185d0f
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2026-03-11 10:29:01 +0200

    Poista TODO-kommentteja;
    Suorituskyky on nykyisellään suorituskykytestien perusteella tarpeeksi hyvä, joten näitä
    asioita ei tarvitse toteuttaa.

--- src/main/scala/fi/oph/koski/todistus/pdfgenerator/TodistusPdfGenerator.scala
+++ src/main/scala/fi/oph/koski/todistus/pdfgenerator/TodistusPdfGenerator.scala
@@ -51,7 +51,6 @@ class TodistusPdfGenerator extends Logging {
     templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name())
     templateResolver.setCacheable(true)
 
-    // TODO: TOR-2400: Varmista, että templatet cachetetaan? Vaatii ehkä jonkun option?
     val engine = new TemplateEngine()
     engine.setTemplateResolver(templateResolver)
     engine
@@ -86,24 +85,15 @@ class TodistusPdfGenerator extends Logging {
   }
 
   private def convertHtmlToPdf(html: String, metadata: Map[String, String], outputStream: OutputStream): Unit = {
-    // TODO: TOR-2400: Älä tee builderiä aina uudestaan, ja lisää cache?
     val builder = new PdfRendererBuilder()
     builder.useDefaultPageSize(210.0f, 297.0f, BaseRendererBuilder.PageSizeUnits.MM)
-    builder.usePdfUaAccessibility(true) // TODO: TOR-2400: riittääkö tämä, vai tarvitaanko muitakin optioita?
+    builder.usePdfUaAccessibility(true)
     builder.useSVGDrawer(new BatikSVGDrawer())
 
     builder.withHtmlContent(html, null)
 
     // Lisää producer-tieto
     builder.withProducer(s"Koski (commit: ${metadata("CommitHash")})")
-
-    // TODO: TOR-2400: Fontit ovat nyt base64:nä HTML:ssä, jotta täsmälleen saman koodin saa toimimaan JAR:in sisältä ja tiedostojärjestelmästä, kun
-    // haluaa rendata esim. Chromella. Lisää nämä ja poista @font-face:t HTML:stä, jos haluat käyttää näitä. Tämä lähinnä helpottaisi HTML-tiedostojen
-    // editointia, mutta myös muistinkäyttö olisi tehokkaampaa, jos fontteja ei olisi erikseen muistissa jokaisella templatella. Jotta fontit saisi
-    // pysymään muisti-cachessa, se pitäisi rakentaa itse byte-array -datalla: openhtmltopdf lukee fontit uudestaan aina pdf:ää tehdessään.
-//    registerFont(builder, "fonts/OpenSans-Regular.ttf", 400)
-//    registerFont(builder, "fonts/OpenSans-SemiBold.ttf", 600)
-//    registerFont(builder, "fonts/OpenSans-Bold.ttf", 700)
 
     builder.toStream(outputStream)
 
