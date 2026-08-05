cdf7d48f530b692d8d91cb9f84c4d584f655e8a4
Juho Tammela <juho.tammela@itrex.fi>
2026-08-05 17:11:41 +0300

    Korjaa inkrementaalisten johdettujen taulujen update-lause

--- src/main/scala/fi/oph/koski/raportointikanta/OpiskeluoikeudenUlkopuolellaArvioidutOsasuoritukset.scala
+++ src/main/scala/fi/oph/koski/raportointikanta/OpiskeluoikeudenUlkopuolellaArvioidutOsasuoritukset.scala
@@ -1,31 +1,36 @@
 package fi.oph.koski.raportointikanta
 
 import fi.oph.koski.db.PostgresDriverWithJsonSupport.plainAPI._
+import fi.oph.koski.db.SQLHelpers
 import slick.dbio.DBIO
+import slick.jdbc.SQLActionBuilder
 
 object OpiskeluoikeudenUlkopuolellaArvioidutOsasuoritukset extends OpiskeluoikeusPrecomputedTable {
 
   val precomputedTableName = "osasuoritus_arvioitu_opiskeluoikeuden_ulkopuolella"
 
-  protected def precomputedTableSelectSql(schemaName: String): String =
-    s"""
-      select
-        opiskeluoikeus.opiskeluoikeus_oid,
-        opiskeluoikeus.oppilaitos_oid,
-        osasuoritus.osasuoritus_id,
-        osasuoritus.arviointi_paiva osasuorituksen_arviointi_paiva,
-        osasuoritus.suorituksen_tyyppi osasuorituksen_tyyppi,
-        paatason_suoritus.suorituksen_tyyppi paatason_suorituksen_tyyppi,
-        paatason_suoritus.oppimaara_koodiarvo
-      from $schemaName.r_opiskeluoikeus opiskeluoikeus
-        join $schemaName.r_osasuoritus osasuoritus on osasuoritus.opiskeluoikeus_oid = opiskeluoikeus.opiskeluoikeus_oid
-          and (
+  protected def precomputedTableSelectSql(schemaName: String, opiskeluoikeusRajaus: SQLActionBuilder): SQLActionBuilder =
+    SQLHelpers.concat(
+      sql"""
+        select
+          opiskeluoikeus.opiskeluoikeus_oid,
+          opiskeluoikeus.oppilaitos_oid,
+          osasuoritus.osasuoritus_id,
+          osasuoritus.arviointi_paiva osasuorituksen_arviointi_paiva,
+          osasuoritus.suorituksen_tyyppi osasuorituksen_tyyppi,
+          paatason_suoritus.suorituksen_tyyppi paatason_suorituksen_tyyppi,
+          paatason_suoritus.oppimaara_koodiarvo
+        from #$schemaName.r_opiskeluoikeus opiskeluoikeus
+          join #$schemaName.r_osasuoritus osasuoritus on osasuoritus.opiskeluoikeus_oid = opiskeluoikeus.opiskeluoikeus_oid
+          join #$schemaName.r_paatason_suoritus paatason_suoritus on paatason_suoritus.paatason_suoritus_id = osasuoritus.paatason_suoritus_id
+        where (
             osasuoritus.arviointi_paiva < opiskeluoikeus.alkamispaiva
             or
            (osasuoritus.arviointi_paiva > coalesce(opiskeluoikeus.paattymispaiva, '9999-12-31') and viimeisin_tila = 'valmistunut')
           )
-        join $schemaName.r_paatason_suoritus paatason_suoritus on paatason_suoritus.paatason_suoritus_id = osasuoritus.paatason_suoritus_id
-    """
+      """,
+      opiskeluoikeusRajaus
+    )
 
   def createIndex(s: Schema): DBIO[Unit] =
     DBIO.seq(
--- src/main/scala/fi/oph/koski/raportointikanta/OpiskeluoikeusPrecomputedTable.scala
+++ src/main/scala/fi/oph/koski/raportointikanta/OpiskeluoikeusPrecomputedTable.scala
@@ -1,28 +1,32 @@
 package fi.oph.koski.raportointikanta
 
 import fi.oph.koski.db.PostgresDriverWithJsonSupport.plainAPI._
+import fi.oph.koski.db.SQLHelpers
 import fi.oph.koski.raportit.lukio.lops2021.{Lukio2019OppiaineEriVuonnaKorotetutOpintopisteet, Lukio2019OppiaineRahoitusmuodonMukaan}
 import fi.oph.koski.raportit.lukio.{LukioOppiaineEriVuonnaKorotetutKurssit, LukioOppiaineRahoitusmuodonMukaan}
 import slick.dbio.DBIO
+import slick.jdbc.SQLActionBuilder
 
 trait OpiskeluoikeusPrecomputedTable {
   def precomputedTableName: String
 
-  protected def precomputedTableSelectSql(schemaName: String): String
+  protected def precomputedTableSelectSql(schemaName: String, opiskeluoikeusRajaus: SQLActionBuilder): SQLActionBuilder
 
   def createPrecomputedTable(s: Schema): DBIO[Int] =
-    sqlu"create table #${s.name}.#$precomputedTableName as #${precomputedTableSelectSql(s.name)}"
+    SQLHelpers.concat(
+      sql"create table #${s.name}.#$precomputedTableName as ",
+      precomputedTableSelectSql(s.name, sql"")
+    ).asUpdate
 
   def createIndex(s: Schema): DBIO[Unit]
 
   def updatePrecomputedTable(s: Schema, opiskeluoikeusOids: Seq[String]): DBIO[Unit] =
     DBIO.seq(
       sqlu"delete from #${s.name}.#$precomputedTableName where opiskeluoikeus_oid = any($opiskeluoikeusOids)",
-      sqlu"""
-        insert into #${s.name}.#$precomputedTableName
-        select * from ( #${precomputedTableSelectSql(s.name)} ) precomputed
-        where precomputed.opiskeluoikeus_oid = any($opiskeluoikeusOids)
-      """
+      SQLHelpers.concat(
+        sql"insert into #${s.name}.#$precomputedTableName ",
+        precomputedTableSelectSql(s.name, sql" and opiskeluoikeus.opiskeluoikeus_oid = any($opiskeluoikeusOids)")
+      ).asUpdate
     )
 }
 
