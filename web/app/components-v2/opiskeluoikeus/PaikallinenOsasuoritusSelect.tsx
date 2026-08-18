183e44010a8e44eceb5b9b6742ed59c49f3e82f1
Axel Wikström <axel.wikstrom@reaktor.com>
2026-08-18 11:50:36 +0300

    TOR-2596: Poista paikallisen osasuorituksen turha skeemahaku
    Paikallisen osasuorituksen nimi tallennetaan PaikallinenKoodi-rakenteen koodiarvoksi ja lokalisoiduksi nimeksi. Rakenteen palvelimelta ladattu skeema ei rajaa arvoja koodistolla tai muilla sallittujen arvojen ehdoilla, vaan tarkistaa käytännössä vain että pakolliset merkkijonot eivät ole tyhjiä.
    
    Poistetaan siksi modaalia avattaessa tehty PaikallinenKoodi-skeemahaku ja sitä varten lisätty latausnäkymä. Lomake avautuu heti ilman myöhäistä skeemapäivitystä, ja Lisää-painikkeen tila päätellään suoraan koodiarvon tyhjyydestä. Tämä poistaa sekä turhan verkkoriippuvuuden että skeeman saapumisen aiheuttaneen renderöintikilvan.
    
    Päivitetään e2e-testi katkaisemaan mahdollinen PaikallinenKoodi-skeemapyyntö. Testi varmistaa, että kenttä on heti käytettävissä, Lisää-painike aktivoituu yhdellä fill-kutsulla ja osasuoritus lisätään ilman skeemahakua.
    
    Tarkistettu Prettierillä, ESLintillä ja TypeScript-tyyppitarkistuksella. Tuotantobundle rakentuu, taiteen perusopetuksen kohdistetut testit menevät läpi ja jaettua komponenttia käyttävä VST:n uuden osasuorituksen testi menee läpi.

--- web/app/components-v2/opiskeluoikeus/PaikallinenOsasuoritusSelect.tsx
+++ web/app/components-v2/opiskeluoikeus/PaikallinenOsasuoritusSelect.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useMemo, useState } from 'react'
-import { useSchema } from '../../appstate/constraints'
 import { localize, t } from '../../i18n/i18n'
 import { LocalizedString } from '../../types/fi/oph/koski/schema/LocalizedString'
 import { PaikallinenKoodi } from '../../types/fi/oph/koski/schema/PaikallinenKoodi'
@@ -117,8 +116,7 @@ export type UusiOsasuoritusModalProps = CommonProps<{
 export const UusiOsasuoritusModal: React.FC<UusiOsasuoritusModalProps> = (
   props
 ) => {
-  const paikallinenKoodiSchema = useSchema('PaikallinenKoodi')
-  const form = useForm(emptyPaikallinenKoodi, true, paikallinenKoodiSchema)
+  const form = useForm(emptyPaikallinenKoodi, true, null)
   const koodiarvoPath = form.root.prop('koodiarvo')
 
   const { onSubmit } = props
@@ -160,7 +158,7 @@ export const UusiOsasuoritusModal: React.FC<UusiOsasuoritusModalProps> = (
           {t('Peruuta')}
         </FlatButton>
         <RaisedButton
-          disabled={!form.isValid}
+          disabled={form.state.koodiarvo.length === 0}
           onClick={onSubmitCB}
           testId="submit"
         >
