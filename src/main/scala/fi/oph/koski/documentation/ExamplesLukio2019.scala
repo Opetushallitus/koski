ccd28f1e4ce83a3bc0d03b41fd95f1b5c4065c2d
panu.eronen <panu.eronen@gmail.com>
2026-09-22 13:55:11 +0300

    revertoi oppija

--- src/main/scala/fi/oph/koski/documentation/ExamplesLukio2019.scala
+++ src/main/scala/fi/oph/koski/documentation/ExamplesLukio2019.scala
@@ -113,7 +113,7 @@ object ExamplesLukio2019 {
       tila = LukionOpiskeluoikeudenTila(
         List(
           LukionOpiskeluoikeusjakso(alku = date(2019, 8, 1), tila = opiskeluoikeusAktiivinen, opintojenRahoitus = Some(ExampleData.valtionosuusRahoitteinen)),
-          LukionOpiskeluoikeusjakso(alku = date(2021, 9, 5), tila = opiskeluoikeusPäättynyt, opintojenRahoitus = Some(ExampleData.valtionosuusRahoitteinen))
+          LukionOpiskeluoikeusjakso(alku = date(2021, 8, 1), tila = opiskeluoikeusPäättynyt, opintojenRahoitus = Some(ExampleData.valtionosuusRahoitteinen))
         )
       ),
       oppilaitos = Some(jyväskylänNormaalikoulu),
