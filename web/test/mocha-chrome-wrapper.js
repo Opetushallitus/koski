f96a82456fefe8b46817c69371314ad5f6fb5b5a
Juha Helminen <juha.helminen@reaktor.com>
2026-09-24 12:29:33 +0300

    Korvaa mocha-chrome-forkki kokonaan Puppeteerilla
    Mocha-selaintestit kaatuivat satunnaisesti CI:llä, kun Chromen
    debug-portti ei vastannut mocha-chromen 10 sekunnin odotusikkunassa.
    Noin puolet shardeista tarvitsi 1–2 käynnistyksen uudelleenyritystä, ja
    kun yrityksiä kertyi enemmän, ajo saattoi kaatua "socket hang up"
    -virheeseen ennen yhtään testiä.
    
    Testit ajetaan nyt Puppeteerin omalla Chrome for Testingillä, jonka
    test-skripti.
    
    Puppeteer myös käynnistää selaimen. launch() antaa käyttöjärjestelmän
    valita vapaan debug-portin, odottaa käynnistystä oletuksena 30 sekuntia
    ja sulkee Chromen, jos käynnistys epäonnistuu. Omaa
    uudelleenyrityslogiikkaa ei ole. Testien tuloste tulee Mochan omalta
    spec-reporterilta.
    
    Käytöksen muutokset:
    - Exit-koodi on 0 tai 1 eikä epäonnistumisten määrä.
    - Jos Mocha ei käynnisty 60 sekunnissa, ajo kaatuu eikä jää jumiin.
    - Epäonnistuneet testit näkyvät GitHub Actionsissa annotaatioina.
    
    Myös suora chrome-launcher-riippuvuus poistuu, koska se oli lisätty vain
    mocha-chromen tarpeisiin.

--- web/test/mocha-chrome-wrapper.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-process.env.CHROME_PATH = require('puppeteer').executablePath()
-process.on('unhandledRejection', (reason) => {
-  console.error(reason)
-  process.exit(1)
-})
-
-require('mocha-chrome/cli.js')
